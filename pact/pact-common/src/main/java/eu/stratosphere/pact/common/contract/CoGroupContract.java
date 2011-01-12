ce0325c3d9e456265ae78effa23d82e6f378909b
Fabian Hueske <fabian.hueske@tu-berlin.de>
2011-01-12 16:23:50 +0100

    - Added JavaDoc to pact-common contracts - Improve JavaDoc in pact-common

--- pact/pact-common/src/main/java/eu/stratosphere/pact/common/contract/CoGroupContract.java
+++ pact/pact-common/src/main/java/eu/stratosphere/pact/common/contract/CoGroupContract.java
@@ -20,19 +20,42 @@ import eu.stratosphere.pact.common.type.Key;
 import eu.stratosphere.pact.common.type.Value;
 
 /**
- * @TODO
- * @author DIMA
+ * CoGroupContract represents a CoGroup InputContract of the PACT Programming Model.
+ * InputContracts are second-order functions. 
+ * They have one or multiple input sets of key/value-pairs and a first-order user function (stub implementation).
+ * <p> 
+ * CoGroup works on two inputs and calls the first-order user function of a {@see eu.stratosphere.pact.common.stub.CoGroupStub} 
+ * with the groups of key/value-pairs sharing the same key (one group per input) independently.
+ * 
+ * @see eu.stratosphere.pact.common.stub.CoGroupStub
+ * 
+ * @author Erik Nijkamp
+ * @author Fabian Hueske (fabian.hueske@tu-berlin.de)
  */
 public class CoGroupContract<IK extends Key, IV1 extends Value, IV2 extends Value, OK extends Key, OV extends Value>
 		extends DualInputContract<IK, IV1, IK, IV2, OK, OV> {
+	
 	private static String defaultName = "CoGroup #";
 
 	private static int nextID = 1;
 
+	/**
+	 * Creates a CoGroupContract with the provided {@see eu.stratosphere.pact.common.stub.CoGroupStub} implementation 
+	 * and the given name. 
+	 * 
+	 * @param c The {@link CoGroupStub} implementation for this CoGroup InputContract.
+	 * @param n The name of PACT.
+	 */
 	public CoGroupContract(Class<? extends CoGroupStub<IK, IV1, IV2, OK, OV>> c, String n) {
 		super(c, n);
 	}
 
+	/**
+	 * Creates a CoGroupContract with the provided {@see eu.stratosphere.pact.common.stub.CoGroupStub} implementation 
+	 * and a default name.
+	 * 
+	 * @param c The {@link CoGroupStub} implementation for this CoGroup InputContract.
+	 */ 
 	public CoGroupContract(Class<? extends CoGroupStub<IK, IV1, IV2, OK, OV>> c) {
 		super(c, defaultName + (nextID++));
 	}
