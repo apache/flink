bda4f9385210cf981b8c6b4626dba49ca6f68f3b
Matthias Ringwald <matthias.ringwald@campus.tu-berlin.de>
2011-01-14 12:16:41 +0100

    Added JavaDoc for example TPCHQuery3.java

--- pact/pact-examples/src/main/java/eu/stratosphere/pact/example/relational/TPCHQuery3.java
+++ pact/pact-examples/src/main/java/eu/stratosphere/pact/example/relational/TPCHQuery3.java
@@ -40,29 +40,55 @@ import eu.stratosphere.pact.example.relational.util.IntTupleDataInFormat;
 import eu.stratosphere.pact.example.relational.util.StringTupleDataOutFormat;
 import eu.stratosphere.pact.example.relational.util.Tuple;
 
+/**
+ * The TPC-H is a decision support benchmark on relational data.
+ * Its documentation and the data generator (DBGEN) can be found
+ * on http://www.tpc.org/tpch/ .This implementation is tested with
+ * the DB2 data format.  
+ * THe PACT program implements a modified version of the query 3 of 
+ * the TPC-H benchmark including a join, aggregation, filtering and 
+ * projection.
+ * 
+ * SELECT l_orderkey, o_shippriority, sum(l_extendedprice) as revenue
+ * FROM orders, lineitem
+ * WHERE l_orderkey = o_orderkey
+ * AND o_custkey IN [X]
+ * AND o_orderdate > [Y]
+ * GROUP BY l_orderkey, o_shippriority;
+ */
 public class TPCHQuery3 implements PlanAssembler, PlanAssemblerDescription {
 
-	/**
-	 * Implements a modified query 3 of the TPC-H benchmark.
-	 * SELECT l_orderkey, o_shippriority, sum(l_extendedprice) as revenue
-	 * FROM orders, lineitem
-	 * WHERE l_orderkey = o_orderkey
-	 * AND o_custkey IN [X]
-	 * AND o_orderdate > [Y]
-	 * GROUP BY l_orderkey, o_shippriority;
-	 */
 
+	/**
+	 * Concatenation of Integer and String. Used for concatenation of keys
+	 * after join (ORDERKEY, SHIPPRIORITY)
+	 */
 	public static class N_IntStringPair extends PactPair<PactInteger, PactString> {
 
+		/**
+		 * Initializes a blank pair. Required for deserialization
+		 */
 		public N_IntStringPair() {
 			super();
 		}
 
+		/**
+		 * Initializes the concatenation of integer and string.
+		 * 
+		 * @param first Integer value for concatenating
+		 * @param second String value for concatenating
+		 */
 		public N_IntStringPair(PactInteger first, PactString second) {
 			super(first, second);
 		}
 	}
 
+	/**
+	 * Map PACT implements the filter on the orders table. The SameKey
+	 * OutputContract is annotated because the key does not change during
+	 * filtering.
+	 *  
+	 */
 	@SameKey
 	public static class FilterO extends MapStub<PactInteger, Tuple, PactInteger, Tuple> {
 
@@ -70,6 +96,17 @@ public class TPCHQuery3 implements PlanAssembler, PlanAssemblerDescription {
 
 		private final String PRIO_FILTER = "5";
 
+		/**
+		 * Filters the orders table by custKey and orderDate
+		 * TODO
+		 *  o_custkey IN [X] AND o_orderdate > [Y]
+		 *  o_orderstatus = "F" AND year(o_orderdate) = 1993
+		 *  AND o_orderPriority LIKE "5%"
+	 	 *  
+	 	 * Output Schema:
+	 	 *  Key: ORDERKEY
+	 	 *  Value: 0:ORDERKEY, 1:SHIPPRIORITY
+		 */
 		@Override
 		public void map(PactInteger oKey, Tuple value, Collector<PactInteger, Tuple> out) {
 
@@ -81,31 +118,53 @@ public class TPCHQuery3 implements PlanAssembler, PlanAssemblerDescription {
 
 				out.collect(oKey, value);
 
-				// Output Schema:
-				// KEY: ORDERKEY
-				// VALUE: 0:ORDERKEY, 1:SHIPPRIORITY
-
 			}
 		}
 	}
 
+
+	/**
+	 * Map PACT implements the projection on the LineItem table. The SameKey
+	 * OutputContract is annotated because the key does not change during
+	 * projection.
+	 *
+	 */
 	@SameKey
 	public static class ProjectLi extends MapStub<PactInteger, Tuple, PactInteger, Tuple> {
 
+		/**
+		 * Does the projection on the LineItem table 
+		 *
+		 * Output Schema:
+		 *  Key: ORDERKEY
+		 *  Value: 0:ORDERKEY, 1:EXTENDEDPRICE
+		 */
 		@Override
 		public void map(PactInteger oKey, Tuple value, Collector<PactInteger, Tuple> out) {
 			value.project(33);
 			out.collect(oKey, value);
-
-			// Output Schema:
-			// Key: ORDERKEY
-			// Value: 0:ORDERKEY, 1:EXTENDEDPRICE
 		}
 	}
 
+	/**
+	 * Match PACT realizes the join between LineItem and Order table. The 
+	 * SuperKey OutputContract is annotated because the new key is
+	 * built of the keys of the inputs.
+	 *
+	 */
 	@SuperKey
 	public static class JoinLiO extends MatchStub<PactInteger, Tuple, Tuple, N_IntStringPair, Tuple> {
 
+		/**
+		 * Implements the join between LineItem and Order table on the 
+		 * order key.
+		 * 
+		 * WHERE l_orderkey = o_orderkey
+		 * 
+		 * Output Schema:
+		 *  Key: ORDERKEY, SHIPPRIORITY
+		 *  Value: 0:ORDERKEY, 1:SHIPPRIORITY, 2:EXTENDEDPRICE
+		 */
 		@Override
 		public void match(PactInteger oKey, Tuple oVal, Tuple liVal, Collector<N_IntStringPair, Tuple> out) {
 
@@ -115,16 +174,29 @@ public class TPCHQuery3 implements PlanAssembler, PlanAssemblerDescription {
 			N_IntStringPair superKey = new N_IntStringPair(oKey, new PactString(oVal.getStringValueAt(1)));
 
 			out.collect(superKey, oVal);
-
-			// Output Schema:
-			// KEY: ORDERKEY, SHIPPRIORITY
-			// VALUE: 0:ORDERKEY, 1:SHIPPRIORITY, 2:EXTENDEDPRICE
 		}
 	}
 
+	/**
+	 * Reduce PACT implements the aggregation of the results. The 
+	 * Combinable annotation is set as the partial sums can be calculated
+	 * already in the combiner
+	 *
+	 */
 	@Combinable
 	public static class AggLiO extends ReduceStub<N_IntStringPair, Tuple, PactInteger, Tuple> {
 
+		/**
+		 * Does the aggregation of the query. 
+		 * 
+		 * sum(l_extendedprice) as revenue
+		 * GROUP BY l_orderkey, o_shippriority;
+		 * 
+		 * Output Schema:
+		 *  Key: ORDERKEY
+		 *  Value: 0:ORDERKEY, 1:SHIPPRIORITY, 2:EXTENDEDPRICESUM
+		 *
+		 */
 		@Override
 		public void reduce(N_IntStringPair oKeyShipPrio, Iterator<Tuple> values, Collector<PactInteger, Tuple> out) {
 
@@ -143,11 +215,11 @@ public class TPCHQuery3 implements PlanAssembler, PlanAssemblerDescription {
 				out.collect(oKeyShipPrio.getFirst(), value);
 			}
 
-			// Output Schema:
-			// KEY: ORDERKEY
-			// VALUE: 0:ORDERKEY, 1:SHIPPRIORITY, 2:EXTENDEDPRICESUM
 		}
 
+		/**
+		 * Creates partial sums on the price attribute for each data batch
+		 */
 		@Override
 		public void combine(N_IntStringPair oKeyShipPrio, Iterator<Tuple> values, Collector<N_IntStringPair, Tuple> out) {
 
@@ -166,12 +238,12 @@ public class TPCHQuery3 implements PlanAssembler, PlanAssemblerDescription {
 				out.collect(oKeyShipPrio, value);
 			}
 
-			// Output Schema:
-			// KEY: ORDERKEY, SHIPPRIORITY
-			// VALUE: 0:ORDERKEY, 1:SHIPPRIORITY, 2:EXTENDEDPRICESUM
 		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Plan getPlan(String... args) {
 
@@ -186,6 +258,7 @@ public class TPCHQuery3 implements PlanAssembler, PlanAssemblerDescription {
 		String lineitemsPath = args[2];
 		String resultPath = args[3];
 
+		// create DataSourceContract for Orders input
 		DataSourceContract<PactInteger, Tuple> orders = new DataSourceContract<PactInteger, Tuple>(
 			IntTupleDataInFormat.class, ordersPath, "Orders");
 		orders.setFormatParameter("delimiter", "\n");
@@ -193,12 +266,14 @@ public class TPCHQuery3 implements PlanAssembler, PlanAssemblerDescription {
 		orders.setOutputContract(UniqueKey.class);
 		orders.getCompilerHints().setAvgNumValuesPerKey(1);
 
+		// create DataSourceContract for LineItems input
 		DataSourceContract<PactInteger, Tuple> lineitems = new DataSourceContract<PactInteger, Tuple>(
 			IntTupleDataInFormat.class, lineitemsPath, "LineItems");
 		lineitems.setFormatParameter("delimiter", "\n");
 		lineitems.setDegreeOfParallelism(degreeOfParallelism);
 		lineitems.getCompilerHints().setAvgNumValuesPerKey(4);
 
+		// create MapContract for filtering Orders tuples
 		MapContract<PactInteger, Tuple, PactInteger, Tuple> filterO = new MapContract<PactInteger, Tuple, PactInteger, Tuple>(
 			FilterO.class, "FilterO");
 		filterO.setDegreeOfParallelism(degreeOfParallelism);
@@ -206,6 +281,7 @@ public class TPCHQuery3 implements PlanAssembler, PlanAssemblerDescription {
 		filterO.getCompilerHints().setSelectivity(0.05f);
 		filterO.getCompilerHints().setAvgNumValuesPerKey(1);
 
+		// create MapContract for projecting LineItems tuples
 		MapContract<PactInteger, Tuple, PactInteger, Tuple> projectLi = new MapContract<PactInteger, Tuple, PactInteger, Tuple>(
 			ProjectLi.class, "ProjectLi");
 		projectLi.setDegreeOfParallelism(degreeOfParallelism);
@@ -213,6 +289,7 @@ public class TPCHQuery3 implements PlanAssembler, PlanAssemblerDescription {
 		projectLi.getCompilerHints().setSelectivity(1.0f);
 		projectLi.getCompilerHints().setAvgNumValuesPerKey(4);
 
+		// create MatchContract for joining Orders and LineItems
 		MatchContract<PactInteger, Tuple, Tuple, N_IntStringPair, Tuple> joinLiO = new MatchContract<PactInteger, Tuple, Tuple, N_IntStringPair, Tuple>(
 			JoinLiO.class, "JoinLiO");
 		joinLiO.setDegreeOfParallelism(degreeOfParallelism);
@@ -220,6 +297,7 @@ public class TPCHQuery3 implements PlanAssembler, PlanAssemblerDescription {
 		joinLiO.getCompilerHints().setAvgBytesPerRecord(64);
 		joinLiO.getCompilerHints().setAvgNumValuesPerKey(4);
 
+		// create ReduceContract for aggregating the result
 		ReduceContract<N_IntStringPair, Tuple, PactInteger, Tuple> aggLiO = new ReduceContract<N_IntStringPair, Tuple, PactInteger, Tuple>(
 			AggLiO.class, "AggLio");
 		aggLiO.setDegreeOfParallelism(degreeOfParallelism);
@@ -227,10 +305,12 @@ public class TPCHQuery3 implements PlanAssembler, PlanAssemblerDescription {
 		aggLiO.getCompilerHints().setSelectivity(0.25f);
 		aggLiO.getCompilerHints().setAvgNumValuesPerKey(1);
 
+		// create DataSinkContract for writing the result
 		DataSinkContract<PactString, Tuple> result = new DataSinkContract<PactString, Tuple>(
 			StringTupleDataOutFormat.class, resultPath, "Output");
 		result.setDegreeOfParallelism(degreeOfParallelism);
 
+		// assemble the PACT plan
 		result.setInput(aggLiO);
 		aggLiO.setInput(joinLiO);
 		joinLiO.setFirstInput(filterO);
@@ -238,6 +318,7 @@ public class TPCHQuery3 implements PlanAssembler, PlanAssemblerDescription {
 		joinLiO.setSecondInput(projectLi);
 		projectLi.setInput(lineitems);
 
+		// return the PACT plan
 		return new Plan(result, "TPCH Q3");
 	}
 
