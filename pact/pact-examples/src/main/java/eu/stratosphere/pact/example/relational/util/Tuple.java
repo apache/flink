59a383cde2cbe84f6235fd17afbaab28db9a1169
Fabian Hueske <fabian.hueske@tu-berlin.de>
2011-01-26 23:34:48 +0100

    - Added unit test for IntTupleDataInFormat.java - Added unit test for Tuple.java - Added JavaDocs to Tuple.java - Fixed bugs in Tuple.java

--- pact/pact-examples/src/main/java/eu/stratosphere/pact/example/relational/util/Tuple.java
+++ pact/pact-examples/src/main/java/eu/stratosphere/pact/example/relational/util/Tuple.java
@@ -37,16 +37,17 @@ public class Tuple implements Value {
 	}
 
 	/**
-	 * Creates a new tuple with the following properties:
-	 * - The key is initially 0 and must be extracted via <tt>extractKey</tt> - The bytes have one char delimiter
-	 * between the columns.
-	 * - The offsets array is one bigger than the number of columns and contains
-	 * the length of the bytes as its last index.
+	 * Creates a new tuple with a given set of attributes.
 	 * 
 	 * @param bytes
-	 *        The bytes array.
+	 *        The bytes array. Attributes are separated by a single character. The last attribute
+	 *        is also terminated with a single character. 
 	 * @param offsets
-	 *        The offsets of the columns in the byte array.
+	 *        The offsets of the columns in the byte array. The last entry gives the offset of the terminating
+	 *        character + 1 (if the byte array exactly holds all attributes and delimiting characters this is 
+	 *        the length of the array).
+	 * @param cols
+	 *        The number of columns.
 	 */
 	public Tuple(byte[] bytes, short[] offsets, int cols) {
 		this.bytes = bytes;
@@ -58,15 +59,34 @@ public class Tuple implements Value {
 	// Accessors
 	// ------------------------------------------------------------------------
 
+	/**
+	 * Returns the number of attributes / columns of the tuple.
+	 * 
+	 * @return The number of columns of the tuple.
+	 */
 	public int getNumberOfColumns() {
 		return numCols;
 	}
 
+	/**
+	 * Returns the internal byte array of the tuple.
+	 * 
+	 * @return The interal byte array of the tuple.
+	 */
 	public byte[] getBytes() {
 		return bytes;
 	}
 
+	/**
+	 * Returns the length of the column with the specified index. Column indices start at 0.
+	 * 
+	 * @param colNumber Index of the column. Indices start at 0.
+	 * @return The length of the specified column.
+	 */
 	public int getColumnLength(int colNumber) {
+		if(offsets == null) return -1;
+		if(colNumber < 0) return -1;
+		if(colNumber >= offsets.length) return -1;
 		return offsets[colNumber + 1] - offsets[colNumber] - 1;
 	}
 
@@ -74,7 +94,15 @@ public class Tuple implements Value {
 	// Modification
 	// ------------------------------------------------------------------------
 
+	/**
+	 * Appends all columns of the specified tuple to this tuple.
+	 * 
+	 * @param other	The tuple whose columns are appended to this tuple.
+	 */
 	public void concatenate(Tuple other) {
+		
+		if(other.getBytes() == null) return;
+		
 		if (bytes == null) {
 			bytes = (byte[]) other.bytes.clone();
 			offsets = (short[]) other.offsets.clone();
@@ -111,10 +139,19 @@ public class Tuple implements Value {
 		}
 	}
 
+	/**
+	 * Performs a projection on the tuple.
+	 * The int parameter is interpreted as a bitmap on the columns.
+	 * I.e. a bitmap value of 1 projects to the first column, 2 to the second, 3 to the first two columns, and so on.
+	 * 
+	 * @param bitmap the projection bitmap.
+	 */
 	public void project(int bitmap) {
 		int[] lengths = new int[numCols];
 		int lenCount = 0;
 
+		if(bytes == null || offsets == null) return;
+		
 		// go through the bitmap and find the indexes of the columns to retain
 		int k = 0;
 		for (int i = 0; bitmap != 0 && i < numCols; i++, bitmap >>>= 1) {
@@ -143,14 +180,32 @@ public class Tuple implements Value {
 		offsets[numCols] = (short) tmp.length;
 	}
 
+	/**
+	 * Compares a String attribute of this tuple with a String attribute of another tuple.
+	 * The strings are compared lexicographic.
+	 * 
+	 * @param other The other tuple.
+	 * @param thisColumn The index of this tuple's String attribute.
+	 * @param otherColumn The index of the other tuple's String attribute.
+	 * @return 1 if this tuple's attribute is greater, 0 if both attributes have the same value, 
+	 *         -1 if this tuple's attribute is smaller.
+	 * @throws IndexOutOfBoundsException Thrown if one of the column indices is invalid (smaller than 0 or bigger 
+	 *            than the attribute count).
+	 */
 	public int compareStringAttribute(Tuple other, int thisColumn, int otherColumn) {
+		
+		if(thisColumn < 0) throw new IndexOutOfBoundsException();
+		if(otherColumn < 0) throw new IndexOutOfBoundsException();
+		if(thisColumn >= numCols) throw new IndexOutOfBoundsException();
+		if(otherColumn >= other.numCols) throw new IndexOutOfBoundsException();
+		
 		int len = getColumnLength(thisColumn);
 		int otherLen = other.getColumnLength(otherColumn);
 		int min = Math.min(len, otherLen);
 
 		int startPos = offsets[thisColumn];
 		int otherStartPos = other.offsets[otherColumn];
-
+		
 		for (int i = 0; i < min; i++) {
 			if (bytes[startPos + i] < other.bytes[otherStartPos + i]) {
 				return -1;
@@ -168,30 +223,77 @@ public class Tuple implements Value {
 		}
 	}
 
+	/**
+	 * Compares an Integer attribute of this tuple with an Integer attribute of another tuple.
+	 * 
+	 * @param other The other tuple.
+	 * @param thisColumn The index of this tuple's int attribute.
+	 * @param otherColumn The index of the other tuple's int attribute.
+	 * @return 1 if this tuple's attribute is greater, 0 if both attributes have the same value, 
+	 *         -1 if this tuple's attribute is smaller.
+	 * @throws IndexOutOfBoundsException Thrown if one of the column indices is invalid (smaller than 0 or bigger 
+	 *            than the attribute count).
+	 */
 	public int compareIntAttribute(Tuple other, int thisColumn, int otherColumn) {
 		int len = getColumnLength(thisColumn);
 		int otherLen = other.getColumnLength(otherColumn);
 
-		if (len < otherLen) {
-			return -1;
-		} else if (len > otherLen) {
-			return 1;
-		} else {
-			int myStartPos = offsets[thisColumn];
-			int compStartPos = other.offsets[otherColumn];
-
-			for (int i = 0; i < len; i++) {
-				if (bytes[myStartPos + i] < other.bytes[compStartPos + i]) {
-					return -1;
-				} else if (bytes[myStartPos + i] > other.bytes[compStartPos + i]) {
-					return 1;
-				}
-			}
-			return 0;
+		if(thisColumn < 0) throw new IndexOutOfBoundsException();
+		if(otherColumn < 0) throw new IndexOutOfBoundsException();
+		if(thisColumn >= numCols) throw new IndexOutOfBoundsException();
+		if(otherColumn >= other.numCols) throw new IndexOutOfBoundsException();
+		
+		short thisNegative = 1;
+		short otherNegative = 1;
+		
+		if(this.bytes[offsets[thisColumn]] == '-') {
+			thisNegative = -1;
 		}
+		
+		if(other.getBytes()[other.offsets[otherColumn]] == '-') {
+			otherNegative = -1;
+		}
+		
+		// check one int is negative
+		if(thisNegative != otherNegative) {
+			return thisNegative;
+		}
+
+		// check if they vary in length
+		if (len < otherLen) {
+			return -1 * thisNegative;
+		} else if (len > otherLen) {
+			return 1 * thisNegative;
+		}
+
+		// both have the same orientation and length, check digit-wise
+		int myStartPos = offsets[thisColumn];
+		int compStartPos = other.offsets[otherColumn];
+
+		for (int i = 0; i < len; i++) {
+			if (bytes[myStartPos + i] < other.bytes[compStartPos + i]) {
+				return -1 * thisNegative;
+			} else if (bytes[myStartPos + i] > other.bytes[compStartPos + i]) {
+				return 1 * thisNegative;
+			}
+		}
+		return 0;
+
 	}
 
-	public String getStringValueAt(int column) {
+	/**
+	 * Returns the String value of the attribute with the specified index.
+	 * 
+	 * @param column The index of the attribute whose String value is returned.
+	 * @return The String value of the specified attribute.
+	 * @throws IndexOutOfBoundsException Thrown if the index of the column is invalid (smaller than 0 or bigger 
+	 *            than the attribute count).
+	 */
+	public String getStringValueAt(int column) throws IndexOutOfBoundsException {
+		// check for validity of column index
+		if(column < 0) throw new IndexOutOfBoundsException();
+		if(column >= numCols) throw new IndexOutOfBoundsException();
+		
 		int off = offsets[column];
 		int len = getColumnLength(column);
 
@@ -203,26 +305,75 @@ public class Tuple implements Value {
 		return new String(chars);
 	}
 
-	public long getLongValueAt(int column) {
+	/**
+	 * Returns the Long value of the attribute with the specified index.
+	 * The value must be represented in the decimal system.
+	 *  
+	 * @param column The index of the attribute whose value is returned as long.
+	 * @return The long value of the specified attribute.
+	 * @throws IndexOutOfBoundsException Thrown if the index of the column is invalid (smaller than 0 or bigger 
+	 *            than the attribute count).
+	 * @throws NumberFormatException Thrown if the attribute is not a valid long value 
+	 *            (contains any other character than digits or '-'.)
+	 */
+	public long getLongValueAt(int column) throws IndexOutOfBoundsException, NumberFormatException {
+		
+		if(column < 0) throw new IndexOutOfBoundsException();
+		if(column >= numCols) throw new IndexOutOfBoundsException();
+		
 		int off = offsets[column];
 		int len = getColumnLength(column);
 
+		boolean isNegative = false;
+
+		if(bytes[off] == '-') {
+			isNegative = true;
+			off++;
+			len--;
+		}
+		
 		long value = 0;
 		for (int i = off; i < off + len; i++) {
+			
+			if(bytes[i] < '0' || bytes[i] > '9') throw new NumberFormatException();
+			
 			value *= 10;
 			value += (bytes[i] - 48);
+		}
+		
+		if(isNegative) {
+			value *= -1;
 		}
 
 		return value;
 	}
 
-	public byte[] getByteArrayValueAt(int column) {
+	/**
+	 * Returns an attribute which is specified by an index as byte array. 
+	 * 
+	 * @param column The index of the attribute which is returned as byte array.
+	 * @return The value of the specified attribute as byte array value.
+	 * @throws IndexOutOfBoundsException Thrown if the index of the column is invalid (smaller than 0 or bigger 
+	 *            than the attribute count).
+	 */
+	public byte[] getByteArrayValueAt(int column) throws IndexOutOfBoundsException {
+		
+		if(column < 0) throw new IndexOutOfBoundsException();
+		if(column >= numCols) throw new IndexOutOfBoundsException();
+		
 		int len = getColumnLength(column);
 		byte[] buffer = new byte[len];
 		System.arraycopy(bytes, offsets[column], buffer, 0, len);
 		return buffer;
 	}
 
+	/**
+	 * Sets the size of the internal byte array of the tuple to the minimum capacity.
+	 * If the minimum capacity is smaller than the current size of the tuple's byte array,
+	 * nothing is done. Otherwise a new byte array is allocated and the content of the old one copied.
+	 * 
+	 * @param minCapacity The new size of the internal byte array. 
+	 */
 	public void reserveSpace(int minCapacity) {
 		if (bytes.length < minCapacity) {
 			byte[] tmp = new byte[minCapacity];
@@ -231,6 +382,9 @@ public class Tuple implements Value {
 		}
 	}
 
+	/**
+	 * Reduces the size of the internal byte and offset arrays to the currently used size.
+	 */
 	public void compact() {
 		int len = offsets[numCols];
 
@@ -247,6 +401,11 @@ public class Tuple implements Value {
 		}
 	}
 
+	/**
+	 * Appends an attribute at the end of the tuple.
+	 * 
+	 * @param attValue The attribute to append.
+	 */
 	public void addAttribute(byte[] attValue) {
 		int end;
 
@@ -280,37 +439,11 @@ public class Tuple implements Value {
 		offsets[numCols] = (short) end;
 	}
 
-	public void addAttributeFromKVRecord(Tuple other, int column) {
-		int len = other.getColumnLength(column) + 1;
-		int end;
-
-		if (numCols == 0) {
-			offsets = new short[5];
-			bytes = new byte[Math.max(256, len)];
-			end = 0;
-		} else {
-			end = offsets[numCols];
-
-			// increase offset array, if necessary
-			if (numCols + 1 >= offsets.length) {
-				short[] tmp = new short[offsets.length * 2];
-				System.arraycopy(offsets, 0, tmp, 0, numCols + 1);
-				offsets = tmp;
-			}
-
-			// increase byte buffer, if necessary
-			if (bytes.length < end + len) {
-				byte[] tmp = new byte[end + len];
-				System.arraycopy(bytes, 0, tmp, 0, end);
-				bytes = tmp;
-			}
-		}
-
-		System.arraycopy(other.bytes, other.offsets[column], bytes, end, len);
-		numCols++;
-		offsets[numCols] = (short) (end + len);
-	}
-
+	/**
+	 * Appends an attribute at the end of the tuple.
+	 * 
+	 * @param attValue The attribute to append.
+	 */
 	public void addAttribute(String attValue) {
 		int end;
 
@@ -344,6 +477,44 @@ public class Tuple implements Value {
 		numCols++;
 		offsets[numCols] = (short) end;
 	}
+	
+	/**
+	 * Appends an attribute by copying it from another tuple.
+	 * 
+	 * @param other The other tuple to copy from.
+	 * @param column The index of the attribute to copy within the other tuple.
+	 */
+	public void addAttributeFromKVRecord(Tuple other, int column) {
+		int len = other.getColumnLength(column) + 1;
+		int end;
+
+		if (numCols == 0) {
+			offsets = new short[5];
+			bytes = new byte[Math.max(256, len)];
+			end = 0;
+		} else {
+			end = offsets[numCols];
+
+			// increase offset array, if necessary
+			if (numCols + 1 >= offsets.length) {
+				short[] tmp = new short[offsets.length * 2];
+				System.arraycopy(offsets, 0, tmp, 0, numCols + 1);
+				offsets = tmp;
+			}
+
+			// increase byte buffer, if necessary
+			if (bytes.length < end + len) {
+				byte[] tmp = new byte[end + len];
+				System.arraycopy(bytes, 0, tmp, 0, end);
+				bytes = tmp;
+			}
+		}
+
+		System.arraycopy(other.bytes, other.offsets[column], bytes, end, len);
+		numCols++;
+		offsets[numCols] = (short) (end + len);
+	}
+
 
 	// ------------------------------------------------------------------------
 	// Serialization
@@ -399,16 +570,6 @@ public class Tuple implements Value {
 		}
 
 		return bld.toString();
-	}
-
-	public boolean checkDelimiters() {
-		for (int i = 0; i < numCols; i++) {
-			if (bytes[offsets[i] + getColumnLength(i)] != '|') {
-				return false;
-			}
-		}
-
-		return true;
 	}
 
 }
