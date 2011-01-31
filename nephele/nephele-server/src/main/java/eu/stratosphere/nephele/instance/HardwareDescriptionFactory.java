e01366aba32a0ae5c8effad7a43575f5f7b5771a
Philipp Richter <casp@voyager-2.local>
2011-01-31 13:24:12 +0100

    more generic detection of tenured space + Mac OS support

--- nephele/nephele-server/src/main/java/eu/stratosphere/nephele/instance/HardwareDescriptionFactory.java
+++ nephele/nephele-server/src/main/java/eu/stratosphere/nephele/instance/HardwareDescriptionFactory.java
@@ -18,8 +18,10 @@ package eu.stratosphere.nephele.instance;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
+import java.lang.management.MemoryType;
 import java.lang.management.MemoryUsage;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -59,6 +61,11 @@ public class HardwareDescriptionFactory {
 	private static final String WINDOWS_OS_PREFIX = "Windows";
 
 	/**
+	 * The expected prefix for Mac OS operating systems.
+	 */
+	private static final String MAC_OS_PREFIX = "Mac";
+	
+	/**
 	 * The path to the interface to extract memory information under Linux.
 	 */
 	private static final String LINUX_MEMORY_INFO_PATH = "/proc/meminfo";
@@ -69,14 +76,24 @@ public class HardwareDescriptionFactory {
 	private static final Pattern LINUX_MEMORY_REGEX = Pattern.compile("^MemTotal:\\s*(\\d+)\\s+kB$");
 
 	/**
-	 * The name of the tenured memory pool.
+	 * The names of the tenured memory pool
 	 */
-	private static final String TENURED_POOL_NAME = "Tenured Gen";
+	private static final String[] TENURED_POOL_NAMES = {"Tenured Gen", "PS Old Gen", "CMS Old Gen"};
 
 	/**
 	 * The operating system name.
 	 */
 	private static String os = null;
+	
+	/**
+	 * The memory threshold to be used when tenured pool can be determined
+	 */
+	private static float TENURED_POOL_THRESHOLD = 0.8f;
+	
+	/**
+	 * The memory threshold to be used when tenured pool can not be determined
+	 */
+	private static float RUNTIME_MEMORY_THRESHOLD = 0.7f;
 
 	/**
 	 * Private constructor, so class cannot be instantiated.
@@ -133,28 +150,45 @@ public class HardwareDescriptionFactory {
 
 		// in order to prevent allocations of arrays that are too big for the JVM's different memory pools,
 		// make sure that the maximum segment size is 70% of the currently free tenure heap
-		final List<MemoryPoolMXBean> poolBeans = ManagementFactory.getMemoryPoolMXBeans();
-		for (MemoryPoolMXBean bean : poolBeans) {
-			if (bean.getName().equals(TENURED_POOL_NAME)) {
-				// found the tenured pool
-				final MemoryUsage usage = bean.getUsage();
-				long tenuredSize =  usage.getMax() - usage.getUsed();
-				//TODO: make the constant configurable
-				return (long) (tenuredSize * 0.8f);
-			}
+		final MemoryPoolMXBean tenuredpool = findTenuredGenPool();
+		
+		if(tenuredpool != null){
+			final MemoryUsage usage = tenuredpool.getUsage();
+			long tenuredSize =  usage.getMax() - usage.getUsed();
+			LOG.info("found tenured gen pool. Max: " + tenuredSize + " used: " + usage.getUsed() + ".");
+			//TODO: make the constant configurable
+			return (long) (tenuredSize * TENURED_POOL_THRESHOLD);
 		}
 		
+		LOG.info("could not determine tenured gen pool. Using JVM Runtime information instead.");
 		Runtime r = Runtime.getRuntime();
 		final long maximum = r.maxMemory();
 		
 		//TODO: Make 0.7f configurable
-		return (long) ( 0.7f * (maximum - r.totalMemory() + r.freeMemory()));
+		return (long) ( RUNTIME_MEMORY_THRESHOLD * (maximum - r.totalMemory() + r.freeMemory()));
 
-//		LOG.error("Cannot determine size of free memory: Unable to find tenured pool");
-//
-//		return -1;
 	}
 
+	/**
+	 * Returns the tenured gen pool
+	 * @return tenured gen pool, or <code>null</code>, if not detectable 
+	 */
+	private static MemoryPoolMXBean findTenuredGenPool() {
+		for (MemoryPoolMXBean pool : ManagementFactory.getMemoryPoolMXBeans()) {
+			
+			for(String s: TENURED_POOL_NAMES){
+				if(pool.getName().equals(s)){
+					//seems that we found the tenured pool
+					//double check, if it MemoryType is HEAP and usageThreshold supported..
+					if (pool.getType() == MemoryType.HEAP && pool.isUsageThresholdSupported()) {
+						return pool;
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
 	/**
 	 * Returns the operating system this JVM runs on.
 	 * 
@@ -198,6 +232,20 @@ public class HardwareDescriptionFactory {
 	}
 
 	/**
+	 * Checks whether the operating system this JVM runs on is Windows.
+	 * 
+	 * @return <code>true</code> if the operating system this JVM runs on is Windows, <code>false</code> otherwise
+	 */
+	private static boolean isMac() {
+
+		if (getOperatingSystemName().startsWith(MAC_OS_PREFIX)) {
+			return true;
+		}
+
+		return false;
+	}	
+	
+	/**
 	 * Returns the size of the physical memory in bytes.
 	 * 
 	 * @return the size of the physical memory in bytes or <code>-1</code> if the size could not be determined
@@ -208,7 +256,10 @@ public class HardwareDescriptionFactory {
 			return getSizeOfPhysicalMemoryForLinux();
 		} else if (isWindows()) {
 			LOG.error("Cannot determine size of physical memory: Support for Windows is not yet implemented");
-		} else {
+		} else if (isMac()){
+			return getSizeOfPhysicalMemoryForMac();
+		}
+		else {
 			LOG.error("Cannot determine size of physical memory: Unknown operating system");
 		}
 
@@ -261,4 +312,35 @@ public class HardwareDescriptionFactory {
 
 		return -1;
 	}
+	
+	/**
+	 * Returns the size of the physical memory in bytes on a Mac OS-based operating system
+	 * 
+	 * @return the size of the physical memory in bytes or <code>-1</code> if the size could not be determined
+	 */
+	private static long getSizeOfPhysicalMemoryForMac() {
+
+		try {
+			Process proc = Runtime.getRuntime().exec("sysctl hw");
+
+			BufferedReader bi = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+
+			String line;
+
+			while ((line = bi.readLine()) != null) {
+				if (line.startsWith("hw.memsize")) {
+					long memsize = Long.parseLong(line.split(":")[1].trim());
+					bi.close();
+					proc.destroy();
+					return memsize;
+				}
+			}
+
+		} catch (Exception e) {
+			LOG.error(e);
+			return -1;
+		}
+		return -1;
+	}
+	
 }
