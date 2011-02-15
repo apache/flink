cba7ccb093f782c5dd1fcfc3be98e1147f9330b6
Fabian Hueske <fabian.hueske@tu-berlin.de>
2011-02-15 15:15:13 +0100

    - fixed bug in OutputCollector - extended OutputCollectorTest

--- pact/pact-runtime/src/test/java/eu/stratosphere/pact/runtime/task/util/OutputCollectorTest.java
+++ pact/pact-runtime/src/test/java/eu/stratosphere/pact/runtime/task/util/OutputCollectorTest.java
@@ -1,60 +1,23 @@
 package eu.stratosphere.pact.runtime.task.util;
 
+import java.io.IOException;
+import java.util.HashSet;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 
+import eu.stratosphere.nephele.execution.Environment;
+import eu.stratosphere.nephele.io.RecordWriter;
+import eu.stratosphere.nephele.template.AbstractTask;
 import eu.stratosphere.pact.common.type.Key;
+import eu.stratosphere.pact.common.type.KeyValuePair;
 import eu.stratosphere.pact.common.type.Value;
+import eu.stratosphere.pact.common.type.base.PactInteger;
 
 public class OutputCollectorTest {
-
-	/*
-	private class AbstractTaskMock extends AbstractTask {
-
-		@Override
-		public void invoke() throws Exception {
-		}
-
-		@Override
-		public void registerInputOutput() {
-		}
-		
-		@Override
-		public Environment getEnvironment() {
-			return null;
-		}
-	}
-	*/
-	
-	/*
-	private class RecordWriterMock<T extends Record> extends RecordWriter<T> {
-
-		public T lastEmitted = null;
-		
-		public RecordWriterMock(Class<T> outputClass) {
-			super(new AbstractTaskMock(), outputClass);
-		}
-
-		@Override
-		public void emit(T record) {
-			lastEmitted = record;
-		}
-		
-		public T getLastEmitted() {
-			return lastEmitted;
-		}
-		
-		@Override
-		@SuppressWarnings("unchecked")
-		private void connectOutputGate(Class<T> outputClass, ChannelSelector selector) {
-			
-		}
-	}
-	*/
-	
-	
-	
 	
 	@Test 
 	public void testAddWriter() {
@@ -84,70 +47,125 @@ public class OutputCollectorTest {
 		
 	}
 	
-	/*
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testCollect() {
+
+		Class<RecordWriter<KeyValuePair<PactInteger,PactInteger>>> rwC = (Class<RecordWriter<KeyValuePair<PactInteger,PactInteger>>>)((Class<?>)RecordWriter.class);
+		Class<KeyValuePair<PactInteger,PactInteger>> kvpC = (Class<KeyValuePair<PactInteger,PactInteger>>)((Class<?>)KeyValuePair.class);
 		
-		OutputCollector<Key, Value> oc = new OutputCollector<Key, Value>();
+		// create readers
+		RecordWriter<KeyValuePair<PactInteger,PactInteger>> rwMock1 = new RecordWriter<KeyValuePair<PactInteger,PactInteger>>(new MockTask(), kvpC); 
+		RecordWriter<KeyValuePair<PactInteger,PactInteger>> rwMock2 = new RecordWriter<KeyValuePair<PactInteger,PactInteger>>(new MockTask(), kvpC);
+		RecordWriter<KeyValuePair<PactInteger,PactInteger>> rwMock3 = new RecordWriter<KeyValuePair<PactInteger,PactInteger>>(new MockTask(), kvpC);
+		RecordWriter<KeyValuePair<PactInteger,PactInteger>> rwMock4 = new RecordWriter<KeyValuePair<PactInteger,PactInteger>>(new MockTask(), kvpC);
+		RecordWriter<KeyValuePair<PactInteger,PactInteger>> rwMock5 = new RecordWriter<KeyValuePair<PactInteger,PactInteger>>(new MockTask(), kvpC);
+		RecordWriter<KeyValuePair<PactInteger,PactInteger>> rwMock6 = new RecordWriter<KeyValuePair<PactInteger,PactInteger>>(new MockTask(), kvpC);
+		// mock readers
+		rwMock1 = Mockito.mock(rwC);
+		rwMock2 = Mockito.mock(rwC);
+		rwMock3 = Mockito.mock(rwC);
+		rwMock4 = Mockito.mock(rwC);
+		rwMock5 = Mockito.mock(rwC);
+		rwMock6 = Mockito.mock(rwC);
 		
-		RecordWriterMock<KeyValuePair<Key, Value>> rwm1 = new RecordWriterMock(KeyValuePair.class);
-		oc.addWriter(rwm1, false);
-		RecordWriterMock<KeyValuePair<Key, Value>> rwm2 = new RecordWriterMock(KeyValuePair.class);
-		oc.addWriter(rwm2, true);
-		RecordWriterMock<KeyValuePair<Key, Value>> rwm3 = new RecordWriterMock(KeyValuePair.class);
-		oc.addWriter(rwm3, true);
-		RecordWriterMock<KeyValuePair<Key, Value>> rwm4 = new RecordWriterMock(KeyValuePair.class);
-		oc.addWriter(rwm4, true);
-		RecordWriterMock<KeyValuePair<Key, Value>> rwm5 = new RecordWriterMock(KeyValuePair.class);
-		oc.addWriter(rwm5, false);
-		RecordWriterMock<KeyValuePair<Key, Value>> rwm6 = new RecordWriterMock(KeyValuePair.class);
-		oc.addWriter(rwm6, true);		
+		ArgumentCaptor<KeyValuePair<PactInteger,PactInteger>> captor1 = ArgumentCaptor.forClass(kvpC);
+		ArgumentCaptor<KeyValuePair<PactInteger,PactInteger>> captor2 = ArgumentCaptor.forClass(kvpC);
+		ArgumentCaptor<KeyValuePair<PactInteger,PactInteger>> captor3 = ArgumentCaptor.forClass(kvpC);
+		ArgumentCaptor<KeyValuePair<PactInteger,PactInteger>> captor4 = ArgumentCaptor.forClass(kvpC);
+		ArgumentCaptor<KeyValuePair<PactInteger,PactInteger>> captor5 = ArgumentCaptor.forClass(kvpC);
+		ArgumentCaptor<KeyValuePair<PactInteger,PactInteger>> captor6 = ArgumentCaptor.forClass(kvpC);
 		
-		KeyValuePair<PactInteger,PactInteger> testPair = new KeyValuePair<PactInteger, PactInteger>();
+		OutputCollector<PactInteger, PactInteger> oc = new OutputCollector<PactInteger, PactInteger>();
 		
-		testPair.setKey(new PactInteger(24));
-		testPair.setValue(new PactInteger(987));
-		
-		oc.collect(testPair.getKey(), testPair.getValue());
-		
-		ArrayList<KeyValuePair<Key,Value>> distinctKeys = new ArrayList<KeyValuePair<Key,Value>>();
-		distinctKeys.add(rwm2.getLastEmitted());
-		distinctKeys.add(rwm3.getLastEmitted());
-		distinctKeys.add(rwm4.getLastEmitted());
-		distinctKeys.add(rwm6.getLastEmitted());
-		
-		Assert.assertTrue("Keys are not identical", 
-			((PactInteger)rwm1.getLastEmitted().getKey()).getValue() == ((PactInteger)rwm2.getLastEmitted().getKey()).getValue());
-		Assert.assertTrue("Values are not identical", 
-			((PactInteger)rwm1.getLastEmitted().getValue()).getValue() == ((PactInteger)rwm2.getLastEmitted().getValue()).getValue());
-		
-		Assert.assertTrue("Keys are not identical", 
-			((PactInteger)rwm1.getLastEmitted().getKey()).getValue() == ((PactInteger)rwm3.getLastEmitted().getKey()).getValue());
-		Assert.assertTrue("Values are not identical", 
-			((PactInteger)rwm1.getLastEmitted().getValue()).getValue() == ((PactInteger)rwm3.getLastEmitted().getValue()).getValue());
-		
-		Assert.assertTrue("Keys are not identical", 
-			((PactInteger)rwm1.getLastEmitted().getKey()).getValue() == ((PactInteger)rwm4.getLastEmitted().getKey()).getValue());
-		Assert.assertTrue("Values are not identical", 
-			((PactInteger)rwm1.getLastEmitted().getValue()).getValue() == ((PactInteger)rwm4.getLastEmitted().getValue()).getValue());
-		
-		Assert.assertTrue("Keys are not identical", 
-			((PactInteger)rwm1.getLastEmitted().getKey()).getValue() == ((PactInteger)rwm5.getLastEmitted().getKey()).getValue());
-		Assert.assertTrue("Values are not identical", 
-			((PactInteger)rwm1.getLastEmitted().getValue()).getValue() == ((PactInteger)rwm5.getLastEmitted().getValue()).getValue());
-		
-		Assert.assertTrue("Keys are not identical", 
-			((PactInteger)rwm1.getLastEmitted().getKey()).getValue() == ((PactInteger)rwm6.getLastEmitted().getKey()).getValue());
-		Assert.assertTrue("Values are not identical", 
-			((PactInteger)rwm1.getLastEmitted().getValue()).getValue() == ((PactInteger)rwm6.getLastEmitted().getValue()).getValue());
-		
-		for(int i=0;i<distinctKeys.size();i++) {
-			for(int j=i+1;j<distinctKeys.size();j++) {
-				Assert.assertTrue("References are not different",distinctKeys.get(i) != distinctKeys.get(j));
-			}
+		oc.addWriter(rwMock1, false);
+		oc.addWriter(rwMock2, true);
+		oc.addWriter(rwMock3, true);
+		oc.addWriter(rwMock4, true);
+		oc.addWriter(rwMock5, false);
+		oc.addWriter(rwMock6, true);	
+
+		oc.collect(new PactInteger(1), new PactInteger(123));
+		oc.collect(new PactInteger(23), new PactInteger(672));
+		oc.collect(new PactInteger(1673), new PactInteger(-12));
+
+		try {
+			Mockito.verify(rwMock1, Mockito.times(3)).emit(captor1.capture());
+			Mockito.verify(rwMock2, Mockito.times(3)).emit(captor2.capture());
+			Mockito.verify(rwMock3, Mockito.times(3)).emit(captor3.capture());
+			Mockito.verify(rwMock4, Mockito.times(3)).emit(captor4.capture());
+			Mockito.verify(rwMock5, Mockito.times(3)).emit(captor5.capture());
+			Mockito.verify(rwMock6, Mockito.times(3)).emit(captor6.capture());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 		
+		HashSet<Integer> refs = new HashSet<Integer>();
+		
+		// first pair
+		refs.add(System.identityHashCode(captor2.getAllValues().get(0)));
+		Assert.assertFalse(refs.contains(System.identityHashCode(captor3.getAllValues().get(0))));
+		refs.add(System.identityHashCode(captor3.getAllValues().get(0)));
+		Assert.assertFalse(refs.contains(System.identityHashCode(captor4.getAllValues().get(0))));
+		refs.add(System.identityHashCode(captor4.getAllValues().get(0)));
+		Assert.assertFalse(refs.contains(System.identityHashCode(captor6.getAllValues().get(0))));
+		refs.add(System.identityHashCode(captor6.getAllValues().get(0)));
+		
+		Assert.assertTrue(refs.contains(System.identityHashCode(captor5.getAllValues().get(0))));
+		
+		refs.clear();
+
+		// second pair
+		refs.add(System.identityHashCode(captor2.getAllValues().get(1)));
+		Assert.assertFalse(refs.contains(System.identityHashCode(captor3.getAllValues().get(1))));
+		refs.add(System.identityHashCode(captor3.getAllValues().get(1)));
+		Assert.assertFalse(refs.contains(System.identityHashCode(captor4.getAllValues().get(1))));
+		refs.add(System.identityHashCode(captor4.getAllValues().get(1)));
+		Assert.assertFalse(refs.contains(System.identityHashCode(captor6.getAllValues().get(1))));
+		refs.add(System.identityHashCode(captor6.getAllValues().get(1)));
+		
+		Assert.assertTrue(refs.contains(System.identityHashCode(captor5.getAllValues().get(1))));
+		
+		refs.clear();
+		
+		// third pair
+		refs.add(System.identityHashCode(captor2.getAllValues().get(2)));
+		Assert.assertFalse(refs.contains(System.identityHashCode(captor3.getAllValues().get(2))));
+		refs.add(System.identityHashCode(captor3.getAllValues().get(2)));
+		Assert.assertFalse(refs.contains(System.identityHashCode(captor4.getAllValues().get(2))));
+		refs.add(System.identityHashCode(captor4.getAllValues().get(2)));
+		Assert.assertFalse(refs.contains(System.identityHashCode(captor6.getAllValues().get(2))));
+		refs.add(System.identityHashCode(captor6.getAllValues().get(2)));
+		
+		Assert.assertTrue(refs.contains(System.identityHashCode(captor5.getAllValues().get(2))));
+		
+		refs.clear();
 	}
-	*/
+	
+	private class MockTask extends AbstractTask {
+
+		@Override
+		public void invoke() throws Exception {
+		}
+
+		@Override
+		public void registerInputOutput() {
+		}
+		
+		@Override
+		public Environment getEnvironment() {
+			return new MockEnvironment();
+		}
+	}
+	
+	private class MockEnvironment extends Environment {
+		
+		@Override
+		public int getNumberOfOutputGates() {
+			return 0;
+		}
+	}
 	
 }
