dc690a6b3c332d765a70ba43b8bfb3d80c8d5547
Fabian Hueske <fabian.hueske@tu-berlin.de>
2011-04-10 19:20:31 +0200

    - added Asterix TPCH query example and integration test

---
+++ pact/pact-examples/src/main/java/eu/stratosphere/pact/example/relational/TPCHQueryAsterix.java
@@ -0,0 +1,276 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
+package eu.stratosphere.pact.example.relational;
+
+import java.util.Iterator;
+
+import eu.stratosphere.pact.common.contract.DataSinkContract;
+import eu.stratosphere.pact.common.contract.DataSourceContract;
+import eu.stratosphere.pact.common.contract.MapContract;
+import eu.stratosphere.pact.common.contract.MatchContract;
+import eu.stratosphere.pact.common.contract.ReduceContract;
+import eu.stratosphere.pact.common.contract.OutputContract.SameKey;
+import eu.stratosphere.pact.common.contract.OutputContract.UniqueKey;
+import eu.stratosphere.pact.common.io.TextOutputFormat;
+import eu.stratosphere.pact.common.plan.Plan;
+import eu.stratosphere.pact.common.plan.PlanAssembler;
+import eu.stratosphere.pact.common.plan.PlanAssemblerDescription;
+import eu.stratosphere.pact.common.stub.Collector;
+import eu.stratosphere.pact.common.stub.MapStub;
+import eu.stratosphere.pact.common.stub.MatchStub;
+import eu.stratosphere.pact.common.stub.ReduceStub;
+import eu.stratosphere.pact.common.type.KeyValuePair;
+import eu.stratosphere.pact.common.type.base.PactInteger;
+import eu.stratosphere.pact.common.type.base.PactNull;
+import eu.stratosphere.pact.common.type.base.PactString;
+import eu.stratosphere.pact.example.relational.util.IntTupleDataInFormat;
+import eu.stratosphere.pact.example.relational.util.Tuple;
+
+/**
+ * The TPC-H is a decision support benchmark on relational data.
+ * Its documentation and the data generator (DBGEN) can be found
+ * on http://www.tpc.org/tpch/ .This implementation is tested with
+ * the DB2 data format.  
+ * THe PACT program implements a modified version of the query 3 of 
+ * the TPC-H benchmark including one join, some filtering and an
+ * aggregation.
+ * 
+ * SELECT c_mktsegment, COUNT(o_orderkey)
+ *   FROM orders, customer
+ *   WHERE c_custkey = o_custkey
+ * GROUP BY c_mktsegment;
+ */
+public class TPCHQueryAsterix implements PlanAssembler, PlanAssemblerDescription {
+
+	public static class StringIntOutFormat extends TextOutputFormat<PactString, PactInteger> {
+
+		@Override
+		public byte[] writeLine(KeyValuePair<PactString, PactInteger> pair) {
+			
+			return (pair.getKey().getValue()+"|"+pair.getValue().getValue()+"|\n").getBytes();
+		}
+		
+	}
+	
+	/**
+	 * Map PACT implements the filter on the orders table. The SameKey
+	 * OutputContract is annotated because the key does not change during
+	 * filtering.
+	 *  
+	 */
+	public static class ProjectOrder extends MapStub<PactInteger, Tuple, PactInteger, PactNull> {
+
+		/**
+	 	 * Does the projection on the Orders table.
+	 	 *  
+	 	 * Output Schema:
+	 	 *  Key: CUSTKEY
+	 	 *  Value: 0:ORDERKEY
+		 */
+		@Override
+		public void map(final PactInteger oKey, final Tuple value, final Collector<PactInteger, PactNull> out) {
+
+			PactInteger custKey = new PactInteger((int)value.getLongValueAt(1));
+			
+			out.collect(custKey, new PactNull());
+		}
+	}
+
+
+	/**
+	 * Map PACT implements the projection on the LineItem table. The SameKey
+	 * OutputContract is annotated because the key does not change during
+	 * projection.
+	 *
+	 */
+	@SameKey
+	public static class ProjectCust extends MapStub<PactInteger, Tuple, PactInteger, PactString> {
+
+		/**
+		 * Does the projection on the Customer table.
+		 *
+		 * Output Schema:
+		 *  Key: CUSTOMERKEY
+		 *  Value: 0:MKTSEGMENT
+		 */
+		@Override
+		public void map(PactInteger cKey, Tuple value, Collector<PactInteger, PactString> out) {
+
+			PactString mktSegment = new PactString(value.getStringValueAt(6));
+			
+			out.collect(cKey, mktSegment);
+		}
+	}
+
+	/**
+	 * Match PACT realizes the join between Customers and Order table.
+	 */
+	public static class JoinCO extends MatchStub<PactInteger, PactNull, PactString, PactString, PactNull> {
+
+		/**
+		 * Implements the join between LineItem and Order table on the 
+		 * order key.
+		 * 
+		 * WHERE c_custkey = o_custkey
+		 * 
+		 * Output Schema:
+		 *  Key: C_MKTSEGMENT
+		 *  Value: 0:ORDERKEY
+		 */
+		@Override
+		public void match(PactInteger cKey, PactNull oVal, PactString mktSeg, Collector<PactString, PactNull> out) {
+
+			out.collect(mktSeg, oVal);
+		}
+	}
+
+	/**
+	 * Reduce PACT implements the aggregation of the results. The 
+	 * Combinable annotation is set as the partial sums can be calculated
+	 * already in the combiner
+	 *
+	 */
+	// @Combinable
+	public static class AggCO extends ReduceStub<PactString, PactNull, PactString, PactInteger> {
+
+		/**
+		 * Does the aggregation of the query. 
+		 * 
+		 * Output Schema:
+		 *  Key: ORDERKEY
+		 *  Value: 0:ORDERKEY, 1:SHIPPRIORITY, 2:EXTENDEDPRICESUM
+		 *
+		 */
+		@Override
+		public void reduce(PactString mktSeg, Iterator<PactNull> values, Collector<PactString, PactInteger> out) {
+
+			int count = 0;
+
+			while (values.hasNext()) {
+				count++;
+				values.next();
+			}
+
+			out.collect(mktSeg, new PactInteger(count));
+
+		}
+
+		/**
+		 * Creates partial sums on the price attribute for each data batch
+		 */
+		/*
+		@Override
+		public void combine(N_IntStringPair oKeyShipPrio, Iterator<Tuple> values, Collector<N_IntStringPair, Tuple> out) {
+
+			long partExtendedPriceSum = 0;
+
+			Tuple value = null;
+			while (values.hasNext()) {
+				value = values.next();
+				partExtendedPriceSum += ((long) Double.parseDouble(value.getStringValueAt(2)));
+			}
+
+			if (value != null) {
+				value.project(3);
+				value.addAttribute(partExtendedPriceSum + "");
+
+				out.collect(oKeyShipPrio, value);
+			}
+
+		}
+		*/
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Plan getPlan(final String... args) {
+
+		// parse program parameters
+		int noSubtasks       = (args.length > 0 ? Integer.parseInt(args[0]) : 1);
+		String ordersPath    = (args.length > 1 ? args[1] : "");
+		String customerPath  = (args.length > 2 ? args[2] : "");
+		String output        = (args.length > 3 ? args[3] : "");
+
+		// create DataSourceContract for Orders input
+		DataSourceContract<PactInteger, Tuple> orders = new DataSourceContract<PactInteger, Tuple>(
+			IntTupleDataInFormat.class, ordersPath, "Orders");
+		orders.setFormatParameter("delimiter", "\n");
+		orders.setDegreeOfParallelism(noSubtasks);
+		orders.setOutputContract(UniqueKey.class);
+		orders.getCompilerHints().setAvgNumValuesPerKey(1);
+
+		// create DataSourceContract for LineItems input
+		DataSourceContract<PactInteger, Tuple> customers = new DataSourceContract<PactInteger, Tuple>(
+			IntTupleDataInFormat.class, customerPath, "Customers");
+		customers.setFormatParameter("delimiter", "\n");
+		customers.setDegreeOfParallelism(noSubtasks);
+		customers.setOutputContract(UniqueKey.class);
+
+		// create MapContract for filtering Orders tuples
+		MapContract<PactInteger, Tuple, PactInteger, PactNull> projectO = new MapContract<PactInteger, Tuple, PactInteger, PactNull>(
+			ProjectOrder.class, "ProjectO");
+		projectO.setDegreeOfParallelism(noSubtasks);
+		projectO.getCompilerHints().setAvgBytesPerRecord(5);
+		projectO.getCompilerHints().setAvgNumValuesPerKey(10);
+
+		// create MapContract for projecting LineItems tuples
+		MapContract<PactInteger, Tuple, PactInteger, PactString> projectC = new MapContract<PactInteger, Tuple, PactInteger, PactString>(
+			ProjectCust.class, "ProjectC");
+		projectC.setDegreeOfParallelism(noSubtasks);
+		projectC.getCompilerHints().setAvgNumValuesPerKey(1);
+		projectC.getCompilerHints().setAvgBytesPerRecord(20);
+
+		// create MatchContract for joining Orders and LineItems
+		MatchContract<PactInteger, PactNull, PactString, PactString, PactNull> joinCO = new MatchContract<PactInteger, PactNull, PactString, PactString, PactNull>(
+			JoinCO.class, "JoinCO");
+		joinCO.setDegreeOfParallelism(noSubtasks);
+		joinCO.getCompilerHints().setAvgBytesPerRecord(17);
+
+		// create ReduceContract for aggregating the result
+		ReduceContract<PactString, PactNull, PactString, PactInteger> aggCO = new ReduceContract<PactString, PactNull, PactString, PactInteger>(
+			AggCO.class, "AggCo");
+		aggCO.setDegreeOfParallelism(noSubtasks);
+		aggCO.getCompilerHints().setAvgBytesPerRecord(17);
+		aggCO.getCompilerHints().setAvgNumValuesPerKey(1);
+
+		// create DataSinkContract for writing the result
+		DataSinkContract<PactString, PactInteger> result = new DataSinkContract<PactString, PactInteger>(
+			StringIntOutFormat.class, output, "Output");
+		result.setDegreeOfParallelism(noSubtasks);
+
+		// assemble the PACT plan
+		result.setInput(aggCO);
+		aggCO.setInput(joinCO);
+		joinCO.setFirstInput(projectO);
+		projectO.setInput(orders);
+		joinCO.setSecondInput(projectC);
+		projectC.setInput(customers);
+
+		// return the PACT plan
+		return new Plan(result, "TPCH Benchmark");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getDescription() {
+		return "Parameters: [noSubStasks], [orders], [lineitem], [output]";
+	}
+
+}
