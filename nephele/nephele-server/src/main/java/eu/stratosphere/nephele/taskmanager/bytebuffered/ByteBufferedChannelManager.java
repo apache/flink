56713d36c65344d448b58f697bb109c882f44dc0
Daniel Warneke <daniel.warneke@tu-berlin.de>
2011-04-19 17:46:19 +0200

    Fixed #144

--- nephele/nephele-server/src/main/java/eu/stratosphere/nephele/taskmanager/bytebuffered/ByteBufferedChannelManager.java
+++ nephele/nephele-server/src/main/java/eu/stratosphere/nephele/taskmanager/bytebuffered/ByteBufferedChannelManager.java
@@ -50,11 +50,11 @@ public class ByteBufferedChannelManager {
 
 	private static final Log LOG = LogFactory.getLog(ByteBufferedChannelManager.class);
 
-	private static final int DEFAULT_NUMBER_OF_READ_BUFFERS = 128;
+	private static final int DEFAULT_NUMBER_OF_READ_BUFFERS = 256;
 
-	private static final int DEFAULT_NUMBER_OF_WRITE_BUFFERS = 128;
+	private static final int DEFAULT_NUMBER_OF_WRITE_BUFFERS = 256;
 
-	private static final int DEFAULT_BUFFER_SIZE_IN_BYTES = 128 * 1024; // 128k
+	private static final int DEFAULT_BUFFER_SIZE_IN_BYTES = 64 * 1024; // 64k
 
 	private static final boolean DEFAULT_ALLOW_SPILLING = true;
 
