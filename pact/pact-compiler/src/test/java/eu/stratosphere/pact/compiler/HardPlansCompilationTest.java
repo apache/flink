30cac4b8e51feeb56682f4e7f47c925d700538f8
Moritz Kaufmann <moritz.kaufmann@campus.tu-berlin.de>
2011-07-06 10:14:28 +0200

    Trying to reproduce bug #158, not yet possible

--- pact/pact-compiler/src/test/java/eu/stratosphere/pact/compiler/HardPlansCompilationTest.java
+++ pact/pact-compiler/src/test/java/eu/stratosphere/pact/compiler/HardPlansCompilationTest.java
@@ -18,36 +18,36 @@ import eu.stratosphere.nephele.instance.InstanceTypeFactory;
 import eu.stratosphere.nephele.jobgraph.AbstractJobVertex;
 import eu.stratosphere.nephele.jobgraph.JobGraph;
 import eu.stratosphere.nephele.jobgraph.JobTaskVertex;
+import eu.stratosphere.pact.common.contract.CrossContract;
 import eu.stratosphere.pact.common.contract.DataSinkContract;
 import eu.stratosphere.pact.common.contract.DataSourceContract;
 import eu.stratosphere.pact.common.contract.MapContract;
+import eu.stratosphere.pact.common.contract.ReduceContract;
+import eu.stratosphere.pact.common.contract.OutputContract.SameKeyFirst;
 import eu.stratosphere.pact.common.plan.Plan;
-import eu.stratosphere.pact.common.plan.Visitor;
 import eu.stratosphere.pact.common.type.base.PactInteger;
 import eu.stratosphere.pact.compiler.DataStatistics.BasicFileStatistics;
 import eu.stratosphere.pact.compiler.costs.FixedSizeClusterCostEstimator;
 import eu.stratosphere.pact.compiler.jobgen.JobGraphGenerator;
-import eu.stratosphere.pact.compiler.plan.MapNode;
 import eu.stratosphere.pact.compiler.plan.OptimizedPlan;
-import eu.stratosphere.pact.compiler.plan.OptimizerNode;
-import eu.stratosphere.pact.compiler.plan.PactConnection.TempMode;
+import eu.stratosphere.pact.compiler.util.DummyCrossStub;
 import eu.stratosphere.pact.compiler.util.DummyInputFormat;
 import eu.stratosphere.pact.compiler.util.DummyOutputFormat;
 import eu.stratosphere.pact.compiler.util.IdentityMap;
+import eu.stratosphere.pact.compiler.util.IdentityReduce;
 import eu.stratosphere.pact.compiler.util.MockDataStatistics;
 import eu.stratosphere.pact.runtime.task.MapTask;
 import eu.stratosphere.pact.runtime.task.TempTask;
 
 /**
- * Tests in this class:
+ * This class tests plans that once failed because of a bug:
  * <ul>
- *   <li> Tests that temp task is successfully shared with instance of output task if sender side temp task
- *   <li> Tests that temp task is successfully shared with instance of input task if receiver side temp task
+ *   <li> Ticket 158
  * </ul>
  *
  * @author Moritz Kaufmann
  */
-public class TempTaskSharingTest {	
+public class HardPlansCompilationTest {	
 	private static final String IN_FILE_1 = "file:///test/file";
 	
 	private static final String OUT_FILE_1 = "file///test/output";
@@ -86,89 +86,54 @@ public class TempTaskSharingTest {
 	}
 
 	@Test
-	public void testTempTaskReceiverSideSharing()
+	/**
+	 * Source -> Map -> Reduce -> Cross -> Reduce -> Cross -> Reduce ->
+     * |--------------------------/                  /
+     * |--------------------------------------------/
+     * 
+     * First cross has SameKeyFirst output contract
+	 */
+	public void testTicket158()
 	{
-		JobGraph jobGraph = generateTempTaskJobGraph(TempMode.TEMP_RECEIVER_SIDE);
-		
-		//Get temp task vertex and receiver side vertex (map task)
-		Iterator<JobTaskVertex> iter = jobGraph.getTaskVertices();
-		JobTaskVertex mapVertex = null;
-		JobTaskVertex tempVertex = null;
-		while(iter.hasNext()) {
-			JobTaskVertex nextVertex = iter.next();
-			if(nextVertex.getTaskClass() == MapTask.class) {
-				mapVertex = nextVertex;
-			}
-			else if(nextVertex.getTaskClass() == TempTask.class) {
-				tempVertex = nextVertex;
-			}
-		}
-		
-		AbstractJobVertex sharedVertex = tempVertex.getVertexToShareInstancesWith();
-		
-		//Check if temp task is properly shared
-		Assert.assertEquals(mapVertex, sharedVertex);
-	}
-	
-	@Test
-	public void testTempTaskSenderSideSharing()
-	{
-		JobGraph jobGraph = generateTempTaskJobGraph(TempMode.TEMP_SENDER_SIDE);
-		
-		//Get temp task vertex
-		Iterator<JobTaskVertex> iter = jobGraph.getTaskVertices();
-		JobTaskVertex tempVertex = null;
-		while(iter.hasNext()) {
-			JobTaskVertex nextVertex = iter.next();
-			if(nextVertex.getTaskClass() == TempTask.class) {
-				tempVertex = nextVertex;
-			}
-		}
-		
-		//Get sender side vertex (data source)
-		AbstractJobVertex sourceVertex = jobGraph.getInputVertices().next();
-		
-		AbstractJobVertex sharedVertex = tempVertex.getVertexToShareInstancesWith();
-		
-		//Check if temp task is properly shared
-		Assert.assertEquals(sourceVertex, sharedVertex);
-	}
-	
-	private JobGraph generateTempTaskJobGraph(final TempMode mode) {
 		// construct the plan
 		DataSourceContract<PactInteger, PactInteger> source = new DataSourceContract<PactInteger, PactInteger>(DummyInputFormat.class, IN_FILE_1, "Source");
 		source.setDegreeOfParallelism(defaultParallelism);
 		
-		MapContract<PactInteger, PactInteger, PactInteger, PactInteger> map1 = new MapContract<PactInteger, PactInteger, PactInteger, PactInteger>(IdentityMap.class, "Map1");
-		map1.setDegreeOfParallelism(defaultParallelism);
-		map1.setInput(source);
+		MapContract<PactInteger, PactInteger, PactInteger, PactInteger> map = new MapContract<PactInteger, PactInteger, PactInteger, PactInteger>(IdentityMap.class, "Map1");
+		map.setDegreeOfParallelism(defaultParallelism);
+		map.setInput(source);
+		
+		ReduceContract<PactInteger, PactInteger, PactInteger, PactInteger> reduce1 = new ReduceContract<PactInteger, PactInteger, PactInteger, PactInteger>(IdentityReduce.class, "Reduce1");
+		reduce1.setDegreeOfParallelism(defaultParallelism);
+		reduce1.setInput(map);
+		
+		CrossContract<PactInteger, PactInteger, PactInteger, PactInteger, PactInteger, PactInteger> cross1 = new CrossContract<PactInteger, PactInteger, PactInteger, PactInteger, PactInteger, PactInteger>(DummyCrossStub.class, "Cross1");
+		cross1.setDegreeOfParallelism(defaultParallelism);
+		cross1.setFirstInput(reduce1);
+		cross1.setSecondInput(source);
+		cross1.setOutputContract(SameKeyFirst.class);
+		
+		ReduceContract<PactInteger, PactInteger, PactInteger, PactInteger> reduce2 = new ReduceContract<PactInteger, PactInteger, PactInteger, PactInteger>(IdentityReduce.class, "Reduce2");
+		reduce2.setDegreeOfParallelism(defaultParallelism);
+		reduce2.setInput(cross1);
+		
+		CrossContract<PactInteger, PactInteger, PactInteger, PactInteger, PactInteger, PactInteger> cross2 = new CrossContract<PactInteger, PactInteger, PactInteger, PactInteger, PactInteger, PactInteger>(DummyCrossStub.class, "Cross2");
+		cross2.setDegreeOfParallelism(defaultParallelism);
+		cross2.setFirstInput(reduce2);
+		cross2.setSecondInput(source);
+		
+		ReduceContract<PactInteger, PactInteger, PactInteger, PactInteger> reduce3 = new ReduceContract<PactInteger, PactInteger, PactInteger, PactInteger>(IdentityReduce.class, "Reduce3");
+		reduce3.setDegreeOfParallelism(defaultParallelism);
+		reduce3.setInput(cross2);
 		
 		DataSinkContract<PactInteger, PactInteger> sink = new DataSinkContract<PactInteger, PactInteger>(DummyOutputFormat.class, OUT_FILE_1, "Sink");
 		sink.setDegreeOfParallelism(defaultParallelism);
-		sink.setInput(map1);
+		sink.setInput(reduce3);
 		
 		Plan plan = new Plan(sink, "Test Temp Task");
 		
 		OptimizedPlan oPlan = this.compiler.compile(plan, this.instanceType);
-		
-		//Inject temp strategy artificially
-		oPlan.accept(new Visitor<OptimizerNode>() {
-			@Override
-			public boolean preVisit(OptimizerNode visitable) {
-				if(visitable instanceof MapNode) {
-					//Only OptimizerNode is MapContract
-					visitable.getIncomingConnections().get(0).setTempMode(mode);
-					return false;
-				}
-				return true;
-			}
-			
-			@Override
-			public void postVisit(OptimizerNode visitable) {
-			}
-		});
-		
 		JobGraphGenerator jobGen = new JobGraphGenerator();
-		return jobGen.compileJobGraph(oPlan);
+		jobGen.compileJobGraph(oPlan);
 	}
 }
