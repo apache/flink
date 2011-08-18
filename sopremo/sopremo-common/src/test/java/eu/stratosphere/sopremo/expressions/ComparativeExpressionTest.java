e7f0e3c8a987f757884b8bc653db02347fdc93e5
Michael Hopstock <michael.hopstock@student.hpi.uni-potsdam.de>
2011-08-18 17:41:51 +0200

    made ComparativeExpressionTest to a parametrisized Test to include more TestCases

--- sopremo/sopremo-common/src/test/java/eu/stratosphere/sopremo/expressions/ComparativeExpressionTest.java
+++ sopremo/sopremo-common/src/test/java/eu/stratosphere/sopremo/expressions/ComparativeExpressionTest.java
@@ -1,5 +1,8 @@
 package eu.stratosphere.sopremo.expressions;
 
+import java.util.Arrays;
+import java.util.List;
+
 import junit.framework.Assert;
 
 import org.codehaus.jackson.JsonNode;
@@ -7,43 +10,72 @@ import org.codehaus.jackson.node.BooleanNode;
 import org.codehaus.jackson.node.IntNode;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import eu.stratosphere.sopremo.expressions.ComparativeExpression.BinaryOperator;
 
+@RunWith(Parameterized.class)
 public class ComparativeExpressionTest extends EvaluableExpressionTest<ComparativeExpression> {
+
+	private int expr1, expr2;
+
+	private BinaryOperator op;
+
+	private BooleanNode ExpectedResult;
+
+	public ComparativeExpressionTest(int expr1, BinaryOperator op, int expr2, BooleanNode ExpectedResult) {
+		this.expr1 = expr1;
+		this.expr2 = expr2;
+		this.op = op;
+		this.ExpectedResult = ExpectedResult;
+	}
 
 	@Override
 	protected ComparativeExpression createDefaultInstance(int index) {
 		return new ComparativeExpression(new UnaryExpression(new ConstantExpression(IntNode.valueOf(index))),
 			BinaryOperator.NOT_EQUAL, new UnaryExpression(new ConstantExpression(IntNode.valueOf(index + 1))));
-		// return super.createDefaultInstance(index);
 	}
 
 	@Ignore
-	public JsonNode Evaluate(BinaryOperator op) {
+	public JsonNode Evaluate(int expr1, BinaryOperator op, int expr2) {
 		return new ComparativeExpression(new InputSelection(0), op, new InputSelection(1)).evaluate(
-			createArrayNode(IntNode.valueOf(42), IntNode.valueOf(23)), this.context);
+			createArrayNode(IntNode.valueOf(expr1), IntNode.valueOf(expr2)), this.context);
 
 	}
 
 	@Test
-	public void ShouldReturnTrueIfEvaluationReturnsTrue() {
-		JsonNode result = this.Evaluate(BinaryOperator.GREATER);
-		Assert.assertEquals(BooleanNode.TRUE, result);
-		result = this.Evaluate(BinaryOperator.GREATER_EQUAL);
-		Assert.assertEquals(BooleanNode.TRUE, result);
-		result = this.Evaluate(BinaryOperator.NOT_EQUAL);
-		Assert.assertEquals(BooleanNode.TRUE, result);
+	public void ShouldReturnExpectedResultsForComparators() {
+		JsonNode result = this.Evaluate(this.expr1, this.op, this.expr2);
+		Assert.assertEquals(this.ExpectedResult, result);
 
 	}
 
-	@Test
-	public void ShouldReturnFalseIfEvaluationReturnsFalse() {
-		JsonNode result = this.Evaluate(BinaryOperator.EQUAL);
-		Assert.assertEquals(BooleanNode.FALSE, result);
-		result = this.Evaluate(BinaryOperator.LESS);
-		Assert.assertEquals(BooleanNode.FALSE, result);
-		result = this.Evaluate(BinaryOperator.LESS_EQUAL);
-		Assert.assertEquals(BooleanNode.FALSE, result);
+	@Parameters
+	public static List<Object[]> combinations() {
+		return Arrays.asList(new Object[][] {
+			{ 42, BinaryOperator.EQUAL, 42, BooleanNode.TRUE },
+			{ 42, BinaryOperator.NOT_EQUAL, 42, BooleanNode.FALSE },
+			{ 42, BinaryOperator.GREATER, 42, BooleanNode.FALSE },
+			{ 42, BinaryOperator.GREATER_EQUAL, 42, BooleanNode.TRUE },
+			{ 42, BinaryOperator.LESS, 42, BooleanNode.FALSE },
+			{ 42, BinaryOperator.LESS_EQUAL, 42, BooleanNode.TRUE },
+
+			{ 42, BinaryOperator.EQUAL, 23, BooleanNode.FALSE },
+			{ 42, BinaryOperator.NOT_EQUAL, 23, BooleanNode.TRUE },
+			{ 42, BinaryOperator.GREATER, 23, BooleanNode.TRUE },
+			{ 42, BinaryOperator.GREATER_EQUAL, 23, BooleanNode.TRUE },
+			{ 42, BinaryOperator.LESS, 23, BooleanNode.FALSE },
+			{ 42, BinaryOperator.LESS_EQUAL, 23, BooleanNode.FALSE },
+			
+			{ 23, BinaryOperator.EQUAL, 42, BooleanNode.FALSE },
+			{ 23, BinaryOperator.NOT_EQUAL, 42, BooleanNode.TRUE },
+			{ 23, BinaryOperator.GREATER, 42, BooleanNode.FALSE },
+			{ 23, BinaryOperator.GREATER_EQUAL, 42, BooleanNode.FALSE },
+			{ 23, BinaryOperator.LESS, 42, BooleanNode.TRUE },
+			{ 23, BinaryOperator.LESS_EQUAL, 42, BooleanNode.TRUE }
+			
+		});
 	}
 }
