e68b9038b64e1b3ad8068541216baf17da23ddde
Matthias Ringwald <matthias.ringwald@campus.tu-berlin.de>
2012-01-27 15:39:44 +0100

    Fixed union bug in self-match

--- pact/pact-compiler/src/main/java/eu/stratosphere/pact/compiler/plan/MatchNode.java
+++ pact/pact-compiler/src/main/java/eu/stratosphere/pact/compiler/plan/MatchNode.java
@@ -804,7 +804,7 @@ public class MatchNode extends TwoInputNode {
 			gp1 = new GlobalProperties();
 		}
 
-		if(preds2.size() == 1) {
+		if(preds2 != null && preds2.size() == 1) {
 			gp2 = PactConnection.getGlobalPropertiesAfterConnection(preds2.get(0), this, ss2);
 		} else {
 			// TODO right now we drop all properties in the union case; need to figure out what properties can be kept
