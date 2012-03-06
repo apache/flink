6534ac6c0128340a53656fc23f73f96fc5c5c184
Daniel Warneke <daniel.warneke@tu-berlin.de>
2012-03-06 13:53:17 +0100

    Added missing Stratosphere headers

--- nephele/nephele-server/src/main/java/eu/stratosphere/nephele/checkpointing/ReplayOutputGateContext.java
+++ nephele/nephele-server/src/main/java/eu/stratosphere/nephele/checkpointing/ReplayOutputGateContext.java
@@ -1,3 +1,18 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
 package eu.stratosphere.nephele.checkpointing;
 
 import eu.stratosphere.nephele.io.GateID;
@@ -38,7 +53,8 @@ final class ReplayOutputGateContext extends AbstractReplayGateContext implements
 		final SpillingBarrier spillingBarrier = new SpillingBarrier(isReceiverRunning, mergeSpillBuffers,
 			runtimeDispatcher);
 		final ForwardingBarrier forwardingBarrier = new ForwardingBarrier(channelID, spillingBarrier);
-		final ReplayOutputChannelBroker outputChannelBroker = new ReplayOutputChannelBroker(this.taskContext, forwardingBarrier);
+		final ReplayOutputChannelBroker outputChannelBroker = new ReplayOutputChannelBroker(this.taskContext,
+			forwardingBarrier);
 
 		final OutputChannelForwardingChain forwardingChain = new OutputChannelForwardingChain(outputChannelBroker,
 			runtimeDispatcher);
