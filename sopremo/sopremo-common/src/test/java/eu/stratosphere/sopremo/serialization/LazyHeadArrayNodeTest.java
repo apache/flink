9f9bf2d5097433860725e5c7422e9c9e269a691b
Michael Hopstock <michael.hopstock@student.hpi.uni-potsdam.de>
2012-04-20 13:56:18 +0200

    fixed wrong Schema used in LazyHeadArrayNodeTest

--- sopremo/sopremo-common/src/test/java/eu/stratosphere/sopremo/serialization/LazyHeadArrayNodeTest.java
+++ sopremo/sopremo-common/src/test/java/eu/stratosphere/sopremo/serialization/LazyHeadArrayNodeTest.java
@@ -12,16 +12,16 @@ import eu.stratosphere.sopremo.type.ArrayNodeBaseTest;
 import eu.stratosphere.sopremo.type.IArrayNode;
 import eu.stratosphere.sopremo.type.IntNode;
 
-public class LazyHeadArrayNodeTest extends ArrayNodeBaseTest<LazyTailArrayNode> {
+public class LazyHeadArrayNodeTest extends ArrayNodeBaseTest<LazyHeadArrayNode> {
 
 	@Override
 	public void initArrayNode() {
-		TailArraySchema schema = new TailArraySchema();
+		HeadArraySchema schema = new HeadArraySchema();
 		schema.setHeadSize(5);
 		PactRecord record = schema.jsonToRecord(
 			new ArrayNode(IntNode.valueOf(0), IntNode.valueOf(1), IntNode.valueOf(2)), null, null);
 
-		this.node = new LazyTailArrayNode(record, schema);
+		this.node = new LazyHeadArrayNode(record, schema);
 	}
 
 	@Test
