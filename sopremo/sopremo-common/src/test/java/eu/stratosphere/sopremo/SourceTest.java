d144cd6715b8b9d04ce894f417b23d77c7922b4e
arvid <arvid.heise@hpi.uni-potsdam.de>
2012-04-25 14:12:43 +0200

    Fixed junit test

--- sopremo/sopremo-common/src/test/java/eu/stratosphere/sopremo/SourceTest.java
+++ sopremo/sopremo-common/src/test/java/eu/stratosphere/sopremo/SourceTest.java
@@ -1,9 +1,12 @@
 package eu.stratosphere.sopremo;
 
+import org.junit.Test;
+
 import eu.stratosphere.sopremo.expressions.ConstantExpression;
 
 public class SourceTest extends SopremoTest<Source> {
 	@Override
+	@Test
 	public void shouldComplyEqualsContract() {
 		super.shouldComplyEqualsContract(new Source(new ConstantExpression(0)), new Source(new ConstantExpression(1)),
 			new Source("2"), new Source("3"));
