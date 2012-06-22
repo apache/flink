f8ef6f4217533a5355b0751b27b4146475fb18a3
arvid <arvid.heise@hpi.uni-potsdam.de>
2012-06-22 18:38:45 +0200

    Added sopremo query package

---
+++ sopremo/sopremo-query/src/main/java/eu/stratosphere/sopremo/util/QueryUtil.java
@@ -0,0 +1,19 @@
+package eu.stratosphere.sopremo.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.impl.Log4JLogger;
+import org.apache.log4j.Level;
+
+public class QueryUtil {
+
+	protected static final Log LOG = LogFactory.getLog(AbstractQueryParser.class);
+
+	public static void trace() {
+		(((Log4JLogger) LOG).getLogger()).setLevel(Level.TRACE);
+	}
+
+	public static void untrace() {
+		(((Log4JLogger) LOG).getLogger()).setLevel((((Log4JLogger) LOG).getLogger()).getParent().getLevel());
+	}
+}
