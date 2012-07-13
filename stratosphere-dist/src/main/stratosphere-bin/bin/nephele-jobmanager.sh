6caf71174d20454d483b0457013f1b00be13a1ee
Matthias Ringwald <matthias.ringwald@campus.tu-berlin.de>
2012-07-13 11:13:57 +0200

    Added guava dependency to nephele job and taskmanager startup scripts

--- stratosphere-dist/src/main/stratosphere-bin/bin/nephele-jobmanager.sh
+++ stratosphere-dist/src/main/stratosphere-bin/bin/nephele-jobmanager.sh
@@ -90,6 +90,8 @@ constructJobManagerClassPath() {
 			add=1
 		elif [[ "$jarfile" =~ 'aws-java-sdk' ]]; then
 			add=1
+		elif [[ "$jarfile" =~ 'guava' ]]; then
+			add=1
 		fi
 
 		if [[ "$add" = "1" ]]; then
