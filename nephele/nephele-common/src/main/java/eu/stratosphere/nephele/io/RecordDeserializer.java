74e2c4d25dc25883a5cf1479f55dce9870c195a0
Daniel Warneke <warneke@icsi.berkeley.edu>
2012-10-14 21:44:43 -0700

    Worked on new record serialization

--- nephele/nephele-common/src/main/java/eu/stratosphere/nephele/io/RecordDeserializer.java
+++ nephele/nephele-common/src/main/java/eu/stratosphere/nephele/io/RecordDeserializer.java
@@ -22,23 +22,28 @@ import java.nio.channels.ReadableByteChannel;
  * This interface must be implemented by classes which transfer bytes streams back into {@link Record} objects.
  * 
  * @author Stephan Ewen
- * @param <T> The type of record this record deserializer works with.
+ * @param <T>
+ *        The type of record this record deserializer works with.
  */
-public interface RecordDeserializer<T>
-{
+public interface RecordDeserializer<T> {
+
 	/**
-	 * Transforms a record back from a readable byte channel. The deserialization might not complete, because the channel
+	 * Transforms a record back from a readable byte channel. The deserialization might not complete, because the
+	 * channel
 	 * has not all required data available. In that case, this method must return {@code null}. Furthermore, it may
 	 * not retain a reference to the given target object in that case, but must manage to put the data aside.
 	 * 
-	 * @param target The record object into which to deserialize the data. May be null for deserializers
-	 *               that operate on immutable objects, in which case the deserializer has to instantiate an
-	 *               object. In the case where this object is non-null, but the deserialization does not complete,
-	 *               the object must not be used to cache the partial state, as it is not guaranteed that the object
-	 *               will remain unchanged until the next attempt to continue the deserialization.
-	 * @param in The byte stream which contains the record's data.
+	 * @param target
+	 *        The record object into which to deserialize the data. May be null for deserializers
+	 *        that operate on immutable objects, in which case the deserializer has to instantiate an
+	 *        object. In the case where this object is non-null, but the deserialization does not complete,
+	 *        the object must not be used to cache the partial state, as it is not guaranteed that the object
+	 *        will remain unchanged until the next attempt to continue the deserialization.
+	 * @param in
+	 *        The byte stream which contains the record's data.
 	 * @return The record deserialized from <code>in</code>, or null, if the record .
-	 * @throws IOException Thrown if an I/O error occurs while deserializing the record from the stream
+	 * @throws IOException
+	 *         Thrown if an I/O error occurs while deserializing the record from the stream
 	 */
 	T readData(final T target, final ReadableByteChannel readableByteChannel) throws IOException;
 
@@ -46,9 +51,10 @@ public interface RecordDeserializer<T>
 	 * Clears the internal buffers of the deserializer and resets its state.
 	 */
 	void clear();
-	
+
 	/**
-	 * Checks whether the deserializer has data from a previous deserialization attempt stored in its internal buffers which
+	 * Checks whether the deserializer has data from a previous deserialization attempt stored in its internal buffers
+	 * which
 	 * is not yet finished.
 	 * 
 	 * @return <code>true</code>, if the deserializer's internal buffers contain data from a previous deserialization
---
+++ nephele/nephele-server/src/main/java/eu/stratosphere/nephele/io/channels/bytebuffered/TestRecordDeserializer.java
@@ -0,0 +1,139 @@
+package eu.stratosphere.nephele.io.channels.bytebuffered;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+
+import eu.stratosphere.nephele.io.RecordDeserializer;
+import eu.stratosphere.nephele.io.channels.Buffer;
+import eu.stratosphere.nephele.io.channels.MemoryBuffer;
+import eu.stratosphere.nephele.types.Record;
+
+public class TestRecordDeserializer<T extends Record> implements RecordDeserializer<T> {
+
+	private static final class DataInputWrapper implements DataInput {
+
+		private final ByteBuffer buffer;
+
+		private DataInputWrapper(final ByteBuffer buffer) {
+			this.buffer = buffer;
+		}
+
+		@Override
+		public void readFully(byte[] b) throws IOException {
+			this.buffer.get(b);
+		}
+
+		@Override
+		public void readFully(byte[] b, int off, int len) throws IOException {
+			this.buffer.get(b, off, len);
+		}
+
+		@Override
+		public int skipBytes(int n) throws IOException {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean readBoolean() throws IOException {
+			return (this.buffer.get() == 1);
+		}
+
+		@Override
+		public byte readByte() throws IOException {
+			return this.buffer.get();
+		}
+
+		@Override
+		public int readUnsignedByte() throws IOException {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public short readShort() throws IOException {
+			return this.buffer.getShort();
+		}
+
+		@Override
+		public int readUnsignedShort() throws IOException {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public char readChar() throws IOException {
+			return this.buffer.getChar();
+		}
+
+		@Override
+		public int readInt() throws IOException {
+			return this.buffer.getInt();
+		}
+
+		@Override
+		public long readLong() throws IOException {
+			return this.buffer.getLong();
+		}
+
+		@Override
+		public float readFloat() throws IOException {
+			return this.buffer.getFloat();
+		}
+
+		@Override
+		public double readDouble() throws IOException {
+			return this.buffer.getDouble();
+		}
+
+		@Override
+		public String readLine() throws IOException {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public String readUTF() throws IOException {
+			throw new UnsupportedOperationException();
+		}
+
+	}
+
+	private Buffer lastBuffer = null;
+
+	private DataInputWrapper wrapper = null;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public T readData(final T target, final ReadableByteChannel readableByteChannel) throws IOException {
+
+		final Buffer buffer = (Buffer) readableByteChannel;
+
+		if (buffer != this.lastBuffer) {
+			this.wrapper = new DataInputWrapper(((MemoryBuffer) buffer).getByteBuffer());
+			this.lastBuffer = buffer;
+		}
+
+		target.read(this.wrapper);
+
+		return target;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void clear() {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean hasUnfinishedData() {
+
+		return false;
+	}
+
+}
