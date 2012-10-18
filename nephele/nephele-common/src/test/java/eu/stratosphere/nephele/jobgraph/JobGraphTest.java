040f74b2ee0b2f77702ccc42cb4ca88558d64002
Daniel Warneke <warneke@icsi.berkeley.edu>
2012-10-17 19:49:35 -0700

    Removed duplicate Apache license notice

--- nephele/nephele-common/src/test/java/eu/stratosphere/nephele/jobgraph/JobGraphTest.java
+++ nephele/nephele-common/src/test/java/eu/stratosphere/nephele/jobgraph/JobGraphTest.java
@@ -13,23 +13,6 @@
  *
  **********************************************************************************************************************/
 
-/*
- *  Copyright 2010 casp.
- * 
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- * 
- *       http://www.apache.org/licenses/LICENSE-2.0
- * 
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *  under the License.
- */
-
 package eu.stratosphere.nephele.jobgraph;
 
 import eu.stratosphere.nephele.jobgraph.JobGraph;
@@ -47,12 +30,10 @@ import static org.junit.Assert.*;
 
 /**
  * This class contains tests related to the JobGraph
+ * 
  * @author casp
  */
 public class JobGraphTest {
-
-	public JobGraphTest() {
-	}
 
 	@BeforeClass
 	public static void setUpClass() throws Exception {
