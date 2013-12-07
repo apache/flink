stratosphere/stratosphere

Rename Pregelsphere to Spargel

stratosphere-addons/spargel/src/main/java/eu/stratosphere/pact/spargel/MessagingFunction.java
@@ -12,7 +12,7 @@
  * specific language governing permissions and limitations under the License.
  *
  **********************************************************************************************************************/
-package eu.stratosphere.pact.vertexcentric;
+package eu.stratosphere.pact.spargel;
 
 import java.io.Serializable;
 import java.util.Iterator;

stratosphere-addons/spargel/src/main/java/eu/stratosphere/pact/spargel/SpargelIteration.java
@@ -12,7 +12,7 @@
  * specific language governing permissions and limitations under the License.
  *
  **********************************************************************************************************************/
-package eu.stratosphere.pact.vertexcentric;
+package eu.stratosphere.pact.spargel;
 
 import java.io.IOException;
 import java.util.Iterator;
@@ -29,10 +29,10 @@ import eu.stratosphere.pact.common.util.InstantiationUtil;
 import eu.stratosphere.pact.common.util.ReflectionUtil;
 import eu.stratosphere.pact.generic.contract.Contract;
 import eu.stratosphere.pact.generic.contract.WorksetIteration;
-import eu.stratosphere.pact.vertexcentric.util.MessageIterator;
+import eu.stratosphere.pact.spargel.util.MessageIterator;
 
 
-public class VertexCentricIteration {
+public class SpargelIteration {
 	
 	private static final String DEFAULT_NAME = "<unnamed vertex-centric iteration>";
 	
@@ -50,16 +50,16 @@ public class VertexCentricIteration {
 	// ----------------------------------------------------------------------------------
 	
 	public <VertexKey extends Key, VertexValue extends Value, Message extends Value, EdgeValue extends Value>
-		VertexCentricIteration(MessagingFunction<VertexKey, VertexValue, Message, EdgeValue> mf, 
-							   VertexUpdateFunction<VertexKey, VertexValue, Message> uf)
+    SpargelIteration(MessagingFunction<VertexKey, VertexValue, Message, EdgeValue> mf,
+                     VertexUpdateFunction<VertexKey, VertexValue, Message> uf)
 	{
 		this(mf, uf, DEFAULT_NAME);
 	}
 	
 	public <VertexKey extends Key, VertexValue extends Value, Message extends Value, EdgeValue extends Value>
-		VertexCentricIteration(MessagingFunction<VertexKey, VertexValue, Message, EdgeValue> mf, 
-							   VertexUpdateFunction<VertexKey, VertexValue, Message> uf,
-							   String name)
+    SpargelIteration(MessagingFunction<VertexKey, VertexValue, Message, EdgeValue> mf,
+                     VertexUpdateFunction<VertexKey, VertexValue, Message> uf,
+                     String name)
 	{
 		// get the types
 		this.vertexKey = ReflectionUtil.getTemplateType1(mf.getClass());

stratosphere-addons/spargel/src/main/java/eu/stratosphere/pact/spargel/examples/connectedcomponents/LongLongInputFormat.java
@@ -13,7 +13,7 @@
  *
  **********************************************************************************************************************/
 
-package eu.stratosphere.pact.vertexcentric.examples.cc;
+package eu.stratosphere.pact.spargel.examples.connectedcomponents;
 
 import eu.stratosphere.pact.common.io.TextInputFormat;
 import eu.stratosphere.pact.common.type.PactRecord;

stratosphere-addons/spargel/src/main/java/eu/stratosphere/pact/spargel/examples/connectedcomponents/SpargelConnectedComponents.java
@@ -12,7 +12,7 @@
  * specific language governing permissions and limitations under the License.
  *
  **********************************************************************************************************************/
-package eu.stratosphere.pact.vertexcentric.examples.cc;
+package eu.stratosphere.pact.spargel.examples.connectedcomponents;
 
 import java.util.Iterator;
 
@@ -25,24 +25,23 @@ import eu.stratosphere.pact.common.plan.PlanAssembler;
 import eu.stratosphere.pact.common.plan.PlanAssemblerDescription;
 import eu.stratosphere.pact.common.type.base.PactLong;
 import eu.stratosphere.pact.common.type.base.PactNull;
-import eu.stratosphere.pact.vertexcentric.MessagingFunction;
-import eu.stratosphere.pact.vertexcentric.VertexCentricIteration;
-import eu.stratosphere.pact.vertexcentric.VertexUpdateFunction;
+import eu.stratosphere.pact.spargel.MessagingFunction;
+import eu.stratosphere.pact.spargel.SpargelIteration;
+import eu.stratosphere.pact.spargel.VertexUpdateFunction;
 
-
-public class VertexCentricCC implements PlanAssembler, PlanAssemblerDescription {
+public class SpargelConnectedComponents implements PlanAssembler, PlanAssemblerDescription {
 	
 	public static void main(String[] args) throws Exception {
-		LocalExecutor.execute(new VertexCentricCC(), args);
+		LocalExecutor.execute(new SpargelConnectedComponents(), args);
 	}
 	
 	@Override
 	public Plan getPlan(String... args) {
 		final int dop = args.length > 0 ? Integer.parseInt(args[0]) : 1;
-		final String verticesPath = args.length > 1 ? args[1] : "file:///data/demodata/cc/vertices";
-		final String edgesPath = args.length > 2 ? args[2] : "file:///data/demodata/cc/edges";
-		final String resultPath = args.length > 3 ? args[3] : "file:///tmp";
-		final int maxIterations = args.length > 4 ? Integer.parseInt(args[4]) : 10;
+		final String verticesPath = args.length > 1 ? args[1] : "";
+		final String edgesPath = args.length > 2 ? args[2] : "";
+		final String resultPath = args.length > 3 ? args[3] : "";
+		final int maxIterations = args.length > 4 ? Integer.parseInt(args[4]) : 1;
 		
 		FileDataSource initialVertices = new FileDataSource(DuplicateLongInputFormat.class, verticesPath, "Vertices");
 		FileDataSource edges = new FileDataSource(LongLongInputFormat.class, edgesPath, "Edges");
@@ -54,13 +53,13 @@ public class VertexCentricCC implements PlanAssembler, PlanAssemblerDescription 
 			.field(PactLong.class, 0)
 			.field(PactLong.class, 1);
 		
-		VertexCentricIteration iteration = new VertexCentricIteration(
-			new CCMessager(), new CCUpdater(), "Connected Components Iteration.");
+		SpargelIteration iteration = new SpargelIteration(
+			new CCMessager(), new CCUpdater(), "Connected Components (Spargel API)");
 		iteration.setVertexInput(initialVertices);
 		iteration.setEdgesInput(edges);
 		iteration.setNumberOfIterations(maxIterations);
 		result.setInput(iteration.getOutput());
-		
+
 		Plan p = new Plan(result);
 		p.setDefaultParallelism(dop);
 		return p;
@@ -92,12 +91,12 @@ public class VertexCentricCC implements PlanAssembler, PlanAssemblerDescription 
 		@Override
 		public void sendMessages(PactLong vertexId, PactLong componentId) {
 			sendMessageToAllTargets(componentId);
-		}
+        }
+
 	}
 
 	@Override
 	public String getDescription() {
 		return "<dop> <vertices> <edges> <result> <maxIterations>";
 	}
-
 }

stratosphere-addons/spargel/src/main/java/eu/stratosphere/pact/spargel/util/MessageIterator.java
@@ -12,28 +12,40 @@
  * specific language governing permissions and limitations under the License.
  *
  **********************************************************************************************************************/
-package eu.stratosphere.pact.vertexcentric;
+package eu.stratosphere.pact.spargel.util;
 
+import java.util.Iterator;
 
-import eu.stratosphere.pact.common.type.Key;
+import eu.stratosphere.pact.common.type.PactRecord;
 import eu.stratosphere.pact.common.type.Value;
 
 
-public final class Edge<VertexKey extends Key, EdgeValue extends Value> {
+public final class MessageIterator<Message extends Value> implements Iterator<Message> {
+
+	private final Message instance;
+	private Iterator<PactRecord> source;
 	
-	private VertexKey target;
-	private EdgeValue edgeValue;
-	
-	void set(VertexKey target, EdgeValue edgeValue) {
-		this.target = target;
-		this.edgeValue = edgeValue;
+	public MessageIterator(Message instance) {
+		this.instance = instance;
 	}
 	
-	public VertexKey target() {
-		return target;
+	public final void setSource(Iterator<PactRecord> source) {
+		this.source = source;
 	}
 	
-	public EdgeValue edgeValue() {
-		return edgeValue;
+	@Override
+	public final boolean hasNext() {
+		return this.source.hasNext();
+	}
+	
+	@Override
+	public final Message next() {
+		this.source.next().getFieldInto(1, this.instance);
+		return this.instance;
+	}
+
+	@Override
+	public final void remove() {
+		throw new UnsupportedOperationException();
 	}
 }

apache/flink

stratosphere-addons/spargel/src/main/java/eu/stratosphere/spargel/java/MessagingFunction.java
@@ -12,7 +12,7 @@
  * specific language governing permissions and limitations under the License.
  *
  **********************************************************************************************************************/
-package eu.stratosphere.pact.vertexcentric;
+package eu.stratosphere.spargel.java;
 
 import java.io.Serializable;
 import java.util.Iterator;

stratosphere-addons/spargel/src/main/java/eu/stratosphere/spargel/java/SpargelIteration.java
@@ -12,7 +12,7 @@
  * specific language governing permissions and limitations under the License.
  *
  **********************************************************************************************************************/
-package eu.stratosphere.pact.vertexcentric;
+package eu.stratosphere.spargel.java;
 
 import java.io.IOException;
 import java.util.Iterator;
@@ -29,10 +29,10 @@ import eu.stratosphere.pact.common.util.InstantiationUtil;
 import eu.stratosphere.pact.common.util.ReflectionUtil;
 import eu.stratosphere.pact.generic.contract.Contract;
 import eu.stratosphere.pact.generic.contract.WorksetIteration;
-import eu.stratosphere.pact.vertexcentric.util.MessageIterator;
+import eu.stratosphere.spargel.java.util.MessageIterator;
 
 
-public class VertexCentricIteration {
+public class SpargelIteration {
 	
 	private static final String DEFAULT_NAME = "<unnamed vertex-centric iteration>";
 	
@@ -50,16 +50,16 @@ public class VertexCentricIteration {
 	// ----------------------------------------------------------------------------------
 	
 	public <VertexKey extends Key, VertexValue extends Value, Message extends Value, EdgeValue extends Value>
-		VertexCentricIteration(MessagingFunction<VertexKey, VertexValue, Message, EdgeValue> mf, 
-							   VertexUpdateFunction<VertexKey, VertexValue, Message> uf)
+    SpargelIteration(MessagingFunction<VertexKey, VertexValue, Message, EdgeValue> mf,
+                     VertexUpdateFunction<VertexKey, VertexValue, Message> uf)
 	{
 		this(mf, uf, DEFAULT_NAME);
 	}
 	
 	public <VertexKey extends Key, VertexValue extends Value, Message extends Value, EdgeValue extends Value>
-		VertexCentricIteration(MessagingFunction<VertexKey, VertexValue, Message, EdgeValue> mf, 
-							   VertexUpdateFunction<VertexKey, VertexValue, Message> uf,
-							   String name)
+    SpargelIteration(MessagingFunction<VertexKey, VertexValue, Message, EdgeValue> mf,
+                     VertexUpdateFunction<VertexKey, VertexValue, Message> uf,
+                     String name)
 	{
 		// get the types
 		this.vertexKey = ReflectionUtil.getTemplateType1(mf.getClass());

stratosphere-addons/spargel/src/main/java/eu/stratosphere/spargel/java/examples/connectedcomponents/LongLongInputFormat.java
@@ -13,7 +13,7 @@
  *
  **********************************************************************************************************************/
 
-package eu.stratosphere.pact.vertexcentric.examples.cc;
+package eu.stratosphere.spargel.java.examples.connectedcomponents;
 
 import eu.stratosphere.pact.common.io.TextInputFormat;
 import eu.stratosphere.pact.common.type.PactRecord;

stratosphere-addons/spargel/src/main/java/eu/stratosphere/spargel/java/examples/connectedcomponents/SpargelConnectedComponents.java
@@ -12,7 +12,7 @@
  * specific language governing permissions and limitations under the License.
  *
  **********************************************************************************************************************/
-package eu.stratosphere.pact.vertexcentric.examples.cc;
+package eu.stratosphere.spargel.java.examples.connectedcomponents;
 
 import java.util.Iterator;
 
@@ -25,24 +25,23 @@ import eu.stratosphere.pact.common.plan.PlanAssembler;
 import eu.stratosphere.pact.common.plan.PlanAssemblerDescription;
 import eu.stratosphere.pact.common.type.base.PactLong;
 import eu.stratosphere.pact.common.type.base.PactNull;
-import eu.stratosphere.pact.vertexcentric.MessagingFunction;
-import eu.stratosphere.pact.vertexcentric.VertexCentricIteration;
-import eu.stratosphere.pact.vertexcentric.VertexUpdateFunction;
+import eu.stratosphere.spargel.java.MessagingFunction;
+import eu.stratosphere.spargel.java.SpargelIteration;
+import eu.stratosphere.spargel.java.VertexUpdateFunction;
 
-
-public class VertexCentricCC implements PlanAssembler, PlanAssemblerDescription {
+public class SpargelConnectedComponents implements PlanAssembler, PlanAssemblerDescription {
 	
 	public static void main(String[] args) throws Exception {
-		LocalExecutor.execute(new VertexCentricCC(), args);
+		LocalExecutor.execute(new SpargelConnectedComponents(), args);
 	}
 	
 	@Override
 	public Plan getPlan(String... args) {
 		final int dop = args.length > 0 ? Integer.parseInt(args[0]) : 1;
-		final String verticesPath = args.length > 1 ? args[1] : "file:///data/demodata/cc/vertices";
-		final String edgesPath = args.length > 2 ? args[2] : "file:///data/demodata/cc/edges";
-		final String resultPath = args.length > 3 ? args[3] : "file:///tmp";
-		final int maxIterations = args.length > 4 ? Integer.parseInt(args[4]) : 10;
+		final String verticesPath = args.length > 1 ? args[1] : "";
+		final String edgesPath = args.length > 2 ? args[2] : "";
+		final String resultPath = args.length > 3 ? args[3] : "";
+		final int maxIterations = args.length > 4 ? Integer.parseInt(args[4]) : 1;
 		
 		FileDataSource initialVertices = new FileDataSource(DuplicateLongInputFormat.class, verticesPath, "Vertices");
 		FileDataSource edges = new FileDataSource(LongLongInputFormat.class, edgesPath, "Edges");
@@ -54,13 +53,13 @@ public class VertexCentricCC implements PlanAssembler, PlanAssemblerDescription 
 			.field(PactLong.class, 0)
 			.field(PactLong.class, 1);
 		
-		VertexCentricIteration iteration = new VertexCentricIteration(
-			new CCMessager(), new CCUpdater(), "Connected Components Iteration.");
+		SpargelIteration iteration = new SpargelIteration(
+			new CCMessager(), new CCUpdater(), "Connected Components (Spargel API)");
 		iteration.setVertexInput(initialVertices);
 		iteration.setEdgesInput(edges);
 		iteration.setNumberOfIterations(maxIterations);
 		result.setInput(iteration.getOutput());
-		
+
 		Plan p = new Plan(result);
 		p.setDefaultParallelism(dop);
 		return p;
@@ -92,12 +91,12 @@ public class VertexCentricCC implements PlanAssembler, PlanAssemblerDescription 
 		@Override
 		public void sendMessages(PactLong vertexId, PactLong componentId) {
 			sendMessageToAllTargets(componentId);
-		}
+        }
+
 	}
 
 	@Override
 	public String getDescription() {
 		return "<dop> <vertices> <edges> <result> <maxIterations>";
 	}
-
 }

stratosphere-addons/spargel/src/main/java/eu/stratosphere/spargel/java/util/MessageIterator.java
@@ -12,28 +12,40 @@
  * specific language governing permissions and limitations under the License.
  *
  **********************************************************************************************************************/
-package eu.stratosphere.pact.vertexcentric;
+package eu.stratosphere.spargel.java.util;
 
+import java.util.Iterator;
 
-import eu.stratosphere.pact.common.type.Key;
+import eu.stratosphere.pact.common.type.PactRecord;
 import eu.stratosphere.pact.common.type.Value;
 
 
-public final class Edge<VertexKey extends Key, EdgeValue extends Value> {
+public final class MessageIterator<Message extends Value> implements Iterator<Message> {
+
+	private final Message instance;
+	private Iterator<PactRecord> source;
 	
-	private VertexKey target;
-	private EdgeValue edgeValue;
-	
-	void set(VertexKey target, EdgeValue edgeValue) {
-		this.target = target;
-		this.edgeValue = edgeValue;
+	public MessageIterator(Message instance) {
+		this.instance = instance;
 	}
 	
-	public VertexKey target() {
-		return target;
+	public final void setSource(Iterator<PactRecord> source) {
+		this.source = source;
 	}
 	
-	public EdgeValue edgeValue() {
-		return edgeValue;
+	@Override
+	public final boolean hasNext() {
+		return this.source.hasNext();
+	}
+	
+	@Override
+	public final Message next() {
+		this.source.next().getFieldInto(1, this.instance);
+		return this.instance;
+	}
+
+	@Override
+	public final void remove() {
+		throw new UnsupportedOperationException();
 	}
 }