5efd24167a5d249cf4942464818e7a029aba38a8
Markus Holzemer <markus.holzemer@gmx.de>
2013-12-09 15:06:13 +0100

    Simple batch scripts to run stratosphere under windows

---
+++ stratosphere-dist/src/main/stratosphere-bin/bin/start-local.bat
@@ -0,0 +1,29 @@
+::#######################################################################################################################
+:: 
+::  Copyright (C) 2010 by the Stratosphere project (http://stratosphere.eu)
+:: 
+::  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+::  the License. You may obtain a copy of the License at
+:: 
+::      http://www.apache.org/licenses/LICENSE-2.0
+:: 
+::  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+::  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+::  specific language governing permissions and limitations under the License.
+:: 
+::#######################################################################################################################
+setlocal
+
+SET bin=%~dp0
+SET NEPHELE_ROOT_DIR=%bin%..
+SET NEPHELE_LIB_DIR=%NEPHELE_ROOT_DIR%\lib
+SET NEPHELE_CONF_DIR=%NEPHELE_ROOT_DIR%\conf
+SET NEPHELE_LOG_DIR=%NEPHELE_ROOT_DIR%\log
+
+SET JVM_ARGS=-Xms768m -Xmx768m
+
+SET NEPHELE_JM_CLASSPATH=%NEPHELE_LIB_DIR%\*;%NEPHELE_LIB_DIR%\dropin\*;%NEPHELE_LIB_DIR%\dropin\
+
+java %JVM_ARGS% -cp %NEPHELE_JM_CLASSPATH% eu.stratosphere.nephele.jobmanager.JobManager -executionMode local -configDir %NEPHELE_CONF_DIR%
+
+endlocal
