stratosphere/stratosphere

Implementation of the remaining BasicTypes for new Java API

stratosphere-core/src/main/java/eu/stratosphere/api/common/typeutils/base/BooleanValueSerializer.java
@@ -1,0 +1,62 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.common.typeutils.base;
+
+import java.io.IOException;
+
+import eu.stratosphere.api.common.typeutils.Serializer;
+import eu.stratosphere.core.memory.DataInputView;
+import eu.stratosphere.core.memory.DataOutputView;
+import eu.stratosphere.types.BooleanValue;
+
+
+public class BooleanValueSerializer extends Serializer<BooleanValue> {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final BooleanValueSerializer INSTANCE = new BooleanValueSerializer();
+	
+	@Override
+	public BooleanValue createInstance() {
+		return new BooleanValue();
+	}
+
+	@Override
+	public BooleanValue copy(BooleanValue from, BooleanValue reuse) {
+		reuse.setValue(from.getValue());
+		return reuse;
+	}
+
+	@Override
+	public int getLength() {
+		return 1;
+	}
+
+	@Override
+	public void serialize(BooleanValue record, DataOutputView target) throws IOException {
+		record.write(target);
+	}
+
+	@Override
+	public BooleanValue deserialize(BooleanValue reuse, DataInputView source) throws IOException {
+		reuse.read(source);
+		return reuse;
+	}
+
+	@Override
+	public void copy(DataInputView source, DataOutputView target) throws IOException {
+		target.writeBoolean(source.readBoolean());
+	}
+}

stratosphere-core/src/main/java/eu/stratosphere/api/common/typeutils/base/ByteSerializer.java
@@ -1,0 +1,63 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.common.typeutils.base;
+
+import java.io.IOException;
+
+import eu.stratosphere.api.common.typeutils.ImmutableTypeUtil;
+import eu.stratosphere.api.common.typeutils.Serializer;
+import eu.stratosphere.core.memory.DataInputView;
+import eu.stratosphere.core.memory.DataOutputView;
+
+
+public class ByteSerializer extends Serializer<Byte> implements ImmutableTypeUtil {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final ByteSerializer INSTANCE = new ByteSerializer();
+	
+	private static final Byte ZERO = Byte.valueOf((byte) 0);
+
+
+	@Override
+	public Byte createInstance() {
+		return ZERO;
+	}
+
+	@Override
+	public Byte copy(Byte from, Byte reuse) {
+		return from;
+	}
+
+	@Override
+	public int getLength() {
+		return 1;
+	}
+
+	@Override
+	public void serialize(Byte record, DataOutputView target) throws IOException {
+		target.writeByte(record.byteValue());
+	}
+
+	@Override
+	public Byte deserialize(Byte reuse, DataInputView source) throws IOException {
+		return Byte.valueOf(source.readByte());
+	}
+
+	@Override
+	public void copy(DataInputView source, DataOutputView target) throws IOException {
+		target.writeByte(source.readByte());
+	}
+}

stratosphere-core/src/main/java/eu/stratosphere/api/common/typeutils/base/ByteValueSerializer.java
@@ -1,0 +1,62 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.common.typeutils.base;
+
+import java.io.IOException;
+
+import eu.stratosphere.api.common.typeutils.Serializer;
+import eu.stratosphere.core.memory.DataInputView;
+import eu.stratosphere.core.memory.DataOutputView;
+import eu.stratosphere.types.ByteValue;
+
+
+public class ByteValueSerializer extends Serializer<ByteValue> {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final ByteValueSerializer INSTANCE = new ByteValueSerializer();
+	
+	@Override
+	public ByteValue createInstance() {
+		return new ByteValue();
+	}
+
+	@Override
+	public ByteValue copy(ByteValue from, ByteValue reuse) {
+		reuse.setValue(from.getValue());
+		return reuse;
+	}
+
+	@Override
+	public int getLength() {
+		return 1;
+	}
+
+	@Override
+	public void serialize(ByteValue record, DataOutputView target) throws IOException {
+		record.write(target);
+	}
+
+	@Override
+	public ByteValue deserialize(ByteValue reuse, DataInputView source) throws IOException {
+		reuse.read(source);
+		return reuse;
+	}
+
+	@Override
+	public void copy(DataInputView source, DataOutputView target) throws IOException {
+		target.writeByte(source.readByte());
+	}
+}

stratosphere-core/src/main/java/eu/stratosphere/api/common/typeutils/base/FloatValueSerializer.java
@@ -1,0 +1,62 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.common.typeutils.base;
+
+import java.io.IOException;
+
+import eu.stratosphere.api.common.typeutils.Serializer;
+import eu.stratosphere.core.memory.DataInputView;
+import eu.stratosphere.core.memory.DataOutputView;
+import eu.stratosphere.types.FloatValue;
+
+
+public class FloatValueSerializer extends Serializer<FloatValue> {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final FloatValueSerializer INSTANCE = new FloatValueSerializer();
+	
+	@Override
+	public FloatValue createInstance() {
+		return new FloatValue();
+	}
+
+	@Override
+	public FloatValue copy(FloatValue from, FloatValue reuse) {
+		reuse.setValue(from.getValue());
+		return reuse;
+	}
+
+	@Override
+	public int getLength() {
+		return 4;
+	}
+
+	@Override
+	public void serialize(FloatValue record, DataOutputView target) throws IOException {
+		record.write(target);
+	}
+
+	@Override
+	public FloatValue deserialize(FloatValue reuse, DataInputView source) throws IOException {
+		reuse.read(source);
+		return reuse;
+	}
+
+	@Override
+	public void copy(DataInputView source, DataOutputView target) throws IOException {
+		target.writeDouble(source.readFloat());
+	}
+}

stratosphere-core/src/main/java/eu/stratosphere/api/common/typeutils/base/ShortSerializer.java
@@ -1,0 +1,63 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.common.typeutils.base;
+
+import java.io.IOException;
+
+import eu.stratosphere.api.common.typeutils.ImmutableTypeUtil;
+import eu.stratosphere.api.common.typeutils.Serializer;
+import eu.stratosphere.core.memory.DataInputView;
+import eu.stratosphere.core.memory.DataOutputView;
+
+
+public class ShortSerializer extends Serializer<Short> implements ImmutableTypeUtil {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final ShortSerializer INSTANCE = new ShortSerializer();
+	
+	private static final Short ZERO = Short.valueOf((short)0);
+
+
+	@Override
+	public Short createInstance() {
+		return ZERO;
+	}
+
+	@Override
+	public Short copy(Short from, Short reuse) {
+		return from;
+	}
+
+	@Override
+	public int getLength() {
+		return 2;
+	}
+
+	@Override
+	public void serialize(Short record, DataOutputView target) throws IOException {
+		target.writeShort(record.shortValue());
+	}
+
+	@Override
+	public Short deserialize(Short reuse, DataInputView source) throws IOException {
+		return Short.valueOf(source.readShort());
+	}
+
+	@Override
+	public void copy(DataInputView source, DataOutputView target) throws IOException {
+		target.writeShort(source.readShort());
+	}
+}

stratosphere-core/src/main/java/eu/stratosphere/api/common/typeutils/base/ShortValueSerializer.java
@@ -1,0 +1,62 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.common.typeutils.base;
+
+import java.io.IOException;
+
+import eu.stratosphere.api.common.typeutils.Serializer;
+import eu.stratosphere.core.memory.DataInputView;
+import eu.stratosphere.core.memory.DataOutputView;
+import eu.stratosphere.types.ShortValue;
+
+
+public class ShortValueSerializer extends Serializer<ShortValue> {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final ShortValueSerializer INSTANCE = new ShortValueSerializer();
+	
+	@Override
+	public ShortValue createInstance() {
+		return new ShortValue();
+	}
+
+	@Override
+	public ShortValue copy(ShortValue from, ShortValue reuse) {
+		reuse.setValue(from.getValue());
+		return reuse;
+	}
+
+	@Override
+	public int getLength() {
+		return 2;
+	}
+
+	@Override
+	public void serialize(ShortValue record, DataOutputView target) throws IOException {
+		record.write(target);
+	}
+
+	@Override
+	public ShortValue deserialize(ShortValue reuse, DataInputView source) throws IOException {
+		reuse.read(source);
+		return reuse;
+	}
+
+	@Override
+	public void copy(DataInputView source, DataOutputView target) throws IOException {
+		target.writeShort(source.readShort());
+	}
+}

apache/flink

stratosphere-core/src/main/java/eu/stratosphere/api/common/typeutils/base/BooleanValueSerializer.java
@@ -1,0 +1,62 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.common.typeutils.base;
+
+import java.io.IOException;
+
+import eu.stratosphere.api.common.typeutils.TypeSerializer;
+import eu.stratosphere.core.memory.DataInputView;
+import eu.stratosphere.core.memory.DataOutputView;
+import eu.stratosphere.types.BooleanValue;
+
+
+public class BooleanValueSerializer extends TypeSerializer<BooleanValue> {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final BooleanValueSerializer INSTANCE = new BooleanValueSerializer();
+	
+	@Override
+	public BooleanValue createInstance() {
+		return new BooleanValue();
+	}
+
+	@Override
+	public BooleanValue copy(BooleanValue from, BooleanValue reuse) {
+		reuse.setValue(from.getValue());
+		return reuse;
+	}
+
+	@Override
+	public int getLength() {
+		return 1;
+	}
+
+	@Override
+	public void serialize(BooleanValue record, DataOutputView target) throws IOException {
+		record.write(target);
+	}
+
+	@Override
+	public BooleanValue deserialize(BooleanValue reuse, DataInputView source) throws IOException {
+		reuse.read(source);
+		return reuse;
+	}
+
+	@Override
+	public void copy(DataInputView source, DataOutputView target) throws IOException {
+		target.writeBoolean(source.readBoolean());
+	}
+}

stratosphere-core/src/main/java/eu/stratosphere/api/common/typeutils/base/ByteSerializer.java
@@ -1,0 +1,63 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.common.typeutils.base;
+
+import java.io.IOException;
+
+import eu.stratosphere.api.common.typeutils.ImmutableTypeUtil;
+import eu.stratosphere.api.common.typeutils.TypeSerializer;
+import eu.stratosphere.core.memory.DataInputView;
+import eu.stratosphere.core.memory.DataOutputView;
+
+
+public class ByteSerializer extends TypeSerializer<Byte> implements ImmutableTypeUtil {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final ByteSerializer INSTANCE = new ByteSerializer();
+	
+	private static final Byte ZERO = Byte.valueOf((byte) 0);
+
+
+	@Override
+	public Byte createInstance() {
+		return ZERO;
+	}
+
+	@Override
+	public Byte copy(Byte from, Byte reuse) {
+		return from;
+	}
+
+	@Override
+	public int getLength() {
+		return 1;
+	}
+
+	@Override
+	public void serialize(Byte record, DataOutputView target) throws IOException {
+		target.writeByte(record.byteValue());
+	}
+
+	@Override
+	public Byte deserialize(Byte reuse, DataInputView source) throws IOException {
+		return Byte.valueOf(source.readByte());
+	}
+
+	@Override
+	public void copy(DataInputView source, DataOutputView target) throws IOException {
+		target.writeByte(source.readByte());
+	}
+}

stratosphere-core/src/main/java/eu/stratosphere/api/common/typeutils/base/ByteValueSerializer.java
@@ -1,0 +1,62 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.common.typeutils.base;
+
+import java.io.IOException;
+
+import eu.stratosphere.api.common.typeutils.TypeSerializer;
+import eu.stratosphere.core.memory.DataInputView;
+import eu.stratosphere.core.memory.DataOutputView;
+import eu.stratosphere.types.ByteValue;
+
+
+public class ByteValueSerializer extends TypeSerializer<ByteValue> {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final ByteValueSerializer INSTANCE = new ByteValueSerializer();
+	
+	@Override
+	public ByteValue createInstance() {
+		return new ByteValue();
+	}
+
+	@Override
+	public ByteValue copy(ByteValue from, ByteValue reuse) {
+		reuse.setValue(from.getValue());
+		return reuse;
+	}
+
+	@Override
+	public int getLength() {
+		return 1;
+	}
+
+	@Override
+	public void serialize(ByteValue record, DataOutputView target) throws IOException {
+		record.write(target);
+	}
+
+	@Override
+	public ByteValue deserialize(ByteValue reuse, DataInputView source) throws IOException {
+		reuse.read(source);
+		return reuse;
+	}
+
+	@Override
+	public void copy(DataInputView source, DataOutputView target) throws IOException {
+		target.writeByte(source.readByte());
+	}
+}

stratosphere-core/src/main/java/eu/stratosphere/api/common/typeutils/base/FloatValueSerializer.java
@@ -1,0 +1,62 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.common.typeutils.base;
+
+import java.io.IOException;
+
+import eu.stratosphere.api.common.typeutils.TypeSerializer;
+import eu.stratosphere.core.memory.DataInputView;
+import eu.stratosphere.core.memory.DataOutputView;
+import eu.stratosphere.types.FloatValue;
+
+
+public class FloatValueSerializer extends TypeSerializer<FloatValue> {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final FloatValueSerializer INSTANCE = new FloatValueSerializer();
+	
+	@Override
+	public FloatValue createInstance() {
+		return new FloatValue();
+	}
+
+	@Override
+	public FloatValue copy(FloatValue from, FloatValue reuse) {
+		reuse.setValue(from.getValue());
+		return reuse;
+	}
+
+	@Override
+	public int getLength() {
+		return 4;
+	}
+
+	@Override
+	public void serialize(FloatValue record, DataOutputView target) throws IOException {
+		record.write(target);
+	}
+
+	@Override
+	public FloatValue deserialize(FloatValue reuse, DataInputView source) throws IOException {
+		reuse.read(source);
+		return reuse;
+	}
+
+	@Override
+	public void copy(DataInputView source, DataOutputView target) throws IOException {
+		target.writeDouble(source.readFloat());
+	}
+}

stratosphere-core/src/main/java/eu/stratosphere/api/common/typeutils/base/ShortSerializer.java
@@ -1,0 +1,63 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.common.typeutils.base;
+
+import java.io.IOException;
+
+import eu.stratosphere.api.common.typeutils.ImmutableTypeUtil;
+import eu.stratosphere.api.common.typeutils.TypeSerializer;
+import eu.stratosphere.core.memory.DataInputView;
+import eu.stratosphere.core.memory.DataOutputView;
+
+
+public class ShortSerializer extends TypeSerializer<Short> implements ImmutableTypeUtil {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final ShortSerializer INSTANCE = new ShortSerializer();
+	
+	private static final Short ZERO = Short.valueOf((short)0);
+
+
+	@Override
+	public Short createInstance() {
+		return ZERO;
+	}
+
+	@Override
+	public Short copy(Short from, Short reuse) {
+		return from;
+	}
+
+	@Override
+	public int getLength() {
+		return 2;
+	}
+
+	@Override
+	public void serialize(Short record, DataOutputView target) throws IOException {
+		target.writeShort(record.shortValue());
+	}
+
+	@Override
+	public Short deserialize(Short reuse, DataInputView source) throws IOException {
+		return Short.valueOf(source.readShort());
+	}
+
+	@Override
+	public void copy(DataInputView source, DataOutputView target) throws IOException {
+		target.writeShort(source.readShort());
+	}
+}

stratosphere-core/src/main/java/eu/stratosphere/api/common/typeutils/base/ShortValueSerializer.java
@@ -1,0 +1,62 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.common.typeutils.base;
+
+import java.io.IOException;
+
+import eu.stratosphere.api.common.typeutils.TypeSerializer;
+import eu.stratosphere.core.memory.DataInputView;
+import eu.stratosphere.core.memory.DataOutputView;
+import eu.stratosphere.types.ShortValue;
+
+
+public class ShortValueSerializer extends TypeSerializer<ShortValue> {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final ShortValueSerializer INSTANCE = new ShortValueSerializer();
+	
+	@Override
+	public ShortValue createInstance() {
+		return new ShortValue();
+	}
+
+	@Override
+	public ShortValue copy(ShortValue from, ShortValue reuse) {
+		reuse.setValue(from.getValue());
+		return reuse;
+	}
+
+	@Override
+	public int getLength() {
+		return 2;
+	}
+
+	@Override
+	public void serialize(ShortValue record, DataOutputView target) throws IOException {
+		record.write(target);
+	}
+
+	@Override
+	public ShortValue deserialize(ShortValue reuse, DataInputView source) throws IOException {
+		reuse.read(source);
+		return reuse;
+	}
+
+	@Override
+	public void copy(DataInputView source, DataOutputView target) throws IOException {
+		target.writeShort(source.readShort());
+	}
+}