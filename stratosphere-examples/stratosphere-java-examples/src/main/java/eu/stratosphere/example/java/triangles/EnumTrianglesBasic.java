stratosphere/stratosphere

Triangle enumeration examples

stratosphere-examples/stratosphere-java-examples/src/main/java/eu/stratosphere/example/java/triangles/EnumTrianglesBasic.java
@@ -1,0 +1,162 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.example.java.triangles;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import eu.stratosphere.api.common.operators.Order;
+import eu.stratosphere.api.java.DataSet;
+import eu.stratosphere.api.java.ExecutionEnvironment;
+import eu.stratosphere.api.java.functions.GroupReduceFunction;
+import eu.stratosphere.api.java.functions.JoinFunction;
+import eu.stratosphere.api.java.functions.MapFunction;
+import eu.stratosphere.example.java.triangles.data.EdgeData;
+import eu.stratosphere.example.java.triangles.dataTypes.EdgeDataTypes.Edge;
+import eu.stratosphere.example.java.triangles.dataTypes.EdgeDataTypes.Triad;
+import eu.stratosphere.example.java.triangles.dataTypes.EdgeDataTypes.TupleEdgeConverter;
+import eu.stratosphere.util.Collector;
+
+/**
+ * Triangle enumeration is a preprocessing step to find closely connected parts in graphs.
+ * A triangle are three edges that connect three vertices with each other.
+ * 
+ * The algorithm works as follows: 
+ * It groups all edges that share a common vertex and builds triads, i.e., triples of vertices 
+ * that are connected by two edges. Finally, all triads are filtered for which no third edge exists 
+ * that closes the triangle.
+ *  
+ * This implementation assumes that edges are represented as pairs of vertices and 
+ * vertices are represented as Integer IDs.
+ * 
+ */
+public class EnumTrianglesBasic {
+
+	/**
+	 *  Projects an edge (pair of vertices) such that the id of the first is smaller than the id of the second.
+	 */
+	private static class EdgeByIdProjector extends MapFunction<Edge, Edge> {
+		private static final long serialVersionUID = 1L;
+	
+		@Override
+		public Edge map(Edge inEdge) throws Exception {
+			
+			// flip vertices if necessary
+			if(inEdge.getFirstVertex() > inEdge.getSecondVertex()) {
+				inEdge.flipVertics();
+			}
+			
+			return inEdge;
+		}
+	}
+	
+	/**
+	 *  Builds triads (triples of vertices) from pairs of edges that share a vertex.
+	 *  The first vertex of a triad is the shared vertex, the second and third vertex are ordered by vertexId. 
+	 *  Assumes that input edges share the first vertex and are in ascending order of the second vertex.
+	 */
+	private static class TriadBuilder extends GroupReduceFunction<Edge, Triad> {
+		private static final long serialVersionUID = 1L;
+		
+		private final List<Integer> vertices = new ArrayList<Integer>();
+		private final Triad outTriad = new Triad();
+		
+		@Override
+		public void reduce(Iterator<Edge> edges, Collector<Triad> out) throws Exception {
+			
+			// clear vertex list
+			vertices.clear();
+			
+			// read first edge
+			Edge firstEdge = edges.next();
+			outTriad.setFirstVertex(firstEdge.getFirstVertex());
+			vertices.add(firstEdge.getSecondVertex());
+			
+			// build and emit triads
+			while(edges.hasNext()) {
+				Integer higherVertexId = edges.next().getSecondVertex();
+				
+				// combine vertex with all previously read vertices
+				for(Integer lowerVertexId : vertices) {
+					outTriad.setSecondVertex(lowerVertexId);
+					outTriad.setThirdVertex(higherVertexId);
+					out.collect(outTriad);
+				}
+				vertices.add(higherVertexId);
+			}
+		}
+	}
+	
+	/**
+	 *  Filters triads (three vertices connected by two edges) without a closing third edge.
+	 */
+	private static class TriadFilter extends JoinFunction<Triad, Edge, Triad> {
+		private static final long serialVersionUID = 1L;
+		
+		@Override
+		public Triad join(Triad triad, Edge edge) throws Exception {
+			return triad;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		String edgePath = "TESTDATA";
+		String outPath = "STDOUT";
+		
+		// parse input arguments
+		if(args.length > 0) {
+			edgePath = args[0];
+		}
+		if(args.length > 1) {
+			outPath = args[1];
+		}
+		
+		// set up execution environment
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+	
+		// read input data
+		DataSet<Edge> edges;
+		if(edgePath.equals("TESTDATA")) {
+			edges = EdgeData.getDefaultEdgeDataSet(env);
+		} else {
+			edges = env.readCsvFile(edgePath)
+				.fieldDelimiter(',')
+				.includeFields(true, true)
+				.types(Integer.class, Integer.class)
+				.map(new TupleEdgeConverter());
+		}
+		
+		// project edges by vertex id
+		DataSet<Edge> edgesById = edges
+				.map(new EdgeByIdProjector());
+		
+		// build and filter triads
+		DataSet<Triad> triangles = edgesById
+				.groupBy(Edge.V1).sortGroup(Edge.V2, Order.ASCENDING).reduceGroup(new TriadBuilder())
+ 				.join(edgesById).where(Triad.V2, Triad.V3).equalTo(Edge.V1, Edge.V2).with(new TriadFilter());
+
+		// emit triangles
+		if(outPath.equals("STDOUT")) {
+			triangles.print();
+		} else {
+			triangles.writeAsCsv(outPath, "\n", ",");
+		}
+		
+		// execute program		
+		env.execute();
+	}
+}

stratosphere-examples/stratosphere-java-examples/src/main/java/eu/stratosphere/example/java/triangles/data/EdgeData.java
@@ -1,0 +1,54 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.example.java.triangles.data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import eu.stratosphere.api.java.DataSet;
+import eu.stratosphere.api.java.ExecutionEnvironment;
+import eu.stratosphere.api.java.typeutils.BasicTypeInfo;
+import eu.stratosphere.api.java.typeutils.TupleTypeInfo;
+import eu.stratosphere.example.java.triangles.dataTypes.EdgeDataTypes.Edge;
+
+public class EdgeData {
+
+	public static DataSet<Edge> getDefaultEdgeDataSet(ExecutionEnvironment env) {
+		
+		Set<Edge> edgeData = new HashSet<Edge>();
+		
+		edgeData.add(new Edge(1, 2));
+		edgeData.add(new Edge(1, 3));
+		edgeData.add(new Edge(1, 4));
+		edgeData.add(new Edge(1, 5));
+		edgeData.add(new Edge(2, 3));
+		edgeData.add(new Edge(2, 5));
+		edgeData.add(new Edge(3, 4));
+		edgeData.add(new Edge(3, 7));
+		edgeData.add(new Edge(3, 8));
+		edgeData.add(new Edge(5, 6));
+		edgeData.add(new Edge(7, 8));
+				
+		TupleTypeInfo<Edge> edgeType = 
+				new TupleTypeInfo<Edge>(
+						Edge.class,
+						BasicTypeInfo.INT_TYPE_INFO,
+						BasicTypeInfo.INT_TYPE_INFO);
+		
+		return env.fromCollection(edgeData, edgeType);
+		
+	}
+	
+}

apache/flink

stratosphere-examples/stratosphere-java-examples/src/main/java/eu/stratosphere/example/java/triangles/EnumTrianglesBasic.java
@@ -1,0 +1,170 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.example.java.triangles;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import eu.stratosphere.api.common.operators.Order;
+import eu.stratosphere.api.java.DataSet;
+import eu.stratosphere.api.java.ExecutionEnvironment;
+import eu.stratosphere.api.java.functions.GroupReduceFunction;
+import eu.stratosphere.api.java.functions.JoinFunction;
+import eu.stratosphere.api.java.functions.MapFunction;
+import eu.stratosphere.example.java.triangles.util.EdgeData;
+import eu.stratosphere.example.java.triangles.util.EdgeDataTypes.Edge;
+import eu.stratosphere.example.java.triangles.util.EdgeDataTypes.Triad;
+import eu.stratosphere.example.java.triangles.util.EdgeDataTypes.TupleEdgeConverter;
+import eu.stratosphere.util.Collector;
+
+/**
+ * Triangle enumeration is a preprocessing step to find closely connected parts in graphs.
+ * A triangle are three edges that connect three vertices with each other.
+ * 
+ * The algorithm works as follows: 
+ * It groups all edges that share a common vertex and builds triads, i.e., triples of vertices 
+ * that are connected by two edges. Finally, all triads are filtered for which no third edge exists 
+ * that closes the triangle.
+ *  
+ * This implementation assumes that edges are represented as pairs of vertices and 
+ * vertices are represented as Integer IDs.
+ * 
+ * The lines of input files need to have the following format:
+ * "<INT: vertexId1>,<INT: vertexId2>\n"
+ * 
+ * For example the input: 
+ * "10,20\n10,30\n20,30\n" 
+ * defines three edges (10,20), (10,30), (20,30) which build a triangle. 
+ * 
+ * 
+ */
+public class EnumTrianglesBasic {
+
+	/**
+	 *  Projects an edge (pair of vertices) such that the id of the first is smaller than the id of the second.
+	 */
+	private static class EdgeByIdProjector extends MapFunction<Edge, Edge> {
+		private static final long serialVersionUID = 1L;
+	
+		@Override
+		public Edge map(Edge inEdge) throws Exception {
+			
+			// flip vertices if necessary
+			if(inEdge.getFirstVertex() > inEdge.getSecondVertex()) {
+				inEdge.flipVertics();
+			}
+			
+			return inEdge;
+		}
+	}
+	
+	/**
+	 *  Builds triads (triples of vertices) from pairs of edges that share a vertex.
+	 *  The first vertex of a triad is the shared vertex, the second and third vertex are ordered by vertexId. 
+	 *  Assumes that input edges share the first vertex and are in ascending order of the second vertex.
+	 */
+	private static class TriadBuilder extends GroupReduceFunction<Edge, Triad> {
+		private static final long serialVersionUID = 1L;
+		
+		private final List<Integer> vertices = new ArrayList<Integer>();
+		private final Triad outTriad = new Triad();
+		
+		@Override
+		public void reduce(Iterator<Edge> edges, Collector<Triad> out) throws Exception {
+			
+			// clear vertex list
+			vertices.clear();
+			
+			// read first edge
+			Edge firstEdge = edges.next();
+			outTriad.setFirstVertex(firstEdge.getFirstVertex());
+			vertices.add(firstEdge.getSecondVertex());
+			
+			// build and emit triads
+			while(edges.hasNext()) {
+				Integer higherVertexId = edges.next().getSecondVertex();
+				
+				// combine vertex with all previously read vertices
+				for(Integer lowerVertexId : vertices) {
+					outTriad.setSecondVertex(lowerVertexId);
+					outTriad.setThirdVertex(higherVertexId);
+					out.collect(outTriad);
+				}
+				vertices.add(higherVertexId);
+			}
+		}
+	}
+	
+	/**
+	 *  Filters triads (three vertices connected by two edges) without a closing third edge.
+	 */
+	private static class TriadFilter extends JoinFunction<Triad, Edge, Triad> {
+		private static final long serialVersionUID = 1L;
+		
+		@Override
+		public Triad join(Triad triad, Edge edge) throws Exception {
+			return triad;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		String edgePath = "TESTDATA";
+		String outPath = "STDOUT";
+		
+		// parse input arguments
+		if(args.length > 0) {
+			edgePath = args[0];
+		}
+		if(args.length > 1) {
+			outPath = args[1];
+		}
+		
+		// set up execution environment
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+	
+		// read input data
+		DataSet<Edge> edges;
+		if(edgePath.equals("TESTDATA")) {
+			edges = EdgeData.getDefaultEdgeDataSet(env);
+		} else {
+			edges = env.readCsvFile(edgePath)
+				.fieldDelimiter(',')
+				.includeFields(true, true)
+				.types(Integer.class, Integer.class)
+				.map(new TupleEdgeConverter());
+		}
+		
+		// project edges by vertex id
+		DataSet<Edge> edgesById = edges
+				.map(new EdgeByIdProjector());
+		
+		// build and filter triads
+		DataSet<Triad> triangles = edgesById
+				.groupBy(Edge.V1).sortGroup(Edge.V2, Order.ASCENDING).reduceGroup(new TriadBuilder())
+ 				.join(edgesById).where(Triad.V2, Triad.V3).equalTo(Edge.V1, Edge.V2).with(new TriadFilter());
+
+		// emit triangles
+		if(outPath.equals("STDOUT")) {
+			triangles.print();
+		} else {
+			triangles.writeAsCsv(outPath, "\n", ",");
+		}
+		
+		// execute program		
+		env.execute();
+	}
+}

stratosphere-examples/stratosphere-java-examples/src/main/java/eu/stratosphere/example/java/triangles/util/EdgeData.java
@@ -1,0 +1,54 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.example.java.triangles.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import eu.stratosphere.api.java.DataSet;
+import eu.stratosphere.api.java.ExecutionEnvironment;
+import eu.stratosphere.api.java.typeutils.BasicTypeInfo;
+import eu.stratosphere.api.java.typeutils.TupleTypeInfo;
+import eu.stratosphere.example.java.triangles.util.EdgeDataTypes.Edge;
+
+public class EdgeData {
+
+	public static DataSet<Edge> getDefaultEdgeDataSet(ExecutionEnvironment env) {
+		
+		Set<Edge> edgeData = new HashSet<Edge>();
+		
+		edgeData.add(new Edge(1, 2));
+		edgeData.add(new Edge(1, 3));
+		edgeData.add(new Edge(1, 4));
+		edgeData.add(new Edge(1, 5));
+		edgeData.add(new Edge(2, 3));
+		edgeData.add(new Edge(2, 5));
+		edgeData.add(new Edge(3, 4));
+		edgeData.add(new Edge(3, 7));
+		edgeData.add(new Edge(3, 8));
+		edgeData.add(new Edge(5, 6));
+		edgeData.add(new Edge(7, 8));
+				
+		TupleTypeInfo<Edge> edgeType = 
+				new TupleTypeInfo<Edge>(
+						Edge.class,
+						BasicTypeInfo.INT_TYPE_INFO,
+						BasicTypeInfo.INT_TYPE_INFO);
+		
+		return env.fromCollection(edgeData, edgeType);
+		
+	}
+	
+}