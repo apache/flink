c7c94f2d3bca2bb923fd67690a5e5c6d345e3fc4
Fabian Hueske <fabian.hueske@tu-berlin.de>
2014-03-19 11:21:38 +0100

    Added integration tests for TriangleEnumeration examples on new Java API

---
+++ stratosphere-tests/src/test/java/eu/stratosphere/test/exampleJavaPrograms/EnumTriangleOptITCase.java
@@ -0,0 +1,42 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.test.exampleJavaPrograms;
+
+import eu.stratosphere.example.java.triangles.EnumTrianglesOpt;
+import eu.stratosphere.test.testdata.EnumTriangleData;
+import eu.stratosphere.test.util.JavaProgramTestBase;
+
+public class EnumTriangleOptITCase extends JavaProgramTestBase {
+	
+	protected String edgePath;
+	protected String resultPath;
+	
+	@Override
+	protected void preSubmit() throws Exception {
+		edgePath = createTempFile("edges", EnumTriangleData.EDGES);
+		resultPath = getTempDirPath("triangles");
+	}
+
+	@Override
+	protected void postSubmit() throws Exception {
+		compareResultsByLinesInMemory(EnumTriangleData.TRIANGLES_BY_DEGREE, resultPath);
+	}
+	
+	@Override
+	protected void testProgram() throws Exception {
+		EnumTrianglesOpt.main(new String[] { edgePath, resultPath });
+	}
+
+}
