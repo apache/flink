b032ff3afb10f9e0158e133a8f5c447ad89a90b6
mingliang <qmlmoon@gmail.com>
2014-03-20 11:37:12 +0100

    Add LocalCollectionOutputFormat for local executor

---
+++ stratosphere-java/src/main/java/eu/stratosphere/api/java/io/LocalCollectionOutputFormat.java
@@ -0,0 +1,91 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.java.io;
+
+
+import eu.stratosphere.api.common.io.OutputFormat;
+import eu.stratosphere.api.common.typeutils.TypeSerializer;
+import eu.stratosphere.api.java.typeutils.InputTypeConfigurable;
+import eu.stratosphere.api.java.typeutils.TypeInformation;
+import eu.stratosphere.configuration.Configuration;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *  An output format that writes record into collection
+ */
+public class LocalCollectionOutputFormat<T> implements OutputFormat<T>, InputTypeConfigurable {
+
+	private static final long serialVersionUID = 1L;
+
+	private static Map<Integer,Collection<?>> resultWrapper = new HashMap<Integer, Collection<?>>();   //since generic collection is not allowed to be static
+
+	private transient ArrayList<T> taskResult;
+
+	private TypeSerializer<T> typeSerializer;
+
+	private int id;
+
+	public LocalCollectionOutputFormat(Collection<T> out) {
+		this.id = generateRandomId();
+		this.resultWrapper.put(this.id, out);
+	}
+
+	private int generateRandomId() {
+		int num = (int) (Math.random() * Integer.MAX_VALUE);
+		while (this.resultWrapper.containsKey(num)) {
+			num = (int) (Math.random() * Integer.MAX_VALUE);
+		}
+		return num;
+	}
+
+	@Override
+	public void configure(Configuration parameters) {
+	}
+
+
+	@Override
+	public void open(int taskNumber, int numTasks) throws IOException {
+		this.taskResult = new ArrayList<T>();
+	}
+
+	@Override
+	public void writeRecord(T record) throws IOException {
+		T recordCopy = this.typeSerializer.createInstance();
+		recordCopy = this.typeSerializer.copy(record, recordCopy);
+		this.taskResult.add(recordCopy);
+	}
+
+
+	@Override
+	public void close() throws IOException {
+		synchronized (this.resultWrapper) {
+			@SuppressWarnings("unchecked")
+			Collection<T> result = (Collection<T>) this.resultWrapper.get(this.id);
+			result.addAll(this.taskResult);
+		}
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public void setInputType(TypeInformation<?> type) {
+		this.typeSerializer = (TypeSerializer<T>)type.createSerializer();
+	}
+
+}
--- stratosphere-tests/src/test/java/eu/stratosphere/test/exampleJavaPrograms/WordCountWithCollectionITCase.java
+++ stratosphere-tests/src/test/java/eu/stratosphere/test/exampleJavaPrograms/WordCountWithCollectionITCase.java
@@ -14,15 +14,54 @@
  **********************************************************************************************************************/
 package eu.stratosphere.test.exampleJavaPrograms;
 
-import eu.stratosphere.example.java.wordcount.WordCountCollection;
+import eu.stratosphere.api.java.DataSet;
+import eu.stratosphere.api.java.ExecutionEnvironment;
+import eu.stratosphere.api.java.aggregation.Aggregations;
+import eu.stratosphere.api.java.io.LocalCollectionOutputFormat;
+import eu.stratosphere.api.java.tuple.Tuple2;
+import eu.stratosphere.example.java.wordcount.WordCount;
+import eu.stratosphere.test.testdata.WordCountData;
 import eu.stratosphere.test.util.JavaProgramTestBase;
+import org.junit.Assert;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WordCountWithCollectionITCase extends JavaProgramTestBase {
-	
+
+	List<Tuple2<String, Integer>> resultsCollected = new ArrayList<Tuple2<String, Integer>>();
+
+
+
+	@Override
+	protected void postSubmit() throws Exception {
+		String[] result = new String[resultsCollected.size()];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = resultsCollected.get(i).toString();
+		}
+		Arrays.sort(result);
+
+		String[] expected = WordCountData.COUNTS_AS_TUPLES.split("\n");
+		Arrays.sort(expected);
+
+		Assert.assertEquals("Different number of lines in expected and obtained result.", expected.length, result.length);
+		Assert.assertArrayEquals(expected, result);
+	}
+
+
 	@Override
 	protected void testProgram() throws Exception {
-		WordCountCollection.main(new String[0]);
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+		DataSet<String> text = env.fromElements(WordCountData.TEXT);
+		DataSet<Tuple2<String, Integer>> words = text.flatMap(new WordCount.Tokenizer());
+		DataSet<Tuple2<String, Integer>> result = words.groupBy(0).aggregate(Aggregations.SUM, 1);
+
+		result.output(new LocalCollectionOutputFormat(resultsCollected));
+		env.execute("Word Count Collection");
+
 	}
+
 
 }
