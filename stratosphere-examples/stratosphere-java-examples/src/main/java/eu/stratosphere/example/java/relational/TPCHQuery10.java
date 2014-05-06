a86eec175ea888a0ac91b3bb5c43177420c6aa16
StephanEwen <stephan.ewen@tu-berlin.de>
2014-05-06 20:00:57 +0200

    TPCH Q10 example cleanup

--- stratosphere-examples/stratosphere-java-examples/src/main/java/eu/stratosphere/example/java/relational/TPCHQuery10.java
+++ stratosphere-examples/stratosphere-java-examples/src/main/java/eu/stratosphere/example/java/relational/TPCHQuery10.java
@@ -14,12 +14,6 @@
  **********************************************************************************************************************/
 package eu.stratosphere.example.java.relational;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 import eu.stratosphere.api.java.DataSet;
 import eu.stratosphere.api.java.ExecutionEnvironment;
 import eu.stratosphere.api.java.functions.FilterFunction;
@@ -66,19 +60,25 @@ import eu.stratosphere.api.java.tuple.Tuple6;
  *        c_address
  *        
  * Compared to the original TPC-H query this version does not print 
- * c_phone and c_comment, only filters by years greater than 1990 instead of  
+ * c_phone and c_comment, only filters by years greater than 1990 instead of
  * a period of 3 months, and does not sort the result by revenue.
- * 
  */
+
+@SuppressWarnings("serial")
 public class TPCHQuery10 {
 	
 	public static void main(String[] args) throws Exception {
 		
-		String customerPath = (args.length > 0 ? args[0] : copyJarFileToTmp("/Testdata/customer.tbl", "customer.tbl"));
-		String ordersPath =  (args.length > 1 ? args[1] : copyJarFileToTmp("/Testdata/orders.tbl", "orders.tbl"));
-		String lineitemPath =  (args.length > 2 ? args[2] : copyJarFileToTmp("/Testdata/lineitem.tbl", "lineitem.tbl"));
-		String nationPath =  (args.length > 3 ? args[3] : copyJarFileToTmp("/Testdata/nation.tbl", "nation.tbl"));		
-		String outPath = (args.length > 4 ? args[4] : null);
+		if (args.length < 5) {
+			System.err.println("Parameters: <customer-csv path> <orders-csv path> <lineitem-csv path> <nation-csv path> <result path>");
+			return;
+		}
+		
+		final String customerPath = args[0];
+		final String ordersPath = args[1];
+		final String lineitemPath = args[2];
+		final String nationPath = args[3];
+		final String outPath = args[4];
 		
 		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
@@ -106,22 +106,17 @@ public class TPCHQuery10 {
 		DataSet<Tuple2<Integer, Integer>> ordersFilteredByYear = orders
 				// filter by year
 				.filter(new FilterFunction<Tuple3<Integer,Integer, String>>() {
-					private static final long serialVersionUID = 1L;
-					private static final int YEAR_FILTER = 1990;
-					
 					@Override
-					public boolean filter(Tuple3<Integer, Integer, String> t) throws Exception {
-						int year = Integer.parseInt(t.T3().substring(0, 4));
-						return year > YEAR_FILTER;
+					public boolean filter(Tuple3<Integer, Integer, String> t) {
+						int year = Integer.parseInt(t.f2.substring(0, 4));
+						return year > 1990;
 					}
 				})
 				// remove date as it is not necessary anymore
 				.map(new MapFunction<Tuple3<Integer,Integer,String>, Tuple2<Integer, Integer>>() {
-					private static final long serialVersionUID = 1L;
-
 					@Override
-					public Tuple2<Integer, Integer> map(Tuple3<Integer, Integer, String> t) throws Exception {
-						return new Tuple2<Integer, Integer>(t.T1(), t.T2());
+					public Tuple2<Integer, Integer> map(Tuple3<Integer, Integer, String> t) {
+						return new Tuple2<Integer, Integer>(t.f0, t.f1);
 					}
 				});
 
@@ -129,21 +124,17 @@ public class TPCHQuery10 {
 		DataSet<Tuple3<Integer, Double, Double>> lineitemsFilteredByFlag = lineitems
 				// filter by flag
 				.filter(new FilterFunction<Tuple4<Integer, Double, Double, String>>() {
-					private static final long serialVersionUID = 1L;
-
 					@Override
 					public boolean filter(Tuple4<Integer, Double, Double, String> t)
 							throws Exception {
-						return t.T4().equals("R");
+						return t.f3.equals("R");
 					}
 				})
 				// remove flag as it is not necessary anymore
 				.map(new MapFunction<Tuple4<Integer, Double, Double, String>, Tuple3<Integer, Double, Double>>() {
-					private static final long serialVersionUID = 1L;
-
 					@Override
-					public Tuple3<Integer, Double, Double> map(Tuple4<Integer, Double, Double, String> t) throws Exception {
-						return new Tuple3<Integer, Double, Double>(t.T1(), t.T2(), t.T3());
+					public Tuple3<Integer, Double, Double> map(Tuple4<Integer, Double, Double, String> t) {
+						return new Tuple3<Integer, Double, Double>(t.f0, t.f1, t.f2);
 					}
 				});
 
@@ -152,11 +143,9 @@ public class TPCHQuery10 {
 		DataSet<Tuple3<Integer, Double, Double>> lineitemsOfCustomerKey = ordersFilteredByYear.joinWithHuge(lineitemsFilteredByFlag)
 				.where(0).equalTo(0)
 				.with(new JoinFunction<Tuple2<Integer, Integer>, Tuple3<Integer, Double, Double>, Tuple3<Integer, Double, Double>>() {
-					private static final long serialVersionUID = 1L;
-
 					@Override
 					public Tuple3<Integer, Double, Double> join(Tuple2<Integer, Integer> o, Tuple3<Integer, Double, Double> l) {
-						return new Tuple3<Integer, Double, Double>(o.T2(), l.T2(), l.T3());
+						return new Tuple3<Integer, Double, Double>(o.f1, l.f1, l.f2);
 					}
 				});
 
@@ -165,21 +154,17 @@ public class TPCHQuery10 {
 		DataSet<Tuple2<Integer, Double>> revenueOfCustomerKey = lineitemsOfCustomerKey
 				// calculate the revenue for each item
 				.map(new MapFunction<Tuple3<Integer, Double, Double>, Tuple2<Integer, Double>>() {
-					private static final long serialVersionUID = 1L;
-
 					@Override
-					public Tuple2<Integer, Double> map(Tuple3<Integer, Double, Double> t) throws Exception {
+					public Tuple2<Integer, Double> map(Tuple3<Integer, Double, Double> t) {
 						// revenue per item = l_extendedprice * (1 - l_discount)
-						return new Tuple2<Integer, Double>(t.T1(), t.T2() * (1 - t.T3()));
+						return new Tuple2<Integer, Double>(t.f0, t.f1 * (1 - t.f2));
 					}
 				})
 				// aggregate the revenues per item to revenue per customer
 				.groupBy(0).reduce(new ReduceFunction<Tuple2<Integer,Double>>() {
-					private static final long serialVersionUID = 1L;
-
 					@Override
-					public Tuple2<Integer, Double> reduce(Tuple2<Integer, Double> t1, Tuple2<Integer, Double> t2) throws Exception {
-						return new Tuple2<Integer, Double>(t1.T1(), t1.T2()+t2.T2());
+					public Tuple2<Integer, Double> reduce(Tuple2<Integer, Double> t1, Tuple2<Integer, Double> t2) {
+						return new Tuple2<Integer, Double>(t1.f0, t1.f1+t2.f1);
 					}
 				});
 
@@ -190,12 +175,9 @@ public class TPCHQuery10 {
 				.where(3)
 				.equalTo(0)
 				.with(new JoinFunction<Tuple5<Integer, String, String, Integer, Double>, Tuple2<Integer, String>, Tuple5<Integer, String, String, String, Double>>() {
-					private static final long serialVersionUID = 1L;
-
 					@Override
-					public Tuple5<Integer, String, String, String, Double> join(Tuple5<Integer, String, String, Integer, Double> c,
-							Tuple2<Integer, String> n) throws Exception {
-						return new Tuple5<Integer, String, String, String, Double>(c.T1(), c.T2(), c.T3(), n.T2(), c.T5());
+					public Tuple5<Integer, String, String, String, Double> join(Tuple5<Integer, String, String, Integer, Double> c, Tuple2<Integer, String> n) {
+						return new Tuple5<Integer, String, String, String, Double>(c.f0, c.f1, c.f2, n.f1, c.f4);
 					}
 				});
 
@@ -206,38 +188,14 @@ public class TPCHQuery10 {
 				.where(0)
 				.equalTo(0)
 				.with(new JoinFunction<Tuple5<Integer, String, String, String, Double>, Tuple2<Integer, Double>, Tuple6<Integer, String, String, String, Double, Double>>() {
-					private static final long serialVersionUID = 1L;
-
 					@Override
-					public Tuple6<Integer, String, String, String, Double, Double> join(Tuple5<Integer, String, String, String, Double> c,
-							Tuple2<Integer, Double> r) throws Exception {
-						return new Tuple6<Integer, String, String, String, Double, Double>(c.T1(), c.T2(), c.T3(), c.T4(), c.T5(), r.T2());
+					public Tuple6<Integer, String, String, String, Double, Double> join(Tuple5<Integer, String, String, String, Double> c, Tuple2<Integer, Double> r) {
+						return new Tuple6<Integer, String, String, String, Double, Double>(c.f0, c.f1, c.f2, c.f3, c.f4, r.f1);
 					}
 				});
 
-		// print the result and execute
-		if (outPath == null) {
-			customerWithRevenue.print();
-		} else {
-			customerWithRevenue.writeAsCsv(outPath);
-		}
+		// write the result and execute
+		customerWithRevenue.writeAsCsv(outPath);
 		env.execute();
-	}
-	
-	// Helper function to extract a testdata file from jar to the default tmp-directory
-	private static String copyJarFileToTmp(String jarPath, String tmpName) throws IOException {
-		File tmpFile = new File(System.getProperty("java.io.tmpdir"), tmpName);
-		
-		InputStream is = TPCHQuery10.class.getResourceAsStream(jarPath);
-		OutputStream os = new FileOutputStream(tmpFile);
-		int read = 0;
-		byte[] bytes = new byte[1024];
- 
-		while ((read = is.read(bytes)) != -1) {
-			os.write(bytes, 0, read);
-		}
-		is.close();
-		os.close();
-		return "file://"+tmpFile.toString();
 	}
 }
