ff96f6ca0815cf0abb5f67cd03487b5307ba2217
Markus Holzemer <markus.holzemer@gmx.de>
2014-05-06 11:25:42 +0200

    Tests for aggregate operator

---
+++ stratosphere-java/src/test/java/eu/stratosphere/api/java/operator/AggregateOperatorTest.java
@@ -0,0 +1,119 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2013 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+package eu.stratosphere.api.java.operator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import eu.stratosphere.api.common.InvalidProgramException;
+import eu.stratosphere.api.java.DataSet;
+import eu.stratosphere.api.java.ExecutionEnvironment;
+import eu.stratosphere.api.java.aggregation.Aggregations;
+import eu.stratosphere.api.java.tuple.Tuple5;
+import eu.stratosphere.api.java.typeutils.BasicTypeInfo;
+import eu.stratosphere.api.java.typeutils.TupleTypeInfo;
+
+public class AggregateOperatorTest {
+
+	// TUPLE DATA
+	
+	private final List<Tuple5<Integer, Long, String, Long, Integer>> emptyTupleData = 
+			new ArrayList<Tuple5<Integer, Long, String, Long, Integer>>();
+	
+	private final TupleTypeInfo<Tuple5<Integer, Long, String, Long, Integer>> tupleTypeInfo = new 
+			TupleTypeInfo<Tuple5<Integer, Long, String, Long, Integer>>(
+					BasicTypeInfo.INT_TYPE_INFO,
+					BasicTypeInfo.LONG_TYPE_INFO,
+					BasicTypeInfo.STRING_TYPE_INFO,
+					BasicTypeInfo.LONG_TYPE_INFO,
+					BasicTypeInfo.INT_TYPE_INFO
+			);
+	
+	// LONG DATA
+	
+	private final List<Long> emptyLongData = new ArrayList<Long>();
+	
+	@Test
+	public void testFieldsAggregate() {
+		
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		DataSet<Tuple5<Integer, Long, String, Long, Integer>> tupleDs = env.fromCollection(emptyTupleData, tupleTypeInfo);
+
+		// should work
+		try {
+			tupleDs.aggregate(Aggregations.AVG, 1);
+		} catch(Exception e) {
+			Assert.fail();
+		}
+		
+		// should not work: index out of bounds
+		try {
+			tupleDs.aggregate(Aggregations.AVG, 10);
+			Assert.fail();
+		} catch(IllegalArgumentException iae) {
+			// we're good here
+		} catch(Exception e) {
+			Assert.fail();
+		}
+		
+		// should not work: not applied to tuple dataset
+		DataSet<Long> longDs = env.fromCollection(emptyLongData, BasicTypeInfo.LONG_TYPE_INFO);
+		try {
+			longDs.aggregate(Aggregations.MIN, 1);
+			Assert.fail();
+		} catch(InvalidProgramException uoe) {
+			// we're good here
+		} catch(Exception e) {
+			Assert.fail();
+		}
+		
+	}
+	
+	@Test
+	public void testAggregationTypes() {
+		
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		DataSet<Tuple5<Integer, Long, String, Long, Integer>> tupleDs = env.fromCollection(emptyTupleData, tupleTypeInfo);
+		
+		// should work: multiple aggregates
+		try {
+			tupleDs.aggregate(Aggregations.AVG, 0).and(Aggregations.MIN, 4);
+		} catch(Exception e) {
+			Assert.fail();
+		}
+		
+		// should work: nested aggregates
+		try {
+			tupleDs.aggregate(Aggregations.AVG, 2).aggregate(Aggregations.SUM, 1);
+		} catch(Exception e) {
+			Assert.fail();
+		}
+		
+		// should not work: average on string
+		try {
+			tupleDs.aggregate(Aggregations.AVG, 2);
+			Assert.fail();
+		} catch(IllegalArgumentException iae) {
+			// we're good here
+		} catch(Exception e) {
+			Assert.fail();
+		}
+				
+	}
+}
