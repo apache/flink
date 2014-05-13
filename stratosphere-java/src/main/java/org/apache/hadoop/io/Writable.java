79af0aee1d45694084058b62af4c975b8df495e9
StephanEwen <stephan.ewen@tu-berlin.de>
2014-05-14 01:46:16 +0200

    Fix checkstyle warning for Writable

--- stratosphere-java/src/main/java/org/apache/hadoop/io/Writable.java
+++ stratosphere-java/src/main/java/org/apache/hadoop/io/Writable.java
@@ -80,22 +80,26 @@ import java.io.IOException;
  * </pre></blockquote></p>
  */
 public interface Writable {
-  /** 
-   * Serialize the fields of this object to <code>out</code>.
-   * 
-   * @param out <code>DataOuput</code> to serialize this object into.
-   * @throws IOException
-   */
-  void write(DataOutput out) throws IOException;
+	/**
+	 * Serialize the fields of this object to <code>out</code>.
+	 * 
+	 * @param out
+	 *            <code>DataOuput</code> to serialize this object into.
+	 * @throws IOException
+	 */
+	void write(DataOutput out) throws IOException;
 
-  /** 
-   * Deserialize the fields of this object from <code>in</code>.  
-   * 
-   * <p>For efficiency, implementations should attempt to re-use storage in the 
-   * existing object where possible.</p>
-   * 
-   * @param in <code>DataInput</code> to deseriablize this object from.
-   * @throws IOException
-   */
-  void readFields(DataInput in) throws IOException;
+	/**
+	 * Deserialize the fields of this object from <code>in</code>.
+	 * 
+	 * <p>
+	 * For efficiency, implementations should attempt to re-use storage in the
+	 * existing object where possible.
+	 * </p>
+	 * 
+	 * @param in
+	 *            <code>DataInput</code> to deseriablize this object from.
+	 * @throws IOException
+	 */
+	void readFields(DataInput in) throws IOException;
 }
