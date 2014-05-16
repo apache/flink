987706266d75a4acb34a19a3a96ba6b36e0bd7f2
Fabian Hueske <fabian.hueske@tu-berlin.de>
2014-05-16 20:38:37 +0200

    Added UnsortedGrouping to separate Grouping methods from SortedGrouping methods

--- stratosphere-java/src/main/java/eu/stratosphere/api/java/operators/Grouping.java
+++ stratosphere-java/src/main/java/eu/stratosphere/api/java/operators/Grouping.java
@@ -15,11 +15,7 @@
 package eu.stratosphere.api.java.operators;
 
 import eu.stratosphere.api.common.InvalidProgramException;
-import eu.stratosphere.api.common.operators.Order;
 import eu.stratosphere.api.java.DataSet;
-import eu.stratosphere.api.java.aggregation.Aggregations;
-import eu.stratosphere.api.java.functions.GroupReduceFunction;
-import eu.stratosphere.api.java.functions.ReduceFunction;
 
 /**
  * Grouping is an intermediate step for a transformation on a grouped DataSet.<br/>
@@ -34,7 +30,7 @@ import eu.stratosphere.api.java.functions.ReduceFunction;
  * 
  * @see DataSet
  */
-public class Grouping<T> {
+public abstract class Grouping<T> {
 	
 	protected final DataSet<T> dataSet;
 	
@@ -60,84 +56,6 @@ public class Grouping<T> {
 	
 	protected Keys<T> getKeys() {
 		return this.keys;
-	}
-	
-	// --------------------------------------------------------------------------------------------
-	//  Operations / Transformations
-	// --------------------------------------------------------------------------------------------
-	
-	/**
-	 * Applies an Aggregate transformation on a grouped {@link Tuple} {@link DataSet}.<br/>
-	 * <b>Note: Only Tuple DataSets can be aggregated.</b>
-	 * The transformation applies a built-in {@link Aggregations Aggregation} on a specified field 
-	 *   of a Tuple group. Additional aggregation functions can be added to the resulting 
-	 *   {@link AggregateOperator} by calling {@link AggregateOperator#and(Aggregations, int)}.
-	 * 
-	 * @param agg The built-in aggregation function that is computed.
-	 * @param field The index of the Tuple field on which the aggregation function is applied.
-	 * @return An AggregateOperator that represents the aggregated DataSet. 
-	 * 
-	 * @see Tuple
-	 * @see Aggregations
-	 * @see AggregateOperator
-	 * @see DataSet
-	 */
-	public AggregateOperator<T> aggregate(Aggregations agg, int field) {
-		return new AggregateOperator<T>(this, agg, field);
-	}
-	
-	/**
-	 * Applies a Reduce transformation on a grouped {@link DataSet}.<br/>
-	 * For each group, the transformation consecutively calls a {@link ReduceFunction} 
-	 *   until only a single element for each group remains. 
-	 * A ReduceFunction combines two elements into one new element of the same type.
-	 * 
-	 * @param reducer The ReduceFunction that is applied on each group of the DataSet.
-	 * @return A ReduceOperator that represents the reduced DataSet.
-	 * 
-	 * @see ReduceFunction
-	 * @see ReduceOperator
-	 * @see DataSet
-	 */
-	public ReduceOperator<T> reduce(ReduceFunction<T> reducer) {
-		return new ReduceOperator<T>(this, reducer);
-	}
-	
-	/**
-	 * Applies a GroupReduce transformation on a grouped {@link DataSet}.<br/>
-	 * The transformation calls a {@link GroupReduceFunction} for each group of the DataSet.
-	 * A GroupReduceFunction can iterate over all elements of a group and emit any
-	 *   number of output elements including none.
-	 * 
-	 * @param reducer The GroupReduceFunction that is applied on each group of the DataSet.
-	 * @return A GroupReduceOperator that represents the reduced DataSet.
-	 * 
-	 * @see GroupReduceFunction
-	 * @see GroupReduceOperator
-	 * @see DataSet
-	 */
-	public <R> ReduceGroupOperator<T, R> reduceGroup(GroupReduceFunction<T, R> reducer) {
-		return new ReduceGroupOperator<T, R>(this, reducer);
-	}
-
-	// --------------------------------------------------------------------------------------------
-	//  Group Operations
-	// --------------------------------------------------------------------------------------------
-	
-	/**
-	 * Sorts {@link Tuple} elements within a group on the specified field in the specified {@link Order}.</br>
-	 * <b>Note: Only groups of Tuple elements can be sorted.</b><br/>
-	 * Groups can be sorted by multiple fields by chaining {@link #sortGroup(int, Order)} calls.
-	 * 
-	 * @param field The Tuple field on which the group is sorted.
-	 * @param order The Order in which the specified Tuple field is sorted.
-	 * @return A SortedGrouping with specified order of group element.
-	 * 
-	 * @see Tuple
-	 * @see Order
-	 */
-	public SortedGrouping<T> sortGroup(int field, Order order) {
-		return new SortedGrouping<T>(this.dataSet, this.keys, field, order);
 	}
 
 }
