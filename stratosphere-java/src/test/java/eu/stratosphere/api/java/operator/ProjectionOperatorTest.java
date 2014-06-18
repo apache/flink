04658f101b30b588174bc53a21c57bed89b5e23e
zentol <s.motsu@web.de>
2014-06-18 14:53:59 +0200

    adjusted test aswell

--- stratosphere-java/src/test/java/eu/stratosphere/api/java/operator/ProjectionOperatorTest.java
+++ stratosphere-java/src/test/java/eu/stratosphere/api/java/operator/ProjectionOperatorTest.java
@@ -62,7 +62,7 @@ public class ProjectionOperatorTest {
 		
 		// should not work: too many fields
 		try {
-			tupleDs.project(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22);
+			tupleDs.project(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25);
 			Assert.fail();
 		} catch(IllegalArgumentException iae) {
 			// we're good here
