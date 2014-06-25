f427d4cbf8fc7e239117ceade7f58d2677b965b7
kay <fleischmann.kay@googlemail.com>
2014-06-25 16:07:33 +0200

    cleanup

--- stratosphere-core/src/main/java/eu/stratosphere/api/common/functions/GenericMapPartition.java
+++ stratosphere-core/src/main/java/eu/stratosphere/api/common/functions/GenericMapPartition.java
@@ -5,13 +5,12 @@ import eu.stratosphere.util.Collector;
 import java.util.Iterator;
 
 public interface GenericMapPartition<T, O> extends Function {
-
-    /**
-     * A user-implemented function that modifies or transforms an incoming object.
-     *
-     * @param records All records for the mapper
-     * @param out The collector to hand results to.
-     * @throws Exception
-     */
-    void mapPartition(Iterator<T> records, Collector<O> out) throws Exception;
+	/**
+	 * A user-implemented function that modifies or transforms an incoming object.
+	 *
+	 * @param records All records for the mapper
+	 * @param out The collector to hand results to.
+	 * @throws Exception
+	 */
+	void mapPartition(Iterator<T> records, Collector<O> out) throws Exception;
 }
