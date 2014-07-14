33b7c6a8f2c0c02de740a61729fe00863aa4e48f
gyfora <gyula.fora@gmail.com>
2014-07-14 16:29:16 +0200

    [streaming] addSource fix + added missing licensing

--- flink-addons/flink-streaming/src/main/java/eu/stratosphere/api/datastream/SourceFunction.java
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/api/datastream/SourceFunction.java
@@ -1,14 +1,25 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2014 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
 package eu.stratosphere.api.datastream;
 
-import java.io.Serializable;
-
 import eu.stratosphere.api.java.tuple.Tuple;
-import eu.stratosphere.streaming.api.StreamCollector;
+import eu.stratosphere.streaming.api.invokable.UserSourceInvokable;
 
-public abstract class SourceFunction<OUT extends Tuple> implements Serializable {
+public abstract class SourceFunction<OUT extends Tuple> extends UserSourceInvokable<OUT> {
 
 	private static final long serialVersionUID = 1L;
-
-	public abstract void invoke(StreamCollector<Tuple> collector);
 
 }
