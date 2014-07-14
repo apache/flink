9d5fc1bc07562d72c924cf322acd0d6861a6be13
jfeher <feherj@gmail.com>
2014-07-14 16:29:25 +0200

    [streaming] RMQSink and Test

--- flink-addons/flink-streaming/src/test/java/eu/stratosphere/streaming/api/StreamCollector2Test.java
+++ flink-addons/flink-streaming/src/test/java/eu/stratosphere/streaming/api/StreamCollector2Test.java
@@ -29,7 +29,7 @@ import eu.stratosphere.streaming.api.streamcomponent.MockRecordWriter;
 import eu.stratosphere.streaming.api.streamrecord.StreamRecord;
 import eu.stratosphere.streaming.util.MockRecordWriterFactory;
 
-public class StreamCollectorManagerTest {
+public class StreamCollector2Test {
 
 	StreamCollectorManager<Tuple> collector;
 	
@@ -55,7 +55,9 @@ public class StreamCollectorManagerTest {
 		fOut.add(rw2);
 		
 		collector = new StreamCollectorManager<Tuple>(batchSizesOfNotPartitioned, batchSizesOfPartitioned, parallelismOfOutput, keyPosition, batchTimeout, channelID, null, fOut,fOut);
+	
 		Tuple1<Integer> t = new Tuple1<Integer>();
+		StreamCollector<Tuple> sc1 = new StreamCollector<Tuple>(1, batchTimeout, channelID, null);
 		
 		t.f0 = 0;
 		collector.collect(t);
