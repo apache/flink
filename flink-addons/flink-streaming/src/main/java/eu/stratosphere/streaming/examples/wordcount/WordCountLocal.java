7345bf6a330d6cba0f8f0f72761ff05e23543581
Márton Balassi <balassi.marton@gmail.com>
2014-07-14 16:29:23 +0200

    [streaming] StreamExecutionEnvironment javadoc update

--- flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/examples/wordcount/WordCountLocal.java
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/examples/wordcount/WordCountLocal.java
@@ -22,8 +22,6 @@ import eu.stratosphere.streaming.util.TestDataUtil;
 
 public class WordCountLocal {
 
-	// This example will count the occurrence of each word in the input file.
-	
 	public static void main(String[] args) {
 
 		TestDataUtil.downloadIfNotExists("hamlet.txt");
