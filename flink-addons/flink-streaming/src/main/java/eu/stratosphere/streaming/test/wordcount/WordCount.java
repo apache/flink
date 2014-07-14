1debcc299578a695a4fab3b259a217798b6e3246
Márton Balassi <balassi.marton@gmail.com>
2014-07-14 16:28:54 +0200

    [streaming] WordCount Refactor

---
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/test/wordcount/WordCount.java
@@ -0,0 +1,39 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2014 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
+package eu.stratosphere.streaming.test.wordcount;
+
+import eu.stratosphere.nephele.jobgraph.JobGraph;
+import eu.stratosphere.streaming.api.JobGraphBuilder;
+
+import eu.stratosphere.test.util.TestBase2;
+import eu.stratosphere.types.StringValue;
+
+public class WordCount extends TestBase2 {
+
+	@Override
+	public JobGraph getJobGraph() {
+		JobGraphBuilder graphBuilder = new JobGraphBuilder("testGraph");
+		graphBuilder.setSource("WorCountSource", WordCountSource.class);
+		graphBuilder.setTask("WorCountTask", WordCountTask.class, 2);
+		graphBuilder.setSink("WordCountSink", WordCountSink.class);
+
+		graphBuilder.fieldsConnect("WorCountSource", "WorCountTask", 0,
+				StringValue.class);
+		graphBuilder.broadcastConnect("WorCountTask", "WordCountSink");
+
+		return graphBuilder.getJobGraph();
+	}
+}
