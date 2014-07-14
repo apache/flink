9d1806cc584440f687fda1830918f2f02002e2ec
mbalassi <balassi.marton@gmail.com>
2014-07-14 16:29:40 +0200

    [streaming] License update

--- flink-addons/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/function/FileSourceFunction.java
+++ flink-addons/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/function/FileSourceFunction.java
@@ -1,4 +1,4 @@
-/***********************************************************************************************************************
+/**
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- **********************************************************************************************************************/
+ */
 
 package org.apache.flink.streaming.api.function;
 
