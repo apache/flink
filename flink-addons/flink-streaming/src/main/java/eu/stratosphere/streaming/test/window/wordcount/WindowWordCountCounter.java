5f4858c296d8b02e7410bd11397f875a00365a47
Yingjun Wu <wu.yj0616@gmail.com>
2014-07-14 16:28:56 +0200

    [streaming] initiate windowing

---
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/test/window/wordcount/WindowWordCountCounter.java
@@ -0,0 +1,58 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2014 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
+package eu.stratosphere.streaming.test.window.wordcount;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import eu.stratosphere.streaming.api.invokable.UserTaskInvokable;
+import eu.stratosphere.types.IntValue;
+import eu.stratosphere.types.Record;
+import eu.stratosphere.types.StringValue;
+
+public class WindowWordCountCounter extends UserTaskInvokable {
+
+	private Map<String, Integer> wordCounts = new HashMap<String, Integer>();
+	private StringValue wordValue = new StringValue("");
+	private IntValue countValue = new IntValue(1);
+	private String word = "";
+	private Record outputRecord = new Record(wordValue, countValue);
+	private int count = 1;
+
+	@Override
+	public void invoke(Record record) throws Exception {
+
+		record.getFieldInto(0, wordValue);
+		word = wordValue.getValue();
+
+		if (wordCounts.containsKey(word)) {
+			count = wordCounts.get(word) + 1;
+			wordCounts.put(word, count);
+			countValue.setValue(count);
+			outputRecord.setField(0, wordValue);
+			outputRecord.setField(1, countValue);
+			emit(outputRecord);
+		} else {
+
+			wordCounts.put(word, 1);
+			countValue.setValue(1);
+			Record outputRecord = new Record(wordValue, countValue);
+			emit(outputRecord);
+
+		}
+
+	}
+}
---
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/test/window/wordcount/WindowWordCountSource.java
@@ -0,0 +1,61 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2014 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
+package eu.stratosphere.streaming.test.window.wordcount;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import eu.stratosphere.streaming.api.invokable.UserSourceInvokable;
+import eu.stratosphere.types.LongValue;
+import eu.stratosphere.types.Record;
+import eu.stratosphere.types.StringValue;
+
+public class WindowWordCountSource extends UserSourceInvokable {
+	private BufferedReader br = null;
+	private String line;
+	// the system should support long int.
+	private long timestamp;
+	private Record hamletRecord=null;
+	public WindowWordCountSource() {
+		try {
+			br = new BufferedReader(
+					new FileReader(
+							"src/main/java/eu/stratosphere/streaming/test/window/wordcount/hamlet.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void invoke() throws Exception {
+		line = br.readLine();
+		line=line.replaceAll("[\\-\\+\\.\\^:,]","");
+		timestamp = 0;
+		//while (line != null) {
+		for(int i=0; i<1000; ++i){
+			if(line==""){
+				continue;
+			}
+			hamletRecord = new Record(new StringValue("You do not understand yourself so clearly"));
+			System.out.println("========line number: "+timestamp+", "+line+"==========");
+			emit(hamletRecord);
+			line = br.readLine();
+			++timestamp;
+		}
+	}
+}
