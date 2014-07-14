634c927edfaf0345ddb2529e0a24ce5659b2893a
jfeher <feherj@gmail.com>
2014-07-14 16:29:33 +0200

    [streaming] Package restructure

---
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/api/FileStreamFunction.java
@@ -0,0 +1,50 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2014 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
+package eu.stratosphere.streaming.api;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import eu.stratosphere.api.java.tuple.Tuple1;
+import eu.stratosphere.util.Collector;
+
+public class FileStreamFunction extends SourceFunction<Tuple1<String>>{
+	private static final long serialVersionUID = 1L;
+	
+	private final String path;
+	private Tuple1<String> outTuple = new Tuple1<String>();
+	
+	public FileStreamFunction(String path) {
+		this.path = path;
+	}
+	
+	@Override
+	public void invoke(Collector<Tuple1<String>> collector) throws IOException {
+		while(true){
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			String line = br.readLine();
+			while (line != null) {
+				if (line != "") {
+					outTuple.f0 = line;
+					collector.collect(outTuple);
+				}
+				line = br.readLine();
+			}
+			br.close();
+		}
+	}
+}
---
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/api/streamcomponent/UnionStreamRecordReader.java
@@ -0,0 +1,84 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2014 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
+package eu.stratosphere.streaming.api.streamcomponent;
+
+import java.io.IOException;
+
+import eu.stratosphere.api.java.tuple.Tuple;
+import eu.stratosphere.api.java.typeutils.runtime.TupleSerializer;
+import eu.stratosphere.nephele.io.AbstractUnionRecordReader;
+import eu.stratosphere.nephele.io.MutableRecordReader;
+import eu.stratosphere.nephele.io.Reader;
+import eu.stratosphere.pact.runtime.plugable.DeserializationDelegate;
+import eu.stratosphere.streaming.api.streamrecord.StreamRecord;
+
+public final class UnionStreamRecordReader extends AbstractUnionRecordReader<StreamRecord>
+		implements Reader<StreamRecord> {
+
+	private final Class<? extends StreamRecord> recordType;
+
+	private StreamRecord lookahead;
+	private DeserializationDelegate<Tuple> deserializationDelegate;
+	private TupleSerializer<Tuple> tupleSerializer;
+
+	public UnionStreamRecordReader(MutableRecordReader<StreamRecord>[] recordReaders, Class<? extends StreamRecord> recordType,
+			DeserializationDelegate<Tuple> deserializationDelegate,
+			TupleSerializer<Tuple> tupleSerializer) {
+		super(recordReaders);
+		this.recordType = recordType;
+		this.deserializationDelegate = deserializationDelegate;
+		this.tupleSerializer = tupleSerializer;
+	}
+
+	@Override
+	public boolean hasNext() throws IOException, InterruptedException {
+		if (this.lookahead != null) {
+			return true;
+		} else {
+			StreamRecord record = instantiateRecordType();
+			record.setDeseralizationDelegate(deserializationDelegate, tupleSerializer);
+			if (getNextRecord(record)) {
+				this.lookahead = record;
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	@Override
+	public StreamRecord next() throws IOException, InterruptedException {
+		if (hasNext()) {
+			StreamRecord tmp = this.lookahead;
+			this.lookahead = null;
+			return tmp;
+		} else {
+			return null;
+		}
+	}
+
+	private StreamRecord instantiateRecordType() {
+		try {
+			return this.recordType.newInstance();
+		} catch (InstantiationException e) {
+			throw new RuntimeException("Cannot instantiate class '" + this.recordType.getName()
+					+ "'.", e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException("Cannot instantiate class '" + this.recordType.getName()
+					+ "'.", e);
+		}
+	}
+}
