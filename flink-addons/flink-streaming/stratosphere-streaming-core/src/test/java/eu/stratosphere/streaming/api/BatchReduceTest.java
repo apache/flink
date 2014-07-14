99375d546ff7c4f8b676a856115ab9b8d4b1e066
mbalassi <balassi.marton@gmail.com>
2014-07-14 16:29:36 +0200

    [streaming] Multiple iteration 1

--- flink-addons/flink-streaming/stratosphere-streaming-core/src/test/java/eu/stratosphere/streaming/api/BatchReduceTest.java
+++ flink-addons/flink-streaming/stratosphere-streaming-core/src/test/java/eu/stratosphere/streaming/api/BatchReduceTest.java
@@ -79,7 +79,8 @@ public class BatchReduceTest {
 	public void test() throws Exception {
 		LocalStreamEnvironment env = StreamExecutionEnvironment.createLocalEnvironment(PARALlELISM);
 
-		DataStream<Tuple1<Double>> dataStream0 = env.addSource(new MySource())
+		@SuppressWarnings("unused")
+		DataStream<Tuple1<Double>> dataStream = env.addSource(new MySource())
 				.batchReduce(new MyBatchReduce(), BATCH_SIZE).addSink(new MySink());
 
 		env.executeTest(MEMORYSIZE);
