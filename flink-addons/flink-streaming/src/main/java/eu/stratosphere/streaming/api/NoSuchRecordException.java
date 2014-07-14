65831b771b835a9989ec970fcbc8a7ce8bb9400f
gaborhermann <reckoner42@gmail.com>
2014-07-14 16:28:59 +0200

    [streaming] Added NoSuchRecordException and StreamRecord tests

--- flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/api/NoSuchRecordException.java
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/api/NoSuchRecordException.java
@@ -2,9 +2,6 @@ package eu.stratosphere.streaming.api;
 
 public class NoSuchRecordException extends StreamRecordException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 4935457355434561574L;
 
 }
--- flink-addons/flink-streaming/src/test/java/eu/stratosphere/streaming/api/StreamRecordTest.java
+++ flink-addons/flink-streaming/src/test/java/eu/stratosphere/streaming/api/StreamRecordTest.java
@@ -1,13 +1,62 @@
 package eu.stratosphere.streaming.api;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
+import eu.stratosphere.types.IntValue;
 import eu.stratosphere.types.StringValue;
 
 public class StreamRecordTest {
 
+	@Test
+	public void singleRecordSetGetTest() {
+		StreamRecord record = new StreamRecord(new StringValue("Stratosphere"), new IntValue(1));
+		
+		assertEquals(2, record.getNumOfFields());
+		assertEquals(1, record.getNumOfRecords());
+		assertEquals("Stratosphere", ((StringValue) record.getField(0)).getValue());
+		assertEquals(1, ((IntValue) record.getField(1)).getValue());
+		
+		record.setField(1, new StringValue("Big Data"));
+		assertEquals("Big Data", ((StringValue) record.getField(1)).getValue());
+		
+		record.setRecord(new IntValue(2), new StringValue("Big Data looks tiny from here."));
+		assertEquals(2, record.getNumOfFields());
+		assertEquals(1, record.getNumOfRecords());
+		assertEquals(2, ((IntValue) record.getField(0)).getValue());
+	}
+	
+	@Test
+	public void batchRecordSetGetTest() {
+		StreamRecord record = new StreamRecord(1, 2);
+		record.addRecord(new StringValue("1"));
+		record.addRecord(new IntValue(2));
+		record.addRecord(new StringValue("three"));
+		
+		try {
+			record.addRecord(new StringValue("4"), new IntValue(5));
+			fail();
+		} catch (RecordSizeMismatchException e) {
+		}
+			
+		assertEquals(1, record.getNumOfFields());
+		assertEquals(3, record.getNumOfRecords());
+		assertEquals("1", ((StringValue) record.getField(0,0)).getValue());
+		assertEquals(2, ((IntValue) record.getField(1,0)).getValue());
+		assertEquals("three", ((StringValue) record.getField(2,0)).getValue());
+		
+		record.setRecord(1, new StringValue("2"));
+		assertEquals("2", ((StringValue) record.getField(1,0)).getValue());
+		
+		record.addRecord(new StringValue("4"));
+		assertEquals(1, record.getNumOfFields());
+		assertEquals(4, record.getNumOfRecords());
+	}
+	
 	@Test
 	public void copyTest() {
 		StreamRecord a = new StreamRecord(new StringValue("Big"));
@@ -32,6 +81,13 @@ public class StreamRecordTest {
 			fail();
 		}
 		catch (RecordSizeMismatchException e) {
+		}
+		
+		try {
+			a.getField(3);
+			fail();
+		}
+		catch (NoSuchFieldException e) {
 		}
 	}
 
