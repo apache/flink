1136413a56fd4b45f82a533be40a2eed1eed0a81
Márton Balassi <balassi.marton@gmail.com>
2014-07-14 16:28:52 +0200

    [streaming] UserSinkInvokable fixed

--- flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/MyStream.java
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/MyStream.java
@@ -1,34 +1,11 @@
 package eu.stratosphere.streaming;
 
-import eu.stratosphere.nephele.io.RecordReader;
 import eu.stratosphere.nephele.io.channels.ChannelType;
 import eu.stratosphere.nephele.jobgraph.JobGraph;
-import eu.stratosphere.nephele.template.AbstractOutputTask;
 import eu.stratosphere.streaming.JobGraphBuilder.Partitioning;
 import eu.stratosphere.test.util.TestBase2;
-import eu.stratosphere.types.Record;
-import eu.stratosphere.types.StringValue;
 
 public class MyStream extends TestBase2 {
-  
-public static class MySink extends AbstractOutputTask {
-    private RecordReader<Record> input = null;
-
-    @Override
-    public void registerInputOutput() {
-      this.input = new RecordReader<Record>(this, Record.class);
-    }
-
-    @Override
-    public void invoke() throws Exception {
-      while (input.hasNext()) {
-      	StringValue value = new StringValue("");
-      	Record record = input.next();
-      	record.getFieldInto(0, value);
-        System.out.println(value.getValue());
-      }
-    }
-  }
   
   @Override
   public JobGraph getJobGraph() {
@@ -36,7 +13,7 @@ public static class MySink extends AbstractOutputTask {
     graphBuilder.setSource("infoSource", TestSourceInvokable.class, Partitioning.BROADCAST);
     graphBuilder.setSource("querySource", QuerySourceInvokable.class, Partitioning.BROADCAST);
     graphBuilder.setTask("cellTask", TestTaskInvokable.class, Partitioning.BROADCAST, 2);
-    graphBuilder.setSink("sink", MySink.class);
+    graphBuilder.setSink("sink", TestSinkInvokable.class);
     
     graphBuilder.connect("infoSource", "cellTask", ChannelType.INMEMORY);
     graphBuilder.connect("querySource", "cellTask", ChannelType.INMEMORY);
--- flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/UserSinkInvokable.java
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/UserSinkInvokable.java
@@ -1,10 +1,10 @@
 package eu.stratosphere.streaming;
 
-import eu.stratosphere.nephele.io.RecordWriter;
+import eu.stratosphere.nephele.io.RecordReader;
 import eu.stratosphere.types.Record;
 
 public interface UserSinkInvokable {
 
   public void invoke(Record record,
-      RecordWriter<Record> output) throws Exception;
+      RecordReader<Record> input) throws Exception;
 }
