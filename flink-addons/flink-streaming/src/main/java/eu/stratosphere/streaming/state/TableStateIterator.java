086d1a72b38a8ec0a5e4300a02daf4097e0897f3
Yingjun Wu <wu.yj0616@gmail.com>
2014-07-14 16:29:29 +0200

    [streaming] add disk dump sink, prepare to support multiple output stream

---
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/state/TableStateIterator.java
@@ -0,0 +1,38 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2014 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
+package eu.stratosphere.streaming.state;
+
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import eu.stratosphere.api.java.tuple.Tuple2;
+
+public class TableStateIterator<K, V>{
+
+	private Iterator<Entry<K, V>> iterator;
+	public TableStateIterator(Iterator<Entry<K, V>> iter){
+		iterator=iter;
+	}
+	
+	public boolean hasNext() {
+		return iterator.hasNext();
+	}
+
+	public Tuple2<K, V> next() {
+		Entry<K, V> entry=iterator.next();
+		return new Tuple2<K, V>(entry.getKey(), entry.getValue());
+	}
+}
