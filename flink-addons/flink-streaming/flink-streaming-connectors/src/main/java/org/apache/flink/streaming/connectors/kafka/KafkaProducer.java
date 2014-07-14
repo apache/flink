361526b0052b11912c60d4d1ff80cd3de4f6330e
mbalassi <balassi.marton@gmail.com>
2014-07-14 16:29:38 +0200

    [streaming] Src licenses updated

--- flink-addons/flink-streaming/flink-streaming-connectors/src/main/java/org/apache/flink/streaming/connectors/kafka/KafkaProducer.java
+++ flink-addons/flink-streaming/flink-streaming-connectors/src/main/java/org/apache/flink/streaming/connectors/kafka/KafkaProducer.java
@@ -1,15 +1,19 @@
 /***********************************************************************************************************************
  *
- * Copyright (C) 2010-2014 by the Stratosphere project (http://stratosphere.eu)
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  **********************************************************************************************************************/
 
@@ -22,18 +26,17 @@ import java.util.Properties;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 
-
 /**
- * This is a simple kafka producer that reads local file from disk and produces line streams.
- * To use the producer, a zookeeper server and a kafka server should be in service.
- * Run the following script to start a zookeeper server:
- * 		bin/zookeeper-server-start.sh config/zookeeper.properties
- * Run the following script to start a kafka server:
- * 		bin/kafka-server-start.sh config/server.properties
- * Run the following script to start the producer:
- * 		java -cp kafka-0.8/libs/*:yourJarFile.jar org.apache.flink.streaming.kafka.KafkaProducer yourTopicID kafkaServerIp
- * As an example:
- * 		java -cp kafka-0.8/libs/*:stratosphere-streaming.jar org.apache.flink.streaming.kafka.KafkaProducer test localhost:9092
+ * This is a simple kafka producer that reads local file from disk and produces
+ * line streams. To use the producer, a zookeeper server and a kafka server
+ * should be in service. Run the following script to start a zookeeper server:
+ * bin/zookeeper-server-start.sh config/zookeeper.properties Run the following
+ * script to start a kafka server: bin/kafka-server-start.sh
+ * config/server.properties Run the following script to start the producer: java
+ * -cp kafka-0.8/libs/*:yourJarFile.jar
+ * org.apache.flink.streaming.kafka.KafkaProducer yourTopicID kafkaServerIp As
+ * an example: java -cp kafka-0.8/libs/*:stratosphere-streaming.jar
+ * org.apache.flink.streaming.kafka.KafkaProducer test localhost:9092
  */
 public class KafkaProducer {
 	static kafka.javaapi.producer.Producer<Integer, String> producer;
@@ -47,24 +50,24 @@ public class KafkaProducer {
 				new ProducerConfig(props));
 	}
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		if (args.length == 3) {
-			String infilename=args[0];
-			String topicId=args[1];
-			String brokerAddr=args[2];
+			String infilename = args[0];
+			String topicId = args[1];
+			String brokerAddr = args[2];
 			ProducerPrepare(brokerAddr);
-			BufferedReader reader = new BufferedReader(new FileReader(infilename));
+			BufferedReader reader = new BufferedReader(new FileReader(
+					infilename));
 			while (true) {
-				String line=reader.readLine();
-				if(line==null){
+				String line = reader.readLine();
+				if (line == null) {
 					reader.close();
 					reader = new BufferedReader(new FileReader(infilename));
 					continue;
 				}
-				producer.send(new KeyedMessage<Integer, String>(
-						topicId, line));
+				producer.send(new KeyedMessage<Integer, String>(topicId, line));
 			}
-		}else{
+		} else {
 			System.out.println("please set filename!");
 			System.exit(-1);
 		}
