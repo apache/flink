cd47b2b4185d4c64479c4ff84f9251c06f13604c
Márton Balassi <balassi.marton@gmail.com>
2014-07-14 16:29:00 +0200

    [streaming] Added test package for Partitioners

---
+++ flink-addons/flink-streaming/src/test/java/eu/stratosphere/streaming/partitioner/BroadcastPartitionerTest.java
@@ -0,0 +1,31 @@
+package eu.stratosphere.streaming.partitioner;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import eu.stratosphere.streaming.api.streamrecord.StreamRecord;
+import eu.stratosphere.types.StringValue;
+
+public class BroadcastPartitionerTest {
+
+	private BroadcastPartitioner broadcastPartitioner;
+	private StreamRecord streamRecord = new StreamRecord(new StringValue());
+
+	@Before
+	public void setBroadcastPartitioner() {
+		broadcastPartitioner = new BroadcastPartitioner();
+	}
+
+	@Test
+	public void testSelectChannels() {
+		int[] first = new int[] { 0 };
+		int[] second = new int[] { 0, 1 };
+		int[] sixth = new int[] { 0, 1, 2, 3, 4, 5 };
+		
+		assertArrayEquals(first, broadcastPartitioner.selectChannels(streamRecord, 1));
+		assertArrayEquals(second, broadcastPartitioner.selectChannels(streamRecord, 1));
+		assertArrayEquals(sixth, broadcastPartitioner.selectChannels(streamRecord, 1));
+	}
+}
