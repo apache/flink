b03c029cff25246d7211f331a54be3ffcbe51fe7
gyfora <gyula.fora@gmail.com>
2014-07-14 16:29:17 +0200

    [streaming] window join modified for new api

--- flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/examples/window/join/WindowJoinSourceTwo.java
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/examples/window/join/WindowJoinSourceTwo.java
@@ -18,30 +18,28 @@ package eu.stratosphere.streaming.examples.window.join;
 import java.util.Random;
 
 import eu.stratosphere.api.java.tuple.Tuple4;
-import eu.stratosphere.streaming.api.invokable.UserSourceInvokable;
-import eu.stratosphere.streaming.api.streamrecord.StreamRecord;
+import eu.stratosphere.streaming.api.SourceFunction;
+import eu.stratosphere.util.Collector;
 
-public class WindowJoinSourceTwo extends UserSourceInvokable {
+public class WindowJoinSourceTwo extends SourceFunction<Tuple4<String, String, Integer, Long>> {
 
 	private static final long serialVersionUID = -5897483980082089771L;
 
-	private String[] names = { "tom", "jerry", "alice", "bob", "john", "grace",
-			"sasa", "lawrance", "andrew", "jean", "richard", "smith", "gorge",
-			"black", "peter" };
+	private String[] names = { "tom", "jerry", "alice", "bob", "john", "grace", "sasa", "lawrance",
+			"andrew", "jean", "richard", "smith", "gorge", "black", "peter" };
 	private Random rand = new Random();
-	private StreamRecord outRecord = new StreamRecord(
-			new Tuple4<String, String, String, Long>());
-	private long progress = 0L;
+	private Tuple4<String, String, Integer, Long> outRecord = new Tuple4<String, String, Integer, Long>();
+	private Long progress = 0L;
 
 	@Override
-	public void invoke() throws Exception {
+	public void invoke(Collector<Tuple4<String, String, Integer, Long>> collector) throws Exception {
 		while (true) {
-			outRecord.setString(0, "grade");
-			outRecord.setString(1, names[rand.nextInt(names.length)]);
-			outRecord.setString(2, String.valueOf((char)(rand.nextInt(26)+'A')));
-			outRecord.setLong(3, progress);
-			emit(outRecord);
-			progress+=1;
+			outRecord.f0 = "grade";
+			outRecord.f1 = names[rand.nextInt(names.length)];
+			outRecord.f2 = rand.nextInt(5) + 1;
+			outRecord.f3 = progress;
+			collector.collect(outRecord);
+			progress += 1;
 		}
 	}
 }
