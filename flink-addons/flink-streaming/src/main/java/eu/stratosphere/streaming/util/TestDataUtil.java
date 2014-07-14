08f578c73a7972a4f9f1e84fdea7fcb903801faf
Eszes Dávid <eszesdavid@gmail.com>
2014-07-14 16:29:22 +0200

    [streaming] remove java.nio from TestDataUtilTest

--- flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/util/TestDataUtil.java
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/util/TestDataUtil.java
@@ -19,7 +19,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
