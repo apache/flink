b2f8f6e10ad8a36a92928185b23827944990748a
Márton Balassi <balassi.marton@gmail.com>
2014-07-14 16:29:27 +0200

    [streaming] Remove dms repo from pom

---
+++ flink-addons/flink-streaming/src/main/java/eu/stratosphere/streaming/performance/WordCountPerformanceSplitter.java
@@ -0,0 +1,52 @@
+/***********************************************************************************************************************
+ *
+ * Copyright (C) 2010-2014 by the Stratosphere project (http://stratosphere.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ **********************************************************************************************************************/
+
+package eu.stratosphere.streaming.performance;
+
+import eu.stratosphere.api.java.functions.FlatMapFunction;
+import eu.stratosphere.api.java.tuple.Tuple1;
+import eu.stratosphere.streaming.util.PerformanceCounter;
+import eu.stratosphere.util.Collector;
+
+public class WordCountPerformanceSplitter extends FlatMapFunction<Tuple1<String>, Tuple1<String>> {
+
+	private static final long serialVersionUID = 1L;
+
+	private Tuple1<String> outTuple = new Tuple1<String>();
+
+	 PerformanceCounter pCounter = new
+	 PerformanceCounter("SplitterEmitCounter", 1000, 1000, 30000,
+	 "/home/judit/strato/perf/broadcast4.csv");
+
+	@Override
+	public void flatMap(Tuple1<String> inTuple, Collector<Tuple1<String>> out) throws Exception {
+
+		for (String word : inTuple.f0.split(" ")) {
+			outTuple.f0 = word;
+			// pTimer.startTimer();
+			out.collect(outTuple);
+			// pTimer.stopTimer();
+			pCounter.count();
+		}
+	}
+
+	// @Override
+	// public String getResult() {
+	// pCounter.writeCSV();
+	// pTimer.writeCSV();
+	// return "";
+	// }
+
+}
