Frank-Wu/incubator-flink

[FLINK-1005] Make GroupReduce configurable to use either mutable or immutable object mode

stratosphere-runtime/src/main/java/eu/stratosphere/pact/runtime/task/GroupReduceDriver.java
@@ -21,6 +21,7 @@ import eu.stratosphere.api.common.typeutils.TypeComparator;
 import eu.stratosphere.api.common.typeutils.TypeSerializer;
 import eu.stratosphere.pact.runtime.task.util.TaskConfig;
 import eu.stratosphere.pact.runtime.util.KeyGroupedIterator;
+import eu.stratosphere.pact.runtime.util.KeyGroupedIteratorImmutable;
 import eu.stratosphere.util.Collector;
 import eu.stratosphere.util.MutableObjectIterator;
 
@@ -46,7 +47,7 @@ public class GroupReduceDriver<IT, OT> implements PactDriver<GenericGroupReduce<
 
 	private TypeComparator<IT> comparator;
 	
-	private boolean mutableObjectMode = false;
+	private boolean mutableObjectMode;
 	
 	private volatile boolean running;
 
@@ -86,6 +87,12 @@ public class GroupReduceDriver<IT, OT> implements PactDriver<GenericGroupReduce<
 		this.serializer = this.taskContext.<IT>getInputSerializer(0).getSerializer();
 		this.comparator = this.taskContext.getInputComparator(0);
 		this.input = this.taskContext.getInput(0);
+		
+		this.mutableObjectMode = config.getMutableObjectMode();
+		
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("GroupReduceDriver uses " + (this.mutableObjectMode ? "MUTABLE" : "IMMUTABLE") + " object mode.");
+		}
 	}
 
 	@Override
@@ -94,15 +101,23 @@ public class GroupReduceDriver<IT, OT> implements PactDriver<GenericGroupReduce<
 			LOG.debug(this.taskContext.formatLogString("GroupReducer preprocessing done. Running GroupReducer code."));
 		}
 
-		final KeyGroupedIterator<IT> iter = new KeyGroupedIterator<IT>(this.input, this.serializer, this.comparator);
-
 		// cache references on the stack
 		final GenericGroupReduce<IT, OT> stub = this.taskContext.getStub();
 		final Collector<OT> output = this.taskContext.getOutputCollector();
-
-		// run stub implementation
-		while (this.running && iter.nextKey()) {
-			stub.reduce(iter.getValues(), output);
+		
+		if (mutableObjectMode) {
+			final KeyGroupedIterator<IT> iter = new KeyGroupedIterator<IT>(this.input, this.serializer, this.comparator);
+			// run stub implementation
+			while (this.running && iter.nextKey()) {
+				stub.reduce(iter.getValues(), output);
+			}
+		}
+		else {
+			final KeyGroupedIteratorImmutable<IT> iter = new KeyGroupedIteratorImmutable<IT>(this.input, this.serializer, this.comparator);
+			// run stub implementation
+			while (this.running && iter.nextKey()) {
+				stub.reduce(iter.getValues(), output);
+			}
 		}
 	}
 

stratosphere-runtime/src/test/java/eu/stratosphere/pact/runtime/task/drivers/GroupReduceDriverTest.java
@@ -15,6 +15,7 @@
 
 package eu.stratosphere.pact.runtime.task.drivers;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -29,7 +30,6 @@ import eu.stratosphere.api.java.typeutils.TupleTypeInfo;
 import eu.stratosphere.api.java.typeutils.TypeExtractor;
 import eu.stratosphere.pact.runtime.task.DriverStrategy;
 import eu.stratosphere.pact.runtime.task.GroupReduceDriver;
-import eu.stratosphere.pact.runtime.test.util.DiscardingOutputCollector;
 import eu.stratosphere.pact.runtime.util.EmptyMutableObjectIterator;
 import eu.stratosphere.pact.runtime.util.RegularToMutableObjectIterator;
 import eu.stratosphere.types.IntValue;
@@ -52,14 +52,18 @@ public class GroupReduceDriverTest {
 			TypeComparator<Tuple2<String, Integer>> comparator = typeInfo.createComparator(new int[]{0}, new boolean[] {true});
 			context.setDriverStrategy(DriverStrategy.SORTED_GROUP_REDUCE);
 			
+			GatheringCollector<Tuple2<String, Integer>> result = new GatheringCollector<Tuple2<String,Integer>>(typeInfo.createSerializer());
+			
 			context.setInput1(input, typeInfo.createSerializer());
 			context.setComparator1(comparator);
-			context.setCollector(new DiscardingOutputCollector<Tuple2<String, Integer>>());
+			context.setCollector(result);
 			
 			GroupReduceDriver<Tuple2<String, Integer>, Tuple2<String, Integer>> driver = new GroupReduceDriver<Tuple2<String, Integer>, Tuple2<String, Integer>>();
 			driver.setup(context);
 			driver.prepare();
 			driver.run();
+			
+			Assert.assertTrue(result.getList().isEmpty());
 		}
 		catch (Exception e) {
 			System.err.println(e.getMessage());
@@ -140,7 +144,84 @@ public class GroupReduceDriverTest {
 		}
 	}
 	
-
+	@Test
+	public void testAllReduceDriverIncorrectlyAccumulatingMutable() {
+		try {
+			TestTaskContext<GenericGroupReduce<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>>, Tuple2<StringValue, IntValue>> context =
+					new TestTaskContext<GenericGroupReduce<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>>, Tuple2<StringValue, IntValue>>();
+			
+			List<Tuple2<StringValue, IntValue>> data = DriverTestData.createReduceMutableData();
+			TupleTypeInfo<Tuple2<StringValue, IntValue>> typeInfo = (TupleTypeInfo<Tuple2<StringValue, IntValue>>) TypeExtractor.getForObject(data.get(0));
+			MutableObjectIterator<Tuple2<StringValue, IntValue>> input = new RegularToMutableObjectIterator<Tuple2<StringValue, IntValue>>(data.iterator(), typeInfo.createSerializer());
+			TypeComparator<Tuple2<StringValue, IntValue>> comparator = typeInfo.createComparator(new int[]{0}, new boolean[] {true});
+			
+			GatheringCollector<Tuple2<StringValue, IntValue>> result = new GatheringCollector<Tuple2<StringValue, IntValue>>(typeInfo.createSerializer());
+			
+			context.setDriverStrategy(DriverStrategy.SORTED_GROUP_REDUCE);
+			context.setInput1(input, typeInfo.createSerializer());
+			context.setComparator1(comparator);
+			context.setCollector(result);
+			context.setUdf(new ConcatSumMutableAccumulatingReducer());
+			
+			GroupReduceDriver<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>> driver = new GroupReduceDriver<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>>();
+			driver.setup(context);
+			driver.prepare();
+			driver.run();
+			
+			Object[] res = result.getList().toArray();
+			Object[] expected = DriverTestData.createReduceMutableDataGroupedResult().toArray();
+			
+			try {
+				DriverTestData.compareTupleArrays(expected, res);
+				Assert.fail("Accumulationg mutable objects is expected to result in incorrect values.");
+			}
+			catch (AssertionError e) {
+				// expected
+			}
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testAllReduceDriverAccumulatingImmutable() {
+		try {
+			TestTaskContext<GenericGroupReduce<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>>, Tuple2<StringValue, IntValue>> context =
+					new TestTaskContext<GenericGroupReduce<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>>, Tuple2<StringValue, IntValue>>();
+			
+			List<Tuple2<StringValue, IntValue>> data = DriverTestData.createReduceMutableData();
+			TupleTypeInfo<Tuple2<StringValue, IntValue>> typeInfo = (TupleTypeInfo<Tuple2<StringValue, IntValue>>) TypeExtractor.getForObject(data.get(0));
+			MutableObjectIterator<Tuple2<StringValue, IntValue>> input = new RegularToMutableObjectIterator<Tuple2<StringValue, IntValue>>(data.iterator(), typeInfo.createSerializer());
+			TypeComparator<Tuple2<StringValue, IntValue>> comparator = typeInfo.createComparator(new int[]{0}, new boolean[] {true});
+			
+			GatheringCollector<Tuple2<StringValue, IntValue>> result = new GatheringCollector<Tuple2<StringValue, IntValue>>(typeInfo.createSerializer());
+			
+			context.setDriverStrategy(DriverStrategy.SORTED_GROUP_REDUCE);
+			context.setInput1(input, typeInfo.createSerializer());
+			context.setComparator1(comparator);
+			context.setCollector(result);
+			context.setUdf(new ConcatSumMutableAccumulatingReducer());
+			context.setMutableObjectMode(false);
+			
+			GroupReduceDriver<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>> driver = new GroupReduceDriver<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>>();
+			driver.setup(context);
+			driver.prepare();
+			driver.run();
+			
+			Object[] res = result.getList().toArray();
+			Object[] expected = DriverTestData.createReduceMutableDataGroupedResult().toArray();
+			
+			DriverTestData.compareTupleArrays(expected, res);
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}
+	}
 	
 	// --------------------------------------------------------------------------------------------
 	//  Test UDFs
@@ -177,6 +258,28 @@ public class GroupReduceDriverTest {
 			}
 			
 			out.collect(current);
+		}
+	}
+	
+	public static final class ConcatSumMutableAccumulatingReducer extends GroupReduceFunction<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>> {
+
+		@Override
+		public void reduce(Iterator<Tuple2<StringValue, IntValue>> values, Collector<Tuple2<StringValue, IntValue>> out) throws Exception {
+			List<Tuple2<StringValue, IntValue>> all = new ArrayList<Tuple2<StringValue,IntValue>>();
+			
+			while (values.hasNext()) {
+				all.add(values.next());
+			}
+			
+			Tuple2<StringValue, IntValue> result = all.get(0);
+			
+			for (int i = 1; i < all.size(); i++) {
+				Tuple2<StringValue, IntValue> e = all.get(i);
+				result.f0.append(e.f0);
+				result.f1.setValue(result.f1.getValue() + e.f1.getValue());
+			}
+			
+			out.collect(result);
 		}
 	}
 }

apache/flink

flink-runtime/src/main/java/org/apache/flink/runtime/operators/GroupReduceDriver.java
@@ -26,6 +26,7 @@ import org.apache.flink.api.common.typeutils.TypeComparator;
 import org.apache.flink.api.common.typeutils.TypeSerializer;
 import org.apache.flink.runtime.operators.util.TaskConfig;
 import org.apache.flink.runtime.util.KeyGroupedIterator;
+import org.apache.flink.runtime.util.KeyGroupedIteratorImmutable;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.MutableObjectIterator;
 
@@ -91,6 +92,12 @@ public class GroupReduceDriver<IT, OT> implements PactDriver<GroupReduceFunction
 		this.serializer = this.taskContext.<IT>getInputSerializer(0).getSerializer();
 		this.comparator = this.taskContext.getDriverComparator(0);
 		this.input = this.taskContext.getInput(0);
+		
+		this.mutableObjectMode = config.getMutableObjectMode();
+		
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("GroupReduceDriver uses " + (this.mutableObjectMode ? "MUTABLE" : "IMMUTABLE") + " object mode.");
+		}
 	}
 
 	@Override
@@ -99,15 +106,23 @@ public class GroupReduceDriver<IT, OT> implements PactDriver<GroupReduceFunction
 			LOG.debug(this.taskContext.formatLogString("GroupReducer preprocessing done. Running GroupReducer code."));
 		}
 
-		final KeyGroupedIterator<IT> iter = new KeyGroupedIterator<IT>(this.input, this.serializer, this.comparator);
-
 		// cache references on the stack
 		final GroupReduceFunction<IT, OT> stub = this.taskContext.getStub();
 		final Collector<OT> output = this.taskContext.getOutputCollector();
-
-		// run stub implementation
-		while (this.running && iter.nextKey()) {
-			stub.reduce(iter.getValues(), output);
+		
+		if (mutableObjectMode) {
+			final KeyGroupedIterator<IT> iter = new KeyGroupedIterator<IT>(this.input, this.serializer, this.comparator);
+			// run stub implementation
+			while (this.running && iter.nextKey()) {
+				stub.reduce(iter.getValues(), output);
+			}
+		}
+		else {
+			final KeyGroupedIteratorImmutable<IT> iter = new KeyGroupedIteratorImmutable<IT>(this.input, this.serializer, this.comparator);
+			// run stub implementation
+			while (this.running && iter.nextKey()) {
+				stub.reduce(iter.getValues(), output);
+			}
 		}
 	}
 

flink-runtime/src/test/java/org/apache/flink/runtime/operators/drivers/GroupReduceDriverTest.java
@@ -18,6 +18,7 @@
 
 package org.apache.flink.runtime.operators.drivers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.flink.api.common.functions.GroupReduceFunction;
@@ -28,7 +29,6 @@ import org.apache.flink.api.java.typeutils.TupleTypeInfo;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.runtime.operators.DriverStrategy;
 import org.apache.flink.runtime.operators.GroupReduceDriver;
-import org.apache.flink.runtime.operators.testutils.DiscardingOutputCollector;
 import org.apache.flink.runtime.util.EmptyMutableObjectIterator;
 import org.apache.flink.runtime.util.RegularToMutableObjectIterator;
 import org.apache.flink.types.IntValue;
@@ -53,14 +53,18 @@ public class GroupReduceDriverTest {
 			TypeComparator<Tuple2<String, Integer>> comparator = typeInfo.createComparator(new int[]{0}, new boolean[] {true});
 			context.setDriverStrategy(DriverStrategy.SORTED_GROUP_REDUCE);
 			
+			GatheringCollector<Tuple2<String, Integer>> result = new GatheringCollector<Tuple2<String,Integer>>(typeInfo.createSerializer());
+			
 			context.setInput1(input, typeInfo.createSerializer());
 			context.setComparator1(comparator);
-			context.setCollector(new DiscardingOutputCollector<Tuple2<String, Integer>>());
+			context.setCollector(result);
 			
 			GroupReduceDriver<Tuple2<String, Integer>, Tuple2<String, Integer>> driver = new GroupReduceDriver<Tuple2<String, Integer>, Tuple2<String, Integer>>();
 			driver.setup(context);
 			driver.prepare();
 			driver.run();
+			
+			Assert.assertTrue(result.getList().isEmpty());
 		}
 		catch (Exception e) {
 			System.err.println(e.getMessage());
@@ -141,7 +145,84 @@ public class GroupReduceDriverTest {
 		}
 	}
 	
-
+	@Test
+	public void testAllReduceDriverIncorrectlyAccumulatingMutable() {
+		try {
+			TestTaskContext<GroupReduceFunction<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>>, Tuple2<StringValue, IntValue>> context =
+					new TestTaskContext<GroupReduceFunction<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>>, Tuple2<StringValue, IntValue>>();
+			
+			List<Tuple2<StringValue, IntValue>> data = DriverTestData.createReduceMutableData();
+			TupleTypeInfo<Tuple2<StringValue, IntValue>> typeInfo = (TupleTypeInfo<Tuple2<StringValue, IntValue>>) TypeExtractor.getForObject(data.get(0));
+			MutableObjectIterator<Tuple2<StringValue, IntValue>> input = new RegularToMutableObjectIterator<Tuple2<StringValue, IntValue>>(data.iterator(), typeInfo.createSerializer());
+			TypeComparator<Tuple2<StringValue, IntValue>> comparator = typeInfo.createComparator(new int[]{0}, new boolean[] {true});
+			
+			GatheringCollector<Tuple2<StringValue, IntValue>> result = new GatheringCollector<Tuple2<StringValue, IntValue>>(typeInfo.createSerializer());
+			
+			context.setDriverStrategy(DriverStrategy.SORTED_GROUP_REDUCE);
+			context.setInput1(input, typeInfo.createSerializer());
+			context.setComparator1(comparator);
+			context.setCollector(result);
+			context.setUdf(new ConcatSumMutableAccumulatingReducer());
+			
+			GroupReduceDriver<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>> driver = new GroupReduceDriver<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>>();
+			driver.setup(context);
+			driver.prepare();
+			driver.run();
+			
+			Object[] res = result.getList().toArray();
+			Object[] expected = DriverTestData.createReduceMutableDataGroupedResult().toArray();
+			
+			try {
+				DriverTestData.compareTupleArrays(expected, res);
+				Assert.fail("Accumulationg mutable objects is expected to result in incorrect values.");
+			}
+			catch (AssertionError e) {
+				// expected
+			}
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testAllReduceDriverAccumulatingImmutable() {
+		try {
+			TestTaskContext<GroupReduceFunction<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>>, Tuple2<StringValue, IntValue>> context =
+					new TestTaskContext<GroupReduceFunction<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>>, Tuple2<StringValue, IntValue>>();
+			
+			List<Tuple2<StringValue, IntValue>> data = DriverTestData.createReduceMutableData();
+			TupleTypeInfo<Tuple2<StringValue, IntValue>> typeInfo = (TupleTypeInfo<Tuple2<StringValue, IntValue>>) TypeExtractor.getForObject(data.get(0));
+			MutableObjectIterator<Tuple2<StringValue, IntValue>> input = new RegularToMutableObjectIterator<Tuple2<StringValue, IntValue>>(data.iterator(), typeInfo.createSerializer());
+			TypeComparator<Tuple2<StringValue, IntValue>> comparator = typeInfo.createComparator(new int[]{0}, new boolean[] {true});
+			
+			GatheringCollector<Tuple2<StringValue, IntValue>> result = new GatheringCollector<Tuple2<StringValue, IntValue>>(typeInfo.createSerializer());
+			
+			context.setDriverStrategy(DriverStrategy.SORTED_GROUP_REDUCE);
+			context.setInput1(input, typeInfo.createSerializer());
+			context.setComparator1(comparator);
+			context.setCollector(result);
+			context.setUdf(new ConcatSumMutableAccumulatingReducer());
+			context.setMutableObjectMode(false);
+			
+			GroupReduceDriver<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>> driver = new GroupReduceDriver<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>>();
+			driver.setup(context);
+			driver.prepare();
+			driver.run();
+			
+			Object[] res = result.getList().toArray();
+			Object[] expected = DriverTestData.createReduceMutableDataGroupedResult().toArray();
+			
+			DriverTestData.compareTupleArrays(expected, res);
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}
+	}
 	
 	// --------------------------------------------------------------------------------------------
 	//  Test UDFs
@@ -176,6 +257,28 @@ public class GroupReduceDriverTest {
 			}
 			
 			out.collect(current);
+		}
+	}
+	
+	public static final class ConcatSumMutableAccumulatingReducer implements GroupReduceFunction<Tuple2<StringValue, IntValue>, Tuple2<StringValue, IntValue>> {
+
+		@Override
+		public void reduce(Iterable<Tuple2<StringValue, IntValue>> values, Collector<Tuple2<StringValue, IntValue>> out) throws Exception {
+			List<Tuple2<StringValue, IntValue>> all = new ArrayList<Tuple2<StringValue,IntValue>>();
+			
+			for (Tuple2<StringValue, IntValue> t : values) {
+				all.add(t);
+			}
+			
+			Tuple2<StringValue, IntValue> result = all.get(0);
+			
+			for (int i = 1; i < all.size(); i++) {
+				Tuple2<StringValue, IntValue> e = all.get(i);
+				result.f0.append(e.f0);
+				result.f1.setValue(result.f1.getValue() + e.f1.getValue());
+			}
+			
+			out.collect(result);
 		}
 	}
 }