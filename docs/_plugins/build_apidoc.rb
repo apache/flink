da21ea3ef23b56d5b4ea514bcc57873537fae157
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2014-10-10 20:43:02 +0200

    [doc] Fix javadoc building.
    The java8 Xdoclint:none fix was not correctly transferred when moving
    the javadoc building to a jekyll plugin.

--- docs/_plugins/build_apidoc.rb
+++ docs/_plugins/build_apidoc.rb
@@ -36,8 +36,10 @@ if ENV['BUILD_API'] == '1' then
   puts `mvn clean install -DskipTests`
 
   puts "Generating Javadoc"
-  puts `mvn javadoc:aggregate -Pdocs-and-source $JAVA8JAVADOCFIX -Dmaven.javadoc.failOnError=false -Dquiet=true -Dheader="<a href=\"/docs/0.7-incubating/\" target=\"_top\"><h1>Back to Flink Documentation</h1></a>"`
-  
+  javadoc_command = %{mvn javadoc:aggregate -Pdocs-and-source #{java8_doc_fix} -Dmaven.javadoc.failOnError=false -Dquiet=true -Dheader="<a href=\"/docs/0.7-incubating/\" target=\"_top\"><h1>Back to Flink Documentation</h1></a>"}
+  puts "Running: #{javadoc_command}"
+  puts `#{javadoc_command}`
+
   cd("docs")
 
   mkdir_p "api"
