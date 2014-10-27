58f7d303866fad6af4b23d2695d6273ef74b6a1d
ghermann <reckoner42@gmail.com>
2014-10-27 19:04:50 +0100

    [FLINK-1174] [streaming] Added window join operator

---
+++ flink-addons/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/function/co/JoinWindowFunction.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.api.function.co;
+
+import java.util.List;
+
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.util.keys.FieldsKeySelector;
+import org.apache.flink.util.Collector;
+
+public class JoinWindowFunction<IN1, IN2> implements CoWindowFunction<IN1, IN2, Tuple2<IN1, IN2>> {
+	private static final long serialVersionUID = 1L;
+
+	private KeySelector<IN1, Object> keySelector1;
+	private KeySelector<IN2, Object> keySelector2;
+
+	public JoinWindowFunction() {
+	}
+
+	public JoinWindowFunction(TypeInformation<IN1> inType1, TypeInformation<IN2> inType2,
+			int positionIn1, int positionIn2) {
+		keySelector1 = new FieldsKeySelector<IN1>(inType1, positionIn1);
+		keySelector2 = new FieldsKeySelector<IN2>(inType2, positionIn2);
+	}
+
+	@Override
+	public void coWindow(List<IN1> first, List<IN2> second, Collector<Tuple2<IN1, IN2>> out)
+			throws Exception {
+		for (IN1 item1 : first) {
+			for (IN2 item2 : second) {
+				if (keySelector1.getKey(item1).equals(keySelector2.getKey(item2))) {
+					out.collect(new Tuple2<IN1, IN2>(item1, item2));
+				}
+			}
+		}
+	}
+}
---
+++ flink-addons/flink-streaming/flink-streaming-core/src/test/java/org/apache/flink/streaming/api/WindowCrossJoinTest.java
@@ -0,0 +1,152 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.api;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.LocalStreamEnvironment;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.function.sink.SinkFunction;
+import org.apache.flink.streaming.api.invokable.util.TimeStamp;
+import org.junit.Test;
+
+public class WindowCrossJoinTest {
+	private static final long MEMORYSIZE = 32;
+
+	private static ArrayList<Tuple2<Tuple2<Integer, String>, Integer>> joinResults = new ArrayList<Tuple2<Tuple2<Integer, String>, Integer>>();
+	private static ArrayList<Tuple2<Tuple2<Integer, String>, Integer>> joinExpectedResults = new ArrayList<Tuple2<Tuple2<Integer, String>, Integer>>();
+
+	private static ArrayList<Tuple2<Tuple2<Integer, String>, Integer>> crossResults = new ArrayList<Tuple2<Tuple2<Integer, String>, Integer>>();
+	private static ArrayList<Tuple2<Tuple2<Integer, String>, Integer>> crossExpectedResults = new ArrayList<Tuple2<Tuple2<Integer, String>, Integer>>();
+
+	@Test
+	public void test() throws Exception {
+		LocalStreamEnvironment env = StreamExecutionEnvironment.createLocalEnvironment(1);
+		env.setBufferTimeout(1);
+
+		ArrayList<Tuple2<Integer, String>> in1 = new ArrayList<Tuple2<Integer, String>>();
+		ArrayList<Integer> in2 = new ArrayList<Integer>();
+
+		in1.add(new Tuple2<Integer, String>(10, "a"));
+		in1.add(new Tuple2<Integer, String>(20, "b"));
+		in1.add(new Tuple2<Integer, String>(20, "x"));
+		in1.add(new Tuple2<Integer, String>(0, "y"));
+
+		in2.add(0);
+		in2.add(5);
+		in2.add(20);
+
+		joinExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(20, "b"), 20));
+		joinExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(20, "x"), 20));
+		joinExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(0, "y"), 0));
+
+		crossExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(10, "a"), 0));
+		crossExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(10, "a"), 5));
+		crossExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(10, "a"), 20));
+		crossExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(20, "b"), 0));
+		crossExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(20, "b"), 5));
+		crossExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(20, "b"), 20));
+		crossExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(20, "x"), 0));
+		crossExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(20, "x"), 5));
+		crossExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(20, "x"), 20));
+		crossExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(0, "y"), 0));
+		crossExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(0, "y"), 5));
+		crossExpectedResults.add(new Tuple2<Tuple2<Integer, String>, Integer>(
+				new Tuple2<Integer, String>(0, "y"), 20));
+
+		DataStream<Tuple2<Integer, String>> inStream1 = env.fromCollection(in1);
+		DataStream<Integer> inStream2 = env.fromCollection(in2);
+
+		inStream1.windowJoin(inStream2, 1000, 1000, new MyTimestamp1(), new MyTimestamp2(), 0, 0)
+				.addSink(new JoinResultSink());
+
+		inStream1.windowCross(inStream2, 1000, 1000, new MyTimestamp1(), new MyTimestamp2())
+			.addSink(new CrossResultSink());
+
+		env.executeTest(MEMORYSIZE);
+		
+		assertEquals(joinExpectedResults, joinResults);
+		assertEquals(crossExpectedResults, crossResults);
+	}
+
+	private static class MyTimestamp1 implements TimeStamp<Tuple2<Integer, String>> {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public long getTimestamp(Tuple2<Integer, String> value) {
+			return 101L;
+		}
+
+		@Override
+		public long getStartTime() {
+			return 100L;
+		}
+	}
+
+	private static class MyTimestamp2 implements TimeStamp<Integer> {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public long getTimestamp(Integer value) {
+			return 101L;
+		}
+
+		@Override
+		public long getStartTime() {
+			return 100L;
+		}
+	}
+
+	private static class JoinResultSink implements
+			SinkFunction<Tuple2<Tuple2<Integer, String>, Integer>> {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void invoke(Tuple2<Tuple2<Integer, String>, Integer> value) {
+			joinResults.add(value);
+		}
+	}
+
+	private static class CrossResultSink implements
+			SinkFunction<Tuple2<Tuple2<Integer, String>, Integer>> {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void invoke(Tuple2<Tuple2<Integer, String>, Integer> value) {
+			crossResults.add(value);
+		}
+	}
+}
