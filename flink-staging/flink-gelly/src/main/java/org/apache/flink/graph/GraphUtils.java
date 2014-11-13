9b000da2521636252d95ccd0e1d02253833b7145
vasia <vasilikikalavri@gmail.com>
2014-11-13 12:15:04 +0100

    [FLINK-1201] [gelly] fix count to work with Datasets of tuples, added test for numberOfVertices, numberOfEdges

--- flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/GraphUtils.java
+++ flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/GraphUtils.java
@@ -1,28 +1,33 @@
 package flink.graphs;
 
-
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.tuple.Tuple;
 
+@SuppressWarnings("serial")
 public class GraphUtils {
 
-    @SuppressWarnings("serial")
-	public static DataSet<Integer> count (DataSet<Object> set) {
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static DataSet<Integer> count(DataSet set) {
         return set
-                .map(new MapFunction<Object, Integer>() {
-                    @Override
-                    public Integer map(Object o) throws Exception {
-                        return 1;
-                    }
-                })
-                .reduce(new ReduceFunction<Integer>() {
-                    @Override
-                    public Integer reduce(Integer one, Integer two) throws Exception {
-                        return one + two;
-                    }
-                })
+                .map(new OneMapper())
+                .reduce(new AddOnesReducer())
                 .first(1);
+    }
+
+	private static final class OneMapper<T extends Tuple> implements MapFunction<T, Integer> {
+            @Override
+            public Integer map(T o) throws Exception {
+                return 1;
+            }
+    }
+    
+    private static final class AddOnesReducer implements ReduceFunction<Integer> {
+            @Override
+            public Integer reduce(Integer one, Integer two) throws Exception {
+                return one + two;
+            }
     }
 
 }
