3a46576184e7cbe84ee906c86da69fad15986993
vasia <vasilikikalavri@gmail.com>
2014-12-09 20:09:21 +0100

    [FLINK-1201] [gelly] fixed type info for the outEdgesFunction

--- flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/OutEdgesFunction.java
+++ flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/OutEdgesFunction.java
@@ -4,10 +4,9 @@ import java.io.Serializable;
 
 import org.apache.flink.api.common.functions.Function;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.tuple.Tuple3;
 
 public interface OutEdgesFunction<K extends Comparable<K> & Serializable, 
 	VV extends Serializable, EV extends Serializable, O> extends Function, Serializable {
 
-	O iterateOutEdges(Tuple2<K, VV> v, Iterable<Tuple3<K, K, EV>> outEdges) throws Exception;
+	Tuple2<K, O> iterateOutEdges(Vertex<K, VV> v, Iterable<Edge<K, EV>> outEdges) throws Exception;
 }
