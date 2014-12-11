a12d87de85f1829473b525440685020467b2f2dd
Chiwan Park <chiwanpark@icloud.com>
2014-12-12 02:49:01 +0900

    [FLINK-1081] Add HDFS file-stream source for streaming

--- flink-addons/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/function/source/FileReadFunction.java
+++ flink-addons/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/function/source/FileReadFunction.java
@@ -17,33 +17,32 @@
 
 package org.apache.flink.streaming.api.function.source;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.core.fs.FSDataInputStream;
+import org.apache.flink.core.fs.FileSystem;
+import org.apache.flink.core.fs.Path;
 import org.apache.flink.util.Collector;
 
-public class FileStreamFunction implements SourceFunction<String> {
-	private static final long serialVersionUID = 1L;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
 
-	private final String path;
-
-	public FileStreamFunction(String path) {
-		this.path = path;
-	}
-
+public class FileReadFunction implements FlatMapFunction<Tuple3<String, Long, Long>, String> {
 	@Override
-	public void invoke(Collector<String> collector) throws IOException {
-		while (true) {
-			BufferedReader br = new BufferedReader(new FileReader(path));
-			String line = br.readLine();
-			while (line != null) {
-				if (!line.equals("")) {
-					collector.collect(line);
-				}
-				line = br.readLine();
+	public void flatMap(Tuple3<String, Long, Long> value, Collector<String> out) throws Exception {
+		FSDataInputStream stream = FileSystem.get(new URI(value.f0)).open(new Path(value.f0));
+		stream.seek(value.f1);
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+		String line;
+
+		try {
+			while ((line = reader.readLine()) != null && (value.f2 == -1L || stream.getPos() <= value.f2)) {
+				out.collect(line);
 			}
-			br.close();
+		} finally {
+			reader.close();
 		}
 	}
 }
