2086132c2ecc6ea864466f040257360b9c76dec8
andralungu <lungu.andra@gmail.com>
2014-12-18 20:27:11 +0100

    [FLINK-1201] [gelly] SSSP with runVertexCentricIteration

--- flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/library/SingleSourceShortestPaths.java
+++ flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/library/SingleSourceShortestPaths.java
@@ -2,103 +2,117 @@ package flink.graphs.library;
 
 import flink.graphs.*;
 import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.aggregation.Aggregations;
-import org.apache.flink.api.java.operators.DeltaIteration;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.util.Collector;
+import org.apache.flink.spargel.java.MessageIterator;
+import org.apache.flink.spargel.java.MessagingFunction;
+import org.apache.flink.spargel.java.OutgoingEdge;
+import org.apache.flink.spargel.java.VertexUpdateFunction;
 
-public class SingleSourceShortestPaths implements GraphAlgorithm<Long, Double, Double> {
+import java.io.Serializable;
 
-    private final Long srcVertexId;
+public class SingleSourceShortestPaths<K extends Comparable<K> & Serializable> implements GraphAlgorithm<K, Double, Double> {
+
+    private final K srcVertexId;
     private final Integer maxIterations;
 
-    public SingleSourceShortestPaths(Long srcVertexId, Integer maxIterations) {
+    public SingleSourceShortestPaths(K srcVertexId, Integer maxIterations) {
         this.srcVertexId = srcVertexId;
         this.maxIterations = maxIterations;
     }
 
-    private Graph<Long, Double, Double> computeDeltaShortestPath(Graph<Long, Double, Double> graph) {
-        DataSet<Vertex<Long, Double>> sourceVertex = graph.getVertices().filter(
-                new SelectVertex(srcVertexId));
-        //assign the initial distance as a value for each vertex
-        DataSet<Vertex<Long, Double>> verticesWithInitialDistance = sourceVertex.cross(graph.getVertices())
-                .map(new InitSrcVertex());
+    @Override
+    public Graph<K, Double, Double> run(Graph<K, Double, Double> input) {
+        DataSet<Vertex<K, Double>> sourceVertex = input.getVertices().filter(
+                new SelectVertex<K>(srcVertexId));
 
-        //open a delta iteration
-        DeltaIteration<Vertex<Long, Double>, Vertex<Long, Double>> iteration =
-                verticesWithInitialDistance.iterateDelta(verticesWithInitialDistance, maxIterations, 0);
+        DataSet<Vertex<K, Double>> verticesWithInitialDistance = sourceVertex.cross(input.getVertices())
+                .map(new InitSrcVertex<K>());
 
-        //apply the step logic: join with the edges, choose the minimum distance and update if
-        //the new distance proposed is smaller
-        DataSet<Vertex<Long, Double>> changes = iteration.getWorkset()
-                .join(graph.getEdges()).where(0).equalTo(0).with(new NeighborWithDistanceJoin())
-                .groupBy(0).aggregate(Aggregations.MIN, 1)
-                .join(iteration.getSolutionSet()).where(0).equalTo(0)
-                .flatMap(new DistanceFilter());
+        Graph<K, Double, Double> graph = Graph.create(verticesWithInitialDistance, input.getEdges(),
+                ExecutionEnvironment.getExecutionEnvironment());
 
-        // close the delta iteration (delta and new workset are identical)
-        DataSet<Vertex<Long, Double>> result = iteration.closeWith(changes, changes);
-
-        return Graph.create(result, graph.getEdges(), ExecutionEnvironment.getExecutionEnvironment());
+        return graph.runVertexCentricIteration(
+                new VertexDistanceUpdater<K>(),
+                new MinDistanceMessenger<K>(),
+                maxIterations
+        );
     }
 
-    private static final class SelectVertex implements FilterFunction<Vertex<Long, Double>> {
 
-        private Long id;
+    /**
+     * Function that updates the value of a vertex by picking the minimum distance from all incoming messages.
+     *
+     * @param <K>
+     */
+    @SuppressWarnings("serial")
+    public static final class VertexDistanceUpdater<K extends Comparable<K> & Serializable>
+            extends VertexUpdateFunction<K, Double, Double> {
 
-        public SelectVertex(Long id) {
+        @Override
+        public void updateVertex(K vertexKey, Double vertexValue, MessageIterator<Double> inMessages) {
+
+            Double minDistance = Double.MAX_VALUE;
+
+            for (double msg : inMessages) {
+                if (msg < minDistance) {
+                    minDistance = msg;
+                }
+            }
+
+            if (vertexValue > minDistance) {
+                vertexValue = minDistance;
+            }
+
+            setNewVertexValue(vertexValue);
+        }
+    }
+
+    /**
+     * Distributes the minimum distance associated with a given vertex among all the target vertices
+     * summed up with the edge's value.
+     *
+     * @param <K>
+     */
+    @SuppressWarnings("serial")
+    public static final class MinDistanceMessenger<K extends Comparable<K> & Serializable>
+            extends MessagingFunction<K, Double, Double, Double> {
+
+        @Override
+        public void sendMessages(K vertexKey, Double newDistance) throws Exception {
+            for (OutgoingEdge<K, Double> edge : getOutgoingEdges()) {
+                sendMessageTo(edge.target(), newDistance + edge.edgeValue());
+            }
+        }
+    }
+
+    private static final class SelectVertex<K extends Comparable<K> & Serializable>
+            implements FilterFunction<Vertex<K, Double>> {
+        private K id;
+
+        public SelectVertex(K id) {
             this.id = id;
         }
 
         @Override
-        public boolean filter(Vertex<Long, Double> kvvVertex) throws Exception {
-            return kvvVertex.getId().equals(id);
+        public boolean filter(Vertex<K, Double> vertex) throws Exception {
+            return vertex.getId().equals(id);
         }
     }
 
-    private static final class InitSrcVertex
-            implements MapFunction<Tuple2<Vertex<Long, Double>, Vertex<Long,Double>>, Vertex<Long,Double>> {
+    private static final class InitSrcVertex<K extends Comparable<K> & Serializable>
+            implements MapFunction<Tuple2<Vertex<K, Double>, Vertex<K,Double>>, Vertex<K,Double>> {
 
         @Override
-        public Vertex<Long, Double> map(Tuple2<Vertex<Long, Double>, Vertex<Long, Double>> vertexVertexTuple2) throws Exception {
+        public Vertex<K, Double> map(Tuple2<Vertex<K, Double>, Vertex<K, Double>> vertexVertexTuple2) throws Exception {
             if(vertexVertexTuple2.f0.f0.equals(vertexVertexTuple2.f1.f0)) {
                 return new Vertex<>(vertexVertexTuple2.f0.f0, 0.0);
             } else {
                 return new Vertex<>(vertexVertexTuple2.f1.f0, Double.MAX_VALUE);
             }
         }
-    }
-
-    public static final class NeighborWithDistanceJoin
-            implements JoinFunction<Vertex<Long, Double>, Edge<Long, Double>, Vertex<Long, Double>> {
-
-        @Override
-        public Vertex<Long, Double> join(Vertex<Long, Double> kvvVertex, Edge<Long, Double> kevEdge) throws Exception {
-            return new Vertex<>(kevEdge.f1, kvvVertex.f1 + kevEdge.f2);
-        }
-    }
-
-    public static final class DistanceFilter
-            implements FlatMapFunction<Tuple2<Vertex<Long, Double>, Vertex<Long, Double>>, Vertex<Long, Double>> {
-
-        @Override
-        public void flatMap(Tuple2<Vertex<Long, Double>, Vertex<Long, Double>> value,
-                            Collector<Vertex<Long, Double>> collector) throws Exception {
-            if (value.f0.f1 < value.f1.f1) {
-                collector.collect(value.f0);
-            }
-        }
-    }
-
-    @Override
-    public Graph<Long, Double, Double> run(Graph<Long, Double, Double> input) {
-
-        return computeDeltaShortestPath(input);
     }
 
 }
