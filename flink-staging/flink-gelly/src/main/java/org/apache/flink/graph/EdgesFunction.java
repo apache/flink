4f53ff1c32a2c0d4130cb839734efc676941be30
vasia <vasilikikalavri@gmail.com>
2014-12-21 16:12:09 +0100

    [FLINK-1201] [gelly] return arbitrary type instead of Tuple2 for the neighbor methods

--- flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/EdgesFunction.java
+++ flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/EdgesFunction.java
@@ -8,5 +8,5 @@ import org.apache.flink.api.java.tuple.Tuple2;
 public interface EdgesFunction<K extends Comparable<K> & Serializable, 
 	EV extends Serializable, O> extends Function, Serializable {
 
-	Tuple2<K, O> iterateEdges(Iterable<Tuple2<K, Edge<K, EV>>> edges) throws Exception;
+	O iterateEdges(Iterable<Tuple2<K, Edge<K, EV>>> edges) throws Exception;
 }
--- flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/NeighborsFunctionWithVertexValue.java
+++ flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/NeighborsFunctionWithVertexValue.java
@@ -8,5 +8,5 @@ import org.apache.flink.api.java.tuple.Tuple2;
 public interface NeighborsFunctionWithVertexValue<K extends Comparable<K> & Serializable, VV extends Serializable, 
 	EV extends Serializable, O> extends Function, Serializable {
 
-	Tuple2<K, O> iterateNeighbors(Vertex<K, VV> vertex, Iterable<Tuple2<Edge<K, EV>, Vertex<K, VV>>> neighbors) throws Exception;
+	O iterateNeighbors(Vertex<K, VV> vertex, Iterable<Tuple2<Edge<K, EV>, Vertex<K, VV>>> neighbors) throws Exception;
 }
