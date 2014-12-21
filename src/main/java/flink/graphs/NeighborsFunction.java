c65fee55f230c56213c3ccd732d3b42cd9c305ef
vasia <vasilikikalavri@gmail.com>
2014-12-21 16:50:52 +0100

    reduceOnNeighbors without vertex value

--- src/main/java/flink/graphs/NeighborsFunction.java
+++ src/main/java/flink/graphs/NeighborsFunction.java
@@ -8,5 +8,5 @@ import org.apache.flink.api.java.tuple.Tuple3;
 public interface NeighborsFunction<K extends Comparable<K> & Serializable, VV extends Serializable, 
 	EV extends Serializable, O> extends Function, Serializable {
 
-	O iterateEdges(Iterable<Tuple3<K, Edge<K, EV>, Vertex<K, VV>>> neighbors) throws Exception;
+	O iterateNeighbors(Iterable<Tuple3<K, Edge<K, EV>, Vertex<K, VV>>> neighbors) throws Exception;
 }
