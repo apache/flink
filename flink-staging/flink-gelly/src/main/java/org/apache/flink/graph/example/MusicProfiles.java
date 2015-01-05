f9f3aaaf286e88e9c6fd301a08087145e7c360ba
vasia <vasilikikalavri@gmail.com>
2015-01-05 20:58:14 +0100

    [FLINK-1201] [gelly] added label propagation step in MusicProfiles

--- flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/example/MusicProfiles.java
+++ flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/example/MusicProfiles.java
@@ -2,9 +2,11 @@ package flink.graphs.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.apache.flink.api.common.ProgramDescription;
 import org.apache.flink.api.common.functions.CoGroupFunction;
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.GroupReduceFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
@@ -20,26 +22,41 @@ import flink.graphs.EdgeDirection;
 import flink.graphs.EdgesFunctionWithVertexValue;
 import flink.graphs.Graph;
 import flink.graphs.Vertex;
+import flink.graphs.example.utils.MusicProfilesData;
+import flink.graphs.library.LabelPropagation;
 import flink.graphs.utils.Tuple3ToEdgeMap;
 
 public class MusicProfiles implements ProgramDescription {
 
+	/**
+	 * This example demonstrates how to mix the "record" Flink API with the graph API.
+	 * The input is a set <userId - songId - playCount> triplets and a set of
+	 * bad records,i.e. song ids that should not be trusted.
+	 * Initially, we use the record API to filter out the bad records.
+	 * Then, we use the graph API to create a user -> song weighted bipartite graph
+	 * and compute the top song (most listened) per user.
+	 * Then, we use the record API again, to create a user-user similarity graph, 
+	 * based on common songs, where two users that listen to the same song are connected.
+	 * Finally, we use the graph API to run the label propagation community detection algorithm
+	 * on the similarity graph.
+	 */
 	public static void main (String [] args) throws Exception {
     	
     	ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+    	final long numberOfLabels = 3;
+    	final int numIterations = 10;
 
     	/** 
     	 *  Read the user-song-play triplets
     	 *  The format is <userID>\t<songID>\t<playcount>
     	 */
-    	DataSet<Tuple3<String, String, Integer>> triplets = env.readCsvFile(args[0])
-    			.lineDelimiter("\n").fieldDelimiter('\t').types(String.class, String.class, Integer.class);
-    	
+    	DataSet<Tuple3<String, String, Integer>> triplets = MusicProfilesData.getUserSongTriplets(env);
+
     	/**
     	 *  Read the mismatches dataset and extract the songIDs
     	 *  The format is "ERROR: <songID trackID> song_title"
     	 */
-    	DataSet<Tuple1<String>> mismatches = env.readTextFile(args[1]).map(new ExtractMismatchSongIds());
+    	DataSet<Tuple1<String>> mismatches = MusicProfilesData.getMismatches(env).map(new ExtractMismatchSongIds());
 
     	/**
     	 *  Filter out the mismatches from the triplets dataset
@@ -58,7 +75,8 @@ public class MusicProfiles implements ProgramDescription {
     	 *  Get the top track (most listened) for each user
     	 */
     	DataSet<Tuple2<String, String>> usersWithTopTrack = userSongGraph.reduceOnEdges(new GetTopSongPerUser(), 
-    			EdgeDirection.OUT);
+    			EdgeDirection.OUT).filter(new FilterSongNodes());
+    	usersWithTopTrack.print();
 
     	/**
     	 * Create a user-user similarity graph, based on common songs, 
@@ -69,6 +87,15 @@ public class MusicProfiles implements ProgramDescription {
     			.reduceGroup(new CreateSimilarUserEdges()).distinct();
     	Graph<String, NullValue, NullValue> similarUsersGraph = Graph.create(similarUsers, env).getUndirected();
 
+    	/**
+    	 * Detect user communities using the label propagation library method
+    	 */
+    	DataSet<Vertex<String, Long>> verticesWithCommunity = similarUsersGraph.mapVertices(
+    			new InitVertexLabels(numberOfLabels))
+    			.run(new LabelPropagation<String>(numIterations)).getVertices();
+    	verticesWithCommunity.print();
+
+    	env.execute();
     }
 
     @SuppressWarnings("serial")
@@ -89,8 +116,17 @@ public class MusicProfiles implements ProgramDescription {
 				Collector<Tuple3<String, String, Integer>> out) {
 			if (!invalidSongs.iterator().hasNext()) {
 				// this is a valid triplet
-				out.collect(triplets.iterator().next());
+				for (Tuple3<String, String, Integer> triplet : triplets) {
+					out.collect(triplet);					
+				}
 			}
+		}
+    }
+
+    @SuppressWarnings("serial")
+ 	public static final class FilterSongNodes implements FilterFunction<Tuple2<String, String>> {
+		public boolean filter(Tuple2<String, String> value) throws Exception {
+			return !value.f1.equals("");
 		}
     }
 
@@ -120,14 +156,26 @@ public class MusicProfiles implements ProgramDescription {
 				listeners.add(edge.getSource());
 			}
 			for (int i=0; i < listeners.size()-1; i++) {
-				out.collect(new Edge<String, NullValue>(listeners.get(i), listeners.get(i+1)));
+				out.collect(new Edge<String, NullValue>(listeners.get(i), listeners.get(i+1),
+						NullValue.getInstance()));
 			}
+		}
+    }
+
+    @SuppressWarnings("serial")
+ 	public static final class InitVertexLabels implements MapFunction<Vertex<String, NullValue>, Long> {
+    	private long numberOfLabels;
+    	public InitVertexLabels(long labels) {
+    		this.numberOfLabels = labels;
+    	}
+		public Long map(Vertex<String, NullValue> value) {
+			Random randomGenerator = new Random();
+			return (long) randomGenerator.nextInt((int) numberOfLabels);
 		}
     }
 	
 	@Override
 	public String getDescription() {
-		return null;
+		return "Music Profiles Example";
 	}
-
 }
