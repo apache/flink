68f8f197e91628e8ffa983fae80a857dbb3d04a0
vasia <vasilikikalavri@gmail.com>
2015-01-05 17:50:29 +0100

    [FLINK-1201] [gelly] added label propagation in library

--- flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/GraphAlgorithm.java
+++ flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/GraphAlgorithm.java
@@ -3,6 +3,11 @@ package flink.graphs;
 
 import java.io.Serializable;
 
+/**
+ * @param <K> key type
+ * @param <VV> vertex value type
+ * @param <EV> edge value type
+ */
 public interface GraphAlgorithm<K extends Comparable<K> & Serializable, VV extends Serializable,
         EV extends Serializable> {
 
