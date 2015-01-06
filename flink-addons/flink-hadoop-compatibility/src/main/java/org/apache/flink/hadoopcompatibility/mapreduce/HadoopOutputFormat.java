b5f399d933f0d0697c7b17752277ad4f751eb2c2
FelixNeutatz <neutatz@googlemail.com>
2015-01-06 20:47:00 +0100

    [FLINK-1271] Remove Writable limitation from Hadoop Format

--- flink-addons/flink-hadoop-compatibility/src/main/java/org/apache/flink/hadoopcompatibility/mapreduce/HadoopOutputFormat.java
+++ flink-addons/flink-hadoop-compatibility/src/main/java/org/apache/flink/hadoopcompatibility/mapreduce/HadoopOutputFormat.java
@@ -40,49 +40,49 @@ import org.apache.hadoop.mapreduce.TaskAttemptID;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputCommitter;
 
 
-public class HadoopOutputFormat<K extends Writable,V extends Writable> implements OutputFormat<Tuple2<K, V>>, FinalizeOnMaster {
-	
+public class HadoopOutputFormat<K,V> implements OutputFormat<Tuple2<K, V>>, FinalizeOnMaster {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private org.apache.hadoop.conf.Configuration configuration;
 	private org.apache.hadoop.mapreduce.OutputFormat<K,V> mapreduceOutputFormat;
 	private transient RecordWriter<K,V> recordWriter;
 	private transient FileOutputCommitter fileOutputCommitter;
 	private transient TaskAttemptContext context;
 	private transient int taskNumber;
-	
+
 	public HadoopOutputFormat(org.apache.hadoop.mapreduce.OutputFormat<K,V> mapreduceOutputFormat, Job job) {
 		super();
 		this.mapreduceOutputFormat = mapreduceOutputFormat;
 		this.configuration = job.getConfiguration();
 		HadoopUtils.mergeHadoopConf(configuration);
 	}
-	
+
 	public void setConfiguration(org.apache.hadoop.conf.Configuration configuration) {
 		this.configuration = configuration;
 	}
-	
+
 	public org.apache.hadoop.conf.Configuration getConfiguration() {
 		return this.configuration;
 	}
-	
+
 	public org.apache.hadoop.mapreduce.OutputFormat<K,V> getHadoopOutputFormat() {
 		return this.mapreduceOutputFormat;
 	}
-	
+
 	public void setHadoopOutputFormat(org.apache.hadoop.mapreduce.OutputFormat<K,V> mapreduceOutputFormat) {
 		this.mapreduceOutputFormat = mapreduceOutputFormat;
 	}
-	
+
 	// --------------------------------------------------------------------------------------------
 	//  OutputFormat
 	// --------------------------------------------------------------------------------------------
-	
+
 	@Override
 	public void configure(Configuration parameters) {
 		// nothing to do
 	}
-	
+
 	/**
 	 * create the temporary output file for hadoop RecordWriter.
 	 * @param taskNumber The number of the parallel instance.
@@ -94,48 +94,48 @@ public class HadoopOutputFormat<K extends Writable,V extends Writable> implement
 		if (Integer.toString(taskNumber + 1).length() > 6) {
 			throw new IOException("Task id too large.");
 		}
-		
+
 		this.taskNumber = taskNumber+1;
-		
+
 		// for hadoop 2.2
 		this.configuration.set("mapreduce.output.basename", "tmp");
-		
-		TaskAttemptID taskAttemptID = TaskAttemptID.forName("attempt__0000_r_" 
-				+ String.format("%" + (6 - Integer.toString(taskNumber + 1).length()) + "s"," ").replace(" ", "0") 
-				+ Integer.toString(taskNumber + 1) 
+
+		TaskAttemptID taskAttemptID = TaskAttemptID.forName("attempt__0000_r_"
+				+ String.format("%" + (6 - Integer.toString(taskNumber + 1).length()) + "s"," ").replace(" ", "0")
+				+ Integer.toString(taskNumber + 1)
 				+ "_0");
-		
+
 		this.configuration.set("mapred.task.id", taskAttemptID.toString());
 		this.configuration.setInt("mapred.task.partition", taskNumber + 1);
 		// for hadoop 2.2
 		this.configuration.set("mapreduce.task.attempt.id", taskAttemptID.toString());
 		this.configuration.setInt("mapreduce.task.partition", taskNumber + 1);
-		
+
 		try {
 			this.context = HadoopUtils.instantiateTaskAttemptContext(this.configuration, taskAttemptID);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		this.fileOutputCommitter = new FileOutputCommitter(new Path(this.configuration.get("mapred.output.dir")), context);
-		
+
 		try {
 			this.fileOutputCommitter.setupJob(HadoopUtils.instantiateJobContext(this.configuration, new JobID()));
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		// compatible for hadoop 2.2.0, the temporary output directory is different from hadoop 1.2.1
 		this.configuration.set("mapreduce.task.output.dir", this.fileOutputCommitter.getWorkPath().toString());
-		
+
 		try {
 			this.recordWriter = this.mapreduceOutputFormat.getRecordWriter(this.context);
 		} catch (InterruptedException e) {
 			throw new IOException("Could not create RecordWriter.", e);
 		}
 	}
-	
-	
+
+
 	@Override
 	public void writeRecord(Tuple2<K, V> record) throws IOException {
 		try {
@@ -144,7 +144,7 @@ public class HadoopOutputFormat<K extends Writable,V extends Writable> implement
 			throw new IOException("Could not write Record.", e);
 		}
 	}
-	
+
 	/**
 	 * commit the task by moving the output file out from the temporary directory.
 	 * @throws IOException
@@ -156,68 +156,68 @@ public class HadoopOutputFormat<K extends Writable,V extends Writable> implement
 		} catch (InterruptedException e) {
 			throw new IOException("Could not close RecordReader.", e);
 		}
-		
+
 		if (this.fileOutputCommitter.needsTaskCommit(this.context)) {
 			this.fileOutputCommitter.commitTask(this.context);
 		}
-		
+
 		Path outputPath = new Path(this.configuration.get("mapred.output.dir"));
-		
+
 		// rename tmp-file to final name
 		FileSystem fs = FileSystem.get(outputPath.toUri(), this.configuration);
-		
+
 		String taskNumberStr = Integer.toString(this.taskNumber);
 		String tmpFileTemplate = "tmp-r-00000";
 		String tmpFile = tmpFileTemplate.substring(0,11-taskNumberStr.length())+taskNumberStr;
-		
+
 		if(fs.exists(new Path(outputPath.toString()+"/"+tmpFile))) {
 			fs.rename(new Path(outputPath.toString()+"/"+tmpFile), new Path(outputPath.toString()+"/"+taskNumberStr));
 		}
 	}
-	
+
 	@Override
 	public void finalizeGlobal(int parallelism) throws IOException {
 
 		JobContext jobContext;
 		TaskAttemptContext taskContext;
 		try {
-			
-			TaskAttemptID taskAttemptID = TaskAttemptID.forName("attempt__0000_r_" 
-					+ String.format("%" + (6 - Integer.toString(1).length()) + "s"," ").replace(" ", "0") 
-					+ Integer.toString(1) 
+
+			TaskAttemptID taskAttemptID = TaskAttemptID.forName("attempt__0000_r_"
+					+ String.format("%" + (6 - Integer.toString(1).length()) + "s"," ").replace(" ", "0")
+					+ Integer.toString(1)
 					+ "_0");
-			
+
 			jobContext = HadoopUtils.instantiateJobContext(this.configuration, new JobID());
 			taskContext = HadoopUtils.instantiateTaskAttemptContext(this.configuration, taskAttemptID);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 		this.fileOutputCommitter = new FileOutputCommitter(new Path(this.configuration.get("mapred.output.dir")), taskContext);
-		
+
 		// finalize HDFS output format
 		this.fileOutputCommitter.commitJob(jobContext);
 	}
-	
+
 	// --------------------------------------------------------------------------------------------
 	//  Custom serialization methods
 	// --------------------------------------------------------------------------------------------
-	
+
 	private void writeObject(ObjectOutputStream out) throws IOException {
 		out.writeUTF(this.mapreduceOutputFormat.getClass().getName());
 		this.configuration.write(out);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
 		String hadoopOutputFormatClassName = in.readUTF();
-		
+
 		org.apache.hadoop.conf.Configuration configuration = new org.apache.hadoop.conf.Configuration();
 		configuration.readFields(in);
-		
+
 		if(this.configuration == null) {
 			this.configuration = configuration;
 		}
-		
+
 		try {
 			this.mapreduceOutputFormat = (org.apache.hadoop.mapreduce.OutputFormat<K,V>) Class.forName(hadoopOutputFormatClassName, true, Thread.currentThread().getContextClassLoader()).newInstance();
 		} catch (Exception e) {
