ffe11034181033aee736d5658bd4471e1f9f26b6
Max <max@posteo.de>
2015-01-06 15:55:39 +0100

    [FLINK-1361] change jobManager log file name
    regression of b313eeb76cb5a9ec60923f3c4ab5b47569bc21e6

--- flink-dist/src/main/flink-bin/bin/jobmanager.sh
+++ flink-dist/src/main/flink-bin/bin/jobmanager.sh
@@ -59,9 +59,9 @@ constructJobManagerClassPath() {
 
 FLINK_JM_CLASSPATH=`manglePathList "$(constructJobManagerClassPath)"`
 
-log=$FLINK_LOG_DIR/flink-$FLINK_IDENT_STRING-jobManager-$HOSTNAME.log
-out=$FLINK_LOG_DIR/flink-$FLINK_IDENT_STRING-jobManager-$HOSTNAME.out
-pid=$FLINK_PID_DIR/flink-$FLINK_IDENT_STRING-jobManager.pid
+log=$FLINK_LOG_DIR/flink-$FLINK_IDENT_STRING-jobmanager-$HOSTNAME.log
+out=$FLINK_LOG_DIR/flink-$FLINK_IDENT_STRING-jobmanager-$HOSTNAME.out
+pid=$FLINK_PID_DIR/flink-$FLINK_IDENT_STRING-jobmanager.pid
 log_setting=(-Dlog.file="$log" -Dlog4j.configuration=file:"$FLINK_CONF_DIR"/log4j.properties -Dlogback.configurationFile=file:"$FLINK_CONF_DIR"/logback.xml)
 
 case $STARTSTOP in
