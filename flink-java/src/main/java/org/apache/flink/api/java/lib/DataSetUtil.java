384816fc778be76a9f47556fca7089c3060f3452
FelixNeutatz <neutatz@googlemail.com>
2015-01-18 23:21:35 +0100

    [FLINK-1398] change naming

--- flink-java/src/main/java/org/apache/flink/api/java/lib/DataSetUtil.java
+++ flink-java/src/main/java/org/apache/flink/api/java/lib/DataSetUtil.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.apache.flink.api.java.utilities;
+package org.apache.flink.api.java.lib;
 
 
 import org.apache.flink.api.common.functions.MapFunction;
--- flink-java/src/test/java/org/apache/flink/api/java/lib/DataSetUtilTest.java
+++ flink-java/src/test/java/org/apache/flink/api/java/lib/DataSetUtilTest.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.apache.flink.api.java.utilities;
+package org.apache.flink.api.java.lib;
 
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.api.java.DataSet;
