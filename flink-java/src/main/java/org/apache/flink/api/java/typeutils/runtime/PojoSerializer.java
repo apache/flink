5a5ec524a6a124426b3ad36589ea6c1d28126e26
twalthr <info@twalthr.com>
2015-01-26 16:09:24 +0100

    [FLINK-1437][Java API] Fixes copy() methods in PojoSerializer for null values
    This closes #342

--- flink-java/src/main/java/org/apache/flink/api/java/typeutils/runtime/PojoSerializer.java
+++ flink-java/src/main/java/org/apache/flink/api/java/typeutils/runtime/PojoSerializer.java
@@ -145,8 +145,14 @@ public final class PojoSerializer<T> extends TypeSerializer<T> {
 		
 		try {
 			for (int i = 0; i < numFields; i++) {
-				Object copy = fieldSerializers[i].copy(fields[i].get(from));
-				fields[i].set(target, copy);
+				Object value = fields[i].get(from);
+				if (value != null) {
+					Object copy = fieldSerializers[i].copy(value);
+					fields[i].set(target, copy);
+				}
+				else {
+					fields[i].set(target, null);
+				}
 			}
 		}
 		catch (IllegalAccessException e) {
@@ -159,8 +165,14 @@ public final class PojoSerializer<T> extends TypeSerializer<T> {
 	public T copy(T from, T reuse) {
 		try {
 			for (int i = 0; i < numFields; i++) {
-				Object copy = fieldSerializers[i].copy(fields[i].get(from), fields[i].get(reuse));
-				fields[i].set(reuse, copy);
+				Object value = fields[i].get(from);
+				if (value != null) {
+					Object copy = fieldSerializers[i].copy(fields[i].get(from), fields[i].get(reuse));
+					fields[i].set(reuse, copy);
+				}
+				else {
+					fields[i].set(reuse, null);
+				}
 			}
 		} catch (IllegalAccessException e) {
 			throw new RuntimeException("Error during POJO copy, this should not happen since we check the fields" +
@@ -260,8 +272,11 @@ public final class PojoSerializer<T> extends TypeSerializer<T> {
 		// copy the Non-Null/Null tag
 		target.writeBoolean(source.readBoolean());
 		for (int i = 0; i < numFields; i++) {
-			target.writeBoolean(source.readBoolean());
-			fieldSerializers[i].copy(source, target);
+			boolean isNull = source.readBoolean();
+			target.writeBoolean(isNull);
+			if (!isNull) {
+				fieldSerializers[i].copy(source, target);
+			}
 		}
 	}
 	
