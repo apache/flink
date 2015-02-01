cf95e6b2d195709672627fe67618aced15eff101
Robert Metzger <rmetzger@apache.org>
2015-02-01 20:03:24 +0100

    further improvements on the QA checks

--- tools/qa-check.sh
+++ tools/qa-check.sh
@@ -28,7 +28,7 @@
 #
 
 
-BRANCH=${BRANCH:-master}
+BRANCH=${BRANCH:-origin/master}
 
 
 
@@ -55,10 +55,13 @@ cd _qa_workdir
 
 if [ ! -d  "flink" ] ; then
 	echo "There is no flink copy in the workdir. Cloning flink"
-	git clone git@github.com:rmetzger/flink.git flink
+	git clone https://github.com/apache/flink.git flink
 	cd flink
-	git checkout $BRANCH
+	
 fi
+cd flink
+git fetch origin
+git checkout $BRANCH
 cd $here
 # go to refrence flink directory
 
@@ -88,10 +91,10 @@ checkJavadocsErrors() {
 	OLD_JAVADOC_ERR_CNT=`cat $VAR_DIR/_JAVADOCS_NUM_WARNINGS` 
 	NEW_JAVADOC_ERR_CNT=`eval $JAVADOC_MVN_COMMAND`
 	if [ "$NEW_JAVADOC_ERR_CNT" -gt "$OLD_JAVADOC_ERR_CNT" ]; then
-		MESSAGES+=":-1: The change increases the number of javadoc errors from $OLD_JAVADOC_ERR_CNT to $NEW_JAVADOC_ERR_CNT"
+		MESSAGES+=":-1: The change increases the number of javadoc errors from $OLD_JAVADOC_ERR_CNT to $NEW_JAVADOC_ERR_CNT"$'\n'
 		TESTS_PASSED=false
 	else
-		MESSAGES+=":+1: The number of javadoc errors was $OLD_JAVADOC_ERR_CNT and is now $NEW_JAVADOC_ERR_CNT"
+		MESSAGES+=":+1: The number of javadoc errors was $OLD_JAVADOC_ERR_CNT and is now $NEW_JAVADOC_ERR_CNT"$'\n'
 	fi
 }
 
@@ -106,30 +109,39 @@ checkCompilerWarnings() {
 	OLD_COMPILER_ERR_CNT=`cat $VAR_DIR/_COMPILER_NUM_WARNINGS` 
 	NEW_COMPILER_ERR_CNT=`eval $COMPILER_WARN_MVN_COMMAND`
 	if [ "$NEW_COMPILER_ERR_CNT" -gt "$OLD_COMPILER_ERR_CNT" ]; then
-		MESSAGES+=":-1: The change increases the number of compiler warnings from $OLD_COMPILER_ERR_CNT to $NEW_COMPILER_ERR_CNT"
+		MESSAGES+=":-1: The change increases the number of compiler warnings from $OLD_COMPILER_ERR_CNT to $NEW_COMPILER_ERR_CNT"$'\n'
 		TESTS_PASSED=false
 	else
-		MESSAGES+=":+1: The number of compiler warnings was $OLD_COMPILER_ERR_CNT and is now $NEW_COMPILER_ERR_CNT"
+		MESSAGES+=":+1: The number of compiler warnings was $OLD_COMPILER_ERR_CNT and is now $NEW_COMPILER_ERR_CNT"$'\n'
 	fi
 }
 
 ############ Files in lib ############
 BUILD_MVN_COMMAND="mvn clean package -DskipTests"
-COUNT_LIB_FILES="find . | grep \"\/lib\/\" | wc -l"
+COUNT_LIB_FILES="find . | grep \"\/lib\/\" | grep -v \"_qa_workdir\" | wc -l"
 referenceLibFiles() {
-	eval $BUILD_MVN_COMMAND
+	eval $BUILD_MVN_COMMAND > /dev/null
 	eval $COUNT_LIB_FILES > "$VAR_DIR/_NUM_LIB_FILES"
 }
 
 checkLibFiles() {
 	OLD_LIB_FILES_CNT=`cat $VAR_DIR/_NUM_LIB_FILES` 
-	eval $BUILD_MVN_COMMAND
+	eval $BUILD_MVN_COMMAND > /dev/null
 	NEW_LIB_FILES_CNT=`eval $COUNT_LIB_FILES`
 	if [ "$NEW_LIB_FILES_CNT" -gt "$OLD_LIB_FILES_CNT" ]; then
-		MESSAGES+=":-1: The change increases the number of dependencies in the lib/ folder from $OLD_LIB_FILES_CNT to $NEW_LIB_FILES_CNT"
+		MESSAGES+=":-1: The change increases the number of dependencies in the lib/ folder from $OLD_LIB_FILES_CNT to $NEW_LIB_FILES_CNT"$'\n'
 		TESTS_PASSED=false
 	else
-		MESSAGES+=":+1: The number of files in the lib/ folder was $OLD_LIB_FILES_CNT before the change and is now $NEW_LIB_FILES_CNT"
+		MESSAGES+=":+1: The number of files in the lib/ folder was $OLD_LIB_FILES_CNT before the change and is now $NEW_LIB_FILES_CNT"$'\n'
+	fi
+}
+
+############ @author tag ############
+
+checkAuthorTag() {
+	if [ `grep -r "@author" . | grep java | wc -l` -gt "0" ]; then
+		MESSAGES+=":-1: The change contains @author tags"$'\n'
+		TESTS_PASSED=false
 	fi
 }
 
@@ -143,17 +155,18 @@ referenceLibFiles
 
 
 goToTestDirectory
-##### Methods to be executed on the changes (in home dir)
+## Methods to be executed on the changes (flink root dir)
 checkJavadocsErrors
 checkCompilerWarnings
 checkLibFiles
+checkAuthorTag
 
 
 MESSAGES+=$'\n'"Test finished."$'\n'
 if [ "$TESTS_PASSED" == "true" ]; then
-	MESSAGES+="Overall result: :+1:. All tests passed"
+	MESSAGES+="Overall result: :+1:. All tests passed"$'\n'
 else 
-	MESSAGES+="Overall result: :-1:. Some tests failed. Please check messages above"
+	MESSAGES+="Overall result: :-1:. Some tests failed. Please check messages above"$'\n'
 fi
 
 echo "$MESSAGES"
