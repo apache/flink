5e3ea996bcf0506a4aef5e9bfc61d0dc86d9f471
Emmanuel <emmanuel@mediumone.com>
2015-03-16 21:02:48 -0700

    Initial commit
    clean up

---
+++ build.sh
@@ -0,0 +1,10 @@
+#Kill cluster and remove all containers
+docker-compose kill
+docker rm $(docker ps -a -q)
+
+#make sure the config file script is executable
+chmod +x /flink/config-flink.sh
+
+#rebuild images
+docker build -t="base" base
+docker build -t="flink" flink
