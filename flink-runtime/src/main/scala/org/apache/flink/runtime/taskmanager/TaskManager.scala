0a3ef15bd584ae3706f1f1bbf3ae52530d34e92d
hongsibao <hongsibao@huawei.com>
2015-03-19 01:06:06 +0800

    [FLINK-1739] [runtime] Fix the bug of the jobManager and TaskManager IPC Port Check.
    This closes #499

--- flink-runtime/src/main/scala/org/apache/flink/runtime/taskmanager/TaskManager.scala
+++ flink-runtime/src/main/scala/org/apache/flink/runtime/taskmanager/TaskManager.scala
@@ -1295,9 +1295,10 @@ object TaskManager {
         "' is missing (hostname/address of JobManager to connect to).")
     }
 
-    if (port <= 0) {
+    if (port <= 0 || port >= 65536) {
       throw new Exception("Invalid value for '" + ConfigConstants.JOB_MANAGER_IPC_PORT_KEY +
-        "' (port of the JobManager actor system) : " + port)
+        "' (port of the JobManager actor system) : " + port +
+        ".  it must be great than 0 and less than 65536.")
     }
 
     (hostname, port)
