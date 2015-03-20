ba8f7b3aa40bf6cd119087902c29713e577cc9a7
Emmanuel <emmanuel@mediumone.com>
2015-03-20 08:05:23 -0700

    Removed potentially harmful `docker rm ` clean up command (it removes all stopped containers)

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 #Kill cluster and remove all containers
 docker-compose kill
-docker rm $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
 
 #make sure the config file script is executable
 chmod +x /flink/config-flink.sh
