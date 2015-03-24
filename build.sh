f46af8c2d952b566a480c4388a410f177993fd68
Robert Metzger <rmetzger@apache.org>
2015-03-24 08:52:35 +0100

    fix path in build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ docker-compose kill
 #docker rm $(docker ps -a -q)
 
 #make sure the config file script is executable
-chmod +x /flink/config-flink.sh
+chmod +x flink/config-flink.sh
 
 #rebuild images
 docker build -t="base" base
