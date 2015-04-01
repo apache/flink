20e65a415eeed6508f01bdf58787ffae7c964ff1
Gyula Fora <gyfora@apache.org>
2015-04-01 11:01:37 +0200

    [streaming] Streaming program cancellation bugfix

--- flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/function/source/GenSequenceFunction.java
+++ flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/function/source/GenSequenceFunction.java
@@ -32,13 +32,16 @@ public class GenSequenceFunction extends RichParallelSourceFunction<Long> {
 	private NumberSequenceIterator fullIterator;
 	private NumberSequenceIterator splitIterator;
 
+	private volatile boolean isRunning;
+
 	public GenSequenceFunction(long from, long to) {
 		fullIterator = new NumberSequenceIterator(from, to);
 	}
 
 	@Override
 	public void run(Collector<Long> collector) throws Exception {
-		while (splitIterator.hasNext()) {
+		isRunning = true;
+		while (splitIterator.hasNext() && isRunning) {
 			collector.collect(splitIterator.next());
 		}
 	}
@@ -52,6 +55,7 @@ public class GenSequenceFunction extends RichParallelSourceFunction<Long> {
 
 	@Override
 	public void cancel() {
+		isRunning = false;
 	}
 
 }
