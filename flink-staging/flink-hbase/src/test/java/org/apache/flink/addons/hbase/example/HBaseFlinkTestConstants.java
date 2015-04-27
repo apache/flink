5fea126ca42e34e3f339887e3c883fcadd15926a
fpompermaier <f.pompermaier@gmail.com>
2015-04-27 17:15:18 +0200

    [FLINK-1928] [hbase] Added HBase write example

---
+++ flink-staging/flink-hbase/src/test/java/org/apache/flink/addons/hbase/example/HBaseFlinkTestConstants.java
@@ -0,0 +1,10 @@
+package org.apache.flink.addons.hbase.example;
+
+public class HBaseFlinkTestConstants {
+	
+	public static final byte[] CF_SOME = "someCf".getBytes();
+	public static final byte[] Q_SOME = "someQual".getBytes();
+	public static final String TEST_TABLE_NAME = "test-table";
+	public static final String TMP_DIR = "/tmp/test";
+	
+}
