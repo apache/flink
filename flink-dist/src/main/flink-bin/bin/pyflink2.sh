2f347a039b1d8e10836ca1e2a2ece9b5e3734035
Robert Metzger <rmetzger@apache.org>
2015-05-18 16:30:10 +0200

    [FLINK-2037] Provide flink-python.jar in lib/
    This closes #691

--- flink-dist/src/main/flink-bin/bin/pyflink2.sh
+++ flink-dist/src/main/flink-bin/bin/pyflink2.sh
@@ -22,4 +22,4 @@ bin=`cd "$bin"; pwd`
 
 . "$bin"/config.sh
 
-"$FLINK_BIN_DIR"/flink run -v "$FLINK_ROOT_DIR"/lib/flink-python-0.9-SNAPSHOT.jar "2" "$@"
+"$FLINK_BIN_DIR"/flink run -v "$FLINK_ROOT_DIR"/lib/flink-python*.jar "2" "$@"
