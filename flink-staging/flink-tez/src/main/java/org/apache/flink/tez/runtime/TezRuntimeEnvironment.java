eba6f9a2f7bfd15a9adf15d356e699a0143bb5a1
Stephan Ewen <sewen@apache.org>
2015-05-26 16:41:54 +0200

    [tez] Fix Flink-on-Tez use of pre-allocating MemoryManager

--- flink-staging/flink-tez/src/main/java/org/apache/flink/tez/runtime/TezRuntimeEnvironment.java
+++ flink-staging/flink-tez/src/main/java/org/apache/flink/tez/runtime/TezRuntimeEnvironment.java
@@ -36,7 +36,7 @@ public class TezRuntimeEnvironment {
 	public TezRuntimeEnvironment(long totalMemory) {
 		int pageSize = DEFAULT_PAGE_SIZE;
 		int numSlots = DEFAULT_NUM_SLOTS;
-		this.memoryManager = new DefaultMemoryManager(totalMemory, numSlots, pageSize);
+		this.memoryManager = new DefaultMemoryManager(totalMemory, numSlots, pageSize, true);
 		this.ioManager = new IOManagerAsync();
 	}
 
