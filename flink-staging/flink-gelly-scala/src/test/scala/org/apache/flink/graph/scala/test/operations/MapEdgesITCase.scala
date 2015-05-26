2b1f6ea0da82ce0eec338d213de3ffb31133a3b0
Pieter-Jan Van Aeken <pieterjan.vanaeken@euranova.eu>
2015-05-26 15:37:42 +0200

    Added the most basic unit test case for MapVertices and MapEdges

---
+++ flink-staging/flink-gelly-scala/src/test/scala/org/apache/flink/graph/scala/test/operations/MapEdgesITCase.scala
@@ -0,0 +1,64 @@
+package org.apache.flink.graph.scala.test.operations
+
+
+import org.apache.flink.api.common.functions.MapFunction
+import org.apache.flink.api.scala._
+import org.apache.flink.graph.Edge
+import org.apache.flink.graph.scala._
+import org.apache.flink.graph.scala.test.TestGraphUtils
+import org.apache.flink.test.util.{AbstractMultipleProgramsTestBase, MultipleProgramsTestBase}
+import org.junit.rules.TemporaryFolder
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.{After, Before, Rule, Test}
+
+@RunWith(classOf[Parameterized])
+class MapEdgesITCase(mode: AbstractMultipleProgramsTestBase.TestExecutionMode) extends MultipleProgramsTestBase(mode) {
+
+    private var resultPath: String = null
+    private var expectedResult: String = null
+
+    var tempFolder: TemporaryFolder = new TemporaryFolder()
+
+    @Rule
+    def getFolder(): TemporaryFolder = {
+        return tempFolder;
+    }
+
+    @Before
+    @throws(classOf[Exception])
+    def before {
+        resultPath = tempFolder.newFile.toURI.toString
+    }
+
+    @After
+    @throws(classOf[Exception])
+    def after {
+        compareResultsByLinesInMemory(expectedResult, resultPath)
+    }
+
+    @Test
+    @throws(classOf[Exception])
+    def testWithSameValue {
+        val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+        val graph: Graph[Long, Long, Long] = Graph.fromDataSet(TestGraphUtils.getLongLongVertexData(env), TestGraphUtils.getLongLongEdgeData(env), env)
+        val mappedEdges: DataSet[Edge[Long, Long]] = graph.mapEdges(new AddOneMapper).getEdges
+        // Convert Edge into a Scala Tuple for writing to csv @TODO: do this implicitly?
+        mappedEdges.map(edge => (edge.getSource, edge.getTarget, edge.getValue)).writeAsCsv(resultPath)
+        env.execute
+        expectedResult = "1,2,13\n" +
+            "1,3,14\n" + "" +
+            "2,3,24\n" +
+            "3,4,35\n" +
+            "3,5,36\n" +
+            "4,5,46\n" +
+            "5,1,52\n"
+    }
+
+    final class AddOneMapper extends MapFunction[Edge[Long, Long], Long] {
+        @throws(classOf[Exception])
+        def map(edge: Edge[Long, Long]): Long = {
+            edge.getValue + 1
+        }
+    }
+}
---
+++ flink-staging/flink-gelly-scala/src/test/scala/org/apache/flink/graph/scala/test/operations/MapVerticesITCase.scala
@@ -0,0 +1,63 @@
+package org.apache.flink.graph.scala.test.operations
+
+import org.apache.flink.api.common.functions.MapFunction
+import org.apache.flink.api.scala._
+import org.apache.flink.graph.scala._
+import org.apache.flink.graph.scala.test.TestGraphUtils
+import org.apache.flink.graph.{Edge, Vertex}
+import org.apache.flink.test.util.{AbstractMultipleProgramsTestBase, MultipleProgramsTestBase}
+import org.junit.rules.TemporaryFolder
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.{After, Before, Rule, Test}
+
+@RunWith(classOf[Parameterized])
+class MapVerticesITCase(mode: AbstractMultipleProgramsTestBase.TestExecutionMode) extends MultipleProgramsTestBase(mode) {
+
+    private var resultPath: String = null
+    private var expectedResult: String = null
+
+    var tempFolder: TemporaryFolder = new TemporaryFolder()
+
+    @Rule
+    def getFolder(): TemporaryFolder = {
+        return tempFolder;
+    }
+
+    @Before
+    @throws(classOf[Exception])
+    def before {
+        resultPath = tempFolder.newFile.toURI.toString
+    }
+
+    @After
+    @throws(classOf[Exception])
+    def after {
+        compareResultsByLinesInMemory(expectedResult, resultPath)
+    }
+
+    @Test
+    @throws(classOf[Exception])
+    def testWithSameValue {
+        val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+        val graph: Graph[Long, Long, Long] = Graph.fromDataSet(TestGraphUtils.getLongLongVertexData(env), TestGraphUtils.getLongLongEdgeData(env), env)
+        val mappedVertices: DataSet[Vertex[Long, Long]] = graph.mapVertices(new AddOneMapper).getVertices
+        // Convert Edge into a Scala Tuple for writing to csv @TODO: do this implicitly?
+        mappedVertices.map(vertex => (vertex.getId, vertex.getValue)).writeAsCsv(resultPath)
+        env.execute
+
+        expectedResult = "1,2\n" +
+            "2,3\n" +
+            "3,4\n" +
+            "4,5\n" +
+            "5,6\n";
+    }
+
+    final class AddOneMapper extends MapFunction[Vertex[Long, Long], Long] {
+        @throws(classOf[Exception])
+        def map(vertex: Vertex[Long, Long]): Long = {
+            vertex.getValue + 1
+        }
+    }
+
+}
