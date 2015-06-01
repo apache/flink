f4e5913f2ff9da85b866df1a926a7615af63ebfa
Pieter-Jan Van Aeken <pieterjan.vanaeken@euranova.eu>
2015-06-01 11:02:01 +0200

    Added subgraph/filtering and degrees methods plus basic unit test

--- flink-staging/flink-gelly-scala/src/main/scala/org/apache/flink/graph/scala/Graph.scala
+++ flink-staging/flink-gelly-scala/src/main/scala/org/apache/flink/graph/scala/Graph.scala
@@ -1,6 +1,6 @@
 package org.apache.flink.graph.scala
 
-import org.apache.flink.api.common.functions.MapFunction
+import org.apache.flink.api.common.functions.{FilterFunction, MapFunction}
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.java.{tuple => jtuple}
 import org.apache.flink.api.scala._
@@ -150,5 +150,64 @@ final class Graph[K: TypeInformation, VV: TypeInformation, EV: TypeInformation](
         new Graph[K, VV, EV](jgraph.joinWithEdgesOnTarget[T](javaTupleSet, newmapper))
     }
 
+    def subgraph(vertexFilter: FilterFunction[Vertex[K, VV]], edgeFilter: FilterFunction[Edge[K, EV]]) = {
+        new Graph[K, VV, EV](jgraph.subgraph(vertexFilter, edgeFilter))
+    }
+
+    def subgraph(vertexFilterFun: Vertex[K, VV] => Boolean, edgeFilterFun: Edge[K, EV] => Boolean) = {
+        val vertexFilter = new FilterFunction[Vertex[K, VV]] {
+            val cleanVertexFun = clean(vertexFilterFun)
+
+            override def filter(value: Vertex[K, VV]): Boolean = cleanVertexFun(value)
+        }
+
+        val edgeFilter = new FilterFunction[Edge[K, EV]] {
+            val cleanEdgeFun = clean(edgeFilterFun)
+
+            override def filter(value: Edge[K, EV]): Boolean = cleanEdgeFun(value)
+        }
+
+        new Graph[K, VV, EV](jgraph.subgraph(vertexFilter, edgeFilter))
+    }
+
+    def filterOnVertices(vertexFilter: FilterFunction[Vertex[K, VV]]) = {
+        new Graph[K, VV, EV](jgraph.filterOnVertices(vertexFilter))
+    }
+
+    def filterOnVertices(vertexFilterFun: Vertex[K, VV] => Boolean) = {
+        val vertexFilter = new FilterFunction[Vertex[K, VV]] {
+            val cleanVertexFun = clean(vertexFilterFun)
+
+            override def filter(value: Vertex[K, VV]): Boolean = cleanVertexFun(value)
+        }
+
+        new Graph[K, VV, EV](jgraph.filterOnVertices(vertexFilter))
+    }
+
+    def filterOnEdges(edgeFilter: FilterFunction[Edge[K, EV]]) = {
+        new Graph[K, VV, EV](jgraph.filterOnEdges(edgeFilter))
+    }
+
+    def filterOnEdges(edgeFilterFun: Edge[K, EV] => Boolean) = {
+        val edgeFilter = new FilterFunction[Edge[K, EV]] {
+            val cleanEdgeFun = clean(edgeFilterFun)
+
+            override def filter(value: Edge[K, EV]): Boolean = cleanEdgeFun(value)
+        }
+
+        new Graph[K, VV, EV](jgraph.filterOnEdges(edgeFilter))
+    }
+
+    def inDegrees(): DataSet[(K, Long)] = {
+       wrap(jgraph.inDegrees).map(javatuple => (javatuple.f0, javatuple.f1))
+    }
+
+    def outDegrees(): DataSet[(K, Long)] = {
+        wrap(jgraph.outDegrees).map(javatuple => (javatuple.f0, javatuple.f1))
+    }
+
+    def getDegrees(): DataSet[(K, Long)] = {
+        wrap(jgraph.getDegrees).map(javatuple => (javatuple.f0, javatuple.f1))
+    }
 
 }
