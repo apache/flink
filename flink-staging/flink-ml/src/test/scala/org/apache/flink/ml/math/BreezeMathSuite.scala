90c0142ef21ae329acc843558e7ba6bc160df8d8
Till Rohrmann <trohrmann@apache.org>
2015-06-02 13:22:47 +0200

    [ml] Fixes implicit issue with BreezeVectorConverter

--- flink-staging/flink-ml/src/test/scala/org/apache/flink/ml/math/BreezeMathSuite.scala
+++ flink-staging/flink-ml/src/test/scala/org/apache/flink/ml/math/BreezeMathSuite.scala
@@ -19,6 +19,7 @@
 package org.apache.flink.ml.math
 
 import Breeze._
+import breeze.linalg
 
 import org.scalatest.{Matchers, FlatSpec}
 
@@ -64,5 +65,34 @@ class BreezeMathSuite extends FlatSpec with Matchers {
     val result = (sm * 2.0).fromBreeze
 
     result should equal(expectedMatrix)
+  }
+
+  it should "convert a dense Flink vector into a dense Breeze vector and vice versa" in {
+    val vector = DenseVector(1, 2, 3)
+
+    val breezeVector = vector.asBreeze
+
+    val flinkVector = breezeVector.fromBreeze
+
+    breezeVector.getClass should be(new linalg.DenseVector[Double](0).getClass())
+    flinkVector.getClass should be (classOf[DenseVector])
+
+    flinkVector should equal(vector)
+  }
+
+  it should "convert a sparse Flink vector into a sparse Breeze vector and given the right " +
+    "converter back into a dense Flink vector" in {
+    implicit val converter = implicitly[BreezeVectorConverter[DenseVector]]
+
+    val vector = SparseVector.fromCOO(3, (1, 1.0), (2, 2.0))
+
+    val breezeVector = vector.asBreeze
+
+    val flinkVector = breezeVector.fromBreeze
+
+    breezeVector.getClass should be(new linalg.SparseVector[Double](null).getClass())
+    flinkVector.getClass should be (classOf[DenseVector])
+
+    flinkVector.equalsVector(vector) should be(true)
   }
 }
