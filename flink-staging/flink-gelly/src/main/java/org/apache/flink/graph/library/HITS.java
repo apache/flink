e090067aacd34e5bad9be23bf4e8c034baf9d6f9
mfahimazizi <mfahimazizi@gmail.com>
2015-06-03 08:37:32 +0200

    HITS algorithm updated

--- flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/library/HITS.java
+++ flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/library/HITS.java
@@ -1,5 +1,22 @@
-package org.apache.flink.graph.library;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package org.apache.flink.graph.library;
 import org.apache.flink.api.common.aggregators.LongSumAggregator;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.graph.Edge;
@@ -101,7 +118,7 @@ public class HITS <K extends Comparable<K> & Serializable>implements GraphAlgori
         public void preSuperstep() {
         // retrieve the Aggregator
             sum = getIterationAggregator("sum");
-            
+
             if(getSuperstepNumber()%4==3)
                 sum=0.0;
         }
