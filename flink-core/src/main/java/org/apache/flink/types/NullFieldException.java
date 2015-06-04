4e9e0d6839ba1a817432169bf6ed7b777e3096d6
Robert Metzger <rmetzger@apache.org>
2015-06-04 13:19:32 +0200

    [FLINK-2158] Add support for null to the DateSerializer
    This closes #780

--- flink-core/src/main/java/org/apache/flink/types/NullFieldException.java
+++ flink-core/src/main/java/org/apache/flink/types/NullFieldException.java
@@ -59,6 +59,18 @@ public class NullFieldException extends RuntimeException
 		super("Field " + fieldIdx + " is null, but expected to hold a value.");
 		this.fieldPos = fieldIdx;
 	}
+
+	/**
+	 * Constructs an {@code NullFieldException} with a default message, referring to
+	 * given field number as the null field and a cause (Throwable)
+	 *
+	 * @param fieldIdx The index of the field that was null, but expected to hold a value.
+	 * @param cause Pass the root cause of the error
+	 */
+	public NullFieldException(int fieldIdx, Throwable cause) {
+		super("Field " + fieldIdx + " is null, but expected to hold a value.", cause);
+		this.fieldPos = fieldIdx;
+	}
 	
 	/**
 	 * Gets the field number that was attempted to access. If the number is not set, this method returns
