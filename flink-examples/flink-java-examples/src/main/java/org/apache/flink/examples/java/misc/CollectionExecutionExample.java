d6dba5b5f376f6cb0a5c3d7eafb636567cdb8731
Gabor Gevay <ggab90@gmail.com>
2015-06-09 15:10:35 +0200

    Fixed a few trivial issues:
    - The description of the input format for the graph examples
      showed an example input as having a newline at the end
      of the last line, but CsvInputFormat doesn't allow that
      (it says "Row too short" for the empty line).
    - Fixed a link to local_execution.html in a javadoc.
    - ConcatenatedExtract had a typo in its name.
    - The creation of the jar for the SessionWindowing example was missing
      from the pom.

--- flink-examples/flink-java-examples/src/main/java/org/apache/flink/examples/java/misc/CollectionExecutionExample.java
+++ flink-examples/flink-java-examples/src/main/java/org/apache/flink/examples/java/misc/CollectionExecutionExample.java
@@ -31,7 +31,7 @@ import org.apache.flink.api.java.tuple.Tuple2;
  * DataSet transformations are executed on Java collections.
  * 
  * See the "Local Execution" section in the documentation for more details: 
- * 	http://flink.apache.org/docs/latest/local_execution.html
+ * 	http://flink.apache.org/docs/latest/apis/local_execution.html
  * 
  */
 public class CollectionExecutionExample {
