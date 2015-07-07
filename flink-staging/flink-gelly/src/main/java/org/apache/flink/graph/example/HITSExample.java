60fedf81789b0866627fc03989a9cf7a41c57f71
mfahimazizi <mfahimazizi@gmail.com>
2015-07-07 13:57:39 +0200

    [FLINK-2044]-updated

--- flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/example/HITSExample.java
+++ flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/example/HITSExample.java
@@ -1,20 +1,20 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package org.apache.flink.graph.example;
 
@@ -34,7 +34,7 @@ import org.apache.flink.util.Collector;
 /**
  * This program is an example for HITS algorithm.
  * the result is either a hub value or authority value base user selection.
- *
+ * <p>
  * If no arguments are provided, the example runs with a random graph of 10 vertices
  * and random edge weights.
  */
@@ -43,7 +43,7 @@ public class HITSExample implements ProgramDescription {
     @SuppressWarnings("serial")
     public static void main(String[] args) throws Exception {
 
-        if(!parseParameters(args)) {
+        if (!parseParameters(args)) {
             return;
         }
 
@@ -57,16 +57,15 @@ public class HITSExample implements ProgramDescription {
             }
         }, env);
 
-    // add  graph to HITS class with iteration value and hub or authority enum value.
-        DataSet<Vertex<Long, Double>> HitsValue =network.run(
-                    new HITS<Long>(Hits.AUTHORITY,maxIterations)).getVertices();
+// add  graph to HITS class with iteration value and hub or authority enum value.
+        DataSet<Vertex<Long, Double>> HitsValue = network.run(
+                new HITS<Long>(Hits.AUTHORITY, maxIterations)).getVertices();
 
         if (fileOutput) {
             HitsValue.writeAsCsv(outputPath, "\n", "\t");
             env.execute("HITS algorithm");
         } else {
             HitsValue.print();
-//            env.execute("HITS algorithm");
         }
 
     }
@@ -76,9 +75,9 @@ public class HITSExample implements ProgramDescription {
         return "HITS algorithm example";
     }
 
-    // *************************************************************************
-    //     UTIL METHODS
-    // *************************************************************************
+// *************************************************************************
+//     UTIL METHODS
+// *************************************************************************
 
     private static boolean fileOutput = false;
     private static long numPages = 10;
@@ -88,8 +87,8 @@ public class HITSExample implements ProgramDescription {
 
     private static boolean parseParameters(String[] args) {
 
-        if(args.length > 0) {
-            if(args.length != 3) {
+        if (args.length > 0) {
+            if (args.length != 3) {
                 System.err.println("Usage: HITS <input edges path> <output path> <num iterations>");
                 return false;
             }
--- flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/library/HITS.java
+++ flink-staging/flink-gelly/src/main/java/org/apache/flink/graph/library/HITS.java
@@ -1,22 +1,23 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 
 package org.apache.flink.graph.library;
+
 import org.apache.flink.api.common.aggregators.DoubleSumAggregator;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.graph.Edge;
@@ -33,49 +34,47 @@ import java.io.Serializable;
 
 
 /**
- *
  * This class implements the HITS algorithm by using flink Gelly API
  * Hyperlink-Induced Topic Search (HITS; also known as hubs and authorities) is a link analysis algorithm that rates
  * Web pages,
  * developed by Jon Kleinberg.
- *
+ * <p>
  * The algorithm performs a series of iterations, each consisting of two basic steps:
- *
+ * <p>
  * Authority Update: Update each node's Authority score to be equal to the sum of the Hub Scores of each node that
  * points to it.
  * That is, a node is given a high authority score by being linked from pages that are recognized as Hubs for information.
  * Hub Update: Update each node's Hub Score to be equal to the sum of the Authority Scores of each node that it
  * points to.
  * That is, a node is given a high hub score by linking to nodes that are considered to be authorities on the subject.
- *
+ * <p>
  * The Hub score and Authority score for a node is calculated with the following algorithm:
- *  *Start with each node having a hub score and authority score of 1.
- *  *Run the Authority Update Rule
- *  *Run the Hub Update Rule
- *  *Normalize the values by dividing each Hub score by square root of the sum of the squares of all Hub scores, and
- *      dividing each Authority score by square root of the sum of the squares of all Authority scores.
- *  *Repeat from the second step as necessary.
+ * *Start with each node having a hub score and authority score of 1.
+ * *Run the Authority Update Rule
+ * *Run the Hub Update Rule
+ * *Normalize the values by dividing each Hub score by square root of the sum of the squares of all Hub scores, and
+ * dividing each Authority score by square root of the sum of the squares of all Authority scores.
+ * *Repeat from the second step as necessary.
  *
  * @see "http://en.wikipedia.org/wiki/HITS_algorithm "
- *
  */
 
-public class HITS <K extends Comparable<K> & Serializable>implements GraphAlgorithm<K, Double, String> {
+public class HITS<K extends Comparable<K> & Serializable> implements GraphAlgorithm<K, Double, String> {
 
     private Hits HubAuthority;
     private int maxIterations;
 
-    public HITS(Hits choice, int maxIter){
+    public HITS(Hits choice, int maxIter) {
 
-        this.HubAuthority=choice;
+        this.HubAuthority = choice;
 
-        if(this.HubAuthority.equals(Hits.AUTHORITY)) {
+        if (this.HubAuthority.equals(Hits.AUTHORITY)) {
 
-            this.maxIterations=(maxIter*4)-2;
+            this.maxIterations = (maxIter * 4) - 2;
 
-        }else {
+        } else {
 
-            this.maxIterations=maxIter*4;
+            this.maxIterations = maxIter * 4;
 
         }
 
@@ -84,6 +83,7 @@ public class HITS <K extends Comparable<K> & Serializable>implements GraphAlgori
     /**
      * this method will get a graph and process for Hub and Authority and it will return a graph( Hub or Authority
      * values).
+     *
      * @param HitsGraph
      * @return Graph
      * @throws Exception
@@ -92,10 +92,13 @@ public class HITS <K extends Comparable<K> & Serializable>implements GraphAlgori
     @Override
     public Graph<K, Double, String> run(Graph<K, Double, String> HitsGraph) throws Exception {
 
-        HitsGraph=(HitsGraph.mapEdges(new MapFunction<Edge<K,String>, String>() {
+        HitsGraph = (HitsGraph.mapEdges(new MapFunction<Edge<K, String>, String>() {
 
             @Override
-            public String map(Edge<K, String> value) throws Exception { return "H"; } })).
+            public String map(Edge<K, String> value) throws Exception {
+                return "H";
+            }
+        })).
 
                 union(HitsGraph.reverse().mapEdges(new MapFunction<Edge<K, String>, String>() {
 
@@ -105,30 +108,33 @@ public class HITS <K extends Comparable<K> & Serializable>implements GraphAlgori
                     }
                 }));
 
-        VertexCentricConfiguration para=new VertexCentricConfiguration();
-        para.registerAggregator("sum",new DoubleSumAggregator());
+        VertexCentricConfiguration para = new VertexCentricConfiguration();
+        para.registerAggregator("sum", new DoubleSumAggregator());
 
-            return HitsGraph.runVertexCentricIteration(new VertexHitsUpdater<K>(),new HitsMessenger<K>(),
-                    maxIterations,para);
-        }
-                /**
-                 * Function that updates in odd superStep Iteration either the Hub or Authority values of a vertex by summing up
-                 * the partial Hits or Authority values from all incoming messages and then applying the normalization process on
-                 * even superstep for either Hub or Authority values.
-                 */
-        @SuppressWarnings("serial")
+        return HitsGraph.runVertexCentricIteration(new VertexHitsUpdater<K>(), new HitsMessenger<K>(),
+                maxIterations, para);
+    }
+
+    /**
+     * Function that updates in odd superStep Iteration either the Hub or Authority values of a vertex by summing up
+     * the partial Hits or Authority values from all incoming messages and then applying the normalization process on
+     * even superstep for either Hub or Authority values.
+     */
+    @SuppressWarnings("serial")
     public static final class VertexHitsUpdater<K> extends VertexUpdateFunction<K, Double, Double> {
 
-        DoubleSumAggregator aggregator=new DoubleSumAggregator();
+        DoubleSumAggregator aggregator = new DoubleSumAggregator();
+
         public void preSuperstep() {
-        // retrieve the Aggregator
+// retrieve the Aggregator
             aggregator = getIterationAggregator("sum");
 
-            if(getSuperstepNumber()!=1 && getSuperstepNumber()%4==1)
+            if (getSuperstepNumber() % 4 == 1)
                 aggregator.reset();
-            if(getSuperstepNumber()%4==3)
+            if (getSuperstepNumber() % 4 == 3)
                 aggregator.reset();
         }
+
         @Override
 //        for distinguishing Authority and Hub edges, need following parameter in Message.
 //        public void updateVertex(Vertex<K, Double> vertex, MessageIterator<Edge<Long,String>,Double>> inMessages)
@@ -136,37 +142,39 @@ public class HITS <K extends Comparable<K> & Serializable>implements GraphAlgori
         public void updateVertex(Vertex<K, Double> vertex, MessageIterator<Double> inMessages) {
 
 
-            double num=0.0;
-            switch (getSuperstepNumber()%4){
+            double num = 0.0;
+            switch (getSuperstepNumber() % 4) {
                 case 1:
-                    for(double m:inMessages) {
+                    for (double m : inMessages) {
 //                     if(m.f0.f1.equle("A"))
+//                          num+=m.f1.f0;
                         num += m;
                     }
-                    aggregator.aggregate(Math.pow(num,2));
+                    aggregator.aggregate(Math.pow(num, 2));
                     break;
 
                 case 2:
-                    setNewVertexValue(vertex.f1/(Math.sqrt(aggregator.getAggregate().getValue())));
+                    setNewVertexValue(vertex.f1 / (Math.sqrt(aggregator.getAggregate().getValue())));
                     break;
 
                 case 3:
-                    for(double m:inMessages) {
+                    for (double m : inMessages) {
 //                        if(m.f0.f1.equle("H"))
-                            num += m;
+//                             num+=m.f1.f0;
+                        num += m;
                     }
-                    aggregator.aggregate(Math.pow(num,2));
+                    aggregator.aggregate(Math.pow(num, 2));
                     break;
 
-                case 4:
-                    setNewVertexValue(vertex.f1/(Math.sqrt(aggregator.getAggregate().getValue())));
+                case 0:
+                    setNewVertexValue(vertex.f1 / (Math.sqrt(aggregator.getAggregate().getValue())));
                     break;
             }
         }
     }
 
     /**
-     *Distributes in even superstep iteration the normalization value to the target vertices.
+     * Distributes in even superstep iteration the normalization value to the target vertices.
      */
     @SuppressWarnings("serial")
     public static final class HitsMessenger<K> extends MessagingFunction<K, Double, Double, String> {
@@ -174,13 +182,13 @@ public class HITS <K extends Comparable<K> & Serializable>implements GraphAlgori
         @Override
         public void sendMessages(Vertex<K, Double> vertex) {
 
-            switch (getSuperstepNumber()%2) {
-                case 0:
+            switch (getSuperstepNumber() % 2) {
+                case 1:
                     sendMessageToAllNeighbors(vertex.f1);
                     break;
 
-                case 1:
-                    sendMessageTo(vertex.f0,vertex.f1);
+                case 0:
+                    sendMessageTo(vertex.f0, vertex.f1);
                     break;
             }
         }
