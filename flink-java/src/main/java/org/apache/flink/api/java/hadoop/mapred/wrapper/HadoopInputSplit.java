91675296e1d0fcc503ab3af9c5da7fdb83b78fc5
Fabian Hueske <fhueske@apache.org>
2015-07-14 00:59:20 +0200

    [FLINK-2353] Respect JobConfigurable interface in Hadoop mapred wrappers
    This closes #908

--- flink-java/src/main/java/org/apache/flink/api/java/hadoop/mapred/wrapper/HadoopInputSplit.java
+++ flink-java/src/main/java/org/apache/flink/api/java/hadoop/mapred/wrapper/HadoopInputSplit.java
@@ -28,6 +28,7 @@ import org.apache.flink.core.io.LocatableInputSplit;
 import org.apache.hadoop.conf.Configurable;
 import org.apache.hadoop.io.WritableFactories;
 import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.JobConfigurable;
 
 /**
  * A wrapper that represents an input split from the Hadoop mapred API as
@@ -112,6 +113,9 @@ public class HadoopInputSplit extends LocatableInputSplit {
 
 		if (hadoopInputSplit instanceof Configurable) {
 			((Configurable) hadoopInputSplit).setConf(this.jobConf);
+		}
+		else if (hadoopInputSplit instanceof JobConfigurable) {
+			((JobConfigurable) hadoopInputSplit).configure(this.jobConf);
 		}
 		hadoopInputSplit.readFields(in);
 	}
