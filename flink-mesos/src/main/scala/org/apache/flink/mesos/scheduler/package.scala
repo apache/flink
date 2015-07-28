cd793892c8ac939824f675ab293af947ad66b211
Ankur Chauhan <achauhan@brightcove.com>
2015-07-28 16:06:30 -0700

    Add comments for each of the properties

--- flink-mesos/src/main/scala/org/apache/flink/mesos/scheduler/package.scala
+++ flink-mesos/src/main/scala/org/apache/flink/mesos/scheduler/package.scala
@@ -25,28 +25,35 @@ import org.rogach.scallop.ScallopConf
 
 package object scheduler {
 
-  // configuration key and default value for streamingMode
+  // configuration for streamingMode
   val STREAMING_MODE_KEY = "streamingMode"
   val DEFAULT_STREAMING_MODE = "batch"
 
+  // maximum number of task managers, by default we will greedily consume whole cluster
   val TASK_MANAGER_COUNT_KEY = "flink.mesos.taskmanagers.maxcount"
-  val DEFAULT_TASK_MANAGER_COUNT = Integer.MAX_VALUE // consume whole cluster
+  val DEFAULT_TASK_MANAGER_COUNT = Integer.MAX_VALUE
 
+  // number of CPU cores to be allocated per task manager, default = 1 core
   val TASK_MANAGER_CPU_KEY = "flink.mesos.taskmanagers.cpu"
   val DEFAULT_TASK_MANAGER_CPU = 1.0F // 1 core
 
+  // Amount of memory (in MiB) to be allocated per task manager, default = 512MiB
   val TASK_MANAGER_MEM_KEY = "flink.mesos.taskmanagers.mem"
   val DEFAULT_TASK_MANAGER_MEM = 512.0F // 1GB
 
+  // Amount of disk (in MiB) to be allocated per task manager, default = 1GB
   val TASK_MANAGER_DISK_KEY = "flink.mesos.taskmanager.disk"
   val DEFAULT_TASK_MANGER_DISK = 1024.0F // 1GB
 
+  // offer attribute constraints to be checked while accepting offers, default = no constraints
   val TASK_MANAGER_OFFER_ATTRIBUTES_KEY = "flink.mesos.taskmanager.attributes"
   val DEFAULT_TASK_MANAGER_OFFER_ATTRIBUTES = ""
 
+  // offer role to use for resource offers
   val MESOS_FRAMEWORK_ROLE_KEY = "flink.mesos.framework.role"
   val DEFAULT_MESOS_FRAMEWORK_ROLE = "*"
 
+  // additional JVM arguments for task managers
   val TASK_MANAGER_JVM_ARGS_KEY = "flink.mesos.taskmanager.jvmArgs"
   val DEFAULT_TASK_MANAGER_JVM_ARGS = "-server " +
                                       "-XX:+UseConcMarkSweepGC " +
@@ -58,9 +65,11 @@ package object scheduler {
                                       "-XX:+UseFastAccessorMethods " +
                                       "-XX:+AlwaysPreTouch"
 
+  // mesos-slave visible url to the flink uberjar (that contains the TaskManagerExecutor)
   val FLINK_UBERJAR_LOCATION_KEY = "flink.uberjar.location"
   val DEFAULT_FLINK_UBERJAR_LOCATION = "file:///vagrant/flink-dist-0.10-SNAPSHOT.jar"
 
+  // path to the native library of the slave
   val MESOS_NATIVE_JAVA_LIBRARY_KEY = "mesos.native.lib"
   val DEFAULT_MESOS_NATIVE_JAVA_LIBRARY = "/usr/local/lib/libmesos.so"
 
@@ -68,24 +77,27 @@ package object scheduler {
   // to a jvm process's resource requirement, in addition to its heap size.
   val JVM_MEM_OVERHEAD_PERCENT_DEFAULT = 0.20
 
+  // The user to use to spawn the taskManager, default = same as the scheduler
   val MESOS_FRAMEWORK_USER_KEY = "flink.mesos.framework.user"
   val DEFAULT_MESOS_FRAMEWORK_USER = ""
 
+  // Framework Id to use for the mesos scheduler, default = mesos-assigned id is used
   val MESOS_FRAMEWORK_ID_KEY = "flink.mesos.framework.id"
   val DEFAULT_MESOS_FRAMEWORK_ID = null
 
+  // Human friendly framework name (to be used in the mesos UI)
   val MESOS_FRAMEWORK_NAME_KEY = "flink.mesos.framework.name"
   val DEFAULT_MESOS_FRAMEWORK_NAME = "Apache Flink on Mesos"
 
-  val MESOS_FRAMEWORK_FAILOVER_TIMEOUT_KEY = "flink.mesos.framework.failoverTimeout"
-  val DEFAULT_MESOS_FRAMEWORK_FAILOVER_TIMEOUT = 300
-
+  // Mesos authentication principal to use, default = no authentication
   val MESOS_FRAMEWORK_PRINCIPAL_KEY = "flink.mesos.framework.principal"
   val DEFAULT_MESOS_FRAMEWORK_PRINCIPAL = null
 
+  // Mesos authentication secret to use, default = no authentication
   val MESOS_FRAMEWORK_SECRET_KEY = "flink.mesos.framework.secret"
   val DEFAULT_MESOS_FRAMEWORK_SECRET = null
 
+  // Mesos master url (generally the zookeeper path at witch mesos master is registered)
   val MESOS_MASTER_KEY = "flink.mesos.master"
   val DEFAULT_MESOS_MASTER = "zk://127.0.0.1:2181/mesos"
 
@@ -94,7 +106,7 @@ package object scheduler {
                             descr = "Configuration directory for flink")
     val host = opt[String](required = false,
                          default = Some(InetAddress.getLocalHost.getHostName),
-                         descr = "override hostname for this jobmanager")
+                         descr = "Override hostname for this jobManager")
   }
 
   sealed case class RunningTaskManager(taskId: TaskID, slaveId: SlaveID)
