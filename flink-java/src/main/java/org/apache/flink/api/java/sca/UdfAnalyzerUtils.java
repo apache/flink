2c9c3e0e4d58b6a289f77f6495ea665deab09343
tedyu <yuzhihong@gmail.com>
2015-08-02 09:51:41 -0700

    Address checkstyle errors

--- flink-java/src/main/java/org/apache/flink/api/java/sca/UdfAnalyzerUtils.java
+++ flink-java/src/main/java/org/apache/flink/api/java/sca/UdfAnalyzerUtils.java
@@ -114,12 +114,12 @@ public final class UdfAnalyzerUtils {
 	 */
 	@SuppressWarnings("unchecked")
 	public static Object[] findMethodNode(String internalClassName, String name, String desc) {
-    InputStream stream = null;
+	  InputStream stream = null;
 		try {
 			// iterate through hierarchy and search for method node /
 			// class that really implements the method
 			while (internalClassName != null) {
-			    stream = Thread.currentThread().getContextClassLoader()
+			stream = Thread.currentThread().getContextClassLoader()
 						.getResourceAsStream(internalClassName.replace('.', '/') + ".class");
 				ClassReader cr = new ClassReader(stream);
 				final ClassNode cn = new ClassNode();
@@ -134,7 +134,9 @@ public final class UdfAnalyzerUtils {
 		} catch (IOException e) {
 			throw new IllegalStateException("Method '" + name + "' could not be found", e);
 		} finally {
-		    if (stream != null) stream.close();
+		    if (stream != null) {
+		      stream.close();
+		    }
 		}
 		throw new IllegalStateException("Method '" + name + "' could not be found");
 	}
