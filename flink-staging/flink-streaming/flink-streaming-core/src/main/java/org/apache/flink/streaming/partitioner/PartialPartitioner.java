9b05ebd91efbaf6b3a809625c81ea20a59d0f46d
Anis Nasir <anis.uddin@hotmail.com>
2015-08-27 15:53:41 +0300

    removed comments

--- flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/partitioner/PartialPartitioner.java
+++ flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/partitioner/PartialPartitioner.java
@@ -23,16 +23,10 @@ import org.apache.flink.streaming.api.streamrecord.StreamRecord;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 
-/**
- * Partitioner that map each key on any two channels using power of two choices.
- *
- * @param <T>
- *            Type of the Tuple
- */
 public class PartialPartitioner<T> extends StreamPartitioner<T> {
 	private static final long serialVersionUID = 1L;
 
-	private long[] targetTaskStats; // maintain past history of forwarded messages
+	private long[] targetTaskStats; 
 	private HashFunction h1 = Hashing.murmur3_128(13);
 	private HashFunction h2 = Hashing.murmur3_128(17);
 	KeySelector<T, ?> keySelector;
@@ -47,7 +41,7 @@ public class PartialPartitioner<T> extends StreamPartitioner<T> {
 	@Override
 	public int[] selectChannels(SerializationDelegate<StreamRecord<T>> record,
 			int numberOfOutputChannels) {
-		String str = record.getInstance().getKey(keySelector).toString(); // assume key is the first field
+		String str = record.getInstance().getKey(keySelector).toString();
 		int firstChoice = (int) ( Math.abs(h1.hashBytes(str.getBytes()).asLong()) % numberOfOutputChannels );
 		int secondChoice = (int) ( Math.abs(h2.hashBytes(str.getBytes()).asLong()) % numberOfOutputChannels );
 		int selected = targetTaskStats[firstChoice] > targetTaskStats[secondChoice] ? secondChoice : firstChoice;
