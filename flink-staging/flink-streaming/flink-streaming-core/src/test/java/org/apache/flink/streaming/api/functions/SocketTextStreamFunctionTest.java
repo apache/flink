b7e0c2ec3ea299ad67737f148ffb8097bef919f3
ogokal <ogokal@gmail.com>
2015-09-01 21:55:02 +0300

    [FLINK-2125][streaming] changed port and assertion for full test content

--- flink-staging/flink-streaming/flink-streaming-core/src/test/java/org/apache/flink/streaming/api/functions/SocketTextStreamFunctionTest.java
+++ flink-staging/flink-streaming/flink-streaming-core/src/test/java/org/apache/flink/streaming/api/functions/SocketTextStreamFunctionTest.java
@@ -35,19 +35,6 @@ import static org.junit.Assert.assertTrue;
 
 
 public class SocketTextStreamFunctionTest {
-    //Actual text
-    /*
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sagittis nisl non euismod fermentum. Curabitur lacinia vehicula enim quis tristique. Suspendisse imperdiet arcu sed bibendum vulputate. Sed vitae nisl vitae turpis dapibus lacinia in id elit. Integer lorem dolor, porttitor ut nisi in, tincidunt sodales leo. Aliquam tristique dui sit amet odio bibendum, et rutrum turpis auctor. Morbi sit amet mollis augue, ac rutrum velit. Vestibulum suscipit finibus sapien, et congue enim laoreet consequat.
-
-    Integer aliquam metus iaculis risus hendrerit maximus. Suspendisse vestibulum nibh ac mauris cursus molestie sit amet vel turpis. Nulla et posuere orci. Aliquam dui quam, posuere vitae erat vitae, finibus commodo ipsum. Aliquam eu dui quis arcu porttitor sollicitudin. Integer sodales finibus ullamcorper. Praesent et felis tempor, laoreet libero eget, consequat nisl. Aenean molestie rutrum lorem, ac cursus nisl dapibus vitae.
-
-    Quisque sodales dui et sem bibendum semper. Pellentesque luctus leo nec lacus euismod pellentesque. Phasellus a metus dignissim risus auctor lacinia. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean consectetur bibendum imperdiet. Etiam dignissim rutrum enim, non volutpat nisi condimentum sed. Quisque condimentum ultrices est sit amet facilisis.
-
-    Ut vitae volutpat odio. Sed eget vestibulum libero, eu tincidunt lorem. Nam pretium nulla nisl. Maecenas fringilla nunc ut turpis consectetur, et fringilla sem placerat. Etiam nec scelerisque nisi, at sodales ligula. Aliquam euismod faucibus egestas. Curabitur eget enim quam. Praesent convallis mattis lobortis. Pellentesque a consectetur nisl. Duis molestie diam est. Nam a malesuada augue. Vivamus enim massa, luctus ac elit ut, vestibulum laoreet nulla. Curabitur pellentesque vel mi eget tempus. Donec cursus et leo quis viverra.
-
-    In ac imperdiet ex, nec aliquet erat. Nullam sit amet enim in dolor finibus convallis id eu nibh. Fusce aliquam convallis orci aliquam.
-    */
-    // Generated 5 paragraphs, 290 words, 2000 bytes of Lorem Ipsum
 
     private static final String content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sagittis nisl non euismod fermentum. " +
             "Curabitur lacinia vehicula enim quis tristique. Suspendisse imperdiet arcu sed bibendum vulputate. " +
@@ -75,26 +62,29 @@ public class SocketTextStreamFunctionTest {
             "Curabitur pellentesque vel mi eget tempus. Donec cursus et leo quis viverra.\r\nIn ac imperdiet ex, " +
             "nec aliquet erat. Nullam sit amet enim in dolor finibus convallis id eu nibh. Fusce aliquam convallis orci aliquam.";
 
-    private static final String[] fakeServerResponses = content.split("\\.");
-
     private class TestServer implements Runnable{
-        private int port = 44444;
+        private volatile int port = 0;
         public int getPort(){
             return port;
         }
         private ServerSocket serverSocket;
+        public TestServer() {
+            try {
+                serverSocket = new ServerSocket(port);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            port = serverSocket.getLocalPort();
+        }
 
         @Override public void run() {
             try {
-                serverSocket = new ServerSocket(port);
                 while(true){
                     Socket clientSocket = serverSocket.accept();
                     try {
                         PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-                        for (String res : fakeServerResponses){
-                            out.print(res+".");
-                            out.flush();
-                        }
+                        out.print(content);
+                        out.flush();
                     }finally {
                         clientSocket.close();
                     }
@@ -135,76 +125,99 @@ public class SocketTextStreamFunctionTest {
         source.run(flinkCollector);
     }
 
+    private String restoreContentFromActual(String delimiter, List<String> actualList, boolean newLineAsWindows){
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        for (; i < actualList.size()-1; i++) {
+            sb.append(actualList.get(i)+delimiter);
+        }
+        sb.append(actualList.get(i));
+        return sb.toString().replace("\r","").replace("\n","\r\n");
+    }
+
     @Test
     public void testNewLineDelimitedOldApiWithChar() throws Exception {
         List<String> actualList = new ArrayList<>();
-        prepareTestForOld('\n',actualList);
+        char delimiter = '\n';
+        prepareTestForOld(delimiter, actualList);
         assertEquals(5, actualList.size());
+        assertEquals(content,restoreContentFromActual(String.valueOf(delimiter),actualList,true));
     }
 
     @Test
     public void testCarriageDelimitedOldApiWithChar() throws Exception {
         List<String> actualList = new ArrayList<>();
-        prepareTestForOld('\r',actualList);
+        char delimiter = '\r';
+        prepareTestForOld(delimiter,actualList);
         assertEquals(5, actualList.size());
+        assertEquals(content,restoreContentFromActual(String.valueOf(delimiter), actualList, false));
     }
 
     @Test
     public void testNewLineDelimited() throws Exception {
 		List<String> actualList = new ArrayList<>();
-        prepareTest("\n",actualList);
+        String delimiter = "\n";
+        prepareTest(delimiter,actualList);
         assertEquals(5, actualList.size());
+        assertEquals(content,restoreContentFromActual(delimiter,actualList,true));
     }
 
     @Test
     public void testCarriageDelimited() throws Exception {
         List<String> actualList = new ArrayList<>();
-        prepareTest("\r",actualList);
+        String delimiter = "\r";
+        prepareTest(delimiter,actualList);
         assertEquals(5, actualList.size());
         assertTrue(actualList.get(1).indexOf('\n') != -1);
+        assertEquals(content, restoreContentFromActual(delimiter, actualList, false));
     }
 
     @Test
     public void testWindowsLineEndDelimited() throws Exception {
         List<String> actualList = new ArrayList<>();
-        prepareTest("\r\n",actualList);
+        String delimiter = "\r\n";
+        prepareTest(delimiter,actualList);
         assertEquals(5, actualList.size());
         assertTrue(actualList.get(0).indexOf('\r') == -1);
         assertTrue(actualList.get(0).indexOf('\n') == -1);
+        assertEquals(content, restoreContentFromActual(delimiter, actualList, false));
     }
     
     @Test
     public void testWindowsLineEndSuffixDelimited() throws Exception {
         List<String> actualList = new ArrayList<>();
-        prepareTest(".\r\n",actualList);
+        String delimiter = ".\r\n";
+        prepareTest(delimiter,actualList);
         assertEquals(5, actualList.size());
         assertTrue(actualList.get(0).indexOf('\r') == -1);
         assertTrue(actualList.get(0).indexOf('\n') == -1);
+        assertEquals(content, restoreContentFromActual(delimiter, actualList, false));
     }
 
     @Test
     public void testLongDelimited() throws Exception {
         List<String> actualList = new ArrayList<>();
+        String delimiter = "Integer aliquam metus iaculis risus hendrerit maximus. " +
+                "Suspendisse vestibulum nibh ac mauris cursus molestie sit amet vel turpis. " +
+                "Nulla et posuere orci. Aliquam dui quam, posuere vitae erat vitae, " +
+                "finibus commodo ipsum. Aliquam eu dui quis arcu porttitor sollicitudin. " +
+                "Integer sodales finibus ullamcorper. Praesent et felis tempor, laoreet libero eget, " +
+                "consequat nisl. Aenean molestie rutrum lorem, ac cursus nisl dapibus vitae."+
+                "\r\nQuisque sodales dui et sem bibendum semper. Pellentesque luctus leo nec lacus euismod pellentesque. " +
+                "Phasellus a metus dignissim risus auctor lacinia. Class " +
+                "aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos" +
+                ". Aenean consectetur bibendum imperdiet. Etiam dignissim rutrum enim, " +
+                "non volutpat nisi condimentum sed. Quisque condimentum ultrices est sit amet " +
+                "facilisis.\r\nUt vitae volutpat odio. Sed eget vestibulum libero, eu " +
+                "tincidunt lorem. Nam pretium nulla nisl. Maecenas fringilla nunc ut turpis consectetur, " +
+                "et fringilla sem placerat. Etiam nec scelerisque nisi, at sodales ligula. Aliquam " +
+                "euismod faucibus egestas. Curabitur eget enim quam. Praesent convallis mattis lobortis. " +
+                "Pellentesque a consectetur nisl. Duis molestie diam est. Nam a malesuada augue. " +
+                "Vivamus enim massa, luctus ac elit ut, vestibulum laoreet nulla. " +
+                "Curabitur pellentesque vel mi eget tempus. Donec cursus et leo quis viverra.";
 
-        prepareTest("Integer aliquam metus iaculis risus hendrerit maximus. " +
-            "Suspendisse vestibulum nibh ac mauris cursus molestie sit amet vel turpis. " +
-            "Nulla et posuere orci. Aliquam dui quam, posuere vitae erat vitae, " +
-            "finibus commodo ipsum. Aliquam eu dui quis arcu porttitor sollicitudin. " +
-            "Integer sodales finibus ullamcorper. Praesent et felis tempor, laoreet libero eget, " +
-            "consequat nisl. Aenean molestie rutrum lorem, ac cursus nisl dapibus vitae."+
-            "\r\nQuisque sodales dui et sem bibendum semper. Pellentesque luctus leo nec lacus euismod pellentesque. " +
-            "Phasellus a metus dignissim risus auctor lacinia. Class " +
-            "aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos" +
-            ". Aenean consectetur bibendum imperdiet. Etiam dignissim rutrum enim, " +
-            "non volutpat nisi condimentum sed. Quisque condimentum ultrices est sit amet " +
-            "facilisis.\r\nUt vitae volutpat odio. Sed eget vestibulum libero, eu " +
-            "tincidunt lorem. Nam pretium nulla nisl. Maecenas fringilla nunc ut turpis consectetur, " +
-            "et fringilla sem placerat. Etiam nec scelerisque nisi, at sodales ligula. Aliquam " +
-            "euismod faucibus egestas. Curabitur eget enim quam. Praesent convallis mattis lobortis. " +
-            "Pellentesque a consectetur nisl. Duis molestie diam est. Nam a malesuada augue. " +
-            "Vivamus enim massa, luctus ac elit ut, vestibulum laoreet nulla. " +
-            "Curabitur pellentesque vel mi eget tempus. Donec cursus et leo quis viverra.",actualList);
-
+        prepareTest(delimiter,actualList);
+        assertEquals(content, restoreContentFromActual(delimiter, actualList, false));
         assertEquals(2, actualList.size());
         assertTrue(actualList.get(0).indexOf('\r') == -1);
         assertTrue(actualList.get(0).indexOf('\n') != -1);
