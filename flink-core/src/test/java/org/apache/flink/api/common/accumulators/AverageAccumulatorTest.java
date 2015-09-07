c8836d4413a3d8b03bce8284354da16dec1fe569
HuangWHWHW <404823056@qq.com>
2015-09-07 20:19:33 +0800

    [FLINK-2626][core]remove the class documentation

--- flink-core/src/test/java/org/apache/flink/api/common/accumulators/AverageAccumulatorTest.java
+++ flink-core/src/test/java/org/apache/flink/api/common/accumulators/AverageAccumulatorTest.java
@@ -24,9 +24,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-/**
- * Test for {@link org.apache.flink.api.common.accumulators.AverageAccumulator}
- */
 public class AverageAccumulatorTest {
 
 	@Test
