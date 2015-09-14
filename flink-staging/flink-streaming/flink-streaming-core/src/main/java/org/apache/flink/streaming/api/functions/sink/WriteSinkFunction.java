8acd15d82f4d29088d0ecda5b74479ede48b84e3
HuangWHWHW <404823056@qq.com>
2015-09-14 20:02:38 +0800

    [streaming] Appropriate Exception message in WriteSinkFunction
    Closes #1119

--- flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/functions/sink/WriteSinkFunction.java
+++ flink-staging/flink-streaming/flink-streaming-core/src/main/java/org/apache/flink/streaming/api/functions/sink/WriteSinkFunction.java
@@ -56,7 +56,7 @@ public abstract class WriteSinkFunction<IN> implements SinkFunction<IN> {
 			writer.print("");
 			writer.close();
 		} catch (FileNotFoundException e) {
-			throw new RuntimeException("File not found " + path, e);
+			throw new RuntimeException("An error occurred while cleaning the file: " + e.getMessage(), e);
 		}
 	}
 
