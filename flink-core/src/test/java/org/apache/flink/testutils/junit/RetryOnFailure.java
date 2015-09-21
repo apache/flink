9a5ea66dd75214de602b6121719fe722c922fa3a
Ufuk Celebi <uce@apache.org>
2015-09-21 21:23:04 +0200

    [tests] Add RetryOnFailure annotation

---
+++ flink-core/src/test/java/org/apache/flink/testutils/junit/RetryOnFailure.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.testutils.junit;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to use with {@link RetryRule}.
+ *
+ * <p>Add the {@link RetryRule} to your test and annotate it with {@link RetryOnFailure} annotation.
+ * Instead of adding the rule manually, you can also extend the {@link TestRetry} class.
+ *
+ * <pre>
+ * public class YourTest {
+ *
+ *     {@literal@}Rule
+ *     public RetryRule retryRule = new RetryRule();
+ *
+ *     {@literal@}Test
+ *     {@literal@}RetryOnFailure(times=10)
+ *     public void yourTest() {
+ *         // This will be retried 10 times (total runs 11) before failing the test.
+ *         throw new Exception("Failing test");
+ *     }
+ * }
+ * </pre>
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ java.lang.annotation.ElementType.METHOD })
+public @interface RetryOnFailure {
+	int times();
+}
