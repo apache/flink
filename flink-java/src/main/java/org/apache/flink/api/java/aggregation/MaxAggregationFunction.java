12a70569a6ab0f27c166c3f704a6cd639bf0bfa9
Greg Hogan <code@greghogan.com>
2015-09-22 13:01:47 -0400

    [FLINK-2725] Add Max/Min/Sum aggregation for mutable types.
    This closes #1191

--- flink-java/src/main/java/org/apache/flink/api/java/aggregation/MaxAggregationFunction.java
+++ flink-java/src/main/java/org/apache/flink/api/java/aggregation/MaxAggregationFunction.java
@@ -18,35 +18,74 @@
 
 package org.apache.flink.api.java.aggregation;
 
+import org.apache.flink.types.CopyableValue;
+import org.apache.flink.types.ResettableValue;
 
-public class MaxAggregationFunction<T extends Comparable<T>> extends AggregationFunction<T> {
+
+public abstract class MaxAggregationFunction<T extends Comparable<T>> extends AggregationFunction<T> {
 	private static final long serialVersionUID = 1L;
 
-	private T value;
-
-	@Override
-	public void initializeAggregate() {
-		value = null;
-	}
-
-	@Override
-	public void aggregate(T val) {
-		if (value != null) {
-			int cmp = value.compareTo(val);
-			value = (cmp > 0) ? value : val;
-		} else {
-			value = val;
-		}
-	}
-
-	@Override
-	public T getAggregate() {
-		return value;
-	}
-	
 	@Override
 	public String toString() {
 		return "MAX";
+	}
+
+	// --------------------------------------------------------------------------------------------
+
+	public static final class ImmutableMaxAgg<U extends Comparable<U>> extends MaxAggregationFunction<U> {
+		private static final long serialVersionUID = 1L;
+
+		private U value;
+
+		@Override
+		public void initializeAggregate() {
+			value = null;
+		}
+
+		@Override
+		public void aggregate(U val) {
+			if (value != null) {
+				int cmp = value.compareTo(val);
+				value = (cmp > 0) ? value : val;
+			} else {
+				value = val;
+			}
+		}
+
+		@Override
+		public U getAggregate() {
+			return value;
+		}
+	}
+	
+	// --------------------------------------------------------------------------------------------
+
+	public static final class MutableMaxAgg<U extends Comparable<U> & ResettableValue<U> & CopyableValue<U>> extends MaxAggregationFunction<U> {
+		private static final long serialVersionUID = 1L;
+
+		private U value;
+
+		@Override
+		public void initializeAggregate() {
+			value = null;
+		}
+
+		@Override
+		public void aggregate(U val) {
+			if (value != null) {
+				int cmp = value.compareTo(val);
+				if (cmp < 0) {
+					value.setValue(val);
+				}
+			} else {
+				value = val.copy();
+			}
+		}
+
+		@Override
+		public U getAggregate() {
+			return value;
+		}
 	}
 	
 	// --------------------------------------------------------------------------------------------
@@ -58,7 +97,11 @@ public class MaxAggregationFunction<T extends Comparable<T>> extends Aggregation
 		@Override
 		public <T> AggregationFunction<T> createAggregationFunction(Class<T> type) {
 			if (Comparable.class.isAssignableFrom(type)) {
-				return (AggregationFunction<T>) new MaxAggregationFunction();
+				if (ResettableValue.class.isAssignableFrom(type) & CopyableValue.class.isAssignableFrom(type)) {
+					return (AggregationFunction<T>) new MutableMaxAgg();
+				} else {
+					return (AggregationFunction<T>) new ImmutableMaxAgg();
+				}
 			} else {
 				throw new UnsupportedAggregationTypeException("The type " + type.getName() + 
 					" is not supported for maximum aggregation. " +
