eec2d151165d26176f96535cad184e8f85dea318
Stephan Ewen <sewen@apache.org>
2015-10-05 15:57:49 +0200

    [hotfix] Correct name of HDFS tests from 'org.apache.flink.tachyon' to 'org.apache.flink.hdfstests'

--- flink-staging/flink-fs-tests/src/test/java/org/apache/flink/hdfstests/FileStateHandleTest.java
+++ flink-staging/flink-fs-tests/src/test/java/org/apache/flink/hdfstests/FileStateHandleTest.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.apache.flink.tachyon;
+package org.apache.flink.hdfstests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
