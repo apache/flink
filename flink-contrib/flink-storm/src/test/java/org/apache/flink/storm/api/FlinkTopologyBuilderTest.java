1d7ddd762f4d23826b34ed7725cdb2e784d2b630
Stephan Ewen <sewen@apache.org>
2015-10-16 12:43:35 +0200

    [temp] Deactivate Storm Topology Builder Test for split streams

--- flink-contrib/flink-storm/src/test/java/org/apache/flink/storm/api/FlinkTopologyBuilderTest.java
+++ flink-contrib/flink-storm/src/test/java/org/apache/flink/storm/api/FlinkTopologyBuilderTest.java
@@ -21,6 +21,7 @@ import org.apache.flink.storm.util.TestDummyBolt;
 import org.apache.flink.storm.util.TestDummySpout;
 import org.apache.flink.storm.util.TestSink;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import backtype.storm.tuple.Fields;
@@ -53,6 +54,7 @@ public class FlinkTopologyBuilderTest {
 	}
 
 	@Test
+	@Ignore
 	public void testFieldsGroupingOnMultipleSpoutOutputStreams() {
 		FlinkTopologyBuilder flinkBuilder = new FlinkTopologyBuilder();
 
@@ -64,6 +66,7 @@ public class FlinkTopologyBuilderTest {
 	}
 
 	@Test
+	@Ignore
 	public void testFieldsGroupingOnMultipleBoltOutputStreams() {
 		FlinkTopologyBuilder flinkBuilder = new FlinkTopologyBuilder();
 
