47f5da6fd5705a3f7662a118504085616d949512
Till Rohrmann <trohrmann@apache.org>
2015-10-21 15:35:02 +0200

    [FLINK-2878] [webmonitor] Fix unexpected leader address pattern
    The HandlerRedirectUtils.getRedirectAddress decides whether the retrieved leader address is equal to the local job manager address. The local job manager address is, however, in the form akka.tcp://flink@url/user/jobmanager whereas the leader address can be akka://flink/user/jobmanager if the local job manager is the current leader. Such a case produced a warning which is not correct. This PR checks for the local job manager address and signals that no redirection has to be done if it receives akka://flink/user/jobmanager.
    
    Add test for HandlerRedirectUtils
    
    This closes #1280.

--- flink-runtime/src/test/java/org/apache/flink/runtime/instance/DummyActorGateway.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/instance/DummyActorGateway.java
@@ -30,6 +30,17 @@ import java.util.UUID;
  */
 public class DummyActorGateway implements ActorGateway {
 	public static final DummyActorGateway INSTANCE = new DummyActorGateway();
+	private static final long serialVersionUID = -833861606769367952L;
+
+	private final String path;
+
+	public DummyActorGateway() {
+		this("DummyActorGateway");
+	}
+
+	public DummyActorGateway(String path) {
+		this.path = path;
+	}
 
 	@Override
 	public Future<Object> ask(Object message, FiniteDuration timeout) {
@@ -52,7 +63,7 @@ public class DummyActorGateway implements ActorGateway {
 
 	@Override
 	public String path() {
-		return "DummyInstanceGateway";
+		return path;
 	}
 
 	@Override
