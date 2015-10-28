5618cf7ff65c972dc33c77ba953966224e8c2a1e
Till Rohrmann <trohrmann@apache.org>
2015-10-28 18:40:41 +0100

    [FLINK-2800] [kryo] Fix Kryo serialization to clear buffered data
    The Kryo serializer uses Kryo's Output class to buffer individual write operations before
    it is written to the underlying output stream. This Output class is flushed by Flink's
    KryoSerializer upon finishing its serialize call. However, in case of an exception when
    flushing the Output, the buffered data is kept in the buffer. Since Flink uses EOFExceptions
    to mark that an underlying buffer is full and has to be spilled, for example, it can happen
    that the record triggering the spilling is written twice after it is rewritten. The reason
    is that Kryo's Output buffer still contains the serialization data of the failed attempt which
    is also flushed to the emptied output stream.
    
    This duplication of records can lead to corrupted data which eventually let's the Flink program
    crash. The problem is solved by clearing Kryo's Output when the flush operation was not successful.

--- flink-java/src/main/java/org/apache/flink/api/java/typeutils/runtime/kryo/KryoSerializer.java
+++ flink-java/src/main/java/org/apache/flink/api/java/typeutils/runtime/kryo/KryoSerializer.java
@@ -58,7 +58,7 @@ import java.util.Objects;
  * @param <T> The type to be serialized.
  */
 public class KryoSerializer<T> extends TypeSerializer<T> {
-	
+
 	private static final long serialVersionUID = 3L;
 
 	// ------------------------------------------------------------------------
@@ -82,7 +82,7 @@ public class KryoSerializer<T> extends TypeSerializer<T> {
 	
 	private transient Input input;
 	private transient Output output;
-	
+
 	// ------------------------------------------------------------------------
 
 	public KryoSerializer(Class<T> type, ExecutionConfig executionConfig){
@@ -182,11 +182,22 @@ public class KryoSerializer<T> extends TypeSerializer<T> {
 			previousOut = target;
 		}
 
+		// Sanity check: Make sure that the output is cleared/has been flushed by the last call
+		// otherwise data might be written multiple times in case of a previous EOFException
+		if (output.position() != 0) {
+			throw new IllegalStateException("The Kryo Output still contains data from a previous " +
+				"serialize call. It has to be flushed or cleared at the end of the serialize call.");
+		}
+
 		try {
 			kryo.writeClassAndObject(output, record);
 			output.flush();
 		}
 		catch (KryoException ke) {
+			// make sure that the Kryo output buffer is cleared in case that we can recover from
+			// the exception (e.g. EOFException which denotes buffer full)
+			output.clear();
+
 			Throwable cause = ke.getCause();
 			if (cause instanceof EOFException) {
 				throw (EOFException) cause;
@@ -212,7 +223,7 @@ public class KryoSerializer<T> extends TypeSerializer<T> {
 		} catch (KryoException ke) {
 			Throwable cause = ke.getCause();
 
-			if(cause instanceof EOFException) {
+			if (cause instanceof EOFException) {
 				throw (EOFException) cause;
 			} else {
 				throw ke;
