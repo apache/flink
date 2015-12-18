698e4eb8dee63751a53b8202436b59479b25a344
Ufuk Celebi <uce@apache.org>
2015-12-18 14:49:19 +0100

    [FLINK-3185] [tests] Don't swallow test failure Exception

--- flink-tests/src/test/java/org/apache/flink/test/recovery/JobManagerCheckpointRecoveryITCase.java
+++ flink-tests/src/test/java/org/apache/flink/test/recovery/JobManagerCheckpointRecoveryITCase.java
@@ -245,7 +245,7 @@ public class JobManagerCheckpointRecoveryITCase extends TestLogger {
 				jobManagerProcess[1].printProcessLog();
 			}
 
-			t.printStackTrace();
+			throw t;
 		}
 		finally {
 			if (jobManagerProcess[0] != null) {
