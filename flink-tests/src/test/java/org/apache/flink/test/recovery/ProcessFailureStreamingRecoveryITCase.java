senorcarbone/incubator-flink

[streaming] Allow multiple operator states + stateful function test rework

flink-tests/src/test/java/org/apache/flink/test/recovery/ProcessFailureStreamingRecoveryITCase.java
@@ -176,7 +176,7 @@ public class ProcessFailureStreamingRecoveryITCase extends AbstractProcessFailur
 			stepSize = getRuntimeContext().getNumberOfParallelSubtasks();
 			congruence = getRuntimeContext().getIndexOfThisSubtask();
 			toCollect = (end % stepSize > congruence) ? (end / stepSize + 1) : (end / stepSize);
-			collected = getRuntimeContext().getOperatorState(0L);
+			collected = getRuntimeContext().getOperatorState("offset",0L);
 			proceedFile = new File(coordinateDir, PROCEED_MARKER_FILE);
 			checkForProceedFile = true;
 		}

apache/flink

flink-tests/src/test/java/org/apache/flink/test/recovery/ProcessFailureStreamingRecoveryITCase.java
@@ -154,7 +154,7 @@ public class ProcessFailureStreamingRecoveryITCase extends AbstractProcessFailur
 		
 		@Override
 		public void open(Configuration conf) {
-			collected = getRuntimeContext().getOperatorState(0L);
+			collected = getRuntimeContext().getOperatorState("count", 0L);
 		}
 
 		@Override
@@ -199,7 +199,7 @@ public class ProcessFailureStreamingRecoveryITCase extends AbstractProcessFailur
 			stepSize = getRuntimeContext().getNumberOfParallelSubtasks();
 			congruence = getRuntimeContext().getIndexOfThisSubtask();
 			toCollect = (end % stepSize > congruence) ? (end / stepSize + 1) : (end / stepSize);
-			collected = getRuntimeContext().getOperatorState(0L);
+			collected = getRuntimeContext().getOperatorState("count", 0L);
 		}
 
 		@Override