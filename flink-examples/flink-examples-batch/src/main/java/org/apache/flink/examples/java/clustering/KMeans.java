63bb710babcd1b0fb8d7a5253e442612ef3a86e6
Stefano Baghino <stefano.baghino@radicalbit.io>
2016-01-21 18:08:47 +0100

    [FLINK-2021] Removing static members, ditching RequiredParameters to improve readability

--- flink-examples/flink-examples-batch/src/main/java/org/apache/flink/examples/java/clustering/KMeans.java
+++ flink-examples/flink-examples-batch/src/main/java/org/apache/flink/examples/java/clustering/KMeans.java
@@ -27,10 +27,7 @@ import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.java.functions.FunctionAnnotation.ForwardedFields;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
-import org.apache.flink.api.java.utils.Option;
 import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.api.java.utils.RequiredParameters;
-import org.apache.flink.api.java.utils.RequiredParametersException;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.examples.java.clustering.util.KMeansData;
 import org.apache.flink.api.java.DataSet;
@@ -39,23 +36,23 @@ import org.apache.flink.api.java.operators.IterativeDataSet;
 
 /**
  * This example implements a basic K-Means clustering algorithm.
- * 
+ *
  * <p>
  * K-Means is an iterative clustering algorithm and works as follows:<br>
  * K-Means is given a set of data points to be clustered and an initial set of <i>K</i> cluster centers.
  * In each iteration, the algorithm computes the distance of each data point to each cluster center.
  * Each point is assigned to the cluster center which is closest to it.
  * Subsequently, each cluster center is moved to the center (<i>mean</i>) of all points that have been assigned to it.
- * The moved cluster centers are fed into the next iteration. 
- * The algorithm terminates after a fixed number of iterations (as in this implementation) 
+ * The moved cluster centers are fed into the next iteration.
+ * The algorithm terminates after a fixed number of iterations (as in this implementation)
  * or if cluster centers do not (significantly) move in an iteration.<br>
  * This is the Wikipedia entry for the <a href="http://en.wikipedia.org/wiki/K-means_clustering">K-Means Clustering algorithm</a>.
- * 
+ *
  * <p>
  * This implementation works on two-dimensional data points. <br>
- * It computes an assignment of data points to cluster centers, i.e., 
+ * It computes an assignment of data points to cluster centers, i.e.,
  * each data point is annotated with the id of the final cluster (center) it belongs to.
- * 
+ *
  * <p>
  * Input files are plain text files and must be formatted as follows:
  * <ul>
@@ -65,11 +62,11 @@ import org.apache.flink.api.java.operators.IterativeDataSet;
  * <li>Cluster centers are represented by an integer id and a point value.<br>
  * For example <code>"1 6.2 3.2\n2 2.9 5.7\n"</code> gives two centers (id=1, x=6.2, y=3.2) and (id=2, x=2.9, y=5.7).
  * </ul>
- * 
+ *
  * <p>
- * Usage: <code>KMeans &lt;points path&gt; &lt;centers path&gt; &lt;result path&gt; &lt;num iterations&gt;</code><br>
- * If no parameters are provided, the program is run with default data from {@link org.apache.flink.examples.java.clustering.util.KMeansData} and 10 iterations. 
- * 
+ * Usage: <code>KMeans --points --centroids --output --iterations</code><br>
+ * If no parameters are provided, the program is run with default data from {@link org.apache.flink.examples.java.clustering.util.KMeansData} and 10 iterations.
+ *
  * <p>
  * This example shows how to use:
  * <ul>
@@ -80,56 +77,52 @@ import org.apache.flink.api.java.operators.IterativeDataSet;
  */
 @SuppressWarnings("serial")
 public class KMeans {
-	
-	// *************************************************************************
-	//     PROGRAM
-	// *************************************************************************
-	
+
 	public static void main(String[] args) throws Exception {
 
 		// Checking input parameters
 		final ParameterTool params = ParameterTool.fromArgs(args);
-		final RequiredParameters requiredParameters = new RequiredParameters();
-		boolean paramsOk = false;
-
-		requiredParameters.add(POINTS_PATH_OPTION);
-		requiredParameters.add(CENTERS_PATH_OPTION);
-		requiredParameters.add(OUTPUT_PATH_OPTION);
-		requiredParameters.add(NUM_ITERATIONS_OPTION);
-
-		try {
-			requiredParameters.applyTo(params);
-			pointsPath = params.get(POINTS_PATH_OPTION.getName());
-			centersPath = params.get(CENTERS_PATH_OPTION.getName());
-			outputPath = params.get(OUTPUT_PATH_OPTION.getName());
-			numIterations = params.getInt(NUM_ITERATIONS_OPTION.getName());
-			fileOutput = true;
-			paramsOk = true;
-		} catch (RequiredParametersException e) {
-			if (params.getNumberOfParameters() == 0) {
-				System.out.println("Executing K-Means example with default parameters and built-in default data.");
-				System.out.println("  Provide parameters to read input data from files.");
-				System.out.println("  See the documentation for the correct format of input files.");
-				System.out.println("  We provide a data generator to create synthetic input files for this program.");
-				paramsOk = true;
-			}
-			System.out.println(requiredParameters.getHelp(e.getMissingArguments()));
+		if (params.getNumberOfParameters() < 4) {
+			System.out.println("Executing K-Means example with default parameters and built-in default data.");
+			System.out.println("  Provide parameters to read input data from files.");
+			System.out.println("  See the documentation for the correct format of input files.");
+			System.out.println("  We provide a data generator to create synthetic input files for this program.");
+			System.out.println("  Usage: KMeans --points --centroids --output --iterations");
 		}
 
-		if(!paramsOk) {
-			return;
-		}
-	
 		// set up execution environment
 		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-		
-		// get input data
-		DataSet<Point> points = getPointDataSet(env);
-		DataSet<Centroid> centroids = getCentroidDataSet(env);
-		
+		env.getConfig().setGlobalJobParameters(params); // make parameters available in the web interface
+
+		// get input data:
+		// points
+		DataSet<Point> points;
+		if(params.has("points")) {
+			// read points from CSV file
+			points = env.readCsvFile(params.get("points"))
+				.fieldDelimiter(" ")
+				.includeFields(true, true)
+				.types(Double.class, Double.class)
+				.map(new TuplePointConverter());
+		} else {
+			points = KMeansData.getDefaultPointDataSet(env);
+		}
+
+		// centroids
+		DataSet<Centroid> centroids;
+		if(params.has("centroids")) {
+			centroids = env.readCsvFile(params.get("centroids"))
+				.fieldDelimiter(" ")
+				.includeFields(true, true, true)
+				.types(Integer.class, Double.class, Double.class)
+				.map(new TupleCentroidConverter());
+		} else {
+			centroids = KMeansData.getDefaultCentroidDataSet(env);
+		}
+
 		// set number of bulk iterations for KMeans algorithm
-		IterativeDataSet<Centroid> loop = centroids.iterate(numIterations);
-		
+		IterativeDataSet<Centroid> loop = centroids.iterate(params.getInt("iterations", 10));
+
 		DataSet<Centroid> newCentroids = points
 			// compute closest centroid for each point
 			.map(new SelectNearestCenter()).withBroadcastSet(loop, "centroids")
@@ -138,99 +131,98 @@ public class KMeans {
 			.groupBy(0).reduce(new CentroidAccumulator())
 			// compute new centroids from point counts and coordinate sums
 			.map(new CentroidAverager());
-		
+
 		// feed new centroids back into next iteration
 		DataSet<Centroid> finalCentroids = loop.closeWith(newCentroids);
-		
+
 		DataSet<Tuple2<Integer, Point>> clusteredPoints = points
-				// assign points to final clusters
-				.map(new SelectNearestCenter()).withBroadcastSet(finalCentroids, "centroids");
-		
+			// assign points to final clusters
+			.map(new SelectNearestCenter()).withBroadcastSet(finalCentroids, "centroids");
+
 		// emit result
-		if (fileOutput) {
-			clusteredPoints.writeAsCsv(outputPath, "\n", " ");
+		if (params.has("output")) {
+			clusteredPoints.writeAsCsv(params.get("output"), "\n", " ");
 
 			// since file sinks are lazy, we trigger the execution explicitly
 			env.execute("KMeans Example");
-		}
-		else {
+		} else {
 			clusteredPoints.print();
 		}
 	}
-	
+
 	// *************************************************************************
 	//     DATA TYPES
 	// *************************************************************************
-	
+
 	/**
 	 * A simple two-dimensional point.
 	 */
 	public static class Point implements Serializable {
-		
+
 		public double x, y;
-		
+
 		public Point() {}
 
 		public Point(double x, double y) {
 			this.x = x;
 			this.y = y;
 		}
-		
+
 		public Point add(Point other) {
 			x += other.x;
 			y += other.y;
 			return this;
 		}
-		
+
 		public Point div(long val) {
 			x /= val;
 			y /= val;
 			return this;
 		}
-		
+
 		public double euclideanDistance(Point other) {
 			return Math.sqrt((x-other.x)*(x-other.x) + (y-other.y)*(y-other.y));
 		}
-		
+
 		public void clear() {
 			x = y = 0.0;
 		}
-		
+
 		@Override
 		public String toString() {
 			return x + " " + y;
 		}
 	}
-	
+
 	/**
-	 * A simple two-dimensional centroid, basically a point with an ID. 
+	 * A simple two-dimensional centroid, basically a point with an ID.
 	 */
 	public static class Centroid extends Point {
-		
+
 		public int id;
-		
+
 		public Centroid() {}
-		
+
 		public Centroid(int id, double x, double y) {
 			super(x,y);
 			this.id = id;
 		}
-		
+
 		public Centroid(int id, Point p) {
 			super(p.x, p.y);
 			this.id = id;
 		}
-		
+
 		@Override
 		public String toString() {
 			return id + " " + super.toString();
 		}
 	}
-	
+
 	// *************************************************************************
 	//     USER FUNCTIONS
 	// *************************************************************************
-	
+
 	/** Converts a {@code Tuple2<Double,Double>} into a Point. */
 	@ForwardedFields("0->x; 1->y")
 	public static final class TuplePointConverter implements MapFunction<Tuple2<Double, Double>, Point> {
@@ -240,7 +232,7 @@ public class KMeans {
 			return new Point(t.f0, t.f1);
 		}
 	}
-	
+
 	/** Converts a {@code Tuple3<Integer, Double,Double>} into a Centroid. */
 	@ForwardedFields("0->id; 1->x; 2->y")
 	public static final class TupleCentroidConverter implements MapFunction<Tuple3<Integer, Double, Double>, Centroid> {
@@ -250,7 +242,7 @@ public class KMeans {
 			return new Centroid(t.f0, t.f1, t.f2);
 		}
 	}
-	
+
 	/** Determines the closest cluster center for a data point. */
 	@ForwardedFields("*->1")
 	public static final class SelectNearestCenter extends RichMapFunction<Point, Tuple2<Integer, Point>> {
@@ -261,19 +253,19 @@ public class KMeans {
 		public void open(Configuration parameters) throws Exception {
 			this.centroids = getRuntimeContext().getBroadcastVariable("centroids");
 		}
-		
+
 		@Override
 		public Tuple2<Integer, Point> map(Point p) throws Exception {
-			
+
 			double minDistance = Double.MAX_VALUE;
 			int closestCentroidId = -1;
-			
+
 			// check all cluster centers
 			for (Centroid centroid : centroids) {
 				// compute distance
 				double distance = p.euclideanDistance(centroid);
-				
-				// update nearest cluster if necessary 
+
+				// update nearest cluster if necessary
 				if (distance < minDistance) {
 					minDistance = distance;
 					closestCentroidId = centroid.id;
@@ -281,30 +273,30 @@ public class KMeans {
 			}
 
 			// emit a new record with the center id and the data point.
-			return new Tuple2<Integer, Point>(closestCentroidId, p);
+			return new Tuple2<>(closestCentroidId, p);
 		}
 	}
-	
+
 	/** Appends a count variable to the tuple. */
 	@ForwardedFields("f0;f1")
 	public static final class CountAppender implements MapFunction<Tuple2<Integer, Point>, Tuple3<Integer, Point, Long>> {
 
 		@Override
 		public Tuple3<Integer, Point, Long> map(Tuple2<Integer, Point> t) {
-			return new Tuple3<Integer, Point, Long>(t.f0, t.f1, 1L);
-		} 
+			return new Tuple3<>(t.f0, t.f1, 1L);
+		}
 	}
-	
+
 	/** Sums and counts point coordinates. */
 	@ForwardedFields("0")
 	public static final class CentroidAccumulator implements ReduceFunction<Tuple3<Integer, Point, Long>> {
 
 		@Override
 		public Tuple3<Integer, Point, Long> reduce(Tuple3<Integer, Point, Long> val1, Tuple3<Integer, Point, Long> val2) {
-			return new Tuple3<Integer, Point, Long>(val1.f0, val1.f1.add(val2.f1), val1.f2 + val2.f2);
+			return new Tuple3<>(val1.f0, val1.f1.add(val2.f1), val1.f2 + val2.f2);
 		}
 	}
-	
+
 	/** Computes new centroid from coordinate sum and count of points. */
 	@ForwardedFields("0->id")
 	public static final class CentroidAverager implements MapFunction<Tuple3<Integer, Point, Long>, Centroid> {
@@ -312,50 +304,6 @@ public class KMeans {
 		@Override
 		public Centroid map(Tuple3<Integer, Point, Long> value) {
 			return new Centroid(value.f0, value.f1.div(value.f2));
-		}
-	}
-	
-	// *************************************************************************
-	//     UTIL METHODS
-	// *************************************************************************
-	
-	private static boolean fileOutput = false;
-	private static String pointsPath = null;
-	private static String centersPath = null;
-	private static String outputPath = null;
-	private static int numIterations = 10;
-
-	private static final Option POINTS_PATH_OPTION =
-		new Option("points").alt("P").help("The path to the input points");
-	private static final Option CENTERS_PATH_OPTION =
-		new Option("centroids").alt("C").help("The path to the input centroids");
-	private static final Option OUTPUT_PATH_OPTION =
-		new Option("output").alt("O").help("The path where the output will be written");
-	private static final Option NUM_ITERATIONS_OPTION =
-		new Option("iterations").alt("I").help("The number of iteration performed by the K-Means algorithm");
-
-	private static DataSet<Point> getPointDataSet(ExecutionEnvironment env) {
-		if(fileOutput) {
-			// read points from CSV file
-			return env.readCsvFile(pointsPath)
-						.fieldDelimiter(" ")
-						.includeFields(true, true)
-						.types(Double.class, Double.class)
-						.map(new TuplePointConverter());
-		} else {
-			return KMeansData.getDefaultPointDataSet(env);
-		}
-	}
-	
-	private static DataSet<Centroid> getCentroidDataSet(ExecutionEnvironment env) {
-		if(fileOutput) {
-			return env.readCsvFile(centersPath)
-						.fieldDelimiter(" ")
-						.includeFields(true, true, true)
-						.types(Integer.class, Double.class, Double.class)
-						.map(new TupleCentroidConverter());
-		} else {
-			return KMeansData.getDefaultCentroidDataSet(env);
 		}
 	}
 }
