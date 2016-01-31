dd74aeb6d501361d052798d6f461d167e0472686
f-sander <fsander@mailbox.tu-berlin.de>
2016-01-31 16:22:01 +0100

    [FLINK-3128] Add Isotonic Regression To ML Library

--- flink-libraries/flink-ml/src/main/scala/org/apache/flink/ml/regression/IsotonicRegression.scala
+++ flink-libraries/flink-ml/src/main/scala/org/apache/flink/ml/regression/IsotonicRegression.scala
@@ -47,8 +47,7 @@ case class IsotonicRegressionModel(boundaries: Array[Double], predictions: Array
   *
   * @see [[http://en.wikipedia.org/wiki/Isotonic_regression Isotonic regression (Wikipedia)]]
   *
-  * This is a port from the implementation in Apache Spark.
-  *
+  *      This is a port from the implementation in Apache Spark.
   * @example
   * {{{
   *             val ir = IsotonicRegression()
@@ -70,214 +69,214 @@ case class IsotonicRegressionModel(boundaries: Array[Double], predictions: Array
   */
 class IsotonicRegression extends Predictor[IsotonicRegression] {
 
-    var isotonic = true
+  var isotonic = true
 
-    var model: Option[DataSet[IsotonicRegressionModel]] = None
+  var model: Option[DataSet[IsotonicRegressionModel]] = None
 
-    def setIsotonic(isotonic: Boolean): this.type = {
-        this.isotonic = isotonic
-        this
-    }
+  def setIsotonic(isotonic: Boolean): this.type = {
+    this.isotonic = isotonic
+    this
+  }
 
 }
 
 object IsotonicRegression {
 
-    // ====================================== Parameters ===========================================
+  // ====================================== Parameters ===========================================
 
-    case object Isotonic extends Parameter[Boolean] {
-        val defaultValue = Some(true)
-    }
+  case object Isotonic extends Parameter[Boolean] {
+    val defaultValue = Some(true)
+  }
 
-    // ======================================== Factory methods ====================================
+  // ======================================== Factory methods ====================================
 
-    def apply(): IsotonicRegression = {
-        new IsotonicRegression()
-    }
+  def apply(): IsotonicRegression = {
+    new IsotonicRegression()
+  }
 
-    // ====================================== Operations ===========================================
+  // ====================================== Operations ===========================================
 
-    class AdjacentPoolViolatersMapper extends MapFunction[Array[(Double, Double, Double)], Array[
-        (Double, Double, Double)]] {
+  class AdjacentPoolViolatersMapper extends MapFunction[Array[(Double, Double, Double)], Array[
+    (Double, Double, Double)]] {
 
-        /**
-          * Performs a pool adjacent violators algorithm (PAV).
-          * Uses approach with single processing of data where violators
-          * in previously processed data created by pooling are fixed immediately.
-          * Uses optimization of discovering monotonicity violating sequences (blocks).
-          *
-          * @param input Input data of tuples (label, feature, weight).
-          * @return Result tuples (label, feature, weight) where labels were updated
-          *         to form a monotone sequence as per isotonic regression definition.
-          */
-        override def map(input: Array[(Double, Double, Double)]):
-        Array[(Double, Double, Double)] = {
-            if (input.isEmpty) {
-                return Array.empty
-            }
+    /**
+      * Performs a pool adjacent violators algorithm (PAV).
+      * Uses approach with single processing of data where violators
+      * in previously processed data created by pooling are fixed immediately.
+      * Uses optimization of discovering monotonicity violating sequences (blocks).
+      *
+      * @param input Input data of tuples (label, feature, weight).
+      * @return Result tuples (label, feature, weight) where labels were updated
+      *         to form a monotone sequence as per isotonic regression definition.
+      */
+    override def map(input: Array[(Double, Double, Double)]):
+    Array[(Double, Double, Double)] = {
+      if (input.isEmpty) {
+        return Array.empty
+      }
 
-            // Pools sub array within given bounds assigning weighted average value to all
-            // elements.
-            def pool(input: Array[(Double, Double, Double)], start: Int, end: Int): Unit = {
-                val poolSubArray = input.slice(start, end + 1)
+      // Pools sub array within given bounds assigning weighted average value to all
+      // elements.
+      def pool(input: Array[(Double, Double, Double)], start: Int, end: Int): Unit = {
+        val poolSubArray = input.slice(start, end + 1)
 
-                val weightedSum = poolSubArray.map(lp => lp._1 * lp._3).sum
-                val weight = poolSubArray.map(_._3).sum
+        val weightedSum = poolSubArray.map(lp => lp._1 * lp._3).sum
+        val weight = poolSubArray.map(_._3).sum
 
-                var i = start
-                while (i <= end) {
-                    input(i) = (weightedSum / weight, input(i)._2, input(i)._3)
-                    i = i + 1
-                }
-            }
-
-            var i = 0
-            val len = input.length
-            while (i < len) {
-                var j = i
-
-                // Find monotonicity violating sequence, if any.
-                while (j < len - 1 && input(j)._1 > input(j + 1)._1) {
-                    j = j + 1
-                }
-
-                // If monotonicity was not violated, move to next data point.
-                if (i == j) {
-                    i = i + 1
-                } else {
-                    // Otherwise pool the violating sequence
-                    // and check if pooling caused monotonicity violation in previously processed
-                    // points.
-                    while (i >= 0 && input(i)._1 > input(i + 1)._1) {
-                        pool(input, i, j)
-                        i = i - 1
-                    }
-
-                    i = j
-                }
-            }
-            // For points having the same prediction, we only keep two boundary points.
-            val compressed = ArrayBuffer.empty[(Double, Double, Double)]
-
-            var (curLabel, curFeature, curWeight) = input.head
-            var rightBound = curFeature
-            def merge(): Unit = {
-                compressed += ((curLabel, curFeature, curWeight))
-                if (rightBound > curFeature) {
-                    compressed += ((curLabel, rightBound, 0.0))
-                }
-            }
-            i = 1
-            while (i < input.length) {
-                val (label, feature, weight) = input(i)
-                if (label == curLabel) {
-                    curWeight += weight
-                    rightBound = feature
-                } else {
-                    merge()
-                    curLabel = label
-                    curFeature = feature
-                    curWeight = weight
-                    rightBound = curFeature
-                }
-                i += 1
-            }
-            merge()
-
-            compressed.toArray
+        var i = start
+        while (i <= end) {
+          input(i) = (weightedSum / weight, input(i)._2, input(i)._3)
+          i = i + 1
         }
-    }
+      }
 
+      var i = 0
+      val len = input.length
+      while (i < len) {
+        var j = i
 
-    implicit val fitIR = new FitOperation[IsotonicRegression, (Double, Double, Double)] {
-
-        override def fit(instance: IsotonicRegression,
-                         fitParameters: ParameterMap,
-                         input: DataSet[(Double, Double, Double)]): Unit = {
-
-            val isotonic = instance.isotonic
-
-            val preprocessedInput = if (isotonic) {
-                input
-            } else {
-                input.map(x => (-x._1, x._2, x._3))
-            }
-
-            val parallelStepResult = preprocessedInput
-                .partitionByRange(1)
-                .mapPartition(partition => {
-                    val buffer = new ArrayBuffer[(Double, Double, Double)]
-                    buffer ++= partition
-                    Seq(buffer.sortBy(x => (x._2, x._1)).toArray)
-                })
-                .map(new AdjacentPoolViolatersMapper)
-                .setParallelism(1)
-                .mapPartition(partitions => {
-                    val buffer = new ArrayBuffer[(Double, Double, Double)]
-                    for (partition <- partitions) {
-                        buffer ++= partition
-                    }
-                    Seq(buffer.sortBy(x => (x._2, x._1)).toArray)
-                })
-                .map(new AdjacentPoolViolatersMapper)
-                .map(arr => {
-                    val boundaries = new ArrayBuffer[Double]
-                    val predictions = new ArrayBuffer[Double]
-                    for (x <- arr) {
-                        predictions += (if (isotonic) x._1 else -x._1)
-                        boundaries += x._2
-                    }
-                    IsotonicRegressionModel(boundaries.toArray, predictions.toArray)
-                })
-
-            instance.model = Some(parallelStepResult)
+        // Find monotonicity violating sequence, if any.
+        while (j < len - 1 && input(j)._1 > input(j + 1)._1) {
+          j = j + 1
         }
-    }
 
-    implicit val predictIR =
-        new PredictOperation[IsotonicRegression, IsotonicRegressionModel, Double, Double] {
+        // If monotonicity was not violated, move to next data point.
+        if (i == j) {
+          i = i + 1
+        } else {
+          // Otherwise pool the violating sequence
+          // and check if pooling caused monotonicity violation in previously processed
+          // points.
+          while (i >= 0 && input(i)._1 > input(i + 1)._1) {
+            pool(input, i, j)
+            i = i - 1
+          }
 
-            override def getModel(self: IsotonicRegression, predictParameters: ParameterMap):
-            DataSet[IsotonicRegressionModel] =
-                self.model match {
-                    case Some(model) => model
-                    case None => throw new RuntimeException("The IsotonicRegression has not " +
-                        "been fitted to the data. This is necessary to learn the model.")
-                }
-
-            override def predict(value: Double, model: IsotonicRegressionModel): Double = {
-
-                def linearInterpolation(x1: Double, y1: Double, x2: Double, y2: Double, x:
-                Double):
-                Double = {
-                    y1 + (y2 - y1) * (x - x1) / (x2 - x1)
-                }
-
-                val boundaries = model.boundaries
-                val predictions = model.predictions
-
-                val foundIndex = binarySearch(boundaries, value)
-                val insertIndex = -foundIndex - 1
-
-                // Find if the index was lower than all values,
-                // higher than all values, in between two values or exact match.
-                if (insertIndex == 0) {
-                    predictions.head
-                } else if (insertIndex == boundaries.length) {
-                    predictions.last
-                } else if (foundIndex < 0) {
-                    linearInterpolation(
-                        boundaries(insertIndex - 1),
-                        predictions(insertIndex - 1),
-                        boundaries(insertIndex),
-                        predictions(insertIndex),
-                        value)
-                } else {
-                    predictions(foundIndex)
-                }
-
-            }
+          i = j
         }
+      }
+      // For points having the same prediction, we only keep two boundary points.
+      val compressed = ArrayBuffer.empty[(Double, Double, Double)]
+
+      var (curLabel, curFeature, curWeight) = input.head
+      var rightBound = curFeature
+      def merge(): Unit = {
+        compressed += ((curLabel, curFeature, curWeight))
+        if (rightBound > curFeature) {
+          compressed += ((curLabel, rightBound, 0.0))
+        }
+      }
+      i = 1
+      while (i < input.length) {
+        val (label, feature, weight) = input(i)
+        if (label == curLabel) {
+          curWeight += weight
+          rightBound = feature
+        } else {
+          merge()
+          curLabel = label
+          curFeature = feature
+          curWeight = weight
+          rightBound = curFeature
+        }
+        i += 1
+      }
+      merge()
+
+      compressed.toArray
+    }
+  }
+
+
+  implicit val fitIR = new FitOperation[IsotonicRegression, (Double, Double, Double)] {
+
+    override def fit(instance: IsotonicRegression,
+                     fitParameters: ParameterMap,
+                     input: DataSet[(Double, Double, Double)]): Unit = {
+
+      val isotonic = instance.isotonic
+
+      val preprocessedInput = if (isotonic) {
+        input
+      } else {
+        input.map(x => (-x._1, x._2, x._3))
+      }
+
+      val parallelStepResult = preprocessedInput
+        .partitionByRange(1)
+        .mapPartition(partition => {
+          val buffer = new ArrayBuffer[(Double, Double, Double)]
+          buffer ++= partition
+          Seq(buffer.sortBy(x => (x._2, x._1)).toArray)
+        })
+        .map(new AdjacentPoolViolatersMapper)
+        .setParallelism(1)
+        .mapPartition(partitions => {
+          val buffer = new ArrayBuffer[(Double, Double, Double)]
+          for (partition <- partitions) {
+            buffer ++= partition
+          }
+          Seq(buffer.sortBy(x => (x._2, x._1)).toArray)
+        })
+        .map(new AdjacentPoolViolatersMapper)
+        .map(arr => {
+          val boundaries = new ArrayBuffer[Double]
+          val predictions = new ArrayBuffer[Double]
+          for (x <- arr) {
+            predictions += (if (isotonic) x._1 else -x._1)
+            boundaries += x._2
+          }
+          IsotonicRegressionModel(boundaries.toArray, predictions.toArray)
+        })
+
+      instance.model = Some(parallelStepResult)
+    }
+  }
+
+  implicit val predictIR =
+    new PredictOperation[IsotonicRegression, IsotonicRegressionModel, Double, Double] {
+
+      override def getModel(self: IsotonicRegression, predictParameters: ParameterMap):
+      DataSet[IsotonicRegressionModel] =
+        self.model match {
+          case Some(model) => model
+          case None => throw new RuntimeException("The IsotonicRegression has not " +
+            "been fitted to the data. This is necessary to learn the model.")
+        }
+
+      override def predict(value: Double, model: IsotonicRegressionModel): Double = {
+
+        def linearInterpolation(x1: Double, y1: Double, x2: Double, y2: Double, x:
+        Double):
+        Double = {
+          y1 + (y2 - y1) * (x - x1) / (x2 - x1)
+        }
+
+        val boundaries = model.boundaries
+        val predictions = model.predictions
+
+        val foundIndex = binarySearch(boundaries, value)
+        val insertIndex = -foundIndex - 1
+
+        // Find if the index was lower than all values,
+        // higher than all values, in between two values or exact match.
+        if (insertIndex == 0) {
+          predictions.head
+        } else if (insertIndex == boundaries.length) {
+          predictions.last
+        } else if (foundIndex < 0) {
+          linearInterpolation(
+            boundaries(insertIndex - 1),
+            predictions(insertIndex - 1),
+            boundaries(insertIndex),
+            predictions(insertIndex),
+            value)
+        } else {
+          predictions(foundIndex)
+        }
+
+      }
+    }
 
 }
 
