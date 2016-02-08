e9c83ea2c36decf02d0ea9c2b76b0fd50606b51b
Stephan Ewen <sewen@apache.org>
2016-02-08 15:08:09 +0100

    [hotfix] Update comments in 'ChainingStrategy' and remove outdated 'FORCE_ALWAYS' constant

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/operators/ChainingStrategy.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/operators/ChainingStrategy.java
@@ -18,23 +18,22 @@
 
 package org.apache.flink.streaming.api.operators;
 
-
 /**
- * Defines the chaining scheme for the operator.
- * By default {@link #ALWAYS} is used, which means operators will be eagerly chained whenever possible.
+ * Defines the chaining scheme for the operator. When an operator is chained to the
+ * predecessor, it means that they run in the same thread. They become one operator
+ * consisting of multiple steps.
+ * 
+ * <p>The default value used by the {@link StreamOperator} is {@link #HEAD}, which means that
+ * the operator is not chained to its predecessor. Most operators override this with
+ * {@link #ALWAYS}, meaning they will be chained to predecessors whenever possible.
  */
 public enum ChainingStrategy {
 
-	/**
-	 * Chaining will happen even if chaining is disabled on the execution environment.
-	 * This should only be used by system-level operators, not operators implemented by users.
-	 */
-	FORCE_ALWAYS,
-
 	/** 
-	 * Operators will be eagerly chained whenever possible, for
-	 * maximal performance. It is generally a good practice to allow maximal
-	 * chaining and increase operator parallelism
+	 * Operators will be eagerly chained whenever possible.
+	 * 
+	 * <p>To optimize performance, it is generally a good practice to allow maximal
+	 * chaining and increase operator parallelism.
 	 */
 	ALWAYS,
 
@@ -42,7 +41,10 @@ public enum ChainingStrategy {
 	 * The operator will not be chained to the preceding or succeeding operators.
 	 */
 	NEVER,
-	
-	
+
+	/**
+	 * The operator will not be chained to the predecessor, but successors may chain to this
+	 * operator.
+	 */
 	HEAD
 }
