88d84d9fa4cf3d7ea0658beddcc5f25723745a11
f-sander <fsander@mailbox.tu-berlin.de>
2016-02-15 14:37:26 +0100

    corrected comment widths

--- flink-libraries/flink-ml/src/main/scala/org/apache/flink/ml/regression/IsotonicRegression.scala
+++ flink-libraries/flink-ml/src/main/scala/org/apache/flink/ml/regression/IsotonicRegression.scala
@@ -36,9 +36,9 @@ case class IsotonicRegressionModel(boundaries: Array[Double], predictions: Array
   *
   * PAV implementation based on:
   * - Isotonic Regression implementation in the scikit-learn Python module for machine learning:
-  *   [[https://github.com/scikit-learn/scikit-learn/blob/c9572494a82b364529374aafca15660a7366e2c4/sklearn/_isotonic.pyx]]
+  *   [[http://bit.ly/242Xnyg]]
   * - Isotonic Regression implementation in Apache Spark:
-  *   [[https://github.com/apache/spark/blob/master/mllib/src/main/scala/org/apache/spark/mllib/regression/IsotonicRegression.scala]]
+  *   [[http://bit.ly/1KNMpGE]]
   *
   * @see [[http://en.wikipedia.org/wiki/Isotonic_regression Isotonic regression (Wikipedia)]]
   *
@@ -67,13 +67,14 @@ class IsotonicRegression extends Predictor[IsotonicRegression] {
   // The IR model that will be created from training data and that will be used for predictiions.
   var model: Option[DataSet[IsotonicRegressionModel]] = None
 
-	/** @param isotonic True if the labels shall be ascending, false if they shall be descending
+  /** @param isotonic True if the labels shall be ascending, false if they shall be descending
     * @return This IsotonicRegression instance
     */
   def setIsotonic(isotonic: Boolean): this.type = {
     this.isotonic = isotonic
     this
   }
+
 }
 
 object IsotonicRegression {
@@ -92,8 +93,9 @@ object IsotonicRegression {
 
   // ====================================== Operations ===========================================
 
-	/**
-    * Map function that can be applied to a data partition and that runs the PAV algorithm on that data.
+  /**
+    * Map function that can be applied to a data partition and that runs the PAV algorithm on that
+    * data.
     */
   class AdjacentPoolViolatersMapper extends MapFunction[Array[(Double, Double, Double)], Array[
     (Double, Double, Double)]] {
@@ -111,7 +113,8 @@ object IsotonicRegression {
         return Array.empty
       }
 
-      var pooled = true // Indicates whether any data points were pooled in the current PAV iteration.
+      // Indicates whether any data points were pooled in the current PAV iteration.
+      var pooled = true
       while (pooled) {
         val n = input.length - 1
         var i = 0
@@ -119,18 +122,19 @@ object IsotonicRegression {
 
         // Iterate over all data points.
         while (i < n) {
-          // Check if the data points following the current one form a monotonically decreasing sequence. After the
-          // while loop, k will contain the index of the last data point of that monotonically decreasing sequence. If
-          // the points following the current one don't form such a sequence, k will still point to the current data
-          // point.
+          // Check if the data points following the current one form a monotonically decreasing
+          // sequence. After the while loop, k will contain the index of the last data point of that
+          // monotonically decreasing sequence. If the points following the current one don't form
+          // such a sequence, k will still point to the current data point.
           var k = i
           while (k < n && input(k)._1 >= input(k + 1)._1) {
             k += 1
           }
 
-          // If the Y values of the current data point and the data point at index k differ, we know that all data
-          // points between the current one and the one at index k form a monotonically decreasing sequence, in which
-          // case we want to combine them into a single pool.
+          // If the Y values of the current data point and the data point at index k differ, we know
+          // that all data points between the current one and the one at index k form a
+          // monotonically decreasing sequence, in which case we want to combine them into a single
+          // pool.
           if (input(i)._1 != input(k)._1) {
             val dataPointCount = k - i
             var weightedSum = 0.0
@@ -141,15 +145,16 @@ object IsotonicRegression {
               sumOfWeights += input(j)._3
             }
 
-            // If the sum of weights is 0, we can't use that for calculating the weighted average. But since we know
-            // that all data points we want to pool have a weight of 0, we can just pretend they all have a weight of 1
-            // by using the number of data points as the sum of weights.
+            // If the sum of weights is 0, we can't use that for calculating the weighted average.
+            // But since we know that all data points we want to pool have a weight of 0, we can
+            // just pretend they all have a weight of 1 by using the number of data points as the
+            // sum of weights.
             if (sumOfWeights == 0) {
               sumOfWeights = dataPointCount
             }
 
-            // Calculate the weighted average and pool all data points between i and k by setting their Y values to that
-            // weighted average.
+            // Calculate the weighted average and pool all data points between i and k by setting
+            // their Y values to that weighted average.
             val weightedAverage = weightedSum / sumOfWeights
             for (j <- i until k + 1) {
               input(j) = (weightedAverage, input(j)._2, input(j)._3)
@@ -158,8 +163,8 @@ object IsotonicRegression {
             pooled = true
           }
 
-          // Continue with the next data point after the current pool or simply with the next data point if the current
-          // one is not part of a pool (in which case k still equals i).
+          // Continue with the next data point after the current pool or simply with the next data
+          // point if the current one is not part of a pool (in which case k still equals i).
           i = k + 1
         }
       }
@@ -173,16 +178,16 @@ object IsotonicRegression {
       while (i < input.length) {
         val (label, feature, weight) = input(i)
         if (label == curLabel) {
-          // If the next Y value is equal to the current Y value, add its weight to the current weight and move the
-          // current right boundary to the right, effectively removing the next data point by merging its information
-          // into the current one.
+          // If the next Y value is equal to the current Y value, add its weight to the current
+          // weight and move the current right boundary to the right, effectively removing the next
+          // data point by merging its information into the current one.
           curWeight += weight
           rightBound = feature
         } else {
-          // If the next data point's Y value differs from the current Y value, add the current data point to the
-          // result. If the current point actually compresses multiple data points (if it contains information of more
-          // than one data point), we have to add another tuple so we don't lose the information on where the right
-          // boundary is.
+          // If the next data point's Y value differs from the current Y value, add the current data
+          // point to the result. If the current point actually compresses multiple data points
+          // (if it contains information of more than one data point), we have to add another tuple
+          // so we don't lose the information on where the right boundary is.
           compressed += ((curLabel, curFeature, curWeight))
           if (rightBound > curFeature) {
             compressed += ((curLabel, rightBound, 0.0))
@@ -223,9 +228,10 @@ object IsotonicRegression {
         input.map(x => (-x._1, x._2, x._3))
       }
 
-      // First, run the PAV algorithm in parallel on separate partitions. Then, collect the combined results of that
-      // parallel step on a single node (parallelism = 1) and run the algorithm again on that combined data. Finally,
-      // create a new IR model from the resulting boundaries and predictions.
+      // First, run the PAV algorithm in parallel on separate partitions. Then, collect the combined
+      // results of that parallel step on a single node (parallelism = 1) and run the algorithm
+      // again on that combined data. Finally, create a new IR model from the resulting boundaries
+      // and predictions.
       val parallelStepResult = preprocessedInput
         .partitionByRange(1)
         .mapPartition(partition => {
@@ -260,9 +266,9 @@ object IsotonicRegression {
   implicit val predictIR =
     new PredictOperation[IsotonicRegression, IsotonicRegressionModel, Double, Double] {
 
-			/**
-        * Returns an instance's IR model, if available, or throw a [[RuntimeException]] if this instance wasn't given
-        * any training data yet that it could have created a model on.
+      /**
+        * Returns an instance's IR model, if available, or throw a [[RuntimeException]] if this
+        * instance wasn't given any training data yet that it could have created a model on.
         * @param self The instance whose model should be returned
         * @param predictParameters The parameters for the prediction
         * @return A DataSet with the model representation as its only element
@@ -276,20 +282,20 @@ object IsotonicRegression {
         }
       }
 
-			/**
-        * Linear interpolation that will be used for predicting the label of a feature that lies between two features
-        * that actually exist in the model.
-				*/
+      /**
+        * Linear interpolation that will be used for predicting the label of a feature that lies
+        * between two features that actually exist in the model.
+        */
       private def linearInterpolation(x1: Double, y1: Double,
                                       x2: Double, y2: Double, x: Double): Double = {
         y1 + (y2 - y1) * (x - x1) / (x2 - x1)
       }
 
-			/**
+      /**
         * Predict the label for a given feature value.
         * @param value The unlabeled example on which we make the prediction
         * @param model The model representation of the prediciton algorithm
-        * @return A label for the provided example of type [[Prediction]]
+        * @return A label for the provided example of type Prediction
         */
       override def predict(value: Double, model: IsotonicRegressionModel): Double = {
         val boundaries = model.boundaries
@@ -299,16 +305,16 @@ object IsotonicRegression {
         val insertIndex = -foundIndex - 1
 
         if (insertIndex == 0) {
-          // If provided feature is lower than the model's first data point, simply return the label of that first model
-          // data point.
+          // If provided feature is lower than the model's first data point, simply return the label
+          // of that first modeldata point.
           predictions.head
         } else if (insertIndex == boundaries.length) {
-          // If provided feature is larger than the model's last data point, simply return the label of that last model
-          // data point.
+          // If provided feature is larger than the model's last data point, simply return the label
+          // of that last model data point.
           predictions.last
         } else if (foundIndex < 0) {
-          // If provided feature lies between two features that exist in the model, return a linear interpolation of
-          // these data points as the prediction.
+          // If provided feature lies between two features that exist in the model, return a linear
+          // interpolation of these data points as the prediction.
           linearInterpolation(
             boundaries(insertIndex - 1),
             predictions(insertIndex - 1),
@@ -316,8 +322,8 @@ object IsotonicRegression {
             predictions(insertIndex),
             value)
         } else {
-          // If provided feature matches a feature that exists in the model exactly, return that feature's label as the
-          // prediction.
+          // If provided feature matches a feature that exists in the model exactly, return that
+          // feature's label as the prediction.
           predictions(foundIndex)
         }
       }
