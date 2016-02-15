567e64f185b7405adf73da2494407894db04c6d4
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2016-02-15 13:57:15 +0100

    [FLINK-3400] Move RocksDB Copy Utils to flink-streaming-java
    They are not specific to RocksDB, just utilities for copying local
    folders to/from HDFS. Moving them to flink-streaming-java means that
    they are always in the classpath of the TaskManager, not only in the
    user-code jar when using RocksDB. If they are only in the user-code jar
    the external process runner cannot find the class files, leading to
    ClassNotFoundExceptions.

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/util/HDFSCopyUtilitiesTest.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/util/HDFSCopyUtilitiesTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.flink.contrib.streaming.state;
+package org.apache.flink.streaming.util;
 
 import org.apache.hadoop.conf.Configuration;
 import org.junit.Rule;
