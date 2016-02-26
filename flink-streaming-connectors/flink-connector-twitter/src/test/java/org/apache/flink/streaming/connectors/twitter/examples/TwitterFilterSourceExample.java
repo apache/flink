c0ce1b6d97bea3a938dc81454fcb17f373a58f12
Till Rohrmann <trohrmann@apache.org>
2016-02-26 16:27:06 +0100

    [FLINK-3511] [twitter] Move twitter examples to test scope
    This closes #1725

--- flink-streaming-connectors/flink-connector-twitter/src/test/java/org/apache/flink/streaming/connectors/twitter/examples/TwitterFilterSourceExample.java
+++ flink-streaming-connectors/flink-connector-twitter/src/test/java/org/apache/flink/streaming/connectors/twitter/examples/TwitterFilterSourceExample.java
@@ -1,12 +1,13 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,11 +16,12 @@
  * limitations under the License.
  */
 
-package org.apache.flink.streaming.connectors.twitter;
+package org.apache.flink.streaming.connectors.twitter.examples;
 
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.json.JSONParseFlatMap;
+import org.apache.flink.streaming.connectors.twitter.TwitterFilterSource;
 import org.apache.flink.util.Collector;
 
 /**
