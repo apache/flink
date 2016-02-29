de03c00959b3001e2aadc25bd5be1f977c2a6063
Stephan Ewen <sewen@apache.org>
2016-02-29 22:04:26 +0100

    [FLINK-3550] [examples] Rework WindowJoin to properly demonstrate continuous window joins (Java + Scala)

--- flink-examples/flink-examples-streaming/src/test/java/org/apache/flink/streaming/test/exampleJavaPrograms/join/WindowJoinITCase.java
+++ flink-examples/flink-examples-streaming/src/test/java/org/apache/flink/streaming/test/exampleJavaPrograms/join/WindowJoinITCase.java
@@ -18,36 +18,64 @@
 
 package org.apache.flink.streaming.test.exampleJavaPrograms.join;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.core.fs.FileSystem.WriteMode;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.examples.join.WindowJoin;
-import org.apache.flink.streaming.examples.join.util.WindowJoinData;
-import org.apache.flink.streaming.util.StreamingProgramTestBase;
+import org.apache.flink.streaming.util.StreamingMultipleProgramsTestBase;
 
-public class WindowJoinITCase extends StreamingProgramTestBase {
+import org.junit.Test;
 
-	protected String gradesPath;
-	protected String salariesPath;
-	protected String resultPath;
+import java.io.File;
 
-	@Override
-	protected void preSubmit() throws Exception {
-		gradesPath = createTempFile("gradesText.txt", WindowJoinData.GRADES_INPUT);
-		salariesPath = createTempFile("salariesText.txt", WindowJoinData.SALARIES_INPUT);
-		resultPath = getTempDirPath("result");
+@SuppressWarnings("serial")
+public class WindowJoinITCase extends StreamingMultipleProgramsTestBase {
+
+	@Test
+	public void testProgram() throws Exception {
+		final String resultPath = File.createTempFile("result-path", "dir").toURI().toString();
+		try {
+			final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+			env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime);
+			
+			DataStream<Tuple2<String, Integer>> grades = env
+					.fromElements(WindowJoinData.GRADES_INPUT.split("\n"))
+					.map(new Parser());
+	
+			DataStream<Tuple2<String, Integer>> salaries = env
+					.fromElements(WindowJoinData.SALARIES_INPUT.split("\n"))
+					.map(new Parser());
+			
+			WindowJoin
+					.runWindowJoin(grades, salaries, 100)
+					.writeAsText(resultPath, WriteMode.OVERWRITE);
+			
+			env.execute();
+
+			// since the two sides of the join might have different speed
+			// the exact output can not be checked just whether it is well-formed
+			// checks that the result lines look like e.g. (bob, 2, 2015)
+			checkLinesAgainstRegexp(resultPath, "^\\([a-z]+,(\\d),(\\d)+\\)");
+		}
+		finally {
+			try {
+				FileUtils.deleteDirectory(new File(resultPath));
+			} catch (Throwable ignored) {}
+		}
 	}
+	
+	//-------------------------------------------------------------------------
+	
+	public static final class Parser implements MapFunction<String, Tuple2<String, Integer>> {
 
-	@Override
-	protected void postSubmit() throws Exception {
-		// since the two sides of the join might have different speed
-		// the exact output can not be checked just whether it is well-formed
-		// checks that the result lines look like e.g. (bob, 2, 2015)
-		checkLinesAgainstRegexp(resultPath, "^\\([a-z]+,(\\d),(\\d)+\\)");
-	}
-
-	@Override
-	protected void testProgram() throws Exception {
-		WindowJoin.main(new String[]{
-				"--grades", gradesPath,
-				"--salaries", salariesPath,
-				"--output", resultPath});
+		@Override
+		public Tuple2<String, Integer> map(String value) throws Exception {
+			String[] fields = value.split(",");
+			return new Tuple2<>(fields[1], Integer.parseInt(fields[2]));
+		}
 	}
 }
