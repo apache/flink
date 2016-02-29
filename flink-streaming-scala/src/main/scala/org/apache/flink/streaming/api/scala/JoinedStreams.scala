9b9f84e800b19b585e147ab6add4eb946356caa0
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2016-02-29 17:02:38 +0100

    [FLINK-3538] [api-breaking] Streamline Scala DataStream.join/coGroup
    This enforces that the user always has to specify keys for both inputs
    before .window() can be called.

--- flink-streaming-scala/src/main/scala/org/apache/flink/streaming/api/scala/JoinedStreams.scala
+++ flink-streaming-scala/src/main/scala/org/apache/flink/streaming/api/scala/JoinedStreams.scala
@@ -54,262 +54,171 @@ import org.apache.flink.util.Collector
  * } }}}
  */
 @Public
-object JoinedStreams {
+class JoinedStreams[T1, T2](input1: DataStream[T1], input2: DataStream[T2]) {
 
   /**
-   * A join operation that does not yet have its [[KeySelector]]s defined.
-   *
-   * @tparam T1 Type of the elements from the first input
-   * @tparam T2 Type of the elements from the second input
+   * Specifies a [[KeySelector]] for elements from the first input.
    */
-  class Unspecified[T1, T2](input1: DataStream[T1], input2: DataStream[T2]) {
-
-    /**
-     * Specifies a [[KeySelector]] for elements from the first input.
-     */
-    def where[KEY: TypeInformation](keySelector: T1 => KEY): WithKey[T1, T2, KEY] = {
-      val cleanFun = clean(keySelector)
-      val keyType = implicitly[TypeInformation[KEY]]
-      val javaSelector = new KeySelector[T1, KEY] with ResultTypeQueryable[KEY] {
-        def getKey(in: T1) = cleanFun(in)
-        override def getProducedType: TypeInformation[KEY] = keyType
-      }
-      new WithKey[T1, T2, KEY](input1, input2, javaSelector, null, keyType)
+  def where[KEY: TypeInformation](keySelector: T1 => KEY): Where[KEY] = {
+    val cleanFun = clean(keySelector)
+    val keyType = implicitly[TypeInformation[KEY]]
+    val javaSelector = new KeySelector[T1, KEY] with ResultTypeQueryable[KEY] {
+      def getKey(in: T1) = cleanFun(in)
+      override def getProducedType: TypeInformation[KEY] = keyType
     }
-
-    /**
-     * Specifies a [[KeySelector]] for elements from the second input.
-     */
-    def equalTo[KEY: TypeInformation](keySelector: T2 => KEY): WithKey[T1, T2, KEY] = {
-      val cleanFun = clean(keySelector)
-      val keyType = implicitly[TypeInformation[KEY]]
-      val javaSelector = new KeySelector[T2, KEY] with ResultTypeQueryable[KEY] {
-        def getKey(in: T2) = cleanFun(in)
-        override def getProducedType: TypeInformation[KEY] = keyType
-      }
-      new WithKey[T1, T2, KEY](input1, input2, null, javaSelector, keyType)
-    }
-
-    /**
-     * Returns a "closure-cleaned" version of the given function. Cleans only if closure cleaning
-     * is not disabled in the [[org.apache.flink.api.common.ExecutionConfig]].
-     */
-    private[flink] def clean[F <: AnyRef](f: F): F = {
-      new StreamExecutionEnvironment(input1.javaStream.getExecutionEnvironment).scalaClean(f)
-    }
+    new Where[KEY](javaSelector, keyType)
   }
 
   /**
-   * A join operation that has [[KeySelector]]s defined for either both or
-   * one input.
-   *
-   * You need to specify a [[KeySelector]] for both inputs using [[where()]] and [[equalTo()]]
-   * before you can proceeed with specifying a [[WindowAssigner]] using [[window()]].
-   *
-   * @tparam T1 Type of the elements from the first input
-   * @tparam T2 Type of the elements from the second input
-   * @tparam KEY Type of the key. This must be the same for both inputs
-   */
-  class WithKey[T1, T2, KEY](
-      input1: DataStream[T1],
-      input2: DataStream[T2],
-      keySelector1: KeySelector[T1, KEY],
-      keySelector2: KeySelector[T2, KEY],
-      keyType: TypeInformation[KEY]) {
+    * A join operation that has a [[KeySelector]] defined for the first input.
+    *
+    * You need to specify a [[KeySelector]] for the second input using [[equalTo()]]
+    * before you can proceeed with specifying a [[WindowAssigner]] using [[EqualTo.window()]].
+    *
+    * @tparam KEY Type of the key. This must be the same for both inputs
+    */
+  class Where[KEY](keySelector1: KeySelector[T1, KEY], keyType: TypeInformation[KEY]) {
 
     /**
-     * Specifies a [[KeySelector]] for elements from the first input.
-     */
-    def where(keySelector: T1 => KEY): JoinedStreams.WithKey[T1, T2, KEY] = {
-      val cleanFun = clean(keySelector)
-      val localKeyType = keyType
-      val javaSelector = new KeySelector[T1, KEY] with ResultTypeQueryable[KEY] {
-        def getKey(in: T1) = cleanFun(in)
-        override def getProducedType: TypeInformation[KEY] = localKeyType
-      }
-      new WithKey[T1, T2, KEY](input1, input2, javaSelector, keySelector2, localKeyType)
-    }
-
-    /**
-     * Specifies a [[KeySelector]] for elements from the second input.
-     */
-    def equalTo(keySelector: T2 => KEY): JoinedStreams.WithKey[T1, T2, KEY] = {
+      * Specifies a [[KeySelector]] for elements from the second input.
+      */
+    def equalTo(keySelector: T2 => KEY): EqualTo = {
       val cleanFun = clean(keySelector)
       val localKeyType = keyType
       val javaSelector = new KeySelector[T2, KEY] with ResultTypeQueryable[KEY] {
         def getKey(in: T2) = cleanFun(in)
         override def getProducedType: TypeInformation[KEY] = localKeyType
       }
-      new WithKey[T1, T2, KEY](input1, input2, keySelector1, javaSelector, localKeyType)
+      new EqualTo(javaSelector)
     }
 
     /**
-     * Specifies the window on which the join operation works.
-     */
-    @PublicEvolving
-    def window[W <: Window](
-        assigner: WindowAssigner[_ >: JavaCoGroupedStreams.TaggedUnion[T1, T2], W])
-        : JoinedStreams.WithWindow[T1, T2, KEY, W] = {
-      if (keySelector1 == null || keySelector2 == null) {
-        throw new UnsupportedOperationException("You first need to specify KeySelectors for both" +
-          "inputs using where() and equalTo().")
+      * A join operation that has a [[KeySelector]] defined for the first and the second input.
+      *
+      * A window can now be specified using [[window()]].
+      */
+    class EqualTo(keySelector2: KeySelector[T2, KEY]) {
+      /**
+        * Specifies the window on which the join operation works.
+        */
+      @PublicEvolving
+      def window[W <: Window](
+          assigner: WindowAssigner[_ >: JavaCoGroupedStreams.TaggedUnion[T1, T2], W])
+          : WithWindow[W] = {
+        if (keySelector1 == null || keySelector2 == null) {
+          throw new UnsupportedOperationException(
+            "You first need to specify KeySelectors for both inputs using where() and equalTo().")
+        }
+
+        new WithWindow[W](clean(assigner), null, null)
       }
-      new JoinedStreams.WithWindow[T1, T2, KEY, W](
-        input1,
-        input2,
-        keySelector1,
-        keySelector2,
-        clean(assigner),
-        null,
-        null)
-    }
 
-    /**
-     * Returns a "closure-cleaned" version of the given function. Cleans only if closure cleaning
-     * is not disabled in the [[org.apache.flink.api.common.ExecutionConfig]].
-     */
-    private[flink] def clean[F <: AnyRef](f: F): F = {
-      new StreamExecutionEnvironment(input1.javaStream.getExecutionEnvironment).scalaClean(f)
+      /**
+       * A join operation that has [[KeySelector]]s defined for both inputs as
+       * well as a [[WindowAssigner]].
+       *
+       * @tparam W Type of { @link Window} on which the join operation works.
+       */
+      class WithWindow[W <: Window](
+          windowAssigner: WindowAssigner[_ >: JavaCoGroupedStreams.TaggedUnion[T1, T2], W],
+          trigger: Trigger[_ >: JavaCoGroupedStreams.TaggedUnion[T1, T2], _ >: W],
+          evictor: Evictor[_ >: JavaCoGroupedStreams.TaggedUnion[T1, T2], _ >: W]) {
+
+        /**
+         * Sets the [[Trigger]] that should be used to trigger window emission.
+         */
+        @PublicEvolving
+        def trigger(newTrigger: Trigger[_ >: JavaCoGroupedStreams.TaggedUnion[T1, T2], _ >: W])
+        : WithWindow[W] = {
+          new WithWindow[W](windowAssigner, newTrigger, evictor)
+        }
+
+        /**
+         * Sets the [[Evictor]] that should be used to evict elements from a window before emission.
+         *
+         * Note: When using an evictor window performance will degrade significantly, since
+         * pre-aggregation of window results cannot be used.
+         */
+        @PublicEvolving
+        def evictor(newEvictor: Evictor[_ >: JavaCoGroupedStreams.TaggedUnion[T1, T2], _ >: W])
+        : WithWindow[W] = {
+          new WithWindow[W](windowAssigner, trigger, newEvictor)
+        }
+
+        /**
+         * Completes the join operation with the user function that is executed
+         * for windowed groups.
+         */
+        def apply[O: TypeInformation](fun: (T1, T2) => O): DataStream[O] = {
+          require(fun != null, "Join function must not be null.")
+
+          val joiner = new FlatJoinFunction[T1, T2, O] {
+            val cleanFun = clean(fun)
+            def join(left: T1, right: T2, out: Collector[O]) = {
+              out.collect(cleanFun(left, right))
+            }
+          }
+          apply(joiner)
+        }
+
+        /**
+         * Completes the join operation with the user function that is executed
+         * for windowed groups.
+         */
+        def apply[O: TypeInformation](fun: (T1, T2, Collector[O]) => Unit): DataStream[O] = {
+          require(fun != null, "Join function must not be null.")
+
+          val joiner = new FlatJoinFunction[T1, T2, O] {
+            val cleanFun = clean(fun)
+            def join(left: T1, right: T2, out: Collector[O]) = {
+              cleanFun(left, right, out)
+            }
+          }
+          apply(joiner)
+        }
+
+        /**
+         * Completes the join operation with the user function that is executed
+         * for windowed groups.
+         */
+        def apply[T: TypeInformation](function: JoinFunction[T1, T2, T]): DataStream[T] = {
+
+          val join = new JavaJoinedStreams[T1, T2](input1.javaStream, input2.javaStream)
+
+          asScalaStream(join
+            .where(keySelector1)
+            .equalTo(keySelector2)
+            .window(windowAssigner)
+            .trigger(trigger)
+            .evictor(evictor)
+            .apply(clean(function), implicitly[TypeInformation[T]]))
+        }
+
+        /**
+         * Completes the join operation with the user function that is executed
+         * for windowed groups.
+         */
+        def apply[T: TypeInformation](function: FlatJoinFunction[T1, T2, T]): DataStream[T] = {
+
+          val join = new JavaJoinedStreams[T1, T2](input1.javaStream, input2.javaStream)
+
+          asScalaStream(join
+            .where(keySelector1)
+            .equalTo(keySelector2)
+            .window(windowAssigner)
+            .trigger(trigger)
+            .evictor(evictor)
+            .apply(clean(function), implicitly[TypeInformation[T]]))
+        }
+      }
     }
   }
 
   /**
-   * A join operation that has [[KeySelector]]s defined for both inputs as
-   * well as a [[WindowAssigner]].
-   *
-   * @tparam T1 Type of the elements from the first input
-   * @tparam T2 Type of the elements from the second input
-   * @tparam KEY Type of the key. This must be the same for both inputs
-   * @tparam W Type of { @link Window} on which the join operation works.
+   * Returns a "closure-cleaned" version of the given function. Cleans only if closure cleaning
+   * is not disabled in the [[org.apache.flink.api.common.ExecutionConfig]].
    */
-  class WithWindow[T1, T2, KEY, W <: Window](
-      input1: DataStream[T1],
-      input2: DataStream[T2],
-      keySelector1: KeySelector[T1, KEY],
-      keySelector2: KeySelector[T2, KEY],
-      windowAssigner: WindowAssigner[_ >: JavaCoGroupedStreams.TaggedUnion[T1, T2], W],
-      trigger: Trigger[_ >: JavaCoGroupedStreams.TaggedUnion[T1, T2], _ >: W],
-      evictor: Evictor[_ >: JavaCoGroupedStreams.TaggedUnion[T1, T2], _ >: W]) {
-
-
-    /**
-     * Sets the [[Trigger]] that should be used to trigger window emission.
-     */
-    @PublicEvolving
-    def trigger(newTrigger: Trigger[_ >: JavaCoGroupedStreams.TaggedUnion[T1, T2], _ >: W])
-    : JoinedStreams.WithWindow[T1, T2, KEY, W] = {
-      new WithWindow[T1, T2, KEY, W](
-        input1,
-        input2,
-        keySelector1,
-        keySelector2,
-        windowAssigner,
-        newTrigger,
-        evictor)
-    }
-
-    /**
-     * Sets the [[Evictor]] that should be used to evict elements from a window before emission.
-     *
-     * Note: When using an evictor window performance will degrade significantly, since
-     * pre-aggregation of window results cannot be used.
-     */
-    @PublicEvolving
-    def evictor(newEvictor: Evictor[_ >: JavaCoGroupedStreams.TaggedUnion[T1, T2], _ >: W])
-    : JoinedStreams.WithWindow[T1, T2, KEY, W] = {
-      new WithWindow[T1, T2, KEY, W](
-        input1,
-        input2,
-        keySelector1,
-        keySelector2,
-        windowAssigner,
-        trigger,
-        newEvictor)
-    }
-
-    /**
-     * Completes the join operation with the user function that is executed
-     * for windowed groups.
-     */
-    def apply[O: TypeInformation](fun: (T1, T2) => O): DataStream[O] = {
-      require(fun != null, "Join function must not be null.")
-
-      val joiner = new FlatJoinFunction[T1, T2, O] {
-        val cleanFun = clean(fun)
-        def join(left: T1, right: T2, out: Collector[O]) = {
-          out.collect(cleanFun(left, right))
-        }
-      }
-      apply(joiner)
-    }
-
-    /**
-     * Completes the join operation with the user function that is executed
-     * for windowed groups.
-     */
-    def apply[O: TypeInformation](fun: (T1, T2, Collector[O]) => Unit): DataStream[O] = {
-      require(fun != null, "Join function must not be null.")
-
-      val joiner = new FlatJoinFunction[T1, T2, O] {
-        val cleanFun = clean(fun)
-        def join(left: T1, right: T2, out: Collector[O]) = {
-          cleanFun(left, right, out)
-        }
-      }
-      apply(joiner)
-    }
-
-    /**
-     * Completes the join operation with the user function that is executed
-     * for windowed groups.
-     */
-    def apply[T: TypeInformation](function: JoinFunction[T1, T2, T]): DataStream[T] = {
-
-      val join = new JavaJoinedStreams[T1, T2](input1.javaStream, input2.javaStream)
-
-      asScalaStream(join
-        .where(keySelector1)
-        .equalTo(keySelector2)
-        .window(windowAssigner)
-        .trigger(trigger)
-        .evictor(evictor)
-        .apply(clean(function), implicitly[TypeInformation[T]]))
-    }
-
-    /**
-     * Completes the join operation with the user function that is executed
-     * for windowed groups.
-     */
-    def apply[T: TypeInformation](function: FlatJoinFunction[T1, T2, T]): DataStream[T] = {
-
-      val join = new JavaJoinedStreams[T1, T2](input1.javaStream, input2.javaStream)
-
-      asScalaStream(join
-        .where(keySelector1)
-        .equalTo(keySelector2)
-        .window(windowAssigner)
-        .trigger(trigger)
-        .evictor(evictor)
-        .apply(clean(function), implicitly[TypeInformation[T]]))
-    }
-
-    /**
-     * Returns a "closure-cleaned" version of the given function. Cleans only if closure cleaning
-     * is not disabled in the [[org.apache.flink.api.common.ExecutionConfig]].
-     */
-    private[flink] def clean[F <: AnyRef](f: F): F = {
-      new StreamExecutionEnvironment(input1.javaStream.getExecutionEnvironment).scalaClean(f)
-    }
+  private[flink] def clean[F <: AnyRef](f: F): F = {
+    new StreamExecutionEnvironment(input1.javaStream.getExecutionEnvironment).scalaClean(f)
   }
-
-
-  /**
-   * Creates a new join operation from the two given inputs.
-   */
-  def createJoin[T1, T2](input1: DataStream[T1], input2: DataStream[T2])
-      : JoinedStreams.Unspecified[T1, T2] = {
-    new JoinedStreams.Unspecified[T1, T2](input1, input2)
-  }
-
 }
-
