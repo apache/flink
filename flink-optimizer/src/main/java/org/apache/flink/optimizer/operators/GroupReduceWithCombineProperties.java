76da442837d8606d8c4c4d8367b29fd79dc5ba35
ramkrishna <ramkrishna.s.vasudevan@gmail.com>
2016-03-21 17:03:42 +0530

    [FLINK-3179] [dataSet][optimizer] Log a WARN message if combiner is not added in front of PartitionOperator
    This closes #1822
    This closes #1553

--- flink-optimizer/src/main/java/org/apache/flink/optimizer/operators/GroupReduceWithCombineProperties.java
+++ flink-optimizer/src/main/java/org/apache/flink/optimizer/operators/GroupReduceWithCombineProperties.java
@@ -27,6 +27,7 @@ import org.apache.flink.api.common.operators.Ordering;
 import org.apache.flink.api.common.operators.util.FieldSet;
 import org.apache.flink.optimizer.costs.Costs;
 import org.apache.flink.optimizer.dag.GroupReduceNode;
+import org.apache.flink.optimizer.dag.PartitionNode;
 import org.apache.flink.optimizer.dag.SingleInputNode;
 import org.apache.flink.optimizer.dataproperties.GlobalProperties;
 import org.apache.flink.optimizer.dataproperties.LocalProperties;
@@ -39,8 +40,11 @@ import org.apache.flink.runtime.io.network.DataExchangeMode;
 import org.apache.flink.runtime.operators.DriverStrategy;
 import org.apache.flink.runtime.operators.shipping.ShipStrategyType;
 import org.apache.flink.runtime.operators.util.LocalStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class GroupReduceWithCombineProperties extends OperatorDescriptorSingle {
+	private static final Logger LOG = LoggerFactory.getLogger(GroupReduceWithCombineProperties.class);
 	
 	private final Ordering ordering;		// ordering that we need to use if an additional ordering is requested 
 	
@@ -90,6 +94,9 @@ public final class GroupReduceWithCombineProperties extends OperatorDescriptorSi
 	@Override
 	public SingleInputPlanNode instantiate(Channel in, SingleInputNode node) {
 		if (in.getShipStrategy() == ShipStrategyType.FORWARD) {
+			if(in.getSource().getOptimizerNode() instanceof PartitionNode) {
+				LOG.warn("Cannot automatically inject combiner for GroupReduceFunction. Please add an explicit combiner with combineGroup() in front of the partition operator.");
+			}
 			// adjust a sort (changes grouping, so it must be for this driver to combining sort
 			if (in.getLocalStrategy() == LocalStrategy.SORT) {
 				if (!in.getLocalStrategyKeys().isValidUnorderedPrefix(this.keys)) {
