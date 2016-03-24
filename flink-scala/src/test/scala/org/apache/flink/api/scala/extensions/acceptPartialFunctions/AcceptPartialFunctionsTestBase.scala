401058e9b26379f1e7e08011752835d147fde2a6
Stefano Baghino <stefano.baghino@radicalbit.io>
2016-03-24 17:51:05 +0100

    [FLINK-1159] Add grouped fixtures

--- flink-scala/src/test/scala/org/apache/flink/api/scala/extensions/acceptPartialFunctions/AcceptPartialFunctionsTestBase.scala
+++ flink-scala/src/test/scala/org/apache/flink/api/scala/extensions/acceptPartialFunctions/AcceptPartialFunctionsTestBase.scala
@@ -11,4 +11,7 @@ abstract class AcceptPartialFunctionsTestBase extends TestLogger with JUnitSuite
   protected val tuples = env.fromElements(1 -> "hello", 2 -> "world")
   protected val caseObjects = env.fromElements(KeyValuePair(1, "hello"), KeyValuePair(2, "world"))
 
+  protected val groupedTuples = tuples.groupBy(_._1)
+  protected val groupedCaseObjects = caseObjects.groupBy(_.id)
+
 }
