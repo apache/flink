03dd7b4c4edb65a5d0b6acff56a5ab72ed824c08
Stefano Baghino <stefano.baghino@radicalbit.io>
2016-03-24 15:46:25 +0100

    [FLINK-1159] Add unit tests for OnDataSet

---
+++ flink-scala/src/test/scala/org/apache/flink/api/scala/extensions/acceptPartialFunctions/KeyValuePair.scala
@@ -0,0 +1,8 @@
+package org.apache.flink.api.scala.extensions.acceptPartialFunctions
+
+/**
+  * Simple case class to test the `acceptPartialFunctions` extension
+  * @param id A numerical identifier
+  * @param value A textual value
+  */
+private[acceptPartialFunctions] case class KeyValuePair(id: Int, value: String)
