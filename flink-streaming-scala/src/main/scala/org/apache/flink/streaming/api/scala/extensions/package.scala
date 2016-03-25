fd4a649d6bbd6042b1c8c266c44b9342e8878783
Stefano Baghino <stefano.baghino@radicalbit.io>
2016-03-25 20:52:57 +0100

    [FLINK-1159] Improve DataStream ext. Scaladoc, provide return types

--- flink-streaming-scala/src/main/scala/org/apache/flink/streaming/api/scala/extensions/package.scala
+++ flink-streaming-scala/src/main/scala/org/apache/flink/streaming/api/scala/extensions/package.scala
@@ -21,182 +21,67 @@ import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.streaming.api.scala.extensions.acceptPartialFunctions._
 import org.apache.flink.streaming.api.windowing.windows.Window
 
+/**
+  * acceptPartialFunctions extends the original DataStream with methods with unique names
+  * that delegate to core higher-order functions (e.g. `map`) so that we can work around
+  * the fact that overloaded methods taking functions as parameters can't accept partial
+  * functions as well. This enables the possibility to directly apply pattern matching
+  * to decompose inputs such as tuples, case classes and collections.
+  *
+  * The following is a small example that showcases how this extensions would work on
+  * a Flink data stream:
+  *
+  * {{{
+  *   object Main {
+  *     import org.apache.flink.streaming.api.scala.extensions._
+  *     case class Point(x: Double, y: Double)
+  *     def main(args: Array[String]): Unit = {
+  *       val env = StreamExecutionEnvironment.getExecutionEnvironment
+  *       val ds = env.fromElements(Point(1, 2), Point(3, 4), Point(5, 6))
+  *       ds.filterWith {
+  *         case Point(x, _) => x > 1
+  *       }.reduceWith {
+  *         case (Point(x1, y1), (Point(x2, y2))) => Point(x1 + y1, x2 + y2)
+  *       }.mapWith {
+  *         case Point(x, y) => (x, y)
+  *       }.flatMapWith {
+  *         case (x, y) => Seq('x' -> x, 'y' -> y)
+  *       }.keyingBy {
+  *         case (id, value) => id
+  *       }
+  *     }
+  *   }
+  * }}}
+  *
+  * The extension consists of several implicit conversions over all the data stream representations
+  * that could gain from this feature. To use this set of extensions methods the user has to
+  * explicitly opt-in by importing
+  * `org.apache.flink.streaming.api.scala.extensions.acceptPartialFunctions`.
+  *
+  * For more information and usage examples please consult the Apache Flink official documentation.
+  *
+  */
 package object extensions {
 
-  /**
-    * acceptPartialFunctions extends the original DataStream with methods with unique names
-    * that delegate to core higher-order functions (e.g. `map`) so that we can work around
-    * the fact that overloaded methods taking functions as parameters can't accept partial
-    * functions as well. This enables the possibility to directly apply pattern matching
-    * to decompose inputs such as tuples, case classes and collections.
-    *
-    * e.g.
-    * {{{
-    *   object Main {
-    *     import org.apache.flink.api.scala.extensions._
-    *     case class Point(x: Double, y: Double)
-    *     def main(args: Array[String]): Unit = {
-    *       val env = StreamExecutionEnvironment.getExecutionEnvironment
-    *       val ds = env.fromElements(Point(1, 2), Point(3, 4), Point(5, 6))
-    *       ds.filterWith {
-    *         case Point(x, _) => x > 1
-    *       }.reduceWith {
-    *         case (Point(x1, y1), (Point(x2, y2))) => Point(x1 + y1, x2 + y2)
-    *       }.mapWith {
-    *         case Point(x, y) => (x, y)
-    *       }.flatMapWith {
-    *         case (x, y) => Seq('x' -> x, 'y' -> y)
-    *       }.keyingBy {
-    *         case (id, value) => id
-    *       }
-    *     }
-    *   }
-    * }}}
-    *
-    */
   implicit def acceptPartialFunctionsOnDataStream[T: TypeInformation](ds: DataStream[T]):
       OnDataStream[T] =
     new OnDataStream[T](ds)
 
-  /**
-    * acceptPartialFunctions extends the original DataStream with methods with unique names
-    * that delegate to core higher-order functions (e.g. `map`) so that we can work around
-    * the fact that overloaded methods taking functions as parameters can't accept partial
-    * functions as well. This enables the possibility to directly apply pattern matching
-    * to decompose inputs such as tuples, case classes and collections.
-    *
-    * e.g.
-    * {{{
-    *   object Main {
-    *     import org.apache.flink.api.scala.extensions._
-    *     case class Point(x: Double, y: Double)
-    *     def main(args: Array[String]): Unit = {
-    *       val env = StreamExecutionEnvironment.getExecutionEnvironment
-    *       val ds = env.fromElements(Point(1, 2), Point(3, 4), Point(5, 6))
-    *       ds.filterWith {
-    *         case Point(x, _) => x > 1
-    *       }.reduceWith {
-    *         case (Point(x1, y1), (Point(x2, y2))) => Point(x1 + y1, x2 + y2)
-    *       }.mapWith {
-    *         case Point(x, y) => (x, y)
-    *       }.flatMapWith {
-    *         case (x, y) => Seq('x' -> x, 'y' -> y)
-    *       }.keyingBy {
-    *         case (id, value) => id
-    *       }
-    *     }
-    *   }
-    * }}}
-    *
-    */
   implicit def acceptPartialFunctionsOnKeyedStream[T: TypeInformation, K](ds: KeyedStream[T, K]):
       OnKeyedStream[T, K] =
     new OnKeyedStream[T, K](ds)
 
-  /**
-    * acceptPartialFunctions extends the original DataStream with methods with unique names
-    * that delegate to core higher-order functions (e.g. `map`) so that we can work around
-    * the fact that overloaded methods taking functions as parameters can't accept partial
-    * functions as well. This enables the possibility to directly apply pattern matching
-    * to decompose inputs such as tuples, case classes and collections.
-    *
-    * e.g.
-    * {{{
-    *   object Main {
-    *     import org.apache.flink.api.scala.extensions._
-    *     case class Point(x: Double, y: Double)
-    *     def main(args: Array[String]): Unit = {
-    *       val env = StreamExecutionEnvironment.getExecutionEnvironment
-    *       val ds = env.fromElements(Point(1, 2), Point(3, 4), Point(5, 6))
-    *       ds.filterWith {
-    *         case Point(x, _) => x > 1
-    *       }.reduceWith {
-    *         case (Point(x1, y1), (Point(x2, y2))) => Point(x1 + y1, x2 + y2)
-    *       }.mapWith {
-    *         case Point(x, y) => (x, y)
-    *       }.flatMapWith {
-    *         case (x, y) => Seq('x' -> x, 'y' -> y)
-    *       }.keyingBy {
-    *         case (id, value) => id
-    *       }
-    *     }
-    *   }
-    * }}}
-    *
-    */
   implicit def acceptPartialFunctionsOnJoinedStream
       [L: TypeInformation, R: TypeInformation, K, W <: Window](
-      ds: JoinedStreams[L, R]#Where[K]#EqualTo#WithWindow[W]) =
+      ds: JoinedStreams[L, R]#Where[K]#EqualTo#WithWindow[W]): OnJoinedStream[L, R, K, W] =
     new OnJoinedStream[L, R, K, W](ds)
 
-  /**
-    * acceptPartialFunctions extends the original DataStream with methods with unique names
-    * that delegate to core higher-order functions (e.g. `map`) so that we can work around
-    * the fact that overloaded methods taking functions as parameters can't accept partial
-    * functions as well. This enables the possibility to directly apply pattern matching
-    * to decompose inputs such as tuples, case classes and collections.
-    *
-    * e.g.
-    * {{{
-    *   object Main {
-    *     import org.apache.flink.api.scala.extensions._
-    *     case class Point(x: Double, y: Double)
-    *     def main(args: Array[String]): Unit = {
-    *       val env = StreamExecutionEnvironment.getExecutionEnvironment
-    *       val ds = env.fromElements(Point(1, 2), Point(3, 4), Point(5, 6))
-    *       ds.filterWith {
-    *         case Point(x, _) => x > 1
-    *       }.reduceWith {
-    *         case (Point(x1, y1), (Point(x2, y2))) => Point(x1 + y1, x2 + y2)
-    *       }.mapWith {
-    *         case Point(x, y) => (x, y)
-    *       }.flatMapWith {
-    *         case (x, y) => Seq('x' -> x, 'y' -> y)
-    *       }.keyingBy {
-    *         case (id, value) => id
-    *       }
-    *     }
-    *   }
-    * }}}
-    *
-    */
   implicit def acceptPartialFunctionsOnConnectedStream[IN1: TypeInformation, IN2: TypeInformation](
-      ds: ConnectedStreams[IN1, IN2]) =
+      ds: ConnectedStreams[IN1, IN2]): OnConnectedStream[IN1, IN2] =
     new OnConnectedStream[IN1, IN2](ds)
 
-  /**
-    * acceptPartialFunctions extends the original DataStream with methods with unique names
-    * that delegate to core higher-order functions (e.g. `map`) so that we can work around
-    * the fact that overloaded methods taking functions as parameters can't accept partial
-    * functions as well. This enables the possibility to directly apply pattern matching
-    * to decompose inputs such as tuples, case classes and collections.
-    *
-    * e.g.
-    * {{{
-    *   object Main {
-    *     import org.apache.flink.api.scala.extensions._
-    *     case class Point(x: Double, y: Double)
-    *     def main(args: Array[String]): Unit = {
-    *       val env = StreamExecutionEnvironment.getExecutionEnvironment
-    *       val ds = env.fromElements(Point(1, 2), Point(3, 4), Point(5, 6))
-    *       ds.filterWith {
-    *         case Point(x, _) => x > 1
-    *       }.reduceWith {
-    *         case (Point(x1, y1), (Point(x2, y2))) => Point(x1 + y1, x2 + y2)
-    *       }.mapWith {
-    *         case Point(x, y) => (x, y)
-    *       }.flatMapWith {
-    *         case (x, y) => Seq('x' -> x, 'y' -> y)
-    *       }.keyingBy {
-    *         case (id, value) => id
-    *       }
-    *     }
-    *   }
-    * }}}
-    *
-    */
   implicit def acceptPartialFunctionsOnWindowedStream[T, K, W <: Window](
-      ds: WindowedStream[T, K, W]) =
+      ds: WindowedStream[T, K, W]): OnWindowedStream[T, K, W] =
     new OnWindowedStream[T, K, W](ds)
 
 }
