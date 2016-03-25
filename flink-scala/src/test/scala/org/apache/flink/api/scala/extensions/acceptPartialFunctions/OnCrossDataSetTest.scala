135013a02c060cd4beb5e5057a48058059462b39
Stefano Baghino <stefano.baghino@radicalbit.io>
2016-03-25 15:21:57 +0100

    [FLINK-1159] Add tests for OnCrossDataSet

---
+++ flink-scala/src/test/scala/org/apache/flink/api/scala/extensions/acceptPartialFunctions/OnCrossDataSetTest.scala
@@ -0,0 +1,32 @@
+package org.apache.flink.api.scala.extensions.acceptPartialFunctions
+
+import org.apache.flink.api.java.operators.CrossOperator
+import org.apache.flink.api.scala._
+import org.apache.flink.api.scala.extensions._
+import org.apache.flink.api.scala.extensions.acceptPartialFunctions.base.AcceptPartialFunctionsTestBase
+import org.apache.flink.api.scala.extensions.acceptPartialFunctions.data.KeyValuePair
+import org.junit.Test
+
+class OnCrossDataSetTest extends AcceptPartialFunctionsTestBase {
+
+  @Test
+  def testCrossProjectingOnTuple(): Unit = {
+    val test =
+      tuples.cross(tuples).projecting {
+        case ((_, v1), (_, v2)) => s"$v1 $v2"
+      }
+    assert(test.javaSet.isInstanceOf[CrossOperator[_, _, _]],
+      "projecting for cross on tuples should produce a CrossOperator")
+  }
+
+  @Test
+  def testCrossProjectingOnCaseClass(): Unit = {
+    val test =
+      caseObjects.cross(caseObjects).projecting {
+        case (KeyValuePair(_, v1), KeyValuePair(_, v2)) => s"$v1 $v2"
+      }
+    assert(test.javaSet.isInstanceOf[CrossOperator[_, _, _]],
+      "projecting for cross on case objects should produce a CrossOperator")
+  }
+
+}
