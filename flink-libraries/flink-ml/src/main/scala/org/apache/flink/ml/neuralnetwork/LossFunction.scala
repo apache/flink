d8b75aeb5ece02afe35d3c811347c70e18a419b6
Trevor Grant <trevor.d.grant@gmail.com>
2016-03-28 17:58:28 -0500

    [FLINK][WIP][neural-nets] Initial Commit- working MLP

---
+++ flink-libraries/flink-ml/src/main/scala/org/apache/flink/ml/neuralnetwork/LossFunction.scala
@@ -0,0 +1,8 @@
+package org.apache.flink.ml.neuralnetwork
+
+/**
+ * Created by trevor on 3/28/16.
+ */
+class LossFunction {
+
+}
---
+++ flink-libraries/flink-ml/src/main/scala/org/apache/flink/ml/neuralnetwork/MultiLayerPerceptron.scala
@@ -0,0 +1,8 @@
+package org.apache.flink.ml.neuralnetwork
+
+/**
+ * Created by trevor on 3/5/16.
+ */
+class MultiLayerPerceptron {
+
+}
