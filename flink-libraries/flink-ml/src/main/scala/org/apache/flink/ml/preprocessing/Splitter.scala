ec1e65a31d80b33589b73619f2a5dd0a8e09c568
Trevor Grant <trevor.d.grant@gmail.com>
2016-04-15 17:37:51 -0500

    Add Splitter Pre-processing

---
+++ flink-libraries/flink-ml/src/main/scala/org/apache/flink/ml/preprocessing/Splitter.scala
@@ -0,0 +1,8 @@
+package org.apache.flink.ml.preprocessing
+
+/**
+ * Created by trevor on 4/13/16.
+ */
+class Splitter {
+
+}
---
+++ flink-libraries/flink-ml/src/test/scala/org/apache/flink/ml/preprocessing/SplitterITSuite.scala
@@ -0,0 +1,8 @@
+package org.apache.flink.ml.preprocessing
+
+/**
+ * Created by trevor on 4/15/16.
+ */
+class SplitterITSuite {
+
+}
