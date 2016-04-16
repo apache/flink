85fcfc4d43feddacb76f2d14229229340174fed8
Greg Hogan <code@greghogan.com>
2016-04-16 06:04:07 -0400

    [FLINK-3770] [gelly] Fix TriangleEnumerator performance
    Implement optimization of ordering edges by degree and a JoinHint for
    the joining of edges and vertices.
    
    This closes #1899

--- flink-libraries/flink-gelly-examples/src/test/java/org/apache/flink/graph/library/TriangleEnumeratorITCase.java
+++ flink-libraries/flink-gelly-examples/src/test/java/org/apache/flink/graph/library/TriangleEnumeratorITCase.java
@@ -47,9 +47,9 @@ public class TriangleEnumeratorITCase extends MultipleProgramsTestBase {
 				env);
 
 		List<Tuple3<Long,Long,Long>> actualOutput = graph.run(new TriangleEnumerator<Long, NullValue, NullValue>()).collect();
-		List<Tuple3<Long,Long,Long>>  expectedResult = TriangleCountData.getListOfTriangles();
+		List<Tuple3<Long,Long,Long>> expectedResult = TriangleCountData.getListOfTriangles();
 
-		Assert.assertEquals(actualOutput.size(), expectedResult.size());
+		Assert.assertEquals(expectedResult.size(), actualOutput.size());
 		for(Tuple3<Long,Long,Long> resultTriangle:actualOutput)	{
 			Assert.assertTrue(expectedResult.indexOf(resultTriangle)>=0);
 		}
