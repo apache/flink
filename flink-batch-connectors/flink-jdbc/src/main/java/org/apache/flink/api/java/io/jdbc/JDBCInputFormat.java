3fa858554032cd8e00ab37a9219763a3d1fbe8d9
Flavio Pompermaier <f.pompermaier@gmail.com>
2016-04-26 18:47:01 +0200

    Removed commented method

--- flink-batch-connectors/flink-jdbc/src/main/java/org/apache/flink/api/java/io/jdbc/JDBCInputFormat.java
+++ flink-batch-connectors/flink-jdbc/src/main/java/org/apache/flink/api/java/io/jdbc/JDBCInputFormat.java
@@ -108,19 +108,6 @@ public class JDBCInputFormat extends RichInputFormat<GenericRow, InputSplit> {
 			dbConn = null;
 		}
 	}
-	
-//	@Override
-//	public void closeInputFormat() {
-//		//called once per inputFormat (on close)
-//		try {
-//			dbConn.close();
-//		} catch (SQLException se) {
-//			LOG.info("Inputformat couldn't be closed - " + se.getMessage());
-//		} catch (NullPointerException npe) {
-//		} finally {
-//			dbConn = null;
-//		}
-//	}
 
 	/**
 	 * Connects to the source database and executes the query.
