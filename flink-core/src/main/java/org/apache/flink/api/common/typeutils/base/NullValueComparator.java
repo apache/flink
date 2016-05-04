5644f767b9de7c7aab0497618a0bca8b8d62d291
Greg Hogan <code@greghogan.com>
2016-05-04 16:56:16 -0400

    [FLINK-3868] [core] Specialized CopyableValue serializers and comparators
    Update ValueTypeInfo to use specialized serializers and comparators,
    many of which were already present.

---
+++ flink-core/src/main/java/org/apache/flink/api/common/typeutils/base/NullValueComparator.java
@@ -0,0 +1,153 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.api.common.typeutils.base;
+
+import org.apache.flink.api.common.typeutils.TypeComparator;
+import org.apache.flink.core.memory.DataInputView;
+import org.apache.flink.core.memory.DataOutputView;
+import org.apache.flink.core.memory.MemorySegment;
+import org.apache.flink.types.NormalizableKey;
+import org.apache.flink.types.NullValue;
+
+import java.io.IOException;
+
+/**
+ * Specialized comparator for NullValue based on CopyableValueComparator.
+ */
+public class NullValueComparator extends TypeComparator<NullValue> {
+
+	private static final long serialVersionUID = 1L;
+
+	private final boolean ascendingComparison;
+
+	private final NullValue reference = new NullValue();
+
+	private final NullValue tempReference = new NullValue();
+
+	private final TypeComparator<?>[] comparators = new TypeComparator[] {this};
+
+	public NullValueComparator(boolean ascending) {
+		this.ascendingComparison = ascending;
+	}
+
+	@Override
+	public int hash(NullValue record) {
+		return record.hashCode();
+	}
+
+	@Override
+	public void setReference(NullValue toCompare) {
+		toCompare.copyTo(reference);
+	}
+
+	@Override
+	public boolean equalToReference(NullValue candidate) {
+		return candidate.equals(this.reference);
+	}
+
+	@Override
+	public int compareToReference(TypeComparator<NullValue> referencedComparator) {
+		NullValue otherRef = ((NullValueComparator) referencedComparator).reference;
+		int comp = otherRef.compareTo(reference);
+		return ascendingComparison ? comp : -comp;
+	}
+
+	@Override
+	public int compare(NullValue first, NullValue second) {
+		int comp = first.compareTo(second);
+		return ascendingComparison ? comp : -comp;
+	}
+
+	@Override
+	public int compareSerialized(DataInputView firstSource, DataInputView secondSource) throws IOException {
+		reference.read(firstSource);
+		tempReference.read(secondSource);
+		int comp = reference.compareTo(tempReference);
+		return ascendingComparison ? comp : -comp;
+	}
+
+	@Override
+	public boolean supportsNormalizedKey() {
+		return NormalizableKey.class.isAssignableFrom(NullValue.class);
+	}
+
+	@Override
+	public int getNormalizeKeyLen() {
+		return reference.getMaxNormalizedKeyLen();
+	}
+
+	@Override
+	public boolean isNormalizedKeyPrefixOnly(int keyBytes) {
+		return keyBytes < getNormalizeKeyLen();
+	}
+
+	@Override
+	public void putNormalizedKey(NullValue record, MemorySegment target, int offset, int numBytes) {
+		record.copyNormalizedKey(target, offset, numBytes);
+	}
+
+	@Override
+	public boolean invertNormalizedKey() {
+		return !ascendingComparison;
+	}
+
+	@Override
+	public TypeComparator<NullValue> duplicate() {
+		return new NullValueComparator(ascendingComparison);
+	}
+
+	@Override
+	public int extractKeys(Object record, Object[] target, int index) {
+		target[index] = record;
+		return 1;
+	}
+
+	@Override
+	public TypeComparator<?>[] getFlatComparators() {
+		return comparators;
+	}
+
+	// --------------------------------------------------------------------------------------------
+	// unsupported normalization
+	// --------------------------------------------------------------------------------------------
+
+	@Override
+	public boolean supportsSerializationWithKeyNormalization() {
+		return false;
+	}
+
+	@Override
+	public void writeWithKeyNormalization(NullValue record, DataOutputView target) throws IOException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public NullValue readWithKeyDenormalization(NullValue reuse, DataInputView source) throws IOException {
+		throw new UnsupportedOperationException();
+	}
+
+	// --------------------------------------------------------------------------------------------
+	// serialization
+	// --------------------------------------------------------------------------------------------
+
+	private void readObject(java.io.ObjectInputStream s) throws IOException, ClassNotFoundException {
+		// read basic object and the type
+		s.defaultReadObject();
+	}
+}
---
+++ flink-core/src/main/java/org/apache/flink/api/common/typeutils/base/RecordSerializer.java
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.api.common.typeutils.base;
+
+import org.apache.flink.core.memory.DataInputView;
+import org.apache.flink.core.memory.DataOutputView;
+import org.apache.flink.types.Record;
+
+import java.io.IOException;
+
+public class RecordSerializer extends TypeSerializerSingleton<Record> {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final RecordSerializer INSTANCE = new RecordSerializer();
+
+	private transient Record tmp;
+
+	@Override
+	public boolean isImmutableType() {
+		return false;
+	}
+
+	@Override
+	public Record createInstance() {
+		return new Record();
+	}
+
+	@Override
+	public Record copy(Record from) {
+		return copy(from, new Record());
+	}
+
+	@Override
+	public Record copy(Record from, Record reuse) {
+		from.copyTo(reuse);
+		return reuse;
+	}
+
+	@Override
+	public int getLength() {
+		return -1;
+	}
+
+	@Override
+	public void serialize(Record record, DataOutputView target) throws IOException {
+		record.write(target);
+	}
+
+	@Override
+	public Record deserialize(DataInputView source) throws IOException {
+		return deserialize(new Record(), source);
+	}
+
+	@Override
+	public Record deserialize(Record reuse, DataInputView source) throws IOException {
+		reuse.read(source);
+		return reuse;
+	}
+
+	@Override
+	public void copy(DataInputView source, DataOutputView target) throws IOException {
+		if (tmp == null) {
+			tmp = createInstance();
+		}
+
+		tmp.copy(source, target);
+	}
+
+	@Override
+	public boolean canEqual(Object obj) {
+		return obj instanceof RecordSerializer;
+	}
+}
