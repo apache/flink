0d6af444c613fe93e48a59a8ac49ef0cceb6e289
Phetsarath, Sourigna <Gna.Phetsarath@teamaol.com>
2016-05-12 17:10:59 -0400

    Revert "Squash commit"
    This reverts commit 9e19f8d5c616598b16cd6f2cf1f2d43df4155f74.

--- flink-libraries/flink-table/src/main/scala/org/apache/flink/api/table/plan/nodes/dataset/DataSetRel.scala
+++ flink-libraries/flink-table/src/main/scala/org/apache/flink/api/table/plan/nodes/dataset/DataSetRel.scala
@@ -25,9 +25,9 @@ import org.apache.flink.api.common.functions.MapFunction
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.java.DataSet
 import org.apache.flink.api.table.codegen.CodeGenerator
+import org.apache.flink.api.table.{BatchTableEnvironment, TableConfig, TableEnvironment}
 import org.apache.flink.api.table.plan.nodes.FlinkRel
 import org.apache.flink.api.table.runtime.MapRunner
-import org.apache.flink.api.table.{BatchTableEnvironment, TableConfig}
 
 import scala.collection.JavaConversions._
 
@@ -69,7 +69,6 @@ trait DataSetRel extends RelNode with FlinkRel {
 
   private[dataset] def getConversionMapper(
       config: TableConfig,
-      nullableInput: Boolean,
       inputType: TypeInformation[Any],
       expectedType: TypeInformation[Any],
       conversionOperatorName: String,
@@ -78,7 +77,6 @@ trait DataSetRel extends RelNode with FlinkRel {
 
     val generator = new CodeGenerator(
       config,
-      nullableInput,
       inputType,
       None,
       inputPojoFieldMapping)
