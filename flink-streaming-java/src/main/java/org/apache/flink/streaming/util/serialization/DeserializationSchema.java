24e9f833a4ce4004b861c07772aa842e4169577b
Stephan Ewen <sewen@apache.org>
2016-05-19 12:37:05 +0200

    [FLINK-3933] [streaming API] Add AbstractDeserializationSchema that handles produced type extraction.
    The AbstractDeserializationSchema creates the produced type information automatically from the
    generic parameters.

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/util/serialization/DeserializationSchema.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/util/serialization/DeserializationSchema.java
@@ -28,6 +28,9 @@ import org.apache.flink.api.java.typeutils.ResultTypeQueryable;
  * data sources (for example Apache Kafka) into data types (Java/Scala objects) that are
  * processed by Flink.
  * 
+ * <p>Note: In most cases, one should start from {@link AbstractDeserializationSchema}, which
+ * takes care of producing the return type information automatically.
+ * 
  * @param <T> The type created by the deserialization schema.
  */
 @Public
