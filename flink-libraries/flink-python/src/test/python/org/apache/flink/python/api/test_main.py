784a602167f396cdcd1201509d3c122a5a85248f
omaralvarez <omar.alvarez@udc.es>
2016-06-02 11:09:08 +0200

    [FLINK-4002] [py] Improve testing infraestructure

--- flink-libraries/flink-python/src/test/python/org/apache/flink/python/api/test_main.py
+++ flink-libraries/flink-python/src/test/python/org/apache/flink/python/api/test_main.py
@@ -29,6 +29,12 @@ import struct
 #Utilities
 class Id(MapFunction):
     def map(self, value):
+        """
+        Simple map function to forward test results.
+
+        :param value: Input value.
+        :return: Forwarded value.
+        """
         return value
 
 
@@ -39,10 +45,19 @@ class Verify(MapPartitionFunction):
         self.name = name
 
     def map_partition(self, iterator, collector):
+        """
+        Compares elements in the expected values list against actual values in resulting DataSet.
+        
+        :param iterator: Iterator for the corresponding DataSet partition.
+        :param collector: Collector for the result records.
+        """
         index = 0
         for value in iterator:
-            if value != self.expected[index]:
-                raise Exception(self.name + " Test failed. Expected: " + str(self.expected[index]) + " Actual: " + str(value))
+            try:
+                if value != self.expected[index]:
+                    raise Exception(self.name + " Test failed. Expected: " + str(self.expected[index]) + " Actual: " + str(value))
+            except IndexError:
+                raise Exception(self.name + " Test failed. Discrepancy in the number of elements between expected and actual values.")
             index += 1
         #collector.collect(self.name + " successful!")
 
@@ -54,6 +69,16 @@ class Verify2(MapPartitionFunction):
         self.name = name
 
     def map_partition(self, iterator, collector):
+        """
+        Compares elements in the expected values list against actual values in resulting DataSet.
+
+        This function does not compare element by element, since for example in a Union order is not guaranteed.
+
+        Elements are removed from the expected values list for the whole DataSet. 
+        
+        :param iterator: Iterator for the corresponding DataSet partition.
+        :param collector: Collector for the result records.
+        """
         for value in iterator:
             if value in self.expected:
                 try:
