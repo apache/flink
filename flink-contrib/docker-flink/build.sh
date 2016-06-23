15bf574bfec2b12ee50f4a21f1b1a43421f632e2
Ismaël Mejía <iemejia@gmail.com>
2016-06-23 18:39:50 +0200

    [FLINK-4118] Update docker image to 1.0.3 and remove unneeded deps
    Some of the changes include:
    
    - Remove unneeded dependencies (nano, wget)
    - Remove apt lists to reduce image size
    - Reduce number of layers on the docker image (best docker practice)
    - Remove useless variables and base the code in generic ones e.g.
    FLINK_HOME
    - Change the default JDK from oracle to openjdk-8-jre-headless, based on
    two reasons:
    
    1. You cannot legally repackage the oracle jdk in docker images
    2. The open-jdk headless is more appropriate for a server image (no GUI stuff)
    
    - Return port assignation to the standard FLINK one:
    
    Variable: docker-flink -> flink
    
    taskmanager.rpc.port: 6121 -> 6122
    taskmanager.data.port: 6122 -> 6121
    jobmanager.web.port: 8080 -> 8081

--- flink-contrib/docker-flink/build.sh
+++ flink-contrib/docker-flink/build.sh
@@ -23,9 +23,6 @@
 docker-compose kill
 #docker rm $(docker ps -a -q)
 
-#make sure the config file script is executable
-chmod +x flink/config-flink.sh
-
 #rebuild images
-docker build -t="base" base
-docker build -t="flink" flink
+docker build -t "base" base
+docker build -t "flink" flink
