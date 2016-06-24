bbb33f8befae7672f2c9948eb3b8595a8a9745a7
zentol <chesnay@apache.org>
2016-06-24 16:03:52 +0200

    added test case

--- flink-core/src/test/java/org/apache/flink/metrics/reporter/JMXReporterTest.java
+++ flink-core/src/test/java/org/apache/flink/metrics/reporter/JMXReporterTest.java
@@ -18,9 +18,19 @@
 
 package org.apache.flink.metrics.reporter;
 
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.metrics.Gauge;
+import org.apache.flink.metrics.MetricRegistry;
+import org.apache.flink.metrics.groups.TaskManagerMetricGroup;
+import org.apache.flink.metrics.util.TestReporter;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+
+import static org.apache.flink.metrics.MetricRegistry.KEY_METRICS_REPORTER_CLASS;
+import static org.junit.Assert.assertEquals;
 
 public class JMXReporterTest {
 
@@ -50,5 +60,52 @@ public class JMXReporterTest {
 		String jmxName = JMXReporter.generateJmxName("TestMetric", scope);
 
 		assertEquals("org.apache.flink.metrics:key0=value0,key1=value1,key2=value2_(test)------,name=TestMetric", jmxName);
+	}
+
+	/**
+	 * Verifies that multiple JMXReporters can be started on the same machine and register metrics at the MBeanServer.
+     */
+	@Test
+	public void testPortConflictHandling() throws Exception {
+		Configuration cfg = new Configuration();
+		cfg.setString(KEY_METRICS_REPORTER_CLASS, TestReporter.class.getName());
+		MetricRegistry reg = new MetricRegistry(cfg);
+
+		TaskManagerMetricGroup mg = new TaskManagerMetricGroup(reg, "host", "tm");
+
+		JMXReporter rep1 = new JMXReporter();
+		JMXReporter rep2 = new JMXReporter();
+
+		Configuration cfg1 = new Configuration();
+		Configuration cfg2 = new Configuration();
+
+		rep1.open(cfg1);
+		rep2.open(cfg2);
+
+		rep1.notifyOfAddedMetric(new Gauge<Integer>() {
+			@Override
+			public Integer getValue() {
+				return 1;
+			}
+		}, "rep1", new TaskManagerMetricGroup(reg, "host", "tm"));
+
+		rep2.notifyOfAddedMetric(new Gauge<Integer>() {
+			@Override
+			public Integer getValue() {
+				return 2;
+			}
+		}, "rep2", new TaskManagerMetricGroup(reg, "host", "tm"));
+
+		MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+
+		ObjectName objectName1 = new ObjectName(JMXReporter.generateJmxName("rep1", mg.getScopeComponents()));
+		ObjectName objectName2 = new ObjectName(JMXReporter.generateJmxName("rep2", mg.getScopeComponents()));
+
+		assertEquals(1, mBeanServer.getAttribute(objectName1, "Value"));
+		assertEquals(2, mBeanServer.getAttribute(objectName2, "Value"));
+		
+		rep1.close();
+		rep2.close();
+		reg.shutdown();
 	}
 }
