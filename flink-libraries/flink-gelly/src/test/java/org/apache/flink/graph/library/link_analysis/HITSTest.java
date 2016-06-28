cb57970ae547ba7749e1809320b70fa155c5e781
Greg Hogan <code@greghogan.com>
2016-06-28 08:50:36 -0400

    Ensure all tests check double deltas

--- flink-libraries/flink-gelly/src/test/java/org/apache/flink/graph/library/link_analysis/HITSTest.java
+++ flink-libraries/flink-gelly/src/test/java/org/apache/flink/graph/library/link_analysis/HITSTest.java
@@ -20,15 +20,16 @@ package org.apache.flink.graph.library.link_analysis;
 
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.Utils.ChecksumHashCode;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.DataSetUtils;
 import org.apache.flink.graph.asm.AsmTestBase;
 import org.apache.flink.graph.library.link_analysis.HITS.Result;
-import org.apache.flink.test.util.TestBaseUtils;
 import org.apache.flink.types.IntValue;
 import org.apache.flink.types.LongValue;
 import org.apache.flink.types.NullValue;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -42,15 +43,19 @@ extends AsmTestBase {
 		DataSet<Result<IntValue>> hits = new HITS<IntValue, NullValue, NullValue>(10)
 			.run(directedSimpleGraph);
 
-		String expectedResult =
-			"(0,(0.5446287864731747,0.0))\n" +
-			"(1,(0.0,0.8363240238999012))\n" +
-			"(2,(0.6072453524686667,0.26848532437604833))\n" +
-			"(3,(0.5446287864731747,0.39546603929699625))\n" +
-			"(4,(0.0,0.26848532437604833))\n" +
-			"(5,(0.194966796646811,0.0))";
+		List<Tuple2<Double, Double>> expectedResults = new ArrayList<>();
+		expectedResults.add(Tuple2.of(0.5446287864731747, 0.0));
+		expectedResults.add(Tuple2.of(0.0, 0.8363240238999012));
+		expectedResults.add(Tuple2.of(0.6072453524686667,0.26848532437604833));
+		expectedResults.add(Tuple2.of(0.5446287864731747,0.39546603929699625));
+		expectedResults.add(Tuple2.of(0.0, 0.26848532437604833));
+		expectedResults.add(Tuple2.of(0.194966796646811, 0.0));
 
-		TestBaseUtils.compareResultAsText(hits.collect(), expectedResult);
+		for (Result<IntValue> result : hits.collect()) {
+			int id = result.f0.getValue();
+			assertEquals(expectedResults.get(id).f0, result.getHubScore().getValue(), 0.000001);
+			assertEquals(expectedResults.get(id).f1, result.getAuthorityScore().getValue(), 0.000001);
+		}
 	}
 
 	@Test
