21d6a292844521c6f2665a9f5b0f2867659afe5e
Stefan Richter <s.richter@data-artisans.com>
2016-07-13 10:23:17 +0200

    [hotfix] Reuse getMaxJvmHeapMemory() in EnvironmentInformation#getSizeOfFreeHeapMemory()
    This closes #2235

--- flink-runtime/src/main/java/org/apache/flink/runtime/util/EnvironmentInformation.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/util/EnvironmentInformation.java
@@ -160,23 +160,7 @@ public class EnvironmentInformation {
 	 */
 	public static long getSizeOfFreeHeapMemory() {
 		Runtime r = Runtime.getRuntime();
-		long maxMemory = r.maxMemory();
-
-		if (maxMemory == Long.MAX_VALUE) {
-			// amount of free memory unknown
-			try {
-				// workaround for Oracle JDK
-				OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
-				Class<?> clazz = Class.forName("com.sun.management.OperatingSystemMXBean");
-				Method method = clazz.getMethod("getTotalPhysicalMemorySize");
-				maxMemory = (Long) method.invoke(operatingSystemMXBean) / 4;
-			} catch (Throwable e) {
-				throw new RuntimeException("Could not determine the amount of free memory.\n" +
-						"Please set the maximum memory for the JVM, e.g. -Xmx512M for 512 megabytes.");
-			}
-		}
-
-		return maxMemory - r.totalMemory() + r.freeMemory();
+		return getMaxJvmHeapMemory() - r.totalMemory() + r.freeMemory();
 	}
 
 	/**
