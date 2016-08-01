950e07b9e4df0a3df062c74017cd856cc9efa059
wrighe3 <eron.wright@emc.com>
2016-08-01 11:09:47 -0700

    [FLINK-1984] Mesos ResourceManager - T1 milestone (2)
    Add license information

--- flink-mesos/src/main/java/org/apache/flink/mesos/util/MesosConfiguration.java
+++ flink-mesos/src/main/java/org/apache/flink/mesos/util/MesosConfiguration.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.mesos.util;
 
 import org.apache.mesos.MesosSchedulerDriver;
@@ -9,6 +27,9 @@ import scala.Option;
 
 import java.util.Map;
 
+/**
+ * The typed configuration settings associated with a Mesos scheduler.
+ */
 public class MesosConfiguration {
 
 	private String masterUrl;
@@ -27,14 +48,33 @@ public class MesosConfiguration {
 		this.credential = credential;
 	}
 
+	/**
+	 * The Mesos connection string.
+	 *
+	 * The value should be in one of the following forms:
+	 * <pre>
+	 * {@code
+	 *     host:port
+	 *     zk://host1:port1,host2:port2,.../path
+	 *     zk://username:password@host1:port1,host2:port2,.../path
+	 *     file:///path/to/file (where file contains one of the above)
+	 * }
+	 * </pre>
+     */
 	public String masterUrl() {
 		return masterUrl;
 	}
 
+	/**
+	 * The framework registration info.
+     */
 	public Protos.FrameworkInfo.Builder frameworkInfo() {
 		return frameworkInfo;
 	}
 
+	/**
+	 * The credential to authenticate the framework principal.
+     */
 	public Option<Protos.Credential.Builder> credential() {
 		return credential;
 	}
