68addf39e0e5f9e1656818f923be362680ed93b0
Stephan Ewen <sewen@apache.org>
2016-08-10 20:42:45 +0200

    [FLINK-4355] [cluster management] Implement TaskManager side of registration at ResourceManager.
    This closes #2353

--- flink-runtime/src/main/java/org/apache/flink/runtime/rpc/RpcEndpoint.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/rpc/RpcEndpoint.java
@@ -237,7 +237,6 @@ public abstract class RpcEndpoint<C extends RpcGateway> {
 	 * }</pre>
 	 */
 	public void validateRunsInMainThread() {
-		// because the initialization is lazy, it can be that certain methods are
 		assert currentMainThread.get() == Thread.currentThread();
 	}
 
