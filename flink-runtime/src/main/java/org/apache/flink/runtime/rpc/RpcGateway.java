2452014057432a061c55caa70ac5140e78b3cbd5
Till Rohrmann <trohrmann@apache.org>
2016-08-18 16:34:47 +0200

    [FLINK-4414] [cluster] Add getAddress method to RpcGateway
    The RpcGateway.getAddress method allows to retrieve the fully qualified address of the
    associated RpcEndpoint.
    
    This closes #2392.

--- flink-runtime/src/main/java/org/apache/flink/runtime/rpc/RpcGateway.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/rpc/RpcGateway.java
@@ -22,4 +22,11 @@ package org.apache.flink.runtime.rpc;
  * Rpc gateway interface which has to be implemented by Rpc gateways.
  */
 public interface RpcGateway {
+
+	/**
+	 * Returns the fully qualified address under which the associated rpc endpoint is reachable.
+	 *
+	 * @return Fully qualified address under which the associated rpc endpoint is reachable
+	 */
+	String getAddress();
 }
