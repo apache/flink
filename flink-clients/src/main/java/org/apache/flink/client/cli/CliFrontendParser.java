f2d106ab700e1cbe3ac9c2defa90d718c40978f3
chobeat <simone.robutti@gmail.com>
2016-09-07 11:12:54 +0200

    Checkstyle import

--- flink-clients/src/main/java/org/apache/flink/client/cli/CliFrontendParser.java
+++ flink-clients/src/main/java/org/apache/flink/client/cli/CliFrontendParser.java
@@ -17,7 +17,12 @@
  */
 package org.apache.flink.client.cli;
 
-import org.apache.commons.cli.*;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.ParseException;
 import org.apache.flink.client.CliFrontend;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
