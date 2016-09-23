e2c506249d59fff52b80295356434459abfe05fd
Ramkrishna <ramkrishna.s.vasudevan@intel.com>
2016-09-23 16:08:10 +0530

    Changes to hanging test case

--- flink-runtime/src/test/java/org/apache/flink/runtime/operators/CachedMatchTaskTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/operators/CachedMatchTaskTest.java
@@ -302,7 +302,7 @@ public class CachedMatchTaskTest extends DriverTestBase<FlatJoinFunction<Record,
 			@Override
 			public void run() {
 				try {
-					testResettableDriver(testTask, MockFailingMatchStub.class, 1);
+					testDriver(testTask, MockFailingMatchStub.class);
 					success.set(true);
 				} catch (Exception ie) {
 					ie.printStackTrace();
@@ -346,7 +346,7 @@ public class CachedMatchTaskTest extends DriverTestBase<FlatJoinFunction<Record,
 			@Override
 			public void run() {
 				try {
-					testResettableDriver(testTask, MockMatchStub.class, 1);
+					testDriver(testTask, MockMatchStub.class);
 					success.set(true);
 				} catch (Exception ie) {
 					ie.printStackTrace();
@@ -390,7 +390,7 @@ public class CachedMatchTaskTest extends DriverTestBase<FlatJoinFunction<Record,
 			@Override
 			public void run() {
 				try {
-					testResettableDriver(testTask, MockMatchStub.class, 1);
+					testDriver(testTask, MockMatchStub.class);
 					success.set(true);
 				} catch (Exception ie) {
 					ie.printStackTrace();
@@ -435,7 +435,7 @@ public class CachedMatchTaskTest extends DriverTestBase<FlatJoinFunction<Record,
 			@Override
 			public void run() {
 				try {
-					testResettableDriver(testTask, MockMatchStub.class, 3);
+					testResettableDriver(testTask, MockMatchStub.class, 1);
 					success.set(true);
 				} catch (Exception ie) {
 					ie.printStackTrace();
