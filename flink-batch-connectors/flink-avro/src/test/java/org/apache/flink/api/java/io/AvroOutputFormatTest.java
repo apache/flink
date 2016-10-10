e68460f257552ab77440669f97c646c9d2137baf
larsbachmann <lars.bachmann@posteo.de>
2016-10-10 18:52:18 +0200

    FLINK-4771: pull request code review changes

--- flink-batch-connectors/flink-avro/src/test/java/org/apache/flink/api/java/io/AvroOutputFormatTest.java
+++ flink-batch-connectors/flink-avro/src/test/java/org/apache/flink/api/java/io/AvroOutputFormatTest.java
@@ -1,14 +1,19 @@
 package org.apache.flink.api.java.io;
 
+import static org.apache.flink.api.java.io.AvroOutputFormat.Codec;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import org.apache.avro.file.CodecFactory;
+import org.apache.flink.api.io.avro.example.User;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.core.fs.Path;
@@ -20,13 +25,13 @@ import org.junit.Test;
 public class AvroOutputFormatTest {
 
     @Test
-    public void testSetCodecFactory() throws Exception {
+    public void testSetCodec() throws Exception {
         // given
-        final AvroOutputFormat<DummyAvroType> outputFormat = new AvroOutputFormat<>(DummyAvroType.class);
+        final AvroOutputFormat<User> outputFormat = new AvroOutputFormat<>(User.class);
 
         // when
         try {
-            outputFormat.setCodecFactory(CodecFactory.snappyCodec());
+            outputFormat.setCodec(Codec.SNAPPY);
         } catch (Exception ex) {
             // then
             fail("unexpected exception");
@@ -34,14 +39,14 @@ public class AvroOutputFormatTest {
     }
 
     @Test
-    public void testSetCodecFactoryError() throws Exception {
+    public void testSetCodecError() throws Exception {
         // given
         boolean error = false;
-        final AvroOutputFormat<DummyAvroType> outputFormat = new AvroOutputFormat<>(DummyAvroType.class);
+        final AvroOutputFormat<User> outputFormat = new AvroOutputFormat<>(User.class);
 
         // when
         try {
-            outputFormat.setCodecFactory(null);
+            outputFormat.setCodec(null);
         } catch (Exception ex) {
             error = true;
         }
@@ -51,16 +56,48 @@ public class AvroOutputFormatTest {
     }
 
     @Test
+    public void testSerialization() throws Exception {
+
+        serializeAndDeserialize(null);
+        for (final Codec codec : Codec.values()) {
+            serializeAndDeserialize(codec);
+        }
+    }
+
+    private void serializeAndDeserialize(final Codec codec) throws IOException, ClassNotFoundException {
+        // given
+        final AvroOutputFormat<User> outputFormat = new AvroOutputFormat<>(User.class);
+        if (codec != null) {
+            outputFormat.setCodec(codec);
+        }
+        outputFormat.setSchema(User.SCHEMA$);
+
+        final File serialized = File.createTempFile("avro-output-format", ".serialized");
+
+        // when
+        try (final ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(serialized))) {
+            oos.writeObject(outputFormat);
+        }
+        try (final ObjectInputStream ois = new ObjectInputStream(new FileInputStream(serialized))) {
+            // then
+            assertTrue(ois.readObject() instanceof AvroOutputFormat);
+        }
+
+        // cleanup
+        Files.delete(Paths.get(serialized.getPath()));
+    }
+
+    @Test
     public void testCompression() throws Exception {
         // given
-        final Path outputPath = path("avro-output-file.avro");
-        final AvroOutputFormat<DummyAvroType> outputFormat = new AvroOutputFormat<>(outputPath, DummyAvroType.class);
+        final Path outputPath = new Path(File.createTempFile("avro-output-file", "avro").getAbsolutePath());
+        final AvroOutputFormat<User> outputFormat = new AvroOutputFormat<>(outputPath, User.class);
         outputFormat.setWriteMode(FileSystem.WriteMode.OVERWRITE);
 
-        final Path compressedOutputPath = path("avro-output-file-compressed.avro");
-        final AvroOutputFormat<DummyAvroType> compressedOutputFormat = new AvroOutputFormat<>(compressedOutputPath, DummyAvroType.class);
+        final Path compressedOutputPath = new Path(File.createTempFile("avro-output-file", "compressed.avro").getAbsolutePath());
+        final AvroOutputFormat<User> compressedOutputFormat = new AvroOutputFormat<>(compressedOutputPath, User.class);
         compressedOutputFormat.setWriteMode(FileSystem.WriteMode.OVERWRITE);
-        compressedOutputFormat.setCodecFactory(CodecFactory.snappyCodec());
+        compressedOutputFormat.setCodec(Codec.SNAPPY);
 
         // when
         output(outputFormat);
@@ -68,39 +105,22 @@ public class AvroOutputFormatTest {
 
         // then
         assertTrue(fileSize(outputPath) > fileSize(compressedOutputPath));
+
+        // cleanup
+        Files.delete(Paths.get(outputPath.getPath()));
+        Files.delete(Paths.get(compressedOutputPath.getPath()));
     }
 
     private long fileSize(Path path) throws IOException {
         return Files.size(Paths.get(path.getPath()));
     }
 
-    private void output(final AvroOutputFormat<DummyAvroType> outputFormat) throws IOException {
+    private void output(final AvroOutputFormat<User> outputFormat) throws IOException {
         outputFormat.configure(new Configuration());
         outputFormat.open(1,1);
         for (int i = 0; i < 100; i++) {
-            outputFormat.writeRecord(new DummyAvroType(1));
+            outputFormat.writeRecord(new User("testUser", 1, "blue"));
         }
         outputFormat.close();
-    }
-
-    private Path path(final String virtualPath) throws URISyntaxException {
-        return new Path(Paths.get(getClass().getResource("/").toURI()).toString() + "/" + virtualPath);
-    }
-
-    private static class DummyAvroType {
-
-        private int id;
-
-        public DummyAvroType(final int id) {
-            this.id = id;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(final int id) {
-            this.id = id;
-        }
     }
 }
