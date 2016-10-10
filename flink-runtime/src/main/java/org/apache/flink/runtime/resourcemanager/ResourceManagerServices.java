66f152930b31d002beabd78bbe231042207f8289
Maximilian Michels <mxm@apache.org>
2016-10-10 17:36:10 +0200

    [FLINK-4516] update leadership information in ResourceManager
    The leadership information remained static for connected
    JobMasters. This updates it to remove stale JobMasters when they lose
    leadership status.
    
    This closes #2624

--- flink-runtime/src/main/java/org/apache/flink/runtime/resourcemanager/ResourceManagerServices.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/resourcemanager/ResourceManagerServices.java
@@ -19,12 +19,18 @@ package org.apache.flink.runtime.resourcemanager;
 
 import org.apache.flink.runtime.clusterframework.types.ResourceProfile;
 
+import java.util.UUID;
 import java.util.concurrent.Executor;
 
 /**
  * Interface which provides access to services of the ResourceManager.
  */
 public interface ResourceManagerServices {
+
+	/**
+	 * Gets the current leader id assigned at the ResourceManager.
+	 */
+	UUID getLeaderID();
 
 	/**
 	 * Allocates a resource according to the resource profile.
