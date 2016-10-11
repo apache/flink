1dda3ad009667697a620359e997e83a5ba2447dd
twalthr <twalthr@apache.org>
2016-10-11 15:33:20 +0200

    [FLINK-4793] [types] Improve lambda constructor reference handling
    This closes #2621.

--- flink-core/src/main/java/org/apache/flink/api/common/functions/util/FunctionUtils.java
+++ flink-core/src/main/java/org/apache/flink/api/common/functions/util/FunctionUtils.java
@@ -18,8 +18,6 @@
 
 package org.apache.flink.api.common.functions.util;
 
-import java.lang.reflect.Method;
-
 import org.apache.flink.annotation.Internal;
 import org.apache.flink.api.common.functions.Function;
 import org.apache.flink.api.common.functions.RichFunction;
@@ -60,73 +58,6 @@ public final class FunctionUtils {
 		}
 		else {
 			return defaultContext;
-		}
-	}
-	
-	public static Method checkAndExtractLambdaMethod(Function function) {
-		try {
-			// get serialized lambda
-			Object serializedLambda = null;
-			for (Class<?> clazz = function.getClass(); clazz != null; clazz = clazz.getSuperclass()) {
-				try {
-					Method replaceMethod = clazz.getDeclaredMethod("writeReplace");
-					replaceMethod.setAccessible(true);
-					Object serialVersion = replaceMethod.invoke(function);
-
-					// check if class is a lambda function
-					if (serialVersion.getClass().getName().equals("java.lang.invoke.SerializedLambda")) {
-
-						// check if SerializedLambda class is present
-						try {
-							Class.forName("java.lang.invoke.SerializedLambda");
-						}
-						catch (Exception e) {
-							throw new UnsupportedOperationException("User code tries to use lambdas, but framework is running with a Java version < 8");
-						}
-						serializedLambda = serialVersion;
-						break;
-					}
-				}
-				catch (NoSuchMethodException e) {
-					// thrown if the method is not there. fall through the loop
-				}
-			}
-
-			// not a lambda method -> return null
-			if (serializedLambda == null) {
-				return null;
-			}
-
-			// find lambda method
-			Method implClassMethod = serializedLambda.getClass().getDeclaredMethod("getImplClass");
-			Method implMethodNameMethod = serializedLambda.getClass().getDeclaredMethod("getImplMethodName");
-
-			String className = (String) implClassMethod.invoke(serializedLambda);
-			String methodName = (String) implMethodNameMethod.invoke(serializedLambda);
-
-			Class<?> implClass = Class.forName(className.replace('/', '.'), true, Thread.currentThread().getContextClassLoader());
-
-			Method[] methods = implClass.getDeclaredMethods();
-			Method parameterizedMethod = null;
-			for (Method method : methods) {
-				if(method.getName().equals(methodName)) {
-					if(parameterizedMethod != null) {
-						// It is very unlikely that a class contains multiple e.g. "lambda$2()" but its possible
-						// Actually, the signature need to be checked, but this is very complex
-						throw new Exception("Lambda method name is not unique.");
-					}
-					else {
-						parameterizedMethod = method;
-					}
-				}
-			}
-			if (parameterizedMethod == null) {
-				throw new Exception("No lambda method found.");
-			}
-			return parameterizedMethod;
-		}
-		catch (Exception e) {
-			throw new RuntimeException("Could not extract lambda method out of function: " + e.getClass().getSimpleName() + " - " + e.getMessage(), e);
 		}
 	}
 
