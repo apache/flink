dea99c21bc54ffe7ac35a642f8e519a24503732b
twalthr <twalthr@apache.org>
2016-10-13 18:03:29 +0200

    Convert Java to Scala classes

---
+++ flink-libraries/flink-table/src/main/scala/org/apache/flink/api/java/table/windows/Tumble.scala
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.api.java.table.windows
+
+import org.apache.flink.api.table.TumblingWindow
+
+/**
+  * Helper class for creating a tumbling window. In a tumbling window elements are assigned to
+  * non-overlapping windows of a specified fixed length. For example, if you specify a tumbling
+  * window of 5 minutes size, elements will be grouped in 5 minutes intervals.
+  */
+object Tumble {
+
+  /**
+    * Creates a tumbling window. In a tumbling window elements are assigned to
+    * non-overlapping windows of a specified fixed length. For example, if you specify a tumbling
+    * window of 5 minutes size, elements will be grouped in 5 minutes intervals.
+    *
+    * @param size size of the window either as number of rows or interval of milliseconds
+    * @return a tumbling window
+    */
+  def over(size: String): TumblingWindow = new TumblingWindow(size)
+}
