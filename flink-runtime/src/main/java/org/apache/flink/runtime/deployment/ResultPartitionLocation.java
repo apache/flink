0f9378fa48c344b37977b8e07b93adac762965ac
shuai.xus <shuai.xus@alibaba-inc.com>
2016-10-17 17:54:28 +0800

    [FLINK-4444][runtime]Add a InputChannel and ResultSubPartition for DFS

--- flink-runtime/src/main/java/org/apache/flink/runtime/deployment/ResultPartitionLocation.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/deployment/ResultPartitionLocation.java
@@ -56,6 +56,7 @@ public class ResultPartitionLocation implements Serializable {
 	private enum LocationType {
 		LOCAL,
 		REMOTE,
+		DFS,
 		UNKNOWN
 	}
 
@@ -72,6 +73,10 @@ public class ResultPartitionLocation implements Serializable {
 		return new ResultPartitionLocation(LocationType.LOCAL, null);
 	}
 
+	public static ResultPartitionLocation createDFS() {
+		return new ResultPartitionLocation(LocationType.DFS, null);
+	}
+
 	public static ResultPartitionLocation createUnknown() {
 		return new ResultPartitionLocation(LocationType.UNKNOWN, null);
 	}
@@ -84,6 +89,10 @@ public class ResultPartitionLocation implements Serializable {
 
 	public boolean isRemote() {
 		return locationType == LocationType.REMOTE;
+	}
+
+	public boolean isDFS() {
+		return locationType == LocationType.DFS;
 	}
 
 	public boolean isUnknown() {
