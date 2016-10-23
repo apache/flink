e107e7242e6aa69161c669acadbac08295278fb8
Thomas FOURNIER <tfournier@nextperf.com>
2016-10-23 14:59:29 +0200

    Try changes typo

--- flink-libraries/flink-ml/src/main/scala/org/apache/flink/ml/MLUtils.scala
+++ flink-libraries/flink-ml/src/main/scala/org/apache/flink/ml/MLUtils.scala
@@ -27,102 +27,102 @@ import org.apache.flink.ml.math.SparseVector
 import org.apache.flink.util.Collector
 
 /** Convenience functions for machine learning tasks
-  *
-  * This object contains convenience functions for machine learning tasks:
-  *
-  * - readLibSVM:
-  * Reads a libSVM/SVMLight input file and returns a data set of [[LabeledVector]].
-  * The file format is specified [http://svmlight.joachims.org/ here].
-  *
-  * - writeLibSVM:
-  * Writes a data set of [[LabeledVector]] in libSVM/SVMLight format to disk. THe file format
-  * is specified [http://svmlight.joachims.org/ here].
-  */
+ *
+ * This object contains convenience functions for machine learning tasks:
+ *
+ * - readLibSVM:
+ * Reads a libSVM/SVMLight input file and returns a data set of [[LabeledVector]].
+ * The file format is specified [http://svmlight.joachims.org/ here].
+ *
+ * - writeLibSVM:
+ * Writes a data set of [[LabeledVector]] in libSVM/SVMLight format to disk. THe file format
+ * is specified [http://svmlight.joachims.org/ here].
+ */
 object MLUtils {
 
   val DIMENSION = "dimension"
 
   /** Reads a file in libSVM/SVMLight format and converts the data into a data set of
-    * [[LabeledVector]]. The dimension of the [[LabeledVector]] is determined automatically.
-    *
-    * Since the libSVM/SVMLight format stores a vector in its sparse form, the [[LabeledVector]]
-    * will also be instantiated with a [[SparseVector]].
-    *
-    * @param env executionEnvironment [[ExecutionEnvironment]]
-    * @param filePath Path to the input file
-    * @return [[DataSet]] of [[LabeledVector]] containing the information of the libSVM/SVMLight
-    *         file
-    */
-  def readLibSVM(env: ExecutionEnvironment, filePath: String): DataSet[LabeledVector] = {
-    val labelCOODS = env.readTextFile(filePath).flatMap(
+   * [[LabeledVector]]. The dimension of the [[LabeledVector]] is determined automatically.
+   *
+   * Since the libSVM/SVMLight format stores a vector in its sparse form, the [[LabeledVector]]
+   * will also be instantiated with a [[SparseVector]].
+   *
+   * @param env executionEnvironment [[ExecutionEnvironment]]
+   * @param filePath Path to the input file
+   * @return [[DataSet]] of [[LabeledVector]] containing the information of the libSVM/SVMLight
+   *         file
+   */
+  def readLibSVM(env: ExecutionEnvironment, filePath: String): DataSet[LabeledVector] ={
+    val labelCOODS = env.readTextFile( filePath ).flatMap(
       new RichFlatMapFunction[String, (Double, Array[(Int, Double)])] {
         val splitPattern = "\\s+".r
 
         override def flatMap(
           line: String,
           out: Collector[(Double, Array[(Int, Double)])]
-        ): Unit = {
-          val commentFreeLine = line.takeWhile(_ != '#').trim
+        ): Unit ={
+          val commentFreeLine = line.takeWhile( _ != '#' ).trim
 
           if (commentFreeLine.nonEmpty) {
-            val splits = splitPattern.split(commentFreeLine)
+            val splits = splitPattern.split( commentFreeLine )
             val label = splits.head.toDouble
             val sparseFeatures = splits.tail
             val coos = sparseFeatures.flatMap { str =>
-              val pair = str.split(':')
-              require(pair.length == 2, "Each feature entry has to have the form <feature>:<value>")
+              val pair = str.split( ':' )
+              require( pair.length == 2, "Each feature entry has to have the form <feature>:<value>" )
 
               // libSVM index is 1-based, but we expect it to be 0-based
-              val index = pair(0).toInt - 1
-              val value = pair(1).toDouble
+              val index = pair( 0 ).toInt - 1
+              val value = pair( 1 ).toDouble
 
-              Some((index, value))
+              Some( (index, value) )
             }
 
-            out.collect((label, coos))
+            out.collect( (label, coos) )
           }
         }
-      })
+      } )
 
     // Calculate maximum dimension of vectors
     val dimensionDS = labelCOODS.map {
       labelCOO =>
-        labelCOO._2.map(_._1 + 1).max
-    }.reduce(scala.math.max(_, _))
+        labelCOO._2.map( _._1 + 1 ).max
+    }.reduce( scala.math.max( _, _ ) )
 
     labelCOODS.map {
       new RichMapFunction[(Double, Array[(Int, Double)]), LabeledVector] {
         var dimension = 0
 
-        override def open(configuration: Configuration): Unit = {
-          dimension = getRuntimeContext.getBroadcastVariable(DIMENSION).get(0)
+        override def open(configuration: Configuration): Unit ={
+          dimension = getRuntimeContext.getBroadcastVariable( DIMENSION ).get( 0 )
         }
 
-        override def map(value: (Double, Array[(Int, Double)])): LabeledVector = {
-          new LabeledVector(value._1, SparseVector.fromCOO(dimension, value._2))
+        override def map(value: (Double, Array[(Int, Double)])): LabeledVector ={
+          new LabeledVector( value._1, SparseVector.fromCOO( dimension, value._2 ) )
         }
       }
-    }.withBroadcastSet(dimensionDS, DIMENSION)
+    }.withBroadcastSet( dimensionDS, DIMENSION )
   }
 
   /** Writes a [[DataSet]] of [[LabeledVector]] to a file using the libSVM/SVMLight format.
-    *
-    * @param filePath Path to output file
-    * @param labeledVectors [[DataSet]] of [[LabeledVector]] to write to disk
-    * @return
-    */
-  def writeLibSVM(filePath: String, labeledVectors: DataSet[LabeledVector]): DataSink[String] = {
+   *
+   * @param filePath Path to output file
+   * @param labeledVectors [[DataSet]] of [[LabeledVector]] to write to disk
+   * @return
+   */
+  def writeLibSVM(filePath: String, labeledVectors: DataSet[LabeledVector]): DataSink[String] ={
     val stringRepresentation = labeledVectors.map {
       labeledVector =>
         val vectorStr = labeledVector.vector.
           // remove zero entries
-          filter(_._2 != 0).
+          filter( _._2 != 0 ).
           map { case (idx, value) => (idx + 1) + ":" + value }.
-          mkString(" ")
+          mkString( " " )
 
         labeledVector.label + " " + vectorStr
     }
 
-    stringRepresentation.writeAsText(filePath)
+    stringRepresentation.writeAsText( filePath )
   }
 }
