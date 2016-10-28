59a5551ef81a57007035526ab2c14ec07f13971b
kl0u <kkloudas@gmail.com>
2016-10-28 14:10:25 +0200

    [hotfix] Fixes the TimestampedInputSplit.EOS comparison.
    This closes #2718

--- flink-tests/src/test/java/org/apache/flink/test/checkpointing/TimestampedFileInputSplitTest.java
+++ flink-tests/src/test/java/org/apache/flink/test/checkpointing/TimestampedFileInputSplitTest.java
@@ -17,11 +17,16 @@
  */
 package org.apache.flink.test.checkpointing;
 
-import org.apache.flink.core.fs.FileInputSplit;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.functions.source.TimestampedFileInputSplit;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class TimestampedFileInputSplitTest {
 
@@ -58,7 +63,7 @@ public class TimestampedFileInputSplitTest {
 	@Test
 	public void testSplitComparison() {
 		TimestampedFileInputSplit richFirstSplit =
-			new TimestampedFileInputSplit(10, 3, new Path("test/test1"), 0, 100, null);
+			new TimestampedFileInputSplit(0, 3, new Path("test/test1"), 0, 100, null);
 
 		TimestampedFileInputSplit richSecondSplit =
 			new TimestampedFileInputSplit(10, 2, new Path("test/test2"), 0, 100, null);
@@ -69,15 +74,32 @@ public class TimestampedFileInputSplitTest {
 		TimestampedFileInputSplit richForthSplit =
 			new TimestampedFileInputSplit(11, 0, new Path("test/test3"), 0, 100, null);
 
-		// lexicographically on the path order
+		TimestampedFileInputSplit richFifthSplit =
+			new TimestampedFileInputSplit(11, 1, new Path("test/test3"), 0, 100, null);
+
+		// smaller mod time
 		Assert.assertTrue(richFirstSplit.compareTo(richSecondSplit) < 0);
-		Assert.assertTrue(richFirstSplit.compareTo(richThirdSplit) < 0);
+
+		// lexicographically on the path
+		Assert.assertTrue(richThirdSplit.compareTo(richFifthSplit) < 0);
 
 		// same mod time, same file so smaller split number first
 		Assert.assertTrue(richThirdSplit.compareTo(richSecondSplit) < 0);
 
 		// smaller modification time first
 		Assert.assertTrue(richThirdSplit.compareTo(richForthSplit) < 0);
+
+		Assert.assertTrue(richFirstSplit.compareTo(TimestampedFileInputSplit.EOS) < 0);
+		Assert.assertTrue(richSecondSplit.compareTo(TimestampedFileInputSplit.EOS) < 0);
+		Assert.assertTrue(richThirdSplit.compareTo(TimestampedFileInputSplit.EOS) < 0);
+		Assert.assertTrue(richForthSplit.compareTo(TimestampedFileInputSplit.EOS) < 0);
+
+		Assert.assertEquals(0, TimestampedFileInputSplit.EOS.compareTo(TimestampedFileInputSplit.EOS));
+
+		Assert.assertTrue(TimestampedFileInputSplit.EOS.compareTo(richFirstSplit) > 0);
+		Assert.assertTrue(TimestampedFileInputSplit.EOS.compareTo(richSecondSplit) > 0);
+		Assert.assertTrue(TimestampedFileInputSplit.EOS.compareTo(richThirdSplit) > 0);
+		Assert.assertTrue(TimestampedFileInputSplit.EOS.compareTo(richForthSplit) > 0);
 	}
 
 	@Test
@@ -89,5 +111,56 @@ public class TimestampedFileInputSplitTest {
 				Assert.fail(e.getMessage());
 			}
 		}
+	}
+
+	@Test
+	public void testPriorityQ() {
+		TimestampedFileInputSplit richFirstSplit =
+			new TimestampedFileInputSplit(0, 3, new Path("test/test1"), 0, 100, null);
+
+		TimestampedFileInputSplit richSecondSplit =
+			new TimestampedFileInputSplit(10, 2, new Path("test/test2"), 0, 100, null);
+
+		TimestampedFileInputSplit richThirdSplit =
+			new TimestampedFileInputSplit(10, 1, new Path("test/test2"), 0, 100, null);
+
+		TimestampedFileInputSplit richForthSplit =
+			new TimestampedFileInputSplit(11, 0, new Path("test/test3"), 0, 100, null);
+
+		TimestampedFileInputSplit richFifthSplit =
+			new TimestampedFileInputSplit(11, 1, new Path("test/test3"), 0, 100, null);
+
+		TimestampedFileInputSplit eos = TimestampedFileInputSplit.EOS;
+
+		Queue<TimestampedFileInputSplit> pendingSplits = new PriorityQueue<>();
+
+		pendingSplits.add(eos);
+		pendingSplits.add(richSecondSplit);
+		pendingSplits.add(richForthSplit);
+		pendingSplits.add(eos);
+		pendingSplits.add(richFirstSplit);
+		pendingSplits.add(richFifthSplit);
+		pendingSplits.add(richFifthSplit);
+		pendingSplits.add(richThirdSplit);
+
+		List<TimestampedFileInputSplit> actualSortedSplits = new ArrayList<>();
+		while (true) {
+			actualSortedSplits.add(pendingSplits.poll());
+			if (pendingSplits.isEmpty()) {
+				break;
+			}
+		}
+
+		List<TimestampedFileInputSplit> expectedSortedSplits = new ArrayList<>();
+		expectedSortedSplits.add(richFirstSplit);
+		expectedSortedSplits.add(richThirdSplit);
+		expectedSortedSplits.add(richSecondSplit);
+		expectedSortedSplits.add(richForthSplit);
+		expectedSortedSplits.add(richFifthSplit);
+		expectedSortedSplits.add(richFifthSplit);
+		expectedSortedSplits.add(eos);
+		expectedSortedSplits.add(eos);
+
+		Assert.assertArrayEquals(expectedSortedSplits.toArray(), actualSortedSplits.toArray());
 	}
 }
