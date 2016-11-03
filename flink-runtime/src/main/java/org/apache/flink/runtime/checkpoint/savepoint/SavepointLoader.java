f4c336b16bb9219f438551ba069707c21e555de5
Ufuk Celebi <uce@apache.org>
2016-11-03 13:24:04 +0100

    [hotfix] [checkpointing] Fix error message in SavepointLoader
    The message referred to parallelism although max parallelism is
    checked.

--- flink-runtime/src/main/java/org/apache/flink/runtime/checkpoint/savepoint/SavepointLoader.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/checkpoint/savepoint/SavepointLoader.java
@@ -74,12 +74,12 @@ public class SavepointLoader {
 					String msg = String.format("Failed to rollback to savepoint %s. " +
 									"Max parallelism mismatch between savepoint state and new program. " +
 									"Cannot map operator %s with max parallelism %d to new program with " +
-									"parallelism %d. This indicates that the program has been changed " +
+									"max parallelism %d. This indicates that the program has been changed " +
 									"in a non-compatible way after the savepoint.",
 							savepoint,
 							taskState.getJobVertexID(),
 							taskState.getMaxParallelism(),
-							executionJobVertex.getParallelism());
+							executionJobVertex.getMaxParallelism());
 
 					throw new IllegalStateException(msg);
 				}
