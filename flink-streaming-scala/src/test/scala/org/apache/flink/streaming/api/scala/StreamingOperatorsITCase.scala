5bbe38a5f0f09b33f73d2c9ca1c9541e90e52a36
shijinkui <shijinkui@huawei.com>
2016-11-26 16:14:13 +0800

    [FLINK-5168] Scaladoc annotation link use [[]] instead of {@link}
    This closes #2875

--- flink-streaming-scala/src/test/scala/org/apache/flink/streaming/api/scala/StreamingOperatorsITCase.scala
+++ flink-streaming-scala/src/test/scala/org/apache/flink/streaming/api/scala/StreamingOperatorsITCase.scala
@@ -58,8 +58,8 @@ class StreamingOperatorsITCase extends ScalaStreamingMultipleProgramsTestBase {
     * The stream is grouped by the first field. For each group, the resulting stream is folded by
     * summing up the second tuple field.
     *
-    * This test relies on the hash function used by the {@link DataStream#keyBy}, which is
-    * assumed to be {@link MathUtils#murmurHash}.
+    * This test relies on the hash function used by the [[DataStream#keyBy]], which is
+    * assumed to be [[MathUtils#murmurHash]].
     */
   @Test
   def testGroupedFoldOperator(): Unit = {
