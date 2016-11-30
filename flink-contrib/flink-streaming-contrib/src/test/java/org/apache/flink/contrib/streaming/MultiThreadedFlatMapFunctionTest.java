84792e6dbe44921a7c998c048c47b65b6f381115
Vipul Modi <vipulm@qubole.com>
2016-11-30 21:42:09 +0530

    Initial try

---
+++ flink-contrib/flink-streaming-contrib/src/test/java/org/apache/flink/contrib/streaming/MultiThreadedFlatMapFunctionTest.java
@@ -0,0 +1,16 @@
+package org.apache.flink.contrib.streaming;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.operators.DataSource;
+import org.junit.Test;
+
+/**
+ * Created by vipulmodi on 11/30/16.
+ */
+public class MultiThreadedFlatMapFunctionTest {
+
+    @Test
+    public void testMultiThreadedFlatMap() {
+        new DataSource<Integer>()
+    }
+}
