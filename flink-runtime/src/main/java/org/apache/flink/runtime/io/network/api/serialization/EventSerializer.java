d100433c9062332fae04a5c0a30c9edfd4c4fe52
Nico Kruber <nico@data-artisans.com>
2016-12-08 14:17:34 +0100

    [FLINK-5066] add EventSerializer.isEvent
    This allows PartitionRequestQueue to peak into event buffers instead of de-serializing the full event class.

--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/api/serialization/EventSerializer.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/api/serialization/EventSerializer.java
@@ -91,6 +91,65 @@ public class EventSerializer {
 		}
 	}
 
+	/**
+	 * Identifies whether the given buffer encodes the given event.
+	 *
+	 * @param buffer the buffer to peak into
+	 * @param eventClass the expected class of the event type
+	 * @param classLoader the class loader to use for custom event classes
+	 * @return whether the event class of the <tt>buffer</tt> matches the given <tt>eventClass</tt>
+	 * @throws IOException
+	 */
+	public static boolean isEvent(ByteBuffer buffer, Class<?> eventClass, ClassLoader classLoader) throws IOException {
+		if (buffer.remaining() < 4) {
+			throw new IOException("Incomplete event");
+		}
+
+		final ByteOrder bufferOrder = buffer.order();
+		buffer.order(ByteOrder.BIG_ENDIAN);
+
+		try {
+			int type = buffer.getInt();
+
+			switch (type) {
+				case END_OF_PARTITION_EVENT:
+					return eventClass.equals(EndOfPartitionEvent.class);
+				case CHECKPOINT_BARRIER_EVENT:
+					return eventClass.equals(CheckpointBarrier.class);
+				case END_OF_SUPERSTEP_EVENT:
+					return eventClass.equals(EndOfSuperstepEvent.class);
+				case CANCEL_CHECKPOINT_MARKER_EVENT:
+					return eventClass.equals(CancelCheckpointMarker.class);
+				case OTHER_EVENT:
+					try {
+						final DataInputDeserializer deserializer = new DataInputDeserializer(buffer);
+						final String className = deserializer.readUTF();
+
+						final Class<? extends AbstractEvent> clazz;
+						try {
+							clazz = classLoader.loadClass(className).asSubclass(AbstractEvent.class);
+						}
+						catch (ClassNotFoundException e) {
+							throw new IOException("Could not load event class '" + className + "'.", e);
+						}
+						catch (ClassCastException e) {
+							throw new IOException("The class '" + className + "' is not a valid subclass of '"
+								+ AbstractEvent.class.getName() + "'.", e);
+						}
+						return eventClass.equals(clazz);
+					}
+					catch (Exception e) {
+						throw new IOException("Error while deserializing or instantiating event.", e);
+					}
+				default:
+					throw new IOException("Corrupt byte stream for event");
+			}
+		}
+		finally {
+			buffer.order(bufferOrder);
+		}
+	}
+
 	public static AbstractEvent fromSerializedEvent(ByteBuffer buffer, ClassLoader classLoader) throws IOException {
 		if (buffer.remaining() < 4) {
 			throw new IOException("Incomplete event");
@@ -169,5 +228,18 @@ public class EventSerializer {
 
 	public static AbstractEvent fromBuffer(Buffer buffer, ClassLoader classLoader) throws IOException {
 		return fromSerializedEvent(buffer.getNioBuffer(), classLoader);
+	}
+
+	/**
+	 * Identifies whether the given buffer encodes the given event.
+	 *
+	 * @param buffer the buffer to peak into
+	 * @param eventClass the expected class of the event type
+	 * @param classLoader the class loader to use for custom event classes
+	 * @return whether the event class of the <tt>buffer</tt> matches the given <tt>eventClass</tt>
+	 * @throws IOException
+	 */
+	public static boolean isEvent(Buffer buffer, Class<?> eventClass, ClassLoader classLoader) throws IOException {
+		return isEvent(buffer.getNioBuffer(), eventClass, classLoader);
 	}
 }
