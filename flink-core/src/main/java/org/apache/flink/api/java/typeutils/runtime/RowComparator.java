a88a8f4478886c529db5b3eb938ea562ef18aa9b
Jark Wu <wuchong.wc@alibaba-inc.com>
2016-12-13 13:36:06 +0800

    remove unused ArrayList import

--- flink-core/src/main/java/org/apache/flink/api/java/typeutils/runtime/RowComparator.java
+++ flink-core/src/main/java/org/apache/flink/api/java/typeutils/runtime/RowComparator.java
@@ -28,7 +28,6 @@ import org.apache.flink.types.KeyFieldOutOfBoundsException;
 import org.apache.flink.types.Row;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
