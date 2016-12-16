dd470ec4a77155ece90b24b54dc6695df5125aa6
Jark Wu <wuchong.wc@alibaba-inc.com>
2016-12-16 17:24:01 +0800

    [FLINK-5348] [core] Support custom field names for RowTypeInfo

--- flink-core/src/test/java/org/apache/flink/api/java/typeutils/RowTypeInfoTest.java
+++ flink-core/src/test/java/org/apache/flink/api/java/typeutils/RowTypeInfoTest.java
@@ -18,12 +18,86 @@
 package org.apache.flink.api.java.typeutils;
 
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeutils.CompositeType;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class RowTypeInfoTest {
+	private static List<TypeInformation<?>> typeList = new ArrayList<>();
+
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		typeList.add(BasicTypeInfo.INT_TYPE_INFO);
+		typeList.add(new RowTypeInfo(
+			BasicTypeInfo.SHORT_TYPE_INFO,
+			BasicTypeInfo.BIG_DEC_TYPE_INFO));
+		typeList.add(BasicTypeInfo.STRING_TYPE_INFO);
+	}
+
+
+	@Test
+	public void testDuplicateCustomFieldNames() {
+		// test number of field names should be equal to number of types
+		try {
+			new RowTypeInfo(typeList, Arrays.asList("int", "string"));
+			fail("Expected an IllegalArgumentException, but no exception is thrown.");
+		} catch (Exception e) {
+			assertTrue(e instanceof IllegalArgumentException);
+		}
+
+		// test field names should not be the same.
+		try {
+			new RowTypeInfo(typeList, Arrays.asList("int", "string", "string"));
+			fail("Expected an IllegalArgumentException, but no exception is thrown.");
+		} catch (Exception e) {
+			assertTrue(e instanceof IllegalArgumentException);
+		}
+	}
+
+	@Test
+	public void testCustomFieldNames() {
+		RowTypeInfo typeInfo1 = new RowTypeInfo(typeList, Arrays.asList("int", "row", "string"));
+		assertArrayEquals(new String[] {"int", "row", "string"}, typeInfo1.getFieldNames());
+
+		assertEquals(BasicTypeInfo.STRING_TYPE_INFO, typeInfo1.getTypeAt("string"));
+		assertEquals(BasicTypeInfo.STRING_TYPE_INFO, typeInfo1.getTypeAt(2));
+		assertEquals(BasicTypeInfo.SHORT_TYPE_INFO, typeInfo1.getTypeAt("row.0"));
+		assertEquals(BasicTypeInfo.BIG_DEC_TYPE_INFO, typeInfo1.getTypeAt("row.f1"));
+
+		List<CompositeType.FlatFieldDescriptor> result = new ArrayList<>();
+		typeInfo1.getFlatFields("row.*", 0, result);
+		assertEquals(2, result.size());
+	}
+
+	@Test
+	public void testGetTypeAt() {
+		RowTypeInfo typeInfo = new RowTypeInfo(
+			BasicTypeInfo.INT_TYPE_INFO,
+			new RowTypeInfo(
+				BasicTypeInfo.SHORT_TYPE_INFO,
+				BasicTypeInfo.BIG_DEC_TYPE_INFO
+			),
+			BasicTypeInfo.STRING_TYPE_INFO);
+
+
+		assertArrayEquals(new String[] {"f0", "f1", "f2"}, typeInfo.getFieldNames());
+
+		assertEquals(BasicTypeInfo.STRING_TYPE_INFO, typeInfo.getTypeAt("f2"));
+		assertEquals(BasicTypeInfo.SHORT_TYPE_INFO, typeInfo.getTypeAt("f1.f0"));
+		assertEquals(BasicTypeInfo.BIG_DEC_TYPE_INFO, typeInfo.getTypeAt("f1.1"));
+	}
 
 	@Test
 	public void testRowTypeInfoEquality() {
