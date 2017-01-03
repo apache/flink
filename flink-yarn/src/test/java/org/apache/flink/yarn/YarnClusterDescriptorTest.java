deb04f971764a675b5e39a5609465f0179fa8d02
Nico Kruber <nico@data-artisans.com>
2017-01-03 11:53:23 +0100

    [FLINK-3150] add a test for AbstractYarnClusterDescriptor#setupApplicationMasterContainer

--- flink-yarn/src/test/java/org/apache/flink/yarn/YarnClusterDescriptorTest.java
+++ flink-yarn/src/test/java/org/apache/flink/yarn/YarnClusterDescriptorTest.java
@@ -20,7 +20,9 @@ package org.apache.flink.yarn;
 import org.apache.flink.configuration.ConfigConstants;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.IllegalConfigurationException;
+import org.apache.flink.yarn.cli.FlinkYarnSessionCli;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.yarn.api.ApplicationConstants;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -29,6 +31,8 @@ import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 
 public class YarnClusterDescriptorTest {
 
@@ -88,5 +92,127 @@ public class YarnClusterDescriptorTest {
 			e.printStackTrace();
 			Assert.assertTrue(e.getCause() instanceof IllegalConfigurationException);
 		}
+	}
+
+	@Test
+	public void testSetupApplicationMasterContainer() {
+		YarnClusterDescriptor clusterDescriptor = new YarnClusterDescriptor();
+		final Configuration cfg = new Configuration();
+		clusterDescriptor.setFlinkConfiguration(cfg);
+
+		final String java = "$JAVA_HOME/bin/java";
+		final String jvmmem = "-Xmx424M";
+		final String jvmOpts = "-Djvm"; // if set
+		final String krb5 = "-Djava.security.krb5.conf=krb5.conf";
+		final String logfile =
+			"-Dlog.file=\"" + ApplicationConstants.LOG_DIR_EXPANSION_VAR +
+				"/jobmanager.log\""; // if set
+		final String logback =
+			"-Dlogback.configurationFile=file:" + FlinkYarnSessionCli.CONFIG_FILE_LOGBACK_NAME; // if set
+		final String log4j =
+			"-Dlog4j.configuration=file:" + FlinkYarnSessionCli.CONFIG_FILE_LOG4J_NAME; // if set
+		final String mainClass = clusterDescriptor.getApplicationMasterClass().getName();
+		final String redirects =
+			" 1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/jobmanager.out" +
+			" 2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/jobmanager.err";
+
+		// no logging, with/out krb5
+		assertEquals(
+			java + " " + jvmmem +
+				" " + // jvmOpts
+				" " + mainClass + " " + redirects,
+			clusterDescriptor
+				.setupApplicationMasterContainer(false, false, false)
+				.getCommands().get(0));
+
+		assertEquals(
+			java + " " + jvmmem +
+				" " + // jvmOpts
+				" " + krb5 +
+				" " + mainClass + " " + redirects,
+			clusterDescriptor
+				.setupApplicationMasterContainer(false, false, true)
+				.getCommands().get(0));
+
+		// logback only, with/out krb5
+		assertEquals(
+			java + " " + jvmmem +
+				" " + // jvmOpts
+				" " + logfile + " " + logback +
+				" " + mainClass + " " + redirects,
+			clusterDescriptor
+				.setupApplicationMasterContainer(true, false, false)
+				.getCommands().get(0));
+
+		assertEquals(
+			java + " " + jvmmem +
+				" " + // jvmOpts
+				" " + logfile + " " + logback +
+				" " + krb5 +
+				" " + mainClass + " " + redirects,
+			clusterDescriptor
+				.setupApplicationMasterContainer(true, false, true)
+				.getCommands().get(0));
+
+		// log4j, with/out krb5
+		assertEquals(
+			java + " " + jvmmem +
+				" " + // jvmOpts
+				" " + logfile + " " + log4j +
+				" " + mainClass + " " + redirects,
+			clusterDescriptor
+				.setupApplicationMasterContainer(false, true, false)
+				.getCommands().get(0));
+
+		assertEquals(
+			java + " " + jvmmem +
+				" " + // jvmOpts
+				" " + logfile + " " + log4j +
+				" " + krb5 +
+				" " + mainClass + " " + redirects,
+			clusterDescriptor
+				.setupApplicationMasterContainer(false, true, true)
+				.getCommands().get(0));
+
+		// logback + log4j, with/out krb5
+		assertEquals(
+			java + " " + jvmmem +
+				" " + // jvmOpts
+				" " + logfile + " " + logback + " " + log4j +
+				" " + mainClass + " " + redirects,
+			clusterDescriptor
+				.setupApplicationMasterContainer(true, true, false)
+				.getCommands().get(0));
+
+		assertEquals(
+			java + " " + jvmmem +
+				" " + // jvmOpts
+				" " + logfile + " " + logback + " " + log4j +
+				" " + krb5 +
+				" " + mainClass + " " + redirects,
+			clusterDescriptor
+				.setupApplicationMasterContainer(true, true, true)
+				.getCommands().get(0));
+
+		// logback + log4j, with/out krb5, different JVM opts
+		cfg.setString(ConfigConstants.FLINK_JVM_OPTIONS, jvmOpts);
+		assertEquals(
+			java + " " + jvmmem +
+				" " + jvmOpts +
+				" " + logfile + " " + logback + " " + log4j +
+				" " + mainClass + " " + redirects,
+			clusterDescriptor
+				.setupApplicationMasterContainer(true, true, false)
+				.getCommands().get(0));
+
+		assertEquals(
+			java + " " + jvmmem +
+				" " + jvmOpts + // jvmOpts
+				" " + logfile + " " + logback + " " + log4j +
+				" " + krb5 +
+				" " + mainClass + " " + redirects,
+			clusterDescriptor
+				.setupApplicationMasterContainer(true, true, true)
+				.getCommands().get(0));
 	}
 }
