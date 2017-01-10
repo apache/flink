e4ed2142447daf585cdd0c76fadc559429f8ac11
mtunique <oatgnem@gmail.com>
2017-01-10 23:05:44 +0800

    [FLINK-4917] Deprecate "CheckpointedAsynchronously" interface

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/checkpoint/CheckpointedAsynchronously.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/checkpoint/CheckpointedAsynchronously.java
@@ -37,5 +37,6 @@ import java.io.Serializable;
  * {@link #snapshotState(long, long)} method is typically a copy or shadow copy
  * of the actual state.</p>
  */
+@Deprecated
 @PublicEvolving
 public interface CheckpointedAsynchronously<T extends Serializable> extends Checkpointed<T> {}
