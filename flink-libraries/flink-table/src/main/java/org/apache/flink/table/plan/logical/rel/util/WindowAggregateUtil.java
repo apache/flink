9c0dc0b17824ec967635c93974004533db3118b9
rtudoran <tudoranradu@ymail.com>
2017-02-09 17:57:49 +0100

    partition verification

--- flink-libraries/flink-table/src/main/java/org/apache/flink/table/plan/logical/rel/util/WindowAggregateUtil.java
+++ flink-libraries/flink-table/src/main/java/org/apache/flink/table/plan/logical/rel/util/WindowAggregateUtil.java
@@ -20,17 +20,13 @@ package org.apache.flink.table.plan.logical.rel.util;
 import java.io.Serializable;
 import java.util.List;
 
-import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.Window.Group;
 import org.apache.calcite.rel.core.Window.RexWinAggCall;
 import org.apache.calcite.rel.logical.LogicalWindow;
 import org.apache.calcite.rel.type.RelDataType;
-import org.apache.calcite.rel.type.RelDataTypeField;
-import org.apache.calcite.rex.RexLiteral;
-import org.apache.calcite.rex.RexVariable;
-import org.apache.calcite.sql.type.SqlTypeName;
-import org.apache.calcite.rex.RexCall;
 import org.apache.calcite.rex.RexInputRef;
+import org.apache.calcite.rex.RexLiteral;
+import org.apache.calcite.sql.type.SqlTypeName;
 import org.apache.flink.api.java.tuple.Tuple5;
 
 import com.google.common.collect.ImmutableList;
@@ -38,6 +34,17 @@ import com.google.common.collect.ImmutableList;
 public class WindowAggregateUtil implements Serializable {
 
 	private static final long serialVersionUID = -3916551736243544540L;
+
+	private LogicalWindow windowPointer = null;
+
+	public WindowAggregateUtil() {
+
+	}
+
+	public WindowAggregateUtil(LogicalWindow window) {
+		this.windowPointer = window;
+
+	}
 
 	public List<Tuple5<String, String, String, Integer, Integer>> getAggregateFunctions(LogicalWindow window) {
 
@@ -76,25 +83,46 @@ public class WindowAggregateUtil implements Serializable {
 	}
 
 	/**
-	 * This method returns the [[int]] lowerbound of a window when expressed with an integer e.g.
-	 * ... ROWS BETWEEN [[value]] PRECEDING AND CURRENT ROW
+	 * This method returns the [[int]] lowerbound of a window when expressed
+	 * with an integer e.g. ... ROWS BETWEEN [[value]] PRECEDING AND CURRENT ROW
 	 * 
-	 * @param group The group for analyzed window from which keys are extracted
-	 * @param constants the list of constant to get the offset value
+	 * @param group
+	 *            The group for analyzed window from which keys are extracted
+	 * @param constants
+	 *            the list of constant to get the offset value
 	 * @return return the value of the lowerbound if available -1 otherwise
 	 */
 	public int getLowerBoundary(Group group, ImmutableList<RexLiteral> constants) {
 		Integer lowerBoundKey = group.keys.asList().get(0);
 		Object lowerbound = constants.get(lowerBoundKey).getValue2();
-		Object offset =group.lowerBound.getOffset();
-		
-		if(offset instanceof RexInputRef) { 
+		Object offset = group.lowerBound.getOffset();
+
+		if (offset instanceof RexInputRef) {
 			RelDataType type = ((RexInputRef) offset).getType();
-			if(type.getSqlTypeName().equals(SqlTypeName.INTEGER)) {
+			if (type.getSqlTypeName().equals(SqlTypeName.INTEGER)) {
 				return Integer.parseInt(lowerbound.toString());
 			}
 		}
 		return -1;
+	}
+
+	
+	/**
+	 * This method return the keys based on which the object is partitioned or null
+	 * 
+	 * @return return null for non-partitioned windows or the list of key indexes
+	 */
+	public List<Integer> getPartitions() {
+		Group windowBoundaries = windowPointer.groups.asList().get(0);
+		List<Integer> partitionKeys = null;
+
+		if (windowBoundaries.keys.isEmpty()) {
+			// partitionKeys = null is indicator of non partitioned OVER clause
+		} else {
+			partitionKeys = windowBoundaries.keys.asList();
+		}
+
+		return partitionKeys;
 	}
 
 }
