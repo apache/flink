33ea78ea37fab819a329b09aa213e61c16252067
shuai.xus <shuai.xus@alibaba-inc.com>
2017-02-14 14:56:41 +0800

    [FLINK-5793] [runtime] fix running slot may not be add to AllocatedMap in SlotPool bug
    This closes #3306

--- flink-runtime/src/main/java/org/apache/flink/runtime/instance/SlotPool.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/instance/SlotPool.java
@@ -436,7 +436,9 @@ public class SlotPool extends RpcEndpoint<SlotPoolGateway> {
 					LOG.debug("Fulfilling pending request [{}] early with returned slot [{}]",
 							pendingRequest.allocationID(), taskManagerSlot.getSlotAllocationId());
 
-					pendingRequest.future().complete(createSimpleSlot(taskManagerSlot, Locality.UNKNOWN));
+					SimpleSlot newSlot = createSimpleSlot(taskManagerSlot, Locality.UNKNOWN);
+					allocatedSlots.add(newSlot);
+					pendingRequest.future().complete(newSlot);
 				}
 				else {
 					LOG.debug("Adding returned slot [{}] to available slots", taskManagerSlot.getSlotAllocationId());
@@ -624,6 +626,15 @@ public class SlotPool extends RpcEndpoint<SlotPoolGateway> {
 			result.setLocality(locality);
 		}
 		return result;
+	}
+
+	// ------------------------------------------------------------------------
+	//  Methods for tests
+	// ------------------------------------------------------------------------
+
+	@VisibleForTesting
+	AllocatedSlots getAllocatedSlots() {
+		return allocatedSlots;
 	}
 
 	// ------------------------------------------------------------------------
