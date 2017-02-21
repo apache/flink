0f2c37eac3d81152c3a5a9852d1bcd38681bb5f8
Nico Kruber <nico@data-artisans.com>
2017-02-21 14:35:05 +0100

    [FLINK-5090] Fixups for "Add metrics for details about inbound/outbound network queues"
    This closes #3348.

--- flink-core/src/main/java/org/apache/flink/configuration/ConfigConstants.java
+++ flink-core/src/main/java/org/apache/flink/configuration/ConfigConstants.java
@@ -251,12 +251,6 @@ public final class ConfigConstants {
 	public static final String TASK_MANAGER_NETWORK_NUM_BUFFERS_KEY = "taskmanager.network.numberOfBuffers";
 
 	/**
-	 * Boolean flag to enable/disable more detailed metrics about inbound/outbound network queue lengths
-	 */
-	@PublicEvolving
-	public static final String NETWORK_DETAILED_METRICS_KEY = "taskmanager.net.detailed-metrics";
-
-	/**
 	 * Config parameter defining the size of memory buffers used by the network stack and the memory manager.
 	 *
 	 * @deprecated Use {@link TaskManagerOptions#MEMORY_SEGMENT_SIZE} instead
--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/ResultPartitionMetrics.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/ResultPartitionMetrics.java
@@ -23,15 +23,12 @@ import org.apache.flink.metrics.MetricGroup;
 
 import static org.apache.flink.util.Preconditions.checkNotNull;
 
+/**
+ * Collects metrics of a result partition.
+ */
 public class ResultPartitionMetrics {
 
 	private final ResultPartition partition;
-
-	private int lastMin = -1;
-
-	private int lastMax = -1;
-
-	private float lastAvg = -1.0f;
 
 	// ------------------------------------------------------------------------
 
@@ -41,59 +38,94 @@ public class ResultPartitionMetrics {
 
 	// ------------------------------------------------------------------------
 
-	// these methods are package private to make access from the nested classes faster 
+	// these methods are package private to make access from the nested classes faster
 
-	int refreshAndGetMin() {
-		int min;
-		if ((min = lastMin) == -1) {
-			refresh();
-			min = lastMin;
+	/**
+	 * Iterates over all sub-partitions and collects the total number of queued buffers in a
+	 * best-effort way.
+	 *
+	 * @return total number of queued buffers
+	 */
+	long refreshAndGetTotal() {
+		long total = 0;
+
+		for (ResultSubpartition part : partition.getAllPartitions()) {
+			total += part.unsynchronizedGetNumberOfQueuedBuffers();
 		}
 
-		lastMin = -1;
+		return total;
+	}
+
+	/**
+	 * Iterates over all sub-partitions and collects the minimum number of queued buffers in a
+	 * sub-partition in a best-effort way.
+	 *
+	 * @return minimum number of queued buffers per sub-partition (<tt>0</tt> if sub-partitions exist)
+	 */
+	int refreshAndGetMin() {
+		int min = Integer.MAX_VALUE;
+
+		ResultSubpartition[] allPartitions = partition.getAllPartitions();
+		if (allPartitions.length == 0) {
+			// meaningful value when no channels exist:
+			return 0;
+		}
+
+		for (ResultSubpartition part : allPartitions) {
+			int size = part.unsynchronizedGetNumberOfQueuedBuffers();
+			min = Math.min(min, size);
+		}
+
 		return min;
 	}
 
+	/**
+	 * Iterates over all sub-partitions and collects the maximum number of queued buffers in a
+	 * sub-partition in a best-effort way.
+	 *
+	 * @return maximum number of queued buffers per sub-partition
+	 */
 	int refreshAndGetMax() {
-		int max;
-		if ((max = lastMax) == -1) {
-			refresh();
-			max = lastMax;
-		}
-
-		lastMax = -1;
-		return max;
-	}
-
-	float refreshAndGetAvg() {
-		float avg;
-		if ((avg = lastAvg) < 0.0f) {
-			refresh();
-			avg = lastAvg;
-		}
-
-		lastAvg = -1.0f;
-		return avg;
-	}
-
-	private void refresh() {
-		int min = Integer.MAX_VALUE;
 		int max = 0;
 
 		for (ResultSubpartition part : partition.getAllPartitions()) {
 			int size = part.unsynchronizedGetNumberOfQueuedBuffers();
-			min = Math.min(min, size);
 			max = Math.max(max, size);
 		}
 
-		this.lastMin = min;
-		this.lastMax = max;
-		this.lastAvg = partition.getTotalNumberOfBuffers() / (float) partition.getNumberOfSubpartitions();
+		return max;
+	}
+
+	/**
+	 * Iterates over all sub-partitions and collects the average number of queued buffers in a
+	 * sub-partition in a best-effort way.
+	 *
+	 * @return average number of queued buffers per sub-partition
+	 */
+	float refreshAndGetAvg() {
+		long total = 0;
+
+		ResultSubpartition[] allPartitions = partition.getAllPartitions();
+		for (ResultSubpartition part : allPartitions) {
+			int size = part.unsynchronizedGetNumberOfQueuedBuffers();
+			total += size;
+		}
+
+		return total / (float) allPartitions.length;
 	}
 
 	// ------------------------------------------------------------------------
 	//  Gauges to access the stats
 	// ------------------------------------------------------------------------
+
+	private Gauge<Long> getTotalQueueLenGauge() {
+		return new Gauge<Long>() {
+			@Override
+			public Long getValue() {
+				return refreshAndGetTotal();
+			}
+		};
+	}
 
 	private Gauge<Integer> getMinQueueLenGauge() {
 		return new Gauge<Integer>() {
@@ -129,8 +161,9 @@ public class ResultPartitionMetrics {
 	public static void registerQueueLengthMetrics(MetricGroup group, ResultPartition partition) {
 		ResultPartitionMetrics metrics = new ResultPartitionMetrics(partition);
 
-		group.gauge("min-queue-len", metrics.getMinQueueLenGauge());
-		group.gauge("max-queue-len", metrics.getMaxQueueLenGauge());
-		group.gauge("avg-queue-len", metrics.getAvgQueueLenGauge());
+		group.gauge("totalQueueLen", metrics.getTotalQueueLenGauge());
+		group.gauge("minQueueLen", metrics.getMinQueueLenGauge());
+		group.gauge("maxQueueLen", metrics.getMaxQueueLenGauge());
+		group.gauge("avgQueueLen", metrics.getAvgQueueLenGauge());
 	}
 }
