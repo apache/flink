1cc492182e1b06bc764cced930761a07773da119
zentol <chesnay@apache.org>
2017-03-02 15:10:14 +0100

    add JsonArchivist javadoc

--- flink-runtime/src/main/java/org/apache/flink/runtime/webmonitor/history/JsonArchivist.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/webmonitor/history/JsonArchivist.java
@@ -22,6 +22,25 @@ import org.apache.flink.runtime.executiongraph.AccessExecutionGraph;
 import java.io.IOException;
 import java.util.Collection;
 
+/**
+ * Interface for all classes that want to participate in the archiving of job-related json responses.
+ * 
+ * Note that all JsonArchivists that are to be used for the history server must be added
+ * to {@link WebRuntimeMonitor#getArchivers()}.
+ */
 public interface JsonArchivist {
+
+	/**
+	 * Returns a {@link Collection} of {@link ArchivedJson}s containing JSON responses and their respective REST URL
+	 * for a given job.
+	 *
+	 * The collection should contain one entry for every response that could be generated for the given
+	 * job, for example one entry for each task. The REST URLs should be unique and must not contain placeholders.
+	 *
+	 * @param graph AccessExecutionGraph for which the responses should be generated
+	 *
+	 * @return Collection containing an ArchivedJson for every response that could be generated for the given job
+	 * @throws IOException thrown if the JSON generation fails
+	 */
 	Collection<ArchivedJson> archiveJsonWithPath(AccessExecutionGraph graph) throws IOException;
 }
