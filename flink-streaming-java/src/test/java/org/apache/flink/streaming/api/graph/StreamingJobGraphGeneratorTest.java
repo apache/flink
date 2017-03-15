71d6df5ef8170efa180724fd4d501d4c3bbf0ad1
Stephan Ewen <sewen@apache.org>
2017-03-15 15:25:20 +0100

    [hotfix] [core] Fix/cleanup serialization test for ExecutionConfig

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/api/graph/StreamingJobGraphGeneratorTest.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/api/graph/StreamingJobGraphGeneratorTest.java
@@ -17,7 +17,6 @@
 
 package org.apache.flink.streaming.api.graph;
 
-import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.runtime.io.network.partition.ResultPartitionType;
@@ -27,96 +26,20 @@ import org.apache.flink.runtime.jobgraph.tasks.JobSnapshottingSettings;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
-import org.apache.flink.util.InstantiationUtil;
-import org.apache.flink.util.SerializedValue;
 import org.apache.flink.util.TestLogger;
+
 import org.junit.Test;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @SuppressWarnings("serial")
 public class StreamingJobGraphGeneratorTest extends TestLogger {
-	
-	@Test
-	public void testExecutionConfigSerialization() throws IOException, ClassNotFoundException {
-		final long seed = System.currentTimeMillis();
-		final Random r = new Random(seed);
 
-		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
-		StreamGraph streamingJob = new StreamGraph(env);
-		StreamingJobGraphGenerator compiler = new StreamingJobGraphGenerator(streamingJob);
-		
-		boolean closureCleanerEnabled = r.nextBoolean(), forceAvroEnabled = r.nextBoolean(), forceKryoEnabled = r.nextBoolean(), disableGenericTypes = r.nextBoolean(), objectReuseEnabled = r.nextBoolean(), sysoutLoggingEnabled = r.nextBoolean();
-		int dop = 1 + r.nextInt(10);
-		
-		ExecutionConfig config = streamingJob.getExecutionConfig();
-		if(closureCleanerEnabled) {
-			config.enableClosureCleaner();
-		} else {
-			config.disableClosureCleaner();
-		}
-		if(forceAvroEnabled) {
-			config.enableForceAvro();
-		} else {
-			config.disableForceAvro();
-		}
-		if(forceKryoEnabled) {
-			config.enableForceKryo();
-		} else {
-			config.disableForceKryo();
-		}
-		if(disableGenericTypes) {
-			config.disableGenericTypes();
-		} else {
-			config.enableGenericTypes();
-		}
-		if(objectReuseEnabled) {
-			config.enableObjectReuse();
-		} else {
-			config.disableObjectReuse();
-		}
-		if(sysoutLoggingEnabled) {
-			config.enableSysoutLogging();
-		} else {
-			config.disableSysoutLogging();
-		}
-		config.setParallelism(dop);
-		
-		JobGraph jobGraph = compiler.createJobGraph();
-
-		final String EXEC_CONFIG_KEY = "runtime.config";
-
-		InstantiationUtil.writeObjectToConfig(jobGraph.getSerializedExecutionConfig(),
-			jobGraph.getJobConfiguration(),
-			EXEC_CONFIG_KEY);
-
-		SerializedValue<ExecutionConfig> serializedExecutionConfig = InstantiationUtil.readObjectFromConfig(
-				jobGraph.getJobConfiguration(),
-				EXEC_CONFIG_KEY,
-				Thread.currentThread().getContextClassLoader());
-
-		assertNotNull(serializedExecutionConfig);
-
-		ExecutionConfig executionConfig = serializedExecutionConfig.deserializeValue(getClass().getClassLoader());
-
-		assertEquals(closureCleanerEnabled, executionConfig.isClosureCleanerEnabled());
-		assertEquals(forceAvroEnabled, executionConfig.isForceAvroEnabled());
-		assertEquals(forceKryoEnabled, executionConfig.isForceKryoEnabled());
-		assertEquals(disableGenericTypes, executionConfig.hasGenericTypesDisabled());
-		assertEquals(objectReuseEnabled, executionConfig.isObjectReuseEnabled());
-		assertEquals(sysoutLoggingEnabled, executionConfig.isSysoutLoggingEnabled());
-		assertEquals(dop, executionConfig.getParallelism());
-	}
-	
 	@Test
 	public void testParallelismOneNotChained() {
 
