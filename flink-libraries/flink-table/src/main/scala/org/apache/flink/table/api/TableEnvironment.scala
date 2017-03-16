6cccafd2e97488d838107b77126cf2dd0218a494
Zhuoluo Yang <zhuoluo.yzl@alibaba-inc.com>
2017-03-16 14:34:39 +0800

    [FLINK-6039] [core] Revert some modifications

--- flink-libraries/flink-table/src/main/scala/org/apache/flink/table/api/TableEnvironment.scala
+++ flink-libraries/flink-table/src/main/scala/org/apache/flink/table/api/TableEnvironment.scala
@@ -337,16 +337,15 @@ abstract class TableEnvironment(val config: TableConfig) {
     // check if class could be instantiated
     checkForInstantiation(function.getClass)
 
+    // register in Table API
+    functionCatalog.registerFunction(name, function.getClass)
+
+    // register in SQL API
     val typeInfo: TypeInformation[_] = if (function.getResultType != null) {
       function.getResultType
     } else {
       implicitly[TypeInformation[T]]
     }
-
-    // register in Table API
-    functionCatalog.registerFunction(name, function.getClass)
-
-    // register in SQL API
     val sqlFunctions = createTableSqlFunctions(name, function, typeInfo, typeFactory)
     functionCatalog.registerSqlFunctions(sqlFunctions)
   }
