713335fecb42a19def0bddf5bbcaabb058db7666
DmytroShkvyra <dshkvyra@gmail.com>
2017-03-16 11:43:40 +0200

    [FLINK-4565] Prevent execute test in parallel threads

--- flink-libraries/flink-table/src/test/scala/org/apache/flink/table/api/scala/batch/table/InITCase.scala
+++ flink-libraries/flink-table/src/test/scala/org/apache/flink/table/api/scala/batch/table/InITCase.scala
@@ -19,6 +19,7 @@
 package org.apache.flink.table.api.scala.batch.table
 
 import java.sql.Timestamp
+import javax.annotation.concurrent.NotThreadSafe
 import org.apache.flink.api.scala._
 import org.apache.flink.table.api.scala._
 import org.apache.flink.api.scala.util.CollectionDataSets
