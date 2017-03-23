c8b833e712462e42ea781a61cfd70ca03aa072e4
DmytroShkvyra <dshkvyra@gmail.com>
2017-03-23 19:41:56 +0200

    [FLINK-4565] Fix tests console output

--- flink-libraries/flink-table/src/test/scala/org/apache/flink/table/api/scala/batch/table/InITCase.scala
+++ flink-libraries/flink-table/src/test/scala/org/apache/flink/table/api/scala/batch/table/InITCase.scala
@@ -19,7 +19,6 @@
 package org.apache.flink.table.api.scala.batch.table
 
 import java.sql.Timestamp
-import javax.annotation.concurrent.NotThreadSafe
 import org.apache.flink.api.scala._
 import org.apache.flink.table.api.scala._
 import org.apache.flink.api.scala.util.CollectionDataSets
