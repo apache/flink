d977455629463b0e45194916eae000444108d94a
Stephan Ewen <sewen@apache.org>
2017-03-28 15:09:02 +0200

    [FLINK-6338] [core] Rename SimpleStringUtils to StringValueUtils
    This more intuitivce name clarifies the role of the class.
    This also marks the class as PublicEvolving, as it is not actually used as an internal class.

--- flink-core/src/test/java/org/apache/flink/util/StringValueUtilsTest.java
+++ flink-core/src/test/java/org/apache/flink/util/StringValueUtilsTest.java
@@ -23,31 +23,33 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.apache.flink.types.StringValue;
-import org.apache.flink.util.SimpleStringUtils;
-import org.apache.flink.util.SimpleStringUtils.WhitespaceTokenizer;
+import org.apache.flink.util.StringValueUtils.WhitespaceTokenizer;
+
 import org.junit.Test;
 
-
-public class SimpleStringUtilsTest extends TestLogger {
+/**
+ * Tests for the {@link StringValueUtils}.
+ */
+public class StringValueUtilsTest extends TestLogger {
 
 	@Test
 	public void testToLowerCaseConverting() {
 		StringValue testString = new StringValue("TEST");
-		SimpleStringUtils.toLowerCase(testString);
+		StringValueUtils.toLowerCase(testString);
 		assertEquals(new StringValue("test"), testString);
 	}
 	
 	@Test
 	public void testReplaceNonWordChars() {
 		StringValue testString = new StringValue("TEST123_@");
-		SimpleStringUtils.replaceNonWordChars(testString, '!');
+		StringValueUtils.replaceNonWordChars(testString, '!');
 		assertEquals(new StringValue("TEST123_!"), testString);
 	}
 	
 	@Test
 	public void testTokenizerOnStringWithoutNexToken() {
 		StringValue testString = new StringValue("test");
-		SimpleStringUtils.WhitespaceTokenizer tokenizer = new WhitespaceTokenizer();
+		StringValueUtils.WhitespaceTokenizer tokenizer = new WhitespaceTokenizer();
 		tokenizer.setStringToTokenize(testString);
 		//first token
 		tokenizer.next(testString);
@@ -58,7 +60,7 @@ public class SimpleStringUtilsTest extends TestLogger {
 	@Test
 	public void testTokenizerOnStringWithNexToken() {
 		StringValue testString = new StringValue("test test");
-		SimpleStringUtils.WhitespaceTokenizer tokenizer = new WhitespaceTokenizer();
+		StringValueUtils.WhitespaceTokenizer tokenizer = new WhitespaceTokenizer();
 		tokenizer.setStringToTokenize(testString);
 		assertTrue(tokenizer.next(testString));
 	}
@@ -66,7 +68,7 @@ public class SimpleStringUtilsTest extends TestLogger {
 	@Test
 	public void testTokenizerOnStringOnlyWithDelimiter() {
 		StringValue testString = new StringValue("    ");
-		SimpleStringUtils.WhitespaceTokenizer tokenizer = new WhitespaceTokenizer();
+		StringValueUtils.WhitespaceTokenizer tokenizer = new WhitespaceTokenizer();
 		tokenizer.setStringToTokenize(testString);
 		assertFalse(tokenizer.next(testString));
 	}
