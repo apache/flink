72ebd5e210f583cd7e8df21ea8d73c06e835e198
p4nna <beer@dbs.ifi.lmu.de>
2017-03-30 10:08:49 +0200

    [FLINK-5785] Add an Imputer for preparing data,
    removed unnecessary things and comments, added license

--- flink-libraries/flink-ml/src/main/java/Imputer.java
+++ flink-libraries/flink-ml/src/main/java/Imputer.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package Imputer;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -5,8 +22,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-//import java.util.Set;
-//import java.util.SortedSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -41,44 +56,10 @@ public class Imputer {
 	static ExecutionEnvironment env= ExecutionEnvironment.getExecutionEnvironment();
 	
 	static DataSet<DenseVector> ds = env.fromElements(testvec1, testvec2, testvec3, testvec4, testvec5);
-//	static DataSet<DenseVector> ds = env.fromElements( testvec2, testvec3);
 	private static double[] meansA;
 	private static double[] medians;
 	private static double[] mostValues;
-//	final static ConcurrentHashMap<Integer, Double> meansHM= new ConcurrentHashMap<>();
 
-	
-	public static void main(String[] args){
-		try {
-//			DataSet<DenseVector> dsMean = impute(ds, Strategy.MEAN, 1);
-//			System.out.println("data set mean ");
-//			dsMean.print();
-//			
-			DataSet<DenseVector> dsMedian = impute(ds, Strategy.MEDIAN, 1);
-			System.out.println("data set median ");
-			dsMedian.print();
-//			
-//			DataSet<DenseVector> dsMost = impute(ds, Strategy.MOST_FREQUENT, 1);
-//			System.out.println("data set most frequent ");
-//			dsMost.print();
-//			
-//			DataSet<DenseVector> dsMean0 = impute(ds, Strategy.MEAN, 0);
-//			System.out.println("data set mean ");
-//			dsMean0.print();
-//
-//			DataSet<DenseVector> dsMedian0 = impute(ds, Strategy.MEDIAN, 0);
-//			System.out.println("data set median ");
-//			dsMedian0.print();
-//		
-//			DataSet<DenseVector> dsMax0 = impute(ds, Strategy.MOST_FREQUENT, 0);
-//			System.out.println("data set max ");
-//			dsMax0.print();
-			
-		} catch (Exception e) {
-			System.out.println("here happened an exception");
-			e.printStackTrace();
-		}
-	}
 	
 	/**
 	 * 
