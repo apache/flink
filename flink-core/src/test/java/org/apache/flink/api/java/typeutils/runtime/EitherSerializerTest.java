ae17718fb9262f65b89b4551d2aecaab3ee23d9e
Greg Hogan <code@greghogan.com>
2017-04-05 10:12:04 -0400

    [FLINK-6268] [core] Object reuse for Either type
    Implement object reuse for Flink's Either type by storing a reference to
    Right in Left and Left in Right. These references are private and remain
    null until set by EitherSerializer when copying or deserializing with
    object reuse.
    
    This closes #3680

--- flink-core/src/test/java/org/apache/flink/api/java/typeutils/runtime/EitherSerializerTest.java
+++ flink-core/src/test/java/org/apache/flink/api/java/typeutils/runtime/EitherSerializerTest.java
@@ -18,20 +18,29 @@
 
 package org.apache.flink.api.java.typeutils.runtime;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
-import static org.apache.flink.types.Either.Left;
-import static org.apache.flink.types.Either.Right;
-
 import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.common.typeutils.ComparatorTestBase.TestInputView;
+import org.apache.flink.api.common.typeutils.ComparatorTestBase.TestOutputView;
 import org.apache.flink.api.common.typeutils.SerializerTestInstance;
 import org.apache.flink.api.common.typeutils.TypeSerializer;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.types.Either;
 import org.apache.flink.api.java.typeutils.EitherTypeInfo;
 import org.apache.flink.api.java.typeutils.TupleTypeInfo;
+import org.apache.flink.api.java.typeutils.ValueTypeInfo;
+import org.apache.flink.types.DoubleValue;
+import org.apache.flink.types.Either;
+import org.apache.flink.types.LongValue;
+import org.apache.flink.types.StringValue;
 import org.junit.Test;
+
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertSame;
+import static org.apache.flink.types.Either.Left;
+import static org.apache.flink.types.Either.Right;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 public class EitherSerializerTest {
 
@@ -53,6 +62,26 @@ public class EitherSerializerTest {
 	SerializerTestInstance<Either<String, Double>> testInstance =
 			new EitherSerializerTestInstance<Either<String, Double>>(eitherSerializer, eitherTypeInfo.getTypeClass(), -1, testData);
 	testInstance.testAll();
+	}
+
+	@Test
+	public void testStringValueDoubleValueEither() {
+		@SuppressWarnings("unchecked")
+		Either<StringValue, DoubleValue>[] testData = new Either[] {
+			Left(new StringValue("banana")),
+			Left.of(new StringValue("apple")),
+			new Left(new StringValue("")),
+			Right(new DoubleValue(32.0)),
+			Right.of(new DoubleValue(Double.MIN_VALUE)),
+			new Right(new DoubleValue(Double.MAX_VALUE))};
+
+		EitherTypeInfo<StringValue, DoubleValue> eitherTypeInfo = new EitherTypeInfo<>(
+			ValueTypeInfo.STRING_VALUE_TYPE_INFO, ValueTypeInfo.DOUBLE_VALUE_TYPE_INFO);
+		EitherSerializer<StringValue, DoubleValue> eitherSerializer =
+			(EitherSerializer<StringValue, DoubleValue>) eitherTypeInfo.createSerializer(new ExecutionConfig());
+		SerializerTestInstance<Either<StringValue, DoubleValue>> testInstance =
+			new EitherSerializerTestInstance<>(eitherSerializer, eitherTypeInfo.getTypeClass(), -1, testData);
+		testInstance.testAll();
 	}
 
 	@SuppressWarnings("unchecked")
@@ -78,6 +107,90 @@ public class EitherSerializerTest {
 	testInstance.testAll();
 	}
 
+	@Test
+	public void testEitherWithTupleValues() {
+		@SuppressWarnings("unchecked")
+		Either<Tuple2<LongValue, LongValue>, DoubleValue>[] testData = new Either[] {
+			Left(new Tuple2<>(new LongValue(2L), new LongValue(9L))),
+			new Left<>(new Tuple2<>(new LongValue(Long.MIN_VALUE), new LongValue(Long.MAX_VALUE))),
+			new Right<>(new DoubleValue(32.0)),
+			Right(new DoubleValue(Double.MIN_VALUE)),
+			Right(new DoubleValue(Double.MAX_VALUE))};
+
+		EitherTypeInfo<Tuple2<LongValue, LongValue>, DoubleValue> eitherTypeInfo = new EitherTypeInfo<>(
+			new TupleTypeInfo<Tuple2<LongValue, LongValue>>(ValueTypeInfo.LONG_VALUE_TYPE_INFO, ValueTypeInfo.LONG_VALUE_TYPE_INFO),
+			ValueTypeInfo.DOUBLE_VALUE_TYPE_INFO);
+		EitherSerializer<Tuple2<LongValue, LongValue>, DoubleValue> eitherSerializer =
+			(EitherSerializer<Tuple2<LongValue, LongValue>, DoubleValue>) eitherTypeInfo.createSerializer(new ExecutionConfig());
+		SerializerTestInstance<Either<Tuple2<LongValue, LongValue>, DoubleValue>> testInstance =
+			new EitherSerializerTestInstance<>(eitherSerializer, eitherTypeInfo.getTypeClass(), -1, testData);
+		testInstance.testAll();
+	}
+
+	@Test
+	public void testEitherWithObjectReuse() {
+		EitherTypeInfo<LongValue, DoubleValue> eitherTypeInfo = new EitherTypeInfo<>(
+			ValueTypeInfo.LONG_VALUE_TYPE_INFO, ValueTypeInfo.DOUBLE_VALUE_TYPE_INFO);
+		EitherSerializer<LongValue, DoubleValue> eitherSerializer =
+			(EitherSerializer<LongValue, DoubleValue>) eitherTypeInfo.createSerializer(new ExecutionConfig());
+
+		LongValue lv = new LongValue();
+		DoubleValue dv = new DoubleValue();
+
+		Either<LongValue, DoubleValue> left = Left(lv);
+		Either<LongValue, DoubleValue> right = Right(dv);
+
+		// the first copy creates a new instance of Left
+		Either<LongValue, DoubleValue> copy0 = eitherSerializer.copy(left, right);
+
+		// then the cross-references are used for future copies
+		Either<LongValue, DoubleValue> copy1 = eitherSerializer.copy(right, copy0);
+		Either<LongValue, DoubleValue> copy2 = eitherSerializer.copy(left, copy1);
+
+		// validate reference equality
+		assertSame(right, copy1);
+		assertSame(copy0, copy2);
+
+		// validate reference equality of contained objects
+		assertSame(right.right(), copy1.right());
+		assertSame(copy0.left(), copy2.left());
+	}
+
+	@Test
+	public void testSerializeIndividually() throws IOException {
+		EitherTypeInfo<LongValue, DoubleValue> eitherTypeInfo = new EitherTypeInfo<>(
+			ValueTypeInfo.LONG_VALUE_TYPE_INFO, ValueTypeInfo.DOUBLE_VALUE_TYPE_INFO);
+		EitherSerializer<LongValue, DoubleValue> eitherSerializer =
+			(EitherSerializer<LongValue, DoubleValue>) eitherTypeInfo.createSerializer(new ExecutionConfig());
+
+		LongValue lv = new LongValue();
+		DoubleValue dv = new DoubleValue();
+
+		Either<LongValue, DoubleValue> left = Left(lv);
+		Either<LongValue, DoubleValue> right = Right(dv);
+
+		TestOutputView out = new TestOutputView();
+		eitherSerializer.serialize(left, out);
+		eitherSerializer.serialize(right, out);
+		eitherSerializer.serialize(left, out);
+
+		TestInputView in = out.getInputView();
+		// the first deserialization creates a new instance of Left
+		Either<LongValue, DoubleValue> copy0 = eitherSerializer.deserialize(right, in);
+
+		// then the cross-references are used for future copies
+		Either<LongValue, DoubleValue> copy1 = eitherSerializer.deserialize(copy0, in);
+		Either<LongValue, DoubleValue> copy2 = eitherSerializer.deserialize(copy1, in);
+
+		// validate reference equality
+		assertSame(right, copy1);
+		assertSame(copy0, copy2);
+
+		// validate reference equality of contained objects
+		assertSame(right.right(), copy1.right());
+		assertSame(copy0.left(), copy2.left());
+	}
+
 	/**
 	 * {@link org.apache.flink.api.common.typeutils.SerializerTestBase#testInstantiate()}
 	 * checks that the type of the created instance is the same as the type class parameter.
@@ -98,7 +211,7 @@ public class EitherSerializerTest {
 
 				T instance = serializer.createInstance();
 				assertNotNull("The created instance must not be null.", instance);
-				
+
 				Class<T> type = getTypeClass();
 				assertNotNull("The test is corrupt: type class is null.", type);
 			}
@@ -108,6 +221,6 @@ public class EitherSerializerTest {
 				fail("Exception in test: " + e.getMessage());
 			}
 		}
-		
+
 	}
 }
