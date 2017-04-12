a0685707543cfd99c5ee463b4687cf630f978eeb
Nico Kruber <nico@data-artisans.com>
2017-04-12 18:11:37 +0200

    [FLINK-4545] update the docs with the changed network buffer parameter
    Also update the descriptions of taskmanager.memory.fraction not being relative
    to the full size of taskmanager.heap.mb but that network buffer memory is
    subtracted before!

--- flink-runtime/src/main/java/org/apache/flink/runtime/clusterframework/BootstrapTools.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/clusterframework/BootstrapTools.java
@@ -304,7 +304,7 @@ public class BootstrapTools {
 	 * Get an instance of the dynamic properties option.
 	 *
 	 * Dynamic properties allow the user to specify additional configuration values with -D, such as
-	 *  -Dfs.overwrite-files=true  -Dtaskmanager.network.numberOfBuffers=16368
+	 * <tt> -Dfs.overwrite-files=true  -Dtaskmanager.network.memory.min=536346624</tt>
      */
 	public static Option newDynamicPropertiesOption() {
 		return new Option(DYNAMIC_PROPERTIES_OPT, true, "Dynamic properties");
