f78fd99a97e5fcf09914fc5238a2d975d47fc5fa
FlorianFan <fanzhidongyzby@163.com>
2017-04-30 18:06:22 +0800

    [FLINK-6393] [gelly] Rewrite implementation of CompleteGraph and EvenlyGraph with CirculantGraph Generator

--- flink-libraries/flink-gelly/src/main/java/org/apache/flink/graph/generator/EvenlyGraph.java
+++ flink-libraries/flink-gelly/src/main/java/org/apache/flink/graph/generator/EvenlyGraph.java
@@ -18,21 +18,20 @@
 
 package org.apache.flink.graph.generator;
 
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.functions.FunctionAnnotation.ForwardedFields;
-import org.apache.flink.graph.Edge;
 import org.apache.flink.graph.Graph;
 import org.apache.flink.graph.Vertex;
 import org.apache.flink.types.LongValue;
 import org.apache.flink.types.NullValue;
-import org.apache.flink.util.Collector;
-import org.apache.flink.util.LongValueSequenceIterator;
 import org.apache.flink.util.Preconditions;
 
+import java.util.ArrayList;
+
 /**
- * Evenly graph means every {@link Vertex} in the {@link Graph} has the same degree.
+ * Every {@link Vertex} in the {@link EvenlyGraph} has the same degree.
+ * there may exist multiple cases satisfy the condition above, so further vertices
+ * are chose to be linked.
+ * {@link EvenlyGraph} is a specific case of {@link CirculantGraph}.
  * when vertex degree is 0, {@link EmptyGraph} will be generated.
  * when vertex degree is vertex count - 1, {@link CompleteGraph} will be generated.
  */
@@ -63,6 +62,8 @@ extends AbstractGraphGenerator<LongValue, NullValue, NullValue> {
 			"Vertex count must be at least " + MINIMUM_VERTEX_COUNT);
 		Preconditions.checkArgument(vertexDegree >= MINIMUM_VERTEX_DEGREE,
 				"Vertex degree must be at least " + MINIMUM_VERTEX_DEGREE);
+		Preconditions.checkArgument(vertexDegree <= vertexCount - 1,
+				"Vertex degree must be at most " + (vertexCount - 1));
 		Preconditions.checkArgument(vertexCount % 2 == 0 || vertexDegree % 2 == 0,
 				"Vertex degree must be even when vertex count is odd number");
 
@@ -73,73 +74,21 @@ extends AbstractGraphGenerator<LongValue, NullValue, NullValue> {
 
 	@Override
 	public Graph<LongValue, NullValue, NullValue> generate() {
-		// Vertices
-		DataSet<Vertex<LongValue, NullValue>> vertices = GraphGeneratorUtils.vertexSequence(env, parallelism, vertexCount);
+		ArrayList<Long> offsetList = new ArrayList<Long>();
+		long maxOffset = vertexCount / 2;
 
-		// Edges
-		LongValueSequenceIterator iterator = new LongValueSequenceIterator(0, this.vertexCount - 1);
-
-		DataSet<Edge<LongValue, NullValue>> edges = env
-			.fromParallelCollection(iterator, LongValue.class)
-				.setParallelism(parallelism)
-				.name("Edge iterators")
-			.flatMap(new LinkVertexToOpposite(vertexCount, vertexDegree))
-				.setParallelism(parallelism)
-				.name("Evenly graph edges");
-
-		// Graph
-		return Graph.fromDataSet(vertices, edges, env);
-	}
-
-	@ForwardedFields("*->f0")
-	public class LinkVertexToOpposite
-	implements FlatMapFunction<LongValue, Edge<LongValue, NullValue>> {
-
-		private final long vertexCount;
-
-		private final long vertexDegree;
-
-		private LongValue target = new LongValue();
-
-		private Edge<LongValue, NullValue> edge = new Edge<>(null, target, NullValue.getInstance());
-
-		public LinkVertexToOpposite(long vertex_count, long vertexDegree) {
-			this.vertexCount = vertex_count;
-			this.vertexDegree = vertexDegree;
+		// add max offset when vertex degree is even and vertex count is odd
+		if (vertexDegree % 2 == 1 && vertexCount % 2 == 0) {
+			offsetList.add(maxOffset);
 		}
 
-		@Override
-		public void flatMap(LongValue source, Collector<Edge<LongValue, NullValue>> out)
-				throws Exception {
-			// empty graph
-			if (vertexDegree == 0) {
-				return;
-			}
-
-			edge.f0 = source;
-
-			// get opposite vertex's id
-			long opposite = (source.getValue() + vertexCount / 2) % vertexCount;
-
-			// link to opposite vertex if possible
-			if (vertexCount % 2 == 0 && vertexDegree % 2 == 1) {
-				target.setValue(opposite);
-				out.collect(edge);
-			}
-
-			// link to vertices on both sides of opposite
-			long initialOffset = (vertexCount + 1) % 2;
-			long oneSideVertexCount = (vertexDegree - vertexCount % 2) / 2;
-			for (long i = initialOffset; i <= oneSideVertexCount; i++) {
-				long previous = (opposite - i + vertexCount) % vertexCount;
-				long next = (opposite + i + vertexCount % 2) % vertexCount;
-
-				target.setValue(previous);
-				out.collect(edge);
-
-				target.setValue(next);
-				out.collect(edge);
-			}
+		// add other offset nearby max offset
+		for (long i = 0; i < vertexDegree / 2; i++) {
+			offsetList.add(maxOffset - i - (vertexCount + 1) % 2);
 		}
+
+		return new CirculantGraph(env, vertexCount, offsetList)
+				.setParallelism(parallelism)
+				.generate();
 	}
 }
