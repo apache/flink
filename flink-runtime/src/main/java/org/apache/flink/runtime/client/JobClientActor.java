c9623be2ac1ce44c1d04eeda336723b92e2fadf9
Stephan Ewen <sewen@apache.org>
2017-05-02 22:10:40 +0200

    [hotfix] [client] Unwrap SerializedThrowable in client log statements when possible

--- flink-runtime/src/main/java/org/apache/flink/runtime/client/JobClientActor.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/client/JobClientActor.java
@@ -34,6 +34,7 @@ import org.apache.flink.runtime.messages.JobClientMessages;
 import org.apache.flink.runtime.messages.JobClientMessages.JobManagerActorRef;
 import org.apache.flink.runtime.messages.JobClientMessages.JobManagerLeaderAddress;
 import org.apache.flink.runtime.messages.JobManagerMessages;
+import org.apache.flink.runtime.util.SerializedThrowable;
 import org.apache.flink.util.Preconditions;
 import scala.concurrent.duration.FiniteDuration;
 
@@ -287,7 +288,8 @@ public abstract class JobClientActor extends FlinkUntypedActor implements Leader
 				System.out.println(message.toString());
 			}
 		} else {
-			LOG.info(message.toString(), message.error());
+			Throwable error = SerializedThrowable.get(message.error(), getClass().getClassLoader());
+			LOG.info(message.toString(), error);
 			if (sysoutUpdates) {
 				System.out.println(message.toString());
 				message.error().printStackTrace(System.out);
