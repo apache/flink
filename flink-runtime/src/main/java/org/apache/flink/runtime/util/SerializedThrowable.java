03721cff116bc3d233b6b3f82c47f9edf42410af
Stephan Ewen <sewen@apache.org>
2017-05-02 19:33:28 +0200

    [hotfix] [runtime] SerializedThrowable copy constructor preserves cause

--- flink-runtime/src/main/java/org/apache/flink/runtime/util/SerializedThrowable.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/util/SerializedThrowable.java
@@ -108,6 +108,8 @@ public class SerializedThrowable extends Exception implements Serializable {
 			this.originalErrorClassName = other.originalErrorClassName;
 			this.fullStingifiedStackTrace = other.fullStingifiedStackTrace;
 			this.cachedException = other.cachedException;
+			this.setStackTrace(other.getStackTrace());
+			this.initCause(other.getCause());
 		}
 	}
 
