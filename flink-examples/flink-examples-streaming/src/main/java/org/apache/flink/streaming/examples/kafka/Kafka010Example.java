c86f71be9d83290c60a8dd63afe92ef9dcebac9c
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2017-05-09 14:46:39 +0200

    [FLINK-6539] Combine ReadFromKafka/WriteIntoKafka into one Kafka010Example
    This also updates the Kafka version we use in the examples module to
    0.10.x.

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/kafka/Kafka010Example.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/kafka/Kafka010Example.java
@@ -17,32 +17,38 @@
 
 package org.apache.flink.streaming.examples.kafka;
 
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer08;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 
 
 /**
- * Read Strings from Kafka and print them to standard out.
- * Note: On a cluster, DataStream.print() will print to the TaskManager's .out file!
+ * An example that shows how to read from and write to Kafka. This will read String messages
+ * from the input topic, prefix them by a configured prefix and output to the output topic.
  *
- * <p>Please pass the following arguments to run the example:
- * 	--topic test --bootstrap.servers localhost:9092 --zookeeper.connect localhost:2181 --group.id myconsumer
+ * <p>Example usage:
+ * 	--input-topic test-input --output-topic test-output --bootstrap.servers localhost:9092 --zookeeper.connect localhost:2181 --group.id myconsumer
  */
-public class ReadFromKafka {
+public class Kafka010Example {
 
 	public static void main(String[] args) throws Exception {
 		// parse input arguments
 		final ParameterTool parameterTool = ParameterTool.fromArgs(args);
 
-		if (parameterTool.getNumberOfParameters() < 4) {
-			System.out.println("Missing parameters!\nUsage: Kafka --topic <topic> " +
-					"--bootstrap.servers <kafka brokers> --zookeeper.connect <zk quorum> --group.id <some id>");
+		if (parameterTool.getNumberOfParameters() < 5) {
+			System.out.println("Missing parameters!\n" +
+					"Usage: Kafka --input-topic <topic> --output-topic <topic> " +
+					"--bootstrap.servers <kafka brokers> " +
+					"--zookeeper.connect <zk quorum> --group.id <some id> [--prefix <prefix>]");
 			return;
 		}
+
+		String prefix = parameterTool.get("prefix", "PREFIX:");
 
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 		env.getConfig().disableSysoutLogging();
@@ -50,15 +56,35 @@ public class ReadFromKafka {
 		env.enableCheckpointing(5000); // create a checkpoint every 5 seconds
 		env.getConfig().setGlobalJobParameters(parameterTool); // make parameters available in the web interface
 
-		DataStream<String> messageStream = env
-				.addSource(new FlinkKafkaConsumer08<>(
-						parameterTool.getRequired("topic"),
+		// make parameters available in the web interface
+		env.getConfig().setGlobalJobParameters(parameterTool);
+
+		DataStream<String> input = env
+				.addSource(new FlinkKafkaConsumer010<>(
+						parameterTool.getRequired("input-topic"),
+						new SimpleStringSchema(),
+						parameterTool.getProperties()))
+				.map(new PrefixingMapper(prefix));
+
+		input.addSink(
+				new FlinkKafkaProducer010<>(
+						parameterTool.getRequired("output-topic"),
 						new SimpleStringSchema(),
 						parameterTool.getProperties()));
 
-		// write kafka stream to standard out.
-		messageStream.print();
+		env.execute("Kafka 0.10 Example");
+	}
 
-		env.execute("Read from Kafka example");
+	private static class PrefixingMapper implements MapFunction<String, String> {
+		private final String prefix;
+
+		public PrefixingMapper(String prefix) {
+			this.prefix = prefix;
+		}
+
+		@Override
+		public String map(String value) throws Exception {
+			return prefix + value;
+		}
 	}
 }
--- flink-examples/flink-examples-streaming/src/main/scala/org/apache/flink/streaming/scala/examples/kafka/Kafka010Example.scala
+++ flink-examples/flink-examples-streaming/src/main/scala/org/apache/flink/streaming/scala/examples/kafka/Kafka010Example.scala
@@ -21,22 +21,23 @@ package org.apache.flink.streaming.scala.examples.kafka
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer08
+import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer010, FlinkKafkaProducer010}
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema
 
 /**
- * Read Strings from Kafka and print them to standard out.
- * Note: On a cluster, DataStream.print() will print to the TaskManager's .out file!
+ * An example that shows how to read from and write to Kafka. This will read String messages
+ * from the input topic, prefix them by a configured prefix and output to the output topic.
  *
  * Please pass the following arguments to run the example:
  * {{{
- * --topic test
+ * --input-topic test-input
+ * --output-topic test-output
  * --bootstrap.servers localhost:9092
  * --zookeeper.connect localhost:2181
  * --group.id myconsumer
  * }}}
  */
-object ReadFromKafka {
+object Kafka010Example {
 
   def main(args: Array[String]): Unit = {
 
@@ -44,10 +45,15 @@ object ReadFromKafka {
     val params = ParameterTool.fromArgs(args)
 
     if (params.getNumberOfParameters < 4) {
-      println("Missing parameters!\nUsage: Kafka --topic <topic> " +
-        "--bootstrap.servers <kafka brokers> --zookeeper.connect <zk quorum> --group.id <some id>")
+      println("Missing parameters!\n"
+        + "Usage: Kafka --input-topic <topic> --output-topic <topic> "
+        + "--bootstrap.servers <kafka brokers> "
+        + "--zookeeper.connect <zk quorum> --group.id <some id> [--prefix <prefix>]")
       return
     }
+
+    val prefix = params.get("prefix", "PREFIX:")
+
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.getConfig.disableSysoutLogging
@@ -57,17 +63,26 @@ object ReadFromKafka {
     // make parameters available in the web interface
     env.getConfig.setGlobalJobParameters(params)
 
-    // create a Kafka streaming source consumer for Kafka 0.8.x
-    val kafkaConsumer = new FlinkKafkaConsumer08(
-      params.getRequired("topic"),
+    // create a Kafka streaming source consumer for Kafka 0.10.x
+    val kafkaConsumer = new FlinkKafkaConsumer010(
+      params.getRequired("input-topic"),
       new SimpleStringSchema,
       params.getProperties)
-    val messageStream = env.addSource(kafkaConsumer)
 
-    // write kafka stream to standard out.
-    messageStream.print()
+    val messageStream = env
+      .addSource(kafkaConsumer)
+      .map(in => prefix + in)
 
-    env.execute("Read from Kafka example")
+    // create a Kafka producer for Kafka 0.10.x
+    val kafkaProducer = new FlinkKafkaProducer010(
+      params.getRequired("output-topic"),
+      new SimpleStringSchema,
+      params.getProperties)
+
+    // write data into Kafka
+    messageStream.addSink(kafkaProducer)
+
+    env.execute("Kafka 0.10 Example")
   }
 
 }
