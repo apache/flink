347100de7527dc4ba3664b8e8306a081834f84a4
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2017-05-12 20:06:01 +0800

    [FLINK-6566] [core] More restricted interface for VersionedIOReadableWritable hooks
    This commit makes the method hooks for defining compatibile
    serialization versions of VersionedIOReadableWritables more restricted.
    
    Functionally everything remains the same, but with lesser space for
    error-prone user implementations. It also allows for a better error
    message to indicate version mismatch.
    
    This closes #3883.

--- flink-runtime/src/main/java/org/apache/flink/runtime/state/heap/HeapKeyedStateBackend.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/state/heap/HeapKeyedStateBackend.java
@@ -448,7 +448,7 @@ public class HeapKeyedStateBackend<K> extends AbstractKeyedStateBackend<K> {
 						StateTableByKeyGroupReader keyGroupReader =
 								StateTableByKeyGroupReaders.readerForVersion(
 										stateTable,
-										serializationProxy.getRestoredVersion());
+										serializationProxy.getReadVersion());
 
 						keyGroupReader.readMappingsInKeyGroup(inView, keyGroupIndex);
 					}
