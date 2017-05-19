b93396c5a48c604c403dc383cf34eba3d446642e
Till Rohrmann <trohrmann@apache.org>
2017-05-19 17:24:21 +0200

    [FLINK-6641] [ha] Don't let the ClusterClient clean up HA services data when being shut down
    The ClusterClient should not call HighAvailabilityServices#closeAndCleanupAllData when being shut down.
    The reason is that this call will delete all HA data needed for a future recovery. Only the JobManager
    should be allowed to decide when to discard HA data or not.

--- flink-clients/src/main/java/org/apache/flink/client/program/ClusterClient.java
+++ flink-clients/src/main/java/org/apache/flink/client/program/ClusterClient.java
@@ -233,7 +233,7 @@ public abstract class ClusterClient {
 			}
 
 			if (highAvailabilityServices != null) {
-				highAvailabilityServices.closeAndCleanupAllData();
+				highAvailabilityServices.close();
 			}
 		}
 	}
