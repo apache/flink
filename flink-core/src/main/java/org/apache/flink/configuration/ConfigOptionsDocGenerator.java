2f765f00f482057a7e8e56c5875318c563525fcc
zentol <chesnay@apache.org>
2017-05-31 14:04:19 +0200

    add missing line before package

--- flink-core/src/main/java/org/apache/flink/configuration/ConfigOptionsDocGenerator.java
+++ flink-core/src/main/java/org/apache/flink/configuration/ConfigOptionsDocGenerator.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.flink.configuration;
 
 import org.apache.flink.annotation.VisibleForTesting;
--- flink-core/src/test/java/org/apache/flink/util/OutputTagTest.java
+++ flink-core/src/test/java/org/apache/flink/util/OutputTagTest.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.flink.util;
 
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
