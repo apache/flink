06695bec6cc18943c5e89c4cd9e3c76d65e5557f
zentol <chesnay@apache.org>
2017-06-02 22:18:49 +0200

    [FLINK-6823] Activate checkstyle for runtime/broadcast

--- flink-runtime/src/main/java/org/apache/flink/runtime/broadcast/MaterializationExpiredException.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/broadcast/MaterializationExpiredException.java
@@ -18,6 +18,10 @@
 
 package org.apache.flink.runtime.broadcast;
 
+/**
+ * Indicates that the {@link BroadcastVariableMaterialization} has materialized the broadcast variable at some point
+ * but discarded it already.
+ */
 public class MaterializationExpiredException extends Exception {
 	private static final long serialVersionUID = 7476456353634121934L;
 }
