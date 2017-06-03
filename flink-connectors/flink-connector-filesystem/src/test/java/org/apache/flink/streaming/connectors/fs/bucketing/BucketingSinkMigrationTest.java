0544b44808d3680fa22695f23d69e341820afde9
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2017-06-03 23:45:22 +0200

    [FLINK-6830] [fileSink] Port BucketingSinkFrom12MigrationTest for Flink 1.3

--- flink-connectors/flink-connector-filesystem/src/test/java/org/apache/flink/streaming/connectors/fs/bucketing/BucketingSinkMigrationTest.java
+++ flink-connectors/flink-connector-filesystem/src/test/java/org/apache/flink/streaming/connectors/fs/bucketing/BucketingSinkMigrationTest.java
@@ -26,6 +26,7 @@ import java.util.Map;
 import org.apache.commons.io.FileUtils;
 import org.apache.flink.api.common.state.ListState;
 import org.apache.flink.api.common.state.OperatorStateStore;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.runtime.state.FunctionInitializationContext;
 import org.apache.flink.streaming.api.operators.StreamSink;
 import org.apache.flink.streaming.connectors.fs.StringWriter;
@@ -33,22 +34,37 @@ import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
 import org.apache.flink.streaming.runtime.tasks.OperatorStateHandles;
 import org.apache.flink.streaming.util.OneInputStreamOperatorTestHarness;
 import org.apache.flink.streaming.util.OperatorSnapshotUtil;
+import org.apache.flink.streaming.util.migration.MigrationTestUtil;
+import org.apache.flink.streaming.util.migration.MigrationVersion;
+
 import org.apache.hadoop.fs.Path;
+
 import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * Tests for checking whether {@link BucketingSink} can restore from snapshots that were done
- * using the Flink 1.2 {@link BucketingSink}.
+ * using previous Flink versions' {@link BucketingSink}.
  *
  * <p>For regenerating the binary snapshot file you have to run the {@code write*()} method on
- * the Flink 1.2 branch.
+ * the corresponding Flink release-* branch.
  */
+@RunWith(Parameterized.class)
+public class BucketingSinkMigrationTest {
 
-public class BucketingSinkFrom12MigrationTest {
+	/**
+	 * TODO change this to the corresponding savepoint version to be written (e.g. {@link MigrationVersion#v1_3} for 1.3)
+	 * TODO and remove all @Ignore annotations on write*Snapshot() methods to generate savepoints
+	 */
+	private final MigrationVersion flinkGenerateSavepointVersion = null;
 
 	@ClassRule
 	public static TemporaryFolder tempFolder = new TemporaryFolder();
@@ -57,6 +73,21 @@ public class BucketingSinkFrom12MigrationTest {
 	private static final String PENDING_SUFFIX = ".pending";
 	private static final String IN_PROGRESS_SUFFIX = ".in-progress";
 	private static final String VALID_LENGTH_SUFFIX = ".valid";
+
+	@Parameterized.Parameters(name = "Migration Savepoint / Bucket Files Prefix: {0}")
+	public static Collection<Tuple2<MigrationVersion, String>> parameters () {
+		return Arrays.asList(
+			Tuple2.of(MigrationVersion.v1_2, "/var/folders/v_/ry2wp5fx0y7c1rvr41xy9_700000gn/T/junit9160378385359106772/junit479663758539998903/1970-01-01--01/part-0-"),
+			Tuple2.of(MigrationVersion.v1_3, "/var/folders/tv/b_1d8fvx23dgk1_xs8db_95h0000gn/T/junit4273542175898623023/junit3801102997056424640/1970-01-01--01/part-0-"));
+	}
+
+	private final MigrationVersion testMigrateVersion;
+	private final String expectedBucketFilesPrefix;
+
+	public BucketingSinkMigrationTest(Tuple2<MigrationVersion, String> migrateVersionAndExpectedBucketFilesPrefix) {
+		this.testMigrateVersion = migrateVersionAndExpectedBucketFilesPrefix.f0;
+		this.expectedBucketFilesPrefix = migrateVersionAndExpectedBucketFilesPrefix.f1;
+	}
 
 	/**
 	 * Manually run this to write binary snapshot data. Remove @Ignore to run.
@@ -97,7 +128,7 @@ public class BucketingSinkFrom12MigrationTest {
 
 		OperatorStateHandles snapshot = testHarness.snapshot(0L, 0L);
 
-		OperatorSnapshotUtil.writeStateHandle(snapshot, "src/test/resources/bucketing-sink-migration-test-flink1.2-snapshot");
+		OperatorSnapshotUtil.writeStateHandle(snapshot, "src/test/resources/bucketing-sink-migration-test-flink" + flinkGenerateSavepointVersion + "-snapshot");
 		testHarness.close();
 	}
 
@@ -105,7 +136,8 @@ public class BucketingSinkFrom12MigrationTest {
 	public void testRestore() throws Exception {
 		final File outDir = tempFolder.newFolder();
 
-		ValidatingBucketingSink<String> sink = (ValidatingBucketingSink<String>) new ValidatingBucketingSink<String>(outDir.getAbsolutePath())
+		ValidatingBucketingSink<String> sink = (ValidatingBucketingSink<String>)
+				new ValidatingBucketingSink<String>(outDir.getAbsolutePath(), expectedBucketFilesPrefix)
 			.setWriter(new StringWriter<String>())
 			.setBatchSize(5)
 			.setPartPrefix(PART_PREFIX)
@@ -119,9 +151,13 @@ public class BucketingSinkFrom12MigrationTest {
 		OneInputStreamOperatorTestHarness<String, Object> testHarness = new OneInputStreamOperatorTestHarness<>(
 			new StreamSink<>(sink), 10, 1, 0);
 		testHarness.setup();
-		testHarness.initializeState(
-				OperatorSnapshotUtil.readStateHandle(
-						OperatorSnapshotUtil.getResourceFilename("bucketing-sink-migration-test-flink1.2-snapshot")));
+
+		MigrationTestUtil.restoreFromSnapshot(
+			testHarness,
+			OperatorSnapshotUtil.getResourceFilename(
+				"bucketing-sink-migration-test-flink" + testMigrateVersion + "-snapshot"),
+			testMigrateVersion);
+
 		testHarness.open();
 
 		assertTrue(sink.initializeCalled);
@@ -168,8 +204,11 @@ public class BucketingSinkFrom12MigrationTest {
 
 		public boolean initializeCalled = false;
 
-		ValidatingBucketingSink(String basePath) {
+		private final String expectedBucketFilesPrefix;
+
+		ValidatingBucketingSink(String basePath, String expectedBucketFilesPrefix) {
 			super(basePath);
+			this.expectedBucketFilesPrefix = expectedBucketFilesPrefix;
 		}
 
 		/**
@@ -190,7 +229,7 @@ public class BucketingSinkFrom12MigrationTest {
 						String current = state.currentFile;
 						long validLength = state.currentFileValidLength;
 
-						Assert.assertEquals("/var/folders/v_/ry2wp5fx0y7c1rvr41xy9_700000gn/T/junit9160378385359106772/junit479663758539998903/1970-01-01--01/part-0-4", current);
+						Assert.assertEquals(expectedBucketFilesPrefix + "4", current);
 						Assert.assertEquals(6, validLength);
 
 						List<String> pendingFiles = state.pendingFiles;
@@ -208,7 +247,7 @@ public class BucketingSinkFrom12MigrationTest {
 
 							for (int i = 0; i < 4; i++) {
 								Assert.assertEquals(
-										"/var/folders/v_/ry2wp5fx0y7c1rvr41xy9_700000gn/T/junit9160378385359106772/junit479663758539998903/1970-01-01--01/part-0-" + i,
+										expectedBucketFilesPrefix + i,
 										files.get(i));
 							}
 						}
