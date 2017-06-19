daed1eecbab283803c48c13fb380d442f3cfd1a5
zentol <chesnay@apache.org>
2017-06-19 15:57:14 +0200

    [FLINK-6550] Reject null OutputTags in Context#collect(OutputTag<X>, X)
    This closes #4312.

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/api/operators/ProcessOperatorTest.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/api/operators/ProcessOperatorTest.java
@@ -27,7 +27,9 @@ import org.apache.flink.streaming.util.TestHarnessUtil;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.TestLogger;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -35,6 +37,9 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  * Tests {@link ProcessOperator}.
  */
 public class ProcessOperatorTest extends TestLogger {
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
 
 	@Test
 	public void testTimestampAndWatermarkQuerying() throws Exception {
@@ -92,6 +97,32 @@ public class ProcessOperatorTest extends TestLogger {
 		TestHarnessUtil.assertOutputEquals("Output was not correct.", expectedOutput, testHarness.getOutput());
 
 		testHarness.close();
+	}
+
+	@Test
+	public void testNullOutputTagRefusal() throws Exception {
+		ProcessOperator<Integer, String> operator = new ProcessOperator<>(new NullOutputTagEmittingProcessFunction());
+
+		OneInputStreamOperatorTestHarness<Integer, String> testHarness = new OneInputStreamOperatorTestHarness<>(operator);
+
+		testHarness.setup();
+		testHarness.open();
+
+		testHarness.setProcessingTime(17);
+		try {
+			expectedException.expect(IllegalArgumentException.class);
+			testHarness.processElement(new StreamRecord<>(5));
+		} finally {
+			testHarness.close();
+		}
+	}
+
+	private static class NullOutputTagEmittingProcessFunction extends ProcessFunction<Integer, String> {
+
+		@Override
+		public void processElement(Integer value, Context ctx, Collector<String> out) throws Exception {
+			ctx.output(null, value);
+		}
 	}
 
 	private static class QueryingProcessFunction extends ProcessFunction<Integer, String> {
