c6d3cf27b2a4e413098ade1f95370434f045d50f
Piotr Nowojski <piotr.nowojski@gmail.com>
2017-07-12 14:01:49 +0200

    [misc] Commit read offsets in Kafka integration tests
    Previously offsets were not commited so the same records could be read more then once.
    It was not a big issue, because so far this methods were used only for at-least-once tests.

--- flink-connectors/flink-connector-kafka-0.10/src/test/java/org/apache/flink/streaming/connectors/kafka/KafkaTestEnvironmentImpl.java
+++ flink-connectors/flink-connector-kafka-0.10/src/test/java/org/apache/flink/streaming/connectors/kafka/KafkaTestEnvironmentImpl.java
@@ -123,23 +123,26 @@ public class KafkaTestEnvironmentImpl extends KafkaTestEnvironment {
 	@Override
 	public <K, V> Collection<ConsumerRecord<K, V>> getAllRecordsFromTopic(Properties properties, String topic, int partition, long timeout) {
 		List<ConsumerRecord<K, V>> result = new ArrayList<>();
-		KafkaConsumer<K, V> consumer = new KafkaConsumer<>(properties);
-		consumer.assign(Arrays.asList(new TopicPartition(topic, partition)));
 
-		while (true) {
-			boolean processedAtLeastOneRecord = false;
+		try (KafkaConsumer<K, V> consumer = new KafkaConsumer<>(properties)) {
+			consumer.assign(Arrays.asList(new TopicPartition(topic, partition)));
 
-			// wait for new records with timeout and break the loop if we didn't get any
-			Iterator<ConsumerRecord<K, V>> iterator = consumer.poll(timeout).iterator();
-			while (iterator.hasNext()) {
-				ConsumerRecord<K, V> record = iterator.next();
-				result.add(record);
-				processedAtLeastOneRecord = true;
+			while (true) {
+				boolean processedAtLeastOneRecord = false;
+
+				// wait for new records with timeout and break the loop if we didn't get any
+				Iterator<ConsumerRecord<K, V>> iterator = consumer.poll(timeout).iterator();
+				while (iterator.hasNext()) {
+					ConsumerRecord<K, V> record = iterator.next();
+					result.add(record);
+					processedAtLeastOneRecord = true;
+				}
+
+				if (!processedAtLeastOneRecord) {
+					break;
+				}
 			}
-
-			if (!processedAtLeastOneRecord) {
-				break;
-			}
+			consumer.commitSync();
 		}
 
 		return UnmodifiableList.decorate(result);
