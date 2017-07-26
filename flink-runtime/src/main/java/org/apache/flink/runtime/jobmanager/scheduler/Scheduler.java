81a381ef0483a26efd7f9ae2c12b30a84ba203ae
summerleafs <summerleafs@163.com>
2017-07-26 19:42:32 +0800

    Fix FLINK-7218
    rewrite the test case for getPreferredLocationsBasedOnInputs().

--- flink-runtime/src/main/java/org/apache/flink/runtime/jobmanager/scheduler/Scheduler.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/jobmanager/scheduler/Scheduler.java
@@ -139,7 +139,7 @@ public class Scheduler implements InstanceListener, SlotAvailabilityListener, Sl
 			final Object ret = scheduleTask(task, allowQueued);
 
 			if (ret instanceof SimpleSlot) {
-				//record the slot info in execution as soon as we got one.
+				//record the slot info as soon as we got one.
 				task.getTaskToExecute().setAssignedFutureResource((SimpleSlot) ret);
 				return FlinkCompletableFuture.completed((SimpleSlot) ret);
 			}
