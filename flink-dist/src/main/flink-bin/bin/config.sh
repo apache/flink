f63e2d03d739014f0cd94634d731e552a02c76d9
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2017-08-18 16:40:55 +0200

    [FLINK-7480] Set HADOOP_CONF_DIR to sane default if not set
    This improves the out-of-box experience on GCE and AWS, both of which
    don't set HADOOP_CONF_DIR by default but use /etc/hadoop/conf

--- flink-dist/src/main/flink-bin/bin/config.sh
+++ flink-dist/src/main/flink-bin/bin/config.sh
@@ -351,6 +351,13 @@ if [ -z "$HADOOP_CONF_DIR" ]; then
     fi
 fi
 
+# try and set HADOOP_CONF_DIR to some common default if it's not set
+if [ -z "$HADOOP_CONF_DIR" ]; then
+    if [ -d "/etc/hadoop/conf" ]; then
+        HADOOP_CONF_DIR="/etc/hadoop/conf"
+    fi
+fi
+
 INTERNAL_HADOOP_CLASSPATHS="${HADOOP_CLASSPATH}:${HADOOP_CONF_DIR}:${YARN_CONF_DIR}"
 
 # check if the "hadoop" binary is available, if yes, use that to augment the CLASSPATH
