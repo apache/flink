de38f2b6e421ddfaed9d457c0c6c02ae1edde98d
Till Rohrmann <trohrmann@apache.org>
2017-08-18 14:29:29 +0200

    [FLINK-7533] Let LeaderGatewayRetriever retry failed gateway retrievals
    Add test case
    
    Only log LeaderGatewayRetriever exception on Debug log level
    
    Properly fail outdated gateway retrieval operations
    
    This closes #4602.

--- flink-runtime/src/main/java/org/apache/flink/runtime/webmonitor/retriever/LeaderGatewayRetriever.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/webmonitor/retriever/LeaderGatewayRetriever.java
@@ -19,10 +19,14 @@
 package org.apache.flink.runtime.webmonitor.retriever;
 
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.runtime.highavailability.HighAvailabilityServices;
 import org.apache.flink.runtime.rpc.RpcGateway;
+import org.apache.flink.util.ExceptionUtils;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Retrieves and stores the leading {@link RpcGateway}.
@@ -31,24 +35,86 @@ import java.util.concurrent.CompletableFuture;
  */
 public abstract class LeaderGatewayRetriever<T extends RpcGateway> extends LeaderRetriever implements GatewayRetriever<T> {
 
-	private volatile CompletableFuture<T> gatewayFuture;
+	private final AtomicReference<CompletableFuture<T>> atomicGatewayFuture;
+
+	private volatile CompletableFuture<T> initialGatewayFuture;
 
 	public LeaderGatewayRetriever() {
-		gatewayFuture = createGateway(getLeaderFuture());
+		initialGatewayFuture = new CompletableFuture<>();
+		atomicGatewayFuture = new AtomicReference<>(initialGatewayFuture);
 	}
 
 	@Override
 	public CompletableFuture<T> getFuture() {
-		return gatewayFuture;
+		final CompletableFuture<T> currentGatewayFuture = atomicGatewayFuture.get();
+
+		if (currentGatewayFuture.isCompletedExceptionally()) {
+			try {
+				currentGatewayFuture.get();
+			} catch (ExecutionException | InterruptedException executionException) {
+				String leaderAddress;
+
+				try {
+					Tuple2<String, UUID> leaderAddressSessionId = getLeaderNow()
+						.orElse(Tuple2.of("unknown address", HighAvailabilityServices.DEFAULT_LEADER_ID));
+
+					leaderAddress = leaderAddressSessionId.f0;
+				} catch (Exception e) {
+					log.warn("Could not obtain the current leader.", e);
+					leaderAddress = "unknown leader address";
+				}
+
+				if (log.isDebugEnabled() || log.isTraceEnabled()) {
+					// only log exceptions on debug or trace level
+					log.warn(
+						"Error while retrieving the leader gateway. Retrying to connect to {}.",
+						leaderAddress,
+						ExceptionUtils.stripExecutionException(executionException));
+				} else {
+					log.warn(
+						"Error while retrieving the leader gateway. Retrying to connect to {}.",
+						leaderAddress);
+				}
+			}
+
+			// we couldn't resolve the gateway --> let's try again
+			final CompletableFuture<T> newGatewayFuture = createGateway(getLeaderFuture());
+
+			// let's check if there was a concurrent createNewFuture call
+			if (atomicGatewayFuture.compareAndSet(currentGatewayFuture, newGatewayFuture)) {
+				return newGatewayFuture;
+			} else {
+				return atomicGatewayFuture.get();
+			}
+		} else {
+			return atomicGatewayFuture.get();
+		}
 	}
 
 	@Override
-	public CompletableFuture<Tuple2<String, UUID>> createNewFuture() {
-		CompletableFuture<Tuple2<String, UUID>> newFuture = super.createNewFuture();
+	public void notifyNewLeaderAddress(CompletableFuture<Tuple2<String, UUID>> newLeaderAddressFuture) {
+		final CompletableFuture<T> newGatewayFuture = createGateway(newLeaderAddressFuture);
 
-		gatewayFuture = createGateway(newFuture);
+		final CompletableFuture<T> oldGatewayFuture = atomicGatewayFuture.getAndSet(newGatewayFuture);
 
-		return newFuture;
+		// check if the old gateway future was the initial future
+		if (oldGatewayFuture == initialGatewayFuture) {
+			// we have to complete it because a caller might wait on the initial future
+			newGatewayFuture.whenComplete(
+				(t, throwable) -> {
+					if (throwable != null) {
+						oldGatewayFuture.completeExceptionally(throwable);
+					} else {
+						oldGatewayFuture.complete(t);
+					}
+				});
+
+			// free the initial gateway future
+			initialGatewayFuture = null;
+		} else {
+			// try to cancel old gateway retrieval operation
+			oldGatewayFuture.cancel(false);
+		}
 	}
 
 	protected abstract CompletableFuture<T> createGateway(CompletableFuture<Tuple2<String, UUID>> leaderFuture);
