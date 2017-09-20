642e488d3d24f93ae022278454c54f4e0eb372ee
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2017-09-20 19:07:11 +0200

    [FLINK-7647] [flip6] Introduce test base for REST response marshalling
    Introduces a common test base that for all REST responses, a subclass
    should be implemented to verify that the response can be correctly
    marshalled and unmarshalled.

--- flink-runtime/src/test/java/org/apache/flink/runtime/rest/handler/legacy/messages/ClusterConfigurationTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/rest/handler/legacy/messages/ClusterConfigurationTest.java
@@ -18,36 +18,22 @@
 
 package org.apache.flink.runtime.rest.handler.legacy.messages;
 
-import org.apache.flink.runtime.rest.util.RestMapperUtils;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
 /**
  * Tests for the {@link ClusterConfiguration}.
  */
-public class ClusterConfigurationTest {
+public class ClusterConfigurationTest extends RestResponseMarshallingTestBase<ClusterConfiguration> {
 
-	/**
-	 * Tests that we can marshal and unmarshal {@link ClusterConfiguration} objects.
-	 */
-	@Test
-	public void testJsonMarshalling() throws JsonProcessingException {
+	@Override
+	protected Class<ClusterConfiguration> getTestResponseClass() {
+		return ClusterConfiguration.class;
+	}
+
+	@Override
+	protected ClusterConfiguration getTestResponseInstance() {
 		final ClusterConfiguration expected = new ClusterConfiguration(2);
 		expected.add(new ClusterConfigurationEntry("key1", "value1"));
 		expected.add(new ClusterConfigurationEntry("key2", "value2"));
 
-		final ObjectMapper objectMapper = RestMapperUtils.getStrictObjectMapper();
-
-		JsonNode marshaled = objectMapper.valueToTree(expected);
-
-		final ClusterConfiguration unmarshaled = objectMapper.treeToValue(marshaled, ClusterConfiguration.class);
-
-		assertEquals(expected, unmarshaled);
+		return expected;
 	}
 }
