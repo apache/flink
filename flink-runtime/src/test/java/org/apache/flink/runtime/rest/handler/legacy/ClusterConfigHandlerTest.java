8d945b14dd2953ed261920b30c642bf91bbc088f
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2017-09-21 15:51:27 +0200

    Fix NPE in ClusterConfigHandlerTest

--- flink-runtime/src/test/java/org/apache/flink/runtime/rest/handler/legacy/ClusterConfigHandlerTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/rest/handler/legacy/ClusterConfigHandlerTest.java
@@ -18,6 +18,7 @@
 
 package org.apache.flink.runtime.rest.handler.legacy;
 
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.runtime.concurrent.Executors;
 
 import org.junit.Assert;
@@ -29,7 +30,7 @@ import org.junit.Test;
 public class ClusterConfigHandlerTest {
 	@Test
 	public void testGetPaths() {
-		ClusterConfigHandler handler = new ClusterConfigHandler(Executors.directExecutor(), null);
+		ClusterConfigHandler handler = new ClusterConfigHandler(Executors.directExecutor(), new Configuration());
 		String[] paths = handler.getPaths();
 		Assert.assertEquals(1, paths.length);
 		Assert.assertEquals("/jobmanager/config", paths[0]);
