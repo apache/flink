925b3763458034fd1589b5c6077e84653bd99c28
yew1eb <yew1eb@gmail.com>
2017-10-18 23:21:53 +0800

    fix

--- flink-connectors/flink-connector-kafka-base/src/main/java/org/apache/flink/streaming/connectors/kafka/internals/AbstractFetcher.java
+++ flink-connectors/flink-connector-kafka-base/src/main/java/org/apache/flink/streaming/connectors/kafka/internals/AbstractFetcher.java
@@ -55,7 +55,6 @@ public abstract class AbstractFetcher<T, KPH> {
 	protected static final int NO_TIMESTAMPS_WATERMARKS = 0;
 	protected static final int PERIODIC_WATERMARKS = 1;
 	protected static final int PUNCTUATED_WATERMARKS = 2;
-	protected static final Object NULLOBJECT = new Object();
 
 	// ------------------------------------------------------------------------
 
@@ -552,7 +551,7 @@ public abstract class AbstractFetcher<T, KPH> {
 			addOffsetStateGauge(kafkaMetricGroup);
 			return kafkaMetricGroup;
 		}
-		return (MetricGroup) NULLOBJECT;
+		return metricGroup;
 	}
 
 	/**
