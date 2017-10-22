3e8d7ad7b5c610e8dbf9e531897f423829c109c2
Bowen Li <bowenli86@gmail.com>
2017-10-22 22:26:37 +0800

    update comments

--- flink-tests/src/test/java/org/apache/flink/test/streaming/runtime/SideOutputITCase.java
+++ flink-tests/src/test/java/org/apache/flink/test/streaming/runtime/SideOutputITCase.java
@@ -498,7 +498,7 @@ public class SideOutputITCase extends StreamingMultipleProgramsTestBase implemen
 	}
 
 	/**
-	 * Test CoProcessFunction side output on keyed streams.
+	 * Test keyed CoProcessFunction side output.
 	 */
 	@Test
 	public void testKeyedCoProcessFunctionSideOutput() throws Exception {
@@ -543,7 +543,7 @@ public class SideOutputITCase extends StreamingMultipleProgramsTestBase implemen
 	}
 
 	/**
-	 * Test CoProcessFunction side output on keyed streams with multiple consumers.
+	 * Test keyed CoProcessFunction side output with multiple consumers.
 	 */
 	@Test
 	public void testKeyedCoProcessFunctionSideOutputWithMultipleConsumers() throws Exception {
