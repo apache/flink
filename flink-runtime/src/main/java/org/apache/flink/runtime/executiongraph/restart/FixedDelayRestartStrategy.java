624d64f5ed5cbdc64aa9fbe53342e74be91771d6
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2017-11-20 10:13:25 +0100

    fixup! turn option into ConfigOption

--- flink-runtime/src/main/java/org/apache/flink/runtime/executiongraph/restart/FixedDelayRestartStrategy.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/executiongraph/restart/FixedDelayRestartStrategy.java
@@ -80,10 +80,7 @@ public class FixedDelayRestartStrategy implements RestartStrategy {
 	public static FixedDelayRestartStrategyFactory createFactory(Configuration configuration) throws Exception {
 		int maxAttempts = configuration.getInteger(ConfigConstants.RESTART_STRATEGY_FIXED_DELAY_ATTEMPTS, 1);
 
-		String delayString = configuration.getString(
-			ConfigConstants.RESTART_STRATEGY_FIXED_DELAY_DELAY,
-			"1 s"
-		);
+		String delayString = configuration.getString(ConfigConstants.RESTART_STRATEGY_FIXED_DELAY_DELAY);
 
 		long delay;
 
