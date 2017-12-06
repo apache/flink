01d0d256d633108177ddc77288287e1201e31de0
Bowen Li <bowenli86@gmail.com>
2017-12-06 11:37:54 -0800

    [hotfix][javadocs] Clarify replacement for deprecated FoldingState
    This closes #5129.

--- flink-core/src/main/java/org/apache/flink/api/common/state/FoldingState.java
+++ flink-core/src/main/java/org/apache/flink/api/common/state/FoldingState.java
@@ -36,7 +36,7 @@ import org.apache.flink.annotation.PublicEvolving;
  * @param <T> Type of the values folded into the state
  * @param <ACC> Type of the value in the state
  *
- * @deprecated will be removed in a future version
+ * @deprecated will be removed in a future version in favor of {@link AggregatingState}
  */
 @PublicEvolving
 @Deprecated
