c816191113d813156467f3e33856636ef0bcce38
Piotr Nowojski <piotr.nowojski@gmail.com>
2017-12-07 10:03:32 +0100

    [FLINK-8220][network-benchmarks] Define network benchmarks in Flink project

---
+++ flink-runtime/src/test/java/org/apache/flink/runtime/io/network/benchmark/NetworkThroughputBenchmarkTests.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.io.network.benchmark;
+
+import org.junit.Test;
+
+/**
+ * Tests for various network benchmarks based on {@link NetworkThroughputBenchmark}.
+ */
+public class NetworkThroughputBenchmarkTests {
+	@Test
+	public void pointToPointBenchmark() throws Exception {
+		NetworkThroughputBenchmark benchmark = new NetworkThroughputBenchmark();
+		benchmark.setUp(1, 1);
+		try {
+			benchmark.executeBenchmark(1_000);
+		}
+		finally {
+			benchmark.tearDown();
+		}
+	}
+
+	@Test
+	public void pointToMultiPointBenchmark() throws Exception {
+		NetworkThroughputBenchmark benchmark = new NetworkThroughputBenchmark();
+		benchmark.setUp(1, 100);
+		try {
+			benchmark.executeBenchmark(1_000);
+		}
+		finally {
+			benchmark.tearDown();
+		}
+	}
+
+	@Test
+	public void multiPointToPointBenchmark() throws Exception {
+		NetworkThroughputBenchmark benchmark = new NetworkThroughputBenchmark();
+		benchmark.setUp(4, 1);
+		try {
+			benchmark.executeBenchmark(1_000);
+		}
+		finally {
+			benchmark.tearDown();
+		}
+	}
+
+	@Test
+	public void multiPointToMultiPointBenchmark() throws Exception {
+		NetworkThroughputBenchmark benchmark = new NetworkThroughputBenchmark();
+		benchmark.setUp(4, 100);
+		try {
+			benchmark.executeBenchmark(1_000);
+		}
+		finally {
+			benchmark.tearDown();
+		}
+	}
+}
