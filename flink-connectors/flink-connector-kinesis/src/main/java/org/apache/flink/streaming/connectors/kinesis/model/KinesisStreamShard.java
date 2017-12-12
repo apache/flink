686ab6888eb7260e8c69c62c60d7dd8fb54fa58e
Bowen Li <bowenli86@gmail.com>
2017-12-11 16:16:45 -0800

    address feedbacks

--- flink-connectors/flink-connector-kinesis/src/main/java/org/apache/flink/streaming/connectors/kinesis/model/KinesisStreamShard.java
+++ flink-connectors/flink-connector-kinesis/src/main/java/org/apache/flink/streaming/connectors/kinesis/model/KinesisStreamShard.java
@@ -17,6 +17,8 @@
 
 package org.apache.flink.streaming.connectors.kinesis.model;
 
+import org.apache.flink.annotation.Internal;
+
 import com.amazonaws.services.kinesis.model.Shard;
 
 import java.io.Serializable;
@@ -31,6 +33,7 @@ import static org.apache.flink.util.Preconditions.checkNotNull;
  * @deprecated Will be remove in a future version in favor of {@link StreamShardHandle}.
  */
 @Deprecated
+@Internal
 public class KinesisStreamShard implements Serializable {
 
 	private static final long serialVersionUID = -6004217801761077536L;
