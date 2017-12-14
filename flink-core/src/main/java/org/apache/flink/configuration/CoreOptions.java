28d9b20da6e0b79306ef3ebbbb406371da15392f
Stephan Ewen <sewen@apache.org>
2017-12-14 13:50:39 +0100

    [FLINK-8264] [core] Add 'scala.' to the 'parent-first' classloading patterns.
    This closes #5166

--- flink-core/src/main/java/org/apache/flink/configuration/CoreOptions.java
+++ flink-core/src/main/java/org/apache/flink/configuration/CoreOptions.java
@@ -36,7 +36,7 @@ public class CoreOptions {
 
 	public static final ConfigOption<String> ALWAYS_PARENT_FIRST_LOADER = ConfigOptions
 		.key("classloader.parent-first-patterns")
-		.defaultValue("java.;org.apache.flink.;javax.annotation;org.slf4j;org.apache.log4j;org.apache.logging.log4j;ch.qos.logback");
+		.defaultValue("java.;scala.;org.apache.flink.;javax.annotation;org.slf4j;org.apache.log4j;org.apache.logging.log4j;ch.qos.logback");
 
 
 	// ------------------------------------------------------------------------
