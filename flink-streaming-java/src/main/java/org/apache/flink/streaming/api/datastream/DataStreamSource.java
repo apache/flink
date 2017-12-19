3cdc5d1dce7c8cf0db0352d86867f73f6ccd9be9
Matrix42 <934336389@qq.com>
2017-12-19 16:20:39 +0800

    [FLINK-8292] Remove unnecessary force cast in DataStreamSource
    This closes #5180.

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/datastream/DataStreamSource.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/datastream/DataStreamSource.java
@@ -54,7 +54,8 @@ public class DataStreamSource<T> extends SingleOutputStreamOperator<T> {
 		if (parallelism != 1 && !isParallel) {
 			throw new IllegalArgumentException("Source: " + transformation.getId() + " is not a parallel source");
 		} else {
-			return (DataStreamSource<T>) super.setParallelism(parallelism);
+			super.setParallelism(parallelism);
+			return this;
 		}
 	}
 }
