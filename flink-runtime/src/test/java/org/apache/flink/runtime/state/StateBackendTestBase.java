a0b47dbe3328e924d920885312dfba056e65bd22
Bowen Li <bowenli86@gmail.com>
2018-01-15 15:24:19 -0800

    [FLINK-8411] inconsistent behavior between HeapListState#add() and RocksDBListState#add()

--- flink-runtime/src/test/java/org/apache/flink/runtime/state/StateBackendTestBase.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/state/StateBackendTestBase.java
@@ -1298,6 +1298,8 @@ public abstract class StateBackendTestBase<B extends AbstractStateBackend> exten
 
 			keyedBackend.setCurrentKey("abc");
 			assertNull(state.get());
+			state.add(null);
+			assertNull(state.get());
 
 			keyedBackend.setCurrentKey("def");
 			assertNull(state.get());
@@ -1311,6 +1313,8 @@ public abstract class StateBackendTestBase<B extends AbstractStateBackend> exten
 			state.update(Arrays.asList());
 			assertNull(state.get());
 			state.update(Arrays.asList(10L, 16L));
+			assertThat(state.get(), containsInAnyOrder(16L, 10L));
+			state.add(null);
 			assertThat(state.get(), containsInAnyOrder(16L, 10L));
 
 			keyedBackend.setCurrentKey("abc");
@@ -1335,6 +1339,8 @@ public abstract class StateBackendTestBase<B extends AbstractStateBackend> exten
 			state.addAll(Arrays.asList(5L, 6L));
 			assertThat(state.get(), containsInAnyOrder(3L, 4L, 5L, 6L));
 
+			state.add(null);
+			assertThat(state.get(), containsInAnyOrder(3L, 4L, 5L, 6L));
 			state.update(Arrays.asList(1L, 2L));
 			assertThat(state.get(), containsInAnyOrder(1L, 2L));
 
