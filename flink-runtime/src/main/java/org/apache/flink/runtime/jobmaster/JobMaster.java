62a777bc8ddfb4e34d7beaf7091a90b0bcc70c51
gyao <gary@data-artisans.com>
2018-01-31 20:06:09 +0100

    [hotfix] Log swallowed exception in JobMaster

--- flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/JobMaster.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/JobMaster.java
@@ -682,7 +682,7 @@ public class JobMaster extends FencedRpcEndpoint<JobMasterId> implements JobMast
 
 				return CompletableFuture.completedFuture(Acknowledge.get());
 			} catch (Exception e) {
-				log.error("Failed to notify KvStateRegistry about registration {}.", registrationName);
+				log.error("Failed to notify KvStateRegistry about registration {}.", registrationName, e);
 				return FutureUtils.completedExceptionally(e);
 			}
 		} else {
