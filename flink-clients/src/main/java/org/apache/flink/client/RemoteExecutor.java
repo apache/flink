facf2ac67ae3ffd4cffd7c6f8536fd7953795fb9
Till Rohrmann <trohrmann@apache.org>
2018-02-19 13:28:41 +0100

    [FLINK-8705] [flip6] Add Flip-6 support to Remote(Stream)Environment
    This commit enables the Remote(Stream)Environment to submit jobs to a Flip-6
    based cluster. It achieves this by instantiating a RestClusterClient instead
    of a StandaloneClusterClient.

--- flink-clients/src/main/java/org/apache/flink/client/RemoteExecutor.java
+++ flink-clients/src/main/java/org/apache/flink/client/RemoteExecutor.java
@@ -25,7 +25,9 @@ import org.apache.flink.api.common.PlanExecutor;
 import org.apache.flink.client.program.ClusterClient;
 import org.apache.flink.client.program.JobWithJars;
 import org.apache.flink.client.program.StandaloneClusterClient;
+import org.apache.flink.client.program.rest.RestClusterClient;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.CoreOptions;
 import org.apache.flink.configuration.JobManagerOptions;
 import org.apache.flink.optimizer.DataStatistics;
 import org.apache.flink.optimizer.Optimizer;
@@ -147,7 +149,11 @@ public class RemoteExecutor extends PlanExecutor {
 	public void start() throws Exception {
 		synchronized (lock) {
 			if (client == null) {
-				client = new StandaloneClusterClient(clientConfiguration);
+				if (CoreOptions.OLD_MODE.equals(clientConfiguration.getString(CoreOptions.MODE))) {
+					client = new StandaloneClusterClient(clientConfiguration);
+				} else {
+					client = new RestClusterClient<>(clientConfiguration, "RemoteExecutor");
+				}
 				client.setPrintStatusDuringExecution(isPrintingStatusDuringExecution());
 			}
 			else {
--- flink-core/src/main/java/org/apache/flink/configuration/CoreOptions.java
+++ flink-core/src/main/java/org/apache/flink/configuration/CoreOptions.java
@@ -235,6 +235,11 @@ public class CoreOptions {
 	public static final String FLIP6_MODE = "flip6";
 
 	/**
+	 * Constant value for the old execution mode.
+	 */
+	public static final String OLD_MODE = "old";
+
+	/**
 	 * Switch to select the execution mode. Possible values are 'flip6' and 'old'.
 	 */
 	public static final ConfigOption<String> MODE = ConfigOptions
