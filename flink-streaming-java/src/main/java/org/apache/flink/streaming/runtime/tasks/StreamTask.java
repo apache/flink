f9a583b727c9aecbec3213b12266f1d598223400
Stefan Richter <s.richter@data-artisans.com>
2018-02-23 18:21:24 +0100

    [hotfix] Clear interrupted flag in stream task cancellation
    We clear the interrupted flag before the cleanup code block of task cancellation.
    Otherwise, code that would like to wait until services are properly shutdown will
    always immediately return from calls that are supposed to be blocking waits.

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/tasks/StreamTask.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/tasks/StreamTask.java
@@ -348,6 +348,9 @@ public abstract class StreamTask<OUT, OP extends StreamOperator<OUT>>
 			// clean up everything we initialized
 			isRunning = false;
 
+			// clear the interrupted status so that we can wait for the following resource shutdowns to complete
+			Thread.interrupted();
+
 			// stop all timers and threads
 			if (timerService != null && !timerService.isTerminated()) {
 				try {
