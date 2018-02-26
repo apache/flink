735c95fbb93316475dc6dd6daa2aa91a7158c843
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2018-02-26 11:55:14 +0100

    [FLINK-8778] Port queryable state ITCases to use MiniClusterResource

--- flink-queryable-state/flink-queryable-state-runtime/src/test/java/org/apache/flink/queryablestate/itcases/NonHAQueryableStateRocksDBBackendITCase.java
+++ flink-queryable-state/flink-queryable-state-runtime/src/test/java/org/apache/flink/queryablestate/itcases/NonHAQueryableStateRocksDBBackendITCase.java
@@ -18,28 +18,79 @@
 
 package org.apache.flink.queryablestate.itcases;
 
+import org.apache.flink.configuration.ConfigConstants;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.QueryableStateOptions;
+import org.apache.flink.configuration.TaskManagerOptions;
+import org.apache.flink.configuration.WebOptions;
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
+import org.apache.flink.queryablestate.client.QueryableStateClient;
 import org.apache.flink.runtime.state.AbstractStateBackend;
+import org.apache.flink.test.util.MiniClusterResource;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 
 /**
  * Several integration tests for queryable state using the {@link RocksDBStateBackend}.
  */
-public class NonHAQueryableStateRocksDBBackendITCase extends NonHAAbstractQueryableStateTestBase {
+public class NonHAQueryableStateRocksDBBackendITCase extends AbstractQueryableStateTestBase {
+
+	// NUM_TMS * NUM_SLOTS_PER_TM must match the parallelism of the pipelines so that
+	// we always use all TaskManagers so that the JM oracle is always properly re-registered
+	private static final int NUM_TMS = 2;
+	private static final int NUM_SLOTS_PER_TM = 2;
+
+	private static final int QS_PROXY_PORT_RANGE_START = 9094;
+	private static final int QS_SERVER_PORT_RANGE_START = 9099;
 
 	@Rule
 	public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
-	@BeforeClass
-	public static void setup() {
-		setup(9094, 9099);
-	}
+	@ClassRule
+	public static final MiniClusterResource MINI_CLUSTER_RESOURCE = new MiniClusterResource(
+		new MiniClusterResource.MiniClusterResourceConfiguration(
+			getConfig(),
+			NUM_TMS,
+			NUM_SLOTS_PER_TM),
+		true);
 
 	@Override
 	protected AbstractStateBackend createStateBackend() throws Exception {
 		return new RocksDBStateBackend(temporaryFolder.newFolder().toURI().toString());
 	}
+
+	@BeforeClass
+	public static void setup() throws Exception {
+		client = new QueryableStateClient("localhost", QS_PROXY_PORT_RANGE_START);
+
+		clusterClient = MINI_CLUSTER_RESOURCE.getClusterClient();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		client.shutdownAndWait();
+	}
+
+	private static Configuration getConfig() {
+		Configuration config = new Configuration();
+		config.setLong(TaskManagerOptions.MANAGED_MEMORY_SIZE, 4L);
+		config.setInteger(ConfigConstants.LOCAL_NUMBER_TASK_MANAGER, NUM_TMS);
+		config.setInteger(TaskManagerOptions.NUM_TASK_SLOTS, NUM_SLOTS_PER_TM);
+		config.setInteger(QueryableStateOptions.CLIENT_NETWORK_THREADS, 1);
+		config.setInteger(QueryableStateOptions.PROXY_NETWORK_THREADS, 1);
+		config.setInteger(QueryableStateOptions.SERVER_NETWORK_THREADS, 1);
+		config.setString(
+			QueryableStateOptions.PROXY_PORT_RANGE,
+			QS_PROXY_PORT_RANGE_START + "-" + (QS_PROXY_PORT_RANGE_START + NUM_TMS));
+		config.setString(
+			QueryableStateOptions.SERVER_PORT_RANGE,
+			QS_SERVER_PORT_RANGE_START + "-" + (QS_SERVER_PORT_RANGE_START + NUM_TMS));
+		config.setBoolean(WebOptions.SUBMIT_ENABLE, false);
+		return config;
+	}
+
 }
