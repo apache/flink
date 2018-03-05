81e1e4c312b467dc64c664e30a7132a9f7d55140
Nico Kruber <nico@data-artisans.com>
2018-03-05 18:24:17 +0100

    [FLINK-8872][flip6] fix yarn detached mode command parsing
    The detached flag if given by "-yd" was not passed correctly into the
    CliFrontend and resulted in the CLI waiting for submitted jobs to finish instead
    of detaching from the execution.
    
    [FLINK-8872][yarn] add tests for YARN detached mode command line parsing with CliFrontend
    
    - create a test-jar of flink-clients
    - create CliFrontendRunWithYarnTest based on CliFrontendRunTest that verifies
      CliFrontend's parsing in conjunction with FlinkYarnSessionCli
    -> verify detached mode in this test (can be extended further in the future)
    
    This closes #5672.

--- flink-yarn/src/test/java/org/apache/flink/yarn/FlinkYarnSessionCliTest.java
+++ flink-yarn/src/test/java/org/apache/flink/yarn/FlinkYarnSessionCliTest.java
@@ -119,6 +119,25 @@ public class FlinkYarnSessionCliTest extends TestLogger {
 	}
 
 	@Test
+	public void testCorrectSettingOfDetachedMode() throws Exception {
+		String[] params =
+			new String[] {"-yd"};
+
+		FlinkYarnSessionCli yarnCLI = new FlinkYarnSessionCli(
+			new Configuration(),
+			tmp.getRoot().getAbsolutePath(),
+			"y",
+			"yarn");
+
+		final CommandLine commandLine = yarnCLI.parseCommandLineOptions(params, true);
+
+		AbstractYarnClusterDescriptor descriptor = yarnCLI.createClusterDescriptor(commandLine);
+
+		// each task manager has 3 slots but the parallelism is 7. Thus the slots should be increased.
+		assertTrue(descriptor.isDetachedMode());
+	}
+
+	@Test
 	public void testZookeeperNamespaceProperty() throws Exception {
 		String zkNamespaceCliInput = "flink_test_namespace";
 
