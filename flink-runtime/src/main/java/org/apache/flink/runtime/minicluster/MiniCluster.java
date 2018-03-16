0caff357848929b736fca0f78e358f77423ce355
gyao <gary@data-artisans.com>
2018-03-16 06:57:24 +0100

    [hotfix][flip6] Only create new terminationFuture if MiniCluster is running

--- flink-runtime/src/main/java/org/apache/flink/runtime/minicluster/MiniCluster.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/minicluster/MiniCluster.java
@@ -217,9 +217,6 @@ public class MiniCluster implements JobExecutorService, AutoCloseableAsync {
 			LOG.info("Starting Flink Mini Cluster");
 			LOG.debug("Using configuration {}", miniClusterConfiguration);
 
-			// create a new termination future
-			terminationFuture = new CompletableFuture<>();
-
 			final Configuration configuration = miniClusterConfiguration.getConfiguration();
 			final Time rpcTimeout = miniClusterConfiguration.getRpcTimeout();
 			final int numTaskManagers = miniClusterConfiguration.getNumTaskManagers();
@@ -383,6 +380,9 @@ public class MiniCluster implements JobExecutorService, AutoCloseableAsync {
 				}
 				throw e;
 			}
+
+			// create a new termination future
+			terminationFuture = new CompletableFuture<>();
 
 			// now officially mark this as running
 			running = true;
