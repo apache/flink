7a14340827f5a31a11c71e05075732d3c455cda5
Fabian Hueske <fhueske@apache.org>
2018-03-23 21:42:53 +0100

    [FLINK-9067] [e2eTests] Add StreamSQLTestProgram and test run script.

--- flink-end-to-end-tests/run-nightly-tests.sh
+++ flink-end-to-end-tests/run-nightly-tests.sh
@@ -47,5 +47,13 @@ EXIT_CODE=0
 #     EXIT_CODE=$?
 # fi
 
+if [ $EXIT_CODE == 0 ]; then
+  printf "\n==============================================================================\n"
+  printf "Running Streaming SQL nightly end-to-end test\n"
+  printf "==============================================================================\n"
+  $END_TO_END_DIR/test-scripts/test_streaming_sql.sh
+  EXIT_CODE=$?
+fi
+
 # Exit code for Travis build success/failure
 exit $EXIT_CODE
