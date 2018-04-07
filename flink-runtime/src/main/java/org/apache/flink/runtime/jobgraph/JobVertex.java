ef36e3a10a28d6ed5bb7116307d4470e34314b36
sihuazhou <summerleafs@163.com>
2018-04-07 22:12:37 +0800

    fix javadoc for JobVertex#setMaxParallelism()

--- flink-runtime/src/main/java/org/apache/flink/runtime/jobgraph/JobVertex.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/jobgraph/JobVertex.java
@@ -27,6 +27,7 @@ import org.apache.flink.runtime.jobgraph.tasks.AbstractInvokable;
 import org.apache.flink.runtime.jobgraph.tasks.StoppableTask;
 import org.apache.flink.runtime.jobmanager.scheduler.CoLocationGroup;
 import org.apache.flink.runtime.jobmanager.scheduler.SlotSharingGroup;
+import org.apache.flink.runtime.state.KeyGroupRangeAssignment;
 import org.apache.flink.util.Preconditions;
 
 import java.util.ArrayList;
@@ -304,7 +305,7 @@ public class JobVertex implements java.io.Serializable {
 	/**
 	 * Sets the maximum parallelism for the task.
 	 *
-	 * @param maxParallelism The maximum parallelism to be set. must be between 1 and Short.MAX_VALUE.
+	 * @param maxParallelism The maximum parallelism to be set. must be between 1 and {@link KeyGroupRangeAssignment#UPPER_BOUND_MAX_PARALLELISM}.
 	 */
 	public void setMaxParallelism(int maxParallelism) {
 		this.maxParallelism = maxParallelism;
