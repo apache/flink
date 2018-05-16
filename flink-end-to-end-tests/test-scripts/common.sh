971822fe8d8b7ee399a432c45185edec7638688b
zentol <chesnay@apache.org>
2018-05-16 10:17:52 +0200

    Add utility function for running tests

--- flink-end-to-end-tests/test-scripts/common.sh
+++ flink-end-to-end-tests/test-scripts/common.sh
@@ -391,6 +391,26 @@ function end_timer {
     echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 }
 
+#######################################
+# Prints the given description, runs the given test and prints how long the execution took.
+# Arguments:
+#   $1: description of the test
+#   $2: command to execute
+#######################################
+function run_test {
+    description="$1"
+    command="$2"
+
+    printf "\n==============================================================================\n"
+    printf "Running ${description}\n"
+    printf "==============================================================================\n"
+    start_timer
+    ${command}
+    exit_code="$?"
+    end_timer
+    return "${exit_code}"
+}
+
 # make sure to clean up even in case of failures
 function cleanup {
   stop_cluster
