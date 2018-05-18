5093122804f481021f3b33d9d5b4da2144c8cc61
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2018-05-18 18:45:57 +0800

    [FLINK-9008] [e2e] Reuse flink-elasticsearch5-test job code as quickstart e2e test's modified job
    Previously, the modified job used in the `test_quickstarts.sh` test
    script is maintained as a new Maven module. This is an overkill, since all
    we are doing is replacing the quickstart's contained job with something
    more complex and with additional dependencies.
    
    This commit changes this by simply reusing job code in
    flink-elasticsearch5-test as the modified job,
    which is copied to the quickstart project.
    
    This closes #5823.

--- flink-end-to-end-tests/test-scripts/test_quickstarts.sh
+++ flink-end-to-end-tests/test-scripts/test_quickstarts.sh
@@ -20,6 +20,7 @@
 # End to end test for quick starts test.
 
 source "$(dirname "$0")"/common.sh
+source "$(dirname "$0")"/elasticsearch-common.sh
 
 mkdir -p $TEST_DATA_DIR
 
@@ -37,18 +38,20 @@ mvn archetype:generate                             \
 
 cd flink-java-project
 
-cp $TEST_DATA_DIR/../../flink-quickstart-test/src/main/java/org/apache/flink/quickstart/ElasticsearchStreamingJob.java $TEST_DATA_DIR/flink-java-project/src/main/java/org/apache/flink/quickstart/
+# use the Flink Elasticsearch sink example job code in flink-elasticsearch5-tests to simulate modifications to contained job
+cp $TEST_INFRA_DIR/../flink-elasticsearch5-test/src/main/java/org/apache/flink/streaming/tests/Elasticsearch5SinkExample.java $TEST_DATA_DIR/flink-java-project/src/main/java/org/apache/flink/quickstart/
+sed -i -e 's/package org.apache.flink.streaming.tests;/package org.apache.flink.quickstart;/' $TEST_DATA_DIR/flink-java-project/src/main/java/org/apache/flink/quickstart/Elasticsearch5SinkExample.java
 
 position=$(awk '/<dependencies>/ {print NR}' pom.xml | head -1)
 
 sed -i -e ''"$(($position + 1))"'i\
 <dependency>\
 <groupId>org.apache.flink</groupId>\
-<artifactId>flink-connector-elasticsearch2_${scala.binary.version}</artifactId>\
+<artifactId>flink-connector-elasticsearch5_${scala.binary.version}</artifactId>\
 <version>${flink.version}</version>\
 </dependency>' pom.xml
 
-sed -i -e "s/org.apache.flink.quickstart.StreamingJob/org.apache.flink.quickstart.ElasticsearchStreamingJob/" pom.xml
+sed -i -e "s/org.apache.flink.quickstart.StreamingJob/org.apache.flink.streaming.tests.Elasticsearch5SinkExample/" pom.xml
 
 mvn clean package -nsu
 
@@ -69,59 +72,35 @@ else
 fi
 
 if [[ `grep -c "org/apache/flink/quickstart/StreamingJob.class" contentsInJar.txt` -eq '0' && \
-      `grep -c "org/apache/flink/quickstart/ElasticsearchStreamingJob.class" contentsInJar.txt` -eq '0' && \
-      `grep -c "org/apache/flink/streaming/connectors/elasticsearch2" contentsInJar.txt` -eq '0' ]]; then
+      `grep -c "org/apache/flink/quickstart/Elasticsearch5SinkExample.class" contentsInJar.txt` -eq '0' && \
+      `grep -c "org/apache/flink/streaming/connectors/elasticsearch5" contentsInJar.txt` -eq '0' ]]; then
 
-    echo "Failure: Since ElasticsearchStreamingJob.class and other user classes are not included in the jar. "
+    echo "Failure: Since Elasticsearch5SinkExample.class and other user classes are not included in the jar. "
     PASS=""
     exit 1
 else
-    echo "Success: ElasticsearchStreamingJob.class and other user classes are included in the jar."
+    echo "Success: Elasticsearch5SinkExample.class and other user classes are included in the jar."
 fi
 
-ELASTICSEARCH_URL="https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.3.5/elasticsearch-2.3.5.tar.gz"
+setup_elasticsearch "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.1.2.tar.gz"
+verify_elasticsearch_process_exist
 
-curl "$ELASTICSEARCH_URL" > $TEST_DATA_DIR/elasticsearch.tar.gz
-tar xzf $TEST_DATA_DIR/elasticsearch.tar.gz -C $TEST_DATA_DIR/
-ELASTICSEARCH_DIR=$TEST_DATA_DIR/elasticsearch-2.3.5
+function shutdownAndCleanup {
+    shutdown_elasticsearch_cluster
 
-nohup $ELASTICSEARCH_DIR/bin/elasticsearch &
-
-ELASTICSEARCH_PROCESS=$(jps | grep Elasticsearch | awk '{print $2}')
-
-# make sure the elasticsearch node is actually running
-if [ "$ELASTICSEARCH_PROCESS" != "Elasticsearch" ]; then
-  echo "Elasticsearch node is not running."
-  PASS=""
-  exit 1
-else
-  echo "Elasticsearch node is running."
-fi
+    # make sure to run regular cleanup as well
+    cleanup
+}
+trap shutdownAndCleanup INT
+trap shutdownAndCleanup EXIT
 
 TEST_PROGRAM_JAR=$TEST_DATA_DIR/flink-java-project/target/flink-java-project-0.1.jar
 
 start_cluster
 
-# run the Flink job
-$FLINK_DIR/bin/flink run -c org.apache.flink.quickstart.ElasticsearchStreamingJob $TEST_PROGRAM_JAR
+$FLINK_DIR/bin/flink run -c org.apache.flink.quickstart.Elasticsearch5SinkExample $TEST_PROGRAM_JAR \
+  --numRecords 20 \
+  --index index \
+  --type type
 
-curl 'localhost:9200/my-index/_search?q=*&pretty&size=21' > $TEST_DATA_DIR/output
-
-if [ -n "$(grep '\"total\" : 21' $TEST_DATA_DIR/output)" ]; then
-    echo "Quickstarts end to end test pass."
-else
-    echo "Quickstarts end to end test failed."
-    PASS=""
-    exit 1
-fi
-
-function shutdownAndCleanup {
-    pid=$(jps | grep Elasticsearch | awk '{print $1}')
-    kill -SIGTERM $pid
-
-    # make sure to run regular cleanup as well
-    cleanup
-}
-
-trap shutdownAndCleanup INT
-trap shutdownAndCleanup EXIT
+verify_result 20
