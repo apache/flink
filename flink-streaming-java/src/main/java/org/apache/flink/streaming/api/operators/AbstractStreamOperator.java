2a80de7686ac6828b06d68890162a6dfdb69b75a
Stephan Ewen <sewen@apache.org>
2018-05-23 17:24:40 +0200

    [FLINK-9428] [runtime] Add a 'pre-barrier-emit' checkpoint notification to stream operators.
    This allows operators with small transient unmanaged state (for example pre-aggregates) to
    flush the state prior to the checkpoint barrier.

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/operators/AbstractStreamOperator.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/operators/AbstractStreamOperator.java
@@ -347,6 +347,12 @@ public abstract class AbstractStreamOperator<OUT>
 	}
 
 	@Override
+	public void prepareSnapshotPreBarrier(long checkpointId) throws Exception {
+		// the default implementation does nothing and accepts the checkpoint
+		// this is purely for subclasses to override
+	}
+
+	@Override
 	public final OperatorSnapshotFutures snapshotState(long checkpointId, long timestamp, CheckpointOptions checkpointOptions,
 			CheckpointStreamFactory factory) throws Exception {
 
