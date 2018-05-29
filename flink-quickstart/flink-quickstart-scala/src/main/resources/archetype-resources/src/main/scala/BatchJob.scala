20f5471661fbcec65503bf8a40752b3525492163
Alexey Tsitkin <alexeyts@users.noreply.github.com>
2018-05-29 11:42:53 -0700

    [FLINK-9472] fix archetype documentation typo
    This closes #6098

--- flink-quickstart/flink-quickstart-scala/src/main/resources/archetype-resources/src/main/scala/BatchJob.scala
+++ flink-quickstart/flink-quickstart-scala/src/main/resources/archetype-resources/src/main/scala/BatchJob.scala
@@ -26,7 +26,7 @@ import org.apache.flink.api.scala._
  * For a tutorial how to write a Flink batch application, check the
  * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
  *
- * To package your appliation into a JAR file for execution,
+ * To package your application into a JAR file for execution,
  * change the main class in the POM.xml file to this class (simply search for 'mainClass')
  * and run 'mvn clean package' on the command line.
  */
