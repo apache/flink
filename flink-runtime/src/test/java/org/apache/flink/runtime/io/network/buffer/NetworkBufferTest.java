1c5b732345c32bb48e694afb3636e7b3c148efec
Piotr Nowojski <piotr.nowojski@gmail.com>
2018-05-30 11:24:53 +0200

    [hotfix][runtime-test] Rename BufferTest to NetworkBufferTest
    BufferTest was not testing a Buffer interface but NetworkBuffer class.

--- flink-runtime/src/test/java/org/apache/flink/runtime/io/network/buffer/NetworkBufferTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/io/network/buffer/NetworkBufferTest.java
@@ -35,9 +35,9 @@ import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Tests for the {@link Buffer} class.
+ * Tests for the {@link NetworkBuffer} class.
  */
-public class BufferTest extends AbstractByteBufTest {
+public class NetworkBufferTest extends AbstractByteBufTest {
 
 	/**
 	 * Upper limit for the max size that is sufficient for all the tests.
