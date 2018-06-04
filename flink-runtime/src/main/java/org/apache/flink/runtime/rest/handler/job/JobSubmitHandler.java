42db1bffeb7d8d49f0a0c0280f9c3110b8587dc1
zentol <chesnay@apache.org>
2018-06-04 15:43:11 +0200

    [FLINK-9280][rest] server modifications

--- flink-runtime/src/main/java/org/apache/flink/runtime/rest/handler/job/JobSubmitHandler.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/rest/handler/job/JobSubmitHandler.java
@@ -18,7 +18,12 @@
 
 package org.apache.flink.runtime.rest.handler.job;
 
+import org.apache.flink.api.common.cache.DistributedCache;
 import org.apache.flink.api.common.time.Time;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.blob.BlobClient;
+import org.apache.flink.runtime.blob.PermanentBlobKey;
 import org.apache.flink.runtime.dispatcher.DispatcherGateway;
 import org.apache.flink.runtime.jobgraph.JobGraph;
 import org.apache.flink.runtime.rest.handler.AbstractRestHandler;
@@ -28,35 +33,51 @@ import org.apache.flink.runtime.rest.messages.EmptyMessageParameters;
 import org.apache.flink.runtime.rest.messages.job.JobSubmitHeaders;
 import org.apache.flink.runtime.rest.messages.job.JobSubmitRequestBody;
 import org.apache.flink.runtime.rest.messages.job.JobSubmitResponseBody;
+import org.apache.flink.runtime.util.ScalaUtils;
 import org.apache.flink.runtime.webmonitor.retriever.GatewayRetriever;
 
 import org.apache.flink.shaded.netty4.io.netty.handler.codec.http.HttpResponseStatus;
 
+import akka.actor.AddressFromURIString;
+import org.slf4j.Logger;
+
 import javax.annotation.Nonnull;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.stream.Collectors;
 
 /**
  * This handler can be used to submit jobs to a Flink cluster.
  */
 public final class JobSubmitHandler extends AbstractRestHandler<DispatcherGateway, JobSubmitRequestBody, JobSubmitResponseBody, EmptyMessageParameters> {
 
+	private final Configuration config;
+
 	public JobSubmitHandler(
 			CompletableFuture<String> localRestAddress,
 			GatewayRetriever<? extends DispatcherGateway> leaderRetriever,
 			Time timeout,
-			Map<String, String> headers) {
+			Map<String, String> headers,
+			Configuration config) {
 		super(localRestAddress, leaderRetriever, timeout, headers, JobSubmitHeaders.getInstance());
+		this.config = config;
 	}
 
 	@Override
 	protected CompletableFuture<JobSubmitResponseBody> handleRequest(@Nonnull HandlerRequest<JobSubmitRequestBody, EmptyMessageParameters> request, @Nonnull DispatcherGateway gateway) throws RestHandlerException {
+		final JobSubmitRequestBody requestBody = request.getRequestBody();
 		JobGraph jobGraph;
-		try {
-			ObjectInputStream objectIn = new ObjectInputStream(new ByteArrayInputStream(request.getRequestBody().serializedJobGraph));
+		try (ObjectInputStream objectIn = new ObjectInputStream(new ByteArrayInputStream(requestBody.serializedJobGraph))) {
 			jobGraph = (JobGraph) objectIn.readObject();
 		} catch (Exception e) {
 			throw new RestHandlerException(
@@ -65,7 +86,96 @@ public final class JobSubmitHandler extends AbstractRestHandler<DispatcherGatewa
 				e);
 		}
 
-		return gateway.submitJob(jobGraph, timeout)
+		updateJarEntriesInJobGraph(jobGraph, requestBody.getUploadedJars(), log);
+		updateUserArtifactEntriesInJobGraph(jobGraph, requestBody.getUploadedArtifacts(), log);
+
+		CompletableFuture<Integer> blobServerPortFuture = gateway.getBlobServerPort(timeout);
+
+		CompletableFuture<JobGraph> jobGraphFuture = blobServerPortFuture.thenApply(blobServerPort -> {
+			final InetSocketAddress address = new InetSocketAddress(getDispatcherHost(gateway), blobServerPort);
+			final List<PermanentBlobKey> keys;
+			try {
+				keys = BlobClient.uploadFiles(address, config, jobGraph.getJobID(), jobGraph.getUserJars());
+				jobGraph.uploadUserArtifacts(address, config);
+			} catch (IOException ioe) {
+				log.error("Could not upload job jar files.", ioe);
+				throw new CompletionException(new RestHandlerException("Could not upload job jar files.", HttpResponseStatus.INTERNAL_SERVER_ERROR));
+			}
+
+			for (PermanentBlobKey key : keys) {
+				jobGraph.addUserJarBlobKey(key);
+			}
+
+			return jobGraph;
+		});
+
+		CompletableFuture<JobSubmitResponseBody> submissionFuture = jobGraphFuture
+			.thenCompose(finalizedJobGraph -> gateway.submitJob(jobGraph, timeout))
 			.thenApply(ack -> new JobSubmitResponseBody("/jobs/" + jobGraph.getJobID()));
+
+		CompletableFuture<Void> submissionCleanupFuture = submissionFuture.thenRun(requestBody::cleanup);
+
+		return submissionFuture.thenCombine(submissionCleanupFuture, (responseBody, ignored) -> responseBody);
+	}
+
+	/**
+	 * Updates the jar entries in the given JobGraph to refer to the uploaded jar files instead of client-local files.
+	 */
+	private static void updateJarEntriesInJobGraph(JobGraph jobGraph, Collection<Path> uploadedJars, Logger log) {
+		// the existing entries still reference client-local jars
+		jobGraph.getUserJars().clear();
+		for (Path jar : uploadedJars) {
+			log.debug("Adding jar {} to JobGraph({}).", jar, jobGraph.getJobID());
+			jobGraph.addJar(new org.apache.flink.core.fs.Path(jar.toUri()));
+		}
+	}
+
+	/**
+	 * Updates the user-artifact entries in the given JobGraph to refer to the uploaded artifacts instead of client-local artifacts.
+	 */
+	private static void updateUserArtifactEntriesInJobGraph(JobGraph jobGraph, Collection<Path> uploadedArtifacts, Logger log) {
+		// match the names of uploaded files to the names stored in the distributed cache entries to find entries we have to override
+
+		// create a new map from file name -> distributed cache map entry
+		Map<String, Tuple2<String, DistributedCache.DistributedCacheEntry>> remappedArtifactEntries = jobGraph.getUserArtifacts().entrySet().stream()
+			.collect(Collectors.toMap(
+				entry -> new org.apache.flink.core.fs.Path(entry.getValue().filePath).getName(),
+				entry -> Tuple2.of(entry.getKey(), entry.getValue())
+			));
+		// create a new map from file name -> local file
+		Map<String, Path> mappedUploadedArtifacts = uploadedArtifacts.stream()
+			.collect(Collectors.toMap(
+				artifact -> new org.apache.flink.core.fs.Path(artifact.toUri()).getName(),
+				artifact -> artifact
+			));
+
+		if (!remappedArtifactEntries.isEmpty() && !mappedUploadedArtifacts.isEmpty()) {
+			jobGraph.getUserArtifacts().clear();
+			for (Map.Entry<String, Tuple2<String, DistributedCache.DistributedCacheEntry>> entry : remappedArtifactEntries.entrySet()) {
+				String fileName = entry.getKey();
+				String dcEntryName = entry.getValue().f0;
+				DistributedCache.DistributedCacheEntry dcEntry = entry.getValue().f1;
+
+				Path uploadedArtifact = mappedUploadedArtifacts.get(fileName);
+				if (uploadedArtifact != null) {
+					log.debug("Overwriting path {} for distributed-cache entry {} with {}.", dcEntry.filePath, dcEntryName, uploadedArtifact.toFile());
+					jobGraph.addUserArtifact(dcEntryName, new DistributedCache.DistributedCacheEntry(
+						uploadedArtifact.toString(), dcEntry.isExecutable, dcEntry.isZipped));
+				} else {
+					jobGraph.addUserArtifact(dcEntryName, dcEntry);
+				}
+			}
+		}
+	}
+
+	private static String getDispatcherHost(DispatcherGateway gateway) {
+		String dispatcherAddress = gateway.getAddress();
+		final Optional<String> host = ScalaUtils.toJava(AddressFromURIString.parse(dispatcherAddress).host());
+
+		return host.orElseGet(() -> {
+			// if the dispatcher address does not contain a host part, then assume it's running
+			// on the same machine as the handler
+			return "localhost";
+		});
 	}
 }
--- flink-runtime/src/main/java/org/apache/flink/runtime/rest/messages/job/JobSubmitRequestBody.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/rest/messages/job/JobSubmitRequestBody.java
@@ -20,15 +20,20 @@ package org.apache.flink.runtime.rest.messages.job;
 
 import org.apache.flink.runtime.jobgraph.JobGraph;
 import org.apache.flink.runtime.rest.messages.RequestBody;
+import org.apache.flink.util.FileUtils;
 import org.apache.flink.util.Preconditions;
 
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonCreator;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonIgnore;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Request for submitting a job.
@@ -45,14 +50,41 @@ public final class JobSubmitRequestBody implements RequestBody {
 	@JsonProperty(FIELD_NAME_SERIALIZED_JOB_GRAPH)
 	public final byte[] serializedJobGraph;
 
+	private final java.nio.file.Path storageDirectory;
+	private final List<Path> uploadedJars;
+	private final List<Path> uploadedArtifacts;
+
+	/**
+	 * Client-side constructor.
+	 */
 	public JobSubmitRequestBody(JobGraph jobGraph) throws IOException {
-		this(serializeJobGraph(jobGraph));
+		this(serializeJobGraph(jobGraph), Collections.emptyList(), Collections.emptyList(), null);
 	}
 
 	@JsonCreator
 	public JobSubmitRequestBody(
 			@JsonProperty(FIELD_NAME_SERIALIZED_JOB_GRAPH) byte[] serializedJobGraph) {
+		this(serializedJobGraph, Collections.emptyList(), Collections.emptyList(), null);
+	}
+
+	/**
+	 * Intermediate constructor used by the {@link org.apache.flink.runtime.rest.FileUploadHandler}.
+	 */
+	public JobSubmitRequestBody(byte[] serializedJobGraph, List<Path> uploadedJars, List<Path> uploadedArtifacts, java.nio.file.Path storageDirectory) {
 		this.serializedJobGraph = Preconditions.checkNotNull(serializedJobGraph);
+		this.uploadedJars = Preconditions.checkNotNull(uploadedJars);
+		this.uploadedArtifacts = Preconditions.checkNotNull(uploadedArtifacts);
+		this.storageDirectory = storageDirectory;
+	}
+
+	@JsonIgnore
+	public List<Path> getUploadedJars() {
+		return uploadedJars;
+	}
+
+	@JsonIgnore
+	public List<Path> getUploadedArtifacts() {
+		return uploadedArtifacts;
 	}
 
 	@Override
@@ -76,6 +108,12 @@ public final class JobSubmitRequestBody implements RequestBody {
 			out.writeObject(jobGraph);
 
 			return baos.toByteArray();
+		}
+	}
+
+	public void cleanup() {
+		if (storageDirectory != null) {
+			FileUtils.deleteDirectoryQuietly(storageDirectory.toFile());
 		}
 	}
 }
