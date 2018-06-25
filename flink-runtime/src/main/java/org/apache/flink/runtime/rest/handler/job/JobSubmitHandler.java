7b5adaa3b875f4d3aaf57ff10d97282b600aeabe
zentol <chesnay@apache.org>
2018-06-25 11:25:41 +0200

    move jar/artifact file matching into separate methods

--- flink-runtime/src/main/java/org/apache/flink/runtime/rest/handler/job/JobSubmitHandler.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/rest/handler/job/JobSubmitHandler.java
@@ -95,17 +95,9 @@ public final class JobSubmitHandler extends AbstractRestHandler<DispatcherGatewa
 			return jobGraph;
 		});
 
-		Collection<org.apache.flink.core.fs.Path> jarFiles = new ArrayList<>(requestBody.jarFileNames.size());
-		for (String jarFileName : requestBody.jarFileNames) {
-			Path jarFile = getPathAndAssertUpload(jarFileName, FILE_TYPE_JAR, nameToFile);
-			jarFiles.add(new org.apache.flink.core.fs.Path(jarFile.toString()));
-		}
+		Collection<org.apache.flink.core.fs.Path> jarFiles = getJarFilesToUpload(nameToFile, requestBody.jarFileNames);
 
-		Collection<Tuple2<String, org.apache.flink.core.fs.Path>> artifacts = new ArrayList<>(requestBody.artifactFileNames.size());
-		for (JobSubmitRequestBody.DistributedCacheFile artifactFileName : requestBody.artifactFileNames) {
-			Path artifactFile = getPathAndAssertUpload(artifactFileName.fileName, FILE_TYPE_ARTIFACT, nameToFile);
-			artifacts.add(Tuple2.of(artifactFileName.entryName, new org.apache.flink.core.fs.Path(artifactFile.toString())));
-		}
+		Collection<Tuple2<String, org.apache.flink.core.fs.Path>> artifacts = getArtifactFilesToUpload(nameToFile, requestBody.artifactFileNames);
 
 		CompletableFuture<Integer> blobServerPortFuture = gateway.getBlobServerPort(timeout);
 
@@ -129,6 +121,25 @@ public final class JobSubmitHandler extends AbstractRestHandler<DispatcherGatewa
 
 		return jobSubmissionFuture.thenCombine(jobGraphFuture,
 			(ack, jobGraph) -> new JobSubmitResponseBody("/jobs/" + jobGraph.getJobID()));
+	}
+
+	private static Collection<org.apache.flink.core.fs.Path> getJarFilesToUpload(Map<String, Path> nameToFileMap, Collection<String> jarFileNames) throws MissingFileException {
+		Collection<org.apache.flink.core.fs.Path> jarFiles = new ArrayList<>(jarFileNames.size());
+		for (String jarFileName : jarFileNames) {
+			Path jarFile = getPathAndAssertUpload(jarFileName, FILE_TYPE_JAR, nameToFileMap);
+			jarFiles.add(new org.apache.flink.core.fs.Path(jarFile.toString()));
+		}
+		return jarFiles;
+	}
+
+	private static Collection<Tuple2<String, org.apache.flink.core.fs.Path>> getArtifactFilesToUpload(Map<String, Path> nameToFileMap, Collection<JobSubmitRequestBody.DistributedCacheFile> artifactEntries) throws MissingFileException {
+		Collection<Tuple2<String, org.apache.flink.core.fs.Path>> artifacts = new ArrayList<>(artifactEntries.size());
+		for (JobSubmitRequestBody.DistributedCacheFile artifactFileName : artifactEntries) {
+			Path artifactFile = getPathAndAssertUpload(artifactFileName.fileName, FILE_TYPE_ARTIFACT, nameToFileMap);
+			artifacts.add(Tuple2.of(artifactFileName.entryName, new org.apache.flink.core.fs.Path(artifactFile.toString())));
+		}
+		
+		return artifacts;
 	}
 
 	private static Path getPathAndAssertUpload(String fileName, String type, Map<String, Path> uploadedFiles) throws MissingFileException {
