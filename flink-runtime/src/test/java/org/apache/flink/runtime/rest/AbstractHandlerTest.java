cfa5c5ce8e2158e623b3775d2cb65dff8f92816a
zentol <chesnay@apache.org>
2018-06-25 13:00:42 +0200

    [proposal] extend AbstractHandler test for cleanup timing

--- flink-runtime/src/test/java/org/apache/flink/runtime/rest/AbstractHandlerTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/rest/AbstractHandlerTest.java
@@ -81,7 +81,8 @@ public class AbstractHandlerTest extends TestLogger {
 		final GatewayRetriever<RestfulGateway> mockGatewayRetriever = () ->
 			CompletableFuture.completedFuture(mockRestfulGateway);
 
-		TestHandler handler = new TestHandler(CompletableFuture.completedFuture(restAddress), mockGatewayRetriever);
+		CompletableFuture<Void> requestProcessingCompleteFuture = new CompletableFuture<>();
+		TestHandler handler = new TestHandler(requestProcessingCompleteFuture, CompletableFuture.completedFuture(restAddress), mockGatewayRetriever);
 
 		RouteResult<?> routeResult = new RouteResult<>("", "", Collections.emptyMap(), Collections.emptyMap(), "");
 		HttpRequest request = new DefaultFullHttpRequest(
@@ -101,6 +102,9 @@ public class AbstractHandlerTest extends TestLogger {
 
 		handler.respondAsLeader(context, routerRequest, mockRestfulGateway);
 
+		// the (asynchronous) request processing is not yet complete so the files should still exist
+		Assert.assertTrue(Files.exists(file));
+		requestProcessingCompleteFuture.complete(null);
 		Assert.assertFalse(Files.exists(file));
 	}
 
@@ -156,14 +160,16 @@ public class AbstractHandlerTest extends TestLogger {
 	}
 
 	private static class TestHandler extends AbstractHandler<RestfulGateway, EmptyRequestBody, EmptyMessageParameters> {
+		private final CompletableFuture<Void> completionFuture;
 
-		protected TestHandler(@Nonnull CompletableFuture<String> localAddressFuture, @Nonnull GatewayRetriever<? extends RestfulGateway> leaderRetriever) {
+		protected TestHandler(CompletableFuture<Void> completionFuture, @Nonnull CompletableFuture<String> localAddressFuture, @Nonnull GatewayRetriever<? extends RestfulGateway> leaderRetriever) {
 			super(localAddressFuture, leaderRetriever, RpcUtils.INF_TIMEOUT, Collections.emptyMap(), TestHeaders.INSTANCE);
+			this.completionFuture = completionFuture;
 		}
 
 		@Override
 		protected CompletableFuture<Void> respondToRequest(ChannelHandlerContext ctx, HttpRequest httpRequest, HandlerRequest<EmptyRequestBody, EmptyMessageParameters> handlerRequest, RestfulGateway gateway) throws RestHandlerException {
-			return CompletableFuture.completedFuture(null);
+			return completionFuture;
 		}
 
 		private enum TestHeaders implements UntypedResponseMessageHeaders<EmptyRequestBody, EmptyMessageParameters> {
@@ -187,6 +193,11 @@ public class AbstractHandlerTest extends TestLogger {
 			@Override
 			public String getTargetRestEndpointURL() {
 				return "/test";
+			}
+
+			@Override
+			public boolean acceptsFileUploads() {
+				return true;
 			}
 		}
 	}
