8193d5dc68289760ad68cf0b6b237fd86b0fd906
Nico Kruber <nico@data-artisans.com>
2018-07-05 00:48:33 +0200

    [FLINK-9755][network] forward exceptions in RemoteInputChannel#notifyBufferAvailable() to the responsible thread
    This mainly involves state checks but previously these have only been swallowed
    without re-registration or any other logging/handling. This may have lead to
    some thread stalling while waiting for the notification that never came.
    
    This closes #6272.

--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/buffer/BufferListener.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/buffer/BufferListener.java
@@ -27,6 +27,15 @@ public interface BufferListener {
 	/**
 	 * Notification callback if a buffer is recycled and becomes available in buffer pool.
 	 *
+	 * <p>Note: responsibility on recycling the given buffer is transferred to this implementation,
+	 * including any errors that lead to exceptions being thrown!
+	 *
+	 * <p><strong>BEWARE:</strong> since this may be called from outside the thread that relies on
+	 * the listener's logic, any exception that occurs with this handler should be forwarded to the
+	 * responsible thread for handling and otherwise ignored in the processing of this method. The
+	 * buffer pool forwards any {@link Throwable} from here upwards to a potentially unrelated call
+	 * stack!
+	 *
 	 * @param buffer buffer that becomes available in buffer pool.
 	 * @return true if the listener wants to be notified next time.
 	 */
