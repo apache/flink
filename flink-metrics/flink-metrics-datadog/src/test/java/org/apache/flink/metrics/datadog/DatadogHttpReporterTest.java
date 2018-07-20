6b32e3bf6e603a707945fe6be1c31659eceb6bf2
Addison Higham <ahigham@instructure.com>
2018-07-20 13:21:28 -0600

    Fix whitespace in DataHttpReporter

--- flink-metrics/flink-metrics-datadog/src/test/java/org/apache/flink/metrics/datadog/DatadogHttpReporterTest.java
+++ flink-metrics/flink-metrics-datadog/src/test/java/org/apache/flink/metrics/datadog/DatadogHttpReporterTest.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.flink.metrics.datadog;
 
 import org.apache.flink.configuration.Configuration;
