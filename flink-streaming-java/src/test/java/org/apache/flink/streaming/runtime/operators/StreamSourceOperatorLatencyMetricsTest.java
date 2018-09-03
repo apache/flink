b0522e37181f8485610fd37dda9fc45858b6429e
zentol <chesnay@apache.org>
2018-09-03 13:03:24 +0200

    [FLINK-10242][metrics] Disable latency metrics by default

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/runtime/operators/StreamSourceOperatorLatencyMetricsTest.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/runtime/operators/StreamSourceOperatorLatencyMetricsTest.java
@@ -21,10 +21,12 @@ package org.apache.flink.streaming.runtime.operators;
 import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.accumulators.Accumulator;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.MetricOptions;
 import org.apache.flink.runtime.execution.Environment;
 import org.apache.flink.runtime.jobgraph.OperatorID;
-import org.apache.flink.runtime.operators.testutils.DummyEnvironment;
+import org.apache.flink.runtime.operators.testutils.MockEnvironment;
 import org.apache.flink.runtime.state.memory.MemoryStateBackend;
+import org.apache.flink.runtime.util.TestingTaskManagerRuntimeInfo;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.api.graph.StreamConfig;
@@ -38,6 +40,7 @@ import org.apache.flink.streaming.runtime.tasks.ProcessingTimeService;
 import org.apache.flink.streaming.runtime.tasks.StreamTask;
 import org.apache.flink.streaming.runtime.tasks.TestProcessingTimeService;
 import org.apache.flink.streaming.util.CollectorOutput;
+import org.apache.flink.util.TestLogger;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -55,20 +58,104 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
- * Tests for {@link StreamSource} operators.
+ * Tests for the emission of latency markers by {@link StreamSource} operators.
  */
-@SuppressWarnings("serial")
-public class StreamSourceOperatorLatencyMetricsTest {
+public class StreamSourceOperatorLatencyMetricsTest extends TestLogger {
+
+	private static final long maxProcessingTime = 100L;
+	private static final long latencyMarkInterval = 10L;
 
 	/**
-	 * Test that latency marks are emitted.
+	 * Verifies that by default no latency metrics are emitted.
 	 */
 	@Test
-	public void testLatencyMarkEmission() throws Exception {
-		final List<StreamElement> output = new ArrayList<>();
+	public void testLatencyMarkEmissionDisabled() throws Exception {
+		testLatencyMarkEmission(0, (operator, timeProvider) -> {
+			setupSourceOperator(operator, new ExecutionConfig(), MockEnvironment.builder().build(), timeProvider);
+		});
+	}
 
-		final long maxProcessingTime = 100L;
-		final long latencyMarkInterval = 10L;
+	/**
+	 * Verifies that latency metrics can be enabled via the {@link ExecutionConfig}.
+	 */
+	@Test
+	public void testLatencyMarkEmissionEnabledViaExecutionConfig() throws Exception {
+		testLatencyMarkEmission((int) (maxProcessingTime / latencyMarkInterval) + 1, (operator, timeProvider) -> {
+			ExecutionConfig executionConfig = new ExecutionConfig();
+			executionConfig.setLatencyTrackingInterval(latencyMarkInterval);
+
+			setupSourceOperator(operator, executionConfig, MockEnvironment.builder().build(), timeProvider);
+		});
+	}
+
+	/**
+	 * Verifies that latency metrics can be enabled via the configuration.
+	 */
+	@Test
+	public void testLatencyMarkEmissionEnabledViaFlinkConfig() throws Exception {
+		testLatencyMarkEmission((int) (maxProcessingTime / latencyMarkInterval) + 1, (operator, timeProvider) -> {
+			Configuration tmConfig = new Configuration();
+			tmConfig.setLong(MetricOptions.LATENCY_INTERVAL, latencyMarkInterval);
+
+			Environment env = MockEnvironment.builder()
+				.setTaskManagerRuntimeInfo(new TestingTaskManagerRuntimeInfo(tmConfig))
+				.build();
+
+			setupSourceOperator(operator, new ExecutionConfig(), env, timeProvider);
+		});
+	}
+
+	/**
+	 * Verifies that latency metrics can be enabled via the {@link ExecutionConfig} even if they are disabled via
+	 * the configuration.
+	 */
+	@Test
+	public void testLatencyMarkEmissionEnabledOverrideViaExecutionConfig() throws Exception {
+		testLatencyMarkEmission((int) (maxProcessingTime / latencyMarkInterval) + 1, (operator, timeProvider) -> {
+			ExecutionConfig executionConfig = new ExecutionConfig();
+			executionConfig.setLatencyTrackingInterval(latencyMarkInterval);
+
+			Configuration tmConfig = new Configuration();
+			tmConfig.setLong(MetricOptions.LATENCY_INTERVAL, 0L);
+
+			Environment env = MockEnvironment.builder()
+				.setTaskManagerRuntimeInfo(new TestingTaskManagerRuntimeInfo(tmConfig))
+				.build();
+
+			setupSourceOperator(operator, executionConfig, env, timeProvider);
+		});
+	}
+
+	/**
+	 * Verifies that latency metrics can be disabled via the {@link ExecutionConfig} even if they are enabled via
+	 * the configuration.
+	 */
+	@Test
+	public void testLatencyMarkEmissionDisabledOverrideViaExecutionConfig() throws Exception {
+		testLatencyMarkEmission(0, (operator, timeProvider) -> {
+			Configuration tmConfig = new Configuration();
+			tmConfig.setLong(MetricOptions.LATENCY_INTERVAL, latencyMarkInterval);
+
+			Environment env = MockEnvironment.builder()
+				.setTaskManagerRuntimeInfo(new TestingTaskManagerRuntimeInfo(tmConfig))
+				.build();
+
+			ExecutionConfig executionConfig = new ExecutionConfig();
+			executionConfig.setLatencyTrackingInterval(0);
+
+			setupSourceOperator(operator, executionConfig, env, timeProvider);
+		});
+	}
+
+	private interface OperatorSetupOperation {
+		void setupSourceOperator(
+			StreamSource<Long, ?> operator,
+			TestProcessingTimeService testProcessingTimeService
+		);
+	}
+
+	private void testLatencyMarkEmission(int numberLatencyMarkers, OperatorSetupOperation operatorSetup) throws Exception {
+		final List<StreamElement> output = new ArrayList<>();
 
 		final TestProcessingTimeService testProcessingTimeService = new TestProcessingTimeService();
 		testProcessingTimeService.setCurrentTime(0L);
@@ -78,13 +165,10 @@ public class StreamSourceOperatorLatencyMetricsTest {
 		final StreamSource<Long, ProcessingTimeServiceSource> operator =
 			new StreamSource<>(new ProcessingTimeServiceSource(testProcessingTimeService, processingTimes));
 
-		// emit latency marks every 10 milliseconds.
-		setupSourceOperator(operator, TimeCharacteristic.EventTime, latencyMarkInterval, testProcessingTimeService);
+		operatorSetup.setupSourceOperator(operator, testProcessingTimeService);
 
 		// run and wait to be stopped
 		operator.run(new Object(), mock(StreamStatusMaintainer.class), new CollectorOutput<Long>(output));
-
-		int numberLatencyMarkers = (int) (maxProcessingTime / latencyMarkInterval) + 1;
 
 		assertEquals(
 			numberLatencyMarkers + 1, // + 1 is the final watermark element
@@ -93,8 +177,8 @@ public class StreamSourceOperatorLatencyMetricsTest {
 		long timestamp = 0L;
 
 		int i = 0;
-		// and that its only latency markers + a final watermark
-		for (; i < output.size() - 1; i++) {
+		// verify that its only latency markers + a final watermark
+		for (; i < numberLatencyMarkers; i++) {
 			StreamElement se = output.get(i);
 			Assert.assertTrue(se.isLatencyMarker());
 			Assert.assertEquals(operator.getOperatorID(), se.asLatencyMarker().getOperatorId());
@@ -109,22 +193,17 @@ public class StreamSourceOperatorLatencyMetricsTest {
 
 	// ------------------------------------------------------------------------
 
-	@SuppressWarnings("unchecked")
-	private static <T> void setupSourceOperator(StreamSource<T, ?> operator,
-	                                            TimeCharacteristic timeChar,
-	                                            long latencyMarkInterval,
-	                                            final ProcessingTimeService timeProvider) {
-
-		ExecutionConfig executionConfig = new ExecutionConfig();
-		executionConfig.setLatencyTrackingInterval(latencyMarkInterval);
+	private static <T> void setupSourceOperator(
+			StreamSource<T, ?> operator,
+			ExecutionConfig executionConfig,
+			Environment env,
+			ProcessingTimeService timeProvider) {
 
 		StreamConfig cfg = new StreamConfig(new Configuration());
 		cfg.setStateBackend(new MemoryStateBackend());
 
-		cfg.setTimeCharacteristic(timeChar);
+		cfg.setTimeCharacteristic(TimeCharacteristic.EventTime);
 		cfg.setOperatorID(new OperatorID());
-
-		Environment env = new DummyEnvironment("MockTwoInputTask", 1, 0);
 
 		StreamStatusMaintainer streamStatusMaintainer = mock(StreamStatusMaintainer.class);
 		when(streamStatusMaintainer.getStreamStatus()).thenReturn(StreamStatus.ACTIVE);
