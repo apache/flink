6c4983e7b094358538c53c088dac830d86231973
Stephan Ewen <sewen@apache.org>
2018-09-13 20:49:15 +0200

    [FLINK-10366] [s3] Adjust Presto-based S3 adapter to use common S3 base

--- flink-filesystems/flink-s3-fs-presto/src/main/java/org/apache/flink/fs/s3presto/S3FileSystemFactory.java
+++ flink-filesystems/flink-s3-fs-presto/src/main/java/org/apache/flink/fs/s3presto/S3FileSystemFactory.java
@@ -28,18 +28,16 @@ import com.facebook.presto.hive.PrestoS3FileSystem;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Simple factory for the S3 file system.
  */
 public class S3FileSystemFactory extends AbstractFileSystemFactory {
-	private static final Set<String> PACKAGE_PREFIXES_TO_SHADE =
-		new HashSet<>(Collections.singletonList("com.amazonaws."));
 
-	private static final Set<String> CONFIG_KEYS_TO_SHADE =
-		Collections.unmodifiableSet(new HashSet<>(Collections.singleton("presto.s3.credentials-provider")));
+	private static final Set<String> PACKAGE_PREFIXES_TO_SHADE = Collections.singleton("com.amazonaws.");
+
+	private static final Set<String> CONFIG_KEYS_TO_SHADE = Collections.singleton("presto.s3.credentials-provider");
 
 	private static final String FLINK_SHADING_PREFIX = "org.apache.flink.fs.s3presto.shaded.";
 
