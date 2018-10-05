5cb2bb9327079b5553f7edc965aec89f338124fa
Till Rohrmann <trohrmann@apache.org>
2018-10-05 20:24:43 +0200

    [FLINK-10421][tests] Exclude FluentPropertyBeanIntrospector info message in common.sh
    FluentPropertyBeanIntrospector logs an info message containing the word error. This can cause
    end-to-end tests to fail. In order to prevent this, the respective logging statement is excluded.

--- flink-end-to-end-tests/test-scripts/common.sh
+++ flink-end-to-end-tests/test-scripts/common.sh
@@ -314,6 +314,7 @@ function check_logs_for_errors {
       | grep -v "An exception was thrown by an exception handler" \
       | grep -v "java.lang.NoClassDefFoundError: org/apache/hadoop/yarn/exceptions/YarnException" \
       | grep -v "java.lang.NoClassDefFoundError: org/apache/hadoop/conf/Configuration" \
+      | grep -v "org.apache.flink.fs.shaded.hadoop3.org.apache.commons.beanutils.FluentPropertyBeanIntrospector  - Error when creating PropertyDescriptor for public final void org.apache.flink.fs.shaded.hadoop3.org.apache.commons.configuration2.AbstractConfiguration.setProperty(java.lang.String,java.lang.Object)! Ignoring this property." \
       | grep -iq "error"; then
     echo "Found error in log files:"
     cat $FLINK_DIR/log/*
