a756141cf2201262d68375f8696656d3f1c6aa1a
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2018-10-08 15:38:56 +0800

    [hotfix] [core] Do not initialize serializer snapshot class when loading it
    This lets errors during static initialization happen not in the
    deserialization stack, but upon use of the class later, which is often a
    bit easier to understand.

--- flink-core/src/main/java/org/apache/flink/api/common/typeutils/TypeSerializerSnapshotSerializationUtil.java
+++ flink-core/src/main/java/org/apache/flink/api/common/typeutils/TypeSerializerSnapshotSerializationUtil.java
@@ -149,7 +149,7 @@ public class TypeSerializerSnapshotSerializationUtil {
 			Class<? extends TypeSerializerSnapshot> serializerConfigSnapshotClass;
 			try {
 				serializerConfigSnapshotClass = (Class<? extends TypeSerializerSnapshot>)
-					Class.forName(serializerConfigClassname, true, userCodeClassLoader);
+					Class.forName(serializerConfigClassname, false, userCodeClassLoader);
 			} catch (ClassNotFoundException e) {
 				throw new IOException(
 					"Could not find requested TypeSerializerConfigSnapshot class "
