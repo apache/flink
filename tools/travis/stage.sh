86f88f71253c8fc916de0dae1c0621072b0321c0
kkloudas <kkloudas@gmail.com>
2018-10-10 12:16:08 +0200

    [FLINK-10529][build] Add flink-s3-fs-base to travis stage file.
    This closes #6795.

--- tools/travis/stage.sh
+++ tools/travis/stage.sh
@@ -58,6 +58,7 @@ MODULES_CONNECTORS="\
 flink-contrib/flink-connector-wikiedits,\
 flink-filesystems/flink-hadoop-fs,\
 flink-filesystems/flink-mapr-fs,\
+flink-filesystems/flink-s3-fs-base,\
 flink-filesystems/flink-s3-fs-hadoop,\
 flink-filesystems/flink-s3-fs-presto,\
 flink-formats/flink-avro,\
