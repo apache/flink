27390db1b136f9395697994e6e2dc39f83a298b7
jrthe42 <jrthe42@gmail.com>
2018-10-15 23:43:48 +0800

    Unified sink and source table name in SQL statement

--- flink-libraries/flink-table/src/main/scala/org/apache/flink/table/api/TableEnvironment.scala
+++ flink-libraries/flink-table/src/main/scala/org/apache/flink/table/api/TableEnvironment.scala
@@ -747,7 +747,8 @@ abstract class TableEnvironment(val config: TableConfig) {
         val queryResult = new Table(this, LogicalRelNode(planner.rel(validatedQuery).rel))
 
         // get name of sink table
-        val targetTableName = insert.getTargetTable.asInstanceOf[SqlIdentifier].names.get(0)
+        val targetTableName =
+          insert.getTargetTable.asInstanceOf[SqlIdentifier].names.asScala.mkString(".")
 
         // insert query result into sink table
         insertInto(queryResult, targetTableName, config)
--- flink-libraries/flink-table/src/test/scala/org/apache/flink/table/api/ExternalCatalogInsertTest.scala
+++ flink-libraries/flink-table/src/test/scala/org/apache/flink/table/api/ExternalCatalogInsertTest.scala
@@ -53,7 +53,7 @@ class ExternalCatalogInsertTest extends TableTestBase {
       "test",
       CommonTestData.getInMemoryTestCatalog(isStreaming = false))
 
-    val sqlInsert = "INSERT INTO `test.db3.tb3` SELECT d * 2, e, g FROM test.db2.tb2 WHERE d < 3 " +
+    val sqlInsert = "INSERT INTO test.db3.tb3 SELECT d * 2, e, g FROM test.db2.tb2 WHERE d < 3 " +
       "UNION ALL (SELECT a * 2, b, c FROM test.db1.tb1)"
 
     tableBatchEnv.sqlUpdate(sqlInsert)
@@ -84,7 +84,7 @@ class ExternalCatalogInsertTest extends TableTestBase {
       "test",
       CommonTestData.getInMemoryTestCatalog(isStreaming = true))
 
-    val sqlInsert = "INSERT INTO `test.db3.tb3` SELECT d * 2, e, g FROM test.db2.tb2 WHERE d < 3 " +
+    val sqlInsert = "INSERT INTO test.db3.tb3 SELECT d * 2, e, g FROM test.db2.tb2 WHERE d < 3 " +
       "UNION ALL (SELECT a * 2, b, c FROM test.db1.tb1)"
 
     tableEnv.sqlUpdate(sqlInsert)
