d1d78aac1cfd4ab0a1265b8211b5c5681e5e849a
Till Rohrmann <trohrmann@apache.org>
2018-10-17 14:52:02 +0200

    [FLINK-10582] Make REST executor's thread priority configurable
    Introduce RestOptions#SERVER_THREAD_PRIORITY("rest.server.thread-priority") to configure the
    thread priority of the REST executor's threads.

--- flink-runtime/src/main/java/org/apache/flink/runtime/minicluster/MiniCluster.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/minicluster/MiniCluster.java
@@ -348,6 +348,7 @@ public class MiniCluster implements JobExecutorService, AutoCloseableAsync {
 					blobServer.getTransientBlobService(),
 					WebMonitorEndpoint.createExecutorService(
 						configuration.getInteger(RestOptions.SERVER_NUM_THREADS, 1),
+						configuration.getInteger(RestOptions.SERVER_THREAD_PRIORITY),
 						"DispatcherRestEndpoint"),
 					new AkkaQueryServiceRetriever(
 						actorSystem,
