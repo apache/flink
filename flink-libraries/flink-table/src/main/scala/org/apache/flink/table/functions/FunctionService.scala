c91ed539715253776616e402056eba3782eccddc
Timo Walther <twalthr@apache.org>
2018-10-22 13:48:01 +0200

    [FLINK-9737] [table] Make function validation optional

--- flink-libraries/flink-table/src/main/scala/org/apache/flink/table/functions/FunctionService.scala
+++ flink-libraries/flink-table/src/main/scala/org/apache/flink/table/functions/FunctionService.scala
@@ -47,18 +47,22 @@ object FunctionService extends Logging {
     *
     * @param descriptor the descriptor that describes a function
     * @param classLoader the class loader to load the function and its parameter's classes
+    * @param performValidation whether or not the descriptor should be validated
     * @return the generated user-defined function
     */
   def createFunction(
       descriptor: FunctionDescriptor,
-      classLoader: ClassLoader)
+      classLoader: ClassLoader,
+      performValidation: Boolean = true)
     : UserDefinedFunction = {
 
     val descriptorProperties = new DescriptorProperties(true)
     descriptor.addProperties(descriptorProperties)
 
     // validate
-    new FunctionDescriptorValidator().validate(descriptorProperties)
+    if (performValidation) {
+      new FunctionDescriptorValidator().validate(descriptorProperties)
+    }
 
     // instantiate
     val (instanceClass, instance) = generateInstance[AnyRef](
