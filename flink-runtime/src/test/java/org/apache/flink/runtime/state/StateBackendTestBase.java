eacfd1b4b81fb4fdb8d1c40dcf9e6f94c865560a
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2018-10-26 13:43:57 +0800

    fixup! [FLINK-9808] [state backends] Migrate state when necessary in state backends

--- flink-runtime/src/test/java/org/apache/flink/runtime/state/StateBackendTestBase.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/state/StateBackendTestBase.java
@@ -133,7 +133,6 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.junit.Assume.assumeThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
