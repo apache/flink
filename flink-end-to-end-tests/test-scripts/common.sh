ed47ff8d9128f19eb1a1f1b0db19fb95290a9182
Till Rohrmann <trohrmann@apache.org>
2018-11-04 19:32:44 +0100

    [FLINK-10773] Harden resume externalized checkpoint end-to-end test
    Ignore the 'Artificial Failure' exceptions and rename
    ExceptionThrowingFailureMapper into FailureMapper to avoid false
    positive exception matchings.

--- flink-end-to-end-tests/test-scripts/common.sh
+++ flink-end-to-end-tests/test-scripts/common.sh
@@ -340,6 +340,7 @@ function check_logs_for_exceptions {
       | grep -v "java.lang.NoClassDefFoundError: org/apache/hadoop/conf/Configuration" \
       | grep -v "java.lang.Exception: Execution was suspended" \
       | grep -v "Caused by: java.lang.Exception: JobManager is shutting down" \
+      | grep -v "java.lang.Exception: Artificial failure" \
       | grep -ic "exception")
   if [[ ${exception_count} -gt 0 ]]; then
     echo "Found exception in log files:"
