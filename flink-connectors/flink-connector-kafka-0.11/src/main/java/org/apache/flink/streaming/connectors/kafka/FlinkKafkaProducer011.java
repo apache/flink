e493d8361e685a84af971aef2d889d8162569ca4
azagrebin <azagrebin@users.noreply.github.com>
2018-11-14 10:08:39 +0100

    [FLINK-10455][Kafka Tx] Close transactional producers in case of failure and termination (#6989)
    This commit addresses the problem of potential leak of resources associated with unclosed Kafka transactional producers in case of commitment failure or task shutdown.
    
    1. always close producer even if commit fails in TwoPhaseCommitSinkFunction#notifyCheckpointComplete
    2. close pending transactions in close method of Kafka Flink function in case of task shutdown
    3. continue trying to commit other transactions in TwoPhaseCommitSinkFunction#notifyCheckpointComplete if any of them failed

--- flink-connectors/flink-connector-kafka-0.11/src/main/java/org/apache/flink/streaming/connectors/kafka/FlinkKafkaProducer011.java
+++ flink-connectors/flink-connector-kafka-0.11/src/main/java/org/apache/flink/streaming/connectors/kafka/FlinkKafkaProducer011.java
@@ -45,6 +45,7 @@ import org.apache.flink.streaming.connectors.kafka.partitioner.FlinkKafkaPartiti
 import org.apache.flink.streaming.util.serialization.KeyedSerializationSchema;
 import org.apache.flink.streaming.util.serialization.KeyedSerializationSchemaWrapper;
 import org.apache.flink.util.ExceptionUtils;
+import org.apache.flink.util.IOUtils;
 import org.apache.flink.util.NetUtils;
 
 import org.apache.flink.shaded.guava18.com.google.common.collect.Lists;
@@ -671,6 +672,9 @@ public class FlinkKafkaProducer011<IN>
 		}
 		// make sure we propagate pending errors
 		checkErroneous();
+		pendingTransactions().forEach(transaction ->
+			IOUtils.closeQuietly(transaction.getValue().producer)
+		);
 	}
 
 	// ------------------- Logic for handling checkpoint flushing -------------------------- //
@@ -714,8 +718,11 @@ public class FlinkKafkaProducer011<IN>
 	protected void commit(KafkaTransactionState transaction) {
 		switch (semantic) {
 			case EXACTLY_ONCE:
-				transaction.producer.commitTransaction();
-				recycleTransactionalProducer(transaction.producer);
+				try {
+					transaction.producer.commitTransaction();
+				} finally {
+					recycleTransactionalProducer(transaction.producer);
+				}
 				break;
 			case AT_LEAST_ONCE:
 			case NONE:
