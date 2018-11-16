58f296d4b723bb99887d90d00ef63c9aa2eb06db
Timo Walther <twalthr@apache.org>
2018-11-16 17:23:24 +0100

    [hotfix] Update SQL Client tests

--- splits/split_misc.sh
+++ splits/split_misc.sh
@@ -69,6 +69,9 @@ run_test "State TTL Heap backend end-to-end test" "$END_TO_END_DIR/test-scripts/
 run_test "State TTL RocksDb backend end-to-end test" "$END_TO_END_DIR/test-scripts/test_stream_state_ttl.sh rocks"
 
 run_test "SQL Client end-to-end test" "$END_TO_END_DIR/test-scripts/test_sql_client.sh"
+run_test "SQL Client end-to-end test for Kafka 0.10" "$END_TO_END_DIR/test-scripts/test_sql_client_kafka010.sh"
+run_test "SQL Client end-to-end test for Kafka 0.11" "$END_TO_END_DIR/test-scripts/test_sql_client_kafka011.sh"
+run_test "SQL Client end-to-end test for modern Kafka" "$END_TO_END_DIR/test-scripts/test_sql_client_kafka.sh"
 
 printf "\n[PASS] All tests passed\n"
 exit 0
