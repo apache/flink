29955cdf784c5b1b3de568f56dd0eb395fb2d2e1
Gary Yao <gary@data-artisans.com>
2018-11-26 22:07:49 +0100

    [FLINK-10986][tests] Add example on how to run a Jepsen test with Kafka

--- flink-jepsen/docker/run-tests.sh
+++ flink-jepsen/docker/run-tests.sh
@@ -36,18 +36,39 @@ common_jepsen_args+=(--ha-storage-dir hdfs:///flink
 for i in $(seq 1 ${1})
 do
   echo "Executing run #${i} of ${1}"
+
+  # YARN session cluster
   lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-task-managers --test-spec "${dockerdir}/test-specs/yarn-session.edn"
   lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-job-managers --test-spec "${dockerdir}/test-specs/yarn-session.edn"
   lein run test "${common_jepsen_args[@]}" --nemesis-gen fail-name-node-during-recovery --test-spec "${dockerdir}/test-specs/yarn-session.edn"
 
+  # YARN per-job cluster
   lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-task-managers --test-spec "${dockerdir}/test-specs/yarn-job.edn"
   lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-job-managers --test-spec "${dockerdir}/test-specs/yarn-job.edn"
   lein run test "${common_jepsen_args[@]}" --nemesis-gen fail-name-node-during-recovery --test-spec "${dockerdir}/test-specs/yarn-job.edn"
 
+  # Mesos
   lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-task-managers --test-spec "${dockerdir}/test-specs/mesos-session.edn"
   lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-job-managers --test-spec "${dockerdir}/test-specs/mesos-session.edn"
 
-  lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-job-managers --test-spec "${dockerdir}/standalone-session.edn"
-  lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-job-managers --client-gen cancel-jobs --test-spec "${dockerdir}/standalone-session.edn"
-  echo
+  # Standalone
+  lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-job-managers --test-spec "${dockerdir}/test-specs/standalone-session.edn"
+  lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-job-managers --client-gen cancel-jobs --test-spec "${dockerdir}/test-specs/standalone-session.edn"
+
+  # Below is a test that uses Flink's exactly-once Kafka producer/consumer.
+  # The test submits two jobs:
+  #
+  #   (1) DataGeneratorJob - Publishes data to a Kafka topic
+  #   (2) StateMachineJob  - Consumes data from the same Kafka topic, and validates exactly-once semantics
+  #
+  # To enable the test, you first need to build the flink-state-machine-kafka job jar,
+  # and copy the artifact to flink-jepsen/bin:
+  #
+  #   git clone https://github.com/igalshilman/flink-state-machine-example
+  #   cd flink-state-machine-example
+  #   mvn clean package -pl flink-state-machine-kafka/flink-state-machine-kafka -am
+  #   cp flink-state-machine-kafka/flink-state-machine-kafka/target/flink-state-machine-kafka-1.0-SNAPSHOT.jar /path/to/flink-jepsen/bin
+  #
+  # lein run test "${common_jepsen_args[@]}" --nemesis-gen kill-task-managers-bursts --time-limit 60 --test-spec "${dockerdir}/test-specs/standalone-session-kafka.edn" --job-running-healthy-threshold 15
+
 done
