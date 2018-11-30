fe967acbba2143d7f9facd2d6ac884649a87782a
Andrey Zagrebin <azagrebin@gmail.com>
2018-11-30 14:06:49 +0100

    [FLINK-10946] Silent checkpoint async failures in task executor if job is not runnning

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/operators/AbstractStreamOperator.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/operators/AbstractStreamOperator.java
@@ -415,7 +415,9 @@ public abstract class AbstractStreamOperator<OUT>
 			String snapshotFailMessage = "Could not complete snapshot " + checkpointId + " for operator " +
 				getOperatorName() + ".";
 
-			LOG.info(snapshotFailMessage, snapshotException);
+			if (!getContainingTask().isCanceled()) {
+				LOG.info(snapshotFailMessage, snapshotException);
+			}
 			throw new Exception(snapshotFailMessage, snapshotException);
 		}
 
