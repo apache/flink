a3acb4ec0df547b7f0d4f20b3aa0eb860ec700b5
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2018-12-08 18:28:13 +0800

    [hotfix] [state backends, tests] Make StateBackendMigrationTestBase more understandable
    The StateBackendMigrationTestBase previously mocked user behaviour of
    upgrading serializers by using a single serializer class, that can be
    configured with different target compatibility results when they are
    checked for compatibility.
    
    This is a bit hard to understand, also doesn't really reflect how a user
    would actually approach the feature. Instead, instead of configuring a
    single serializer class with different compatibility "personalities",
    this commit uses actual different classes, V1TestTypeSerializer,
    V2TestTypeSerializer, and IncompatibleTestTypeSerializer, to simulate
    the compatibility cases in tests.
    
    This commit also refactors the serializer migration related test
    serializers / types / snapshots to the testutil package, so that it can
    be shared by other state migration related tests in the future.

---
+++ flink-runtime/src/test/java/org/apache/flink/runtime/testutils/statemigration/V1TestTypeSerializerSnapshot.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.testutils.statemigration;
+
+import org.apache.flink.api.common.typeutils.TypeSerializer;
+import org.apache.flink.api.common.typeutils.TypeSerializerSchemaCompatibility;
+import org.apache.flink.api.common.typeutils.TypeSerializerSnapshot;
+import org.apache.flink.core.memory.DataInputView;
+import org.apache.flink.core.memory.DataOutputView;
+
+import java.io.IOException;
+
+/**
+ * Snapshot class for {@link TestType.V1TestTypeSerializer}.
+ */
+public class V1TestTypeSerializerSnapshot implements TypeSerializerSnapshot<TestType> {
+
+	@Override
+	public int getCurrentVersion() {
+		return 1;
+	}
+
+	@Override
+	public TypeSerializerSchemaCompatibility<TestType> resolveSchemaCompatibility(TypeSerializer<TestType> newSerializer) {
+		if (newSerializer instanceof TestType.V1TestTypeSerializer) {
+			return TypeSerializerSchemaCompatibility.compatibleAsIs();
+		} else if (newSerializer instanceof TestType.V2TestTypeSerializer) {
+			return TypeSerializerSchemaCompatibility.compatibleAfterMigration();
+		} else {
+			return TypeSerializerSchemaCompatibility.incompatible();
+		}
+	}
+
+	@Override
+	public TypeSerializer<TestType> restoreSerializer() {
+		return new TestType.V1TestTypeSerializer();
+	}
+
+	@Override
+    public void writeSnapshot(DataOutputView out) throws IOException {
+	}
+
+    @Override
+    public void readSnapshot(int readVersion, DataInputView in, ClassLoader userCodeClassLoader) throws IOException {
+	}
+}
