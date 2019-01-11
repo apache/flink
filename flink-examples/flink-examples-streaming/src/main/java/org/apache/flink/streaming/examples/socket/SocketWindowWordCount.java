fece3f2ae3c19e682098324594d383eb2f51be83
eaglewatcherwb <wbeaglewatcher@gmail.com>
2019-01-11 15:12:44 +0800

    [FLINK-11071][test] revert default FlatMapFunction in SocketWindowWordCount
    Change-Id: Iebd8dca836a9ac56717c2ca7b6d85fa7b2335e78

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/socket/SocketWindowWordCount.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/socket/SocketWindowWordCount.java
@@ -26,11 +26,6 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
 
-import java.io.Serializable;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Proxy;
-import java.util.function.Supplier;
-
 /**
  * Implements a streaming windowed version of the "WordCount" program.
  *
@@ -50,12 +45,10 @@ public class SocketWindowWordCount {
 		// the host and the port to connect to
 		final String hostname;
 		final int port;
-		final boolean userClass;
 		try {
 			final ParameterTool params = ParameterTool.fromArgs(args);
 			hostname = params.has("hostname") ? params.get("hostname") : "localhost";
 			port = params.getInt("port");
-			userClass = params.getBoolean("userclass", false);
 		} catch (Exception e) {
 			System.err.println("No port specified. Please run 'SocketWindowWordCount " +
 				"--hostname <hostname> --port <port>', where hostname (localhost by default) " +
@@ -71,57 +64,32 @@ public class SocketWindowWordCount {
 		// get input data by connecting to the socket
 		DataStream<String> text = env.socketTextStream(hostname, port, "\n");
 
-		FlatMapFunction<String, WordWithCount> flatMapFunction = userClass ?
-			serializableProxy(
-				(Supplier<UserDefinedFlatMapFunction<String, WordWithCount>> & Serializable) () ->
-					new UserDefinedFlatMapFunction<String, WordWithCount>() {
-						@Override
-						public void flatMap(String value, Collector<WordWithCount> out) {
-							for (String word : value.split("\\s")) {
-								out.collect(new WordWithCount(word, 1L));
-							}
-						}
-					}, UserDefinedFlatMapFunction.class) :
-			new FlatMapFunction<String, WordWithCount>() {
-				@Override
-				public void flatMap(String value, Collector<WordWithCount> out) {
-					for (String word : value.split("\\s")) {
-						out.collect(new WordWithCount(word, 1L));
-					}
-				}
-		};
-
 		// parse the data, group it, window it, and aggregate the counts
-		DataStream<WordWithCount> windowCounts = text.flatMap(flatMapFunction)
-			.returns(WordWithCount.class)
-			.keyBy("word")
-			.timeWindow(Time.seconds(5))
+		DataStream<WordWithCount> windowCounts = text
 
-			.reduce(new ReduceFunction<WordWithCount>() {
-				@Override
-				public WordWithCount reduce(WordWithCount a, WordWithCount b) {
-					return new WordWithCount(a.word, a.count + b.count);
-				}
-			});
+				.flatMap(new FlatMapFunction<String, WordWithCount>() {
+					@Override
+					public void flatMap(String value, Collector<WordWithCount> out) {
+						for (String word : value.split("\\s")) {
+							out.collect(new WordWithCount(word, 1L));
+						}
+					}
+				})
+
+				.keyBy("word")
+				.timeWindow(Time.seconds(5))
+
+				.reduce(new ReduceFunction<WordWithCount>() {
+					@Override
+					public WordWithCount reduce(WordWithCount a, WordWithCount b) {
+						return new WordWithCount(a.word, a.count + b.count);
+					}
+				});
 
 		// print the results with a single thread, rather than in parallel
 		windowCounts.print().setParallelism(1);
 
 		env.execute("Socket Window WordCount");
-	}
-
-	interface UserDefinedFlatMapFunction<T, O> extends FlatMapFunction<T, O> { }
-
-	@SuppressWarnings("unchecked")
-	private static <T> T serializableProxy(final Supplier<? extends T> realObjectSupplier, final Class<?> clazz) {
-		return (T) Proxy.newProxyInstance(
-			clazz.getClassLoader(), new Class<?>[] {clazz, Serializable.class}, delegateTo(realObjectSupplier));
-	}
-
-	private static <T> InvocationHandler delegateTo(final Supplier<T> realObjectSupplier) {
-		return (InvocationHandler & Serializable) (proxy, method, args) -> {
-			return method.invoke(realObjectSupplier.get(), args);
-		};
 	}
 
 	// ------------------------------------------------------------------------
