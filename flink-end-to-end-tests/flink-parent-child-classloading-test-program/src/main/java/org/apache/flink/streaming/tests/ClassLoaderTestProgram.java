de04658819d1a38d561a19802c8972a8c241e1c2
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2019-01-15 10:12:55 +0100

    [FLINK-11036] Fix test_streaming_classloader.sh end-to-end test
    Before, this was relying on .version.properties being available in the
    flink-dist jar. This was not the case when building from a Flink source
    release.
    
    This is fixed by introducing a fake lib package that we place in the
    lib/ folder to test the classloader resolution order. Both the lib
    package and the user package contain a parent-child-test.properties and
    we check the resolution order in the test.
    
    Also change the test to not use TaskManager anymore.
    
    This test was relying on TaskManager being present in Flink, this might
    not be the case in the future. We change this test to place a fake class
    in the "lib" package and the "user" package to make it independent of
    the rest of Flink.

--- flink-end-to-end-tests/flink-parent-child-classloading-test-program/src/main/java/org/apache/flink/streaming/tests/ClassLoaderTestProgram.java
+++ flink-end-to-end-tests/flink-parent-child-classloading-test-program/src/main/java/org/apache/flink/streaming/tests/ClassLoaderTestProgram.java
@@ -20,7 +20,6 @@ package org.apache.flink.streaming.tests;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.core.fs.FileSystem;
-import org.apache.flink.runtime.taskmanager.TaskManager;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import java.io.InputStream;
@@ -29,12 +28,11 @@ import java.util.Enumeration;
 import java.util.Properties;
 
 /**
- * End-to-end test program for verifying that the {@code classloader.resolve-order} setting
- * is being honored by Flink. We test this by creating a fake {@code TaskManager} with a single
- * method that we call in the same package as the original Flink {@code TaskManager} and verify that
- * we get a {@link NoSuchMethodError} if we're running with {@code parent-first} class loading
- * and that we get the correct result from the method when we're running with {@code child-first}
- * class loading.
+ * End-to-end test program for verifying that the {@code classloader.resolve-order} setting is being
+ * honored by Flink. We test this by creating a {@code ParentChildTestingVehicle} with a single
+ * method that we call in the same package as the {@code ParentChildTestingVehicle} in the "lib"
+ * package (flink-parent-child-classloading-test-lib-package) and verify the message in the
+ * end-to-end test script.
  */
 public class ClassLoaderTestProgram {
 
@@ -42,58 +40,39 @@ public class ClassLoaderTestProgram {
 
 		final ParameterTool params = ParameterTool.fromArgs(args);
 
-		final String resolveOrder = params.getRequired("resolve-order");
-
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
 		env
 			.fromElements("Hello")
 			.map((MapFunction<String, String>) value -> {
 
-				String gitCommitId;
+				String messageFromPropsFile;
 
-				try (InputStream propFile = ClassLoaderTestProgram.class.getClassLoader().getResourceAsStream(".version.properties")) {
+				try (InputStream propFile = ClassLoaderTestProgram.class.getClassLoader().getResourceAsStream("parent-child-test.properties")) {
 					Properties properties = new Properties();
 					properties.load(propFile);
-					gitCommitId = properties.getProperty("git.commit.id.abbrev");
+					messageFromPropsFile = properties.getProperty("message");
 				}
 
+				// Enumerate all properties files we can find and store the messages in the
+				// order we find them. The order will be different between parent-first and
+				// child-first classloader mode.
 				Enumeration<URL> resources = ClassLoaderTestProgram.class.getClassLoader().getResources(
-					".version.properties");
+					"parent-child-test.properties");
 
-				StringBuilder sortedProperties = new StringBuilder();
+				StringBuilder orderedProperties = new StringBuilder();
 				while (resources.hasMoreElements()) {
 					URL url = resources.nextElement();
 					try (InputStream in = url.openStream()) {
 						Properties properties = new Properties();
 						properties.load(in);
-						String orderedGitCommitId = properties.getProperty("git.commit.id.abbrev");
-						sortedProperties.append(orderedGitCommitId);
+						String messageFromEnumeratedPropsFile = properties.getProperty("message");
+						orderedProperties.append(messageFromEnumeratedPropsFile);
 					}
 				}
 
-				if (resolveOrder.equals("parent-first")) {
-					try {
-						@SuppressWarnings("unused")
-						String ignored = TaskManager.getMessage();
-
-						throw new RuntimeException(
-							"TaskManager.getMessage() should not be available with parent-first " +
-								"ClassLoader order.");
-
-					} catch (NoSuchMethodError e) {
-						// expected
-					}
-					return "NoSuchMethodError:" + gitCommitId + ":" + sortedProperties;
-				} else if (resolveOrder.equals("child-first")) {
-					String message = TaskManager.getMessage();
-					if (!message.equals("Hello, World!")) {
-						throw new RuntimeException("Wrong message from fake TaskManager.");
-					}
-					return message + ":" + gitCommitId + ":" + sortedProperties;
-				} else {
-					throw new RuntimeException("Unknown resolve order: " + resolveOrder);
-				}
+				String message = ParentChildTestingVehicle.getMessage();
+				return message + ":" + messageFromPropsFile + ":" + orderedProperties;
 			})
 			.writeAsText(params.getRequired("output"), FileSystem.WriteMode.OVERWRITE);
 
