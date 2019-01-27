fa84869864b6e5faa4abec1c359f55c4fe4b4ab9
Dawid Wysakowicz <dwysakowicz@apache.org>
2019-01-27 19:13:41 +0100

    [FLINK-11071][core] Improved proxy class serialization test
    This closes #7436

--- flink-core/src/test/java/org/apache/flink/util/InstantiationUtilTest.java
+++ flink-core/src/test/java/org/apache/flink/util/InstantiationUtilTest.java
@@ -32,12 +32,11 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
 import java.net.URLClassLoader;
 import java.util.Objects;
 import java.util.Random;
@@ -56,27 +55,45 @@ public class InstantiationUtilTest extends TestLogger {
 	@ClassRule
 	public static TemporaryFolder temporaryFolder = new TemporaryFolder();
 
+	private static final String PROXY_DEFINITION_FORMAT =
+			"import java.lang.reflect.InvocationHandler;" +
+			"import java.lang.reflect.Method;" +
+			"import java.io.Serializable;" +
+			"public class %s implements InvocationHandler, Serializable {\n" +
+			"\n" +
+			"  @Override\n" +
+			"  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {\n" +
+			"    return null;\n" +
+			"  }\n" +
+			"}";
+
 	@Test
 	public void testResolveProxyClass() throws Exception {
+		final String interfaceName = "UserDefinedInterface";
+		final String proxyName = "UserProxy";
 
-		UserDefineFunctionImpl method = new UserDefineFunctionImpl();
-		final String className = "UserDefineFunctionImpl";
-		final URLClassLoader userClassLoader = ClassLoaderUtils.compileAndLoadJava(
-			temporaryFolder.newFolder(),
-			className + ".java",
-			"import java.io.Serializable;\n"
-				+ "interface UserDefineFunction { void test();}\n"
-				+ "public class " + className + " implements UserDefineFunction, Serializable {public void test() {} }");
+		try (URLClassLoader userClassLoader = createClassLoader(interfaceName, proxyName)) {
+			Class<?> userInterface = Class.forName(interfaceName, false, userClassLoader);
+			InvocationHandler userProxy = (InvocationHandler) Class.forName(proxyName, false, userClassLoader)
+				.newInstance();
 
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			Object proxy = Proxy.newProxyInstance(userClassLoader, new Class[]{userInterface}, userProxy);
 
-		InstantiationUtil.serializeObject(baos, method);
+			byte[] serializeObject = InstantiationUtil.serializeObject(proxy);
+			Object deserializedProxy = InstantiationUtil.deserializeObject(serializeObject, userClassLoader);
+			assertNotNull(deserializedProxy);
+		}
+	}
 
-		InstantiationUtil.ClassLoaderObjectInputStream cloi = new InstantiationUtil.ClassLoaderObjectInputStream(
-			new ByteArrayInputStream(baos.toByteArray()), userClassLoader);
-		cloi.resolveProxyClass(new String[]{"UserDefineFunction"});
+	private URLClassLoader createClassLoader(String interfaceName, String proxyName) throws IOException {
+		return ClassLoaderUtils.withRoot(temporaryFolder.newFolder())
+			.addClass(interfaceName, String.format("interface %s { void test();}", interfaceName))
+			.addClass(proxyName, createProxyDefinition(proxyName))
+			.build();
+	}
 
-		userClassLoader.close();
+	private String createProxyDefinition(String proxyName) {
+		return String.format(PROXY_DEFINITION_FORMAT, proxyName);
 	}
 
 	@Test
@@ -185,17 +202,6 @@ public class InstantiationUtilTest extends TestLogger {
 	}
 
 	// --------------------------------------------------------------------------------------------
-
-	interface UserDefineFunction extends Serializable {
-		void test();
-	}
-
-	private static class UserDefineFunctionImpl implements UserDefineFunction {
-		@Override
-		public void test() {
-
-		}
-	}
 
 	private class TestClass {}
 
