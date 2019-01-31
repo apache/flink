57a2b754f6a5d8844aa35afb511901ad7ee43068
zentol <chesnay@apache.org>
2019-01-31 14:50:15 +0100

    [FLINK-11495][runtime] Remove legacy job archiving paths

--- flink-runtime/src/main/java/org/apache/flink/runtime/history/FsJobArchivist.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/history/FsJobArchivist.java
@@ -24,9 +24,7 @@ import org.apache.flink.core.fs.FSDataInputStream;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.runtime.executiongraph.AccessExecutionGraph;
-import org.apache.flink.runtime.webmonitor.WebMonitorUtils;
 import org.apache.flink.runtime.webmonitor.history.ArchivedJson;
-import org.apache.flink.runtime.webmonitor.history.JsonArchivist;
 import org.apache.flink.util.IOUtils;
 
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.core.JsonEncoding;
@@ -58,48 +56,6 @@ public class FsJobArchivist {
 	private static final String JSON = "json";
 
 	private FsJobArchivist() {
-	}
-
-	/**
-	 * Writes the given {@link AccessExecutionGraph} to the {@link FileSystem} pointed to by
-	 * {@link JobManagerOptions#ARCHIVE_DIR}.
-	 *
-	 * @param rootPath directory to which the archive should be written to
-	 * @param graph  graph to archive
-	 * @return path to where the archive was written, or null if no archive was created
-	 * @throws IOException
-	 * @deprecated only kept for legacy reasons
-	 */
-	@Deprecated
-	public static Path archiveJob(Path rootPath, AccessExecutionGraph graph) throws IOException {
-		try {
-			FileSystem fs = rootPath.getFileSystem();
-			Path path = new Path(rootPath, graph.getJobID().toString());
-			OutputStream out = fs.create(path, FileSystem.WriteMode.NO_OVERWRITE);
-
-			try (JsonGenerator gen = jacksonFactory.createGenerator(out, JsonEncoding.UTF8)) {
-				gen.writeStartObject();
-				gen.writeArrayFieldStart(ARCHIVE);
-				for (JsonArchivist archiver : WebMonitorUtils.getJsonArchivists()) {
-					for (ArchivedJson archive : archiver.archiveJsonWithPath(graph)) {
-						gen.writeStartObject();
-						gen.writeStringField(PATH, archive.getPath());
-						gen.writeStringField(JSON, archive.getJson());
-						gen.writeEndObject();
-					}
-				}
-				gen.writeEndArray();
-				gen.writeEndObject();
-			} catch (Exception e) {
-				fs.delete(path, false);
-				throw e;
-			}
-			LOG.info("Job {} has been archived at {}.", graph.getJobID(), path);
-			return path;
-		} catch (IOException e) {
-			LOG.error("Failed to archive job.", e);
-			throw e;
-		}
 	}
 
 	/**
