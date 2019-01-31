91d2d384543ea13c7b37a651c0df4647475f1aa0
Till Rohrmann <trohrmann@apache.org>
2019-01-31 16:41:09 +0100

    [hotfix] Move YarnTestBase#waitUntilCondition into runtime.CommonTestUtils

--- flink-yarn-tests/src/test/java/org/apache/flink/yarn/YarnTestBase.java
+++ flink-yarn-tests/src/test/java/org/apache/flink/yarn/YarnTestBase.java
@@ -26,7 +26,6 @@ import org.apache.flink.runtime.clusterframework.BootstrapTools;
 import org.apache.flink.test.util.TestBaseUtils;
 import org.apache.flink.util.Preconditions;
 import org.apache.flink.util.TestLogger;
-import org.apache.flink.util.function.SupplierWithException;
 import org.apache.flink.yarn.cli.FlinkYarnSessionCli;
 
 import org.apache.commons.io.FileUtils;
@@ -83,7 +82,6 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.TimeoutException;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -106,8 +104,6 @@ public abstract class YarnTestBase extends TestLogger {
 	protected static final String TEST_CLUSTER_NAME_KEY = "flink-yarn-minicluster-name";
 
 	protected static final int NUM_NODEMANAGERS = 2;
-
-	private static final long RETRY_TIMEOUT = 100L;
 
 	/** The tests are scanning for these strings in the final output. */
 	protected static final String[] PROHIBITED_STRINGS = {
@@ -279,16 +275,6 @@ public abstract class YarnTestBase extends TestLogger {
 			}
 		}
 		return null;
-	}
-
-	protected static void waitUntilCondition(SupplierWithException<Boolean, Exception> condition, Deadline timeout) throws Exception {
-		while (timeout.hasTimeLeft() && !condition.get()) {
-			Thread.sleep(Math.min(RETRY_TIMEOUT, timeout.timeLeft().toMillis()));
-		}
-
-		if (!timeout.hasTimeLeft()) {
-			throw new TimeoutException("Condition was not met in given timeout.");
-		}
 	}
 
 	@Nonnull
