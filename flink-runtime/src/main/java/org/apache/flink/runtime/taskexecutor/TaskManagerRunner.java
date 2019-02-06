ab39089f00f403d53c2aa32f8b7a87667884d1bb
Gary Yao <gary@apache.org>
2019-02-06 10:34:31 +0100

    [FLINK-11367][tests] Port TaskManagerProcessReapingTestBase to new code base
    This closes #7653.

--- flink-runtime/src/main/java/org/apache/flink/runtime/taskexecutor/TaskManagerRunner.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/taskexecutor/TaskManagerRunner.java
@@ -87,7 +87,7 @@ public class TaskManagerRunner implements FatalErrorHandler, AutoCloseableAsync 
 
 	private static final int STARTUP_FAILURE_RETURN_CODE = 1;
 
-	private static final int RUNTIME_FAILURE_RETURN_CODE = 2;
+	public static final int RUNTIME_FAILURE_RETURN_CODE = 2;
 
 	private final Object lock = new Object();
 
@@ -263,7 +263,7 @@ public class TaskManagerRunner implements FatalErrorHandler, AutoCloseableAsync 
 		}
 	}
 
-	protected void terminateJVM() {
+	private void terminateJVM() {
 		System.exit(RUNTIME_FAILURE_RETURN_CODE);
 	}
 
