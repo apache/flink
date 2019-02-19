d5d45c8cc5924a6f387009eb6d0d053106055c3b
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2019-02-19 14:08:03 +0800

    [FLINK-11485] [core] Refactor multiple nested serializer compatibility resolution logic to CompositeTypeSerializerUtil
    This commit refactors the logic of resolving overall compatibility
    results across multiple nested serializers of a composite serializer out
    of the CompositeTypeSerializerSnapshot class.
    
    This allows us to reuse this functionality when implementing the
    compatibility check for the PojoSerializerSnapshot

--- flink-core/src/main/java/org/apache/flink/api/common/typeutils/CompositeTypeSerializerUtil.java
+++ flink-core/src/main/java/org/apache/flink/api/common/typeutils/CompositeTypeSerializerUtil.java
@@ -19,8 +19,11 @@
 package org.apache.flink.api.common.typeutils;
 
 import org.apache.flink.annotation.Internal;
+import org.apache.flink.util.Preconditions;
 
 import static org.apache.flink.util.Preconditions.checkArgument;
+import static org.apache.flink.util.Preconditions.checkNotNull;
+import static org.apache.flink.util.Preconditions.checkState;
 
 /**
  * Utilities for the {@link CompositeTypeSerializerSnapshot}.
@@ -61,5 +64,148 @@ public class CompositeTypeSerializerUtil {
 
 		NestedSerializersSnapshotDelegate delegate = new NestedSerializersSnapshotDelegate(nestedSnapshots);
 		compositeSnapshot.setNestedSerializersSnapshotDelegate(delegate);
+	}
+
+	/**
+	 * Constructs an {@link IntermediateCompatibilityResult} with the given array of nested serializers and their
+	 * corresponding serializer snapshots.
+	 *
+	 * <p>This result is considered "intermediate", because the actual final result is not yet built if it isn't
+	 * defined. This is the case if the final result is supposed to be
+	 * {@link TypeSerializerSchemaCompatibility#compatibleWithReconfiguredSerializer(TypeSerializer)}, where
+	 * construction of the reconfigured serializer instance should be done by the caller.
+	 *
+	 * <p>For other cases, i.e. {@link TypeSerializerSchemaCompatibility#compatibleAsIs()},
+	 * {@link TypeSerializerSchemaCompatibility#compatibleAfterMigration()}, and
+	 * {@link TypeSerializerSchemaCompatibility#incompatible()}, these results are considered final.
+	 *
+	 * @param newNestedSerializers the new nested serializers to check for compatibility.
+	 * @param nestedSerializerSnapshots the associated nested serializers' snapshots.
+	 *
+	 * @return the intermediate compatibility result of the new nested serializers.
+	 */
+	public static <T> IntermediateCompatibilityResult<T> constructIntermediateCompatibilityResult(
+		TypeSerializer<?>[] newNestedSerializers,
+		TypeSerializerSnapshot<?>[] nestedSerializerSnapshots) {
+
+		Preconditions.checkArgument(newNestedSerializers.length == nestedSerializerSnapshots.length,
+			"Different number of new serializers and existing serializer snapshots.");
+
+		TypeSerializer<?>[] nestedSerializers = new TypeSerializer[newNestedSerializers.length];
+
+		// check nested serializers for compatibility
+		boolean nestedSerializerRequiresMigration = false;
+		boolean hasReconfiguredNestedSerializers = false;
+		for (int i = 0; i < nestedSerializerSnapshots.length; i++) {
+			TypeSerializerSchemaCompatibility<?> compatibility =
+				resolveCompatibility(newNestedSerializers[i], nestedSerializerSnapshots[i]);
+
+			// if any one of the new nested serializers is incompatible, we can just short circuit the result
+			if (compatibility.isIncompatible()) {
+				return IntermediateCompatibilityResult.definedIncompatibleResult();
+			}
+
+			if (compatibility.isCompatibleAfterMigration()) {
+				nestedSerializerRequiresMigration = true;
+			} else if (compatibility.isCompatibleWithReconfiguredSerializer()) {
+				hasReconfiguredNestedSerializers = true;
+				nestedSerializers[i] = compatibility.getReconfiguredSerializer();
+			} else if (compatibility.isCompatibleAsIs()) {
+				nestedSerializers[i] = newNestedSerializers[i];
+			} else {
+				throw new IllegalStateException("Undefined compatibility type.");
+			}
+		}
+
+		if (nestedSerializerRequiresMigration) {
+			return IntermediateCompatibilityResult.definedCompatibleAfterMigrationResult();
+		}
+
+		if (hasReconfiguredNestedSerializers) {
+			return IntermediateCompatibilityResult.undefinedReconfigureResult(nestedSerializers);
+		}
+
+		// ends up here if everything is compatible as is
+		return IntermediateCompatibilityResult.definedCompatibleAsIsResult(nestedSerializers);
+	}
+
+	public static class IntermediateCompatibilityResult<T> {
+
+		private final TypeSerializerSchemaCompatibility.Type compatibilityType;
+		private final TypeSerializer<?>[] nestedSerializers;
+
+		static <T> IntermediateCompatibilityResult<T> definedCompatibleAsIsResult(TypeSerializer<?>[] originalSerializers) {
+			return new IntermediateCompatibilityResult<>(TypeSerializerSchemaCompatibility.Type.COMPATIBLE_AS_IS, originalSerializers);
+		}
+
+		static <T> IntermediateCompatibilityResult<T> definedIncompatibleResult() {
+			return new IntermediateCompatibilityResult<>(TypeSerializerSchemaCompatibility.Type.INCOMPATIBLE, null);
+		}
+
+		static <T> IntermediateCompatibilityResult<T> definedCompatibleAfterMigrationResult() {
+			return new IntermediateCompatibilityResult<>(TypeSerializerSchemaCompatibility.Type.COMPATIBLE_AFTER_MIGRATION, null);
+		}
+
+		static <T> IntermediateCompatibilityResult<T> undefinedReconfigureResult(TypeSerializer<?>[] reconfiguredNestedSerializers) {
+			return new IntermediateCompatibilityResult<>(TypeSerializerSchemaCompatibility.Type.COMPATIBLE_WITH_RECONFIGURED_SERIALIZER, reconfiguredNestedSerializers);
+		}
+
+		private IntermediateCompatibilityResult(
+				TypeSerializerSchemaCompatibility.Type compatibilityType,
+				TypeSerializer<?>[] nestedSerializers) {
+			this.compatibilityType = checkNotNull(compatibilityType);
+			this.nestedSerializers = nestedSerializers;
+		}
+
+		public boolean isCompatibleWithReconfiguredSerializer() {
+			return compatibilityType == TypeSerializerSchemaCompatibility.Type.COMPATIBLE_WITH_RECONFIGURED_SERIALIZER;
+		}
+
+		public boolean isCompatibleAsIs() {
+			return compatibilityType == TypeSerializerSchemaCompatibility.Type.COMPATIBLE_AS_IS;
+		}
+
+		public boolean isCompatibleAfterMigration() {
+			return compatibilityType == TypeSerializerSchemaCompatibility.Type.COMPATIBLE_AFTER_MIGRATION;
+		}
+
+		public boolean isIncompatible() {
+			return compatibilityType == TypeSerializerSchemaCompatibility.Type.INCOMPATIBLE;
+		}
+
+		public TypeSerializerSchemaCompatibility<T> getFinalResult() {
+			checkState(
+				compatibilityType != TypeSerializerSchemaCompatibility.Type.COMPATIBLE_WITH_RECONFIGURED_SERIALIZER,
+				"unable to build final result if intermediate compatibility type is COMPATIBLE_WITH_RECONFIGURED_SERIALIZER.");
+			switch (compatibilityType) {
+				case COMPATIBLE_AS_IS:
+					return TypeSerializerSchemaCompatibility.compatibleAsIs();
+				case COMPATIBLE_AFTER_MIGRATION:
+					return TypeSerializerSchemaCompatibility.compatibleAfterMigration();
+				case INCOMPATIBLE:
+					return TypeSerializerSchemaCompatibility.incompatible();
+				default:
+					throw new IllegalStateException("unrecognized compatibility type.");
+			}
+		}
+
+		public TypeSerializer<?>[] getNestedSerializers() {
+			checkState(
+				compatibilityType == TypeSerializerSchemaCompatibility.Type.COMPATIBLE_AS_IS
+					|| compatibilityType == TypeSerializerSchemaCompatibility.Type.COMPATIBLE_WITH_RECONFIGURED_SERIALIZER,
+				"only intermediate compatibility types COMPATIBLE_AS_IS and COMPATIBLE_WITH_RECONFIGURED_SERIALIZER have nested serializers.");
+			return nestedSerializers;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <E> TypeSerializerSchemaCompatibility<E> resolveCompatibility(
+		TypeSerializer<?> serializer,
+		TypeSerializerSnapshot<?> snapshot) {
+
+		TypeSerializer<E> typedSerializer = (TypeSerializer<E>) serializer;
+		TypeSerializerSnapshot<E> typedSnapshot = (TypeSerializerSnapshot<E>) snapshot;
+
+		return typedSnapshot.resolveSchemaCompatibility(typedSerializer);
 	}
 }
