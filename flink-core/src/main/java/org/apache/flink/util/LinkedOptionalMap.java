ceed450b21e6f37bb40ca44ee5f4c388119cf694
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2019-02-20 13:14:45 +0800

    [hotfix] [core] LinkedOptionalMap should collect keys in LinkedHashSet to guarantee ordering

--- flink-core/src/main/java/org/apache/flink/util/LinkedOptionalMap.java
+++ flink-core/src/main/java/org/apache/flink/util/LinkedOptionalMap.java
@@ -130,7 +130,7 @@ public final class LinkedOptionalMap<K, V> {
 			.stream()
 			.filter(LinkedOptionalMap::keyOrValueIsAbsent)
 			.map(Entry::getKey)
-			.collect(Collectors.toSet());
+			.collect(Collectors.toCollection(LinkedHashSet::new));
 	}
 
 	/**
