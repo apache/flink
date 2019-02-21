22a7eccacc6b5847ac96f791378ecf7bb25d03d0
Igal Shilman <igal.shilman@data-artisans.com>
2019-02-21 13:02:04 +0100

    [FLINK-11323] [tests] Add more test cases to TypeSerializerSnapshotMigrationTestBase
    This commit adds two more tests to TypeSerializerSnapshotMigrationTestBase
    that validates that the new serializer are able to read previously stored
    data (perhaps after reconfiguration).

--- flink-core/src/test/java/org/apache/flink/api/common/typeutils/TypeSerializerSnapshotMigrationTestBase.java
+++ flink-core/src/test/java/org/apache/flink/api/common/typeutils/TypeSerializerSnapshotMigrationTestBase.java
@@ -87,13 +87,21 @@ public abstract class TypeSerializerSnapshotMigrationTestBase<ElementT> extends 
 		TypeSerializerSnapshot<ElementT> snapshot = snapshotUnderTest();
 		TypeSerializer<ElementT> serializer = snapshot.restoreSerializer();
 
-		DataInputView input = dataUnderTest();
+		assertSerializerIsAbleToReadOldData(serializer);
+	}
 
-		final Matcher<ElementT> matcher = testSpecification.testDataElementMatcher;
-		for (int i = 0; i < testSpecification.testDataCount; i++) {
-			final ElementT result = serializer.deserialize(input);
-			assertThat(result, matcher);
+	@Test
+	public void reconfiguredSerializerIsAbleToDeserializePreviousData() throws IOException {
+		TypeSerializerSnapshot<ElementT> snapshot = snapshotUnderTest();
+		TypeSerializerSchemaCompatibility<ElementT> compatibility = snapshot.resolveSchemaCompatibility(testSpecification.createSerializer());
+
+		if (!compatibility.isCompatibleWithReconfiguredSerializer()) {
+			// this test only applies for reconfigured serializers.
+			return;
 		}
+
+		TypeSerializer<ElementT> serializer = compatibility.getReconfiguredSerializer();
+		assertSerializerIsAbleToReadOldData(serializer);
 	}
 
 	@SuppressWarnings("deprecation")
@@ -111,6 +119,28 @@ public abstract class TypeSerializerSnapshotMigrationTestBase<ElementT> extends 
 			notNullValue(),
 			not(instanceOf(TypeSerializerConfigSnapshot.class))
 		));
+	}
+
+	@Test
+	public void restoreSerializerFromNewSerializerSnapshotIsAbleToDeserializePreviousData() throws IOException {
+		TypeSerializer<ElementT> newSerializer = testSpecification.createSerializer();
+
+		TypeSerializerSchemaCompatibility<ElementT> compatibility =
+			snapshotUnderTest().resolveSchemaCompatibility(newSerializer);
+
+		final TypeSerializer<ElementT> nextSerializer;
+		if (compatibility.isCompatibleWithReconfiguredSerializer()) {
+			nextSerializer = compatibility.getReconfiguredSerializer();
+		} else if (compatibility.isCompatibleAsIs()) {
+			nextSerializer = newSerializer;
+		} else {
+			// this test does not apply.
+			return;
+		}
+
+		TypeSerializerSnapshot<ElementT> nextSnapshot = nextSerializer.snapshotConfiguration();
+
+		assertSerializerIsAbleToReadOldData(nextSnapshot.restoreSerializer());
 	}
 
 	// --------------------------------------------------------------------------------------------------------------
@@ -161,6 +191,16 @@ public abstract class TypeSerializerSnapshotMigrationTestBase<ElementT> extends 
 		return contentsOf(testSpecification.getTestDataLocation());
 	}
 
+	private void assertSerializerIsAbleToReadOldData(TypeSerializer<ElementT> serializer) throws IOException {
+		DataInputView input = dataUnderTest();
+
+		final Matcher<ElementT> matcher = testSpecification.testDataElementMatcher;
+		for (int i = 0; i < testSpecification.testDataCount; i++) {
+			final ElementT result = serializer.deserialize(input);
+			assertThat(result, matcher);
+		}
+	}
+
 	// --------------------------------------------------------------------------------------------------------------
 	// Static Helpers
 	// --------------------------------------------------------------------------------------------------------------
@@ -197,6 +237,7 @@ public abstract class TypeSerializerSnapshotMigrationTestBase<ElementT> extends 
 	/**
 	 * Test Specification.
 	 */
+	@SuppressWarnings("WeakerAccess")
 	public static final class TestSpecification<T> {
 		private final Class<? extends TypeSerializer<T>> serializerType;
 		private final Class<? extends TypeSerializerSnapshot<T>> snapshotClass;
