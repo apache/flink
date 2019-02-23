4d5c815434079a033d0e03418ac9e9cab3de7107
shengjk1 <a>
2019-02-23 11:15:40 +0800

    [FLINK-11672][examples] Add example for streaming operators's connect

--- flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/connect/ConnectExample.java
+++ flink-examples/flink-examples-streaming/src/main/java/org/apache/flink/streaming/examples/connect/ConnectExample.java
@@ -33,7 +33,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-
 /**
  * Example illustrating connect between two data streams.
  *
@@ -42,77 +41,73 @@ import java.util.Map;
  *
  * <p>infosTable: order details table
  * ordersTable:   order table
- *
  */
+
 public class ConnectExample {
-	
 	public static void main(String[] args) throws Exception {
-		
-		final List<Tuple2<String,String>> infosTable = new ArrayList<>();
-		infosTable.add(new Tuple2("12001","10"));
-		infosTable.add(new Tuple2("12002","25"));
-		infosTable.add(new Tuple2("12003","36"));
-		infosTable.add(new Tuple2("12004","47"));
-		
-		final List<Tuple2<String,String>> ordersTable = new ArrayList<>();
+		//infosTable
+		final List<Tuple2<String, String>> infosTable = new ArrayList<>();
+		infosTable.add(new Tuple2("12001", "10"));
+		infosTable.add(new Tuple2("12002", "25"));
+		infosTable.add(new Tuple2("12003", "36"));
+		infosTable.add(new Tuple2("12004", "47"));
+		//ordersTable
+		final List<Tuple2<String, String>> ordersTable = new ArrayList<>();
 		ordersTable.add(new Tuple2<>("12001", "100-20190220"));
 		ordersTable.add(new Tuple2<>("12002", "200-20190220"));
 		ordersTable.add(new Tuple2<>("12003", "300-20190220"));
 		ordersTable.add(new Tuple2<>("12004", "400-20190220"));
 		ordersTable.add(new Tuple2<>("12005", "500-20190220"));
-		
+		//get env
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-		
+		//execut
 		SingleOutputStreamOperator<Tuple2<String, String>> orders = env.fromCollection(ordersTable);
 		SingleOutputStreamOperator<Tuple2<String, String>> infos = env.fromCollection(infosTable);
-		
 		ConnectedStreams<Tuple2<String, String>, Tuple2<String, String>> connect = infos.keyBy(0).connect(orders.keyBy(0));
 		SingleOutputStreamOperator<Tuple2<String, String>> process = connect.process(new OrderMergeInfos());
 		process.print("==========");
-		
 		env.execute();
 	}
 	
-	
-	public static class OrderMergeInfos extends CoProcessFunction<Tuple2<String, String>, Tuple2<String, String>, Tuple2<String, String>> {
-		
+	/**
+	 * user defind function OrderMergeInfos.
+	 */
+	private static class OrderMergeInfos extends CoProcessFunction<Tuple2<String, String>, Tuple2<String, String>, Tuple2<String, String>> {
+		private static final long serialVersionUID = -1721031958980429667L;
 		private MapState<String, String> infosMap = null;
 		private MapState<String, String> ordersMap = null;
-		
 		@Override
-		public void open(Configuration parameters){
+		public void open(Configuration parameters) {
 			MapStateDescriptor infosDescriptor = new MapStateDescriptor<String, String>(
-					"infosBuffer",
-					BasicTypeInfo.STRING_TYPE_INFO,
-					BasicTypeInfo.STRING_TYPE_INFO
+				"infosBuffer",
+				BasicTypeInfo.STRING_TYPE_INFO,
+				BasicTypeInfo.STRING_TYPE_INFO
 			);
 			infosMap = getRuntimeContext().getMapState(infosDescriptor);
-			
 			MapStateDescriptor ordersDescriptor = new MapStateDescriptor<String, String>(
-					"ordersBuffer",
-					BasicTypeInfo.STRING_TYPE_INFO,
-					BasicTypeInfo.STRING_TYPE_INFO
+				"ordersBuffer",
+				BasicTypeInfo.STRING_TYPE_INFO,
+				BasicTypeInfo.STRING_TYPE_INFO
 			);
 			ordersMap = getRuntimeContext().getMapState(ordersDescriptor);
 		}
-		
 		@Override
 		public void processElement1(Tuple2<String, String> value, Context ctx, Collector<Tuple2<String, String>> out) throws Exception {
-			infosMap.put(value.f0,value.f1);
+			infosMap.put(value.f0, value.f1);
 		}
 		
-		private Tuple2<String, String> outTuple2=new Tuple2<>();
+		private Tuple2<String, String> outTuple2 = new Tuple2<>();
 		@Override
 		public void processElement2(Tuple2<String, String> value, Context ctx, Collector<Tuple2<String, String>> out) throws Exception {
-			ordersMap.put(value.f0,value.f1);
-			for (Map.Entry<String,String> orders:ordersMap.entries()) {
+			ordersMap.put(value.f0, value.f1);
+			for (Map.Entry<String, String> orders : ordersMap.entries()) {
 				String infosCount = infosMap.get(orders.getKey());
-				if(infosCount==null){
-					ordersMap.put(value.f0,value.f1);
-				}else {
+				if (infosCount == null) {
+					ordersMap.put(value.f0, value.f1);
+				} else {
 					ordersMap.remove(orders.getKey());
-					outTuple2.f0=value.f0;
-					outTuple2.f1=value.f1+"-"+infosCount;
+					outTuple2.f0 = value.f0;
+					outTuple2.f1 = value.f1 + "-" + infosCount;
 					out.collect(outTuple2);
 				}
 			}
