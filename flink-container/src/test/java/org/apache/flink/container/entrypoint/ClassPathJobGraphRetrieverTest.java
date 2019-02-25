8b3741badd966d7da4c97b82815538717954a7d4
Ufuk Celebi <uce@apache.org>
2019-02-25 18:19:29 +0100

    [FLINK-11533] [container] Find job jar on classpath
    [pr-review] Address comments

--- flink-container/src/test/java/org/apache/flink/container/entrypoint/ClassPathJobGraphRetrieverTest.java
+++ flink-container/src/test/java/org/apache/flink/container/entrypoint/ClassPathJobGraphRetrieverTest.java
@@ -21,14 +21,26 @@ package org.apache.flink.container.entrypoint;
 import org.apache.flink.api.common.JobID;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.CoreOptions;
+import org.apache.flink.container.entrypoint.ClassPathJobGraphRetriever.JarsOnClassPath;
 import org.apache.flink.runtime.jobgraph.JobGraph;
 import org.apache.flink.runtime.jobgraph.SavepointRestoreSettings;
 import org.apache.flink.util.FlinkException;
 import org.apache.flink.util.TestLogger;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
@@ -37,6 +49,9 @@ import static org.junit.Assert.assertThat;
  * Tests for the {@link ClassPathJobGraphRetriever}.
  */
 public class ClassPathJobGraphRetrieverTest extends TestLogger {
+
+	@Rule
+	public final TemporaryFolder temporaryFolder = new TemporaryFolder();
 
 	private static final String[] PROGRAM_ARGUMENTS = {"--arg", "suffix"};
 
@@ -61,6 +76,40 @@ public class ClassPathJobGraphRetrieverTest extends TestLogger {
 	}
 
 	@Test
+	public void testJobGraphRetrievalFromJar() throws FlinkException, FileNotFoundException {
+		final File testJar = TestJob.getTestJobJar();
+		final ClassPathJobGraphRetriever classPathJobGraphRetriever = new ClassPathJobGraphRetriever(
+			new JobID(),
+			SavepointRestoreSettings.none(),
+			PROGRAM_ARGUMENTS,
+			// No class name specified, but the test JAR "is" on the class path
+			null,
+			() -> Collections.singleton(testJar));
+
+		final JobGraph jobGraph = classPathJobGraphRetriever.retrieveJobGraph(new Configuration());
+
+		assertThat(jobGraph.getName(), is(equalTo(TestJob.class.getCanonicalName() + "-suffix")));
+	}
+
+	@Test
+	public void testJobGraphRetrievalJobClassNameHasPrecedenceOverClassPath() throws FlinkException, FileNotFoundException {
+		final File testJar = new File("non-existing");
+
+		final ClassPathJobGraphRetriever classPathJobGraphRetriever = new ClassPathJobGraphRetriever(
+			new JobID(),
+			SavepointRestoreSettings.none(),
+			PROGRAM_ARGUMENTS,
+			// Both a class name is specified and a JAR "is" on the class path
+			// The class name should have precedence.
+			TestJob.class.getCanonicalName(),
+			() -> Collections.singleton(testJar));
+
+		final JobGraph jobGraph = classPathJobGraphRetriever.retrieveJobGraph(new Configuration());
+
+		assertThat(jobGraph.getName(), is(equalTo(TestJob.class.getCanonicalName() + "-suffix")));
+	}
+
+	@Test
 	public void testSavepointRestoreSettings() throws FlinkException {
 		final Configuration configuration = new Configuration();
 		final SavepointRestoreSettings savepointRestoreSettings = SavepointRestoreSettings.forPath("foobar", true);
@@ -77,4 +126,54 @@ public class ClassPathJobGraphRetrieverTest extends TestLogger {
 		assertThat(jobGraph.getSavepointRestoreSettings(), is(equalTo(savepointRestoreSettings)));
 		assertEquals(jobGraph.getJobID(), jobId);
 	}
+
+	@Test
+	public void testJarFromClassPathSupplierSanityCheck() {
+		Iterable<File> jarFiles = JarsOnClassPath.INSTANCE.get();
+
+		// Junit executes this test, so it should be returned as part of JARs on the class path
+		assertThat(jarFiles, hasItem(hasProperty("name", containsString("junit"))));
+	}
+
+	@Test
+	public void testJarFromClassPathSupplier() throws IOException {
+		final File file1 = temporaryFolder.newFile();
+		final File file2 = temporaryFolder.newFile();
+		final File directory = temporaryFolder.newFolder();
+
+		// Mock java.class.path property. The empty strings are important as the shell scripts
+		// that prepare the Flink class path often have such entries.
+		final String classPath = javaClassPath(
+			"",
+			"",
+			"",
+			file1.getAbsolutePath(),
+			"",
+			directory.getAbsolutePath(),
+			"",
+			file2.getAbsolutePath(),
+			"",
+			"");
+
+		Iterable<File> jarFiles = setClassPathAndGetJarsOnClassPath(classPath);
+
+		assertThat(jarFiles, contains(file1, file2));
+	}
+
+	private static String javaClassPath(String... entries) {
+		String pathSeparator = System.getProperty(JarsOnClassPath.PATH_SEPARATOR);
+		return String.join(pathSeparator, entries);
+	}
+
+	private static Iterable<File> setClassPathAndGetJarsOnClassPath(String classPath) {
+		final String originalClassPath = System.getProperty(JarsOnClassPath.JAVA_CLASS_PATH);
+		try {
+			System.setProperty(JarsOnClassPath.JAVA_CLASS_PATH, classPath);
+			return JarsOnClassPath.INSTANCE.get();
+		} finally {
+			// Reset property
+			System.setProperty(JarsOnClassPath.JAVA_CLASS_PATH, originalClassPath);
+		}
+	}
+
 }
