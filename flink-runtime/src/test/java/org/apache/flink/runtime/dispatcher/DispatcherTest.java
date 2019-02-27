tillrohrmann/incubator-flink

[hotfix] Allow leader assignment to TestingLeaderElectionService if it has not been started

flink-runtime/src/test/java/org/apache/flink/runtime/dispatcher/DispatcherTest.java
@@ -266,7 +266,7 @@ public class DispatcherTest extends TestLogger {
 
 		assertTrue(
 			"jobManagerRunner was not started",
-			dispatcherLeaderElectionService.isStarted());
+			dispatcherLeaderElectionService.isStarted().isDone());
 	}
 
 	/**

flink-runtime/src/test/java/org/apache/flink/runtime/leaderelection/TestingLeaderElectionService.java
@@ -29,9 +29,10 @@ import java.util.concurrent.CompletableFuture;
  */
 public class TestingLeaderElectionService implements LeaderElectionService {
 
-	private LeaderContender contender;
+	private LeaderContender contender = null;
 	private boolean hasLeadership = false;
 	private CompletableFuture<UUID> confirmationFuture = null;
+	private CompletableFuture<Void> startFuture = new CompletableFuture<>();
 	private UUID issuedLeaderSessionId = null;
 
 	/**
@@ -44,13 +45,25 @@ public class TestingLeaderElectionService implements LeaderElectionService {
 	}
 
 	@Override
-	public synchronized void start(LeaderContender contender) throws Exception {
+	public synchronized void start(LeaderContender contender) {
+		assert(!isStarted().isDone());
+
 		this.contender = contender;
+
+		if (hasLeadership) {
+			contender.grantLeadership(issuedLeaderSessionId);
+		}
+
+		startFuture.complete(null);
 	}
 
 	@Override
 	public synchronized void stop() throws Exception {
-
+		contender = null;
+		hasLeadership = false;
+		issuedLeaderSessionId = null;
+		startFuture.cancel(false);
+		startFuture = new CompletableFuture<>();
 	}
 
 	@Override
@@ -72,31 +85,36 @@ public class TestingLeaderElectionService implements LeaderElectionService {
 		confirmationFuture = new CompletableFuture<>();
 		hasLeadership = true;
 		issuedLeaderSessionId = leaderSessionID;
-		contender.grantLeadership(leaderSessionID);
+
+		if (contender != null) {
+			contender.grantLeadership(leaderSessionID);
+		}
 
 		return confirmationFuture;
 	}
 
 	public synchronized void notLeader() {
 		hasLeadership = false;
-		contender.revokeLeadership();
-	}
 
-	public synchronized void reset() {
-		contender = null;
-		hasLeadership  = false;
+		if (contender != null) {
+			contender.revokeLeadership();
+		}
 	}
 
 	public synchronized String getAddress() {
-		return contender.getAddress();
+		if (contender != null) {
+			return contender.getAddress();
+		} else {
+			throw new IllegalStateException("TestingLeaderElectionService has not been started.");
+		}
 	}
 
 	/**
 	 * Returns <code>true</code> if {@link #start(LeaderContender)} was called,
 	 * <code>false</code> otherwise.
 	 */
-	public synchronized boolean isStarted() {
-		return contender != null;
+	public synchronized CompletableFuture<Void> isStarted() {
+		return startFuture;
 	}
 
 }

apache/flink

flink-runtime/src/test/java/org/apache/flink/runtime/dispatcher/DispatcherTest.java
@@ -266,7 +266,7 @@ public class DispatcherTest extends TestLogger {
 
 		assertTrue(
 			"jobManagerRunner was not started",
-			dispatcherLeaderElectionService.isStarted());
+			dispatcherLeaderElectionService.getStartFuture().isDone());
 	}
 
 	/**

flink-runtime/src/test/java/org/apache/flink/runtime/leaderelection/TestingLeaderElectionService.java
@@ -29,9 +29,10 @@ import java.util.concurrent.CompletableFuture;
  */
 public class TestingLeaderElectionService implements LeaderElectionService {
 
-	private LeaderContender contender;
+	private LeaderContender contender = null;
 	private boolean hasLeadership = false;
 	private CompletableFuture<UUID> confirmationFuture = null;
+	private CompletableFuture<Void> startFuture = new CompletableFuture<>();
 	private UUID issuedLeaderSessionId = null;
 
 	/**
@@ -44,13 +45,25 @@ public class TestingLeaderElectionService implements LeaderElectionService {
 	}
 
 	@Override
-	public synchronized void start(LeaderContender contender) throws Exception {
+	public synchronized void start(LeaderContender contender) {
+		assert(!getStartFuture().isDone());
+
 		this.contender = contender;
+
+		if (hasLeadership) {
+			contender.grantLeadership(issuedLeaderSessionId);
+		}
+
+		startFuture.complete(null);
 	}
 
 	@Override
 	public synchronized void stop() throws Exception {
-
+		contender = null;
+		hasLeadership = false;
+		issuedLeaderSessionId = null;
+		startFuture.cancel(false);
+		startFuture = new CompletableFuture<>();
 	}
 
 	@Override
@@ -72,31 +85,38 @@ public class TestingLeaderElectionService implements LeaderElectionService {
 		confirmationFuture = new CompletableFuture<>();
 		hasLeadership = true;
 		issuedLeaderSessionId = leaderSessionID;
-		contender.grantLeadership(leaderSessionID);
+
+		if (contender != null) {
+			contender.grantLeadership(leaderSessionID);
+		}
 
 		return confirmationFuture;
 	}
 
 	public synchronized void notLeader() {
 		hasLeadership = false;
-		contender.revokeLeadership();
-	}
 
-	public synchronized void reset() {
-		contender = null;
-		hasLeadership  = false;
+		if (contender != null) {
+			contender.revokeLeadership();
+		}
 	}
 
 	public synchronized String getAddress() {
-		return contender.getAddress();
+		if (contender != null) {
+			return contender.getAddress();
+		} else {
+			throw new IllegalStateException("TestingLeaderElectionService has not been started.");
+		}
 	}
 
 	/**
-	 * Returns <code>true</code> if {@link #start(LeaderContender)} was called,
-	 * <code>false</code> otherwise.
+	 * Returns the start future indicating whether this leader election service
+	 * has been started or not.
+	 *
+	 * @return Future which is completed once this service has been started
 	 */
-	public synchronized boolean isStarted() {
-		return contender != null;
+	public synchronized CompletableFuture<Void> getStartFuture() {
+		return startFuture;
 	}
 
 }