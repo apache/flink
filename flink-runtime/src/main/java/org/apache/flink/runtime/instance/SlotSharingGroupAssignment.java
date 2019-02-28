c5b815b69a4e508f23ec0b742fe71d82e77ecbae
leesf <490081539@qq.com>
2019-02-28 20:24:49 +0800

    [hotfix][runtime] fix grammatical errors in description

--- flink-runtime/src/main/java/org/apache/flink/runtime/instance/SlotSharingGroupAssignment.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/instance/SlotSharingGroupAssignment.java
@@ -265,7 +265,7 @@ public class SlotSharingGroupAssignment {
 	 * Gets a slot suitable for the given task vertex. This method will prefer slots that are local
 	 * (with respect to {@link ExecutionVertex#getPreferredLocationsBasedOnInputs()}), but will return non local
 	 * slots if no local slot is available. The method returns null, when this sharing group has
-	 * no slot is available for the given JobVertexID. 
+	 * no slot available for the given JobVertexID.
 	 *
 	 * @param vertexID the vertex id
 	 * @param locationPreferences location preferences
