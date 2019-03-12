f99437b8ebbe54889ee356bbace83fce4f0f3137
Timo Walther <twalthr@apache.org>
2019-03-12 21:49:03 +0100

    [hotfix][table] Fix Scala package object

--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/api/package.scala
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/api/package.scala
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.flink.table.api
+package org.apache.flink.table
 
 /**
  * == Table API ==
