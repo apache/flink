fd7dc33d15e485e62ca7549cf14a10238cabed1f
SuXingLee <913910636@qq.com>
2019-03-22 18:36:55 +0800

    [FLINK-11887][metrics] Fix latency drift

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/operators/StreamSource.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/operators/StreamSource.java
@@ -154,7 +154,7 @@ public class StreamSource<OUT, SRC extends SourceFunction<OUT>> extends Abstract
 					public void onProcessingTime(long timestamp) throws Exception {
 						try {
 							// ProcessingTimeService callbacks are executed under the checkpointing lock
-							output.emitLatencyMarker(new LatencyMarker(timestamp, operatorId, subtaskIndex));
+							output.emitLatencyMarker(new LatencyMarker(processingTimeService.getCurrentProcessingTime(), operatorId, subtaskIndex));
 						} catch (Throwable t) {
 							// we catch the Throwables here so that we don't trigger the processing
 							// timer services async exception handler
