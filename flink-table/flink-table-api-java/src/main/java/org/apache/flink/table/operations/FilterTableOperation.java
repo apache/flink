0d1c2bf41e14675e0ed2f832329c3b7ea73e5759
Dawid Wysakowicz <dwysakowicz@apache.org>
2019-04-02 08:49:12 +0200

    [FLINK-11884][table] DistinctTableOperation & FilterTableOperation construction & transformation to RelNodes

--- flink-table/flink-table-api-java/src/main/java/org/apache/flink/table/operations/FilterTableOperation.java
+++ flink-table/flink-table-api-java/src/main/java/org/apache/flink/table/operations/FilterTableOperation.java
@@ -19,44 +19,43 @@
 package org.apache.flink.table.operations;
 
 import org.apache.flink.annotation.Internal;
+import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.table.expressions.Expression;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
- * A utility {@link TableOperationVisitor} that calls
- * {@link TableOperationDefaultVisitor#defaultMethod(TableOperation)}
- * by default, unless other methods are overridden explicitly.
+ * Filters out rows of underlying relational operation that do not match given condition.
  */
 @Internal
-public abstract class TableOperationDefaultVisitor<T> implements TableOperationVisitor<T> {
+public class FilterTableOperation implements TableOperation {
 
-	@Override
-	public T visitProject(ProjectTableOperation projection) {
-		return defaultMethod(projection);
+	private final Expression condition;
+	private final TableOperation child;
+
+	public FilterTableOperation(Expression condition, TableOperation child) {
+		this.condition = condition;
+		this.child = child;
+	}
+
+	public Expression getCondition() {
+		return condition;
 	}
 
 	@Override
-	public T visitAggregate(AggregateTableOperation aggregation) {
-		return defaultMethod(aggregation);
+	public TableSchema getTableSchema() {
+		return child.getTableSchema();
 	}
 
 	@Override
-	public T visitSetOperation(SetTableOperation setOperation) {
-		return defaultMethod(setOperation);
+	public List<TableOperation> getChildren() {
+		return Collections.singletonList(child);
 	}
 
 	@Override
-	public T visitFilter(FilterTableOperation filter) {
-		return defaultMethod(filter);
+	public <T> T accept(TableOperationVisitor<T> visitor) {
+		return visitor.visitFilter(this);
 	}
 
-	@Override
-	public T visitDistinct(DistinctTableOperation distinct) {
-		return defaultMethod(distinct);
-	}
-
-	@Override
-	public T visitOther(TableOperation other) {
-		return defaultMethod(other);
-	}
-
-	public abstract T defaultMethod(TableOperation other);
 }
