027000c7de44db0eb675eb5204ca27db497c0391
tison <wander4096@gmail.com>
2019-04-10 18:57:59 +0800

    [FLINK-12156][coordination] Remove legacy FlinkActor

--- flink-runtime/src/test/scala/org/apache/flink/runtime/testingUtils/TestingUtils.scala
+++ flink-runtime/src/test/scala/org/apache/flink/runtime/testingUtils/TestingUtils.scala
@@ -36,7 +36,6 @@ import org.apache.flink.runtime.instance.{ActorGateway, AkkaActorGateway}
 import org.apache.flink.runtime.leaderretrieval.StandaloneLeaderRetrievalService
 import org.apache.flink.runtime.messages.TaskManagerMessages.{NotifyWhenRegisteredAtJobManager, RegisteredAtJobManager}
 import org.apache.flink.runtime.metrics.{MetricRegistryConfiguration, MetricRegistryImpl}
-import org.apache.flink.runtime.{FlinkActor, LeaderSessionMessageFilter, LogMessages}
 
 import scala.concurrent.duration.{TimeUnit, _}
 import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor}
