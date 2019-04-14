c10487a7712145189c930bec9dd50fbef5961056
yanghua <yanghua1127@gmail.com>
2019-04-14 12:10:20 +0800

    [FLINK-11763] Update AbstractKeyedOperatorRestoreTestBase for 1.8

--- flink-tests/src/test/java/org/apache/flink/test/state/operator/restore/keyed/AbstractKeyedOperatorRestoreTestBase.java
+++ flink-tests/src/test/java/org/apache/flink/test/state/operator/restore/keyed/AbstractKeyedOperatorRestoreTestBase.java
@@ -47,7 +47,8 @@ public abstract class AbstractKeyedOperatorRestoreTestBase extends AbstractOpera
 			MigrationVersion.v1_4,
 			MigrationVersion.v1_5,
 			MigrationVersion.v1_6,
-			MigrationVersion.v1_7);
+			MigrationVersion.v1_7,
+			MigrationVersion.v1_8);
 	}
 
 	public AbstractKeyedOperatorRestoreTestBase(MigrationVersion migrationVersion) {
