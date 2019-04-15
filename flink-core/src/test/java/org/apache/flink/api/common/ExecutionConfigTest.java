36cd7b90ab2b7328b0325a0e6f181392f1152f61
tianchen <niki.lj@alibaba-inc.com>
2019-04-15 16:38:13 +0800

    [FLINK-9787] Let ExecutionConfig#getGlobalJobParameters always return a non null value

--- flink-core/src/test/java/org/apache/flink/api/common/ExecutionConfigTest.java
+++ flink-core/src/test/java/org/apache/flink/api/common/ExecutionConfigTest.java
@@ -152,4 +152,11 @@ public class ExecutionConfigTest extends TestLogger {
 		assertEquals(objectReuseEnabled, copy1.isObjectReuseEnabled());
 		assertEquals(parallelism, copy1.getParallelism());
 	}
+
+	@Test
+	public void testGlobalParametersNotNull() {
+		final ExecutionConfig config = new ExecutionConfig();
+
+		assertNotNull(config.getGlobalJobParameters());
+	}
 }
