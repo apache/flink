ce4bfae9cc4ca70e68519f5303b56f413e3aecab
tianchen <niki.lj@alibaba-inc.com>
2019-04-15 16:38:13 +0800

    [FLINK-9787] Let ExecutionConfig#getGlobalJobParameters always return a non null value
    This closes #8175.

--- flink-core/src/main/java/org/apache/flink/api/common/ExecutionConfig.java
+++ flink-core/src/main/java/org/apache/flink/api/common/ExecutionConfig.java
@@ -162,7 +162,7 @@ public class ExecutionConfig implements Serializable, Archiveable<ArchivedExecut
 
 	// ------------------------------- User code values --------------------------------------------
 
-	private GlobalJobParameters globalJobParameters;
+	private GlobalJobParameters globalJobParameters = new GlobalJobParameters();
 
 	// Serializers and types registered with Kryo and the PojoSerializer
 	// we store them in linked maps/sets to ensure they are registered in order in all kryo instances.
@@ -761,6 +761,7 @@ public class ExecutionConfig implements Serializable, Archiveable<ArchivedExecut
 	 * @param globalJobParameters Custom user configuration object
 	 */
 	public void setGlobalJobParameters(GlobalJobParameters globalJobParameters) {
+		Preconditions.checkNotNull(globalJobParameters, "globalJobParameters shouldn't be null");
 		this.globalJobParameters = globalJobParameters;
 	}
 
@@ -1064,6 +1065,15 @@ public class ExecutionConfig implements Serializable, Archiveable<ArchivedExecut
 		 */
 		public Map<String, String> toMap() {
 			return Collections.emptyMap();
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (obj == null || this.getClass() != obj.getClass()) {
+				return false;
+			}
+
+			return true;
 		}
 	}
 
