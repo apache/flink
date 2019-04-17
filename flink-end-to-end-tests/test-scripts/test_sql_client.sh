1cb21ad3af3cff2eec64f2d27545ae3a4924001f
yanghua <yanghua1127@gmail.com>
2019-04-17 21:02:39 +0800

    Remove useless arg:KAFKA_CONNECTOR_VERSION

--- flink-end-to-end-tests/test-scripts/test_sql_client.sh
+++ flink-end-to-end-tests/test-scripts/test_sql_client.sh
@@ -19,7 +19,6 @@
 
 set -Eeuo pipefail
 
-KAFKA_CONNECTOR_VERSION="2.0"
 KAFKA_VERSION="2.2.0"
 CONFLUENT_VERSION="5.0.0"
 CONFLUENT_MAJOR_VERSION="5.0"
@@ -27,7 +26,6 @@ KAFKA_SQL_VERSION="universal"
 
 source "$(dirname "$0")"/common.sh
 source "$(dirname "$0")"/kafka_sql_common.sh \
-  $KAFKA_CONNECTOR_VERSION \
   $KAFKA_VERSION \
   $CONFLUENT_VERSION \
   $CONFLUENT_MAJOR_VERSION \
