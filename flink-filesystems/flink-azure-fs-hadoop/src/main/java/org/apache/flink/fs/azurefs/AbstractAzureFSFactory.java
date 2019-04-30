5c5368972e81f47a3f56fd7d41815edc4282d858
Piyush Narang <p.narang@criteo.com>
2019-04-30 10:12:53 -0400

    Move code to AzureFS Factory

--- flink-filesystems/flink-azure-fs-hadoop/src/main/java/org/apache/flink/fs/azurefs/AbstractAzureFSFactory.java
+++ flink-filesystems/flink-azure-fs-hadoop/src/main/java/org/apache/flink/fs/azurefs/AbstractAzureFSFactory.java
@@ -21,7 +21,10 @@ package org.apache.flink.fs.azurefs;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.core.fs.FileSystemFactory;
+import org.apache.flink.runtime.fs.hdfs.HadoopFileSystem;
+import org.apache.flink.runtime.util.HadoopUtils;
 
+import org.apache.hadoop.fs.azure.NativeAzureFileSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,10 +35,14 @@ import static org.apache.flink.util.Preconditions.checkNotNull;
 
 /**
  * Abstract factory for AzureFS. Subclasses override to specify
- * the correct scheme (wasb / wasbs).
+ * the correct scheme (wasb / wasbs). Based on Azure HDFS support in the
+ * <a href="https://hadoop.apache.org/docs/current/hadoop-azure/index.html">hadoop-azure</a> module.
+ *
  */
 public abstract class AbstractAzureFSFactory implements FileSystemFactory {
 	private static final Logger LOG = LoggerFactory.getLogger(AzureFSFactory.class);
+
+	private static final String[] CONFIG_PREFIXES = { "fs.azure.", "azure." };
 
 	private Configuration flinkConfig;
 
@@ -48,6 +55,33 @@ public abstract class AbstractAzureFSFactory implements FileSystemFactory {
 	public FileSystem create(URI fsUri) throws IOException {
 		checkNotNull(fsUri, "passed file system URI object should not be null");
 		LOG.info("Trying to load and instantiate Azure File System");
-		return new AzureFileSystem(fsUri, flinkConfig);
+		return new HadoopFileSystem(createInitializedAzureFS(fsUri, flinkConfig));
+	}
+
+	// uri is of the form: wasb(s)://yourcontainer@youraccount.blob.core.windows.net/testDir
+	private org.apache.hadoop.fs.FileSystem createInitializedAzureFS(URI fsUri, Configuration flinkConfig) throws IOException {
+		org.apache.hadoop.conf.Configuration hadoopConfig = HadoopUtils.getHadoopConfiguration(flinkConfig);
+
+		copyFlinkToHadoopConfig(flinkConfig, hadoopConfig);
+
+		org.apache.hadoop.fs.FileSystem azureFS = new NativeAzureFileSystem();
+		azureFS.initialize(fsUri, hadoopConfig);
+
+		return azureFS;
+	}
+
+	private void copyFlinkToHadoopConfig(Configuration flinkConfig, org.apache.hadoop.conf.Configuration hadoopConfig) {
+		// add additional config entries from the Flink config to the Hadoop config
+		for (String key : flinkConfig.keySet()) {
+			for (String prefix : CONFIG_PREFIXES) {
+				if (key.startsWith(prefix)) {
+					String value = flinkConfig.getString(key, null);
+					String newKey = "fs.azure." + key.substring(prefix.length());
+					hadoopConfig.set(newKey, flinkConfig.getString(key, null));
+
+					LOG.debug("Adding Flink config entry for {} as {}={} to Hadoop config for AzureFS", key, newKey, value);
+				}
+			}
+		}
 	}
 }
