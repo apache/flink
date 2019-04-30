62f88c5299dc743f7ab77d6cded4d83d86a5ffd2
Timo Walther <twalthr@apache.org>
2019-04-30 17:18:00 +0200

    [FLINK-12253][table-common] Add a SMALLINT type

--- flink-table/flink-table-common/src/test/java/org/apache/flink/table/types/LogicalTypesTest.java
+++ flink-table/flink-table-common/src/test/java/org/apache/flink/table/types/LogicalTypesTest.java
@@ -23,6 +23,7 @@ import org.apache.flink.table.types.logical.BooleanType;
 import org.apache.flink.table.types.logical.CharType;
 import org.apache.flink.table.types.logical.DecimalType;
 import org.apache.flink.table.types.logical.LogicalType;
+import org.apache.flink.table.types.logical.SmallIntType;
 import org.apache.flink.table.types.logical.TinyIntType;
 import org.apache.flink.table.types.logical.VarBinaryType;
 import org.apache.flink.table.types.logical.VarCharType;
@@ -132,6 +133,19 @@ public class LogicalTypesTest {
 			new Class[]{Byte.class},
 			new LogicalType[]{},
 			new TinyIntType(false)
+		);
+	}
+
+	@Test
+	public void testSmallIntType() {
+		testAll(
+			new SmallIntType(),
+			"SMALLINT",
+			"SMALLINT",
+			new Class[]{Short.class, short.class},
+			new Class[]{Short.class},
+			new LogicalType[]{},
+			new SmallIntType(false)
 		);
 	}
 
