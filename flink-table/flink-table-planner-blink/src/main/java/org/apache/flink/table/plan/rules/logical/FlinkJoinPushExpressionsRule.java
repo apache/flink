6896202b65d7c58e0317f674d57dab07b34e94fb
godfreyhe <godfreyhe@163.com>
2019-05-06 11:43:09 +0800

    rebase master and fix checkstyle error

--- flink-table/flink-table-planner-blink/src/main/java/org/apache/flink/table/plan/rules/logical/FlinkJoinPushExpressionsRule.java
+++ flink-table/flink-table-planner-blink/src/main/java/org/apache/flink/table/plan/rules/logical/FlinkJoinPushExpressionsRule.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.flink.table.plan.rules.logical;
 
 import org.apache.flink.table.plan.util.FlinkRelOptUtil;
