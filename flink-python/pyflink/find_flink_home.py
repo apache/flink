c493eacb35f75ffcb44517098608677698da8fba
Dian Fu <fudian.fd@alibaba-inc.com>
2019-05-08 13:16:51 +0800

    address review comments

--- flink-python/pyflink/find_flink_home.py
+++ flink-python/pyflink/find_flink_home.py
@@ -16,7 +16,6 @@
 # limitations under the License.
 ################################################################################
 
-from __future__ import print_function
 import logging
 import os
 import sys
