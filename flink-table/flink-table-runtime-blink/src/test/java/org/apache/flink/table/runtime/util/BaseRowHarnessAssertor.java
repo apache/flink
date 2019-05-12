4f478a98a163d558114e13ce49ee48ca5d82e6c5
XuQianJin-Stars <x1q1j1@163.com>
2019-05-12 23:01:24 +0800

    [FLINK-11945] Support Over aggregation for SQL

--- flink-table/flink-table-runtime-blink/src/test/java/org/apache/flink/table/runtime/util/BaseRowHarnessAssertor.java
+++ flink-table/flink-table-runtime-blink/src/test/java/org/apache/flink/table/runtime/util/BaseRowHarnessAssertor.java
@@ -18,9 +18,7 @@
 
 package org.apache.flink.table.runtime.util;
 
-import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.api.common.typeutils.TypeSerializer;
 import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
 import org.apache.flink.table.dataformat.BaseRow;
