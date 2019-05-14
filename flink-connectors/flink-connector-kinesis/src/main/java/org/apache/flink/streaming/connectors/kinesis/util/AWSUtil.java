82b05fcf095cb523d55989fe9bd3b8831becf748
Scott Kidder <scott@mux.com>
2019-05-14 11:55:18 -0700

    [FLINK-10190] Allow AWS_REGION to be supplied along with custom Kinesis endpoint URL for region-specific signing
    Update documentation

--- flink-connectors/flink-connector-kinesis/src/main/java/org/apache/flink/streaming/connectors/kinesis/util/AWSUtil.java
+++ flink-connectors/flink-connector-kinesis/src/main/java/org/apache/flink/streaming/connectors/kinesis/util/AWSUtil.java
@@ -84,13 +84,20 @@ public class AWSUtil {
 				.withCredentials(AWSUtil.getCredentialsProvider(configProps))
 				.withClientConfiguration(awsClientConfig);
 
-		if (configProps.containsKey(AWSConfigConstants.AWS_ENDPOINT)) {
-			// Set signingRegion as null, to facilitate mocking Kinesis for local tests
+		if (configProps.containsKey(AWSConfigConstants.AWS_ENDPOINT) && configProps.containsKey(AWSConfigConstants.AWS_REGION)) {
+			// Set signingRegion to the value of AWS_REGION for region-specific signing on a custom endpoint
 			builder.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
-													configProps.getProperty(AWSConfigConstants.AWS_ENDPOINT),
-													null));
+					configProps.getProperty(AWSConfigConstants.AWS_ENDPOINT),
+					configProps.getProperty(AWSConfigConstants.AWS_REGION)));
 		} else {
-			builder.withRegion(Regions.fromName(configProps.getProperty(AWSConfigConstants.AWS_REGION)));
+			if (configProps.containsKey(AWSConfigConstants.AWS_ENDPOINT)) {
+				// Set signingRegion as null, to facilitate mocking Kinesis for local tests
+				builder.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+						configProps.getProperty(AWSConfigConstants.AWS_ENDPOINT),
+						null));
+			} else {
+				builder.withRegion(Regions.fromName(configProps.getProperty(AWSConfigConstants.AWS_REGION)));
+			}
 		}
 		return builder.build();
 	}
