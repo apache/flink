505b54c182867ccac5d1724d72f4085425ac08a8
sunjincheng121 <sunjincheng121@gmail.com>
2019-05-14 21:13:33 +0800

    [FLINK-12485][python] Add completeness test for Table and TableEnvironment.
    This closes #8439

--- flink-python/pyflink/testing/test_case_utils.py
+++ flink-python/pyflink/testing/test_case_utils.py
@@ -22,9 +22,11 @@ import shutil
 import sys
 import tempfile
 import unittest
+from abc import abstractmethod
 
 from pyflink.find_flink_home import _find_flink_home
 from pyflink.table import TableEnvironment, TableConfig
+from pyflink.java_gateway import get_gateway
 
 if sys.version_info[0] >= 3:
     xrange = range
@@ -90,3 +92,81 @@ class PyFlinkBatchTableTestCase(PyFlinkTestCase):
         super(PyFlinkBatchTableTestCase, self).setUp()
         self.t_config = TableConfig.Builder().as_batch_execution().set_parallelism(4).build()
         self.t_env = TableEnvironment.get_table_environment(self.t_config)
+
+
+class PythonAPICompletenessTestCase(unittest.TestCase):
+    """
+    Base class for Python API completeness tests, i.e.,
+    Python API should be aligned with the Java API as much as possible.
+    """
+
+    @classmethod
+    def get_python_class_methods(cls, python_class):
+        return {cls.snake_to_camel(cls.java_method_name(method_name))
+                for method_name in dir(python_class) if not method_name.startswith('_')}
+
+    @staticmethod
+    def snake_to_camel(method_name):
+        output = ''.join(x.capitalize() or '_' for x in method_name.split('_'))
+        return output[0].lower() + output[1:]
+
+    @staticmethod
+    def get_java_class_methods(java_class):
+        gateway = get_gateway()
+        s = set()
+        method_arr = gateway.jvm.Class.forName(java_class).getDeclaredMethods()
+        for i in range(0, len(method_arr)):
+            s.add(method_arr[i].getName())
+        return s
+
+    @classmethod
+    def check_methods(cls):
+        java_methods = PythonAPICompletenessTestCase.get_java_class_methods(cls.java_class())
+        python_methods = cls.get_python_class_methods(cls.python_class())
+        missing_methods = java_methods - python_methods - cls.excluded_methods()
+        if len(missing_methods) > 0:
+            print(missing_methods)
+            print('The Exception should be raised after FLINK-12407 is merged.')
+            # raise Exception('Methods: %s in Java class %s have not been added in Python class %s.'
+            #                % (missing_methods, cls.java_class(), cls.python_class()))
+
+    @classmethod
+    def java_method_name(cls, python_method_name):
+        """
+        This method should be overwritten when the method name of the Python API cannot be
+        consistent with the Java API method name. e.g.: 'as' is python
+        keyword, so we use 'alias' in Python API corresponding 'as' in Java API.
+
+        :param python_method_name: Method name of Python API.
+        :return: The corresponding method name of Java API.
+        """
+        return python_method_name
+
+    @classmethod
+    @abstractmethod
+    def python_class(cls):
+        """
+        Return the Python class that needs to be compared. such as :class:`Table`.
+        """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def java_class(cls):
+        """
+        Return the Java class that needs to be compared. such as `org.apache.flink.table.api.Table`.
+        """
+        pass
+
+    @classmethod
+    def excluded_methods(cls):
+        """
+        Exclude method names that do not need to be checked. When adding excluded methods
+        to the lists you should give a good reason in a comment.
+        :return:
+        """
+        return {}
+
+    def test_completeness(self):
+        if self.python_class() is not None and self.java_class() is not None:
+            self.check_methods()
