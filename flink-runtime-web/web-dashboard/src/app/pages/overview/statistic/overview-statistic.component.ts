5f1ef5df715272675819beabd50a352b83decd10
Fabian Wollert <fabian@zalando.de>
2019-05-25 10:09:27 +0200

    [FLINK-10707] show task manager CPU cores

--- flink-runtime-web/web-dashboard/src/app/pages/overview/statistic/overview-statistic.component.ts
+++ flink-runtime-web/web-dashboard/src/app/pages/overview/statistic/overview-statistic.component.ts
@@ -17,10 +17,10 @@
  */
 
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
-import { OverviewInterface } from 'interfaces';
+import { OverviewInterface, TaskmanagersItemInterface } from 'interfaces';
 import { Subject } from 'rxjs';
 import { flatMap, takeUntil } from 'rxjs/operators';
-import { OverviewService, StatusService } from 'services';
+import { OverviewService, StatusService, TaskManagerService } from 'services';
 
 @Component({
   selector: 'flink-overview-statistic',
@@ -30,12 +30,15 @@ import { OverviewService, StatusService } from 'services';
 })
 export class OverviewStatisticComponent implements OnInit, OnDestroy {
   statistic: OverviewInterface | null;
+  listOfTaskManager: TaskmanagersItemInterface[] = [];
   taskSlotPercentage: number;
+  taskManagerCPUs: number;
   destroy$ = new Subject();
 
   constructor(
     private statusService: StatusService,
     private overviewService: OverviewService,
+    private taskManagerService: TaskManagerService,
     private cdr: ChangeDetectorRef
   ) {}
 
@@ -52,6 +55,23 @@ export class OverviewStatisticComponent implements OnInit, OnDestroy {
         );
         this.cdr.markForCheck();
       });
+    this.statusService.refresh$
+      .pipe(
+        takeUntil(this.destroy$),
+        flatMap(() => this.taskManagerService.loadManagers())
+      )
+      .subscribe(
+        data => {
+          this.listOfTaskManager = data;
+          this.taskManagerCPUs = 0;
+          this.listOfTaskManager.map(tm => (this.taskManagerCPUs += tm.hardware.cpuCores));
+          console.log(this.listOfTaskManager);
+          this.cdr.markForCheck();
+        },
+        () => {
+          this.cdr.markForCheck();
+        }
+      );
   }
 
   ngOnDestroy() {
