ae69fc2fe6fc0c2e892578e0fe3bdc2e51c52c0a
Rui Li <lirui@apache.org>
2019-05-29 12:08:10 +0800

    remove HiveTableSink

--- flink-connectors/flink-connector-hive/src/test/java/org/apache/flink/batch/connectors/hive/HiveTableOutputFormatTest.java
+++ flink-connectors/flink-connector-hive/src/test/java/org/apache/flink/batch/connectors/hive/HiveTableOutputFormatTest.java
@@ -19,40 +19,43 @@
 package org.apache.flink.batch.connectors.hive;
 
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
-import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.api.java.tuple.Tuple5;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
-import org.apache.flink.configuration.Configuration;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.Table;
-import org.apache.flink.table.api.TableConfigOptions;
 import org.apache.flink.table.api.TableSchema;
-import org.apache.flink.table.api.java.BatchTableEnvironment;
 import org.apache.flink.table.catalog.ObjectPath;
 import org.apache.flink.table.catalog.hive.HiveCatalog;
 import org.apache.flink.table.catalog.hive.HiveCatalogTable;
 import org.apache.flink.table.catalog.hive.HiveTestUtils;
-import org.apache.flink.table.runtime.utils.BatchTableEnvUtil;
+import org.apache.flink.table.dataformat.BaseRow;
+import org.apache.flink.table.dataformat.BinaryString;
+import org.apache.flink.table.dataformat.GenericRow;
 
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.metastore.MetaStoreUtils;
+import org.apache.hadoop.hive.metastore.api.StorageDescriptor;
+import org.apache.hadoop.hive.metastore.api.Table;
 import org.apache.hadoop.mapred.JobConf;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
-import scala.collection.JavaConversions;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
- * Tests {@link HiveTableSink}.
+ * Tests {@link HiveTableOutputFormatTest}.
  */
-public class HiveTableSinkTest {
+public class HiveTableOutputFormatTest {
 
 	private static HiveCatalog hiveCatalog;
 	private static HiveConf hiveConf;
@@ -77,41 +80,60 @@ public class HiveTableSinkTest {
 		final String tblName = "dest";
 		ObjectPath tablePath = new ObjectPath(dbName, tblName);
 		TableSchema tableSchema = new TableSchema(
-			new String[]{"a", "b", "c", "d", "e"},
+			new String[]{"i", "l", "d", "s"},
 			new TypeInformation[]{
 				BasicTypeInfo.INT_TYPE_INFO,
-				BasicTypeInfo.INT_TYPE_INFO,
-				BasicTypeInfo.STRING_TYPE_INFO,
 				BasicTypeInfo.LONG_TYPE_INFO,
-				BasicTypeInfo.DOUBLE_TYPE_INFO}
+				BasicTypeInfo.DOUBLE_TYPE_INFO,
+				BasicTypeInfo.STRING_TYPE_INFO}
 		);
 		HiveCatalogTable catalogTable = new HiveCatalogTable(tableSchema, new HashMap<>(), "");
 		hiveCatalog.createTable(tablePath, catalogTable, false);
-		BatchTableEnvironment tableEnv = createTableEnv(1);
-		Table table = smallTuple5DataSet(tableEnv);
-		HiveTableSink hiveTableSink = new HiveTableSink(new JobConf(hiveConf),
-			new RowTypeInfo(tableSchema.getFieldTypes(), tableSchema.getFieldNames()), dbName, tblName, Collections.emptyList());
-		tableEnv.writeToSink(table, hiveTableSink, null);
-		tableEnv.execute();
-		// TODO: verify data is written properly
+
+		Table hiveTable = hiveCatalog.getHiveTable(tablePath);
+		RowTypeInfo rowTypeInfo = new RowTypeInfo(tableSchema.getFieldTypes(), tableSchema.getFieldNames());
+		StorageDescriptor jobSD = hiveTable.getSd().deepCopy();
+		jobSD.setLocation(hiveTable.getSd().getLocation() + "/.staging");
+		HiveTablePartition hiveTablePartition = new HiveTablePartition(jobSD, null);
+		JobConf jobConf = new JobConf(hiveConf);
+		HiveTableOutputFormat outputFormat = new HiveTableOutputFormat(jobConf, dbName, tblName,
+			Collections.emptyList(), rowTypeInfo, hiveTablePartition, MetaStoreUtils.getTableMetadata(hiveTable), false);
+		outputFormat.open(0, 1);
+		List<BaseRow> toWrite = generateRecords();
+		for (BaseRow row : toWrite) {
+			outputFormat.writeRecord(row);
+		}
+		outputFormat.close();
+		outputFormat.finalizeGlobal(1);
+
+		// verify written data
+		Path outputFile = new Path(hiveTable.getSd().getLocation(), "0");
+		FileSystem fs = outputFile.getFileSystem(jobConf);
+		assertTrue(fs.exists(outputFile));
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(outputFile)))) {
+			int numWritten = 0;
+			String line = reader.readLine();
+			while (line != null) {
+				numWritten++;
+				line = reader.readLine();
+			}
+			reader.close();
+			assertEquals(toWrite.size(), numWritten);
+		}
 	}
 
-	private BatchTableEnvironment createTableEnv(int parallelism) {
-		Configuration config = new Configuration();
-		config.setInteger(TableConfigOptions.SQL_RESOURCE_DEFAULT_PARALLELISM, 1);
-		StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment(parallelism, config);
-		return BatchTableEnvironment.create(env);
-	}
-
-	private Table smallTuple5DataSet(BatchTableEnvironment env) {
-		List<Tuple5<Integer, Integer, String, Long, Double>> data = new ArrayList();
-		data.add(new Tuple5<>(1, 10, "Hi", 11L, 1.11));
-		data.add(new Tuple5<>(2, 20, "Hello", 22L, 2.22));
-		data.add(new Tuple5<>(3, 30, "Hello world!", 33L, 3.33));
-
-		return BatchTableEnvUtil.fromCollection(env, JavaConversions.collectionAsScalaIterable(data),
-			TypeInformation.of(new TypeHint<Tuple5<Integer, Integer, String, Long, Double>>() {
-			}),
-			"a,b,c,d,e");
+	private List<BaseRow> generateRecords() {
+		int numRecords = 5;
+		int arity = 4;
+		List<BaseRow> res = new ArrayList<>(numRecords);
+		for (int i = 0; i < numRecords; i++) {
+			GenericRow row = new GenericRow(arity);
+			row.setInt(0, i);
+			row.setLong(1, i);
+			row.setDouble(2, Double.valueOf(String.valueOf(String.format("%d.%d", i, i))));
+			row.setField(3, new BinaryString(String.valueOf((char) ('a' + i))));
+			res.add(row);
+		}
+		return res;
 	}
 }
