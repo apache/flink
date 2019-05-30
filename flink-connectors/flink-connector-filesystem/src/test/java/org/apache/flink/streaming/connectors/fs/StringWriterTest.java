e8be6ff508fa6c7818850a857435d1ae4128221f
wangxiaowei <wangxiaowei-sal@360.cn>
2019-05-30 16:43:40 +0800

    StringWriter support custom row delimiter

--- flink-connectors/flink-connector-filesystem/src/test/java/org/apache/flink/streaming/connectors/fs/StringWriterTest.java
+++ flink-connectors/flink-connector-filesystem/src/test/java/org/apache/flink/streaming/connectors/fs/StringWriterTest.java
@@ -18,17 +18,56 @@
 
 package org.apache.flink.streaming.connectors.fs;
 
-import org.junit.Test;
+import org.apache.flink.util.NetUtils;
 
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.MiniDFSCluster;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.StringTokenizer;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
 
 /**
  * Tests for {@link StringWriter}.
  */
 public class StringWriterTest {
+
+	@ClassRule
+	public static TemporaryFolder tempFolder = new TemporaryFolder();
+
+	private static MiniDFSCluster hdfsCluster;
+	private static org.apache.hadoop.fs.FileSystem dfs;
+
+	private static String outputDir;
+
+	@Before
+	public void createHDFS() throws IOException {
+		org.apache.hadoop.conf.Configuration conf = new org.apache.hadoop.conf.Configuration();
+
+		File dataDir = tempFolder.newFolder();
+
+		conf.set(MiniDFSCluster.HDFS_MINIDFS_BASEDIR, dataDir.getAbsolutePath());
+		MiniDFSCluster.Builder builder = new MiniDFSCluster.Builder(conf);
+		hdfsCluster = builder.build();
+
+		dfs = hdfsCluster.getFileSystem();
+
+		outputDir = "hdfs://"
+			+ NetUtils.hostAndPortToUrlString(hdfsCluster.getURI().getHost(), hdfsCluster.getNameNodePort());
+	}
 
 	@Test
 	public void testDuplicate() {
@@ -41,5 +80,62 @@ public class StringWriterTest {
 		writer.setSyncOnFlush(false);
 		assertFalse(StreamWriterBaseComparator.equals(writer, other));
 		assertFalse(StreamWriterBaseComparator.equals(writer, new StringWriter<>()));
+
+	}
+
+	@Test
+	public void testMultiRowdelimiters() throws IOException {
+		String rowDelimiter1 = "\n";
+		String testDat1 = "A" + rowDelimiter1 + "B" + rowDelimiter1 + "C" + rowDelimiter1 + "D" + rowDelimiter1 + "E";
+		Path testFile1 = new Path(outputDir + "/test01");
+		testRowdelimiter(rowDelimiter1, testDat1, StandardCharsets.UTF_8.name(), testFile1);
+
+		String rowDelimiter2 = "\r\n";
+		String testDat2 = "A" + rowDelimiter2 + "B" + rowDelimiter2 + "C" + rowDelimiter2 + "D" + rowDelimiter2 + "E";
+		Path testFile2 = new Path(outputDir + "/test02");
+		testRowdelimiter(rowDelimiter2, testDat2, StandardCharsets.UTF_8.name(), testFile2);
+
+		String rowDelimiter3 = "*";
+		String testDat3 = "A" + rowDelimiter3 + "B" + rowDelimiter3 + "C" + rowDelimiter3 + "D" + rowDelimiter3 + "E";
+		Path testFile3 = new Path(outputDir + "/test03");
+		testRowdelimiter(rowDelimiter3, testDat3, StandardCharsets.UTF_8.name(), testFile3);
+
+		String rowDelimiter4 = "##";
+		String testDat4 = "A" + rowDelimiter4 + "B" + rowDelimiter4 + "C" + rowDelimiter4 + "D" + rowDelimiter4 + "E";
+		Path testFile4 = new Path(outputDir + "/test04");
+		testRowdelimiter(rowDelimiter4, testDat4, StandardCharsets.UTF_8.name(), testFile4);
+
+	}
+
+	private void testRowdelimiter(String rowDelimiter, String inputData, String charset, Path outputFile) throws IOException {
+		StringWriter<String> writer = new StringWriter(charset, rowDelimiter);
+		writer.open(dfs, outputFile);
+		StringTokenizer lineTokenizer = new StringTokenizer(inputData, rowDelimiter);
+		while (lineTokenizer.hasMoreTokens()){
+			writer.write(lineTokenizer.nextToken());
+		}
+		writer.close();
+		FSDataInputStream inStream = dfs.open(outputFile);
+		byte[] buffer = new byte[inputData.getBytes(charset).length];
+		readFully(inStream, buffer);
+		inStream.close();
+		String outputData = new String(buffer, charset);
+		Assert.assertEquals(inputData, outputData);
+
+	}
+
+	private void readFully(InputStream in, byte[] buffer) throws IOException {
+		int pos = 0;
+		int remaining = buffer.length;
+
+		while (remaining > 0) {
+			int read = in.read(buffer, pos, remaining);
+			if (read == -1) {
+				throw new EOFException();
+			}
+
+			pos += read;
+			remaining -= read;
+		}
 	}
 }
