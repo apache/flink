eaaf4fbfcca65de4c50a637c25efa59619603950
Aloys <lofterzhang@gmail.com>
2019-06-07 16:16:21 +0800

    [FLINK-12775][build] Bump flink-shaded version to 7.0

--- flink-runtime/src/main/java/org/apache/flink/runtime/rest/handler/AbstractHandler.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/rest/handler/AbstractHandler.java
@@ -51,6 +51,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -122,7 +123,7 @@ public abstract class AbstractHandler<T extends RestfulGateway, R extends Reques
 				}
 			} else {
 				try {
-					ByteBufInputStream in = new ByteBufInputStream(msgContent);
+					InputStream in = new ByteBufInputStream(msgContent);
 					request = MAPPER.readValue(in, untypedResponseMessageHeaders.getRequestClass());
 				} catch (JsonParseException | JsonMappingException je) {
 					throw new RestHandlerException(
