4324d91ffc20279f71f8b0629fda957d4dac27d5
hequn8128 <chenghequn@gmail.com>
2019-06-17 17:38:01 +0800

    reuse function code

--- flink-table/flink-table-api-java/src/main/java/org/apache/flink/table/typeutils/FieldInfoUtils.java
+++ flink-table/flink-table-api-java/src/main/java/org/apache/flink/table/typeutils/FieldInfoUtils.java
@@ -46,6 +46,7 @@ import javax.annotation.Nullable;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -293,32 +294,19 @@ public class FieldInfoUtils {
 	 * @return An array holding the field names
 	 */
 	public static <A> String[] getFieldNames(TypeInformation<A> inputType) {
-		validateInputTypeInfo(inputType);
-
-		final String[] fieldNames;
-		if (inputType instanceof CompositeType) {
-			fieldNames = ((CompositeType<A>) inputType).getFieldNames();
-		} else {
-			fieldNames = new String[]{ATOMIC_FIELD_NAME};
-		}
-
-		if (Arrays.asList(fieldNames).contains("*")) {
-			throw new ValidationException("Field name can not be '*'.");
-		}
-
-		return fieldNames;
+		return getFieldNames(inputType, Collections.emptyList());
 	}
 
 	/**
 	 * Returns field names for a given {@link TypeInformation}. If the input {@link TypeInformation}
-	 * is not a composite type, the result field name should not exist in the invalidNamesForNonCompositeType.
+	 * is not a composite type, the result field name should not exist in the existingNames.
 	 *
 	 * @param inputType The TypeInformation extract the field names.
-	 * @param invalidNamesForNonCompositeType The invalid field names for non-composite types.
+	 * @param existingNames The existing field names for non-composite types that can not be used.
 	 * @param <A> The type of the TypeInformation.
 	 * @return An array holding the field names
 	 */
-	public static <A> String[] getFieldNames(TypeInformation<A> inputType, List<String> invalidNamesForNonCompositeType) {
+	public static <A> String[] getFieldNames(TypeInformation<A> inputType, List<String> existingNames) {
 		validateInputTypeInfo(inputType);
 
 		final String[] fieldNames;
@@ -327,7 +315,7 @@ public class FieldInfoUtils {
 		} else {
 			int i = 0;
 			String fieldName = ATOMIC_FIELD_NAME;
-			while (invalidNamesForNonCompositeType.contains(fieldName)) {
+			while (existingNames.contains(fieldName)) {
 				fieldName = ATOMIC_FIELD_NAME + "_" + i++;
 			}
 			fieldNames = new String[]{fieldName};
