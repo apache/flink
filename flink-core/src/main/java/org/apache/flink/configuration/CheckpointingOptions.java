8cc6c3060f2c6cdd8e0aa02b94816aad4ae1d60b
Yun Tang <myasuka@live.com>
2019-06-18 20:47:31 +0800

    [FLINK-11869] Rename writebuffer config key

--- flink-core/src/main/java/org/apache/flink/configuration/CheckpointingOptions.java
+++ flink-core/src/main/java/org/apache/flink/configuration/CheckpointingOptions.java
@@ -134,7 +134,7 @@ public class CheckpointingOptions {
 	 * The default write buffer size for file checkpoint output stream created by FsCheckpointStreamFactory.
 	 */
 	public static final ConfigOption<Integer> FS_BUFFER_SIZE = ConfigOptions
-		.key("state.backend.fs.buffer-size")
+		.key("state.backend.fs.write-buffer-size")
 		.defaultValue(4 * 1024)
 		.withDescription(String.format("The default write buffer size for file checkpoint output stream created by FsCheckpointStreamFactory. " +
 			"And the final actual write buffer size would consider the maximum of values of this option and option '%s'.", FS_SMALL_FILE_THRESHOLD.key()));
