44192fe5c34e67971a14f615268c9a589d99ef04
Zhijiang <wangzhijiang999@aliyun.com>
2019-06-19 12:36:54 +0800

    [hotfix][runtime] Cleanup IOManager code

--- flink-runtime/src/main/java/org/apache/flink/runtime/io/disk/iomanager/IOManager.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/disk/iomanager/IOManager.java
@@ -19,6 +19,8 @@
 package org.apache.flink.runtime.io.disk.iomanager;
 
 import org.apache.flink.core.memory.MemorySegment;
+import org.apache.flink.runtime.io.disk.iomanager.FileIOChannel.Enumerator;
+import org.apache.flink.runtime.io.disk.iomanager.FileIOChannel.ID;
 import org.apache.flink.runtime.io.network.buffer.Buffer;
 import org.apache.flink.util.FileUtils;
 
@@ -36,10 +38,9 @@ import java.util.concurrent.LinkedBlockingQueue;
  * The facade for the provided I/O manager services.
  */
 public abstract class IOManager {
-	/** Logging */
 	protected static final Logger LOG = LoggerFactory.getLogger(IOManager.class);
 
-	/** The temporary directories for files */
+	/** The temporary directories for files. */
 	private final File[] paths;
 
 	/** A random number generator for the anonymous ChannelIDs. */
@@ -120,41 +121,40 @@ public abstract class IOManager {
 	// ------------------------------------------------------------------------
 
 	/**
-	 * Creates a new {@link FileIOChannel.ID} in one of the temp directories. Multiple
-	 * invocations of this method spread the channels evenly across the different directories.
+	 * Creates a new {@link ID} in one of the temp directories. Multiple invocations of this
+	 * method spread the channels evenly across the different directories.
 	 *
 	 * @return A channel to a temporary directory.
 	 */
-	public FileIOChannel.ID createChannel() {
+	public ID createChannel() {
 		final int num = getNextPathNum();
-		return new FileIOChannel.ID(this.paths[num], num, this.random);
+		return new ID(this.paths[num], num, this.random);
 	}
 
 	/**
-	 * Creates a new {@link FileIOChannel.Enumerator}, spreading the channels in a round-robin fashion
+	 * Creates a new {@link Enumerator}, spreading the channels in a round-robin fashion
 	 * across the temporary file directories.
 	 *
 	 * @return An enumerator for channels.
 	 */
-	public FileIOChannel.Enumerator createChannelEnumerator() {
-		return new FileIOChannel.Enumerator(this.paths, this.random);
+	public Enumerator createChannelEnumerator() {
+		return new Enumerator(this.paths, this.random);
 	}
 
 	/**
 	 * Deletes the file underlying the given channel. If the channel is still open, this
 	 * call may fail.
-	 * 
+	 *
 	 * @param channel The channel to be deleted.
-	 * @throws IOException Thrown if the deletion fails.
 	 */
-	public void deleteChannel(FileIOChannel.ID channel) throws IOException {
+	public static void deleteChannel(ID channel) {
 		if (channel != null) {
 			if (channel.getPathFile().exists() && !channel.getPathFile().delete()) {
 				LOG.warn("IOManager failed to delete temporary file {}", channel.getPath());
 			}
 		}
 	}
-	
+
 	// ------------------------------------------------------------------------
 	//                        Reader / Writer instantiations
 	// ------------------------------------------------------------------------
@@ -167,8 +167,8 @@ public abstract class IOManager {
 	 * @return A block channel writer that writes to the given channel.
 	 * @throws IOException Thrown, if the channel for the writer could not be opened.
 	 */
-	public BlockChannelWriter<MemorySegment> createBlockChannelWriter(FileIOChannel.ID channelID) throws IOException {
-		return createBlockChannelWriter(channelID, new LinkedBlockingQueue<MemorySegment>());
+	public BlockChannelWriter<MemorySegment> createBlockChannelWriter(ID channelID) throws IOException {
+		return createBlockChannelWriter(channelID, new LinkedBlockingQueue<>());
 	}
 
 	/**
@@ -180,8 +180,9 @@ public abstract class IOManager {
 	 * @return A block channel writer that writes to the given channel.
 	 * @throws IOException Thrown, if the channel for the writer could not be opened.
 	 */
-	public abstract BlockChannelWriter<MemorySegment> createBlockChannelWriter(FileIOChannel.ID channelID,
-				LinkedBlockingQueue<MemorySegment> returnQueue) throws IOException;
+	public abstract BlockChannelWriter<MemorySegment> createBlockChannelWriter(
+		ID channelID,
+		LinkedBlockingQueue<MemorySegment> returnQueue) throws IOException;
 
 	/**
 	 * Creates a block channel writer that writes to the given channel. The writer calls the given callback
@@ -193,7 +194,9 @@ public abstract class IOManager {
 	 * @return A block channel writer that writes to the given channel.
 	 * @throws IOException Thrown, if the channel for the writer could not be opened.
 	 */
-	public abstract BlockChannelWriterWithCallback<MemorySegment> createBlockChannelWriter(FileIOChannel.ID channelID, RequestDoneCallback<MemorySegment> callback) throws IOException;
+	public abstract BlockChannelWriterWithCallback<MemorySegment> createBlockChannelWriter(
+		ID channelID,
+		RequestDoneCallback<MemorySegment> callback) throws IOException;
 
 	/**
 	 * Creates a block channel reader that reads blocks from the given channel. The reader pushed
@@ -204,8 +207,8 @@ public abstract class IOManager {
 	 * @return A block channel reader that reads from the given channel.
 	 * @throws IOException Thrown, if the channel for the reader could not be opened.
 	 */
-	public BlockChannelReader<MemorySegment> createBlockChannelReader(FileIOChannel.ID channelID) throws IOException {
-		return createBlockChannelReader(channelID, new LinkedBlockingQueue<MemorySegment>());
+	public BlockChannelReader<MemorySegment> createBlockChannelReader(ID channelID) throws IOException {
+		return createBlockChannelReader(channelID, new LinkedBlockingQueue<>());
 	}
 
 	/**
@@ -217,22 +220,27 @@ public abstract class IOManager {
 	 * @return A block channel reader that reads from the given channel.
 	 * @throws IOException Thrown, if the channel for the reader could not be opened.
 	 */
-	public abstract BlockChannelReader<MemorySegment> createBlockChannelReader(FileIOChannel.ID channelID,
-										LinkedBlockingQueue<MemorySegment> returnQueue) throws IOException;
+	public abstract BlockChannelReader<MemorySegment> createBlockChannelReader(
+		ID channelID,
+		LinkedBlockingQueue<MemorySegment> returnQueue) throws IOException;
 
-	public abstract BufferFileWriter createBufferFileWriter(FileIOChannel.ID channelID) throws IOException;
+	public abstract BufferFileWriter createBufferFileWriter(ID channelID) throws IOException;
 
-	public abstract BufferFileReader createBufferFileReader(FileIOChannel.ID channelID, RequestDoneCallback<Buffer> callback) throws IOException;
+	public abstract BufferFileReader createBufferFileReader(
+		ID channelID,
+		RequestDoneCallback<Buffer> callback) throws IOException;
 
-	public abstract BufferFileSegmentReader createBufferFileSegmentReader(FileIOChannel.ID channelID, RequestDoneCallback<FileSegment> callback) throws IOException;
+	public abstract BufferFileSegmentReader createBufferFileSegmentReader(
+		ID channelID,
+		RequestDoneCallback<FileSegment> callback) throws IOException;
 
 	/**
 	 * Creates a block channel reader that reads all blocks from the given channel directly in one bulk.
 	 * The reader draws segments to read the blocks into from a supplied list, which must contain as many
 	 * segments as the channel has blocks. After the reader is done, the list with the full segments can be
 	 * obtained from the reader.
-	 * <p>
-	 * If a channel is not to be read in one bulk, but in multiple smaller batches, a
+	 *
+	 * <p>If a channel is not to be read in one bulk, but in multiple smaller batches, a
 	 * {@link BlockChannelReader} should be used.
 	 *
 	 * @param channelID The descriptor for the channel to write to.
@@ -241,26 +249,19 @@ public abstract class IOManager {
 	 * @return A block channel reader that reads from the given channel.
 	 * @throws IOException Thrown, if the channel for the reader could not be opened.
 	 */
-	public abstract BulkBlockChannelReader createBulkBlockChannelReader(FileIOChannel.ID channelID,
-			List<MemorySegment> targetSegments, int numBlocks) throws IOException;
+	public abstract BulkBlockChannelReader createBulkBlockChannelReader(
+		ID channelID,
+		List<MemorySegment> targetSegments,
+		int numBlocks) throws IOException;
 
 
 	// ------------------------------------------------------------------------
 	//                          Utilities
 	// ------------------------------------------------------------------------
-	
-	/**
-	 * Gets the number of directories across which the I/O manager rotates its files.
-	 * 
-	 * @return The number of temporary file directories.
-	 */
-	public int getNumberOfSpillingDirectories() {
-		return this.paths.length;
-	}
 
 	/**
 	 * Gets the directories that the I/O manager spills to.
-	 * 
+	 *
 	 * @return The directories that the I/O manager spills to.
 	 */
 	public File[] getSpillingDirectories() {
@@ -279,8 +280,8 @@ public abstract class IOManager {
 		}
 		return strings;
 	}
-	
-	protected int getNextPathNum() {
+
+	private int getNextPathNum() {
 		final int next = this.nextPath;
 		final int newNext = next + 1;
 		this.nextPath = newNext >= this.paths.length ? 0 : newNext;
