5ec6386dcfe97e0e57fdf9a3b5c828173969ecaa
xuyang1706 <xuyang1706@gmail.com>
2019-06-19 21:15:09 +0800

    [FLINK-12744][ml] add shared params in ml package
    - add shared params

---
+++ flink-ml-parent/flink-ml-lib/src/main/java/org/apache/flink/ml/params/shared/colname/HasOutputCol.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.flink.ml.params.shared.colname;
+
+import org.apache.flink.ml.api.misc.param.ParamInfo;
+import org.apache.flink.ml.api.misc.param.ParamInfoFactory;
+import org.apache.flink.ml.api.misc.param.WithParams;
+
+/**
+ * An interface for classes with a parameter specifying the name of the output column.
+ * @see HasOutputCols
+ * @see HasOutputColDefaultAsNull
+ * @see HasOutputColsDefaultAsNull
+ */
+public interface HasOutputCol<T> extends WithParams <T> {
+
+	ParamInfo <String> OUTPUT_COL = ParamInfoFactory
+		.createParamInfo("outputCol", String.class)
+		.setDescription("Name of the output column")
+		.setRequired()
+		.build();
+
+	default String getOutputCol() {
+		return get(OUTPUT_COL);
+	}
+
+	default T setOutputCol(String value) {
+		return set(OUTPUT_COL, value);
+	}
+}
