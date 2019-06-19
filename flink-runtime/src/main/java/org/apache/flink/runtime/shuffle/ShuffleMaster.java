fb8ce3eadcbde57502de4817e4350bf0034dbd94
Andrey Zagrebin <azagrebin@gmail.com>
2019-06-19 09:29:51 +0200

    [FLINK-12890][coordination][shuffle] Introduce ShuffleMaster#removePartitionExternally
    JM should call this whenever the partition does not need to be consumed any more.
    This call releases partition resources possibly occupied externally outside of TM and does not depend on ShuffleDescriptor.hasLocalResources.

--- flink-runtime/src/main/java/org/apache/flink/runtime/shuffle/ShuffleMaster.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/shuffle/ShuffleMaster.java
@@ -18,6 +18,9 @@
 
 package org.apache.flink.runtime.shuffle;
 
+import org.apache.flink.runtime.deployment.ResultPartitionDeploymentDescriptor;
+
+import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -41,4 +44,17 @@ public interface ShuffleMaster<T extends ShuffleDescriptor> {
 	CompletableFuture<T> registerPartitionWithProducer(
 		PartitionDescriptor partitionDescriptor,
 		ProducerDescriptor producerDescriptor);
+
+	/**
+	 * Release any external resources occupied by the given partition.
+	 *
+	 * <p>This call triggers release of any resources which are occupied by the given partition in the external systems
+	 * outside of the producer executor. This is mostly relevant for the batch jobs and blocking result partitions.
+	 * This method is not called if {@link ResultPartitionDeploymentDescriptor#isReleasedOnConsumption()} is {@code true}.
+	 * The producer local resources are managed by {@link ShuffleDescriptor#storesLocalResourcesOn()} and
+	 * {@link ShuffleEnvironment#releasePartitions(Collection)}.
+	 *
+	 * @param shuffleDescriptor shuffle descriptor of the result partition to release externally.
+	 */
+	void releasePartitionExternally(ShuffleDescriptor shuffleDescriptor);
 }
