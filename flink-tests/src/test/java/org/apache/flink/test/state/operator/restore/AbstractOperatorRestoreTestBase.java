3616f5afd0d686e549d0d8ff993d3cd2067b0c49
Yun Tang <myasuka@live.com>
2019-06-21 12:39:38 +0800

    [FLINK-12916][tests] Retry cancelWithSavepoint on cancellation barrier

--- flink-tests/src/test/java/org/apache/flink/test/state/operator/restore/AbstractOperatorRestoreTestBase.java
+++ flink-tests/src/test/java/org/apache/flink/test/state/operator/restore/AbstractOperatorRestoreTestBase.java
@@ -70,7 +70,8 @@ public abstract class AbstractOperatorRestoreTestBase extends TestLogger {
 		.compile(
 			"(was not running)" +
 				"|(Not all required tasks are currently running)" +
-				"|(Checkpoint was declined \\(tasks not ready\\))"
+				"|(Checkpoint was declined \\(tasks not ready\\))" +
+				"|(Task received cancellation from one of its inputs)"
 		);
 
 	@Rule
