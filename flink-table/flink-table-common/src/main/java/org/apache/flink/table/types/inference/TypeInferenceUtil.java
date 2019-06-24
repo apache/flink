1061a0571a1ca87d40ec8b823bb9fd58d5111a79
Timo Walther <twalthr@apache.org>
2019-06-24 10:42:21 +0200

    [FLINK-12924][table] Introduce basic type inference interfaces

---
+++ flink-table/flink-table-common/src/main/java/org/apache/flink/table/types/inference/TypeInferenceUtil.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.types.inference;
+
+import org.apache.flink.annotation.Internal;
+import org.apache.flink.table.types.DataType;
+
+import java.util.List;
+
+/**
+ * Utility for performing type inference.
+ */
+@Internal
+public class TypeInferenceUtil {
+
+	public static Result runTypeInference(TypeInference typeInference, CallContext callContext) {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * The result of a type inference run. It contains information about how arguments need to be
+	 * modified in order to comply with the function's signature. This includes casts that need to be
+	 * inserted, reordering of arguments, or insertion of default values.
+	 */
+	public static class Result {
+
+		private final List<DataType> expectedArgumentTypes;
+
+		private final DataType accumulatorDataType;
+
+		private final DataType outputDataType;
+
+		public Result(
+				List<DataType> expectedArgumentTypes,
+				DataType accumulatorDataType,
+				DataType outputDataType) {
+			this.expectedArgumentTypes = expectedArgumentTypes;
+			this.accumulatorDataType = accumulatorDataType;
+			this.outputDataType = outputDataType;
+		}
+
+		public List<DataType> getExpectedArgumentTypes() {
+			return expectedArgumentTypes;
+		}
+
+		public DataType getAccumulatorDataType() {
+			return accumulatorDataType;
+		}
+
+		public DataType getOutputDataType() {
+			return outputDataType;
+		}
+	}
+
+	private TypeInferenceUtil() {
+		// no instantiation
+	}
+}
