3fa91f277480e4089c47dba6af63c5f38a7ca5dd
Kurt Young <ykt836@gmail.com>
2019-07-08 17:43:04 +0800

    [hotfix][table-planner-blink] Fix static variable name

--- flink-table/flink-table-planner-blink/src/main/java/org/apache/flink/table/executor/ExecutorBase.java
+++ flink-table/flink-table-planner-blink/src/main/java/org/apache/flink/table/executor/ExecutorBase.java
@@ -32,7 +32,9 @@ import java.util.List;
  */
 @Internal
 public abstract class ExecutorBase implements Executor {
-	private final String DEFAULT_JOB_NAME = "Flink Exec Table Job";
+
+	private static final String DEFAULT_JOB_NAME = "Flink Exec Table Job";
+
 	private final StreamExecutionEnvironment executionEnvironment;
 	protected List<Transformation<?>> transformations = new ArrayList<>();
 
