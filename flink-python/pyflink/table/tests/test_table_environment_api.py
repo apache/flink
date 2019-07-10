027e1bf4a84563889016c3d601ab66a44e862a4f
Wei Zhong <weizhong0618@gmail.com>
2019-07-10 16:57:57 +0800

    [hotfix][python] remove query_config.py and move the idle state retention time configuration APIs to TableConfig, add completeness test for TableConfig and complement the TableConfig class.

--- flink-python/pyflink/table/tests/test_table_environment_api.py
+++ flink-python/pyflink/table/tests/test_table_environment_api.py
@@ -22,7 +22,6 @@ from py4j.compat import unicode
 
 from pyflink.dataset import ExecutionEnvironment
 from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.table import StreamQueryConfig
 from pyflink.table.table_environment import BatchTableEnvironment, StreamTableEnvironment
 from pyflink.table.table_config import TableConfig
 from pyflink.table.types import DataTypes, RowType
@@ -137,14 +136,16 @@ class StreamTableEnvironmentTests(PyFlinkStreamTableTestCase):
         expected = ['1,Hi,Hello', '2,Hello,Hello']
         self.assert_equals(actual, expected)
 
-    def test_query_config(self):
-        query_config = StreamQueryConfig()
+    def test_table_config(self):
 
-        query_config.with_idle_state_retention_time(
+        table_config = TableConfig.get_default()
+        table_config.set_idle_state_retention_time(
             datetime.timedelta(days=1), datetime.timedelta(days=2))
+        table_config.set_decimal_context(20, "UP")
 
-        self.assertEqual(2 * 24 * 3600 * 1000, query_config.get_max_idle_state_retention_time())
-        self.assertEqual(24 * 3600 * 1000, query_config.get_min_idle_state_retention_time())
+        self.assertEqual(2 * 24 * 3600 * 1000, table_config.get_max_idle_state_retention_time())
+        self.assertEqual(24 * 3600 * 1000, table_config.get_min_idle_state_retention_time())
+        self.assertEqual((20, "UP"), table_config.get_decimal_context())
 
     def test_create_table_environment(self):
         table_config = TableConfig()
