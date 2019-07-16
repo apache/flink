ccd7dccfc8d79fb9899d695d5f1f0966c7d0c578
Chesnay Schepler <chesnay@apache.org>
2019-07-16 09:46:58 +0200

    remove comma

--- flink-end-to-end-tests/test-scripts/test_streaming_file_sink.sh
+++ flink-end-to-end-tests/test-scripts/test_streaming_file_sink.sh
@@ -30,7 +30,7 @@ s3_setup hadoop
 set_conf_ssl "mutual" "OPENSSL" "${OPENSSL_LINKAGE}"
 set_config_key "metrics.fetcher.update-interval" "2000"
 # this test relies on global failovers
-set_config_key "jobmanager.execution.failover-strategy", "full"
+set_config_key "jobmanager.execution.failover-strategy" "full"
 
 OUT=temp/test_streaming_file_sink-$(uuidgen)
 OUTPUT_PATH="$TEST_DATA_DIR/$OUT"
