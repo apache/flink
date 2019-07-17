b483e1db2618595b782fce5a336a7bd087abec8c
Dian Fu <fudian.fd@alibaba-inc.com>
2019-07-17 21:39:06 +0800

    [FLINK-13308][python] Drop the classifier of the flink-python jar

--- flink-dist/src/main/flink-bin/bin/pyflink-shell.sh
+++ flink-dist/src/main/flink-bin/bin/pyflink-shell.sh
@@ -25,7 +25,7 @@ _FLINK_HOME_DETERMINED=1
 . "$FLINK_HOME"/bin/config.sh
 
 FLINK_CLASSPATH=`constructFlinkClassPath`
-PYTHON_JAR_PATH=`echo "$FLINK_OPT_DIR"/flink-python*java-binding.jar`
+PYTHON_JAR_PATH=`echo "$FLINK_OPT_DIR"/flink-python*.jar`
 
 
 PYFLINK_PYTHON="${PYFLINK_PYTHON:-"python"}"
