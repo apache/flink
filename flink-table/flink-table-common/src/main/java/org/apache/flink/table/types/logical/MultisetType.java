885d64711170dd32c21daacaf86a216708c19953
Timo Walther <twalthr@apache.org>
2019-07-18 12:40:29 +0200

    [FLINK-13262][docs] Add documentation for the new Table & SQL API type system
    This closes #9161.

--- flink-table/flink-table-common/src/main/java/org/apache/flink/table/types/logical/MultisetType.java
+++ flink-table/flink-table-common/src/main/java/org/apache/flink/table/types/logical/MultisetType.java
@@ -34,7 +34,7 @@ import java.util.Set;
  * ensure uniqueness.
  *
  * <p>The serialized string representation is {@code MULTISET<t>} where {@code t} is the logical type
- * of the contained elements.
+ * of the contained elements. {@code t MULTISET} is a synonym for being closer to the SQL standard.
  *
  * <p>A conversion is possible through a map that assigns each value to an integer multiplicity
  * ({@code Map<t, Integer>}).
