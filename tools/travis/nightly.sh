01c06dae21006b363b80bcb7b0071578916eb057
Chesnay Schepler <chesnay@apache.org>
2019-07-24 09:55:08 +0200

    [FLINK-13394][travis] Use fallback unsafe MapR repository

--- tools/travis/nightly.sh
+++ tools/travis/nightly.sh
@@ -37,7 +37,8 @@ mkdir -p $ARTIFACTS_DIR || { echo "FAILURE: cannot create log directory '${ARTIF
 LOG4J_PROPERTIES=${HERE}/../log4j-travis.properties
 
 MVN_LOGGING_OPTIONS="-Dlog.dir=${ARTIFACTS_DIR} -Dlog4j.configuration=file://$LOG4J_PROPERTIES -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
-MVN_COMMON_OPTIONS="-nsu -B -Dflink.forkCount=2 -Dflink.forkCountTestPackage=2 -Dfast -Pskip-webui-build"
+# We use -Punsafe-mapr-repo since the https version fails on Travis for some reason.
+MVN_COMMON_OPTIONS="-nsu -B -Dflink.forkCount=2 -Dflink.forkCountTestPackage=2 -Dfast -Pskip-webui-build -Punsafe-mapr-repo"
 MVN_COMPILE_OPTIONS="-T1C -DskipTests"
 
 cp tools/travis/splits/* flink-end-to-end-tests
