dda22b3fb4fe1af90a9b8e668a22def70a8c6f8d
godfreyhe <godfreyhe@163.com>
2019-07-24 17:14:59 +0800

    [FLINK-13402] [table] Copy RelOptCluster to flink planner to fix "RelOptCluster's constructor can not be accessed when connector depends on both planners"

--- flink-table/flink-table-planner/src/main/java/org/apache/calcite/plan/RelOptCluster.java
+++ flink-table/flink-table-planner/src/main/java/org/apache/calcite/plan/RelOptCluster.java
@@ -1,12 +1,13 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to you under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -35,7 +36,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * This class is copied from Calcite's {@link org.apache.calcite.plan.RelOptCluster},
  * can be removed after https://issues.apache.org/jira/browse/CALCITE-2855 is accepted.
- * NOTES: please make sure to synchronize with RelDecorrelator in flink planner when changing this class.
+ * NOTES: please make sure to synchronize with RelDecorrelator in blink planner when changing this class.
  *
  * Modification:
  * - Make non-deprecated constructor public
