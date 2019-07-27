12fa022f25d91065fd2eeb91e29118611b8ac5c6
vthinkxie <yadong.xyd@alibaba-inc.com>
2019-07-27 15:58:48 +0800

    [FLINK-13386][web]: Fix operators/tasks metrics sort

--- flink-runtime-web/web-dashboard/src/app/services/metrics.service.ts
+++ flink-runtime-web/web-dashboard/src/app/services/metrics.service.ts
@@ -33,9 +33,23 @@ export class MetricsService {
    * @param vertexId
    */
   getAllAvailableMetrics(jobId: string, vertexId: string) {
-    return this.httpClient.get<Array<{ id: string; value: string }>>(
-      `${BASE_URL}/jobs/${jobId}/vertices/${vertexId}/metrics`
-    );
+    return this.httpClient
+      .get<Array<{ id: string; value: string }>>(`${BASE_URL}/jobs/${jobId}/vertices/${vertexId}/metrics`)
+      .pipe(
+        map(item =>
+          item.sort((pre, next) => {
+            const preId = pre.id.toLowerCase();
+            const nextId = next.id.toLowerCase();
+            if (preId < nextId) {
+              return -1;
+            } else if (preId > nextId) {
+              return 1;
+            } else {
+              return 0;
+            }
+          })
+        )
+      );
   }
 
   /**
