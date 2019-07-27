c46ba1bbd28782b9fe4ab5737aee367b5314a818
vthinkxie <yadong.xyd@alibaba-inc.com>
2019-07-27 17:56:26 +0800

    [FLINK-13386][web]: Add all operators watermark

--- flink-runtime-web/web-dashboard/src/app/pages/job/overview/job-overview.component.ts
+++ flink-runtime-web/web-dashboard/src/app/pages/job/overview/job-overview.component.ts
@@ -26,10 +26,11 @@ import {
   ViewChild
 } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subject } from 'rxjs';
-import { filter, takeUntil } from 'rxjs/operators';
+import { LONG_MIN_VALUE } from 'config';
+import { forkJoin, Observable, of, Subject } from 'rxjs';
+import { catchError, filter, map, takeUntil } from 'rxjs/operators';
 import { NodesItemCorrectInterface, NodesItemLinkInterface } from 'interfaces';
-import { JobService } from 'services';
+import { JobService, MetricsService } from 'services';
 import { DagreComponent } from 'share/common/dagre/dagre.component';
 
 @Component({
@@ -62,11 +63,52 @@ export class JobOverviewComponent implements OnInit, OnDestroy {
     }
   }
 
+  mergeWithWatermarks(nodes: NodesItemCorrectInterface[]): Observable<NodesItemCorrectInterface[]> {
+    return forkJoin(
+      nodes.map(node => {
+        const listOfMetricId = [];
+        let lowWatermark = NaN;
+        for (let i = 0; i < node.parallelism; i++) {
+          listOfMetricId.push(`${i}.currentInputWatermark`);
+        }
+        return this.metricService.getMetrics(this.jobId, node.id, listOfMetricId).pipe(
+          map(metrics => {
+            let minValue = NaN;
+            const watermarks: { [index: string]: number } = {};
+            for (const key in metrics.values) {
+              const value = metrics.values[key];
+              const subtaskIndex = key.replace('.currentInputWatermark', '');
+              watermarks[subtaskIndex] = value;
+              if (isNaN(minValue) || value < minValue) {
+                minValue = value;
+              }
+            }
+            if (!isNaN(minValue) && minValue > LONG_MIN_VALUE) {
+              lowWatermark = minValue;
+            } else {
+              lowWatermark = NaN;
+            }
+            return { ...node, lowWatermark };
+          })
+        );
+      })
+    ).pipe(catchError(() => of(nodes)));
+  }
+
+  refreshNodesWithWatermarks() {
+    this.mergeWithWatermarks(this.nodes).subscribe(nodes => {
+      nodes.forEach(node => {
+        this.dagreComponent.updateNode(node.id, node);
+      });
+    });
+  }
+
   constructor(
     private jobService: JobService,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     public elementRef: ElementRef,
+    private metricService: MetricsService,
     private cdr: ChangeDetectorRef
   ) {}
 
@@ -82,11 +124,10 @@ export class JobOverviewComponent implements OnInit, OnDestroy {
           this.links = data.plan.links;
           this.jobId = data.plan.jid;
           this.dagreComponent.flush(this.nodes, this.links, true).then();
+          this.refreshNodesWithWatermarks();
         } else {
           this.nodes = data.plan.nodes;
-          this.nodes.forEach(node => {
-            this.dagreComponent.updateNode(node.id, node);
-          });
+          this.refreshNodesWithWatermarks();
         }
         this.cdr.markForCheck();
       });
