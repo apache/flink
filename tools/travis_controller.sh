d48067bb1882c3515cfbcd6c0b2d502ebd22dbd3
Stephan Ewen <sewen@apache.org>
2019-07-30 12:00:07 +0200

    [FLINK-13499][maprfs] Handle MapR dependency purely through reflection
    This allows us to remove the MapR dependency from the module.
    The MapR maven dependency has frequently caused issues.

--- tools/travis_controller.sh
+++ tools/travis_controller.sh
@@ -90,8 +90,7 @@ EXIT_CODE=0
 
 # Run actual compile&test steps
 if [ $STAGE == "$STAGE_COMPILE" ]; then
-    # We use -Punsafe-mapr-repo since the https version fails on Travis for some reason.
-	MVN="mvn clean install -nsu -Punsafe-mapr-repo -Dflink.convergence.phase=install -Pcheck-convergence -Dflink.forkCount=2 -Dflink.forkCountTestPackage=2 -Dmaven.javadoc.skip=true -B -DskipTests $PROFILE"
+	MVN="mvn clean install -nsu -Dflink.convergence.phase=install -Pcheck-convergence -Dflink.forkCount=2 -Dflink.forkCountTestPackage=2 -Dmaven.javadoc.skip=true -B -DskipTests $PROFILE"
 	$MVN
 	EXIT_CODE=$?
 
