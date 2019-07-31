a7345a9c435a4afbac09b639764c316ec66e1d4e
godfreyhe <godfreyhe@163.com>
2019-07-31 09:46:43 +0800

    [FLINK-13502] [table-planner-blink] move TreeNode to planner.expressions

--- flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/expressions/TreeNode.scala
+++ flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/expressions/TreeNode.scala
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.flink.table.planner.plan
+package org.apache.flink.table.planner.expressions
 
 import org.apache.flink.table.planner.typeutils.TypeInfoCheckUtils
 
