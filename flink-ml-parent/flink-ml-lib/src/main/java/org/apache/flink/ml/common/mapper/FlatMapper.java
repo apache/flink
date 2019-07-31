e26ef3f8a8ef75aca709b1c3761f9c5b9d664aa5
xuyang1706 <xuyang1706@gmail.com>
2019-07-31 21:12:58 +0800

    [FLINK-13513][ml] Add the FlatMapper and related classes for later algorithm implementations.

---
+++ flink-ml-parent/flink-ml-lib/src/main/java/org/apache/flink/ml/common/mapper/FlatMapper.java
@@ -0,0 +1,122 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.flink.ml.common.mapper;
+
+import org.apache.flink.ml.api.misc.param.Params;
+import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.types.Row;
+import org.apache.flink.util.Collector;
+
+import java.io.Serializable;
+
+/**
+ * Abstract class for flatMappers.
+ * FlatMapper transform one Row type data into zero, one, or more Row type result data.
+ * Operations that produce multiple strictly one Row type result data per Row type data
+ * can also use the {@link Mapper}.
+ */
+public abstract class FlatMapper implements Serializable {
+
+    /**
+     * schema of the input.
+     */
+	protected TableSchema dataSchema;
+
+    /**
+     * params used for FlatMapper.
+     * User can set the params before the FlatMapper is executed.
+     */
+	protected Params params;
+
+	public FlatMapper(TableSchema dataSchema, Params params) {
+		this.dataSchema = dataSchema;
+		this.params = (null == params) ? new Params() : params.clone();
+	}
+
+	/**
+	 * The core method of the FlatMapper. Takes an element from the input data set and transforms
+	 * it into zero, one, or more elements.
+	 *
+	 * @param row    The input row.
+	 * @param output The collector for returning result values.
+	 * @throws Exception This method may throw exceptions. Throwing an exception will cause the operation
+	 * to fail.
+	 */
+	public abstract void flatMap(Row row, Collector<Row> output) throws Exception;
+
+    /**
+	 * Wrapper method for the iterable input.
+	 * @param rows the input rows.
+	 * @param output the output collector
+	 * @throws Exception if {@link #flatMap(Row, Collector)} throws exception.
+	 */
+	public void flatMap(Iterable <Row> rows, Collector<Row> output) throws Exception {
+		for (Row row : rows) {
+			flatMap(row, output);
+		}
+	}
+
+	/**
+	 * Get the table schema(includs column names and types) of the calculation result.
+	 *
+	 * @return the table schema of output Row type data
+	 */
+	public abstract TableSchema getOutputSchema();
+
+	/**
+	 * Generate new instance of given FlatMapper class.
+	 *
+	 * @param flatMapperClassName Name of the FlatMapper class
+	 * @param dataSchema The schema of input Table type data.
+	 * @param params The parameters for the instance construction.
+	 * @return new instance of given FlatMapper class
+	 * @throws Exception if flatMapperClass is not the class of {@link FlatMapper}
+	 */
+	public static FlatMapper of(
+		String flatMapperClassName,
+		TableSchema dataSchema,
+		Params params) throws Exception {
+
+		return of(Class.forName(flatMapperClassName), dataSchema, params);
+	}
+
+	/**
+	 * Generate new instance of given FlatMapper class.
+	 *
+	 * @param flatMapperClass FlatMapper class of the new instance
+	 * @param dataSchema The schema of input Table type data.
+	 * @param params the parameters for the instance construction.
+	 * @return new instance of given FlatMapper class
+	 * @throws Exception if flatMapperClass is not the class of {@link FlatMapper}
+	 */
+	public static FlatMapper of(
+		Class flatMapperClass,
+		TableSchema dataSchema,
+		Params params) throws Exception {
+
+		if (FlatMapper.class.isAssignableFrom(flatMapperClass)) {
+			return (FlatMapper) flatMapperClass.getConstructor(TableSchema.class, Params.class)
+				.newInstance(dataSchema, params);
+		} else {
+			throw new IllegalArgumentException(
+				"FlatMapper is not assignable from this class : " + flatMapperClass.getCanonicalName());
+		}
+	}
+}
---
+++ flink-ml-parent/flink-ml-lib/src/main/java/org/apache/flink/ml/common/mapper/Mapper.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.flink.ml.common.mapper;
+
+import org.apache.flink.ml.api.misc.param.Params;
+import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.types.Row;
+import org.apache.flink.util.Collector;
+
+/**
+ * Abstract class for mappers.
+ * Mapper transform one Row type data into one Row type result data.
+ * Operations that produce zero, one or more Row type result data per Row type data
+ * can also use the {@link FlatMapper}.
+ */
+public abstract class Mapper extends FlatMapper implements MapOpInterface {
+
+	public Mapper(TableSchema dataSchema, Params params) {
+		super(dataSchema, params);
+	}
+
+	/**
+	 * Wrapper method for the {@link FlatMapper}.
+	 * @param row the input row.
+	 * @param output the output collector
+	 * @throws Exception if {@link Collector#collect(Object)} throws exception.
+	 */
+	@Override
+	public void flatMap(Row row, Collector<Row> output) throws Exception {
+		output.collect(map(row));
+	}
+
+}
