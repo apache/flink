30d430116e3dc330a170d2754e0396580446a45d
beyond1920 <beyond1920@126.com>
2019-08-01 12:09:29 +0800

    [Blink-13503][API] Add contract in `LookupableTableSource` to specify the behavior when lookupKeys contains null.

--- flink-table/flink-table-common/src/main/java/org/apache/flink/table/sources/LookupableTableSource.java
+++ flink-table/flink-table-common/src/main/java/org/apache/flink/table/sources/LookupableTableSource.java
@@ -33,13 +33,33 @@ import org.apache.flink.table.functions.TableFunction;
 public interface LookupableTableSource<T> extends TableSource<T> {
 
 	/**
-	 * Gets the {@link TableFunction} which supports lookup one key at a time.
+	 * Gets the {@link TableFunction} which supports lookup one key at a time. Calling `eval`
+	 * method in the returned {@link TableFunction} means send a lookup request to the TableSource.
+	 *
+	 * <p><strong>IMPORTANT:</strong>
+	 * Lookup keys in a request may contain null value. When it happens, it expects to lookup
+	 * records with null value on the lookup key field.
+	 * E.g., for a MySQL table with the following schema, send a lookup request with null value
+	 * on `age` field means to find students whose age are unknown (CAUTION: It is equivalent to filter condition:
+	 * `WHERE age IS NULL` instead of `WHERE age = null`).
+	 *
+	 * -----------------
+	 *  Table : Student
+	 * -----------------
+	 * id    |   LONG
+	 * age   |   INT
+	 * name  |   STRING
+	 * -----------------
+	 * For the external system which does not support null value (E.g, HBase does not support null value on rowKey),
+	 * it could throw an exception or discard the request when receiving a request with null value on lookup key.
+	 *
 	 * @param lookupKeys the chosen field names as lookup keys, it is in the defined order
 	 */
 	TableFunction<T> getLookupFunction(String[] lookupKeys);
 
 	/**
 	 * Gets the {@link AsyncTableFunction} which supports async lookup one key at a time.
+	 *
 	 * @param lookupKeys the chosen field names as lookup keys, it is in the defined order
 	 */
 	AsyncTableFunction<T> getAsyncLookupFunction(String[] lookupKeys);
