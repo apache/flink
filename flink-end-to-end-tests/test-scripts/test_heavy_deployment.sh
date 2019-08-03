caf6867dcd541dc4f5a95d13fba7ae69bc0936a2
kevin.cyj <kevin.cyj@alibaba-inc.com>
2019-08-03 10:00:33 +0800

    Addjust config options of heavy deployment e2e test to fix cluster start failure and decrease memory pressure.

--- flink-end-to-end-tests/test-scripts/test_heavy_deployment.sh
+++ flink-end-to-end-tests/test-scripts/test_heavy_deployment.sh
@@ -25,7 +25,10 @@ TEST=flink-heavy-deployment-stress-test
 TEST_PROGRAM_NAME=HeavyDeploymentStressTestProgram
 TEST_PROGRAM_JAR=${END_TO_END_DIR}/$TEST/target/$TEST_PROGRAM_NAME.jar
 
-set_config_key "taskmanager.heap.size" "512m" # 512Mb x 10TMs = 5Gb total heap
+set_config_key "containerized.heap-cutoff-min" "100"
+
+set_config_key "jobmanager.heap.size" "2048m"
+set_config_key "taskmanager.heap.size" "1024m" # 1024Mb x 10TMs = 10Gb total heap
 
 set_config_key "taskmanager.memory.size" "8" # 8Mb
 set_config_key "taskmanager.network.memory.min" "8mb"
