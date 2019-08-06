e54e1c94d6397dac31e0484eeb6112b52ea0ea3c
Dawid Wysakowicz <dwysakowicz@apache.org>
2019-08-06 09:40:42 +0200

    [hotfix][e2e] Include dependency tests in travis splits

--- tools/travis/splits/split_misc_hadoopfree.sh
+++ tools/travis/splits/split_misc_hadoopfree.sh
@@ -67,5 +67,7 @@ run_test "Avro Confluent Schema Registry nightly end-to-end test" "$END_TO_END_D
 run_test "State TTL Heap backend end-to-end test" "$END_TO_END_DIR/test-scripts/test_stream_state_ttl.sh file"
 run_test "State TTL RocksDb backend end-to-end test" "$END_TO_END_DIR/test-scripts/test_stream_state_ttl.sh rocks"
 
+run_test "Dependency shading of table modules test" "$END_TO_END_DIR/test-scripts/test_table_shaded_dependencies.sh"
+
 printf "\n[PASS] All tests passed\n"
 exit 0
