83ca1d7bbc5d977e871fd2df61cf8c602c3ba9ea
zhangjun <jun.zhang1@ly.com>
2019-08-22 14:55:54 +0800

    rollback to master

--- flink-connectors/flink-connector-filesystem/src/test/java/streaming/connectors/filesystem/BucketTest.java
+++ flink-connectors/flink-connector-filesystem/src/test/java/streaming/connectors/filesystem/BucketTest.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package streaming.connectors.filesystem;
 
 import org.apache.flink.streaming.connectors.fs.table.descriptors.Bucket;
@@ -20,7 +37,7 @@ public class BucketTest extends DescriptorTestBase {
 	protected List<Descriptor> descriptors() {
 		final Descriptor fileRawDesc =
 			new Bucket().basePath("file:///tmp/flink-data/json")
-				.rawFormat()
+				.rowFormat()
 				.dateFormat("yyyy-MM-dd-HHmm");
 
 		final Descriptor fileBultDesc =
@@ -38,7 +55,7 @@ public class BucketTest extends DescriptorTestBase {
 		fileRawDesc.put("connector.type", "bucket");
 		fileRawDesc.put("connector.basepath", "file:///tmp/flink-data/json");
 		fileRawDesc.put("connector.date.format", "yyyy-MM-dd-HHmm");
-		fileRawDesc.put("connector.format.type", "raw");
+		fileRawDesc.put("connector.format.type", "row");
 
 		final Map<String, String> fileBultDesc = new HashMap<>();
 		fileBultDesc.put("connector.property-version", "1");
