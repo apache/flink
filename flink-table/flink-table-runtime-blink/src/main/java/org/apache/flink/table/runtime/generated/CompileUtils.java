9d03fbdf7524af6de0e0e99386fff1c1f135d3bf
jrthe42 <jrthe42@gmail.com>
2019-08-23 15:41:16 +0800

    [FLINK-13823][table-planner-blink] Fix incorrect debug log in CompileUtils
    This closes #9518

--- flink-table/flink-table-runtime-blink/src/main/java/org/apache/flink/table/runtime/generated/CompileUtils.java
+++ flink-table/flink-table-runtime-blink/src/main/java/org/apache/flink/table/runtime/generated/CompileUtils.java
@@ -71,7 +71,7 @@ public final class CompileUtils {
 
 	private static <T> Class<T> doCompile(ClassLoader cl, String name, String code) {
 		checkNotNull(cl, "Classloader must not be null.");
-		CODE_LOG.debug("Compiling: %s \n\n Code:\n%s", name, code);
+		CODE_LOG.debug("Compiling: {} \n\n Code:\n{}", name, code);
 		SimpleCompiler compiler = new SimpleCompiler();
 		compiler.setParentClassLoader(cl);
 		try {
