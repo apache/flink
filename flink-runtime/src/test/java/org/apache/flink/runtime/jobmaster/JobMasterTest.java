0e75425e61274ba1591172021868678dee357bb5
ifndef-SleePy <mmyy1110@gmail.com>
2019-08-30 17:21:28 +0800

    [Flink-12164][tests] Harden JobMasterTest#testJobFailureWhenTaskExecutorHeartbeatTimeout
    This closes #9568.

--- flink-runtime/src/test/java/org/apache/flink/runtime/jobmaster/JobMasterTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/jobmaster/JobMasterTest.java
@@ -62,6 +62,7 @@ import org.apache.flink.runtime.executiongraph.ArchivedExecutionGraph;
 import org.apache.flink.runtime.executiongraph.ExecutionAttemptID;
 import org.apache.flink.runtime.executiongraph.failover.FailoverStrategyLoader;
 import org.apache.flink.runtime.heartbeat.HeartbeatServices;
+import org.apache.flink.runtime.heartbeat.TestingHeartbeatServices;
 import org.apache.flink.runtime.highavailability.HighAvailabilityServices;
 import org.apache.flink.runtime.highavailability.TestingHighAvailabilityServices;
 import org.apache.flink.runtime.instance.SimpleSlotContext;
@@ -220,6 +221,8 @@ public class JobMasterTest extends TestLogger {
 
 	private static HeartbeatServices heartbeatServices;
 
+	private static TestingHeartbeatServices testingHeartbeatService;
+
 	private Configuration configuration;
 
 	private ResourceID jmResourceId;
@@ -238,6 +241,7 @@ public class JobMasterTest extends TestLogger {
 
 		fastHeartbeatServices = new HeartbeatServices(fastHeartbeatInterval, fastHeartbeatTimeout);
 		heartbeatServices = new HeartbeatServices(heartbeatInterval, heartbeatTimeout);
+		testingHeartbeatService = new TestingHeartbeatServices(heartbeatInterval, heartbeatTimeout);
 	}
 
 	@Before
@@ -1969,14 +1973,14 @@ public class JobMasterTest extends TestLogger {
 
 	@Test
 	public void testJobFailureWhenTaskExecutorHeartbeatTimeout() throws Exception {
-		final AtomicBoolean respondToHeartbeats = new AtomicBoolean(true);
 		runJobFailureWhenTaskExecutorTerminatesTest(
-			fastHeartbeatServices,
-			(localTaskManagerLocation, jobMasterGateway) -> respondToHeartbeats.set(false),
+			testingHeartbeatService,
+			(localTaskManagerLocation, jobMasterGateway) ->
+				testingHeartbeatService.triggerHeartbeatTimeout(
+					jmResourceId,
+					localTaskManagerLocation.getResourceID()),
 			(jobMasterGateway, taskManagerResourceId) -> (resourceId, ignored) -> {
-				if (respondToHeartbeats.get()) {
-					jobMasterGateway.heartbeatFromTaskManager(taskManagerResourceId, new AccumulatorReport(Collections.emptyList()));
-				}
+				jobMasterGateway.heartbeatFromTaskManager(taskManagerResourceId, new AccumulatorReport(Collections.emptyList()));
 			}
 		);
 	}
