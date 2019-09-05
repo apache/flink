b3e83a280ffb932ac790b189319c18fce0053776
Kostas Kloudas <kkloudas@gmail.com>
2019-09-05 11:26:10 +0200

    [FLINK-13972] Rename the PreviewPlanEnv

--- flink-tests/src/test/java/org/apache/flink/test/util/PlanExposingEnvironment.java
+++ flink-tests/src/test/java/org/apache/flink/test/util/PlanExposingEnvironment.java
@@ -27,7 +27,7 @@ import org.apache.flink.client.program.OptimizerPlanEnvironment;
 /**
  * Environment to extract the pre-optimized plan.
  */
-public final class PreviewPlanEnvironment extends ExecutionEnvironment {
+public final class PlanExposingEnvironment extends ExecutionEnvironment {
 
 	private Plan plan;
 
@@ -48,7 +48,7 @@ public final class PreviewPlanEnvironment extends ExecutionEnvironment {
 		ExecutionEnvironmentFactory factory = new ExecutionEnvironmentFactory() {
 			@Override
 			public ExecutionEnvironment createExecutionEnvironment() {
-				return PreviewPlanEnvironment.this;
+				return PlanExposingEnvironment.this;
 			}
 		};
 		initializeContextEnvironment(factory);
