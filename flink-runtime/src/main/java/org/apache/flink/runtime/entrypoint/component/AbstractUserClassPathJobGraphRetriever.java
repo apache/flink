e66e9e6d82b71ee0417f408205e992bc86e7065e
GuoWei Ma <guowei.mgw@gmail.com>
2019-09-09 14:26:39 +0800

    [FLINK-14464] Introduce the AbstractUserClassPathJobGraphRetriever This abstract class is for the JobGraphRetriever, which wants to use the user's classpath.

---
+++ flink-runtime/src/main/java/org/apache/flink/runtime/entrypoint/component/AbstractUserClassPathJobGraphRetriever.java
@@ -0,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.entrypoint.component;
+
+import org.apache.flink.core.fs.Path;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *  Abstract class for the JobGraphRetriever, which wants to get classpath user's code depends on.
+ */
+
+public abstract class AbstractUserClassPathJobGraphRetriever implements JobGraphRetriever {
+
+	protected static final Logger LOG = LoggerFactory.getLogger(AbstractUserClassPathJobGraphRetriever.class);
+
+	public static final String DEFAULT_JOB_DIR = "job";
+
+	/** The directory contains all the jars, which user code depends on. */
+	@Nullable
+	private final String jobDir;
+
+	private List<URL> userClassPaths;
+
+	public AbstractUserClassPathJobGraphRetriever(String jobDir) {
+		this.jobDir = jobDir;
+	}
+
+	public List<URL> getUserClassPaths() throws IOException {
+		if (userClassPaths == null) {
+			userClassPaths = scanJarsInJobClassDir(jobDir);
+		}
+		return userClassPaths;
+	}
+
+	private List<URL> scanJarsInJobClassDir(String dir) throws IOException {
+
+		if (dir == null) {
+			return Collections.emptyList();
+		}
+
+		final File dirFile = new File(new Path(dir).toString());
+		final List<URL> jarURLs = new LinkedList<>();
+
+		if (!dirFile.exists()) {
+			LOG.warn("the job dir " + dirFile + " dose not exists.");
+			return Collections.emptyList();
+		}
+		if (!dirFile.isDirectory()) {
+			LOG.warn("the job dir " + dirFile + " is not a directory.");
+			return Collections.emptyList();
+		}
+
+		Files.walkFileTree(dirFile.toPath(),
+			EnumSet.of(FileVisitOption.FOLLOW_LINKS),
+			Integer.MAX_VALUE,
+			new SimpleFileVisitor<java.nio.file.Path>(){
+
+			@Override
+			public FileVisitResult visitFile(java.nio.file.Path file, BasicFileAttributes attrs)
+				throws IOException {
+				FileVisitResult fileVisitResult = super.visitFile(file, attrs);
+				if (file.getFileName().toString().endsWith(".jar")) {
+					LOG.info("add " + file.toString() + " to user classpath");
+					if (file.isAbsolute()) {
+						jarURLs.add(file.toUri().toURL());
+					} else {
+						jarURLs.add(
+							new URL(new URL(file.getFileName().toUri().getScheme() + ":"), file.toString())
+						);
+					}
+				}
+				return fileVisitResult;
+			}
+		});
+
+		if (jarURLs.isEmpty()) {
+			return Collections.emptyList();
+		} else {
+				return jarURLs;
+		}
+	}
+}
---
+++ flink-runtime/src/test/java/org/apache/flink/runtime/entrypoint/component/AbstractUserClassPathJobGraphRetrieverTest.java
@@ -0,0 +1,148 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.entrypoint.component;
+
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.jobgraph.JobGraph;
+import org.apache.flink.util.TestLogger;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests for the {@link AbstractUserClassPathJobGraphRetriever}.
+ */
+public class AbstractUserClassPathJobGraphRetrieverTest extends TestLogger {
+
+	@Rule
+	public final TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+	/**
+	 * Test class.
+	 */
+	public static class TestJobGraphRetriever extends AbstractUserClassPathJobGraphRetriever {
+
+		public TestJobGraphRetriever(String jobDir) {
+			super(jobDir);
+		}
+
+		@Override
+		public JobGraph retrieveJobGraph(Configuration configuration) {
+			return null;
+		}
+	}
+
+	@Test
+	public void testGetUserClassPathFromAFile() throws IOException {
+		final String fileName = "a.jar";
+		File file = temporaryFolder.newFile(fileName);
+
+		TestJobGraphRetriever testJobGraphRetriever = new TestJobGraphRetriever(file.getAbsolutePath());
+
+		assertEquals(Collections.emptyList(), testJobGraphRetriever.getUserClassPaths());
+	}
+
+	@Test
+	public void testGetUserClassPathFormADoesNotExistsFile() throws IOException {
+		final String fileName = "_does_not_exists_file";
+		final String doesNotExistsFilePath = temporaryFolder.getRoot() + "/" + fileName;
+
+		TestJobGraphRetriever testJobGraphRetriever = new TestJobGraphRetriever(doesNotExistsFilePath);
+
+		assertEquals(Collections.emptyList(), testJobGraphRetriever.getUserClassPaths());
+
+	}
+
+	@Test
+	public void testGetUserClassPath() throws IOException {
+
+		final Path jobDir = temporaryFolder.newFolder("_job_dir").toPath();
+		final Path jobSubDir1 = Files.createDirectory(jobDir.resolve("_sub_dir1"));
+		final Path jobSubDir2 = Files.createDirectory(jobDir.resolve("_sub_dir2"));
+		final Path jarFile1 = Files.createFile(jobDir.resolve("file1.jar"));
+		final Path jarFile2 = Files.createFile(jobDir.resolve("file2.jar"));
+		final Path jarFile3 = Files.createFile(jobSubDir1.resolve("file3.jar"));
+		final Path jarFile4 = Files.createFile(jobSubDir2.resolve("file4.jar"));
+
+		Files.createFile(jobDir.resolve("file1.txt"));
+		Files.createFile(jobSubDir2.resolve("file2.txt"));
+
+		List<URL> expectedUrls = Arrays.asList(jarFile1.toUri().toURL(),
+			jarFile2.toUri().toURL(),
+			jarFile3.toUri().toURL(),
+			jarFile4.toUri().toURL());
+
+		TestJobGraphRetriever testJobGraphRetriever =
+			new TestJobGraphRetriever(jobDir.toString());
+
+		assertTrue(CollectionUtils.isEqualCollection(expectedUrls, testJobGraphRetriever.getUserClassPaths()));
+	}
+
+	@Test
+	public void testGetUserClassPathWithRelativeJobDir() throws IOException {
+		Path jobRelativeDir = Paths.get("_job_dir");
+		Path jobRelativeSubDir = Paths.get("_job_dir", "_sub_dir");
+		Path jarFile1 = Paths.get(jobRelativeDir.toString(), "file1.jar");
+		Path jarFile2 = Paths.get(jobRelativeSubDir.toString(), "file2.jar");
+
+		URL context = new URL(jobRelativeDir.toUri().getScheme() + ":");
+		List<URL> expectedURLs = Arrays.asList(
+			new URL(context, jarFile1.toString()), new URL(context, jarFile2.toString()));
+
+		try {
+			if (!Files.exists(jobRelativeDir)) {
+				Files.createDirectory(jobRelativeDir);
+			}
+			if (!Files.exists(jobRelativeSubDir)) {
+				Files.createDirectory(jobRelativeSubDir);
+			}
+			if (!Files.exists(jarFile1)) {
+				Files.createFile(jarFile1);
+			}
+			if (!Files.exists(jarFile2)) {
+				Files.createFile(jarFile2);
+			}
+
+			TestJobGraphRetriever testJobGraphRetriever =
+				new TestJobGraphRetriever(jobRelativeDir.toString());
+			assertTrue(CollectionUtils.isEqualCollection(expectedURLs, testJobGraphRetriever.getUserClassPaths()));
+		} finally {
+			Files.deleteIfExists(jarFile1);
+			Files.deleteIfExists(jarFile2);
+			Files.deleteIfExists(jobRelativeSubDir);
+			Files.deleteIfExists(jobRelativeDir);
+		}
+	}
+
+}
