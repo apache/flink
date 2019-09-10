94215849e6feb86c731a8ac931d750645ab446ef
GatsbyNewton <wangzmking@163.com>
2019-09-10 22:44:26 +0800

    [FLINK-14031][examples][table]Added the blink planner dependency and examples that use blink planner to flink-examples-table

--- flink-examples/flink-examples-table/src/main/java/org/apache/flink/table/examples/java/BlinkStreamSQL.java
+++ flink-examples/flink-examples-table/src/main/java/org/apache/flink/table/examples/java/BlinkStreamSQL.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.table.examples.java;
 
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -8,10 +26,19 @@ import org.apache.flink.table.api.java.StreamTableEnvironment;
 
 import java.util.Arrays;
 
+/**
+ * Simple example for demonstrating the use of SQL on a Stream Table with blink planner in Java.
+ *
+ * <p>This example shows how to:
+ *  - Convert DataStreams to Tables
+ *  - Register a Table under a name
+ *  - Run a StreamSQL query on the registered Table
+ */
 public class BlinkStreamSQL {
 
 	public static void main(String[] args)throws Exception{
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		// set blink planner in streaming mode
 		EnvironmentSettings settings = EnvironmentSettings.newInstance()
 			.useBlinkPlanner()
 			.inStreamingMode()
