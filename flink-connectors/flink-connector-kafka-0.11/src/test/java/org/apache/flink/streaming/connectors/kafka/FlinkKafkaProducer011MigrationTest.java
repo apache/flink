d56a6ff167bccb8797766ee6419c3516c1bc37c5
yanghua <yanghua1127@gmail.com>
2019-09-10 21:40:30 +0800

    [FLINK-13619] Update FlinkKafkaProducerMigrationOperatorTest to restore from 1.9 savepoint

--- flink-connectors/flink-connector-kafka-0.11/src/test/java/org/apache/flink/streaming/connectors/kafka/FlinkKafkaProducer011MigrationTest.java
+++ flink-connectors/flink-connector-kafka-0.11/src/test/java/org/apache/flink/streaming/connectors/kafka/FlinkKafkaProducer011MigrationTest.java
@@ -44,6 +44,7 @@ public class FlinkKafkaProducer011MigrationTest extends KafkaMigrationTestBase {
 	@Parameterized.Parameters(name = "Migration Savepoint: {0}")
 	public static Collection<MigrationVersion> parameters() {
 		return Arrays.asList(
+			MigrationVersion.v1_8,
 			MigrationVersion.v1_9);
 	}
 
