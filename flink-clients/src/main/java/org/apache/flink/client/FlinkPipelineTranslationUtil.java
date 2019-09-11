bc92dfda438d11963778a4f739937490a193b971
Aljoscha Krettek <aljoscha@apache.org>
2019-09-11 17:37:33 +0200

    [FLINK-14290] Add Pipeline translation utils for getting a JobGraph from a FlinkPipeline
    For now, we can only translate DataSet Plans, in the future we also need
    this for DataStream StreamGraps.

---
+++ flink-clients/src/main/java/org/apache/flink/client/FlinkPipelineTranslationUtil.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.flink.client;
+
+import org.apache.flink.api.dag.Pipeline;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.jobgraph.JobGraph;
+
+/**
+ * Utility for transforming {@link Pipeline FlinkPipelines} into a {@link JobGraph}. This uses
+ * reflection or service discovery to find the right {@link FlinkPipelineTranslator} for a given
+ * subclass of {@link Pipeline}.
+ */
+public final class FlinkPipelineTranslationUtil {
+
+	/**
+	 * Transmogrifies the given {@link Pipeline} to a {@link JobGraph}.
+	 */
+	public static JobGraph getJobGraph(
+			Pipeline pipeline,
+			Configuration optimizerConfiguration,
+			int defaultParallelism) {
+
+		PlanTranslator planToJobGraphTransmogrifier = new PlanTranslator();
+
+		if (planToJobGraphTransmogrifier.canTranslate(pipeline)) {
+			// we have a DataSet program
+			return planToJobGraphTransmogrifier.translate(pipeline,
+					optimizerConfiguration,
+					defaultParallelism);
+		}
+
+		throw new RuntimeException("Cannot find transmogrifier for given pipeline: " + pipeline);
+	}
+}
