d65968bfc232469cd7d45b551b7279c9c4ac5f10
Till Rohrmann <trohrmann@apache.org>
2019-09-14 18:14:58 +0200

    [FLINK-11843] Add DispatcherRunnerImplNGFactory implementation

---
+++ flink-runtime/src/main/java/org/apache/flink/runtime/dispatcher/runner/DispatcherServiceImpl.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.dispatcher.runner;
+
+import org.apache.flink.runtime.dispatcher.Dispatcher;
+import org.apache.flink.runtime.dispatcher.DispatcherGateway;
+
+import java.util.concurrent.CompletableFuture;
+
+class DispatcherServiceImpl implements DispatcherLeaderProcessImpl.DispatcherService {
+
+	private final Dispatcher dispatcher;
+	private final DispatcherGateway dispatcherGateway;
+
+	private DispatcherServiceImpl(Dispatcher dispatcher) {
+		this.dispatcher = dispatcher;
+		this.dispatcherGateway = dispatcher.getSelfGateway(DispatcherGateway.class);
+	}
+
+	@Override
+	public DispatcherGateway getGateway() {
+		return dispatcherGateway;
+	}
+
+	@Override
+	public CompletableFuture<Void> closeAsync() {
+		return dispatcher.closeAsync();
+	}
+
+	public static DispatcherServiceImpl from(Dispatcher dispatcher) {
+		return new DispatcherServiceImpl(dispatcher);
+	}
+}
