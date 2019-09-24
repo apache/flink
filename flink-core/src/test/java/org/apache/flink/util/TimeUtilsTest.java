95c46083a6afaa0a71de01efd1664252a6f0cf7b
zhuzhu.zz <zhuzhu.zz@alibaba-inc.com>
2019-09-24 16:11:45 +0800

    [FLINK-14182][core] Make TimeUtils able to parse duration string with plural form labels

--- flink-core/src/test/java/org/apache/flink/util/TimeUtilsTest.java
+++ flink-core/src/test/java/org/apache/flink/util/TimeUtilsTest.java
@@ -32,7 +32,9 @@ public class TimeUtilsTest {
 	public void testParseDurationNanos() {
 		assertEquals(424562, TimeUtils.parseDuration("424562ns").getNano());
 		assertEquals(424562, TimeUtils.parseDuration("424562nano").getNano());
+		assertEquals(424562, TimeUtils.parseDuration("424562nanos").getNano());
 		assertEquals(424562, TimeUtils.parseDuration("424562nanosecond").getNano());
+		assertEquals(424562, TimeUtils.parseDuration("424562nanoseconds").getNano());
 		assertEquals(424562, TimeUtils.parseDuration("424562 ns").getNano());
 	}
 
@@ -40,7 +42,9 @@ public class TimeUtilsTest {
 	public void testParseDurationMicros() {
 		assertEquals(565731 * 1000L, TimeUtils.parseDuration("565731µs").getNano());
 		assertEquals(565731 * 1000L, TimeUtils.parseDuration("565731micro").getNano());
+		assertEquals(565731 * 1000L, TimeUtils.parseDuration("565731micros").getNano());
 		assertEquals(565731 * 1000L, TimeUtils.parseDuration("565731microsecond").getNano());
+		assertEquals(565731 * 1000L, TimeUtils.parseDuration("565731microseconds").getNano());
 		assertEquals(565731 * 1000L, TimeUtils.parseDuration("565731 µs").getNano());
 	}
 
@@ -49,14 +53,19 @@ public class TimeUtilsTest {
 		assertEquals(1234, TimeUtils.parseDuration("1234").toMillis());
 		assertEquals(1234, TimeUtils.parseDuration("1234ms").toMillis());
 		assertEquals(1234, TimeUtils.parseDuration("1234milli").toMillis());
+		assertEquals(1234, TimeUtils.parseDuration("1234millis").toMillis());
 		assertEquals(1234, TimeUtils.parseDuration("1234millisecond").toMillis());
+		assertEquals(1234, TimeUtils.parseDuration("1234milliseconds").toMillis());
 		assertEquals(1234, TimeUtils.parseDuration("1234 ms").toMillis());
 	}
 
 	@Test
 	public void testParseDurationSeconds() {
 		assertEquals(667766, TimeUtils.parseDuration("667766s").getSeconds());
+		assertEquals(667766, TimeUtils.parseDuration("667766sec").getSeconds());
+		assertEquals(667766, TimeUtils.parseDuration("667766secs").getSeconds());
 		assertEquals(667766, TimeUtils.parseDuration("667766second").getSeconds());
+		assertEquals(667766, TimeUtils.parseDuration("667766seconds").getSeconds());
 		assertEquals(667766, TimeUtils.parseDuration("667766 s").getSeconds());
 	}
 
@@ -64,6 +73,7 @@ public class TimeUtilsTest {
 	public void testParseDurationMinutes() {
 		assertEquals(7657623, TimeUtils.parseDuration("7657623min").toMinutes());
 		assertEquals(7657623, TimeUtils.parseDuration("7657623minute").toMinutes());
+		assertEquals(7657623, TimeUtils.parseDuration("7657623minutes").toMinutes());
 		assertEquals(7657623, TimeUtils.parseDuration("7657623 min").toMinutes());
 	}
 
@@ -71,6 +81,7 @@ public class TimeUtilsTest {
 	public void testParseDurationHours() {
 		assertEquals(987654, TimeUtils.parseDuration("987654h").toHours());
 		assertEquals(987654, TimeUtils.parseDuration("987654hour").toHours());
+		assertEquals(987654, TimeUtils.parseDuration("987654hours").toHours());
 		assertEquals(987654, TimeUtils.parseDuration("987654 h").toHours());
 	}
 
@@ -78,6 +89,7 @@ public class TimeUtilsTest {
 	public void testParseDurationDays() {
 		assertEquals(987654, TimeUtils.parseDuration("987654d").toDays());
 		assertEquals(987654, TimeUtils.parseDuration("987654day").toDays());
+		assertEquals(987654, TimeUtils.parseDuration("987654days").toDays());
 		assertEquals(987654, TimeUtils.parseDuration("987654 d").toDays());
 	}
 
