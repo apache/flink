9d224102829be248749f45985dad357c42d521fc
Xintong Song <tonysong820@gmail.com>
2019-10-09 15:03:46 +0800

    [hotfix] UNKNOWN ResourceSpec should not be numerically compared.

--- flink-core/src/main/java/org/apache/flink/api/common/operators/util/OperatorValidationUtils.java
+++ flink-core/src/main/java/org/apache/flink/api/common/operators/util/OperatorValidationUtils.java
@@ -69,7 +69,11 @@ public class OperatorValidationUtils {
 	public static void validateMinAndPreferredResources(ResourceSpec minResources, ResourceSpec preferredResources) {
 		Preconditions.checkNotNull(minResources, "The min resources must be not null.");
 		Preconditions.checkNotNull(preferredResources, "The preferred resources must be not null.");
-		Preconditions.checkArgument(minResources.isValid() && preferredResources.isValid() && minResources.lessThanOrEqual(preferredResources),
-			"The values in resources must be not less than 0 and the preferred resources must be greater than the min resources.");
+		Preconditions.checkArgument(minResources.isValid() && preferredResources.isValid(),
+			"The resources must either be UNKNOWN or all the fields are no less than 0.");
+		Preconditions.checkArgument((minResources == ResourceSpec.UNKNOWN && preferredResources == ResourceSpec.UNKNOWN) ||
+				(minResources != ResourceSpec.UNKNOWN && preferredResources != ResourceSpec.UNKNOWN && minResources.lessThanOrEqual(preferredResources)),
+			"The resources must be either both UNKNOWN or both not UNKNOWN. If not UNKNOWN,"
+				+ " the preferred resources must be greater than or equal to the min resources.");
 	}
 }
