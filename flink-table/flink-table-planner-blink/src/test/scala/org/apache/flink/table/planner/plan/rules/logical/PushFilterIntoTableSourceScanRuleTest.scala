79e0e7c6cf258ceeaa3bccfeedcc986f2e2f1f23
Yuval Itzchakov <yuvalos@gmail.com>
2019-10-26 17:01:59 +0300

    Fix upper/lower keywords predicate pushdown

--- flink-table/flink-table-planner-blink/src/test/scala/org/apache/flink/table/planner/plan/rules/logical/PushFilterIntoTableSourceScanRuleTest.scala
+++ flink-table/flink-table-planner-blink/src/test/scala/org/apache/flink/table/planner/plan/rules/logical/PushFilterIntoTableSourceScanRuleTest.scala
@@ -20,10 +20,13 @@ package org.apache.flink.table.planner.plan.rules.logical
 import org.apache.flink.table.planner.expressions.utils.Func1
 import org.apache.flink.table.planner.plan.optimize.program.{FlinkBatchProgram, FlinkHepRuleSetProgramBuilder, HEP_RULES_EXECUTION_TYPE}
 import org.apache.flink.table.planner.utils.{TableConfigUtils, TableTestBase, TestFilterableTableSource}
-
 import org.apache.calcite.plan.hep.HepMatchOrder
 import org.apache.calcite.rel.rules.FilterProjectTransposeRule
 import org.apache.calcite.tools.RuleSets
+import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.api.java.typeutils.RowTypeInfo
+import org.apache.flink.table.api.Types
+import org.apache.flink.types.Row
 import org.junit.{Before, Test}
 
 /**
@@ -141,5 +144,36 @@ class PushFilterIntoTableSourceScanRuleTest extends TableTestBase {
     util.addFunction("myUdf", Func1)
     util.verifyPlan("SELECT * FROM MyTable WHERE amount > 2 AND myUdf(amount) < 32")
   }
+  @Test
+  def testLowerPushdown(): Unit = {
+    val rti =
+      new RowTypeInfo(Array[TypeInformation[_]](Types.STRING, Types.STRING), Array("a", "b"))
+    val rows = List(Row.of("foo", "bar"))
+    util.tableEnv
+      .registerTableSource(
+        "MTable",
+        TestFilterableTableSource(true, rti, rows, Set("a", "b"), Set("lower"))
+      )
 
+    util.verifyPlan("SELECT * FROM MTable WHERE LOWER(a) like 'foo'")
+  }
+
+  @Test
+  def testUpperPushdown(): Unit = {
+    val rti =
+      new RowTypeInfo(Array[TypeInformation[_]](Types.STRING, Types.STRING), Array("a", "b"))
+    val rows = List(Row.of("foo", "bar"))
+    util.tableEnv
+      .registerTableSource(
+        "MTable",
+        TestFilterableTableSource(true, rti, rows, Set("a", "b"), Set("upper"))
+      )
+
+    util.verifyPlan("SELECT * FROM MTable WHERE UPPER(a) like 'foo'")
+  }
+
+  @Test
+  def testGreatThanPushdown(): Unit = {
+    util.verifyPlan("SELECT * FROM MyTable WHERE a > 10")
+  }
 }
