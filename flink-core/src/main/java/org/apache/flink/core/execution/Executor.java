f074d1ff0d7115c64c966778cc08f0eae8db38e6
Kostas Kloudas <kkloudas@gmail.com>
2019-11-01 11:31:24 +0100

    [FLINK-14758] Add Executor-related interfaces + wire their discovery to environments.

---
+++ flink-core/src/main/java/org/apache/flink/core/execution/Executor.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.core.execution;
+
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.dag.Pipeline;
+import org.apache.flink.configuration.Configuration;
+
+/**
+ * The entity responsible for executing a {@link Pipeline}, i.e. a user job.
+ */
+public interface Executor {
+
+	/**
+	 * Executes a {@link Pipeline} based on the provided configuration.
+	 * @param pipeline the {@link Pipeline} to execute
+	 * @param configuration the {@link Configuration} with the required execution parameters
+	 * @return the {@link JobExecutionResult} corresponding to the pipeline execution.
+	 */
+	JobExecutionResult execute(Pipeline pipeline, Configuration configuration) throws Exception;
+}
