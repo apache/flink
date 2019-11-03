4386c2628468c4bcf303c6437abc2acfcf078c20
faaronzheng <faarozheng@gmail.com>
2019-11-03 20:48:32 +0800

    [FLINK-14560] [runtime] The value of taskmanager.memory.size in flink-conf.yaml is set to zero will cause taskmanager not to work

--- flink-runtime/src/main/java/org/apache/flink/runtime/util/ConfigurationParserUtils.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/util/ConfigurationParserUtils.java
@@ -42,8 +42,20 @@ public class ConfigurationParserUtils {
 	 * @return managed memory size (in megabytes)
 	 */
 	public static long getManagedMemorySize(Configuration configuration) {
-		return MemorySize.parse(
-			configuration.getString(TaskManagerOptions.LEGACY_MANAGED_MEMORY_SIZE), MEGA_BYTES).getMebiBytes();
+		long managedMemorySize;
+		try {
+			managedMemorySize = MemorySize.parse(
+				configuration.getString(TaskManagerOptions.LEGACY_MANAGED_MEMORY_SIZE), MEGA_BYTES).getMebiBytes();
+		} catch (IllegalArgumentException e) {
+			throw new IllegalConfigurationException("Could not read " + TaskManagerOptions.LEGACY_MANAGED_MEMORY_SIZE
+
+				.key(), e);
+		}
+		checkConfigParameter(managedMemorySize==Integer.parseInt(TaskManagerOptions.LEGACY_MANAGED_MEMORY_SIZE.defaultValue()) || managedMemorySize > 0,
+			managedMemorySize, TaskManagerOptions.LEGACY_MANAGED_MEMORY_SIZE.key(),
+			"MemoryManager needs at least one MB of memory. " +
+				"If you leave this config parameter empty, the system automatically pick a fraction of the available memory.");
+		return managedMemorySize;
 	}
 
 	/**
