fb22e17536f39a765fae52ff0a6bd4d7ebf12452
Wei Zhong <weizhong0618@gmail.com>
2019-11-07 17:31:29 +0800

    remove unused imports

--- flink-python/src/main/java/org/apache/flink/client/python/PythonEnvUtils.java
+++ flink-python/src/main/java/org/apache/flink/client/python/PythonEnvUtils.java
@@ -28,13 +28,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
