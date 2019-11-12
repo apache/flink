xintongsong/flink-1

[hotfix] Deduplicate setting operator resource / parallelism argument checks.

flink-core/src/main/java/org/apache/flink/api/dag/Transformation.java
@@ -19,9 +19,9 @@
 package org.apache.flink.api.dag;
 
 import org.apache.flink.annotation.Internal;
-import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.functions.InvalidTypesException;
 import org.apache.flink.api.common.operators.ResourceSpec;
+import org.apache.flink.api.common.operators.util.OperatorResourceParallelismUtils;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.typeutils.MissingTypeInfo;
 import org.apache.flink.util.Preconditions;
@@ -202,9 +202,7 @@ public abstract class Transformation<T> {
 	 * @param parallelism The new parallelism to set on this {@code Transformation}.
 	 */
 	public void setParallelism(int parallelism) {
-		Preconditions.checkArgument(
-				parallelism > 0 || parallelism == ExecutionConfig.PARALLELISM_DEFAULT,
-				"The parallelism must be at least one, or ExecutionConfig.PARALLELISM_DEFAULT (use system default).");
+		OperatorResourceParallelismUtils.validateParallelism(parallelism);
 		this.parallelism = parallelism;
 	}
 
@@ -223,10 +221,7 @@ public abstract class Transformation<T> {
 	 * @param maxParallelism Maximum parallelism for this stream transformation.
 	 */
 	public void setMaxParallelism(int maxParallelism) {
-		Preconditions.checkArgument(maxParallelism > 0
-						&& maxParallelism <= UPPER_BOUND_MAX_PARALLELISM,
-				"Maximum parallelism must be between 1 and " + UPPER_BOUND_MAX_PARALLELISM
-						+ ". Found: " + maxParallelism);
+		OperatorResourceParallelismUtils.validateMaxParallelism(maxParallelism, UPPER_BOUND_MAX_PARALLELISM);
 		this.maxParallelism = maxParallelism;
 	}
 

flink-java/src/main/java/org/apache/flink/api/java/operators/DataSink.java
@@ -31,11 +31,11 @@ import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.common.operators.Ordering;
 import org.apache.flink.api.common.operators.ResourceSpec;
 import org.apache.flink.api.common.operators.UnaryOperatorInformation;
+import org.apache.flink.api.common.operators.util.OperatorResourceParallelismUtils;
 import org.apache.flink.api.common.typeinfo.NothingTypeInfo;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.util.Preconditions;
 
 import java.util.Arrays;
 
@@ -286,8 +286,7 @@ public class DataSink<T> {
 	 * @return This data sink with set parallelism.
 	 */
 	public DataSink<T> setParallelism(int parallelism) {
-		Preconditions.checkArgument(parallelism > 0 || parallelism == ExecutionConfig.PARALLELISM_DEFAULT,
-			"The parallelism of an operator must be at least 1.");
+		OperatorResourceParallelismUtils.validateParallelism(parallelism);
 
 		this.parallelism = parallelism;
 
@@ -330,10 +329,7 @@ public class DataSink<T> {
 	 * @return The data sink with set minimum and preferred resources.
 	 */
 	private DataSink<T> setResources(ResourceSpec minResources, ResourceSpec preferredResources) {
-		Preconditions.checkNotNull(minResources, "The min resources must be not null.");
-		Preconditions.checkNotNull(preferredResources, "The preferred resources must be not null.");
-		Preconditions.checkArgument(minResources.isValid() && preferredResources.isValid() && minResources.lessThanOrEqual(preferredResources),
-				"The values in resources must be not less than 0 and the preferred resources must be greater than the min resources.");
+		OperatorResourceParallelismUtils.validateMinAndPreferredResources(minResources, preferredResources);
 
 		this.minResources = minResources;
 		this.preferredResources = preferredResources;
@@ -348,8 +344,7 @@ public class DataSink<T> {
 	 * @return The data sink with set minimum and preferred resources.
 	 */
 	private DataSink<T> setResources(ResourceSpec resources) {
-		Preconditions.checkNotNull(resources, "The resources must be not null.");
-		Preconditions.checkArgument(resources.isValid(), "The values in resources must be not less than 0.");
+		OperatorResourceParallelismUtils.validateResources(resources);
 
 		this.minResources = resources;
 		this.preferredResources = resources;

flink-java/src/main/java/org/apache/flink/api/java/operators/DeltaIteration.java
@@ -27,11 +27,11 @@ import org.apache.flink.api.common.aggregators.AggregatorRegistry;
 import org.apache.flink.api.common.aggregators.ConvergenceCriterion;
 import org.apache.flink.api.common.operators.Keys;
 import org.apache.flink.api.common.operators.ResourceSpec;
+import org.apache.flink.api.common.operators.util.OperatorResourceParallelismUtils;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.types.Value;
-import org.apache.flink.util.Preconditions;
 
 import java.util.Arrays;
 
@@ -182,8 +182,7 @@ public class DeltaIteration<ST, WT> {
 	 * @return The iteration object, for function call chaining.
 	 */
 	public DeltaIteration<ST, WT> parallelism(int parallelism) {
-		Preconditions.checkArgument(parallelism > 0 || parallelism == ExecutionConfig.PARALLELISM_DEFAULT,
-			"The parallelism must be positive, or ExecutionConfig.PARALLELISM_DEFAULT (use default).");
+		OperatorResourceParallelismUtils.validateParallelism(parallelism);
 		this.parallelism = parallelism;
 		return this;
 	}
@@ -211,10 +210,7 @@ public class DeltaIteration<ST, WT> {
 	 * @return The iteration with set minimum and preferred resources.
 	 */
 	private DeltaIteration<ST, WT> setResources(ResourceSpec minResources, ResourceSpec preferredResources) {
-		Preconditions.checkNotNull(minResources, "The min resources must be not null.");
-		Preconditions.checkNotNull(preferredResources, "The preferred resources must be not null.");
-		Preconditions.checkArgument(minResources.isValid() && preferredResources.isValid() && minResources.lessThanOrEqual(preferredResources),
-				"The values in resources must be not less than 0 and the preferred resources must be greater than the min resources.");
+		OperatorResourceParallelismUtils.validateMinAndPreferredResources(minResources, preferredResources);
 
 		this.minResources = minResources;
 		this.preferredResources = preferredResources;
@@ -230,8 +226,7 @@ public class DeltaIteration<ST, WT> {
 	 * @return The iteration with set minimum and preferred resources.
 	 */
 	private DeltaIteration<ST, WT> setResources(ResourceSpec resources) {
-		Preconditions.checkNotNull(resources, "The resources must be not null.");
-		Preconditions.checkArgument(resources.isValid(), "The values in resources must be not less than 0.");
+		OperatorResourceParallelismUtils.validateResources(resources);
 
 		this.minResources = resources;
 		this.preferredResources = resources;

flink-java/src/main/java/org/apache/flink/api/java/operators/Operator.java
@@ -21,10 +21,10 @@ package org.apache.flink.api.java.operators;
 import org.apache.flink.annotation.Public;
 import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.operators.ResourceSpec;
+import org.apache.flink.api.common.operators.util.OperatorResourceParallelismUtils;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.util.Preconditions;
 
 /**
  * Base class of all operators in the Java API.
@@ -119,8 +119,7 @@ public abstract class Operator<OUT, O extends Operator<OUT, O>> extends DataSet<
 	 * @return The operator with set parallelism.
 	 */
 	public O setParallelism(int parallelism) {
-		Preconditions.checkArgument(parallelism > 0 || parallelism == ExecutionConfig.PARALLELISM_DEFAULT,
-				"The parallelism must be at least one, or ExecutionConfig.PARALLELISM_DEFAULT (use system default).");
+		OperatorResourceParallelismUtils.validateParallelism(parallelism);
 
 		this.parallelism = parallelism;
 
@@ -143,11 +142,7 @@ public abstract class Operator<OUT, O extends Operator<OUT, O>> extends DataSet<
 	 * @return The operator with set minimum and preferred resources.
 	 */
 	private O setResources(ResourceSpec minResources, ResourceSpec preferredResources) {
-		Preconditions.checkNotNull(minResources, "The min resources must be not null.");
-		Preconditions.checkNotNull(preferredResources, "The preferred resources must be not null.");
-
-		Preconditions.checkArgument(minResources.isValid() && preferredResources.isValid() && minResources.lessThanOrEqual(preferredResources),
-				"The values in resources must be not less than 0 and the preferred resources must be greater than the min resources.");
+		OperatorResourceParallelismUtils.validateMinAndPreferredResources(minResources, preferredResources);
 
 		this.minResources = minResources;
 		this.preferredResources = preferredResources;
@@ -164,8 +159,7 @@ public abstract class Operator<OUT, O extends Operator<OUT, O>> extends DataSet<
 	 * @return The operator with set minimum and preferred resources.
 	 */
 	private O setResources(ResourceSpec resources) {
-		Preconditions.checkNotNull(resources, "The resources must be not null.");
-		Preconditions.checkArgument(resources.isValid(), "The values in resources must be not less than 0.");
+		OperatorResourceParallelismUtils.validateResources(resources);
 
 		this.minResources = resources;
 		this.preferredResources = resources;

flink-streaming-java/src/main/java/org/apache/flink/streaming/api/datastream/DataStreamSink.java
@@ -21,10 +21,10 @@ import org.apache.flink.annotation.Internal;
 import org.apache.flink.annotation.Public;
 import org.apache.flink.annotation.PublicEvolving;
 import org.apache.flink.api.common.operators.ResourceSpec;
+import org.apache.flink.api.common.operators.util.OperatorResourceParallelismUtils;
 import org.apache.flink.streaming.api.operators.ChainingStrategy;
 import org.apache.flink.streaming.api.operators.StreamSink;
 import org.apache.flink.streaming.api.transformations.SinkTransformation;
-import org.apache.flink.util.Preconditions;
 
 /**
  * A Stream Sink. This is used for emitting elements from a streaming topology.
@@ -127,10 +127,7 @@ public class DataStreamSink<T> {
 	 * @return The sink with set minimum and preferred resources.
 	 */
 	private DataStreamSink<T> setResources(ResourceSpec minResources, ResourceSpec preferredResources) {
-		Preconditions.checkNotNull(minResources, "The min resources must be not null.");
-		Preconditions.checkNotNull(preferredResources, "The preferred resources must be not null.");
-		Preconditions.checkArgument(minResources.isValid() && preferredResources.isValid() && minResources.lessThanOrEqual(preferredResources),
-				"The values in resources must be not less than 0 and the preferred resources must be greater than the min resources.");
+		OperatorResourceParallelismUtils.validateMinAndPreferredResources(minResources, preferredResources);
 
 		transformation.setResources(minResources, preferredResources);
 
@@ -144,8 +141,7 @@ public class DataStreamSink<T> {
 	 * @return The sink with set minimum and preferred resources.
 	 */
 	private DataStreamSink<T> setResources(ResourceSpec resources) {
-		Preconditions.checkNotNull(resources, "The resources must be not null.");
-		Preconditions.checkArgument(resources.isValid(), "The values in resources must be not less than 0.");
+		OperatorResourceParallelismUtils.validateResources(resources);
 
 		transformation.setResources(resources, resources);
 

flink-streaming-java/src/main/java/org/apache/flink/streaming/api/datastream/DataStreamSource.java
@@ -18,6 +18,7 @@
 package org.apache.flink.streaming.api.datastream;
 
 import org.apache.flink.annotation.Public;
+import org.apache.flink.api.common.operators.util.OperatorResourceParallelismUtils;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.operators.StreamSource;
@@ -51,11 +52,8 @@ public class DataStreamSource<T> extends SingleOutputStreamOperator<T> {
 
 	@Override
 	public DataStreamSource<T> setParallelism(int parallelism) {
-		if (parallelism != 1 && !isParallel) {
-			throw new IllegalArgumentException("Source: " + transformation.getId() + " is not a parallel source");
-		} else {
-			super.setParallelism(parallelism);
-			return this;
-		}
+		OperatorResourceParallelismUtils.validateMaxParallelism(parallelism, isParallel);
+		super.setParallelism(parallelism);
+		return this;
 	}
 }

apache/flink

flink-core/src/main/java/org/apache/flink/api/dag/Transformation.java
@@ -19,9 +19,9 @@
 package org.apache.flink.api.dag;
 
 import org.apache.flink.annotation.Internal;
-import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.functions.InvalidTypesException;
 import org.apache.flink.api.common.operators.ResourceSpec;
+import org.apache.flink.api.common.operators.util.OperatorValidationUtils;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.typeutils.MissingTypeInfo;
 import org.apache.flink.util.Preconditions;
@@ -202,9 +202,7 @@ public abstract class Transformation<T> {
 	 * @param parallelism The new parallelism to set on this {@code Transformation}.
 	 */
 	public void setParallelism(int parallelism) {
-		Preconditions.checkArgument(
-				parallelism > 0 || parallelism == ExecutionConfig.PARALLELISM_DEFAULT,
-				"The parallelism must be at least one, or ExecutionConfig.PARALLELISM_DEFAULT (use system default).");
+		OperatorValidationUtils.validateParallelism(parallelism);
 		this.parallelism = parallelism;
 	}
 
@@ -223,10 +221,7 @@ public abstract class Transformation<T> {
 	 * @param maxParallelism Maximum parallelism for this stream transformation.
 	 */
 	public void setMaxParallelism(int maxParallelism) {
-		Preconditions.checkArgument(maxParallelism > 0
-						&& maxParallelism <= UPPER_BOUND_MAX_PARALLELISM,
-				"Maximum parallelism must be between 1 and " + UPPER_BOUND_MAX_PARALLELISM
-						+ ". Found: " + maxParallelism);
+		OperatorValidationUtils.validateMaxParallelism(maxParallelism, UPPER_BOUND_MAX_PARALLELISM);
 		this.maxParallelism = maxParallelism;
 	}
 

flink-java/src/main/java/org/apache/flink/api/java/operators/DataSink.java
@@ -31,11 +31,11 @@ import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.common.operators.Ordering;
 import org.apache.flink.api.common.operators.ResourceSpec;
 import org.apache.flink.api.common.operators.UnaryOperatorInformation;
+import org.apache.flink.api.common.operators.util.OperatorValidationUtils;
 import org.apache.flink.api.common.typeinfo.NothingTypeInfo;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.util.Preconditions;
 
 import java.util.Arrays;
 
@@ -286,8 +286,7 @@ public class DataSink<T> {
 	 * @return This data sink with set parallelism.
 	 */
 	public DataSink<T> setParallelism(int parallelism) {
-		Preconditions.checkArgument(parallelism > 0 || parallelism == ExecutionConfig.PARALLELISM_DEFAULT,
-			"The parallelism of an operator must be at least 1.");
+		OperatorValidationUtils.validateParallelism(parallelism);
 
 		this.parallelism = parallelism;
 
@@ -330,10 +329,7 @@ public class DataSink<T> {
 	 * @return The data sink with set minimum and preferred resources.
 	 */
 	private DataSink<T> setResources(ResourceSpec minResources, ResourceSpec preferredResources) {
-		Preconditions.checkNotNull(minResources, "The min resources must be not null.");
-		Preconditions.checkNotNull(preferredResources, "The preferred resources must be not null.");
-		Preconditions.checkArgument(minResources.isValid() && preferredResources.isValid() && minResources.lessThanOrEqual(preferredResources),
-				"The values in resources must be not less than 0 and the preferred resources must be greater than the min resources.");
+		OperatorValidationUtils.validateMinAndPreferredResources(minResources, preferredResources);
 
 		this.minResources = minResources;
 		this.preferredResources = preferredResources;
@@ -348,8 +344,7 @@ public class DataSink<T> {
 	 * @return The data sink with set minimum and preferred resources.
 	 */
 	private DataSink<T> setResources(ResourceSpec resources) {
-		Preconditions.checkNotNull(resources, "The resources must be not null.");
-		Preconditions.checkArgument(resources.isValid(), "The values in resources must be not less than 0.");
+		OperatorValidationUtils.validateResources(resources);
 
 		this.minResources = resources;
 		this.preferredResources = resources;

flink-java/src/main/java/org/apache/flink/api/java/operators/DeltaIteration.java
@@ -27,11 +27,11 @@ import org.apache.flink.api.common.aggregators.AggregatorRegistry;
 import org.apache.flink.api.common.aggregators.ConvergenceCriterion;
 import org.apache.flink.api.common.operators.Keys;
 import org.apache.flink.api.common.operators.ResourceSpec;
+import org.apache.flink.api.common.operators.util.OperatorValidationUtils;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.types.Value;
-import org.apache.flink.util.Preconditions;
 
 import java.util.Arrays;
 
@@ -182,8 +182,7 @@ public class DeltaIteration<ST, WT> {
 	 * @return The iteration object, for function call chaining.
 	 */
 	public DeltaIteration<ST, WT> parallelism(int parallelism) {
-		Preconditions.checkArgument(parallelism > 0 || parallelism == ExecutionConfig.PARALLELISM_DEFAULT,
-			"The parallelism must be positive, or ExecutionConfig.PARALLELISM_DEFAULT (use default).");
+		OperatorValidationUtils.validateParallelism(parallelism);
 		this.parallelism = parallelism;
 		return this;
 	}
@@ -211,10 +210,7 @@ public class DeltaIteration<ST, WT> {
 	 * @return The iteration with set minimum and preferred resources.
 	 */
 	private DeltaIteration<ST, WT> setResources(ResourceSpec minResources, ResourceSpec preferredResources) {
-		Preconditions.checkNotNull(minResources, "The min resources must be not null.");
-		Preconditions.checkNotNull(preferredResources, "The preferred resources must be not null.");
-		Preconditions.checkArgument(minResources.isValid() && preferredResources.isValid() && minResources.lessThanOrEqual(preferredResources),
-				"The values in resources must be not less than 0 and the preferred resources must be greater than the min resources.");
+		OperatorValidationUtils.validateMinAndPreferredResources(minResources, preferredResources);
 
 		this.minResources = minResources;
 		this.preferredResources = preferredResources;
@@ -230,8 +226,7 @@ public class DeltaIteration<ST, WT> {
 	 * @return The iteration with set minimum and preferred resources.
 	 */
 	private DeltaIteration<ST, WT> setResources(ResourceSpec resources) {
-		Preconditions.checkNotNull(resources, "The resources must be not null.");
-		Preconditions.checkArgument(resources.isValid(), "The values in resources must be not less than 0.");
+		OperatorValidationUtils.validateResources(resources);
 
 		this.minResources = resources;
 		this.preferredResources = resources;

flink-java/src/main/java/org/apache/flink/api/java/operators/Operator.java
@@ -21,10 +21,10 @@ package org.apache.flink.api.java.operators;
 import org.apache.flink.annotation.Public;
 import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.operators.ResourceSpec;
+import org.apache.flink.api.common.operators.util.OperatorValidationUtils;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.util.Preconditions;
 
 /**
  * Base class of all operators in the Java API.
@@ -119,8 +119,7 @@ public abstract class Operator<OUT, O extends Operator<OUT, O>> extends DataSet<
 	 * @return The operator with set parallelism.
 	 */
 	public O setParallelism(int parallelism) {
-		Preconditions.checkArgument(parallelism > 0 || parallelism == ExecutionConfig.PARALLELISM_DEFAULT,
-				"The parallelism must be at least one, or ExecutionConfig.PARALLELISM_DEFAULT (use system default).");
+		OperatorValidationUtils.validateParallelism(parallelism);
 
 		this.parallelism = parallelism;
 
@@ -143,11 +142,7 @@ public abstract class Operator<OUT, O extends Operator<OUT, O>> extends DataSet<
 	 * @return The operator with set minimum and preferred resources.
 	 */
 	private O setResources(ResourceSpec minResources, ResourceSpec preferredResources) {
-		Preconditions.checkNotNull(minResources, "The min resources must be not null.");
-		Preconditions.checkNotNull(preferredResources, "The preferred resources must be not null.");
-
-		Preconditions.checkArgument(minResources.isValid() && preferredResources.isValid() && minResources.lessThanOrEqual(preferredResources),
-				"The values in resources must be not less than 0 and the preferred resources must be greater than the min resources.");
+		OperatorValidationUtils.validateMinAndPreferredResources(minResources, preferredResources);
 
 		this.minResources = minResources;
 		this.preferredResources = preferredResources;
@@ -164,8 +159,7 @@ public abstract class Operator<OUT, O extends Operator<OUT, O>> extends DataSet<
 	 * @return The operator with set minimum and preferred resources.
 	 */
 	private O setResources(ResourceSpec resources) {
-		Preconditions.checkNotNull(resources, "The resources must be not null.");
-		Preconditions.checkArgument(resources.isValid(), "The values in resources must be not less than 0.");
+		OperatorValidationUtils.validateResources(resources);
 
 		this.minResources = resources;
 		this.preferredResources = resources;

flink-streaming-java/src/main/java/org/apache/flink/streaming/api/datastream/DataStreamSink.java
@@ -21,10 +21,10 @@ import org.apache.flink.annotation.Internal;
 import org.apache.flink.annotation.Public;
 import org.apache.flink.annotation.PublicEvolving;
 import org.apache.flink.api.common.operators.ResourceSpec;
+import org.apache.flink.api.common.operators.util.OperatorValidationUtils;
 import org.apache.flink.streaming.api.operators.ChainingStrategy;
 import org.apache.flink.streaming.api.operators.StreamSink;
 import org.apache.flink.streaming.api.transformations.SinkTransformation;
-import org.apache.flink.util.Preconditions;
 
 /**
  * A Stream Sink. This is used for emitting elements from a streaming topology.
@@ -127,10 +127,7 @@ public class DataStreamSink<T> {
 	 * @return The sink with set minimum and preferred resources.
 	 */
 	private DataStreamSink<T> setResources(ResourceSpec minResources, ResourceSpec preferredResources) {
-		Preconditions.checkNotNull(minResources, "The min resources must be not null.");
-		Preconditions.checkNotNull(preferredResources, "The preferred resources must be not null.");
-		Preconditions.checkArgument(minResources.isValid() && preferredResources.isValid() && minResources.lessThanOrEqual(preferredResources),
-				"The values in resources must be not less than 0 and the preferred resources must be greater than the min resources.");
+		OperatorValidationUtils.validateMinAndPreferredResources(minResources, preferredResources);
 
 		transformation.setResources(minResources, preferredResources);
 
@@ -144,8 +141,7 @@ public class DataStreamSink<T> {
 	 * @return The sink with set minimum and preferred resources.
 	 */
 	private DataStreamSink<T> setResources(ResourceSpec resources) {
-		Preconditions.checkNotNull(resources, "The resources must be not null.");
-		Preconditions.checkArgument(resources.isValid(), "The values in resources must be not less than 0.");
+		OperatorValidationUtils.validateResources(resources);
 
 		transformation.setResources(resources, resources);
 

flink-streaming-java/src/main/java/org/apache/flink/streaming/api/datastream/DataStreamSource.java
@@ -18,6 +18,7 @@
 package org.apache.flink.streaming.api.datastream;
 
 import org.apache.flink.annotation.Public;
+import org.apache.flink.api.common.operators.util.OperatorValidationUtils;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.operators.StreamSource;
@@ -51,11 +52,8 @@ public class DataStreamSource<T> extends SingleOutputStreamOperator<T> {
 
 	@Override
 	public DataStreamSource<T> setParallelism(int parallelism) {
-		if (parallelism != 1 && !isParallel) {
-			throw new IllegalArgumentException("Source: " + transformation.getId() + " is not a parallel source");
-		} else {
-			super.setParallelism(parallelism);
-			return this;
-		}
+		OperatorValidationUtils.validateMaxParallelism(parallelism, isParallel);
+		super.setParallelism(parallelism);
+		return this;
 	}
 }