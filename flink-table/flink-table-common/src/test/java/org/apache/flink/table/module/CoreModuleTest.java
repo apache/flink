71142816113a4f1d2160b73cdaed71c4bbfb575f
bowen.li <bowenli86@gmail.com>
2019-11-14 13:21:41 -0800

    [FLINK-14782][table] CoreModule#getFunctionDefinition should return empty optional when function does not exist
    CoreModule#getFunctionDefinition should return empty optional when function does not exist.
    
    this closes #14782.

--- flink-table/flink-table-common/src/test/java/org/apache/flink/table/module/CoreModuleTest.java
+++ flink-table/flink-table-common/src/test/java/org/apache/flink/table/module/CoreModuleTest.java
@@ -18,36 +18,16 @@
 
 package org.apache.flink.table.module;
 
-import org.apache.flink.table.functions.BuiltInFunctionDefinitions;
-import org.apache.flink.table.functions.FunctionDefinition;
+import org.junit.Test;
 
-import java.util.Optional;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import static org.junit.Assert.assertFalse;
 
 /**
- * Module of default core metadata in Flink.
+ * Test for {@link CoreModule}.
  */
-public class CoreModule implements Module {
-	public static final CoreModule INSTANCE = new CoreModule();
-
-	private CoreModule() {
-	}
-
-	@Override
-	public Set<String> listFunctions() {
-		return BuiltInFunctionDefinitions.getDefinitions()
-			.stream()
-			.map(f -> f.getName())
-			.collect(Collectors.toSet());
-	}
-
-	@Override
-	public Optional<FunctionDefinition> getFunctionDefinition(String name) {
-		return BuiltInFunctionDefinitions.getDefinitions().stream()
-				.filter(f -> f.getName().equalsIgnoreCase(name))
-				.findFirst()
-				.map(Function.identity());
+public class CoreModuleTest {
+	@Test
+	public void testGetNonExistFunction() {
+		assertFalse(CoreModule.INSTANCE.getFunctionDefinition("nonexist").isPresent());
 	}
 }
