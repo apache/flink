d76f653c9aa3ea52b4e62f3541a96f09952749f1
Arvid Heise <arvid@ververica.com>
2019-11-14 16:19:30 +0100

    [FLINK-14794][runtime] Fixing KeyedStream#transform to properly relay type information if invoked with factories.

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/datastream/KeyedStream.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/datastream/KeyedStream.java
@@ -49,9 +49,9 @@ import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.graph.StreamGraphGenerator;
 import org.apache.flink.streaming.api.operators.KeyedProcessOperator;
 import org.apache.flink.streaming.api.operators.LegacyKeyedProcessOperator;
-import org.apache.flink.streaming.api.operators.OneInputStreamOperator;
 import org.apache.flink.streaming.api.operators.StreamGroupedFold;
 import org.apache.flink.streaming.api.operators.StreamGroupedReduce;
+import org.apache.flink.streaming.api.operators.StreamOperatorFactory;
 import org.apache.flink.streaming.api.operators.co.IntervalJoinOperator;
 import org.apache.flink.streaming.api.transformations.OneInputTransformation;
 import org.apache.flink.streaming.api.transformations.PartitionTransformation;
@@ -256,11 +256,12 @@ public class KeyedStream<T, KEY> extends DataStream<T> {
 	// ------------------------------------------------------------------------
 
 	@Override
-	@PublicEvolving
-	public <R> SingleOutputStreamOperator<R> transform(String operatorName,
-			TypeInformation<R> outTypeInfo, OneInputStreamOperator<T, R> operator) {
+	protected <R> SingleOutputStreamOperator<R> doTransform(
+			final String operatorName,
+			final TypeInformation<R> outTypeInfo,
+			final StreamOperatorFactory<R> operatorFactory) {
 
-		SingleOutputStreamOperator<R> returnStream = super.transform(operatorName, outTypeInfo, operator);
+		SingleOutputStreamOperator<R> returnStream = super.doTransform(operatorName, outTypeInfo, operatorFactory);
 
 		// inject the key selector and key type
 		OneInputTransformation<T, R> transform = (OneInputTransformation<T, R>) returnStream.getTransformation();
