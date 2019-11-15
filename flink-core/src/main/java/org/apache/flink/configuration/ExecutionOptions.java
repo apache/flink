94fa69ea9e0ad2d4b116bbfeacc012242e33ee25
Dawid Wysakowicz <dwysakowicz@apache.org>
2019-11-15 11:02:42 +0100

    [FLINK-14787][configuration] Add configure method to StreamExecutionEnvironment

--- flink-core/src/main/java/org/apache/flink/configuration/ExecutionOptions.java
+++ flink-core/src/main/java/org/apache/flink/configuration/ExecutionOptions.java
@@ -19,6 +19,10 @@
 package org.apache.flink.configuration;
 
 import org.apache.flink.annotation.PublicEvolving;
+import org.apache.flink.configuration.description.Description;
+import org.apache.flink.configuration.description.TextElement;
+
+import java.time.Duration;
 
 /**
  * {@link ConfigOption}s specific for a single execution of a user program.
@@ -35,4 +39,20 @@ public class ExecutionOptions {
 			.booleanType()
 			.defaultValue(false)
 		.withDescription("Tells if we should use compression for the state snapshot data or not");
+
+	public static final ConfigOption<Duration> BUFFER_TIMEOUT =
+		ConfigOptions.key("execution.buffer-timeout")
+			.durationType()
+			.defaultValue(Duration.ofMillis(100))
+			.withDescription(Description.builder()
+				.text("The maximum time frequency (milliseconds) for the flushing of the output buffers. By default " +
+					"the output buffers flush frequently to provide low latency and to aid smooth developer " +
+					"experience. Setting the parameter can result in three logical modes:")
+				.list(
+					TextElement.text("A positive value triggers flushing periodically by that interval"),
+					TextElement.text("0 triggers flushing after every record thus minimizing latency"),
+					TextElement.text("-1 ms triggers flushing only when the output buffer is full thus maximizing " +
+						"throughput")
+				)
+				.build());
 }
--- flink-streaming-scala/src/main/scala/org/apache/flink/streaming/api/scala/StreamExecutionEnvironment.scala
+++ flink-streaming-scala/src/main/scala/org/apache/flink/streaming/api/scala/StreamExecutionEnvironment.scala
@@ -26,7 +26,7 @@ import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.api.java.typeutils.ResultTypeQueryable
 import org.apache.flink.api.java.typeutils.runtime.kryo.KryoSerializer
 import org.apache.flink.api.scala.ClosureCleaner
-import org.apache.flink.configuration.Configuration
+import org.apache.flink.configuration.{Configuration, ReadableConfig}
 import org.apache.flink.runtime.state.AbstractStateBackend
 import org.apache.flink.runtime.state.StateBackend
 import org.apache.flink.streaming.api.environment.{StreamExecutionEnvironment => JavaEnv}
@@ -403,6 +403,25 @@ class StreamExecutionEnvironment(javaEnv: JavaEnv) {
    */
   @PublicEvolving
   def getStreamTimeCharacteristic = javaEnv.getStreamTimeCharacteristic()
+
+  /**
+   * Sets all relevant options contained in the [[ReadableConfig]] such as e.g.
+   * [[org.apache.flink.streaming.api.environment.StreamPipelineOptions#TIME_CHARACTERISTIC]].
+   * It will reconfigure [[StreamExecutionEnvironment]],
+   * [[org.apache.flink.api.common.ExecutionConfig]] and
+   * [[org.apache.flink.streaming.api.environment.CheckpointConfig]].
+   *
+   * It will change the value of a setting only if a corresponding option was set in the
+   * `configuration`. If a key is not present, the current value of a field will remain
+   * untouched.
+   *
+   * @param configuration a configuration to read the values from
+   * @param classLoader   a class loader to use when loading classes
+   */
+  @PublicEvolving
+  def configure(configuration: ReadableConfig, classLoader: ClassLoader): Unit = {
+    javaEnv.configure(configuration, classLoader)
+  }
 
   // --------------------------------------------------------------------------------------------
   // Data stream creations
