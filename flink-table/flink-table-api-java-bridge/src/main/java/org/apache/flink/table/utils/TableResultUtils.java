e7545bcd6d977fad0295899c8c52acad779079eb
TsReaper <tsreaper96@gmail.com>
2019-11-25 14:42:41 +0800

    [FLINK-13943][table-api] Provide utility method to convert flink table to java List

---
+++ flink-table/flink-table-api-java-bridge/src/main/java/org/apache/flink/table/utils/TableResultUtils.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.utils;
+
+import org.apache.flink.annotation.Experimental;
+import org.apache.flink.api.common.ExecutionConfig;
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.accumulators.Accumulator;
+import org.apache.flink.api.common.accumulators.SerializedListAccumulator;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeutils.TypeSerializer;
+import org.apache.flink.api.java.Utils;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSink;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.TableSchema;
+import org.apache.flink.table.api.internal.TableImpl;
+import org.apache.flink.table.sinks.StreamTableSink;
+import org.apache.flink.table.sinks.TableSink;
+import org.apache.flink.table.types.DataType;
+import org.apache.flink.types.Row;
+import org.apache.flink.util.AbstractID;
+
+import java.util.List;
+
+/**
+ * A collection of utilities for fetching table results.
+ *
+ * <p>NOTE: Methods in this utility class are experimental and can only be used for demonstration or testing
+ * small table results. Please DO NOT use them in production or on large tables.
+ */
+@Experimental
+public class TableResultUtils {
+
+	/**
+	 * Convert Flink table to Java list.
+	 *
+	 * @param table		Flink table to convert
+	 * @return			Converted Java list
+	 */
+	public static List<Row> tableResultToList(Table table) {
+		final TableEnvironment tEnv = ((TableImpl) table).getTableEnvironment();
+
+		final String id = new AbstractID().toString();
+		final TypeSerializer<Row> serializer = table.getSchema().toRowType().createSerializer(new ExecutionConfig());
+		final Utils.CollectHelper<Row> outputFormat = new Utils.CollectHelper<>(id, serializer);
+		final TableResultSink sink = new TableResultSink(table, outputFormat);
+
+		tEnv.registerTableSink("tableResultSink", sink);
+		tEnv.insertInto("tableResultSink", table);
+
+		try {
+			JobExecutionResult executionResult = tEnv.execute("tableResultToList");
+			return SerializedListAccumulator.deserializeList(
+				executionResult.getAccumulatorResult(id),
+				serializer);
+		} catch (Exception e) {
+			throw new RuntimeException("Could not fetch table result into list: " + e);
+		}
+	}
+
+	/**
+	 * A {@link StreamTableSink} which stores rows into {@link Accumulator}s.
+	 */
+	private static class TableResultSink implements StreamTableSink<Row> {
+		private TableSchema schema;
+		private DataType rowType;
+		private Utils.CollectHelper<Row> outputFormat;
+
+		TableResultSink(Table table, Utils.CollectHelper<Row> outputFormat) {
+			this.schema = table.getSchema();
+			this.rowType = schema.toRowDataType();
+			this.outputFormat = outputFormat;
+		}
+
+		@Override
+		public DataType getConsumedDataType() {
+			return rowType;
+		}
+
+		@Override
+		public TableSchema getTableSchema() {
+			return schema;
+		}
+
+		@Override
+		public TableSink<Row> configure(String[] fieldNames, TypeInformation<?>[] fieldTypes) {
+			throw new UnsupportedOperationException(
+				"This sink is configured by passing a static schema when initiating");
+		}
+
+		@Override
+		public void emitDataStream(DataStream<Row> dataStream) {
+			throw new UnsupportedOperationException("Deprecated method, use consumeDataStream instead");
+		}
+
+		@Override
+		public DataStreamSink<?> consumeDataStream(DataStream<Row> dataStream) {
+			return dataStream.writeUsingOutputFormat(outputFormat).setParallelism(1).name("tableResult");
+		}
+	}
+}
---
+++ flink-table/flink-table-planner-blink/src/test/java/org/apache/flink/table/utils/TableResultUtilsITCase.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.utils;
+
+import org.apache.flink.api.java.typeutils.RowTypeInfo;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.planner.runtime.utils.BatchTestBase;
+import org.apache.flink.types.Row;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.flink.api.common.typeinfo.BasicTypeInfo.INT_TYPE_INFO;
+import static org.apache.flink.api.common.typeinfo.BasicTypeInfo.LONG_TYPE_INFO;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * IT case for {@link TableResultUtils}.
+ * As runtime is needed for the tests, we put these cases into planner modules, not in the api modules.
+ */
+public class TableResultUtilsITCase extends BatchTestBase {
+
+	@Test
+	public void testTableResultToList() {
+		final List<Row> sourceData = new ArrayList<>();
+		sourceData.add(row(1, 11L));
+		sourceData.add(row(1, 12L));
+		sourceData.add(row(2, 21L));
+		sourceData.add(row(2, 22L));
+		sourceData.add(row(3, 31L));
+		registerJavaCollection(
+			"T",
+			sourceData,
+			new RowTypeInfo(INT_TYPE_INFO, LONG_TYPE_INFO),
+			"a, b");
+
+		final Table table = tEnv().sqlQuery("SELECT sum(b) FROM T GROUP BY a HAVING a < 3 ORDER BY a");
+		final List<Row> actual = TableResultUtils.tableResultToList(table);
+
+		final List<Row> expected = new ArrayList<>();
+		expected.add(row(23L));
+		expected.add(row(43L));
+		assertEquals(expected, actual);
+	}
+
+	private static Row row(Object ...args) {
+		final Row row = new Row(args.length);
+		for (int i = 0; i < args.length; i++) {
+			row.setField(i, args[i]);
+		}
+		return row;
+	}
+}
