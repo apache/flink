Myasuka/flink-1

[FLINK-14800][hive] Introduce parallelism inference for HiveSource

flink-connectors/flink-connector-hive/src/main/java/org/apache/flink/connectors/hive/HiveTableSource.java
@@ -18,18 +18,22 @@
 
 package org.apache.flink.connectors.hive;
 
-import org.apache.flink.api.common.io.InputFormat;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.GlobalConfiguration;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.TableSchema;
 import org.apache.flink.table.catalog.CatalogTable;
 import org.apache.flink.table.catalog.ObjectPath;
 import org.apache.flink.table.catalog.hive.client.HiveMetastoreClientFactory;
 import org.apache.flink.table.catalog.hive.client.HiveMetastoreClientWrapper;
 import org.apache.flink.table.catalog.hive.descriptors.HiveCatalogValidator;
-import org.apache.flink.table.sources.InputFormatTableSource;
 import org.apache.flink.table.sources.PartitionableTableSource;
 import org.apache.flink.table.sources.ProjectableTableSource;
+import org.apache.flink.table.sources.StreamTableSource;
 import org.apache.flink.table.sources.TableSource;
 import org.apache.flink.table.types.DataType;
 import org.apache.flink.table.types.logical.LogicalTypeRoot;
@@ -44,6 +48,7 @@ import org.apache.thrift.TException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -51,10 +56,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.apache.flink.connectors.hive.HiveOptions.TABLE_EXEC_HIVE_INFER_SOURCE_PARALLELISM;
+import static org.apache.flink.connectors.hive.HiveOptions.TABLE_EXEC_HIVE_INFER_SOURCE_PARALLELISM_MAX;
+import static org.apache.flink.table.runtime.types.TypeInfoDataTypeConverter.fromDataTypeToTypeInfo;
+import static org.apache.flink.table.utils.TableConnectorUtils.generateRuntimeName;
+
 /**
  * A TableSource implementation to read data from Hive tables.
  */
-public class HiveTableSource extends InputFormatTableSource<Row> implements PartitionableTableSource, ProjectableTableSource<Row> {
+public class HiveTableSource implements StreamTableSource<Row>, PartitionableTableSource, ProjectableTableSource<Row> {
 
 	private static Logger logger = LoggerFactory.getLogger(HiveTableSource.class);
 
@@ -100,10 +110,35 @@ public class HiveTableSource extends InputFormatTableSource<Row> implements Part
 	}
 
 	@Override
-	public InputFormat getInputFormat() {
+	public boolean isBounded() {
+		return true;
+	}
+
+	@Override
+	public DataStream<Row> getDataStream(StreamExecutionEnvironment execEnv) {
 		if (!initAllPartitions) {
 			initAllPartitions();
 		}
+		@SuppressWarnings("unchecked")
+		TypeInformation<Row> typeInfo = (TypeInformation<Row>) fromDataTypeToTypeInfo(getProducedDataType());
+		HiveTableInputFormat inputFormat = getInputFormat();
+		DataStreamSource<Row> source = execEnv.createInput(inputFormat, typeInfo);
+
+		Configuration conf = GlobalConfiguration.loadConfiguration();
+		if (conf.getBoolean(TABLE_EXEC_HIVE_INFER_SOURCE_PARALLELISM)) {
+			int max = conf.getInteger(TABLE_EXEC_HIVE_INFER_SOURCE_PARALLELISM_MAX);
+			int splitNum;
+			try {
+				splitNum = inputFormat.createInputSplits(0).length;
+			} catch (IOException e) {
+				throw new FlinkHiveException(e);
+			}
+			source.setParallelism(Math.min(Math.max(1, splitNum), max));
+		}
+		return source;
+	}
+
+	private HiveTableInputFormat getInputFormat() {
 		return new HiveTableInputFormat(jobConf, catalogTable, allHivePartitions, projectedFields);
 	}
 
@@ -242,7 +277,7 @@ public class HiveTableSource extends InputFormatTableSource<Row> implements Part
 		if (projectedFields != null) {
 			explain += ", ProjectedFields: " + Arrays.toString(projectedFields);
 		}
-		return super.explainSource() + explain;
+		return generateRuntimeName(getClass(), getTableSchema().getFieldNames()) + explain;
 	}
 
 	@Override

flink-connectors/flink-connector-hive/src/test/java/org/apache/flink/connectors/hive/HiveTableSourceTest.java
@@ -18,16 +18,22 @@
 
 package org.apache.flink.connectors.hive;
 
+import org.apache.flink.api.dag.Transformation;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.internal.TableEnvironmentImpl;
 import org.apache.flink.table.api.internal.TableImpl;
 import org.apache.flink.table.catalog.hive.HiveCatalog;
 import org.apache.flink.table.catalog.hive.HiveTestUtils;
+import org.apache.flink.table.planner.delegation.PlannerBase;
+import org.apache.flink.table.planner.plan.nodes.exec.ExecNode;
 import org.apache.flink.table.planner.runtime.utils.TableUtil;
+import org.apache.flink.table.planner.utils.TableTestUtil;
 import org.apache.flink.types.Row;
 
 import com.klarna.hiverunner.HiveShell;
 import com.klarna.hiverunner.annotations.HiveSQL;
+import org.apache.calcite.rel.RelNode;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -37,12 +43,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import scala.collection.JavaConverters;
 
+import static org.apache.flink.table.planner.utils.JavaScalaConversionUtil.toScala;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -224,4 +232,29 @@ public class HiveTableSourceTest {
 		}
 	}
 
+	@Test
+	public void testParallelismSetting() {
+		final String catalogName = "hive";
+		final String dbName = "source_db";
+		final String tblName = "test_table_pt_1";
+		hiveShell.execute("CREATE TABLE source_db.test_table_pt_1 " +
+				"(year STRING, value INT) partitioned by (pt int);");
+		HiveTestUtils.createTextTableInserter(hiveShell, dbName, tblName)
+				.addRow(new Object[]{"2014", 3})
+				.addRow(new Object[]{"2014", 4})
+				.commit("pt=0");
+		HiveTestUtils.createTextTableInserter(hiveShell, dbName, tblName)
+				.addRow(new Object[]{"2015", 2})
+				.addRow(new Object[]{"2015", 5})
+				.commit("pt=1");
+		TableEnvironment tEnv = HiveTestUtils.createTableEnv();
+		tEnv.registerCatalog(catalogName, hiveCatalog);
+		Table table = tEnv.sqlQuery("select * from hive.source_db.test_table_pt_1");
+		PlannerBase planner = (PlannerBase) ((TableEnvironmentImpl) tEnv).getPlanner();
+		RelNode relNode = planner.optimize(TableTestUtil.toRelNode(table));
+		ExecNode execNode = planner.translateToExecNodePlan(toScala(Collections.singletonList(relNode))).get(0);
+		@SuppressWarnings("unchecked")
+		Transformation transformation = execNode.translateToPlan(planner);
+		Assert.assertEquals(2, transformation.getParallelism());
+	}
 }

apache/flink

flink-connectors/flink-connector-hive/src/main/java/org/apache/flink/connectors/hive/HiveTableSource.java
@@ -18,8 +18,13 @@
 
 package org.apache.flink.connectors.hive;
 
-import org.apache.flink.api.common.io.InputFormat;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.GlobalConfiguration;
 import org.apache.flink.connectors.hive.read.HiveTableInputFormat;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.TableSchema;
 import org.apache.flink.table.catalog.CatalogTable;
 import org.apache.flink.table.catalog.ObjectPath;
@@ -27,10 +32,10 @@ import org.apache.flink.table.catalog.hive.client.HiveMetastoreClientFactory;
 import org.apache.flink.table.catalog.hive.client.HiveMetastoreClientWrapper;
 import org.apache.flink.table.catalog.hive.descriptors.HiveCatalogValidator;
 import org.apache.flink.table.dataformat.BaseRow;
-import org.apache.flink.table.sources.InputFormatTableSource;
 import org.apache.flink.table.sources.LimitableTableSource;
 import org.apache.flink.table.sources.PartitionableTableSource;
 import org.apache.flink.table.sources.ProjectableTableSource;
+import org.apache.flink.table.sources.StreamTableSource;
 import org.apache.flink.table.sources.TableSource;
 import org.apache.flink.table.types.DataType;
 import org.apache.flink.table.types.logical.LogicalTypeRoot;
@@ -41,7 +46,10 @@ import org.apache.hadoop.hive.metastore.api.Partition;
 import org.apache.hadoop.hive.metastore.api.StorageDescriptor;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -49,11 +57,21 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.apache.flink.connectors.hive.HiveOptions.TABLE_EXEC_HIVE_INFER_SOURCE_PARALLELISM;
+import static org.apache.flink.connectors.hive.HiveOptions.TABLE_EXEC_HIVE_INFER_SOURCE_PARALLELISM_MAX;
+import static org.apache.flink.table.runtime.types.TypeInfoDataTypeConverter.fromDataTypeToTypeInfo;
+import static org.apache.flink.table.utils.TableConnectorUtils.generateRuntimeName;
+
 /**
  * A TableSource implementation to read data from Hive tables.
  */
-public class HiveTableSource extends InputFormatTableSource<BaseRow>
-		implements PartitionableTableSource, ProjectableTableSource<BaseRow>, LimitableTableSource<BaseRow> {
+public class HiveTableSource implements
+		StreamTableSource<BaseRow>,
+		PartitionableTableSource,
+		ProjectableTableSource<BaseRow>,
+		LimitableTableSource<BaseRow> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(HiveTableSource.class);
 
 	private final JobConf jobConf;
 	private final ObjectPath tablePath;
@@ -103,11 +121,44 @@ public class HiveTableSource extends InputFormatTableSource<BaseRow>
 	}
 
 	@Override
-	public InputFormat<BaseRow, ?> getInputFormat() {
+	public boolean isBounded() {
+		return true;
+	}
+
+	@Override
+	public DataStream<BaseRow> getDataStream(StreamExecutionEnvironment execEnv) {
 		if (!initAllPartitions) {
 			initAllPartitions();
 		}
-		return new HiveTableInputFormat(jobConf, catalogTable, allHivePartitions, projectedFields, limit, hiveVersion);
+
+		@SuppressWarnings("unchecked")
+		TypeInformation<BaseRow> typeInfo = (TypeInformation<BaseRow>) fromDataTypeToTypeInfo(getProducedDataType());
+		HiveTableInputFormat inputFormat = getInputFormat();
+		DataStreamSource<BaseRow> source = execEnv.createInput(inputFormat, typeInfo);
+
+		Configuration conf = GlobalConfiguration.loadConfiguration();
+		if (conf.getBoolean(TABLE_EXEC_HIVE_INFER_SOURCE_PARALLELISM)) {
+			int max = conf.getInteger(TABLE_EXEC_HIVE_INFER_SOURCE_PARALLELISM_MAX);
+			int splitNum;
+			try {
+				long nano1 = System.nanoTime();
+				splitNum = inputFormat.createInputSplits(0).length;
+				long nano2 = System.nanoTime();
+				LOG.info(
+						"Hive source({}}) createInputSplits use time: {} ms",
+						tablePath,
+						(nano2 - nano1) / 1_000_000);
+			} catch (IOException e) {
+				throw new FlinkHiveException(e);
+			}
+			source.setParallelism(Math.min(Math.max(1, splitNum), max));
+		}
+		return source.name(explainSource());
+	}
+
+	private HiveTableInputFormat getInputFormat() {
+		return new HiveTableInputFormat(
+				jobConf, catalogTable, allHivePartitions, projectedFields, limit, hiveVersion);
 	}
 
 	@Override
@@ -255,7 +306,7 @@ public class HiveTableSource extends InputFormatTableSource<BaseRow>
 		if (isLimitPushDown) {
 			explain += String.format(", LimitPushDown %s, Limit %d", isLimitPushDown, limit);
 		}
-		return super.explainSource() + explain;
+		return generateRuntimeName(getClass(), getTableSchema().getFieldNames()) + explain;
 	}
 
 	@Override

flink-connectors/flink-connector-hive/src/test/java/org/apache/flink/connectors/hive/HiveTableSourceTest.java
@@ -18,16 +18,22 @@
 
 package org.apache.flink.connectors.hive;
 
+import org.apache.flink.api.dag.Transformation;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.internal.TableEnvironmentImpl;
 import org.apache.flink.table.api.internal.TableImpl;
 import org.apache.flink.table.catalog.hive.HiveCatalog;
 import org.apache.flink.table.catalog.hive.HiveTestUtils;
+import org.apache.flink.table.planner.delegation.PlannerBase;
+import org.apache.flink.table.planner.plan.nodes.exec.ExecNode;
 import org.apache.flink.table.planner.runtime.utils.TableUtil;
+import org.apache.flink.table.planner.utils.TableTestUtil;
 import org.apache.flink.types.Row;
 
 import com.klarna.hiverunner.HiveShell;
 import com.klarna.hiverunner.annotations.HiveSQL;
+import org.apache.calcite.rel.RelNode;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -37,12 +43,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import scala.collection.JavaConverters;
 
+import static org.apache.flink.table.planner.utils.JavaScalaConversionUtil.toScala;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -261,4 +269,29 @@ public class HiveTableSourceTest {
 		}
 	}
 
+	@Test
+	public void testParallelismSetting() {
+		final String catalogName = "hive";
+		final String dbName = "source_db";
+		final String tblName = "test_parallelism";
+		hiveShell.execute("CREATE TABLE source_db.test_parallelism " +
+				"(year STRING, value INT) partitioned by (pt int);");
+		HiveTestUtils.createTextTableInserter(hiveShell, dbName, tblName)
+				.addRow(new Object[]{"2014", 3})
+				.addRow(new Object[]{"2014", 4})
+				.commit("pt=0");
+		HiveTestUtils.createTextTableInserter(hiveShell, dbName, tblName)
+				.addRow(new Object[]{"2015", 2})
+				.addRow(new Object[]{"2015", 5})
+				.commit("pt=1");
+		TableEnvironment tEnv = HiveTestUtils.createTableEnv();
+		tEnv.registerCatalog(catalogName, hiveCatalog);
+		Table table = tEnv.sqlQuery("select * from hive.source_db.test_parallelism");
+		PlannerBase planner = (PlannerBase) ((TableEnvironmentImpl) tEnv).getPlanner();
+		RelNode relNode = planner.optimize(TableTestUtil.toRelNode(table));
+		ExecNode execNode = planner.translateToExecNodePlan(toScala(Collections.singletonList(relNode))).get(0);
+		@SuppressWarnings("unchecked")
+		Transformation transformation = execNode.translateToPlan(planner);
+		Assert.assertEquals(2, transformation.getParallelism());
+	}
 }