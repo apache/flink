ea7125142b2dc21f9bc4bd1d147219976794373d
Till Rohrmann <trohrmann@apache.org>
2019-12-04 15:23:08 +0100

    [hotfix] Let YarnResourceManagerTest run asynchronous tasks in main thread

--- flink-yarn/src/main/java/org/apache/flink/yarn/YarnResourceManager.java
+++ flink-yarn/src/main/java/org/apache/flink/yarn/YarnResourceManager.java
@@ -422,6 +422,8 @@ public class YarnResourceManager extends ResourceManager<YarnWorkerNode> impleme
 	}
 
 	private void releaseFailedContainerAndRequestNewContainerIfRequired(ContainerId containerId, Throwable throwable) {
+		validateRunsInMainThread();
+
 		log.error("Could not start TaskManager in container {}.", containerId, throwable);
 
 		final ResourceID resourceId = new ResourceID(containerId.toString());
