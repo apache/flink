fba7ae37fda85dbac2cd235e5c1c049fe33df3df
Stephan Ewen <sewen@apache.org>
2019-12-05 13:26:04 +0100

    [FLINK-15084][runtime] Add shared resources tracking to MemoryManager
    This allows the memory manager to track shared opaque resources form other components, like for
    example block caches from RocksDB. The memory manager is responsible for allocating and releasing those
    resources, as well as for assigning the memory budget from of the managed memory budget.

---
+++ flink-runtime/src/test/java/org/apache/flink/runtime/memory/SharedResourcesTest.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.memory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests for the {@link SharedResources} class.
+ */
+public class SharedResourcesTest {
+
+	@Test
+	public void testAllocatedResourcesInMap() throws Exception {
+		final SharedResources resources = new SharedResources();
+
+		final TestResource tr = resources
+				.getOrAllocateSharedResource("theType", new Object(), TestResource::new, 100)
+				.resourceHandle();
+
+		assertEquals(1, resources.getNumResources());
+		assertFalse(tr.closed);
+	}
+
+	@Test
+	public void testLastReleaseRemovesFromMap() throws Exception {
+		final SharedResources resources = new SharedResources();
+		final String type = "theType";
+		final Object leaseHolder = new Object();
+		resources.getOrAllocateSharedResource(type, leaseHolder, TestResource::new, 100);
+
+		resources.release(type, leaseHolder);
+
+		assertEquals(0, resources.getNumResources());
+	}
+
+	@Test
+	public void testLastReleaseDisposesResource() throws Exception {
+		final SharedResources resources = new SharedResources();
+		final String type = "theType";
+		final Object leaseHolder = new Object();
+
+		final TestResource tr = resources
+				.getOrAllocateSharedResource(type, leaseHolder, TestResource::new, 100)
+				.resourceHandle();
+
+		resources.release(type, leaseHolder);
+
+		assertTrue(tr.closed);
+	}
+
+	// ------------------------------------------------------------------------
+
+	private static final class TestResource implements AutoCloseable {
+
+		final long size;
+		boolean closed;
+
+		TestResource(long size) {
+			this.size = size;
+		}
+
+		@Override
+		public void close() {
+			closed = true;
+		}
+	}
+}
