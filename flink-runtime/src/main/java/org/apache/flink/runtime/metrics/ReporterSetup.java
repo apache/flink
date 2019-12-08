a81e2ca560a00b47e38b5c918dd8b2e4b0a3973c
Chesnay Schepler <chesnay@apache.org>
2019-12-08 20:23:12 +0100

    chkstl

--- flink-runtime/src/main/java/org/apache/flink/runtime/metrics/ReporterSetup.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/metrics/ReporterSetup.java
@@ -33,7 +33,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
