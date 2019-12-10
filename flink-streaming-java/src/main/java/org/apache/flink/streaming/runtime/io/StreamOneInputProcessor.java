3c0a54f7b5c276e982d520a8e0c7b4ccf6b4477f
Roman Khachatryan <khachatryan.roman@gmail.com>
2019-12-10 10:06:42 +0100

    [FLINK-12484][runtime] remove checkpoint lock *usage* from production code After b4aa6e lock is acquired for every mailbox action if needed.

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/io/StreamOneInputProcessor.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/io/StreamOneInputProcessor.java
@@ -43,19 +43,15 @@ public final class StreamOneInputProcessor<IN> implements StreamInputProcessor {
 	private final StreamTaskInput<IN> input;
 	private final DataOutput<IN> output;
 
-	private final Object lock;
-
 	private final OperatorChain<?, ?> operatorChain;
 
 	public StreamOneInputProcessor(
 			StreamTaskInput<IN> input,
 			DataOutput<IN> output,
-			Object lock,
 			OperatorChain<?, ?> operatorChain) {
 
 		this.input = checkNotNull(input);
 		this.output = checkNotNull(output);
-		this.lock = checkNotNull(lock);
 		this.operatorChain = checkNotNull(operatorChain);
 	}
 
@@ -69,9 +65,7 @@ public final class StreamOneInputProcessor<IN> implements StreamInputProcessor {
 		InputStatus status = input.emitNext(output);
 
 		if (status == InputStatus.END_OF_INPUT) {
-			synchronized (lock) {
-				operatorChain.endHeadOperatorInput(1);
-			}
+			operatorChain.endHeadOperatorInput(1);
 		}
 
 		return status;
