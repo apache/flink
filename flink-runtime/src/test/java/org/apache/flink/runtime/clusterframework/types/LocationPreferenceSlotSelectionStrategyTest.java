7f4dc2cf32178babfb812832b7fd43bfd7fd12ed
Till Rohrmann <trohrmann@apache.org>
2019-12-12 19:22:58 +0100

    [FLINK-15013] Fix selection of NON_LOCAL slots in LocationPreferenceSlotSelectionStrategy
    The LocationPreferenceSlotSelectionStrategy ignored NON_LOCAL slots due to initializing the initial
    candidate score to a positive value. As NON_LOCAL candidates have a value of 0, the initial candidate
    score needs to be negative.
    
    This closes #10556.

--- flink-runtime/src/test/java/org/apache/flink/runtime/clusterframework/types/LocationPreferenceSlotSelectionStrategyTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/clusterframework/types/LocationPreferenceSlotSelectionStrategyTest.java
@@ -22,12 +22,14 @@ import org.apache.flink.runtime.jobmanager.scheduler.Locality;
 import org.apache.flink.runtime.jobmaster.SlotInfo;
 import org.apache.flink.runtime.jobmaster.slotpool.LocationPreferenceSlotSelectionStrategy;
 import org.apache.flink.runtime.jobmaster.slotpool.SlotSelectionStrategy;
+import org.apache.flink.runtime.taskmanager.TaskManagerLocation;
 
 import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.net.InetAddress;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
@@ -108,6 +110,19 @@ public class LocationPreferenceSlotSelectionStrategyTest extends SlotSelectionSt
 
 	private Matcher<SlotSelectionStrategy.SlotInfoAndResources> withSlotInfo(SlotInfo slotInfo) {
 		return new SlotInfoFeatureMatcher(slotInfo);
+	}
+
+	@Test
+	public void returnsNonLocalMatchingIfResourceProfileCanBeFulfilledButNotTheTMLocationPreferences() throws Exception {
+		final InetAddress nonHostLocalInetAddress = InetAddress.getByAddress(new byte[]{10, 0, 0, 24});
+		final TaskManagerLocation nonLocalTm = new TaskManagerLocation(new ResourceID("non-local-tm"), nonHostLocalInetAddress, 42);
+		SlotProfile slotProfile = SlotProfile.preferredLocality(resourceProfile, Collections.singletonList(nonLocalTm));
+		Optional<SlotSelectionStrategy.SlotInfoAndLocality> match = runMatching(slotProfile);
+
+		Assert.assertTrue(match.isPresent());
+		final SlotSelectionStrategy.SlotInfoAndLocality slotInfoAndLocality = match.get();
+		assertThat(candidates, hasItem(withSlotInfo(slotInfoAndLocality.getSlotInfo())));
+		assertThat(slotInfoAndLocality, hasLocality(Locality.NON_LOCAL));
 	}
 
 	@Test
