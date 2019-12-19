ff6b350e39c1f3d24dddd469e627d066a2d7d22f
Kostas Kloudas <kkloudas@gmail.com>
2019-12-19 19:35:53 +0100

    [FLINK-14683] Fix RemoteStreamEnvironment's constructor

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/environment/RemoteStreamEnvironment.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/environment/RemoteStreamEnvironment.java
@@ -139,7 +139,7 @@ public class RemoteStreamEnvironment extends StreamExecutionEnvironment {
 	 *            The protocol must be supported by the {@link java.net.URLClassLoader}.
 	 */
 	public RemoteStreamEnvironment(String host, int port, Configuration clientConfiguration, String[] jarFiles, URL[] globalClasspaths) {
-		this(host, port, clientConfiguration, jarFiles, null, null);
+		this(host, port, clientConfiguration, jarFiles, globalClasspaths, null);
 	}
 
 	/**
