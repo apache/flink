229d02147ee4d57deb05c28a5626f44aca7fcba5
Xintong Song <tonysong820@gmail.com>
2019-12-27 14:05:03 +0800

    [FLINK-15369][runtime] Set MiniCluster to use fixed managed / shuffle memory size by default.
    This closes #10717.

--- flink-runtime/src/test/java/org/apache/flink/runtime/minicluster/MiniClusterConfigurationTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/minicluster/MiniClusterConfigurationTest.java
@@ -20,12 +20,16 @@ package org.apache.flink.runtime.minicluster;
 
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.JobManagerOptions;
+import org.apache.flink.configuration.TaskManagerOptions;
 import org.apache.flink.util.TestLogger;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Tests for the {@link MiniClusterConfiguration}.
@@ -73,5 +77,16 @@ public class MiniClusterConfigurationTest extends TestLogger {
 		Assert.assertEquals(
 			JobManagerOptions.SCHEDULER.defaultValue(),
 			miniClusterConfiguration.getConfiguration().getString(JobManagerOptions.SCHEDULER));
+	}
+
+	@Test
+	public void testDefaultTaskExecutorMemoryConfiguration() {
+		final MiniClusterConfiguration miniClusterConfiguration = new MiniClusterConfiguration.Builder().build();
+		final Configuration actualConfiguration = miniClusterConfiguration.getConfiguration();
+		final Configuration expectedConfiguration = MiniClusterConfiguration.adjustTaskManagerMemoryConfigurations(new Configuration());
+
+		assertThat(actualConfiguration.get(TaskManagerOptions.SHUFFLE_MEMORY_MIN), is(MiniClusterConfiguration.DEFAULT_SHUFFLE_MEMORY_SIZE));
+		assertThat(actualConfiguration.get(TaskManagerOptions.SHUFFLE_MEMORY_MAX), is(MiniClusterConfiguration.DEFAULT_SHUFFLE_MEMORY_SIZE));
+		assertThat(actualConfiguration.get(TaskManagerOptions.MANAGED_MEMORY_SIZE), is(MiniClusterConfiguration.DEFAULT_MANAGED_MEMORY_SIZE));
 	}
 }
