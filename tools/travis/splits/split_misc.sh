cde3f24a476a5256173c8355f9c76901c5c2a985
Yangze Guo <karmagyz@gmail.com>
2019-12-30 19:39:28 +0800

    [FLINK-15428][e2e] Skip Avro Confluent Schema Registry case when running with JDK11
    Signed-off-by: Jiangjie (Becket) Qin <jiangjie.qj@alibaba-inc.com>

--- tools/travis/splits/split_misc.sh
+++ tools/travis/splits/split_misc.sh
@@ -67,7 +67,9 @@ run_test "Elasticsearch (v6.3.1) sink end-to-end test" "$END_TO_END_DIR/test-scr
 run_test "Quickstarts Java nightly end-to-end test" "$END_TO_END_DIR/test-scripts/test_quickstarts.sh java"
 run_test "Quickstarts Scala nightly end-to-end test" "$END_TO_END_DIR/test-scripts/test_quickstarts.sh scala"
 
-run_test "Avro Confluent Schema Registry nightly end-to-end test" "$END_TO_END_DIR/test-scripts/test_confluent_schema_registry.sh"
+if [[ ${PROFILE} != *"jdk11"* ]]; then
+  run_test "Avro Confluent Schema Registry nightly end-to-end test" "$END_TO_END_DIR/test-scripts/test_confluent_schema_registry.sh"
+fi
 
 run_test "State TTL Heap backend end-to-end test" "$END_TO_END_DIR/test-scripts/test_stream_state_ttl.sh file"
 run_test "State TTL RocksDb backend end-to-end test" "$END_TO_END_DIR/test-scripts/test_stream_state_ttl.sh rocks"
