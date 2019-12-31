2dfa76e7bf7319391a57997628e1d3de5ddc7bfa
Jark Wu <jark@apache.org>
2019-12-31 17:43:13 +0800

    [hotfix][csv][json] Deprecate schema methods for CSV and JSON formats
    Deprecate schema methods for CSV and JSON formats because these formats can
    derive schema from table schema by default. It is no longer necessary to
    explicitly declare format schema.

--- flink-formats/flink-json/src/main/java/org/apache/flink/table/descriptors/Json.java
+++ flink-formats/flink-json/src/main/java/org/apache/flink/table/descriptors/Json.java
@@ -66,7 +66,11 @@ public class Json extends FormatDescriptor {
 	 * <p>The schema might be nested.
 	 *
 	 * @param jsonSchema JSON schema
+	 * @deprecated {@link Json} supports derive schema from table schema by default,
+	 *             it is no longer necessary to explicitly declare the format schema.
+	 *             This method will be removed in the future.
 	 */
+	@Deprecated
 	public Json jsonSchema(String jsonSchema) {
 		Preconditions.checkNotNull(jsonSchema);
 		this.jsonSchema = jsonSchema;
@@ -83,7 +87,11 @@ public class Json extends FormatDescriptor {
 	 * <p>The schema might be nested.
 	 *
 	 * @param schemaType type information that describes the schema
+	 * @deprecated {@link Json} supports derive schema from table schema by default,
+	 *             it is no longer necessary to explicitly declare the format schema.
+	 *             This method will be removed in the future.
 	 */
+	@Deprecated
 	public Json schema(TypeInformation<Row> schemaType) {
 		Preconditions.checkNotNull(schemaType);
 		this.schema = TypeStringUtils.writeTypeInfo(schemaType);
