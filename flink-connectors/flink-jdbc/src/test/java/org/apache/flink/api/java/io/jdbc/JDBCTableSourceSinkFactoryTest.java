e8d921d91449b3c3c8afea064af7ec99daf8d976
Zhenghua Gao <docete@gmail.com>
2020-01-13 09:51:43 +0800

    fixup: derby only support decimal with precision [1,31]

--- flink-connectors/flink-jdbc/src/test/java/org/apache/flink/api/java/io/jdbc/JDBCTableSourceSinkFactoryTest.java
+++ flink-connectors/flink-jdbc/src/test/java/org/apache/flink/api/java/io/jdbc/JDBCTableSourceSinkFactoryTest.java
@@ -52,7 +52,7 @@ public class JDBCTableSourceSinkFactoryTest {
 		.field("aaa", DataTypes.INT())
 		.field("bbb", DataTypes.STRING())
 		.field("ccc", DataTypes.DOUBLE())
-		.field("ddd", DataTypes.DECIMAL(38, 18))
+		.field("ddd", DataTypes.DECIMAL(31, 18))
 		.field("eee", DataTypes.TIMESTAMP(3))
 		.build();
 
