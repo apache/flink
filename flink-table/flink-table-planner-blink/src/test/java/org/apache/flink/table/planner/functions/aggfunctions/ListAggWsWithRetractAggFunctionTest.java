ac5e288524991d340309d118fa9fa29109e7427f
Shuo Cheng <mf1533007@smail.nju.edu.cn>
2020-01-19 14:50:27 +0800

    [FLINK-15537][table-planner-blink] Fix code generation to use BinaryRow instead of GenericRow for distinct keys
    Code generation should use BinaryRow instead of GenericRow for distinct keys to have a consistent 
    behavior of equals and hashcode. Because the distinct key will be deseraizlied as BinaryRow from 
    state when job recovery. 
    
    This closes #10815

--- flink-table/flink-table-planner-blink/src/test/java/org/apache/flink/table/planner/functions/aggfunctions/ListAggWsWithRetractAggFunctionTest.java
+++ flink-table/flink-table-planner-blink/src/test/java/org/apache/flink/table/planner/functions/aggfunctions/ListAggWsWithRetractAggFunctionTest.java
@@ -33,7 +33,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Test case for built-in concatWs with retraction aggregate function.
+ * Test case for built-in ListAggWs with retraction aggregate function.
  */
 public class ListAggWsWithRetractAggFunctionTest
 	extends AggFunctionTestBase<BinaryString, ListAggWsWithRetractAccumulator> {
