8becccb8dc4d00eb2c5f2e13827ad02c93331343
Piotr Nowojski <piotr.nowojski@gmail.com>
2020-02-04 10:19:39 +0100

    [FLINK-16060][task] Implemente working StreamMultipleInputProcessor
    This doesn't fully support input selection, watermarks, latency markers and keyed contextes yet

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/tasks/MultipleInputStreamTask.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/tasks/MultipleInputStreamTask.java
@@ -18,8 +18,27 @@
 package org.apache.flink.streaming.runtime.tasks;
 
 import org.apache.flink.annotation.Internal;
+import org.apache.flink.api.common.typeutils.TypeSerializer;
 import org.apache.flink.runtime.execution.Environment;
+import org.apache.flink.runtime.io.network.partition.consumer.InputGate;
+import org.apache.flink.runtime.metrics.MetricNames;
+import org.apache.flink.streaming.api.graph.StreamConfig;
+import org.apache.flink.streaming.api.graph.StreamEdge;
+import org.apache.flink.streaming.api.operators.InputSelectable;
 import org.apache.flink.streaming.api.operators.MultipleInputStreamOperator;
+import org.apache.flink.streaming.runtime.io.CheckpointedInputGate;
+import org.apache.flink.streaming.runtime.io.InputGateUtil;
+import org.apache.flink.streaming.runtime.io.InputProcessorUtil;
+import org.apache.flink.streaming.runtime.io.MultipleInputSelectionHandler;
+import org.apache.flink.streaming.runtime.io.StreamMultipleInputProcessor;
+import org.apache.flink.streaming.runtime.metrics.MinWatermarkGauge;
+import org.apache.flink.streaming.runtime.metrics.WatermarkGauge;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.apache.flink.util.Preconditions.checkState;
 
 /**
  * A {@link StreamTask} for executing a {@link MultipleInputStreamOperator} and supporting
@@ -27,13 +46,78 @@ import org.apache.flink.streaming.api.operators.MultipleInputStreamOperator;
  */
 @Internal
 public class MultipleInputStreamTask<OUT> extends StreamTask<OUT, MultipleInputStreamOperator<OUT>> {
-
 	public MultipleInputStreamTask(Environment env) {
 		super(env);
 	}
 
 	@Override
 	public void init() throws Exception {
-		throw new UnsupportedOperationException();
+		StreamConfig configuration = getConfiguration();
+		ClassLoader userClassLoader = getUserCodeClassLoader();
+
+		TypeSerializer<?>[] inputDeserializers = configuration.getTypeSerializersIn(userClassLoader);
+
+		ArrayList<InputGate>[] inputLists = new ArrayList[inputDeserializers.length];
+		WatermarkGauge[] watermarkGauges = new WatermarkGauge[inputDeserializers.length];
+
+		for (int i = 0; i < inputDeserializers.length; i++) {
+			inputLists[i] = new ArrayList<>();
+			watermarkGauges[i] = new WatermarkGauge();
+			headOperator.getMetricGroup().gauge(MetricNames.currentInputWatermarkName(i), watermarkGauges[i]);
+		}
+
+		MinWatermarkGauge minInputWatermarkGauge = new MinWatermarkGauge(watermarkGauges);
+		headOperator.getMetricGroup().gauge(MetricNames.IO_CURRENT_INPUT_WATERMARK, minInputWatermarkGauge);
+
+		List<StreamEdge> inEdges = configuration.getInPhysicalEdges(userClassLoader);
+		int numberOfInputs = configuration.getNumberOfInputs();
+
+		for (int i = 0; i < numberOfInputs; i++) {
+			int inputType = inEdges.get(i).getTypeNumber();
+			InputGate reader = getEnvironment().getInputGate(i);
+			inputLists[inputType - 1].add(reader);
+		}
+
+		createInputProcessor(inputLists, inputDeserializers, watermarkGauges);
+
+		// wrap watermark gauge since registered metrics must be unique
+		getEnvironment().getMetricGroup().gauge(MetricNames.IO_CURRENT_INPUT_WATERMARK, minInputWatermarkGauge::getValue);
+	}
+
+	protected void createInputProcessor(
+			Collection<InputGate>[] inputGates,
+			TypeSerializer<?>[] inputDeserializers,
+			WatermarkGauge[] inputWatermarkGauges) {
+		if (headOperator instanceof InputSelectable) {
+			throw new UnsupportedOperationException();
+		}
+		MultipleInputSelectionHandler selectionHandler = new MultipleInputSelectionHandler(
+			null,
+			inputGates.length);
+
+		InputGate[] unionedInputGates = new InputGate[inputGates.length];
+		for (int i = 0; i < inputGates.length; i++) {
+			unionedInputGates[i] = InputGateUtil.createInputGate(inputGates[i].toArray(new InputGate[0]));
+		}
+
+		CheckpointedInputGate[] checkpointedInputGates = InputProcessorUtil.createCheckpointedInputGatePair(
+			this,
+			getConfiguration().getCheckpointMode(),
+			getEnvironment().getTaskManagerInfo().getConfiguration(),
+			getEnvironment().getMetricGroup().getIOMetricGroup(),
+			getTaskNameWithSubtaskAndId(),
+			unionedInputGates);
+		checkState(checkpointedInputGates.length == inputGates.length);
+
+		inputProcessor = new StreamMultipleInputProcessor(
+			checkpointedInputGates,
+			inputDeserializers,
+			getEnvironment().getIOManager(),
+			getStreamStatusMaintainer(),
+			headOperator,
+			selectionHandler,
+			inputWatermarkGauges,
+			operatorChain,
+			setupNumRecordsInCounter(headOperator));
 	}
 }
