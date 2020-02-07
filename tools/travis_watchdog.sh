9911e523f1d5737f84c8a18c688649ea496999f4
Chesnay Schepler <chesnay@apache.org>
2020-02-07 09:25:01 +0100

    [hotfix][travis] Fix caching of misc job
    Adding the e2e-pre-commit profile activation only to the misc profile broke the caching

--- tools/travis_watchdog.sh
+++ tools/travis_watchdog.sh
@@ -76,7 +76,8 @@ e2e_modules=$(find flink-end-to-end-tests -mindepth 2 -maxdepth 5 -name 'pom.xml
 
 MVN_COMPILE="mvn $MVN_COMMON_OPTIONS $MVN_COMPILE_OPTIONS $PROFILE $MVN_COMPILE_MODULES install"
 MVN_TEST="mvn $MVN_COMMON_OPTIONS $MVN_TEST_OPTIONS $PROFILE $MVN_TEST_MODULES verify"
-MVN_E2E="mvn $MVN_COMMON_OPTIONS $MVN_TEST_OPTIONS $PROFILE -pl ${e2e_modules},flink-dist verify"
+# don't move the e2e-pre-commit profile activation into the misc entry in .travis.yml, since it breaks caching
+MVN_E2E="mvn $MVN_COMMON_OPTIONS $MVN_TEST_OPTIONS $PROFILE -Pe2e-pre-commit -pl ${e2e_modules},flink-dist verify"
 
 MVN_PID="${ARTIFACTS_DIR}/watchdog.mvn.pid"
 MVN_EXIT="${ARTIFACTS_DIR}/watchdog.mvn.exit"
