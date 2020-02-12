959e1df0dfbea63840bae945e616322c017de740
JingsongLi <lzljs3620320@aliyun.com>
2020-02-12 10:16:18 +0800

    Fix python checkstyle

--- flink-python/pyflink/table/descriptors.py
+++ flink-python/pyflink/table/descriptors.py
@@ -15,7 +15,6 @@
 #  See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
-import warnings
 from abc import ABCMeta
 
 from py4j.java_gateway import get_method
