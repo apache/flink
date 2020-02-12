654aabe49679c59b2df80dd304f1721ce3c10a98
JingsongLi <lzljs3620320@aliyun.com>
2020-02-12 11:59:14 +0800

    Fix comments

--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/api/internal/BatchTableEnvImpl.scala
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/api/internal/BatchTableEnvImpl.scala
@@ -73,18 +73,11 @@ abstract class BatchTableEnvImpl(
    * Provides necessary methods for [[ConnectTableDescriptor]].
    */
   private val registration = new Registration() {
+
     override def createTemporaryTable(path: String, table: CatalogBaseTable): Unit = {
       val unresolvedIdentifier = parseIdentifier(path)
       val objectIdentifier = catalogManager.qualifyIdentifier(unresolvedIdentifier)
       catalogManager.createTemporaryTable(table, objectIdentifier, false)
-    }
-
-    override def createTableSource(name: String, tableSource: TableSource[_]): Unit = {
-      registerTableSource(name, tableSource)
-    }
-
-    override def createTableSink(name: String, tableSource: TableSink[_]): Unit = {
-      registerTableSink(name, tableSource)
     }
   }
 
