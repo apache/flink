8d5d2238515b1d63bf1591e1b1bbef8611af5c99
godfreyhe <godfreyhe@163.com>
2020-02-20 20:11:51 +0800

    [FLINK-16070] [table-planner-blink] blink stream planner supports remove constant keys from an aggregate

--- flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/plan/rules/FlinkStreamRuleSets.scala
+++ flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/plan/rules/FlinkStreamRuleSets.scala
@@ -109,6 +109,8 @@ object FlinkStreamRuleSets {
       REWRITE_COALESCE_RULES.asScala ++
       REDUCE_EXPRESSION_RULES.asScala ++
       List(
+        //removes constant keys from an Agg
+        AggregateProjectPullUpConstantsRule.INSTANCE,
         StreamLogicalWindowAggregateRule.INSTANCE,
         // slices a project into sections which contain window agg functions
         // and sections which do not.
