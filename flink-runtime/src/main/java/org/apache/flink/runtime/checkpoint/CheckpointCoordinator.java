874310f68acd418a0a1390f9bd667e506e0efb90
ifndef-SleePy <mmyy1110@gmail.com>
2020-02-24 23:49:57 +0800

    [FLINK-14971][checkpointing] Handle ack/declined message of checkpoint in timer thread

--- flink-runtime/src/main/java/org/apache/flink/runtime/checkpoint/CheckpointCoordinator.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/checkpoint/CheckpointCoordinator.java
@@ -656,7 +656,9 @@ public class CheckpointCoordinator {
 			props,
 			checkpointStorageLocation,
 			executor,
-			onCompletionPromise);
+			timer,
+			onCompletionPromise,
+			completedCheckpointStore);
 
 		if (statsTracker != null) {
 			PendingCheckpointStats callback = statsTracker.reportPendingCheckpoint(
@@ -1018,61 +1020,44 @@ public class CheckpointCoordinator {
 	 * <p>Important: This method should only be called in the checkpoint lock scope.
 	 *
 	 * @param pendingCheckpoint to complete
-	 * @throws CheckpointException if the completion failed
 	 */
-	private void completePendingCheckpoint(PendingCheckpoint pendingCheckpoint) throws CheckpointException {
-		final long checkpointId = pendingCheckpoint.getCheckpointId();
-		final CompletedCheckpoint completedCheckpoint;
-
+	private void completePendingCheckpoint(PendingCheckpoint pendingCheckpoint) {
 		// As a first step to complete the checkpoint, we register its state with the registry
 		Map<OperatorID, OperatorState> operatorStates = pendingCheckpoint.getOperatorStates();
 		sharedStateRegistry.registerAll(operatorStates.values());
 
-		try {
-			try {
-				completedCheckpoint = pendingCheckpoint.finalizeCheckpoint();
-				failureManager.handleCheckpointSuccess(pendingCheckpoint.getCheckpointId());
-			}
-			catch (Exception e1) {
-				// abort the current pending checkpoint if we fails to finalize the pending checkpoint.
-				if (!pendingCheckpoint.isDiscarded()) {
-					abortPendingCheckpoint(
-						pendingCheckpoint,
-						new CheckpointException(
-							CheckpointFailureReason.FINALIZE_CHECKPOINT_FAILURE, e1));
-				}
-
-				throw new CheckpointException("Could not finalize the pending checkpoint " + checkpointId + '.',
-					CheckpointFailureReason.FINALIZE_CHECKPOINT_FAILURE, e1);
-			}
-
-			// the pending checkpoint must be discarded after the finalization
-			Preconditions.checkState(pendingCheckpoint.isDiscarded() && completedCheckpoint != null);
-
-			try {
-				completedCheckpointStore.addCheckpoint(completedCheckpoint);
-			} catch (Exception exception) {
-				// we failed to store the completed checkpoint. Let's clean up
-				executor.execute(new Runnable() {
-					@Override
-					public void run() {
-						try {
-							completedCheckpoint.discardOnFailedStoring();
-						} catch (Throwable t) {
-							LOG.warn("Could not properly discard completed checkpoint {}.", completedCheckpoint.getCheckpointID(), t);
-						}
+		pendingCheckpoint.finalizeCheckpoint()
+			.whenCompleteAsync((completedCheckpoint, throwable) -> {
+				synchronized (lock) {
+					if (shutdown) {
+						return;
 					}
-				});
+					if (throwable != null) {
+						if (!pendingCheckpoint.isDiscarded()) {
+							abortPendingCheckpoint(
+								pendingCheckpoint,
+								new CheckpointException(
+									CheckpointFailureReason.FINALIZE_CHECKPOINT_FAILURE, throwable));
+						}
+					} else {
+						onCheckpointSuccess(completedCheckpoint);
+					}
+				}
+			}, timer);
+	}
 
-				throw new CheckpointException("Could not complete the pending checkpoint " + checkpointId + '.',
-					CheckpointFailureReason.FINALIZE_CHECKPOINT_FAILURE, exception);
-			}
-		} finally {
-			pendingCheckpoints.remove(checkpointId);
+	/**
+	 * It's the last step of a checkpoint.
+	 *
+	 * @param completedCheckpoint the completed checkpoint
+	 */
+	private void onCheckpointSuccess(CompletedCheckpoint completedCheckpoint) {
+		final long checkpointId = completedCheckpoint.getCheckpointID();
 
-			resumePeriodicTriggering();
-		}
+		pendingCheckpoints.remove(checkpointId);
 
+		resumePeriodicTriggering();
+		failureManager.handleCheckpointSuccess(checkpointId);
 		rememberRecentCheckpointId(checkpointId);
 
 		// drop those pending checkpoints that are at prior to the completed one
--- flink-runtime/src/test/java/org/apache/flink/runtime/checkpoint/CheckpointCoordinatorFailureTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/checkpoint/CheckpointCoordinatorFailureTest.java
@@ -38,7 +38,6 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
@@ -104,13 +103,9 @@ public class CheckpointCoordinatorFailureTest extends TestLogger {
 
 		AcknowledgeCheckpoint acknowledgeMessage = new AcknowledgeCheckpoint(jid, executionAttemptId, checkpointId, new CheckpointMetrics(), subtaskState);
 
-		try {
-			coord.receiveAcknowledgeMessage(acknowledgeMessage, "Unknown location");
-			fail("Expected a checkpoint exception because the completed checkpoint store could not " +
-				"store the completed checkpoint.");
-		} catch (CheckpointException e) {
-			// ignore because we expected this exception
-		}
+		coord.receiveAcknowledgeMessage(acknowledgeMessage, "Unknown location");
+		// CheckpointCoordinator#completePendingCheckpoint is async, we have to finish the completion manually
+		manuallyTriggeredScheduledExecutor.triggerAll();
 
 		// make sure that the pending checkpoint has been discarded after we could not complete it
 		assertTrue(pendingCheckpoint.isDiscarded());
