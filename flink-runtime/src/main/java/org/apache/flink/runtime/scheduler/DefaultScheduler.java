bd757cb2a29cc2d9046d39aee9684d5b2e5ec036
Zhu Zhu <reedpor@gmail.com>
2020-02-25 20:50:29 +0800

    [FLINK-16276][tests] Introduce a builder and factory methods to create DefaultScheduler for testing

--- flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/DefaultScheduler.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/DefaultScheduler.java
@@ -93,7 +93,7 @@ public class DefaultScheduler extends SchedulerBase implements SchedulerOperatio
 
 	private final Set<ExecutionVertexID> verticesWaitingForRestart;
 
-	public DefaultScheduler(
+	DefaultScheduler(
 		final Logger log,
 		final JobGraph jobGraph,
 		final BackPressureStatsTracker backPressureStatsTracker,
