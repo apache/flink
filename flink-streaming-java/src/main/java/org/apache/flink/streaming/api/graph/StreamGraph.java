0cc972cf542776a79767c119b111b3fa2531b9b9
cpugputpu <cpugputpu@126.com>
2020-02-25 22:39:52 -0800

    Update StreamGraph.java

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/graph/StreamGraph.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/graph/StreamGraph.java
@@ -60,7 +60,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
