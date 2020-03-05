37578f75e59d61b396194641e8a83d74bfd2c984
huangxingbo <hxbks2ks@gmail.com>
2020-03-05 17:40:50 +0800

    [FLINK-16435][python] Replace since decorator with versionadd to mark the version an API was introduced
    This closes #11318.

--- flink-python/pyflink/__init__.py
+++ flink-python/pyflink/__init__.py
@@ -24,22 +24,6 @@ if sys.version_info < (3, 5):
         str(sys.version_info) + '].')
 
 
-def since(version):
-    """
-    A decorator that annotates a function to append the version the function was added.
-    """
-    import re
-    indent_p = re.compile(r'\n( +)')
-
-    def deco(f):
-        original_doc = f.__doc__ or ""
-        indents = indent_p.findall(original_doc)
-        indent = ' ' * (min(len(indent) for indent in indents) if indents else 0)
-        f.__doc__ = original_doc.rstrip() + "\n\n%s.. versionadded:: %s" % (indent, version)
-        return f
-    return deco
-
-
 def keyword(func):
     """
     A decorator that forces keyword arguments usage and store actual
