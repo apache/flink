4aa9f60eac0bf05ea85157aef222282f8f246a4d
godfreyhe <godfreyhe@163.com>
2020-03-12 18:33:56 +0800

    fix checkstyle

--- flink-java/src/main/java/org/apache/flink/api/java/ExecutionEnvironment.java
+++ flink-java/src/main/java/org/apache/flink/api/java/ExecutionEnvironment.java
@@ -1032,7 +1032,7 @@ public class ExecutionEnvironment {
 	}
 
 	/**
-	 * Get the registered cached files
+	 * Get the registered cached files.
 	 */
 	@Internal
 	public List<Tuple2<String, DistributedCacheEntry>> getCacheFile() {
