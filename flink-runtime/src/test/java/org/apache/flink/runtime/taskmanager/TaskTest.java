f0a936bd3411aa7ccaa7373fd51c0ed71a363629
Zhijiang <wangzhijiang999@aliyun.com>
2020-03-16 12:13:35 +0800

    [hotfix][task][tests] Remove useless import from TaskTest class
    This cloese #11400.

--- flink-runtime/src/test/java/org/apache/flink/runtime/taskmanager/TaskTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/taskmanager/TaskTest.java
@@ -83,7 +83,6 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
