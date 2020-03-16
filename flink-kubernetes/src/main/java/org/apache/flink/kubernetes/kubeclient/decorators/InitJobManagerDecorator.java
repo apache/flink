3d119079288f3e9da7a19a32d68446ae5228b626
Canbin Zheng <felixzhengcb@gmail.com>
2020-03-16 10:35:23 +0800

    [FLINK-16508][k8s] Name the ports exposed by the main Container in Pod
    This closes #11360 .

--- flink-kubernetes/src/main/java/org/apache/flink/kubernetes/kubeclient/decorators/InitJobManagerDecorator.java
+++ flink-kubernetes/src/main/java/org/apache/flink/kubernetes/kubeclient/decorators/InitJobManagerDecorator.java
@@ -20,6 +20,7 @@ package org.apache.flink.kubernetes.kubeclient.decorators;
 
 import org.apache.flink.kubernetes.kubeclient.FlinkPod;
 import org.apache.flink.kubernetes.kubeclient.parameters.KubernetesJobManagerParameters;
+import org.apache.flink.kubernetes.utils.Constants;
 import org.apache.flink.kubernetes.utils.KubernetesUtils;
 
 import io.fabric8.kubernetes.api.model.Container;
@@ -98,13 +99,16 @@ public class InitJobManagerDecorator extends AbstractKubernetesStepDecorator {
 	private List<ContainerPort> getContainerPorts() {
 		return Arrays.asList(
 			new ContainerPortBuilder()
+				.withName(Constants.REST_PORT_NAME)
 				.withContainerPort(kubernetesJobManagerParameters.getRestPort())
 				.build(),
 			new ContainerPortBuilder()
+				.withName(Constants.JOB_MANAGER_RPC_PORT_NAME)
 				.withContainerPort(kubernetesJobManagerParameters.getRPCPort())
 				.build(),
-			new ContainerPortBuilder().
-				withContainerPort(kubernetesJobManagerParameters.getBlobServerPort())
+			new ContainerPortBuilder()
+				.withName(Constants.BLOB_SERVER_PORT_NAME)
+				.withContainerPort(kubernetesJobManagerParameters.getBlobServerPort())
 				.build());
 	}
 
