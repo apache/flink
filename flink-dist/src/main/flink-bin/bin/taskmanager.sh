d2c6d9b77cd750039683aea2c900e88c6a8beb21
Xintong Song <tonysong820@gmail.com>
2020-03-26 15:37:22 +0800

    [FLINK-16742][dist] config.sh does not assume BashJavaUtils always return two lines of results.

--- flink-dist/src/main/flink-bin/bin/taskmanager.sh
+++ flink-dist/src/main/flink-bin/bin/taskmanager.sh
@@ -50,24 +50,18 @@ if [[ $STARTSTOP == "start" ]] || [[ $STARTSTOP == "start-foreground" ]]; then
 
     java_utils_output=$(runBashJavaUtilsCmd GET_TM_RESOURCE_PARAMS ${FLINK_CONF_DIR} $FLINK_BIN_DIR/bash-java-utils.jar:$(findFlinkDistJar) "${ARGS[@]}")
 
-    num_lines=$(echo "${java_utils_output}" | wc -l)
-    logging_output=$(echo "${java_utils_output}" | head -n $((${num_lines} - 2)))
-    params_output=$(echo "${java_utils_output}" | tail -n 2)
+    logging_output=$(extractLoggingOutputs "${java_utils_output}")
+    params_output=$(extractExecutionResults "${java_utils_output}" 2)
 
-    jvm_params=$(extractExecutionParams "$(echo "$params_output" | head -n 1)")
     if [[ $? -ne 0 ]]; then
-        echo "[ERROR] Could not get JVM parameters properly."
+        echo "[ERROR] Could not get JVM parameters and dynamic configurations properly."
         exit 1
     fi
+
+    jvm_params=$(echo "${params_output}" | head -n 1)
     export JVM_ARGS="${JVM_ARGS} ${jvm_params}"
 
-    IFS=$" "
-
-    dynamic_configs=$(extractExecutionParams "$(echo "$params_output" | tail -n 1)")
-    if [[ $? -ne 0 ]]; then
-        echo "[ERROR] Could not get dynamic configurations properly."
-        exit 1
-    fi
+    IFS=$" " dynamic_configs=$(echo "${params_output}" | tail -n 1)
     ARGS+=("--configDir" "${FLINK_CONF_DIR}" ${dynamic_configs[@]})
 
     export FLINK_INHERITED_LOGS="
