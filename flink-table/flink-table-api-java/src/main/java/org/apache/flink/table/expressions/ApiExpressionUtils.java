17aa82d9ba36e0a6bb3d9bc24ff04f276ffb0219
Dawid Wysakowicz <dwysakowicz@apache.org>
2020-03-30 07:48:42 +0200

    [FLINK-16830][table-api] Let users use Row/List/Map/Seq directly in Expression DSL
    This PR implements conversion logic from Row/List/Map/Seq to a corresponding Expression. From now on users can use those types in the Expression DSL without the need to converting those types manually.
    
    It is also a prerequisite to use those type directly in TableEnvironment#fromValues.

--- flink-table/flink-table-api-java/src/main/java/org/apache/flink/table/expressions/ApiExpressionUtils.java
+++ flink-table/flink-table-api-java/src/main/java/org/apache/flink/table/expressions/ApiExpressionUtils.java
@@ -24,15 +24,26 @@ import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.ValidationException;
 import org.apache.flink.table.functions.BuiltInFunctionDefinition;
+import org.apache.flink.table.functions.BuiltInFunctionDefinitions;
 import org.apache.flink.table.functions.FunctionDefinition;
 import org.apache.flink.table.functions.FunctionIdentifier;
 import org.apache.flink.table.functions.FunctionKind;
 import org.apache.flink.table.operations.QueryOperation;
 import org.apache.flink.table.types.DataType;
+import org.apache.flink.types.Row;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Utilities for API-specific {@link Expression}s.
@@ -52,14 +63,151 @@ public final class ApiExpressionUtils {
 		// private
 	}
 
+	/**
+	 * Converts a given object to an expression.
+	 *
+	 * <p>It converts:
+	 * <ul>
+	 *     <li>{@link Row} to a call to a row constructor expression</li>
+	 *     <li>{@link Map} to a call to a map constructor expression</li>
+	 *     <li>{@link List} to a call to an array constructor expression</li>
+	 *     <li>arrays to a call to an array constructor expression</li>
+	 *     <li>Scala's {@code Seq} to an array constructor via reflection</li>
+	 *     <li>Scala's {@code Map} to a map constructor via reflection</li>
+	 *     <li>Scala's {@code BigDecimal} to a DECIMAL literal</li>
+	 *     <li>if none of the above applies, the function tries to convert the object
+	 *          to a value literal with {@link #valueLiteral(Object)}</li>
+	 * </ul>
+	 *
+	 * @param expression An object to convert to an expression
+	 */
 	public static Expression objectToExpression(Object expression) {
 		if (expression instanceof ApiExpression) {
 			return ((ApiExpression) expression).toExpr();
 		} else if (expression instanceof Expression) {
 			return (Expression) expression;
+		} else if (expression instanceof Row) {
+			return convertRow((Row) expression);
+		} else if (expression instanceof Map) {
+			return convertJavaMap((Map<?, ?>) expression);
+		} else if (expression instanceof List) {
+			return convertJavaList((List<?>) expression);
+		} else if (expression.getClass().isArray()) {
+			return convertArray(expression);
 		} else {
-			return valueLiteral(expression);
+			return convertScala(expression).orElseGet(() -> valueLiteral(expression));
 		}
+	}
+
+	private static Expression convertRow(Row expression) {
+		List<Expression> fields = IntStream.range(0, expression.getArity())
+			.mapToObj(expression::getField)
+			.map(ApiExpressionUtils::objectToExpression)
+			.collect(Collectors.toList());
+
+		return unresolvedCall(BuiltInFunctionDefinitions.ROW, fields);
+	}
+
+	private static Expression convertJavaMap(Map<?, ?> expression) {
+		List<Expression> entries = expression.entrySet()
+			.stream()
+			.flatMap(e -> Stream.of(
+				objectToExpression(e.getKey()),
+				objectToExpression(e.getValue())
+			)).collect(Collectors.toList());
+
+		return unresolvedCall(BuiltInFunctionDefinitions.MAP, entries);
+	}
+
+	private static Expression convertJavaList(List<?> expression) {
+		List<Expression> entries = expression
+			.stream()
+			.map(ApiExpressionUtils::objectToExpression)
+			.collect(Collectors.toList());
+
+		return unresolvedCall(BuiltInFunctionDefinitions.ARRAY, entries);
+	}
+
+	private static Expression convertArray(Object expression) {
+		int length = Array.getLength(expression);
+		List<Expression> entries = IntStream.range(0, length)
+			.mapToObj(idx -> Array.get(expression, idx))
+			.map(ApiExpressionUtils::objectToExpression)
+			.collect(Collectors.toList());
+		return unresolvedCall(BuiltInFunctionDefinitions.ARRAY, entries);
+	}
+
+	private static Optional<Expression> convertScala(Object obj) {
+		try {
+			Optional<Expression> array = convertScalaSeq(obj);
+			if (array.isPresent()) {
+				return array;
+			}
+
+			Optional<Expression> bigDecimal = convertScalaBigDecimal(obj);
+			if (bigDecimal.isPresent()) {
+				return bigDecimal;
+			}
+
+			return convertScalaMap(obj);
+		} catch (Exception e) {
+			return Optional.empty();
+		}
+	}
+
+	private static Optional<Expression> convertScalaMap(Object obj)
+			throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+		Class<?> mapClass = Class.forName("scala.collection.Map");
+		if (mapClass.isAssignableFrom(obj.getClass())) {
+			Class<?> seqClass = Class.forName("scala.collection.Seq");
+			Class<?> productClass = Class.forName("scala.Product");
+			Method getElement = productClass.getMethod("productElement", int.class);
+			Method toSeq = mapClass.getMethod("toSeq");
+			Method getMethod = seqClass.getMethod("apply", Object.class);
+			Method lengthMethod = seqClass.getMethod("length");
+
+			Object mapAsSeq = toSeq.invoke(obj);
+			List<Expression> entries = new ArrayList<>();
+			for (int i = 0; i < (Integer) lengthMethod.invoke(mapAsSeq); i++) {
+				Object mapEntry = getMethod.invoke(mapAsSeq, i);
+
+				Object key = getElement.invoke(mapEntry, 0);
+				Object value = getElement.invoke(mapEntry, 1);
+				entries.add(objectToExpression(key));
+				entries.add(objectToExpression(value));
+			}
+
+			return Optional.of(unresolvedCall(BuiltInFunctionDefinitions.MAP, entries));
+		}
+		return Optional.empty();
+	}
+
+	private static Optional<Expression> convertScalaSeq(Object obj)
+			throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+		Class<?> seqClass = Class.forName("scala.collection.Seq");
+		if (seqClass.isAssignableFrom(obj.getClass())) {
+			Method getMethod = seqClass.getMethod("apply", Object.class);
+			Method lengthMethod = seqClass.getMethod("length");
+
+			List<Expression> entries = new ArrayList<>();
+			for (int i = 0; i < (Integer) lengthMethod.invoke(obj); i++) {
+				entries.add(objectToExpression(getMethod.invoke(obj, i)));
+			}
+
+			return Optional.of(unresolvedCall(BuiltInFunctionDefinitions.ARRAY, entries));
+		}
+		return Optional.empty();
+	}
+
+	private static Optional<Expression> convertScalaBigDecimal(Object obj)
+			throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+		Class<?> decimalClass = Class.forName("scala.math.BigDecimal");
+		if (decimalClass.equals(obj.getClass())) {
+			Method toJava = decimalClass.getMethod("bigDecimal");
+			BigDecimal bigDecimal = (BigDecimal) toJava.invoke(obj);
+			return Optional.of(valueLiteral(bigDecimal));
+		}
+		return Optional.empty();
 	}
 
 	public static Expression unwrapFromApi(Expression expression) {
