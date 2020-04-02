8d66baa1385079089462385072fc6a67831fb1f5
wangyang0918 <danrtsey.wy@alibaba-inc.com>
2020-04-02 16:50:15 +0800

    [FLINK-16921][e2e] Do not start/stop minikube in non-linux environment for k8s e2e tests

--- flink-end-to-end-tests/test-scripts/common_kubernetes.sh
+++ flink-end-to-end-tests/test-scripts/common_kubernetes.sh
@@ -27,6 +27,8 @@ MINIKUBE_START_RETRIES=3
 MINIKUBE_START_BACKOFF=5
 RESULT_HASH="e682ec6622b5e83f2eb614617d5ab2cf"
 
+NON_LINUX_ENV_NOTE="****** Please start/stop minikube manually in non-linux environment. ******"
+
 # If running tests on non-linux os, the kubectl and minikube should be installed manually
 function setup_kubernetes_for_linux {
     # Download kubectl, which is a requirement for using minikube.
@@ -53,32 +55,28 @@ function start_kubernetes_if_not_running {
     if ! check_kubernetes_status; then
         echo "Starting minikube ..."
         # We need sudo permission to set vm-driver to none in linux os.
-        if [[ "${OS_TYPE}" = "linux" ]] ; then
-            # tl;dr: Configure minikube for a low disk space environment
-            #
-            # The VMs provided by azure have ~100GB of disk space, out of which
-            # 85% are allocated, only 15GB are free. That's enough space
-            # for our purposes. However, the kubernetes nodes running during
-            # the k8s tests believe that 85% are not enough free disk space,
-            # so they start garbage collecting their host. During GCing, they
-            # are deleting all docker images currently not in use. 
-            # However, the k8s test is first building a flink image, then launching
-            # stuff on k8s. Sometimes, k8s deletes the new Flink images,
-            # thus it can not find them anymore, letting the test fail /
-            # timeout. That's why we have set the GC threshold to 98% and 99%
-            # here.
-            # Similarly, the kubelets are marking themself as "low disk space",
-            # causing Flink to avoid this node (again, failing the test)
-            sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none \
-                --extra-config=kubelet.image-gc-high-threshold=99 \
-                --extra-config=kubelet.image-gc-low-threshold=98 \
-                --extra-config=kubelet.minimum-container-ttl-duration=120m \
-                --extra-config=kubelet.eviction-hard="memory.available<5Mi,nodefs.available<1Mi,imagefs.available<1Mi" \
-                --extra-config=kubelet.eviction-soft="memory.available<5Mi,nodefs.available<2Mi,imagefs.available<2Mi" \
-                --extra-config=kubelet.eviction-soft-grace-period="memory.available=2h,nodefs.available=2h,imagefs.available=2h"
-        else
-            sudo minikube start
-        fi
+        # tl;dr: Configure minikube for a low disk space environment
+        #
+        # The VMs provided by azure have ~100GB of disk space, out of which
+        # 85% are allocated, only 15GB are free. That's enough space
+        # for our purposes. However, the kubernetes nodes running during
+        # the k8s tests believe that 85% are not enough free disk space,
+        # so they start garbage collecting their host. During GCing, they
+        # are deleting all docker images currently not in use.
+        # However, the k8s test is first building a flink image, then launching
+        # stuff on k8s. Sometimes, k8s deletes the new Flink images,
+        # thus it can not find them anymore, letting the test fail /
+        # timeout. That's why we have set the GC threshold to 98% and 99%
+        # here.
+        # Similarly, the kubelets are marking themself as "low disk space",
+        # causing Flink to avoid this node (again, failing the test)
+        sudo CHANGE_MINIKUBE_NONE_USER=true minikube start --vm-driver=none \
+            --extra-config=kubelet.image-gc-high-threshold=99 \
+            --extra-config=kubelet.image-gc-low-threshold=98 \
+            --extra-config=kubelet.minimum-container-ttl-duration=120m \
+            --extra-config=kubelet.eviction-hard="memory.available<5Mi,nodefs.available<1Mi,imagefs.available<1Mi" \
+            --extra-config=kubelet.eviction-soft="memory.available<5Mi,nodefs.available<2Mi,imagefs.available<2Mi" \
+            --extra-config=kubelet.eviction-soft-grace-period="memory.available=2h,nodefs.available=2h,imagefs.available=2h"
         # Fix the kubectl context, as it's often stale.
         minikube update-context
     fi
@@ -88,21 +86,31 @@ function start_kubernetes_if_not_running {
 }
 
 function start_kubernetes {
-    [[ "${OS_TYPE}" = "linux" ]] && setup_kubernetes_for_linux
-    if ! retry_times ${MINIKUBE_START_RETRIES} ${MINIKUBE_START_BACKOFF} start_kubernetes_if_not_running; then
-        echo "Could not start minikube. Aborting..."
-        exit 1
+    if [[ "${OS_TYPE}" != "linux" ]]; then
+        if ! check_kubernetes_status; then
+            echo "$NON_LINUX_ENV_NOTE"
+            exit 1
+        fi
+    else
+        setup_kubernetes_for_linux
+        if ! retry_times ${MINIKUBE_START_RETRIES} ${MINIKUBE_START_BACKOFF} start_kubernetes_if_not_running; then
+            echo "Could not start minikube. Aborting..."
+            exit 1
+        fi
     fi
     eval $(minikube docker-env)
 }
 
 function stop_kubernetes {
-    echo "Stopping minikube ..."
-    stop_command="minikube stop"
-    [[ "${OS_TYPE}" = "linux" ]] && stop_command="sudo ${stop_command}"
-    if ! retry_times ${MINIKUBE_START_RETRIES} ${MINIKUBE_START_BACKOFF} "${stop_command}"; then
-        echo "Could not stop minikube. Aborting..."
-        exit 1
+    if [[ "${OS_TYPE}" != "linux" ]]; then
+        echo "$NON_LINUX_ENV_NOTE"
+    else
+        echo "Stopping minikube ..."
+        stop_command="sudo minikube stop"
+        if ! retry_times ${MINIKUBE_START_RETRIES} ${MINIKUBE_START_BACKOFF} "${stop_command}"; then
+            echo "Could not stop minikube. Aborting..."
+            exit 1
+        fi
     fi
 }
 
