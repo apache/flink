473ce29a89e212eefc8e9a647aa560e6dce6209b
Yun Tang <myasuka@live.com>
2020-04-14 02:20:32 +0800

    [FLINK-14652][checkpointing] Refactor notifyCheckpointComplete to SubtaskCheckpointCoordinator
    This closes #11721.

--- flink-runtime/src/test/java/org/apache/flink/runtime/state/TestTaskStateManager.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/state/TestTaskStateManager.java
@@ -46,6 +46,7 @@ import java.util.Map;
 public class TestTaskStateManager implements TaskStateManager {
 
 	private long reportedCheckpointId;
+	private long notifiedCompletedCheckpointId;
 
 	private JobID jobId;
 	private ExecutionAttemptID executionAttemptID;
@@ -86,6 +87,7 @@ public class TestTaskStateManager implements TaskStateManager {
 		this.jobManagerTaskStateSnapshotsByCheckpointId = new HashMap<>();
 		this.taskManagerTaskStateSnapshotsByCheckpointId = new HashMap<>();
 		this.reportedCheckpointId = -1L;
+		this.notifiedCompletedCheckpointId = -1L;
 	}
 
 	@Override
@@ -170,7 +172,7 @@ public class TestTaskStateManager implements TaskStateManager {
 
 	@Override
 	public void notifyCheckpointComplete(long checkpointId) throws Exception {
-
+		this.notifiedCompletedCheckpointId = checkpointId;
 	}
 
 	public JobID getJobId() {
@@ -219,6 +221,10 @@ public class TestTaskStateManager implements TaskStateManager {
 
 	public long getReportedCheckpointId() {
 		return reportedCheckpointId;
+	}
+
+	public long getNotifiedCompletedCheckpointId() {
+		return notifiedCompletedCheckpointId;
 	}
 
 	public void setReportedCheckpointId(long reportedCheckpointId) {
