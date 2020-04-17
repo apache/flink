6db2037e6774e1adf36a4718ad35ef8ad818776e
Jark Wu <jark@apache.org>
2020-04-17 22:11:44 +0800

    [FLINK-17169][core] Add shortString() method to RowKind to get short string representation
    This closes #11797

--- flink-core/src/main/java/org/apache/flink/types/RowKind.java
+++ flink-core/src/main/java/org/apache/flink/types/RowKind.java
@@ -29,7 +29,7 @@ public enum RowKind {
 	/**
 	 * Insertion operation.
 	 */
-	INSERT,
+	INSERT("+I", (byte) 0),
 
 	/**
 	 * Update operation with the previous content of the updated row.
@@ -38,7 +38,7 @@ public enum RowKind {
 	 * to retract the previous row first. It is useful in cases of a non-idempotent update, i.e., an
 	 * update of a row that is not uniquely identifiable by a key.
 	 */
-	UPDATE_BEFORE,
+	UPDATE_BEFORE("-U", (byte) 1),
 
 	/**
 	 * Update operation with new content of the updated row.
@@ -47,10 +47,69 @@ public enum RowKind {
 	 * needs to retract the previous row first. OR it describes an idempotent update, i.e., an update
 	 * of a row that is uniquely identifiable by a key.
 	 */
-	UPDATE_AFTER,
+	UPDATE_AFTER("+U", (byte) 2),
 
 	/**
 	 * Deletion operation.
 	 */
-	DELETE
+	DELETE("-D", (byte) 3);
+
+	private final String shortString;
+
+	private final byte value;
+
+	/**
+	 * Creates a {@link RowKind} enum with the given short string and byte value representation of
+	 * the {@link RowKind}.
+	 */
+	RowKind(String shortString, byte value) {
+		this.shortString = shortString;
+		this.value = value;
+	}
+
+	/**
+	 * Returns a short string representation of this {@link RowKind}.
+	 *
+	 * <p><ul>
+	 *     <li>"+I" represents {@link #INSERT}.</li>
+	 *     <li>"-U" represents {@link #UPDATE_BEFORE}.</li>
+	 *     <li>"+U" represents {@link #UPDATE_AFTER}.</li>
+	 *     <li>"-D" represents {@link #DELETE}.</li>
+	 * </ul>
+	 */
+	public String shortString() {
+		return shortString;
+	}
+
+	/**
+	 * Returns the byte value representation of this {@link RowKind}. The byte value is used
+	 * for serialization and deserialization.
+	 *
+	 * <p><ul>
+	 * 	 <li>"0" represents {@link #INSERT}.</li>
+	 * 	 <li>"1" represents {@link #UPDATE_BEFORE}.</li>
+	 * 	 <li>"2" represents {@link #UPDATE_AFTER}.</li>
+	 * 	 <li>"3" represents {@link #DELETE}.</li>
+	 * </ul>
+	 */
+	public byte toByteValue() {
+		return value;
+	}
+
+	/**
+	 * Creates a {@link RowKind} from the given byte value. Each {@link RowKind} has a byte
+	 * value representation.
+	 *
+	 * @see #toByteValue() for mapping of byte value and {@link RowKind}.
+	 */
+	public static RowKind fromByteValue(byte value) {
+		switch (value) {
+			case 0: return INSERT;
+			case 1: return UPDATE_BEFORE;
+			case 2: return UPDATE_AFTER;
+			case 3: return DELETE;
+			default: throw new UnsupportedOperationException(
+				"Unsupported byte value '" + value + "' for row kind.");
+		}
+	}
 }
