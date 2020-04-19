2670def9536876df7899cf712e329b0c6f70f692
Dian Fu <dianfu@apache.org>
2020-04-19 12:25:51 +0800

    [FLINK-17068][python][tests] Ensure the permission of scripts set correctly before executing the Python tests

--- flink-python/dev/lint-python.sh
+++ flink-python/dev/lint-python.sh
@@ -526,6 +526,10 @@ function tox_check() {
     print_function "STAGE" "tox checks"
     # Set created py-env in $PATH for tox's creating virtual env
     activate
+    # Ensure the permission of the scripts set correctly
+    chmod +x $FLINK_PYTHON_DIR/../build-target/bin/pyflink-gateway-server.sh
+    chmod +x $FLINK_PYTHON_DIR/../build-target/bin/pyflink-shell.sh
+
     $TOX_PATH -c $FLINK_PYTHON_DIR/tox.ini --recreate 2>&1 | tee -a $LOG_FILE
 
     TOX_RESULT=$((grep -c "congratulations :)" "$LOG_FILE") 2>&1)
