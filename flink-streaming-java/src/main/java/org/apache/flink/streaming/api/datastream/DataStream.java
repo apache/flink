d249dff765b852cb2b7e6bd9ed81ffb15d9da2bb
Etienne Chauchot <echauchot@apache.org>
2020-04-23 12:44:41 +0200

    [FLINK-17074]Deprecate java Datastream.partitionCustom methods that do not use KeySelector

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/datastream/DataStream.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/datastream/DataStream.java
@@ -354,11 +354,13 @@ public class DataStream<T> {
 	 * This method takes the key position to partition on, and a partitioner that accepts the key type.
 	 *
 	 * <p>Note: This method works only on single field keys.
+	 * @deprecated use {@link DataStream#partitionCustom(Partitioner, KeySelector)}.
 	 *
 	 * @param partitioner The partitioner to assign partitions to keys.
 	 * @param field The field index on which the DataStream is partitioned.
 	 * @return The partitioned DataStream.
 	 */
+	@Deprecated
 	public <K> DataStream<T> partitionCustom(Partitioner<K> partitioner, int field) {
 		Keys.ExpressionKeys<T> outExpressionKeys = new Keys.ExpressionKeys<>(new int[]{field}, getType());
 		return partitionCustom(partitioner, outExpressionKeys);
@@ -369,11 +371,13 @@ public class DataStream<T> {
 	 * This method takes the key expression to partition on, and a partitioner that accepts the key type.
 	 *
 	 * <p>Note: This method works only on single field keys.
+	 * @deprecated use {@link DataStream#partitionCustom(Partitioner, KeySelector)}.
 	 *
 	 * @param partitioner The partitioner to assign partitions to keys.
 	 * @param field The expression for the field on which the DataStream is partitioned.
 	 * @return The partitioned DataStream.
 	 */
+	@Deprecated
 	public <K> DataStream<T> partitionCustom(Partitioner<K> partitioner, String field) {
 		Keys.ExpressionKeys<T> outExpressionKeys = new Keys.ExpressionKeys<>(new String[]{field}, getType());
 		return partitionCustom(partitioner, outExpressionKeys);
