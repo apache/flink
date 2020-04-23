1849f3aa31db5c019b3eb792a7be544077c8fea5
Dawid Wysakowicz <dwysakowicz@apache.org>
2020-04-23 10:47:49 +0200

    [FLINK-17306] Add open to KafkaSerializationSchema

--- flink-connectors/flink-connector-kafka-base/src/main/java/org/apache/flink/streaming/connectors/kafka/KafkaSerializationSchema.java
+++ flink-connectors/flink-connector-kafka-base/src/main/java/org/apache/flink/streaming/connectors/kafka/KafkaSerializationSchema.java
@@ -18,6 +18,7 @@
 package org.apache.flink.streaming.connectors.kafka;
 
 import org.apache.flink.annotation.PublicEvolving;
+import org.apache.flink.api.common.serialization.SerializationSchema;
 
 import org.apache.kafka.clients.producer.ProducerRecord;
 
@@ -38,6 +39,18 @@ import java.io.Serializable;
  */
 @PublicEvolving
 public interface KafkaSerializationSchema<T> extends Serializable {
+
+	/**
+	 * Initialization method for the schema. It is called before the actual working methods
+	 * {@link #serialize(Object, Long)} and thus suitable for one time setup work.
+	 *
+	 * <p>The provided {@link SerializationSchema.InitializationContext} can be used to access additional
+	 * features such as e.g. registering user metrics.
+	 *
+	 * @param context Contextual information that can be used during initialization.
+	 */
+	default void open(SerializationSchema.InitializationContext context) throws Exception {
+	}
 
 	/**
 	 * Serializes given element and returns it as a {@link ProducerRecord}.
