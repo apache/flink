b1316cec224ccda73e1eed8226c0b5b61f2c6e21
Zhu Zhu <reedpor@gmail.com>
2020-04-24 11:39:36 +0800

    [FLINK-17020][runtime] Introduce GlobalDataExchangeMode for JobGraph generation

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/graph/StreamGraph.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/graph/StreamGraph.java
@@ -96,11 +96,7 @@ public class StreamGraph implements Pipeline {
 
 	private TimeCharacteristic timeCharacteristic;
 
-	/**
-	 * If there are some stream edges that can not be chained and the shuffle mode of edge is not
-	 * specified, translate these edges into {@code BLOCKING} result partition type.
-	 */
-	private boolean blockingConnectionsBetweenChains;
+	private GlobalDataExchangeMode globalDataExchangeMode;
 
 	/** Flag to indicate whether to put all vertices into the same slot sharing group by default. */
 	private boolean allVerticesInSameSlotSharingGroupByDefault = true;
@@ -201,20 +197,12 @@ public class StreamGraph implements Pipeline {
 		this.timeCharacteristic = timeCharacteristic;
 	}
 
-	/**
-	 * If there are some stream edges that can not be chained and the shuffle mode of edge is not
-	 * specified, translate these edges into {@code BLOCKING} result partition type.
-	 */
-	public boolean isBlockingConnectionsBetweenChains() {
-		return blockingConnectionsBetweenChains;
+	public GlobalDataExchangeMode getGlobalDataExchangeMode() {
+		return globalDataExchangeMode;
 	}
 
-	/**
-	 * If there are some stream edges that can not be chained and the shuffle mode of edge is not
-	 * specified, translate these edges into {@code BLOCKING} result partition type.
-	 */
-	public void setBlockingConnectionsBetweenChains(boolean blockingConnectionsBetweenChains) {
-		this.blockingConnectionsBetweenChains = blockingConnectionsBetweenChains;
+	public void setGlobalDataExchangeMode(GlobalDataExchangeMode globalDataExchangeMode) {
+		this.globalDataExchangeMode = globalDataExchangeMode;
 	}
 
 	/**
