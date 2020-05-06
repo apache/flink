c5e67bf6cfb59353b1109c060b82820920d30ff8
Leonard Xu <xbjtdcq@gmail.com>
2020-05-06 09:56:57 +0800

    address comment

--- flink-table/flink-table-api-java/src/test/java/org/apache/flink/table/typeutils/FieldInfoUtilsTest.java
+++ flink-table/flink-table-api-java/src/test/java/org/apache/flink/table/typeutils/FieldInfoUtilsTest.java
@@ -20,75 +20,135 @@ package org.apache.flink.table.typeutils;
 
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.typeutils.PojoField;
+import org.apache.flink.api.java.typeutils.PojoTypeInfo;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
-import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.expressions.Expression;
 import org.apache.flink.table.expressions.UnresolvedCallExpression;
 import org.apache.flink.table.expressions.UnresolvedReferenceExpression;
 import org.apache.flink.table.expressions.ValueLiteralExpression;
 import org.apache.flink.table.functions.BuiltInFunctionDefinitions;
-import org.apache.flink.table.functions.FunctionIdentifier;
-import org.apache.flink.table.types.DataType;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.Collection;
+
+import static org.apache.flink.table.functions.BuiltInFunctionDefinitions.PROCTIME;
 
 /**
  * Test suite for {@link FieldInfoUtils}.
  */
+@RunWith(Enclosed.class)
 public class FieldInfoUtilsTest {
 
-	private static final RowTypeInfo typeInfo = new RowTypeInfo(
-		new TypeInformation[]{Types.INT, Types.LONG, Types.STRING},
-		new String[]{"f0", "f1", "f2"});
+	/**
+	 * Test for ByNameMode.
+	 */
+	@RunWith(Parameterized.class)
+	public static final class TestByNameMode {
 
-	@Test
-	public void testByPositionMode() {
-		FieldInfoUtils.TypeInfoSchema schema = FieldInfoUtils.getFieldsInfo(
-			typeInfo,
-			new Expression[]{
-				new UnresolvedReferenceExpression("aa"),
-				new UnresolvedReferenceExpression("bb"),
-				new UnresolvedReferenceExpression("cc")});
+		@Parameterized.Parameters(name = "{0}")
+		public static Collection<TypeInformation> parameters() throws Exception {
+			return Arrays.asList(
+				new RowTypeInfo(
+					new TypeInformation[]{Types.INT, Types.LONG, Types.SQL_TIMESTAMP},
+					new String[]{"f0", "f1", "f2"}),
+				new PojoTypeInfo(MyPojo.class, Arrays.asList(
+					new PojoField(MyPojo.class.getDeclaredField("f0"), Types.INT),
+					new PojoField(MyPojo.class.getDeclaredField("f1"), Types.LONG),
+					new PojoField(MyPojo.class.getDeclaredField("f2"), Types.SQL_TIMESTAMP))));
+		}
 
-		Assert.assertEquals("[aa, bb, cc]", Arrays.asList(schema.getFieldNames()).toString());
-		Assert.assertArrayEquals(new DataType[]{DataTypes.INT(), DataTypes.BIGINT(), DataTypes.STRING()}, schema.getFieldTypes());
+		@Parameterized.Parameter
+		public TypeInformation typeInfo;
+
+		@Test
+		public void testByNameModeReorder() {
+			FieldInfoUtils.TypeInfoSchema schema = FieldInfoUtils.getFieldsInfo(
+				typeInfo,
+				new Expression[]{
+					new UnresolvedReferenceExpression("f2"),
+					new UnresolvedReferenceExpression("f1"),
+					new UnresolvedReferenceExpression("f0")});
+
+			Assert.assertArrayEquals(new String[]{"f2", "f1", "f0"}, schema.getFieldNames());
+		}
+
+		@Test
+		public void testByNameModeReorderAndRename() {
+			FieldInfoUtils.TypeInfoSchema schema = FieldInfoUtils.getFieldsInfo(
+				typeInfo,
+				new Expression[]{
+					new UnresolvedCallExpression(
+						BuiltInFunctionDefinitions.AS,
+						Arrays.asList(new UnresolvedReferenceExpression("f1"), new ValueLiteralExpression("aa"))),
+					new UnresolvedCallExpression(
+						BuiltInFunctionDefinitions.AS,
+						Arrays.asList(new UnresolvedReferenceExpression("f0"), new ValueLiteralExpression("bb"))),
+					new UnresolvedCallExpression(
+						BuiltInFunctionDefinitions.AS,
+						Arrays.asList(new UnresolvedReferenceExpression("f2"), new ValueLiteralExpression("cc")))
+				});
+
+			Assert.assertArrayEquals(new String[]{"aa", "bb", "cc"}, schema.getFieldNames());
+		}
+
+		/**
+		 * Test Pojo class.
+		 */
+		public static class MyPojo {
+			public int f0;
+			public long f1;
+			public Timestamp f2;
+
+			public MyPojo() {
+			}
+		}
 	}
 
-	@Test
-	public void testByNameModeReorder() {
-		FieldInfoUtils.TypeInfoSchema schema = FieldInfoUtils.getFieldsInfo(
-			typeInfo,
-			new Expression[]{
-				new UnresolvedReferenceExpression("f2"),
-				new UnresolvedReferenceExpression("f1"),
-				new UnresolvedReferenceExpression("f0")});
+	/**
+	 * Test for ByPositionMode.
+	 */
+	public static final class TestByPositionMode {
+		private static final RowTypeInfo rowTypeInfo = new RowTypeInfo(
+			new TypeInformation[]{Types.INT, Types.LONG, Types.SQL_TIMESTAMP},
+			new String[]{"f0", "f1", "f2"});
 
-		Assert.assertEquals("[f2, f1, f0]", Arrays.asList(schema.getFieldNames()).toString());
-		Assert.assertArrayEquals(new DataType[]{DataTypes.STRING(), DataTypes.BIGINT(), DataTypes.INT()}, schema.getFieldTypes());
-	}
+		@Test
+		public void testByPositionMode() {
+			FieldInfoUtils.TypeInfoSchema schema = FieldInfoUtils.getFieldsInfo(
+				rowTypeInfo,
+				new Expression[]{
+					new UnresolvedReferenceExpression("aa"),
+					new UnresolvedReferenceExpression("bb"),
+					new UnresolvedReferenceExpression("cc")});
 
-	@Test
-	public void testByNameModeReorderAndRename() {
-		FieldInfoUtils.TypeInfoSchema schema = FieldInfoUtils.getFieldsInfo(
-			typeInfo,
-			new Expression[]{
-				new UnresolvedCallExpression(
-					FunctionIdentifier.of("as"),
-					BuiltInFunctionDefinitions.AS,
-					Arrays.asList(new UnresolvedReferenceExpression("f1"), new ValueLiteralExpression("aa"))),
-				new UnresolvedCallExpression(
-					FunctionIdentifier.of("as"),
-					BuiltInFunctionDefinitions.AS,
-					Arrays.asList(new UnresolvedReferenceExpression("f0"), new ValueLiteralExpression("bb"))),
-				new UnresolvedCallExpression(
-					FunctionIdentifier.of("as"),
-					BuiltInFunctionDefinitions.AS,
-					Arrays.asList(new UnresolvedReferenceExpression("f2"), new ValueLiteralExpression("cc")))});
+			Assert.assertArrayEquals(new String[]{"aa", "bb", "cc"}, schema.getFieldNames());
+		}
 
-		Assert.assertEquals("[aa, bb, cc]", Arrays.asList(schema.getFieldNames()).toString());
-		Assert.assertArrayEquals(new DataType[]{DataTypes.BIGINT(), DataTypes.INT(), DataTypes.STRING()}, schema.getFieldTypes());
+		@Test
+		public void testByPositionModeProcTime() {
+			FieldInfoUtils.TypeInfoSchema schema = FieldInfoUtils.getFieldsInfo(
+				rowTypeInfo,
+				new Expression[]{
+					new UnresolvedReferenceExpression("aa"),
+					new UnresolvedReferenceExpression("bb"),
+					new UnresolvedReferenceExpression("cc"),
+					new UnresolvedCallExpression(
+						BuiltInFunctionDefinitions.AS,
+						Arrays.asList(
+							new UnresolvedCallExpression(
+								PROCTIME,
+								Arrays.asList(new UnresolvedReferenceExpression("cc"))),
+							new ValueLiteralExpression("proctime")))});
+
+			Assert.assertArrayEquals(new String[]{"aa", "bb", "cc", "proctime"}, schema.getFieldNames());
+		}
 	}
 }
