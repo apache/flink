6a49f5ba8580633eaed6487bc1cf34047bdc8cf4
Wei Zhong <weizhong0618@gmail.com>
2020-05-11 17:11:20 +0800

    [FLINK-15138][e2e][python] Add PyFlink e2e tests.

--- flink-end-to-end-tests/run-nightly-tests.sh
+++ flink-end-to-end-tests/run-nightly-tests.sh
@@ -214,6 +214,8 @@ run_test "Dependency shading of table modules test" "$END_TO_END_DIR/test-script
 
 run_test "Shaded Hadoop S3A with credentials provider end-to-end test" "$END_TO_END_DIR/test-scripts/test_batch_wordcount.sh hadoop_with_provider"
 
+run_test "PyFlink end-to-end test" "$END_TO_END_DIR/test-scripts/test_pyflink.sh" "skip_check_exceptions"
+
 ################################################################################
 # Sticky Scheduling
 ################################################################################
