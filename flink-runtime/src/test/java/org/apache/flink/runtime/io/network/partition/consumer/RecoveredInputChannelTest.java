austince/flink-ci-flink

[FLINK-16536][network][checkpointing] Implement InputChannel state recovery for unaligned checkpoint
During recovery process for unaligned checkpoint, the input channel state should also be recovered besides with existing operator states.

We considered three guarantees during the implementation:
1. Make input recovery happen after the output recovery for providing more floating buffers on output side firstly.
2. Make partition request happen after input recovery for avoiding new data overtaking the previous state data.
3. Introduce a dedicated single IO executor for unspilling the channel state one by one, to avoid potential random IO.

This closes #11687.

flink-runtime/src/test/java/org/apache/flink/runtime/io/network/partition/consumer/RecoveredInputChannelTest.java
@@ -1,0 +1,277 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.io.network.partition.consumer;
+
+import org.apache.flink.runtime.checkpoint.channel.ChannelStateReader;
+import org.apache.flink.runtime.execution.CancelTaskException;
+import org.apache.flink.runtime.io.network.buffer.Buffer;
+import org.apache.flink.runtime.io.network.buffer.BufferBuilderAndConsumerTest;
+import org.apache.flink.runtime.io.network.buffer.NetworkBufferPool;
+import org.apache.flink.runtime.io.network.partition.ResultPartitionTest;
+import org.apache.flink.runtime.io.network.partition.consumer.InputChannel.BufferAndAvailability;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.apache.flink.runtime.io.network.partition.ResultPartitionTest.ChannelStateReaderWithException;
+import static org.apache.flink.runtime.io.network.partition.consumer.RemoteInputChannelTest.cleanup;
+import static org.apache.flink.runtime.io.network.partition.consumer.RemoteInputChannelTest.submitTasksAndWaitForResults;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests for {@link RecoveredInputChannel}.
+ */
+@RunWith(Parameterized.class)
+public class RecoveredInputChannelTest {
+
+	private final boolean isRemote;
+
+	@Parameterized.Parameters(name = "isRemote = {0}")
+	public static Collection<Object[]> parameters() {
+		return Arrays.asList(new Object[][] {
+			{true},
+			{false},
+		});
+	}
+
+	public RecoveredInputChannelTest(boolean isRemote) {
+		this.isRemote = isRemote;
+	}
+
+	@Test
+	public void testConcurrentReadStateAndProcess() throws Exception {
+		testConcurrentReadStateAndProcess(isRemote);
+	}
+
+	@Test
+	public void testConcurrentReadStateAndRelease() throws Exception {
+		testConcurrentReadStateAndRelease(isRemote);
+	}
+
+	@Test
+	public void testConcurrentReadStateAndProcessAndRelease() throws Exception {
+		testConcurrentReadStateAndProcessAndRelease(isRemote);
+	}
+
+	/**
+	 * Tests that there are no buffers leak while recovering the empty input channel state.
+	 */
+	@Test
+	public void testReadEmptyState() throws Exception {
+		testReadEmptyStateOrThrowException(isRemote, ChannelStateReader.NO_OP);
+	}
+
+	/**
+	 * Tests that there are no buffers leak while throwing exception during state recovery.
+	 */
+	@Test(expected = IOException.class)
+	public void testReadStateWithException() throws Exception {
+		testReadEmptyStateOrThrowException(isRemote, new ChannelStateReaderWithException());
+	}
+
+	private void testReadEmptyStateOrThrowException(boolean isRemote, ChannelStateReader reader) throws Exception {
+		// setup
+		final int totalBuffers = 10;
+		final NetworkBufferPool globalPool = new NetworkBufferPool(totalBuffers, 32, 2);
+		final SingleInputGate inputGate = createInputGate(globalPool);
+		final RecoveredInputChannel inputChannel = createRecoveredChannel(isRemote, inputGate);
+
+		try {
+			inputGate.setInputChannels(inputChannel);
+			inputGate.setup();
+
+			inputChannel.readRecoveredState(reader);
+			assertEquals(0, inputChannel.getNumberOfQueuedBuffers());
+		} finally {
+			// cleanup and verify no buffer leak
+			inputChannel.releaseAllResources();
+			globalPool.destroyAllBufferPools();
+			assertEquals(totalBuffers, globalPool.getNumberOfAvailableMemorySegments());
+			globalPool.destroy();
+		}
+	}
+
+	/**
+	 * Tests that the process of reading recovered state executes concurrently with channel
+	 * buffer processing, based on the condition of the total number of states is more that
+	 * the total buffer amount, to confirm that the lifecycle(recycle) of exclusive/floating
+	 * buffers works well.
+	 */
+	private void testConcurrentReadStateAndProcess(boolean isRemote) throws Exception {
+		// setup
+		final int totalBuffers = 10;
+		final NetworkBufferPool globalPool = new NetworkBufferPool(totalBuffers, 32, 2);
+		final SingleInputGate inputGate = createInputGate(globalPool);
+		final RecoveredInputChannel inputChannel = createRecoveredChannel(isRemote, inputGate);
+
+		// the number of states is more that the total buffer amount
+		final int totalStates = 15;
+		final int[] states = {1, 2, 3, 4};
+		final ChannelStateReader reader = new ResultPartitionTest.FiniteChannelStateReader(totalStates, states);
+		final ExecutorService executor = Executors.newFixedThreadPool(2);
+
+		Throwable thrown = null;
+		try {
+			inputGate.setInputChannels(inputChannel);
+			inputGate.setup();
+
+			final Callable<Void> processTask = processRecoveredBufferTask(inputChannel, totalStates, states);
+			final Callable<Void> readStateTask = readRecoveredStateTask(inputChannel, reader);
+
+			submitTasksAndWaitForResults(executor, new Callable[] {readStateTask, processTask});
+		} catch (Throwable t) {
+			thrown = t;
+		} finally {
+			// cleanup
+			cleanup(globalPool, executor, null, thrown, inputChannel);
+		}
+	}
+
+	private void testConcurrentReadStateAndRelease(boolean isRemote) throws Exception {
+		// setup
+		final int totalBuffers = 10;
+		final NetworkBufferPool globalPool = new NetworkBufferPool(totalBuffers, 32, 2);
+		final SingleInputGate inputGate = createInputGate(globalPool);
+		final RecoveredInputChannel inputChannel = createRecoveredChannel(isRemote, inputGate);
+
+		// the number of states is more that the total buffer amount
+		final int totalStates = 15;
+		final int[] states = {1, 2, 3, 4};
+		final ChannelStateReader reader = new ResultPartitionTest.FiniteChannelStateReader(totalStates, states);
+		final ExecutorService executor = Executors.newFixedThreadPool(2);
+
+		Throwable thrown = null;
+		try {
+			inputGate.setInputChannels(inputChannel);
+			inputGate.setup();
+
+			submitTasksAndWaitForResults(
+				executor,
+				new Callable[] {readRecoveredStateTask(inputChannel, reader), releaseChannelTask(inputChannel)});
+		} catch (Throwable t) {
+			thrown = t;
+		} finally {
+			// cleanup
+			cleanup(globalPool, executor, null, thrown, inputChannel);
+		}
+	}
+
+	private void testConcurrentReadStateAndProcessAndRelease(boolean isRemote) throws Exception {
+		// setup
+		final int totalBuffers = 10;
+		final NetworkBufferPool globalPool = new NetworkBufferPool(totalBuffers, 32, 2);
+		final SingleInputGate inputGate = createInputGate(globalPool);
+		final RecoveredInputChannel inputChannel = createRecoveredChannel(isRemote, inputGate);
+
+		// the number of states is more that the total buffer amount
+		final int totalStates = 15;
+		final int[] states = {1, 2, 3, 4};
+		final ChannelStateReader reader = new ResultPartitionTest.FiniteChannelStateReader(totalStates, states);
+		final ExecutorService executor = Executors.newFixedThreadPool(2);
+		Throwable thrown = null;
+		try {
+			inputGate.setInputChannels(inputChannel);
+			inputGate.setup();
+
+			final Callable<Void> processTask = processRecoveredBufferTask(inputChannel, totalStates, states);
+			final Callable<Void> readStateTask = readRecoveredStateTask(inputChannel, reader);
+			final Callable<Void> releaseTask = releaseChannelTask(inputChannel);
+
+			submitTasksAndWaitForResults(executor, new Callable[] {readStateTask, processTask, releaseTask});
+		} catch (Throwable t) {
+			thrown = t;
+		} finally {
+			// cleanup
+			cleanup(globalPool, executor, null, thrown, inputChannel);
+		}
+	}
+
+	private Callable<Void> readRecoveredStateTask(RecoveredInputChannel inputChannel, ChannelStateReader reader) {
+		return () -> {
+			try {
+				inputChannel.readRecoveredState(reader);
+			} catch (CancelTaskException ex) {
+				// within expectation
+			}
+
+			return null;
+		};
+	}
+
+	private Callable<Void> processRecoveredBufferTask(RecoveredInputChannel inputChannel, int totalStates, int[] states) {
+		return () -> {
+			// process all the queued state buffers and verify the data
+			int numProcessedStates = 0;
+			while (numProcessedStates < totalStates) {
+				if (inputChannel.isReleased()) {
+					break;
+				}
+				if (inputChannel.getNumberOfQueuedBuffers() == 0) {
+					Thread.sleep(1);
+					continue;
+				}
+				try {
+					Optional<BufferAndAvailability> bufferAndAvailability = inputChannel.getNextBuffer();
+					if (bufferAndAvailability.isPresent()) {
+						Buffer buffer = bufferAndAvailability.get().buffer();
+						BufferBuilderAndConsumerTest.assertContent(buffer, null, states);
+						buffer.recycleBuffer();
+						numProcessedStates++;
+					}
+				} catch (IllegalStateException e) {
+					assertTrue(e.getMessage().contains("Queried for a buffer after channel has been closed"));
+				}
+			}
+
+			return null;
+		};
+	}
+
+	private Callable<Void> releaseChannelTask(RecoveredInputChannel inputChannel) {
+		return () -> {
+			inputChannel.releaseAllResources();
+			return null;
+		};
+	}
+
+	private RecoveredInputChannel createRecoveredChannel(boolean isRemote, SingleInputGate gate) {
+		if (isRemote) {
+			return new InputChannelBuilder().buildRemoteChannel(gate);
+		} else {
+			return new InputChannelBuilder().buildLocalChannel(gate);
+		}
+	}
+
+	private SingleInputGate createInputGate(NetworkBufferPool globalPool) throws Exception {
+		return new SingleInputGateBuilder()
+			.setBufferPoolFactory(globalPool.createBufferPool(8, 8))
+			.setSegmentProvider(globalPool)
+			.build();
+	}
+}

apache/flink

flink-runtime/src/test/java/org/apache/flink/runtime/io/network/partition/consumer/RecoveredInputChannelTest.java
@@ -1,0 +1,283 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.io.network.partition.consumer;
+
+import org.apache.flink.runtime.checkpoint.channel.ChannelStateReader;
+import org.apache.flink.runtime.execution.CancelTaskException;
+import org.apache.flink.runtime.io.network.buffer.Buffer;
+import org.apache.flink.runtime.io.network.buffer.BufferBuilderAndConsumerTest;
+import org.apache.flink.runtime.io.network.buffer.NetworkBufferPool;
+import org.apache.flink.runtime.io.network.partition.ResultPartitionTest;
+import org.apache.flink.runtime.io.network.partition.consumer.InputChannel.BufferAndAvailability;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.apache.flink.runtime.io.network.partition.ResultPartitionTest.ChannelStateReaderWithException;
+import static org.apache.flink.runtime.io.network.partition.consumer.RemoteInputChannelTest.cleanup;
+import static org.apache.flink.runtime.io.network.partition.consumer.RemoteInputChannelTest.submitTasksAndWaitForResults;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests for {@link RecoveredInputChannel}.
+ */
+@RunWith(Parameterized.class)
+public class RecoveredInputChannelTest {
+
+	private final boolean isRemote;
+
+	@Parameterized.Parameters(name = "isRemote = {0}")
+	public static Collection<Object[]> parameters() {
+		return Arrays.asList(new Object[][] {
+			{true},
+			{false},
+		});
+	}
+
+	public RecoveredInputChannelTest(boolean isRemote) {
+		this.isRemote = isRemote;
+	}
+
+	@Test
+	public void testConcurrentReadStateAndProcess() throws Exception {
+		testConcurrentReadStateAndProcess(isRemote);
+	}
+
+	@Test
+	public void testConcurrentReadStateAndRelease() throws Exception {
+		testConcurrentReadStateAndRelease(isRemote);
+	}
+
+	@Test
+	public void testConcurrentReadStateAndProcessAndRelease() throws Exception {
+		testConcurrentReadStateAndProcessAndRelease(isRemote);
+	}
+
+	/**
+	 * Tests that there are no buffers leak while recovering the empty input channel state.
+	 */
+	@Test
+	public void testReadEmptyState() throws Exception {
+		testReadEmptyStateOrThrowException(isRemote, ChannelStateReader.NO_OP);
+	}
+
+	/**
+	 * Tests that there are no buffers leak while throwing exception during state recovery.
+	 */
+	@Test(expected = IOException.class)
+	public void testReadStateWithException() throws Exception {
+		testReadEmptyStateOrThrowException(isRemote, new ChannelStateReaderWithException());
+	}
+
+	private void testReadEmptyStateOrThrowException(boolean isRemote, ChannelStateReader reader) throws Exception {
+		// setup
+		final int totalBuffers = 10;
+		final NetworkBufferPool globalPool = new NetworkBufferPool(totalBuffers, 32, 2);
+		final SingleInputGate inputGate = createInputGate(globalPool);
+		final RecoveredInputChannel inputChannel = createRecoveredChannel(isRemote, inputGate);
+
+		try {
+			inputGate.setInputChannels(inputChannel);
+			inputGate.setup();
+
+			// it would throw expected exception for the case of testReadStateWithException.
+			inputChannel.readRecoveredState(reader);
+
+			// the channel only has one EndOfChannelStateEvent in the queue for the case of testReadEmptyState.
+			assertEquals(1, inputChannel.getNumberOfQueuedBuffers());
+			assertFalse(inputChannel.getNextBuffer().isPresent());
+			assertTrue(inputChannel.getStateConsumedFuture().isDone());
+		} finally {
+			// cleanup and verify no buffer leak
+			inputGate.close();
+			globalPool.destroyAllBufferPools();
+			assertEquals(totalBuffers, globalPool.getNumberOfAvailableMemorySegments());
+			globalPool.destroy();
+		}
+	}
+
+	/**
+	 * Tests that the process of reading recovered state executes concurrently with channel
+	 * buffer processing, based on the condition of the total number of states is more that
+	 * the total buffer amount, to confirm that the lifecycle(recycle) of exclusive/floating
+	 * buffers works well.
+	 */
+	private void testConcurrentReadStateAndProcess(boolean isRemote) throws Exception {
+		// setup
+		final int totalBuffers = 10;
+		final NetworkBufferPool globalPool = new NetworkBufferPool(totalBuffers, 32, 2);
+		final SingleInputGate inputGate = createInputGate(globalPool);
+		final RecoveredInputChannel inputChannel = createRecoveredChannel(isRemote, inputGate);
+
+		// the number of states is more that the total buffer amount
+		final int totalStates = 15;
+		final int[] states = {1, 2, 3, 4};
+		final ChannelStateReader reader = new ResultPartitionTest.FiniteChannelStateReader(totalStates, states);
+		final ExecutorService executor = Executors.newFixedThreadPool(2);
+
+		Throwable thrown = null;
+		try {
+			inputGate.setInputChannels(inputChannel);
+			inputGate.setup();
+
+			final Callable<Void> processTask = processRecoveredBufferTask(inputChannel, totalStates, states);
+			final Callable<Void> readStateTask = readRecoveredStateTask(inputChannel, reader);
+
+			submitTasksAndWaitForResults(executor, new Callable[] {readStateTask, processTask});
+		} catch (Throwable t) {
+			thrown = t;
+		} finally {
+			// cleanup
+			cleanup(globalPool, executor, null, thrown, inputChannel);
+		}
+	}
+
+	private void testConcurrentReadStateAndRelease(boolean isRemote) throws Exception {
+		// setup
+		final int totalBuffers = 10;
+		final NetworkBufferPool globalPool = new NetworkBufferPool(totalBuffers, 32, 2);
+		final SingleInputGate inputGate = createInputGate(globalPool);
+		final RecoveredInputChannel inputChannel = createRecoveredChannel(isRemote, inputGate);
+
+		// the number of states is more that the total buffer amount
+		final int totalStates = 15;
+		final int[] states = {1, 2, 3, 4};
+		final ChannelStateReader reader = new ResultPartitionTest.FiniteChannelStateReader(totalStates, states);
+		final ExecutorService executor = Executors.newFixedThreadPool(2);
+
+		Throwable thrown = null;
+		try {
+			inputGate.setInputChannels(inputChannel);
+			inputGate.setup();
+
+			submitTasksAndWaitForResults(
+				executor,
+				new Callable[] {readRecoveredStateTask(inputChannel, reader), releaseChannelTask(inputChannel)});
+		} catch (Throwable t) {
+			thrown = t;
+		} finally {
+			// cleanup
+			cleanup(globalPool, executor, null, thrown, inputChannel);
+		}
+	}
+
+	private void testConcurrentReadStateAndProcessAndRelease(boolean isRemote) throws Exception {
+		// setup
+		final int totalBuffers = 10;
+		final NetworkBufferPool globalPool = new NetworkBufferPool(totalBuffers, 32, 2);
+		final SingleInputGate inputGate = createInputGate(globalPool);
+		final RecoveredInputChannel inputChannel = createRecoveredChannel(isRemote, inputGate);
+
+		// the number of states is more that the total buffer amount
+		final int totalStates = 15;
+		final int[] states = {1, 2, 3, 4};
+		final ChannelStateReader reader = new ResultPartitionTest.FiniteChannelStateReader(totalStates, states);
+		final ExecutorService executor = Executors.newFixedThreadPool(2);
+		Throwable thrown = null;
+		try {
+			inputGate.setInputChannels(inputChannel);
+			inputGate.setup();
+
+			final Callable<Void> processTask = processRecoveredBufferTask(inputChannel, totalStates, states);
+			final Callable<Void> readStateTask = readRecoveredStateTask(inputChannel, reader);
+			final Callable<Void> releaseTask = releaseChannelTask(inputChannel);
+
+			submitTasksAndWaitForResults(executor, new Callable[] {readStateTask, processTask, releaseTask});
+		} catch (Throwable t) {
+			thrown = t;
+		} finally {
+			// cleanup
+			cleanup(globalPool, executor, null, thrown, inputChannel);
+		}
+	}
+
+	private Callable<Void> readRecoveredStateTask(RecoveredInputChannel inputChannel, ChannelStateReader reader) {
+		return () -> {
+			try {
+				inputChannel.readRecoveredState(reader);
+			} catch (CancelTaskException ex) {
+				// within expectation
+			}
+
+			return null;
+		};
+	}
+
+	private Callable<Void> processRecoveredBufferTask(RecoveredInputChannel inputChannel, int totalStates, int[] states) {
+		return () -> {
+			// process all the queued state buffers and verify the data
+			int numProcessedStates = 0;
+			while (numProcessedStates < totalStates) {
+				if (inputChannel.isReleased()) {
+					break;
+				}
+				if (inputChannel.getNumberOfQueuedBuffers() == 0) {
+					Thread.sleep(1);
+					continue;
+				}
+				try {
+					Optional<BufferAndAvailability> bufferAndAvailability = inputChannel.getNextBuffer();
+					if (bufferAndAvailability.isPresent()) {
+						Buffer buffer = bufferAndAvailability.get().buffer();
+						BufferBuilderAndConsumerTest.assertContent(buffer, null, states);
+						buffer.recycleBuffer();
+						numProcessedStates++;
+					}
+				} catch (IllegalStateException e) {
+					assertTrue(e.getMessage().contains("Queried for a buffer after channel has been closed"));
+				}
+			}
+
+			return null;
+		};
+	}
+
+	private Callable<Void> releaseChannelTask(RecoveredInputChannel inputChannel) {
+		return () -> {
+			inputChannel.releaseAllResources();
+			return null;
+		};
+	}
+
+	private RecoveredInputChannel createRecoveredChannel(boolean isRemote, SingleInputGate gate) {
+		if (isRemote) {
+			return new InputChannelBuilder().buildRemoteRecoveredChannel(gate);
+		} else {
+			return new InputChannelBuilder().buildLocalRecoveredChannel(gate);
+		}
+	}
+
+	private SingleInputGate createInputGate(NetworkBufferPool globalPool) throws Exception {
+		return new SingleInputGateBuilder()
+			.setBufferPoolFactory(globalPool.createBufferPool(8, 8))
+			.setSegmentProvider(globalPool)
+			.build();
+	}
+}