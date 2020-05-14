cc7001d01358400a227d132784534ff6a79b4dc7
TsReaper <tsreaper96@gmail.com>
2020-05-14 11:38:54 +0800

    [FLINK-17680][docs] Support @ExcludeFromDocumentation when generating REST API documentations

---
+++ flink-docs/src/test/java/org/apache/flink/docs/rest/RestAPIDocGeneratorTest.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.docs.rest;
+
+import org.apache.flink.docs.rest.data.TestDocumentingRestEndpoint;
+import org.apache.flink.runtime.rest.versioning.RestAPIVersion;
+import org.apache.flink.util.FileUtils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * Tests for the {@link RestAPIDocGenerator}.
+ */
+public class RestAPIDocGeneratorTest {
+
+	@Test
+	public void testDocGeneration() throws Exception {
+		File file = File.createTempFile("rest_v0_", ".html");
+		RestAPIDocGenerator.createHtmlFile(
+			new TestDocumentingRestEndpoint(),
+			RestAPIVersion.V0,
+			file.toPath());
+		String actual = removeUniqueId(FileUtils.readFile(file, "UTF-8"));
+
+		File expectedFile = new File(
+			RestAPIDocGeneratorTest.class.getClassLoader().getResource("rest_v0_expected.html").getFile());
+		String expected = removeUniqueId(FileUtils.readFile(expectedFile, "UTF-8"));
+
+		Assert.assertEquals(expected, actual);
+	}
+
+	private String removeUniqueId(String s) {
+		return s
+			.replaceAll("id=\\\".+?\\\"", "id=\"\"")
+			.replaceAll("data-target=\\\".+?\\\"", "data-target=\"\"");
+	}
+}
---
+++ flink-docs/src/test/java/org/apache/flink/docs/rest/data/TestRequestBody.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.docs.rest.data;
+
+import org.apache.flink.runtime.rest.messages.RequestBody;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonCreator;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonIgnore;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * A {@link RequestBody} for testing purpose.
+ */
+@SuppressWarnings("unused")
+public class TestRequestBody implements RequestBody {
+
+	private static final String FIELD_NAME_A = "a";
+	private static final String FIELD_NAME_B = "b";
+
+	@JsonProperty(FIELD_NAME_A)
+	private final String a;
+
+	@JsonProperty(FIELD_NAME_B)
+	private final String b;
+
+	@JsonCreator
+	public TestRequestBody(
+			@JsonProperty(FIELD_NAME_A) String a,
+			@JsonProperty(FIELD_NAME_B) String b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	@JsonIgnore
+	public String getA() {
+		return a;
+	}
+
+	@JsonIgnore
+	public String getB() {
+		return b;
+	}
+}
