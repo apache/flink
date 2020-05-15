3b2df3e55d6137b0acfc5746ca97ac164a08ad27
TsReaper <tsreaper96@gmail.com>
2020-05-15 20:37:29 +0800

    [fix] Fix checkstyle

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/operators/collect/CollectSinkFunction.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/operators/collect/CollectSinkFunction.java
@@ -49,7 +49,6 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
