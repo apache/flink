e321e483fa0b3154e5e8417809bd669482783a13
Robert Metzger <rmetzger@apache.org>
2020-05-18 09:37:16 +0200

    [FLINK-17730][CI] Increase 'no output timeout' to 15 minutes

--- tools/azure_controller.sh
+++ tools/azure_controller.sh
@@ -184,7 +184,7 @@ elif [ $STAGE != "$STAGE_CLEANUP" ]; then
     fi
 
 
-    TEST="$STAGE" "./tools/travis_watchdog.sh" 300
+    TEST="$STAGE" "./tools/travis_watchdog.sh" 900
     EXIT_CODE=$?
 elif [ $STAGE == "$STAGE_CLEANUP" ]; then
     echo "Cleaning up $CACHE_BUILD_DIR"
