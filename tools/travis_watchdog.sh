43c3bfa9cf29c229477b9c6bdc3872de44985c20
Robert Metzger <rmetzger@apache.org>
2020-05-18 09:37:16 +0200

    [FLINK-17730][CI] Increase 'no output timeout' to 15 minutes

--- tools/travis_watchdog.sh
+++ tools/travis_watchdog.sh
@@ -39,7 +39,7 @@ echo "Build for commit ${TRAVIS_COMMIT} of ${TRAVIS_REPO_SLUG} [build ID: ${TRAV
 # =============================================================================
 
 # Number of seconds w/o output before printing a stack trace and killing $MVN
-MAX_NO_OUTPUT=${1:-600}
+MAX_NO_OUTPUT=${1:-900}
 
 # Number of seconds to sleep before checking the output again
 SLEEP_TIME=20
