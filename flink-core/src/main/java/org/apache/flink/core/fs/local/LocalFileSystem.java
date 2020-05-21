8c3664c3b50b01c42857356d03d96af1ab661c84
Wei Zhong <weizhong0618@gmail.com>
2020-05-21 09:57:10 +0800

    [FLINK-17866][core][python] Change the implementation of the LocalFileSystem#pathToFile to fix the test case failure of PyFlink when running on Windows.
    This closes #12280.

--- flink-core/src/main/java/org/apache/flink/core/fs/local/LocalFileSystem.java
+++ flink-core/src/main/java/org/apache/flink/core/fs/local/LocalFileSystem.java
@@ -49,7 +49,6 @@ import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
-import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import static org.apache.flink.util.Preconditions.checkNotNull;
@@ -311,7 +310,7 @@ public class LocalFileSystem extends FileSystem {
 	 * Converts the given Path to a File for this file system.
 	 */
 	public File pathToFile(Path path) {
-		return Paths.get(path.getPath()).toFile();
+		return new File(path.getPath());
 	}
 
 	// ------------------------------------------------------------------------
