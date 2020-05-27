89fc7314d7bf14ac3bc667422be3d2d6b6a7b8fe
Xintong Song <tonysong820@gmail.com>
2020-05-27 17:05:13 +0800

    [FLINK-17958][core] Fix MathUtils#divideRoundUp bug for handling zero / negative values.

--- flink-core/src/main/java/org/apache/flink/util/MathUtils.java
+++ flink-core/src/main/java/org/apache/flink/util/MathUtils.java
@@ -209,12 +209,18 @@ public final class MathUtils {
 
 	/**
 	 * Divide and rounding up to integer.
-	 * E.g., divideRoundUp(3, 2) returns 2.
+	 * E.g., divideRoundUp(3, 2) returns 2, divideRoundUp(0, 3) returns 0.
+	 * Note that this method does not support negative values.
 	 * @param dividend value to be divided by the divisor
 	 * @param divisor value by which the dividend is to be divided
 	 * @return the quotient rounding up to integer
 	 */
 	public static int divideRoundUp(int dividend, int divisor) {
+		Preconditions.checkArgument(dividend >= 0, "Negative dividend is not supported.");
+		Preconditions.checkArgument(divisor > 0, "Negative or zero divisor is not supported.");
+		if (dividend == 0) {
+			return 0;
+		}
 		return (dividend - 1) / divisor + 1;
 	}
 
