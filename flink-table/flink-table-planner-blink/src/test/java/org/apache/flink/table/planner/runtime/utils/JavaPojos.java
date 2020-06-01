98634ee7bde0243b169b4b618ba8bb23c66d3507
Jark Wu <jark@apache.org>
2020-06-01 18:11:50 +0800

    [FLINK-17466][table-planner-blink] Fix toRetractStream doesn't work correctly with Pojo conversion class

--- flink-table/flink-table-planner-blink/src/test/java/org/apache/flink/table/planner/runtime/utils/JavaPojos.java
+++ flink-table/flink-table-planner-blink/src/test/java/org/apache/flink/table/planner/runtime/utils/JavaPojos.java
@@ -123,4 +123,28 @@ public class JavaPojos {
 				'}';
 		}
 	}
+
+	/**
+	 * A POJO class.
+	 */
+	public static class Person {
+		public String name;
+		public int age;
+
+		public Person() {
+		}
+
+		public Person(String name, int age) {
+			this.name = name;
+			this.age = age;
+		}
+
+		@Override
+		public String toString() {
+			return "Person{" +
+				"name='" + name + '\'' +
+				", age=" + age +
+				'}';
+		}
+	}
 }
