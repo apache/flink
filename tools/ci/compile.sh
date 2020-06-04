68908598810bb6a954be546ffdd561c94527c5dd
Robert Metzger <rmetzger@apache.org>
2020-06-04 19:53:01 +0200

    address comments

--- tools/ci/compile.sh
+++ tools/ci/compile.sh
@@ -54,20 +54,27 @@ if [ $EXIT_CODE != 0 ]; then
     exit $EXIT_CODE
 fi
 
-echo "============ Checking Javadocs and Scaladocs ============"
+echo "============ Checking Javadocs ============"
 
 # use the same invocation as on buildbot (https://svn.apache.org/repos/infra/infrastructure/buildbot/aegis/buildmaster/master1/projects/flink.conf)
 run_mvn javadoc:aggregate -Paggregate-scaladoc -DadditionalJOption='-Xdoclint:none' \
       -Dmaven.javadoc.failOnError=false -Dcheckstyle.skip=true -Denforcer.skip=true \
-      -Dheader=someTestHeader || exit $?
+      -Dheader=someTestHeader > javadoc.out
+EXIT_CODE=$?
+if [ $EXIT_CODE != 0 ] ; then
+  echo "ERROR in Javadocs. Printing full output:"
+  cat javadoc.out ; rm javadoc.out
+  exit $EXIT_CODE
+fi
+
+echo "============ Checking Scaladocs ============"
 
 cd flink-scala
 run_mvn scala:doc 2> scaladoc.out
 EXIT_CODE=$?
 if [ $EXIT_CODE != 0 ] ; then
   echo "ERROR in Scaladocs. Printing full output:"
-  cat scaladoc.out
-  rm scaladoc.out
+  cat scaladoc.out ; rm scaladoc.out
   exit $EXIT_CODE
 fi
 cd ..
