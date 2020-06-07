05db48562c215417f4faa038689cdc18a4582479
Jiangjie (Becket) Qin <jiangjie.qj@alibaba-inc.com>
2020-06-07 08:19:41 +0800

    [FLINK-18039][connector/common] Introduce a RecreateOnResetOperatorCoordinator class to recreate an OperatorCoordinator instance when resetToCheckpoint() is invoked. Let SourceCoordinator leverage RecreateOnResetOperatorCoordinator to ensure a clean checkpoint reset.
    Signed-off-by: Jiangjie (Becket) Qin <jiangjie.qj@alibaba-inc.com>

--- flink-runtime/src/main/java/org/apache/flink/runtime/source/coordinator/ExecutorNotifier.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/source/coordinator/ExecutorNotifier.java
@@ -25,21 +25,24 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiConsumer;
 
 /**
  * This class is used to coordinate between two components, where one component has an
  * executor following the mailbox model and the other component notifies it when needed.
  */
-public class ExecutorNotifier {
+public class ExecutorNotifier implements AutoCloseable {
 	private static final Logger LOG = LoggerFactory.getLogger(ExecutorNotifier.class);
-	private ScheduledExecutorService workerExecutor;
-	private Executor executorToNotify;
+	private final ScheduledExecutorService workerExecutor;
+	private final Executor executorToNotify;
+	private final AtomicBoolean closed;
 
 	public ExecutorNotifier(ScheduledExecutorService workerExecutor,
 							Executor executorToNotify) {
 		this.executorToNotify = executorToNotify;
 		this.workerExecutor = workerExecutor;
+		this.closed = new AtomicBoolean(false);
 	}
 
 	/**
@@ -133,5 +136,21 @@ public class ExecutorNotifier {
 				handler.accept(null, t);
 			}
 		}, initialDelayMs, periodMs, TimeUnit.MILLISECONDS);
+	}
+
+	/**
+	 * Close the executor notifier. This is a blocking call which waits for all the
+	 * async calls to finish before it returns.
+	 *
+	 * @throws InterruptedException when interrupted during closure.
+	 */
+	public void close() throws InterruptedException {
+		if (!closed.compareAndSet(false, true)) {
+			LOG.debug("The executor notifier has been closed.");
+			return;
+		}
+		// Shutdown the worker executor, so no more worker tasks can run.
+		workerExecutor.shutdownNow();
+		workerExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
 	}
 }
