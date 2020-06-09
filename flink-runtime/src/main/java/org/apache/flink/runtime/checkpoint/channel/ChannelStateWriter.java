apache/flink

commit 24ff415f1b76392f75dea7c3538558d24fcb7058

[FLINK-17218][checkpointing] Ensuring that ChannelStateWriter aborts previous checkpoints before a new checkpoint is started.

flink-runtime/src/main/java/org/apache/flink/runtime/checkpoint/channel/ChannelStateWriter.java
@@ -178,9 +178,7 @@ public interface ChannelStateWriter extends Closeable, CheckpointListener {
 
 		@Override
 		public ChannelStateWriteResult getWriteResult(long checkpointId) {
-			return new ChannelStateWriteResult(
-				CompletableFuture.completedFuture(Collections.emptyList()),
-				CompletableFuture.completedFuture(Collections.emptyList()));
+			return ChannelStateWriteResult.EMPTY;
 		}
 
 		@Override

flink-runtime/src/main/java/org/apache/flink/runtime/checkpoint/channel/ChannelStateWriterImpl.java
@@ -91,6 +91,7 @@ public class ChannelStateWriterImpl implements ChannelStateWriter {
 
 	@Override
 	public void start(long checkpointId, CheckpointOptions checkpointOptions) {
+		results.keySet().forEach(oldCheckpointId -> abort(oldCheckpointId, new Exception("Starting new checkpoint " + checkpointId)));
 		LOG.debug("start checkpoint {} ({})", checkpointId, checkpointOptions);
 		ChannelStateWriteResult result = new ChannelStateWriteResult();
 		ChannelStateWriteResult put = results.computeIfAbsent(checkpointId, id -> {

flink-runtime/src/test/java/org/apache/flink/runtime/checkpoint/channel/ChannelStateWriterImplTest.java
@@ -182,16 +182,19 @@ public class ChannelStateWriterImplTest {
 		unwrappingError(TestException.class, () -> callStart(writer));
 	}
 
-	@Test(expected = IllegalStateException.class)
-	public void testLimit() throws IOException {
-		int maxCheckpoints = 3;
-		try (ChannelStateWriterImpl writer = new ChannelStateWriterImpl(getStreamFactoryFactory(), maxCheckpoints)) {
-			writer.open();
-			for (int i = 0; i < maxCheckpoints; i++) {
+	@Test
+	public void testStartAbortsOldCheckpoints() throws Exception {
+		int maxCheckpoints = 10;
+		runWithSyncWorker((writer, worker) -> {
+			writer.start(0, CheckpointOptions.forCheckpointWithDefaultLocation());
+			ChannelStateWriteResult writeResult = writer.getWriteResult(0);
+			for (int i = 1; i <= maxCheckpoints; i++) {
 				writer.start(i, CheckpointOptions.forCheckpointWithDefaultLocation());
+				worker.processAllRequests();
+				assertTrue(writeResult.isDone());
+				writeResult = writer.getWriteResult(i);
 			}
-			writer.start(maxCheckpoints, CheckpointOptions.forCheckpointWithDefaultLocation());
-		}
+		});
 	}
 
 	@Test(expected = IllegalStateException.class)

commit 1582b47414a048efc0d9b634ffa57b020610a47f

[FLINK-17869][task][checkpointing] Revert "[FLINK-17218][checkpointing] Ensuring that ChannelStateWriter aborts previous checkpoints before a new checkpoint is started."
This reverts commit 24ff415f1b76392f75dea7c3538558d24fcb7058
which introduced a race condition when task thread and netty
thread compete for ChannelStateWriteResult.

Instead, next commits fix it by:
1. Map size validation error will be prevented simply by increasing the limit
2. When a checkpoint is subsumed, it's write result will be removed from on future completion

flink-runtime/src/main/java/org/apache/flink/runtime/checkpoint/channel/ChannelStateWriter.java
@@ -179,7 +179,9 @@ public interface ChannelStateWriter extends Closeable {
 
 		@Override
 		public ChannelStateWriteResult getAndRemoveWriteResult(long checkpointId) {
-			return ChannelStateWriteResult.EMPTY;
+			return new ChannelStateWriteResult(
+				CompletableFuture.completedFuture(Collections.emptyList()),
+				CompletableFuture.completedFuture(Collections.emptyList()));
 		}
 
 		@Override

flink-runtime/src/main/java/org/apache/flink/runtime/checkpoint/channel/ChannelStateWriterImpl.java
@@ -98,7 +98,6 @@ public class ChannelStateWriterImpl implements ChannelStateWriter {
 
 	@Override
 	public void start(long checkpointId, CheckpointOptions checkpointOptions) {
-		results.keySet().forEach(oldCheckpointId -> abort(oldCheckpointId, new Exception("Starting new checkpoint " + checkpointId)));
 		LOG.debug("{} starting checkpoint {} ({})", taskName, checkpointId, checkpointOptions);
 		ChannelStateWriteResult result = new ChannelStateWriteResult();
 		ChannelStateWriteResult put = results.computeIfAbsent(checkpointId, id -> {

flink-runtime/src/test/java/org/apache/flink/runtime/checkpoint/channel/ChannelStateWriterImplTest.java
@@ -186,19 +186,16 @@ public class ChannelStateWriterImplTest {
 		unwrappingError(TestException.class, () -> callStart(writer));
 	}
 
-	@Test
-	public void testStartAbortsOldCheckpoints() throws Exception {
-		int maxCheckpoints = 10;
-		runWithSyncWorker((writer, worker) -> {
-			writer.start(0, CheckpointOptions.forCheckpointWithDefaultLocation());
-			ChannelStateWriteResult writeResult = writer.getWriteResult(0);
-			for (int i = 1; i <= maxCheckpoints; i++) {
+	@Test(expected = IllegalStateException.class)
+	public void testLimit() throws IOException {
+		int maxCheckpoints = 3;
+		try (ChannelStateWriterImpl writer = new ChannelStateWriterImpl(TASK_NAME, getStreamFactoryFactory(), maxCheckpoints)) {
+			writer.open();
+			for (int i = 0; i < maxCheckpoints; i++) {
 				writer.start(i, CheckpointOptions.forCheckpointWithDefaultLocation());
-				worker.processAllRequests();
-				assertTrue(writeResult.isDone());
-				writeResult = writer.getWriteResult(i);
 			}
-		});
+			writer.start(maxCheckpoints, CheckpointOptions.forCheckpointWithDefaultLocation());
+		}
 	}
 
 	@Test(expected = IllegalStateException.class)