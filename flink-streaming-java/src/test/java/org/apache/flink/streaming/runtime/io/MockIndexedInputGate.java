485688fa3c7b72653303d261f32fe4d637098eb7
Arvid Heise <arvid@ververica.com>
2020-06-15 21:09:13 +0200

    [FLINK-18094][network] Add InputGate#getChannelInfos for easier testing.
    In the following commits, this method will be used to fetch information about all channels without explicitly needing to access the channels. Thus, for tests mocks just need to return meaningful InputChannelInfos instead of actually creating the respective channels.

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/runtime/io/MockIndexedInputGate.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/runtime/io/MockIndexedInputGate.java
@@ -19,15 +19,19 @@
 package org.apache.flink.streaming.runtime.io;
 
 import org.apache.flink.runtime.checkpoint.channel.ChannelStateReader;
+import org.apache.flink.runtime.checkpoint.channel.InputChannelInfo;
 import org.apache.flink.runtime.event.TaskEvent;
 import org.apache.flink.runtime.io.network.buffer.BufferReceivedListener;
 import org.apache.flink.runtime.io.network.partition.consumer.BufferOrEvent;
 import org.apache.flink.runtime.io.network.partition.consumer.IndexedInputGate;
 import org.apache.flink.runtime.io.network.partition.consumer.InputChannel;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Mock {@link IndexedInputGate}.
@@ -70,6 +74,13 @@ public class MockIndexedInputGate extends IndexedInputGate {
 	@Override
 	public InputChannel getChannel(int channelIndex) {
 		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public List<InputChannelInfo> getChannelInfos() {
+		return IntStream.range(0, numberOfInputChannels)
+				.mapToObj(channelIndex -> new InputChannelInfo(gateIndex, channelIndex))
+				.collect(Collectors.toList());
 	}
 
 	@Override
