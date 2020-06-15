e517223704857154556fbf49ee5348d59f51d9ae
austin ce <austin.cawley@gmail.com>
2020-06-15 09:31:08 -0400

    [FLINK-10195][connectors/rabbitmq] Allow setting QoS

--- flink-connectors/flink-connector-rabbitmq/src/test/java/org/apache/flink/streaming/connectors/rabbitmq/common/RMQConnectionConfigTest.java
+++ flink-connectors/flink-connector-rabbitmq/src/test/java/org/apache/flink/streaming/connectors/rabbitmq/common/RMQConnectionConfigTest.java
@@ -23,8 +23,11 @@ import org.junit.Test;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for the {@link RMQConnectionConfig}.
@@ -68,5 +71,33 @@ public class RMQConnectionConfigTest {
 			.setConnectionTimeout(5000).build();
 		ConnectionFactory factory = connectionConfig.getConnectionFactory();
 		assertEquals(5000, factory.getConnectionTimeout());
+	}
+
+	@Test
+	public void shouldSetOptionalPrefetchCount() {
+		RMQConnectionConfig connectionConfig = new RMQConnectionConfig.Builder()
+			.setHost("localhost")
+			.setPort(5000)
+			.setUserName("guest")
+			.setPassword("guest")
+			.setVirtualHost("/")
+			.setPrefetchCount(500)
+			.build();
+		Optional<Integer> prefetch = connectionConfig.getPrefetchCount();
+		assertTrue(prefetch.isPresent());
+		assertEquals(500, (int) prefetch.get());
+	}
+
+	@Test
+	public void shouldReturnEmptyOptionalPrefetchCount() {
+		RMQConnectionConfig connectionConfig = new RMQConnectionConfig.Builder()
+			.setHost("localhost")
+			.setPort(5000)
+			.setUserName("guest")
+			.setPassword("guest")
+			.setVirtualHost("/")
+			.build();
+		Optional<Integer> prefetch = connectionConfig.getPrefetchCount();
+		assertFalse(prefetch.isPresent());
 	}
 }
