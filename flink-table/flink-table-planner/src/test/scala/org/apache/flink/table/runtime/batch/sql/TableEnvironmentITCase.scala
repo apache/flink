f3a35502d1c15dc70953d9289f76d491a35c78fa
Dawid Wysakowicz <dwysakowicz@apache.org>
2020-06-18 10:32:08 +0200

    [FLINK-18358] Fix comparing double in TableEnvironmentITCase
    This closes #12703

--- flink-table/flink-table-planner/src/test/scala/org/apache/flink/table/runtime/batch/sql/TableEnvironmentITCase.scala
+++ flink-table/flink-table-planner/src/test/scala/org/apache/flink/table/runtime/batch/sql/TableEnvironmentITCase.scala
@@ -231,6 +231,8 @@ class TableEnvironmentITCase(
 
     val resultFile = _tempFolder.newFile().getAbsolutePath
     result.toDataSet[(Long, Double)]
+      // format the double with 5 fractional digits for comparison
+      .map(ds => (ds._1.toString, "%.5f".format(ds._2)))
       .writeAsCsv(resultFile, writeMode=FileSystem.WriteMode.OVERWRITE)
 
     tEnv.execute("job name")
