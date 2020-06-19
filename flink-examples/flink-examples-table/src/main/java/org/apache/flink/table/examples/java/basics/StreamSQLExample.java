781ad7ee72fb7e99bc14426d3d34a71e65d3195b
Timo Walther <twalthr@apache.org>
2020-06-19 15:03:07 +0200

    [FLINK-18380][examples-table] Add a ChangelogSocketExample

--- flink-examples/flink-examples-table/src/main/java/org/apache/flink/table/examples/java/basics/StreamSQLExample.java
+++ flink-examples/flink-examples-table/src/main/java/org/apache/flink/table/examples/java/basics/StreamSQLExample.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.apache.flink.table.examples.java;
+package org.apache.flink.table.examples.java.basics;
 
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
