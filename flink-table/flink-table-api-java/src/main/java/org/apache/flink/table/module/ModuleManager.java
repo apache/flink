46a8ab9628a3297dccbea2de8cdd2f5f9d7cd5f9
Shengkai <33114724+fsk119@users.noreply.github.com>
2020-06-24 14:28:52 +0800

    [FLINK-18351][table] Fix ModuleManager creates a lot of duplicate/similar log messages
    This closes #12757

--- flink-table/flink-table-api-java/src/main/java/org/apache/flink/table/module/ModuleManager.java
+++ flink-table/flink-table-api-java/src/main/java/org/apache/flink/table/module/ModuleManager.java
@@ -126,7 +126,7 @@ public class ModuleManager {
 			.findFirst();
 
 		if (result.isPresent()) {
-			LOG.info("Got FunctionDefinition '{}' from '{}' module.", name, result.get().getKey());
+			LOG.debug("Got FunctionDefinition '{}' from '{}' module.", name, result.get().getKey());
 
 			return result.get().getValue().getFunctionDefinition(name);
 		} else {
