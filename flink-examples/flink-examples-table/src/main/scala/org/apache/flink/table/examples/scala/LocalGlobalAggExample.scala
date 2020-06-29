6adee0c8aaaca28a90b39d25c7946f39d72f3c8f
wangpeibin <peibin.wpb@koubei.com>
2020-06-29 14:50:49 +0800

    add comment for LocalGlobalAggExample

--- flink-examples/flink-examples-table/src/main/scala/org/apache/flink/table/examples/scala/LocalGlobalAggExample.scala
+++ flink-examples/flink-examples-table/src/main/scala/org/apache/flink/table/examples/scala/LocalGlobalAggExample.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.flink.table.examples.scala
 
 import java.util.Date
@@ -10,8 +27,12 @@ import org.apache.flink.table.api.bridge.scala.{StreamTableEnvironment, _}
 import org.apache.flink.types.Row
 
 /**
-  * @author peibin.wpb
-  * @date 2020-03-09 16:26
+  * Simple example for demonstrating the optimization of local-global-aggregation on a Stream Table in Scala.
+  *
+  *  This example shows how to:
+  *  - Enable local-global-aggregation in StreamTableEnvironment
+  *  - Convert Simple Source Function to Tables
+  *  - Apply sum, last_value  operations
   */
 object LocalGlobalAggExample {
 
