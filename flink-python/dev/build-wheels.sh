7482f0e37d22b0f6f087c99e68c4e7b1b653f8c6
huangxingbo <hxbks2ks@gmail.com>
2020-07-10 19:17:45 +0800

    [FLINK-18561][python] Build manylinux1 with better compatibility instead of manylinux2014 Python Wheel Packages
    This closes #12869.

--- flink-python/dev/build-wheels.sh
+++ flink-python/dev/build-wheels.sh
@@ -27,8 +27,26 @@ done
 
 ## 3. build wheels
 for ((i=0;i<${#py_env[@]};i++)) do
+    if [[ "$(uname)" != "Darwin" ]]; then
+        # force the linker to use the older glibc version in Linux
+        export CFLAGS="-I. -include dev/glibc_version_fix.h"
+    fi
     ${PY_ENV_DIR}/${py_env[i]}/bin/python setup.py bdist_wheel
 done
 
+## 4. convert linux_x86_64 wheel to manylinux1 wheel in Linux
+if [[ "$(uname)" != "Darwin" ]]; then
+    source `pwd`/dev/.conda/bin/activate
+    # 4.1 install patchelf
+    conda install -c conda-forge patchelf=0.11 -y
+    # 4.2 install auditwheel
+    pip install auditwheel==3.2.0
+    # 4.3 convert Linux wheel
+    for wheel_file in dist/*.whl; do
+        auditwheel repair ${wheel_file} -w dist
+        rm -f ${wheel_file}
+    done
+    source deactivate
+fi
 ## see the result
 ls -al dist/
