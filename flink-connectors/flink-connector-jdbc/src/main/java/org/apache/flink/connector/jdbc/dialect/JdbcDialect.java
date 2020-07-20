f0dc6e416ae6f3f6f1d89f980652bf9044285905
maozl <577029120@qq.com>
2020-07-20 21:23:51 +0800

    revert

--- flink-connectors/flink-connector-jdbc/src/main/java/org/apache/flink/connector/jdbc/dialect/JdbcDialect.java
+++ flink-connectors/flink-connector-jdbc/src/main/java/org/apache/flink/connector/jdbc/dialect/JdbcDialect.java
@@ -33,115 +33,127 @@ import java.util.stream.Collectors;
  * Handle the SQL dialect of jdbc driver.
  */
 @Internal
-public interface JDBCDialect extends Serializable {
+public interface JdbcDialect extends Serializable {
 
-    /**
-     * Check if this dialect instance can handle a certain jdbc url.
-     * @param url the jdbc url.
-     * @return True if the dialect can be applied on the given jdbc url.
-     */
-    boolean canHandle(String url);
+	/**
+	 * Get the name of jdbc dialect.
+	 * @return the dialect name.
+	 */
+	String dialectName();
 
-    /**
-     * @return the default driver class name, if user not configure the driver class name,
-     * then will use this one.
-     */
-    default Optional<String> defaultDriverName() {
-        return Optional.empty();
-    }
+	/**
+	 * Check if this dialect instance can handle a certain jdbc url.
+	 * @param url the jdbc url.
+	 * @return True if the dialect can be applied on the given jdbc url.
+	 */
+	boolean canHandle(String url);
 
-    /**
-     * Quotes the identifier. This is used to put quotes around the identifier in case the column
-     * name is a reserved keyword, or in case it contains characters that require quotes (e.g. space).
-     * Default using double quotes {@code "} to quote.
-     */
-    default String quoteIdentifier(String identifier) {
-        return "\"" + identifier + "\"";
-    }
+	/**
+	 * Get converter that convert jdbc object and Flink internal object each other.
+	 * @param rowType the given row type
+	 * @return a row converter for the database
+	 */
+	JdbcRowConverter getRowConverter(RowType rowType);
 
-    /**
-     * Get dialect upsert statement, the database has its own upsert syntax, such as Mysql
-     * using DUPLICATE KEY UPDATE, and PostgresSQL using ON CONFLICT... DO UPDATE SET..
-     *
-     * @return None if dialect does not support upsert statement, the writer will degrade to
-     * the use of select + update/insert, this performance is poor.
-     */
-    default Optional<String> getUpsertStatement(
-            String tableName, String[] fieldNames, String[] uniqueKeyFields) {
-        return Optional.empty();
-    }
+	/**
+	 * Check if this dialect instance support a specific data type in table schema.
+	 * @param schema the table schema.
+	 * @exception ValidationException in case of the table schema contains unsupported type.
+	 */
+	default void validate(TableSchema schema) throws ValidationException {
+	}
 
-    default String quoteTablename(String tableName){
-        if(tableName.contains(".")){
-            String[] strs = tableName.split("\\.");
-            return Arrays.stream(strs).map(s -> quoteIdentifier(s)).collect(Collectors.joining("."));
-        }else {
-            return quoteIdentifier(tableName);
-        }
-    }
-    /**
-     * Get row exists statement by condition fields. Default use SELECT.
-     */
-    default String getRowExistsStatement(String tableName, String[] conditionFields) {
-        String fieldExpressions = Arrays.stream(conditionFields)
-                .map(f -> quoteIdentifier(f) + "=?")
-                .collect(Collectors.joining(" AND "));
+	/**
+	 * @return the default driver class name, if user not configure the driver class name,
+	 * then will use this one.
+	 */
+	default Optional<String> defaultDriverName() {
+		return Optional.empty();
+	}
 
-        return "SELECT 1 FROM " + quoteTablename(tableName) + " WHERE " + fieldExpressions;
-    }
+	/**
+	 * Quotes the identifier. This is used to put quotes around the identifier in case the column
+	 * name is a reserved keyword, or in case it contains characters that require quotes (e.g. space).
+	 * Default using double quotes {@code "} to quote.
+	 */
+	default String quoteIdentifier(String identifier) {
+		return "\"" + identifier + "\"";
+	}
 
-    /**
-     * Get insert into statement.
-     */
-    default String getInsertIntoStatement(String tableName, String[] fieldNames) {
-        String columns = Arrays.stream(fieldNames)
-                .map(this::quoteIdentifier)
-                .collect(Collectors.joining(", "));
-        String placeholders = Arrays.stream(fieldNames)
-                .map(f -> "?")
-                .collect(Collectors.joining(", "));
-        return "INSERT INTO " + quoteTablename(tableName) +
-                "(" + columns + ")" + " VALUES (" + placeholders + ")";
-    }
+	/**
+	 * Get dialect upsert statement, the database has its own upsert syntax, such as Mysql
+	 * using DUPLICATE KEY UPDATE, and PostgresSQL using ON CONFLICT... DO UPDATE SET..
+	 *
+	 * @return None if dialect does not support upsert statement, the writer will degrade to
+	 * the use of select + update/insert, this performance is poor.
+	 */
+	default Optional<String> getUpsertStatement(
+		String tableName, String[] fieldNames, String[] uniqueKeyFields) {
+		return Optional.empty();
+	}
 
-    /**
-     * Get update one row statement by condition fields, default not use limit 1,
-     * because limit 1 is a sql dialect.
-     */
-    default String getUpdateStatement(String tableName, String[] fieldNames, String[] conditionFields) {
-        String setClause = Arrays.stream(fieldNames)
-                .map(f -> quoteIdentifier(f) + "=?")
-                .collect(Collectors.joining(", "));
-        String conditionClause = Arrays.stream(conditionFields)
-                .map(f -> quoteIdentifier(f) + "=?")
-                .collect(Collectors.joining(" AND "));
-        return "UPDATE " + quoteTablename(tableName) +
-                " SET " + setClause +
-                " WHERE " + conditionClause;
-    }
+	/**
+	 * Get row exists statement by condition fields. Default use SELECT.
+	 */
+	default String getRowExistsStatement(String tableName, String[] conditionFields) {
+		String fieldExpressions = Arrays.stream(conditionFields)
+			.map(f -> quoteIdentifier(f) + "=?")
+			.collect(Collectors.joining(" AND "));
+		return "SELECT 1 FROM " + quoteIdentifier(tableName) + " WHERE " + fieldExpressions;
+	}
 
-    /**
-     * Get delete one row statement by condition fields, default not use limit 1,
-     * because limit 1 is a sql dialect.
-     */
-    default String getDeleteStatement(String tableName, String[] conditionFields) {
-        String conditionClause = Arrays.stream(conditionFields)
-                .map(f -> quoteIdentifier(f) + "=?")
-                .collect(Collectors.joining(" AND "));
-        return "DELETE FROM " + quoteTablename(tableName) + " WHERE " + conditionClause;
-    }
+	/**
+	 * Get insert into statement.
+	 */
+	default String getInsertIntoStatement(String tableName, String[] fieldNames) {
+		String columns = Arrays.stream(fieldNames)
+			.map(this::quoteIdentifier)
+			.collect(Collectors.joining(", "));
+		String placeholders = Arrays.stream(fieldNames)
+			.map(f -> "?")
+			.collect(Collectors.joining(", "));
+		return "INSERT INTO " + quoteIdentifier(tableName) +
+			"(" + columns + ")" + " VALUES (" + placeholders + ")";
+	}
 
-    /**
-     * Get select fields statement by condition fields. Default use SELECT.
-     */
-    default String getSelectFromStatement(String tableName, String[] selectFields, String[] conditionFields) {
-        String selectExpressions = Arrays.stream(selectFields)
-                .map(this::quoteIdentifier)
-                .collect(Collectors.joining(", "));
-        String fieldExpressions = Arrays.stream(conditionFields)
-                .map(f -> quoteIdentifier(f) + "=?")
-                .collect(Collectors.joining(" AND "));
-        return "SELECT " + selectExpressions + " FROM " +
-                quoteTablename(tableName) + (conditionFields.length > 0 ? " WHERE " + fieldExpressions : "");
-    }
+	/**
+	 * Get update one row statement by condition fields, default not use limit 1,
+	 * because limit 1 is a sql dialect.
+	 */
+	default String getUpdateStatement(String tableName, String[] fieldNames, String[] conditionFields) {
+		String setClause = Arrays.stream(fieldNames)
+			.map(f -> quoteIdentifier(f) + "=?")
+			.collect(Collectors.joining(", "));
+		String conditionClause = Arrays.stream(conditionFields)
+			.map(f -> quoteIdentifier(f) + "=?")
+			.collect(Collectors.joining(" AND "));
+		return "UPDATE " + quoteIdentifier(tableName) +
+			" SET " + setClause +
+			" WHERE " + conditionClause;
+	}
+
+	/**
+	 * Get delete one row statement by condition fields, default not use limit 1,
+	 * because limit 1 is a sql dialect.
+	 */
+	default String getDeleteStatement(String tableName, String[] conditionFields) {
+		String conditionClause = Arrays.stream(conditionFields)
+			.map(f -> quoteIdentifier(f) + "=?")
+			.collect(Collectors.joining(" AND "));
+		return "DELETE FROM " + quoteIdentifier(tableName) + " WHERE " + conditionClause;
+	}
+
+	/**
+	 * Get select fields statement by condition fields. Default use SELECT.
+	 */
+	default String getSelectFromStatement(String tableName, String[] selectFields, String[] conditionFields) {
+		String selectExpressions = Arrays.stream(selectFields)
+				.map(this::quoteIdentifier)
+				.collect(Collectors.joining(", "));
+		String fieldExpressions = Arrays.stream(conditionFields)
+				.map(f -> quoteIdentifier(f) + "=?")
+				.collect(Collectors.joining(" AND "));
+		return "SELECT " + selectExpressions + " FROM " +
+				quoteIdentifier(tableName) + (conditionFields.length > 0 ? " WHERE " + fieldExpressions : "");
+	}
 }
