4cf50603bdfa17d2f6ea5196b823cab10dbeaccd
Andrey Zagrebin <azagrebin@apache.org>
2020-08-06 16:22:35 +0300

    [hotfix] Move DualKeyLinkedMap to util package

--- flink-runtime/src/test/java/org/apache/flink/runtime/util/DualKeyLinkedMapTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/util/DualKeyLinkedMapTest.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.apache.flink.runtime.jobmaster.slotpool;
+package org.apache.flink.runtime.util;
 
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.TestLogger;
