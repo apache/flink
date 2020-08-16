15c4f2cad1e921a943aa52ee482dcfcf81945e9f
Hequn Cheng <hequn.chq@alibaba-inc.com>
2020-08-16 21:12:04 +0800

    [FLINK-18945][python] Support CoFlatMap for Python DataStream API (#13152)

--- flink-python/pyflink/datastream/functions.py
+++ flink-python/pyflink/datastream/functions.py
@@ -63,7 +63,7 @@ class CoMapFunction(Function):
     The same instance of the transformation function is used to transform both of
     the connected streams. That way, the stream transformations can share state.
 
-    The basic syntax for using a MapFunction is as follows:
+    The basic syntax for using a CoMapFunction is as follows:
     ::
         >>> ds1 = ...
         >>> ds2 = ...
@@ -114,6 +114,57 @@ class FlatMapFunction(Function):
                 >>>     def flat_map(self, value):
                 >>>         for i in range(value):
                 >>>             yield i
+
+        :param value: The input value.
+        :return: A genertaor
+        """
+        pass
+
+
+class CoFlatMapFunction(Function):
+    """
+    A CoFlatMapFunction implements a flat-map transformation over two connected streams.
+
+    The same instance of the transformation function is used to transform both of the
+    connected streams. That way, the stream transformations can share state.
+
+    An example for the use of connected streams would be to apply rules that change over time
+    onto elements of a stream. One of the connected streams has the rules, the other stream the
+    elements to apply the rules to. The operation on the connected stream maintains the
+    current set of rules in the state. It may receive either a rule update (from the first stream)
+    and update the state, or a data element (from the second stream) and apply the rules in the
+    state to the element. The result of applying the rules would be emitted.
+
+    The basic syntax for using a CoFlatMapFunction is as follows:
+    ::
+        >>> ds1 = ...
+        >>> ds2 = ...
+
+        >>> class MyCoFlatMapFunction(CoFlatMapFunction):
+        >>>     def flat_map1(self, value):
+        >>>         for i in range(value):
+        >>>             yield i
+        >>>     def flat_map2(self, value):
+        >>>         for i in range(value):
+        >>>             yield i
+
+        >>> new_ds = ds1.connect(ds2).flat_map(MyCoFlatMapFunction())
+    """
+
+    @abc.abstractmethod
+    def flat_map1(self, value):
+        """
+        This method is called for each element in the first of the connected streams.
+
+        :param value: The input value.
+        :return: A genertaor
+        """
+        pass
+
+    @abc.abstractmethod
+    def flat_map2(self, value):
+        """
+        This method is called for each element in the second of the connected streams.
 
         :param value: The input value.
         :return: A genertaor
