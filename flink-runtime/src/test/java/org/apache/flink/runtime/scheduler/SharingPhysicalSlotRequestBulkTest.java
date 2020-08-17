dbed0f154c63a16b28530d8d1a5ff351ea64f3d0
Andrey Zagrebin <azagrebin@apache.org>
2020-08-17 19:52:59 +0200

    [FLINK-18957] Implement logical request bulk tracking in SlotSharingExecutionSlotAllocator
    After we started to allocate physical slots and then logical slots from them for a bulk request in SlotSharingExecutionSlotAllocator,
    we need to track the fulfil-ability of the logical slot request bulk. If it cannot be fulfil after the requested timeout,
    we have to cancel all logical slot request futures to fail the bulk allocation.
    The underlying shared physical slots may be occupied by logical slots from different bulks.
    Hence, a physical slot request should be canceled only if there are no logical slots/bulks which need it.
    This ensures we will not wait indefinitely if all required slots for a bulk cannot be fully fulfilled.
    
    Track fulfil-ability steps:
    - Create a SharingPhysicalSlotRequestBulk to track
      - all physical requests and
      - logical slot requests (logical slot requests only which belong to the bulk)
    - Mark physical slot request fulfilled in SharingPhysicalSlotRequestBulk, once its future is done
    - If any physical slot request fails then clear the LogicalSlotRequestBulk to stop the fulfil-ability check
    - Schedule a fulfil-ability check in PhysicalSlotRequestBulkChecker for the SharingPhysicalSlotRequestBulk
    - In case of timeout cancel/fail the logical slot requests of the bulk in SharedSlot(s)
    
    This closes #13181.

---
+++ flink-runtime/src/test/java/org/apache/flink/runtime/scheduler/SharingPhysicalSlotRequestBulkTest.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.scheduler;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.runtime.clusterframework.types.AllocationID;
+import org.apache.flink.runtime.clusterframework.types.ResourceProfile;
+import org.apache.flink.runtime.scheduler.strategy.ExecutionVertexID;
+import org.apache.flink.util.TestLogger;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+import static org.apache.flink.runtime.executiongraph.ExecutionGraphTestUtils.createRandomExecutionVertexId;
+import static org.apache.flink.runtime.scheduler.SharedSlotTestingUtils.createExecutionSlotSharingGroup;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test suite for {@link SharingPhysicalSlotRequestBulk}.
+ */
+public class SharingPhysicalSlotRequestBulkTest extends TestLogger {
+	private static final ExecutionVertexID EV1 = createRandomExecutionVertexId();
+	private static final ExecutionVertexID EV2 = createRandomExecutionVertexId();
+	private static final ExecutionVertexID EV3 = createRandomExecutionVertexId();
+	private static final ExecutionVertexID EV4 = createRandomExecutionVertexId();
+	private static final ExecutionVertexID EV5 = createRandomExecutionVertexId();
+
+	private static final ExecutionSlotSharingGroup SG1 = createExecutionSlotSharingGroup(EV1, EV2, EV3);
+	private static final ExecutionSlotSharingGroup SG2 = createExecutionSlotSharingGroup(EV4, EV5);
+
+	private static final ResourceProfile RP1 = ResourceProfile.newBuilder().setCpuCores(3.0).build();
+	private static final ResourceProfile RP2 = ResourceProfile.newBuilder().setCpuCores(2.0).build();
+
+	@Test
+	public void testCreation() {
+		SharingPhysicalSlotRequestBulk bulk = createBulk();
+		assertThat(bulk.getPendingRequests(), containsInAnyOrder(RP1, RP2));
+		assertThat(bulk.getAllocationIdsOfFulfilledRequests(), hasSize(0));
+	}
+
+	@Test
+	public void testMarkFulfilled() {
+		SharingPhysicalSlotRequestBulk bulk = createBulk();
+		AllocationID allocationId = new AllocationID();
+		bulk.markFulfilled(SG1, allocationId);
+		assertThat(bulk.getPendingRequests(), containsInAnyOrder(RP2));
+		assertThat(bulk.getAllocationIdsOfFulfilledRequests(), containsInAnyOrder(allocationId));
+	}
+
+	@Test
+	public void testCancel() {
+		LogicalSlotRequestCanceller canceller = new LogicalSlotRequestCanceller();
+		SharingPhysicalSlotRequestBulk bulk = createBulk(canceller);
+		bulk.markFulfilled(SG1, new AllocationID());
+		Throwable cause = new Throwable();
+		bulk.cancel(cause);
+		assertThat(
+			canceller.cancellations,
+			containsInAnyOrder(Tuple2.of(EV1, cause), Tuple2.of(EV2, cause), Tuple2.of(EV4, cause)));
+	}
+
+	@Test
+	public void testClearPendingRequests() {
+		SharingPhysicalSlotRequestBulk bulk = createBulk();
+		bulk.clearPendingRequests();
+		assertThat(bulk.getPendingRequests(), hasSize(0));
+	}
+
+	private static class LogicalSlotRequestCanceller implements BiConsumer<ExecutionVertexID, Throwable> {
+		private final List<Tuple2<ExecutionVertexID, Throwable>> cancellations = new ArrayList<>();
+
+		@Override
+		public void accept(ExecutionVertexID executionVertexID, Throwable throwable) {
+			cancellations.add(Tuple2.of(executionVertexID, throwable));
+		}
+	}
+
+	private static SharingPhysicalSlotRequestBulk createBulk() {
+		return createBulk((executionVertexID, throwable) -> {});
+	}
+
+	private static SharingPhysicalSlotRequestBulk createBulk(BiConsumer<ExecutionVertexID, Throwable> canceller) {
+		Map<ExecutionSlotSharingGroup, ResourceProfile> pendingRequests = new IdentityHashMap<>();
+		pendingRequests.put(SG1, RP1);
+		pendingRequests.put(SG2, RP2);
+
+		Map<ExecutionSlotSharingGroup, List<ExecutionVertexID>> executions = new HashMap<>();
+		executions.put(SG1, Arrays.asList(EV1, EV2));
+		executions.put(SG2, Collections.singletonList(EV4));
+
+		return new SharingPhysicalSlotRequestBulk(executions, pendingRequests, canceller);
+	}
+}
