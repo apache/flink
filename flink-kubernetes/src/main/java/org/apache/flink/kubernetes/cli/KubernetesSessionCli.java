3d0ec8202d20e69a2e0f76598566378a28152dd6
Chesnay Schepler <chesnay@apache.org>
2020-08-18 11:36:03 +0200

    [FLINK-18986][kubernetes]  Create ClusterClient only for attached deployments

--- flink-kubernetes/src/main/java/org/apache/flink/kubernetes/cli/KubernetesSessionCli.java
+++ flink-kubernetes/src/main/java/org/apache/flink/kubernetes/cli/KubernetesSessionCli.java
@@ -29,6 +29,7 @@ import org.apache.flink.client.deployment.ClusterClientServiceLoader;
 import org.apache.flink.client.deployment.ClusterDescriptor;
 import org.apache.flink.client.deployment.DefaultClusterClientServiceLoader;
 import org.apache.flink.client.program.ClusterClient;
+import org.apache.flink.client.program.ClusterClientProvider;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.DeploymentOptions;
 import org.apache.flink.configuration.GlobalConfiguration;
@@ -96,40 +97,51 @@ public class KubernetesSessionCli {
 			kubernetesClusterClientFactory.createClusterDescriptor(configuration);
 
 		try {
-			final ClusterClient<String> clusterClient;
+			final ClusterClientProvider<String> clusterClientProvider;
 			String clusterId = kubernetesClusterClientFactory.getClusterId(configuration);
 			final boolean detached = !configuration.get(DeploymentOptions.ATTACHED);
-			final FlinkKubeClient kubeClient = KubeClientFactory.fromConfiguration(configuration);
 
-			// Retrieve or create a session cluster.
-			if (clusterId != null && kubeClient.getRestService(clusterId).isPresent()) {
-				clusterClient = kubernetesClusterDescriptor.retrieve(clusterId).getClusterClient();
-			} else {
-				clusterClient = kubernetesClusterDescriptor
-						.deploySessionCluster(
-								kubernetesClusterClientFactory.getClusterSpecification(configuration))
-						.getClusterClient();
-				clusterId = clusterClient.getClusterId();
-			}
-
-			try {
-				if (!detached) {
-					Tuple2<Boolean, Boolean> continueRepl = new Tuple2<>(true, false);
-					try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
-						while (continueRepl.f0) {
-							continueRepl = repStep(in);
-						}
-					} catch (Exception e) {
-						LOG.warn("Exception while running the interactive command line interface.", e);
-					}
-					if (continueRepl.f1) {
-						kubernetesClusterDescriptor.killCluster(clusterId);
-					}
-				}
-				clusterClient.close();
-				kubeClient.close();
+			boolean connectToExistingCluster = false;
+			try (final FlinkKubeClient kubeClient = KubeClientFactory.fromConfiguration(configuration)) {
+				connectToExistingCluster = clusterId != null && kubeClient.getRestService(clusterId).isPresent();
 			} catch (Exception e) {
 				LOG.info("Could not properly shutdown cluster client.", e);
+			}
+
+			// Retrieve or create a session cluster.
+			if (connectToExistingCluster) {
+				clusterClientProvider = kubernetesClusterDescriptor.retrieve(clusterId);
+			} else {
+				clusterClientProvider = kubernetesClusterDescriptor
+						.deploySessionCluster(
+								kubernetesClusterClientFactory.getClusterSpecification(configuration));
+			}
+
+			if (!detached) {
+				if (!connectToExistingCluster) {
+					try (ClusterClient<String> clusterClient = clusterClientProvider.getClusterClient()) {
+						clusterId = clusterClient.getClusterId();
+					} catch (Exception e) {
+						LOG.info("Could not properly shutdown cluster client.", e);
+					}
+				}
+
+				Tuple2<Boolean, Boolean> continueRepl = new Tuple2<>(true, false);
+				try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+					while (continueRepl.f0) {
+						continueRepl = repStep(in);
+					}
+				} catch (Exception e) {
+					LOG.warn("Exception while running the interactive command line interface.", e);
+				}
+
+				if (continueRepl.f1) {
+					try {
+						kubernetesClusterDescriptor.killCluster(clusterId);
+					} catch (FlinkException e) {
+						LOG.info("An error occurred while shutting down the cluster {}.", clusterId, e);
+					}
+				}
 			}
 		} finally {
 			try {
