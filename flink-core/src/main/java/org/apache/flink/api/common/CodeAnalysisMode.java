60725808134a5f873d35a5f63dce893194a63238
Dawid Wysakowicz <dwysakowicz@apache.org>
2020-08-31 17:15:59 +0200

    Update CodeAnalysisMode description

--- flink-core/src/main/java/org/apache/flink/api/common/CodeAnalysisMode.java
+++ flink-core/src/main/java/org/apache/flink/api/common/CodeAnalysisMode.java
@@ -19,12 +19,16 @@
 package org.apache.flink.api.common;
 
 import org.apache.flink.annotation.PublicEvolving;
+import org.apache.flink.api.common.typeutils.TypeSerializerConfigSnapshot;
 import org.apache.flink.api.java.typeutils.runtime.PojoSerializer;
 
 /**
  * @deprecated The code analysis code has been removed and this enum has no effect.
  * <b>NOTE</b> It can not be removed from the codebase for now, because it had been serialized as part
  * of the {@link ExecutionConfig} which in turn had been serialized as part of the {@link PojoSerializer}.
+ *
+ * <p>This class can be removed when we drop support for pre 1.8 serializer snapshots that contained java
+ * serialized serializers ({@link TypeSerializerConfigSnapshot}).
  */
 @PublicEvolving
 @Deprecated
