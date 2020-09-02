b336c72a676125d3fe9b6b42d1bc2a8fc99c6868
Aljoscha Krettek <aljoscha@apache.org>
2020-09-02 16:07:54 +0200

    [FLINK-19123] Move PerJobMiniClusterJobClient to runtime.minicluster package

--- flink-clients/src/main/java/org/apache/flink/client/program/PerJobMiniClusterFactory.java
+++ flink-clients/src/main/java/org/apache/flink/client/program/PerJobMiniClusterFactory.java
@@ -26,6 +26,7 @@ import org.apache.flink.core.execution.JobClient;
 import org.apache.flink.runtime.jobgraph.JobGraph;
 import org.apache.flink.runtime.minicluster.MiniCluster;
 import org.apache.flink.runtime.minicluster.MiniClusterConfiguration;
+import org.apache.flink.runtime.minicluster.PerJobMiniClusterJobClient;
 import org.apache.flink.runtime.minicluster.RpcServiceSharing;
 import org.apache.flink.util.MathUtils;
 import org.apache.flink.util.function.FunctionUtils;
