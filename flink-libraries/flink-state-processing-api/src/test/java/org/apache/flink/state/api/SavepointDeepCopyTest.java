3f5466e288a53fac8d766bffc1ec87e96d34dcc8
Jun Qin <11677043+qinjunjerry@users.noreply.github.com>
2020-09-06 22:57:08 +0200

    [FLINK-14942][state-processor-api] add assert failure reason

--- flink-libraries/flink-state-processing-api/src/test/java/org/apache/flink/state/api/SavepointDeepCopyTest.java
+++ flink-libraries/flink-state-processing-api/src/test/java/org/apache/flink/state/api/SavepointDeepCopyTest.java
@@ -192,8 +192,8 @@ public class SavepointDeepCopyTest extends AbstractTestBase {
 			.map(path -> path.getFileName().toString())
 			.collect(Collectors.toSet());
 
-		// check every state file in savepoint1 are also in savepoint2
-		assertThat(stateFiles1, everyItem(isIn(stateFiles2)));
+		assertThat("At least one state file in savepoint1 are not in savepoint2",
+			stateFiles1, everyItem(isIn(stateFiles2)));
 
 		// Try to load savepoint2 and read the state of "Operator1" (which has not been touched/changed when savepoint2
 		// was created) and make sure the number of keys remain same
