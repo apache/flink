33a0cab7904d3cccd1a01054a4a9fc9f81d6762a
Jun Qin <11677043+qinjunjerry@users.noreply.github.com>
2020-09-07 10:19:10 +0200

    [FLINK-14942][state-processor-api] use flink's filesytem to copy files

--- flink-libraries/flink-state-processing-api/src/main/java/org/apache/flink/state/api/output/FileCopyMapFunction.java
+++ flink-libraries/flink-state-processing-api/src/main/java/org/apache/flink/state/api/output/FileCopyMapFunction.java
@@ -20,11 +20,11 @@ package org.apache.flink.state.api.output;
 
 import org.apache.flink.annotation.Internal;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.core.fs.FSDataInputStream;
+import org.apache.flink.core.fs.FSDataOutputStream;
+import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.util.Preconditions;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 /**
  * This mapper copies files from an existing savepoint into a new directory.
@@ -33,6 +33,7 @@ import java.nio.file.Paths;
 public final class FileCopyMapFunction implements MapFunction<String, String> {
 
 	private static final long serialVersionUID = 1L;
+	private static final int BUFFER_SIZE = 4 * 1024;
 
 	// the destination path to copy file
 	private final String path;
@@ -41,16 +42,33 @@ public final class FileCopyMapFunction implements MapFunction<String, String> {
 		this.path = Preconditions.checkNotNull(path, "The destination path cannot be null");
 	}
 
+	private void copy(Path sourcePath, Path destPath) throws Exception {
+		FSDataInputStream is = null;
+		FSDataOutputStream os = null;
+		try {
+			os = destPath.getFileSystem().create(destPath, FileSystem.WriteMode.NO_OVERWRITE);
+			is = sourcePath.getFileSystem().open(sourcePath);
+			byte[] buffer = new byte[BUFFER_SIZE];
+			int length;
+			while ((length = is.read(buffer)) > 0) {
+				os.write(buffer, 0, length);
+			}
+		} finally {
+			is.close();
+			os.close();
+		}
+	}
+
 	@Override
 	public String map(String sourceFile) throws Exception {
-		// Create the destination directory before copying. It is not a problem if it exists already.
-		Path destPath = new Path(path);
-		destPath.getFileSystem().mkdirs(destPath);
+		// Create the destination parent directory before copy. It is not a problem if it exists already.
+		Path destParent = new Path(path);
+		destParent.getFileSystem().mkdirs(destParent);
 
-		Files.copy(
-			Paths.get(sourceFile), // source file
-			Paths.get(path, Paths.get(sourceFile).getFileName().toString()) // destination file
-		);
+		// Now copy file
+		Path sourcePath = new Path(sourceFile);
+		Path destPath = new Path(path, sourcePath.getName());
+		copy(sourcePath, destPath);
 		return sourceFile;
 	}
 }
