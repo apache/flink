83c45e976772caf791dda5eb8d05db8bb91905dd
Dawid Wysakowicz <dwysakowicz@apache.org>
2020-09-10 10:14:40 +0200

    [hotfix] Fix Pojo comparator field access
    The PojoComparator assumes it can access a field of a pojo directly. It assumes the field is either public or setAccessible was called before. This is the case though only if  the record went through serialization. This is not the case e.g. in CollectionExecution mode.
    
    Starting from this commit we make all fields accessible in
    PojoComparator constructor.

--- flink-core/src/main/java/org/apache/flink/api/java/typeutils/runtime/PojoComparator.java
+++ flink-core/src/main/java/org/apache/flink/api/java/typeutils/runtime/PojoComparator.java
@@ -36,7 +36,7 @@ import org.apache.flink.util.InstantiationUtil;
 
 @Internal
 public final class PojoComparator<T> extends CompositeTypeComparator<T> implements java.io.Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	// Reflection fields for the comp fields
@@ -69,6 +69,10 @@ public final class PojoComparator<T> extends CompositeTypeComparator<T> implemen
 		int nKeys = 0;
 		int nKeyLen = 0;
 		boolean inverted = false;
+
+		for (Field keyField : keyFields) {
+			keyField.setAccessible(true);
+		}
 
 		for (int i = 0; i < this.comparators.length; i++) {
 			TypeComparator<?> k = this.comparators[i];
@@ -170,7 +174,7 @@ public final class PojoComparator<T> extends CompositeTypeComparator<T> implemen
 			}
 		}
 	}
-	
+
 	/**
 	 * This method is handling the IllegalAccess exceptions of Field.get()
 	 */
@@ -180,7 +184,7 @@ public final class PojoComparator<T> extends CompositeTypeComparator<T> implemen
 		} catch (NullPointerException npex) {
 			throw new NullKeyFieldException("Unable to access field "+field+" on object "+object);
 		} catch (IllegalAccessException iaex) {
-			throw new RuntimeException("This should not happen since we call setAccesssible(true) in PojoTypeInfo."
+			throw new RuntimeException("This should not happen since we call setAccesssible(true) in the ctor."
 			+ " fields: " + field + " obj: " + object);
 		}
 		return object;
