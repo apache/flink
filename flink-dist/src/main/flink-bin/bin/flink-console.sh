3387f5265a6c32c503bf4d7d1585e77114f87cc7
Robert Metzger <rmetzger@apache.org>
2020-09-21 12:15:44 +0200

    [hotfix][start scripts] Remove java version check

--- flink-dist/src/main/flink-bin/bin/flink-console.sh
+++ flink-dist/src/main/flink-bin/bin/flink-console.sh
@@ -88,15 +88,6 @@ log="${FLINK_LOG_PREFIX}.log"
 
 log_setting=("-Dlog.file=${log}" "-Dlog4j.configuration=file:${FLINK_CONF_DIR}/log4j-console.properties" "-Dlog4j.configurationFile=file:${FLINK_CONF_DIR}/log4j-console.properties" "-Dlogback.configurationFile=file:${FLINK_CONF_DIR}/logback-console.xml")
 
-JAVA_VERSION=$(${JAVA_RUN} -version 2>&1 | sed 's/.*version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
-
-# Only set JVM 8 arguments if we have correctly extracted the version
-if [[ ${JAVA_VERSION} =~ ${IS_NUMBER} ]]; then
-    if [ "$JAVA_VERSION" -lt 18 ]; then
-        JVM_ARGS="$JVM_ARGS -XX:MaxPermSize=256m"
-    fi
-fi
-
 echo "Starting $SERVICE as a console application on host $HOSTNAME."
 
 # Add the current process id to pid file
