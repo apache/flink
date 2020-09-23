8a3e40bdb01c11bde0f0b78712f9763d12a659c1
huangxingbo <hxbks2ks@gmail.com>
2020-09-23 11:38:28 +0800

    [FLINK-19364][python] Add Batch Physical Pandas Group Window Aggregate Rule and RelNode
    This closes #13460.

--- flink-python/pyflink/table/tests/test_pandas_udaf.py
+++ flink-python/pyflink/table/tests/test_pandas_udaf.py
@@ -17,6 +17,7 @@
 ################################################################################
 import unittest
 
+from pyflink.table import expressions as expr
 from pyflink.table.types import DataTypes
 from pyflink.table.udf import udaf, udf, AggregateFunction
 from pyflink.testing import source_sink_utils
@@ -93,6 +94,100 @@ class BatchPandasUDAFITTests(PyFlinkBlinkBatchTableTestCase):
             .wait()
         actual = source_sink_utils.results()
         self.assert_equals(actual, ["1,2,2.0,6", "2,3,3.0,8", "3,4,4.0,10"])
+
+    def test_tumble_group_window_aggregate_function(self):
+        import datetime
+        from pyflink.table.window import Tumble
+        t = self.t_env.from_elements(
+            [
+                (1, 2, 3, datetime.datetime(2018, 3, 11, 3, 10, 0, 0)),
+                (3, 2, 4, datetime.datetime(2018, 3, 11, 3, 10, 0, 0)),
+                (2, 1, 2, datetime.datetime(2018, 3, 11, 3, 10, 0, 0)),
+                (1, 3, 1, datetime.datetime(2018, 3, 11, 3, 40, 0, 0)),
+                (1, 8, 5, datetime.datetime(2018, 3, 11, 4, 20, 0, 0)),
+                (2, 3, 6, datetime.datetime(2018, 3, 11, 3, 30, 0, 0))
+            ],
+            DataTypes.ROW(
+                [DataTypes.FIELD("a", DataTypes.TINYINT()),
+                 DataTypes.FIELD("b", DataTypes.SMALLINT()),
+                 DataTypes.FIELD("c", DataTypes.INT()),
+                 DataTypes.FIELD("rowtime", DataTypes.TIMESTAMP(3))]))
+
+        table_sink = source_sink_utils.TestAppendSink(
+            ['a', 'b', 'c'],
+            [
+                DataTypes.TIMESTAMP(3),
+                DataTypes.TIMESTAMP(3),
+                DataTypes.FLOAT()
+            ])
+        self.t_env.register_table_sink("Results", table_sink)
+        self.t_env.create_temporary_system_function("mean_udaf", mean_udaf)
+        tumble_window = Tumble.over(expr.lit(1).hours) \
+            .on(expr.col("rowtime")) \
+            .alias("w")
+        t.window(tumble_window) \
+            .group_by("w") \
+            .select("w.start, w.end, mean_udaf(b)") \
+            .execute_insert("Results") \
+            .wait()
+
+        actual = source_sink_utils.results()
+        self.assert_equals(actual,
+                           ["2018-03-11 03:00:00.0,2018-03-11 04:00:00.0,2.2",
+                            "2018-03-11 04:00:00.0,2018-03-11 05:00:00.0,8.0"])
+
+    def test_slide_group_window_aggregate_function(self):
+        import datetime
+        from pyflink.table.window import Slide
+        t = self.t_env.from_elements(
+            [
+                (1, 2, 3, datetime.datetime(2018, 3, 11, 3, 10, 0, 0)),
+                (3, 2, 4, datetime.datetime(2018, 3, 11, 3, 10, 0, 0)),
+                (2, 1, 2, datetime.datetime(2018, 3, 11, 3, 10, 0, 0)),
+                (1, 3, 1, datetime.datetime(2018, 3, 11, 3, 40, 0, 0)),
+                (1, 8, 5, datetime.datetime(2018, 3, 11, 4, 20, 0, 0)),
+                (2, 3, 6, datetime.datetime(2018, 3, 11, 3, 30, 0, 0))
+            ],
+            DataTypes.ROW(
+                [DataTypes.FIELD("a", DataTypes.TINYINT()),
+                 DataTypes.FIELD("b", DataTypes.SMALLINT()),
+                 DataTypes.FIELD("c", DataTypes.INT()),
+                 DataTypes.FIELD("rowtime", DataTypes.TIMESTAMP(3))]))
+
+        table_sink = source_sink_utils.TestAppendSink(
+            ['a', 'b', 'c', 'd', 'e'],
+            [
+                DataTypes.TINYINT(),
+                DataTypes.TIMESTAMP(3),
+                DataTypes.TIMESTAMP(3),
+                DataTypes.FLOAT(),
+                DataTypes.INT()
+            ])
+        self.t_env.register_table_sink("Results", table_sink)
+        self.t_env.register_function("max_add", udaf(MaxAdd(),
+                                                     result_type=DataTypes.INT(),
+                                                     func_type="pandas"))
+        self.t_env.create_temporary_system_function("mean_udaf", mean_udaf)
+        slide_window = Slide.over(expr.lit(1).hours) \
+            .every(expr.lit(30).minutes) \
+            .on(expr.col("rowtime")) \
+            .alias("w")
+        t.window(slide_window) \
+            .group_by("a, w") \
+            .select("a, w.start, w.end, mean_udaf(b), max_add(b, c, 1)") \
+            .execute_insert("Results") \
+            .wait()
+        actual = source_sink_utils.results()
+        self.assert_equals(actual,
+                           ["1,2018-03-11 02:30:00.0,2018-03-11 03:30:00.0,2.0,6",
+                            "1,2018-03-11 03:00:00.0,2018-03-11 04:00:00.0,2.5,7",
+                            "1,2018-03-11 03:30:00.0,2018-03-11 04:30:00.0,5.5,14",
+                            "1,2018-03-11 04:00:00.0,2018-03-11 05:00:00.0,8.0,14",
+                            "2,2018-03-11 02:30:00.0,2018-03-11 03:30:00.0,1.0,4",
+                            "2,2018-03-11 03:00:00.0,2018-03-11 04:00:00.0,2.0,10",
+                            "2,2018-03-11 03:30:00.0,2018-03-11 04:30:00.0,3.0,10",
+                            "3,2018-03-11 03:00:00.0,2018-03-11 04:00:00.0,2.0,7",
+                            "3,2018-03-11 02:30:00.0,2018-03-11 03:30:00.0,2.0,7"])
 
 
 @udaf(result_type=DataTypes.FLOAT(), func_type="pandas")
