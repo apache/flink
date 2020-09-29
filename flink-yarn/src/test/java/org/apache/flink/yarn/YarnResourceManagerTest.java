798c004076b18375050331a976b8dc56c6781fe7
Rong Rong <rongr@apache.org>
2020-09-29 02:18:42 -0700

    [FLINK-16789][coordination][rest] Expose TaskExecutor JMX port

--- flink-yarn/src/test/java/org/apache/flink/yarn/YarnResourceManagerTest.java
+++ flink-yarn/src/test/java/org/apache/flink/yarn/YarnResourceManagerTest.java
@@ -249,6 +249,7 @@ public class YarnResourceManagerTest extends TestLogger {
 		final TestingYarnResourceManager resourceManager;
 
 		final int dataPort = 1234;
+		final int jmxPort = 1234;
 		final HardwareDescription hardwareDescription = new HardwareDescription(1, 2L, 3L, 4L);
 
 		// domain objects for test purposes
@@ -426,6 +427,7 @@ public class YarnResourceManagerTest extends TestLogger {
 					taskHost,
 					taskManagerResourceId,
 					dataPort,
+					jmxPort,
 					hardwareDescription,
 					new TaskExecutorMemoryConfiguration(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L),
 					ResourceProfile.ZERO,
