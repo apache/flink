fb34ce741a26fede33c6ae316558c8bd9b10e5d6
Till Rohrmann <trohrmann@apache.org>
2020-10-09 13:01:48 +0200

    [FLINK-17458] Stop TaskSubmissionTestEnvironment.testingRpcService when closing the environment
    This closes #13569.

--- flink-runtime/src/test/java/org/apache/flink/runtime/taskexecutor/TaskSubmissionTestEnvironment.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/taskexecutor/TaskSubmissionTestEnvironment.java
@@ -43,7 +43,6 @@ import org.apache.flink.runtime.jobmaster.utils.TestingJobMasterGatewayBuilder;
 import org.apache.flink.runtime.leaderretrieval.SettableLeaderRetrievalService;
 import org.apache.flink.runtime.metrics.groups.UnregisteredMetricGroups;
 import org.apache.flink.runtime.rpc.RpcService;
-import org.apache.flink.runtime.rpc.RpcUtils;
 import org.apache.flink.runtime.rpc.TestingRpcService;
 import org.apache.flink.runtime.shuffle.ShuffleEnvironment;
 import org.apache.flink.runtime.state.TaskExecutorLocalStateStoresManager;
@@ -261,7 +260,7 @@ class TaskSubmissionTestEnvironment implements AutoCloseable {
 
 	@Override
 	public void close() throws Exception {
-		RpcUtils.terminateRpcEndpoint(taskExecutor, timeout);
+		testingRpcService.stopService().join();
 
 		timerService.stop();
 
