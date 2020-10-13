756a5cb0b8db4afba02b7a6213f04735ceeeb548
huangxingbo <hxbks2ks@gmail.com>
2020-10-13 16:06:42 +0800

    [FLINK-18836][python] Support Python UDTF return type is Row

--- flink-python/pyflink/table/tests/test_udtf.py
+++ flink-python/pyflink/table/tests/test_udtf.py
@@ -37,6 +37,8 @@ class UserDefinedTableFunctionTests(object):
         t = t.join_lateral(multi_emit(t.a, multi_num(t.b)).alias('x', 'y'))
         t = t.left_outer_join_lateral(condition_multi_emit(t.x, t.y).alias('m')) \
             .select("x, y, m")
+        t = t.left_outer_join_lateral(identity(t.m).alias('n')) \
+            .select("x, y, n")
         actual = self._get_output(t)
         self.assert_equals(actual,
                            ["1,0,null", "1,1,null", "2,0,null", "2,1,null", "3,0,0", "3,0,1",
@@ -104,6 +106,13 @@ class MultiEmit(TableFunction, unittest.TestCase):
         self.assertEqual(self.counter_sum, self.counter.get_count())
         for i in range(y):
             yield x, i
+
+
+@udtf(result_types=[DataTypes.BIGINT()])
+def identity(x):
+    if x is not None:
+        from pyflink.common import Row
+        return Row(x)
 
 
 # test specify the input_types
