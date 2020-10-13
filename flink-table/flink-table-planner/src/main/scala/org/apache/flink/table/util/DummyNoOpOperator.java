786726ddd974d46e97b6baa8f7aafd4e45668b0a
wangxlong <18868816710@163.com>
2020-10-14 00:19:10 +0800

    [Hotfix][docs][checkstyle] Fix DummyInputFormat class lacks Javadoc

--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/util/DummyNoOpOperator.java
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/util/DummyNoOpOperator.java
@@ -40,6 +40,9 @@ public class DummyNoOpOperator<IN> extends NoOpOperator<IN> {
 		setInput(input);
 	}
 
+	/**
+	 * Provides a {@link FileInputFormat} for {@link DummyNoOpOperator}.
+	 */
 	public static class DummyInputFormat<IN> extends FileInputFormat<IN> {
 
 		@Override
