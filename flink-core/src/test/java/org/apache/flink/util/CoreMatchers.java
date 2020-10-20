58643ea3b75e7c575a4d2bf5ac4736918d526b8c
Till Rohrmann <trohrmann@apache.org>
2020-10-20 11:06:43 +0200

    [hotfix] Add CoreMatchers.willNotComplete to assert that a future times out

--- flink-core/src/test/java/org/apache/flink/util/CoreMatchers.java
+++ flink-core/src/test/java/org/apache/flink/util/CoreMatchers.java
@@ -22,7 +22,12 @@ import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeDiagnosingMatcher;
 
+import java.time.Duration;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Hamcrest matchers.
@@ -34,6 +39,13 @@ public class CoreMatchers {
 	 */
 	public static Matcher<Throwable> containsCause(Throwable failureCause) {
 		return new ContainsCauseMatcher(failureCause);
+	}
+
+	/**
+	 * Checks that a {@link CompletableFuture} won't complete within the given timeout.
+	 */
+	public static Matcher<CompletableFuture<?>> willNotComplete(Duration timeout) {
+		return new WillNotCompleteMatcher(timeout);
 	}
 
 	private static class ContainsCauseMatcher extends TypeSafeDiagnosingMatcher<Throwable> {
@@ -67,6 +79,43 @@ public class CoreMatchers {
 			description
 				.appendText("Expected failure cause is ")
 				.appendValue(failureCause);
+		}
+	}
+
+	private static final class WillNotCompleteMatcher extends TypeSafeDiagnosingMatcher<CompletableFuture<?>> {
+
+		private final Duration timeout;
+
+		private WillNotCompleteMatcher(Duration timeout) {
+			this.timeout = timeout;
+		}
+
+		@Override
+		protected boolean matchesSafely(
+				CompletableFuture<?> item,
+				Description mismatchDescription) {
+
+			try {
+				final Object value = item.get(timeout.toMillis(), TimeUnit.MILLISECONDS);
+				mismatchDescription.appendText("The given future completed with ")
+						.appendValue(value);
+			} catch (TimeoutException timeoutException) {
+				return true;
+			} catch (InterruptedException e) {
+				mismatchDescription.appendText("The waiting thread was interrupted.");
+			} catch (ExecutionException e) {
+				mismatchDescription.appendText("The given future was completed exceptionally: ")
+						.appendValue(e);
+			}
+
+			return false;
+		}
+
+		@Override
+		public void describeTo(Description description) {
+			description.appendText("The given future should not complete within ")
+					.appendValue(timeout.toMillis())
+					.appendText(" ms.");
 		}
 	}
 
