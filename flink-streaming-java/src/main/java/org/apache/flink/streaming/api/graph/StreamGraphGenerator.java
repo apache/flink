8fee135e7951eabfd9701f062721e969440fc7f7
Kostas Kloudas <kkloudas@gmail.com>
2020-10-20 09:36:22 +0200

    [FLINK-19732] Disable checkpointing in BATCH mode
    When a pipeline is executed in BATCH mode, its tasks
    are scheduled in independent regions and not all tasks
    are present throughout the execution. In this cases
    checkpointing should be disabled.
    
    This closes #13698

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/graph/StreamGraphGenerator.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/graph/StreamGraphGenerator.java
@@ -194,7 +194,7 @@ public class StreamGraphGenerator {
 			ReadableConfig configuration) {
 		this.transformations = checkNotNull(transformations);
 		this.executionConfig = checkNotNull(executionConfig);
-		this.checkpointConfig = checkNotNull(checkpointConfig);
+		this.checkpointConfig = new CheckpointConfig(checkpointConfig);
 		this.configuration = checkNotNull(configuration);
 	}
 
@@ -266,6 +266,12 @@ public class StreamGraphGenerator {
 		graph.setJobName(jobName);
 
 		if (shouldExecuteInBatchMode) {
+
+			if (checkpointConfig.isCheckpointingEnabled()) {
+				LOG.info("Disabled Checkpointing. Checkpointing is not supported and not needed when executing jobs in BATCH mode.");
+				checkpointConfig.disableCheckpointing();
+			}
+
 			graph.setAllVerticesInSameSlotSharingGroupByDefault(false);
 			graph.setGlobalDataExchangeMode(GlobalDataExchangeMode.POINTWISE_EDGES_PIPELINED);
 			graph.setScheduleMode(ScheduleMode.LAZY_FROM_SOURCES_WITH_BATCH_SLOT_REQUEST);
