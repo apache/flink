cfa8761eea2337a2863df505032ac0c3ef5c5527
godfreyhe <godfreyhe@163.com>
2020-10-22 08:49:59 +0800

    [FLINK-19627][table-runtime] Introduce OneInput, FirstInputOfTwoInput, SecondInputOfTwoInput, InputSelectionHandler for multiple input operator

--- flink-table/flink-table-runtime-blink/src/test/java/org/apache/flink/table/runtime/operators/multipleinput/TestingTwoInputStreamOperator.java
+++ flink-table/flink-table-runtime-blink/src/test/java/org/apache/flink/table/runtime/operators/multipleinput/TestingTwoInputStreamOperator.java
@@ -21,6 +21,8 @@ package org.apache.flink.table.runtime.operators.multipleinput;
 import org.apache.flink.streaming.api.operators.AbstractStreamOperator;
 import org.apache.flink.streaming.api.operators.BoundedMultiInput;
 import org.apache.flink.streaming.api.operators.TwoInputStreamOperator;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.runtime.streamrecord.LatencyMarker;
 import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
 import org.apache.flink.table.data.RowData;
 
@@ -33,17 +35,43 @@ import java.util.List;
 public class TestingTwoInputStreamOperator extends AbstractStreamOperator<RowData>
 		implements TwoInputStreamOperator<RowData, RowData, RowData>, BoundedMultiInput {
 
+	private StreamRecord<RowData> currentElement1 = null;
+	private StreamRecord<RowData> currentElement2 = null;
+	private Watermark currentWatermark1 = null;
+	private Watermark currentWatermark2 = null;
+	private LatencyMarker currentLatencyMarker1 = null;
+	private LatencyMarker currentLatencyMarker2 = null;
 	private final List<Integer> endInputs = new ArrayList<>();
 	private boolean isClosed = false;
 
 	@Override
 	public void processElement1(StreamRecord<RowData> element) throws Exception {
-
+		currentElement1 = element;
 	}
 
 	@Override
 	public void processElement2(StreamRecord<RowData> element) throws Exception {
+		currentElement2 = element;
+	}
 
+	@Override
+	public void processWatermark1(Watermark mark) throws Exception {
+		currentWatermark1 = mark;
+	}
+
+	@Override
+	public void processWatermark2(Watermark mark) throws Exception {
+		currentWatermark2 = mark;
+	}
+
+	@Override
+	public void processLatencyMarker1(LatencyMarker latencyMarker) throws Exception {
+		currentLatencyMarker1 = latencyMarker;
+	}
+
+	@Override
+	public void processLatencyMarker2(LatencyMarker latencyMarker) throws Exception {
+		currentLatencyMarker2 = latencyMarker;
 	}
 
 	@Override
@@ -54,6 +82,30 @@ public class TestingTwoInputStreamOperator extends AbstractStreamOperator<RowDat
 	@Override
 	public void close() throws Exception {
 		isClosed = true;
+	}
+
+	public StreamRecord<RowData> getCurrentElement1() {
+		return currentElement1;
+	}
+
+	public StreamRecord<RowData> getCurrentElement2() {
+		return currentElement2;
+	}
+
+	public Watermark getCurrentWatermark1() {
+		return currentWatermark1;
+	}
+
+	public Watermark getCurrentWatermark2() {
+		return currentWatermark2;
+	}
+
+	public LatencyMarker getCurrentLatencyMarker1() {
+		return currentLatencyMarker1;
+	}
+
+	public LatencyMarker getCurrentLatencyMarker2() {
+		return currentLatencyMarker2;
 	}
 
 	public List<Integer> getEndInputs() {
