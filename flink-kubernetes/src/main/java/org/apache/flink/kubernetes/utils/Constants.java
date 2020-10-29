0198805683ff344bb960a539a679479e7cff73e8
wangyang0918 <danrtsey.wy@alibaba-inc.com>
2020-10-29 23:00:09 +0800

    [FLINK-19542][k8s] Implement LeaderElectionService and LeaderRetrievalService based on Kubernetes API
    This closes #13644.

--- flink-kubernetes/src/main/java/org/apache/flink/kubernetes/utils/Constants.java
+++ flink-kubernetes/src/main/java/org/apache/flink/kubernetes/utils/Constants.java
@@ -57,6 +57,8 @@ public class Constants {
 	public static final String LABEL_COMPONENT_KEY = "component";
 	public static final String LABEL_COMPONENT_JOB_MANAGER = "jobmanager";
 	public static final String LABEL_COMPONENT_TASK_MANAGER = "taskmanager";
+	public static final String LABEL_CONFIGMAP_TYPE_KEY = "configmap-type";
+	public static final String LABEL_CONFIGMAP_TYPE_HIGH_AVAILABILITY = "high-availability";
 
 	// Use fixed port in kubernetes, it needs to be exposed.
 	public static final int REST_PORT = 8081;
@@ -87,4 +89,8 @@ public class Constants {
 	public static final String RESTART_POLICY_OF_NEVER = "Never";
 
 	public static final String NATIVE_KUBERNETES_COMMAND = "native-k8s";
+
+	// Constants for Kubernetes high availability
+	public static final String LEADER_ADDRESS_KEY = "address";
+	public static final String LEADER_SESSION_ID_KEY = "sessionId";
 }
