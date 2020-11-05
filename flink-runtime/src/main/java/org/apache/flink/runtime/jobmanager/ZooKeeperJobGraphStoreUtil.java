d9ec86d5b8629187430e3718b0a101fd8640fe6e
wangyang0918 <danrtsey.wy@alibaba-inc.com>
2020-11-05 14:39:47 +0800

    [hotfix][coordination] Wire ZooKeeperJobGraphStore to the new interface
    We introduce three specific composition implementations for ZooKeeper, ZooKeeperJobGraphStoreWatcher, ZooKeeperJobGraphStoreEventHandler, ZooKeeperJobGraphStoreUtil.
    * ZooKeeperJobGraphStoreWatcher is a watcher on JobGraphStore. It could monitor all the changes on the job graph store and notify the DefaultJobGraphStore via JobGraphStore.JobGraphListener.
    * ZooKeeperJobGraphStoreUtil is a utility class which could convert a ZooKeeper path to JobId, or vice versa.

---
+++ flink-runtime/src/main/java/org/apache/flink/runtime/jobmanager/ZooKeeperJobGraphStoreUtil.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.runtime.jobmanager;
+
+import org.apache.flink.api.common.JobID;
+import org.apache.flink.runtime.util.ZooKeeperUtils;
+
+/**
+ * Singleton {@link JobGraphStoreUtil} implementation for ZooKeeper.
+ */
+public enum  ZooKeeperJobGraphStoreUtil implements JobGraphStoreUtil {
+	INSTANCE;
+
+	@Override
+	public String jobIDToName(JobID jobId) {
+		return ZooKeeperUtils.getPathForJob(jobId);
+	}
+
+	@Override
+	public JobID nameToJobID(String name) {
+		return JobID.fromHexString(name);
+	}
+}
