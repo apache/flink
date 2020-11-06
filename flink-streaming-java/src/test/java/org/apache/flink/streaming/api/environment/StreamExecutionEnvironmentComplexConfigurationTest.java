0ec1c4d6ccf7c811b0328cf00e0833ae588aa112
Dawid Wysakowicz <dwysakowicz@apache.org>
2020-11-06 11:07:31 +0100

    [FLINK-20018] Allow escaping in 'pipeline.cached-files' and 'pipeline.default-kryo-serializers'
    This commit enables escaping in options that expect a map of
    string-string entries. It lets users pass options such as e.g.
    
    pipeline.cached-files=name:file1,path:'oss://bucket/file1'

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/api/environment/StreamExecutionEnvironmentComplexConfigurationTest.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/api/environment/StreamExecutionEnvironmentComplexConfigurationTest.java
@@ -30,11 +30,16 @@ import org.apache.flink.core.execution.JobListener;
 import org.apache.flink.runtime.state.StateBackend;
 import org.apache.flink.runtime.state.memory.MemoryStateBackend;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 import org.junit.Test;
 
 import javax.annotation.Nullable;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -66,18 +71,47 @@ public class StreamExecutionEnvironmentComplexConfigurationTest {
 	@Test
 	public void testLoadingCachedFilesFromConfiguration() {
 		StreamExecutionEnvironment envFromConfiguration = StreamExecutionEnvironment.getExecutionEnvironment();
-		envFromConfiguration.registerCachedFile("/tmp3", "file3", true);
+		envFromConfiguration.registerCachedFile("/tmp4", "file4", true);
 
 		Configuration configuration = new Configuration();
-		configuration.setString("pipeline.cached-files", "name:file1,path:/tmp1,executable:true;name:file2,path:/tmp2");
+		configuration.setString(
+			"pipeline.cached-files",
+			"name:file1,path:/tmp1,executable:true;"
+				+ "name:file2,path:/tmp2;"
+				+ "name:file3,path:'oss://bucket/file1'");
 
 		// mutate config according to configuration
 		envFromConfiguration.configure(configuration, Thread.currentThread().getContextClassLoader());
 
 		assertThat(envFromConfiguration.getCachedFiles(), equalTo(Arrays.asList(
 			Tuple2.of("file1", new DistributedCache.DistributedCacheEntry("/tmp1", true)),
-			Tuple2.of("file2", new DistributedCache.DistributedCacheEntry("/tmp2", false))
+			Tuple2.of("file2", new DistributedCache.DistributedCacheEntry("/tmp2", false)),
+			Tuple2.of(
+				"file3",
+				new DistributedCache.DistributedCacheEntry("oss://bucket/file1", false))
 		)));
+	}
+
+	@Test
+	public void testLoadingKryoSerializersFromConfiguration() {
+		StreamExecutionEnvironment envFromConfiguration = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		Configuration configuration = new Configuration();
+		configuration.setString(
+			"pipeline.default-kryo-serializers",
+			"class:'org.apache.flink.streaming.api.environment.StreamExecutionEnvironmentComplexConfigurationTest$CustomPojo'"
+				+ ",serializer:'org.apache.flink.streaming.api.environment.StreamExecutionEnvironmentComplexConfigurationTest$CustomPojoSerializer'");
+
+		// mutate config according to configuration
+		envFromConfiguration.configure(
+			configuration,
+			Thread.currentThread().getContextClassLoader());
+
+		LinkedHashMap<Object, Object> serializers = new LinkedHashMap<>();
+		serializers.put(CustomPojo.class, CustomPojoSerializer.class);
+		assertThat(
+			envFromConfiguration.getConfig().getDefaultKryoSerializerClasses(),
+			equalTo(serializers));
 	}
 
 	@Test
@@ -154,6 +188,32 @@ public class StreamExecutionEnvironmentComplexConfigurationTest {
 		@Override
 		public void onJobExecuted(@Nullable JobExecutionResult jobExecutionResult, @Nullable Throwable throwable) {
 
+		}
+	}
+
+	/**
+	 * A dummy class to specify a Kryo serializer for.
+	 */
+	public static class CustomPojo {
+	}
+
+	/**
+	 * A dummy Kryo serializer which can be registered.
+	 */
+	public static class CustomPojoSerializer extends Serializer<CustomPojo> {
+		@Override
+		public void write(
+				Kryo kryo,
+				Output output,
+				CustomPojo object) {
+		}
+
+		@Override
+		public CustomPojo read(
+				Kryo kryo,
+				Input input,
+				Class<CustomPojo> type) {
+			return null;
 		}
 	}
 }
