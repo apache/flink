faa8ec808d082248c852a8124e3c6a1d577e60b8
Yuan Mei <yuanmei.work@gmail.com>
2020-11-08 17:54:33 +0800

    [FLINK-19693] Introduce a flag to enable approximate failover

--- flink-runtime/src/test/java/org/apache/flink/runtime/scheduler/DefaultSchedulerComponentsFactoryTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/scheduler/DefaultSchedulerComponentsFactoryTest.java
@@ -26,14 +26,19 @@ import org.apache.flink.configuration.JobManagerOptions;
 import org.apache.flink.runtime.jobgraph.ScheduleMode;
 import org.apache.flink.runtime.jobmaster.slotpool.SlotPool;
 import org.apache.flink.runtime.jobmaster.slotpool.TestingSlotPoolImpl;
+import org.apache.flink.runtime.scheduler.strategy.EagerSchedulingStrategy;
 import org.apache.flink.runtime.scheduler.strategy.LazyFromSourcesSchedulingStrategy;
 import org.apache.flink.runtime.scheduler.strategy.PipelinedRegionSchedulingStrategy;
 import org.apache.flink.util.TestLogger;
 
 import org.junit.Test;
 
+import static org.apache.flink.runtime.jobgraph.ScheduleMode.EAGER;
+import static org.apache.flink.runtime.jobgraph.ScheduleMode.LAZY_FROM_SOURCES_WITH_BATCH_SLOT_REQUEST;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Tests for the factory method {@link DefaultSchedulerComponents#createSchedulerComponents(
@@ -67,9 +72,56 @@ public class DefaultSchedulerComponentsFactoryTest extends TestLogger {
 		assertThat(components.getSchedulingStrategyFactory(), instanceOf(PipelinedRegionSchedulingStrategy.Factory.class));
 	}
 
+	@Test
+	public void testCreatingPipelinedRegionSchedulingStrategyFactoryWithApproximateLocalRecovery() {
+		final Configuration configuration = new Configuration();
+		configuration.setString(JobManagerOptions.SCHEDULING_STRATEGY, "region");
+
+		try {
+			createSchedulerComponents(configuration, true, EAGER);
+			fail("expected failure");
+		} catch (IllegalArgumentException e) {
+			assertTrue(
+				e.getMessage()
+					.contains("Approximate local recovery can not be used together with PipelinedRegionScheduler for now"));
+		}
+	}
+
+	@Test
+	public void testCreatingLegacySchedulingStrategyFactoryWithApproximateLocalRecoveryInLazyMode() {
+		final Configuration configuration = new Configuration();
+		configuration.setString(JobManagerOptions.SCHEDULING_STRATEGY, "legacy");
+
+		try {
+			createSchedulerComponents(configuration, true, LAZY_FROM_SOURCES_WITH_BATCH_SLOT_REQUEST);
+			fail("expected failure");
+		} catch (IllegalArgumentException e) {
+			assertTrue(
+				e.getMessage()
+					.contains("Approximate local recovery can only be used together with EAGER schedule mode"));
+		}
+	}
+
+	@Test
+	public void testCreatingLegacySchedulingStrategyFactoryWithApproximateLocalRecoveryInEagerMode() {
+		final Configuration configuration = new Configuration();
+		configuration.setString(JobManagerOptions.SCHEDULING_STRATEGY, "legacy");
+
+		final DefaultSchedulerComponents components = createSchedulerComponents(configuration, true, EAGER);
+		assertThat(components.getSchedulingStrategyFactory(), instanceOf(EagerSchedulingStrategy.Factory.class));
+	}
+
 	private static DefaultSchedulerComponents createSchedulerComponents(final Configuration configuration) {
+		return createSchedulerComponents(configuration, false, LAZY_FROM_SOURCES_WITH_BATCH_SLOT_REQUEST);
+	}
+
+	private static DefaultSchedulerComponents createSchedulerComponents(
+			final Configuration configuration,
+			boolean iApproximateLocalRecoveryEnabled,
+			ScheduleMode scheduleMode) {
 		return DefaultSchedulerComponents.createSchedulerComponents(
-			ScheduleMode.LAZY_FROM_SOURCES_WITH_BATCH_SLOT_REQUEST,
+			scheduleMode,
+			iApproximateLocalRecoveryEnabled,
 			configuration,
 			new TestingSlotPoolImpl(new JobID()),
 			Time.milliseconds(10L));
