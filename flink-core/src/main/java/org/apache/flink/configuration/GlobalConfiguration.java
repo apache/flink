02a45f1753b5b7e1840ab27d7a9e3388c4983b21
Florian Szabo <flszabo@cisco.com>
2020-11-09 09:46:57 +0100

    FLINK-20055 - add datadog apikey config to array of sensitive keys

--- flink-core/src/main/java/org/apache/flink/configuration/GlobalConfiguration.java
+++ flink-core/src/main/java/org/apache/flink/configuration/GlobalConfiguration.java
@@ -44,7 +44,8 @@ public final class GlobalConfiguration {
 	public static final String FLINK_CONF_FILENAME = "flink-conf.yaml";
 
 	// the keys whose values should be hidden
-	private static final String[] SENSITIVE_KEYS = new String[] {"password", "secret", "fs.azure.account.key"};
+	private static final String[] SENSITIVE_KEYS = new String[] {"password", "secret", 
+		"fs.azure.account.key", "metrics.reporter.dghttp.apikey"};
 
 	// the hidden content to be displayed
 	public static final String HIDDEN_CONTENT = "******";
