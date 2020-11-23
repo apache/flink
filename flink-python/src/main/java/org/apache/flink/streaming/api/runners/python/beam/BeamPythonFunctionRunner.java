2b7ea7b516a26fac95ff87c28b3ee74b76521a04
Dian Fu <dianfu@apache.org>
2020-11-23 12:03:27 +0800

    [FLINK-20283][python] Provide a meaningful exception message when managed memory fraction of Python worker process is invalid
    This closes #14161.

--- flink-python/src/main/java/org/apache/flink/streaming/api/runners/python/beam/BeamPythonFunctionRunner.java
+++ flink-python/src/main/java/org/apache/flink/streaming/api/runners/python/beam/BeamPythonFunctionRunner.java
@@ -250,7 +250,10 @@ public abstract class BeamPythonFunctionRunner implements PythonFunctionRunner {
 		Struct pipelineOptions = PipelineOptionsTranslation.toProto(portableOptions);
 
 		if (memoryManager != null && config.isUsingManagedMemory()) {
-			Preconditions.checkArgument(managedMemoryFraction > 0 && managedMemoryFraction <= 1.0);
+			Preconditions.checkArgument(managedMemoryFraction > 0 && managedMemoryFraction <= 1.0,
+				"The configured managed memory fraction for Python worker process must be within (0, 1], was: %s. " +
+				"It may be because the consumer type \"Python\" was missing or set to 0 for the config option \"taskmanager.memory.managed.consumer-weights\"." +
+				managedMemoryFraction);
 
 			final LongFunctionWithException<PythonSharedResources, Exception> initializer = (size) ->
 				new PythonSharedResources(createJobBundleFactory(pipelineOptions), createPythonExecutionEnvironment(size));
