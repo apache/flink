6226047b9ce026f6431f9b8f7eed7ab49b373170
Jark Wu <jark@apache.org>
2020-11-24 23:01:13 +0800

    [FLINK-20325][build] Move docs_404_check to CI stage
    This closes #14204

--- docs/check_links.sh
+++ docs/check_links.sh
@@ -17,10 +17,20 @@
 # limitations under the License.
 ################################################################################
 
+DOCS_CHECK_DIR="`dirname \"$0\"`" # relative
+DOCS_CHECK_DIR="`( cd \"$DOCS_CHECK_DIR\" && pwd -P)`" # absolutized and normalized
+if [ -z "$DOCS_CHECK_DIR" ] ; then
+    # error; for some reason, the path is not accessible
+    # to the script (e.g. permissions re-evaled after suid)
+    exit 1  # fail
+fi
+
+echo "Check docs directory: $DOCS_CHECK_DIR"
+
 target=${1:-"http://localhost:4000"}
 
 # Crawl the docs, ignoring robots.txt, storing nothing locally
-wget --spider -r -nd -nv -e robots=off -p -o spider.log "$target"
+wget --spider -r -nd -nv -e robots=off -p -o $DOCS_CHECK_DIR/spider.log "$target"
 
 # Abort for anything other than 0 and 4 ("Network failure")
 status=$?
@@ -29,11 +39,24 @@ if [ $status -ne 0 ] && [ $status -ne 4 ]; then
 fi
 
 # Fail the build if any broken links are found
-broken_links_str=$(grep -e 'Found [[:digit:]]\+ broken links' spider.log)
+broken_links_str=$(grep -e 'broken link' $DOCS_CHECK_DIR/spider.log)
 if [ -n "$broken_links_str" ]; then
-    grep -B 1 "Remote file does not exist -- broken link!!!" spider.log
+    grep -B 1 "Remote file does not exist -- broken link!!!" $DOCS_CHECK_DIR/spider.log
     echo "---------------------------------------------------------------------------"
     echo -e "$broken_links_str"
     echo "Search for page containing broken link using 'grep -R BROKEN_PATH DOCS_DIR'"
     exit 1
 fi
+
+# Fail the build if any broken links are found for Chinese
+broken_links_str=$(grep -e '死链接' $DOCS_CHECK_DIR/spider.log)
+if [ -n "$broken_links_str" ]; then
+    grep -B 1 "远程文件不存在 -- 链接失效！！！" $DOCS_CHECK_DIR/spider.log
+    echo "---------------------------------------------------------------------------"
+    echo -e "$broken_links_str"
+    echo "Search for page containing broken link using 'grep -R BROKEN_PATH DOCS_DIR'"
+    exit 1
+fi
+
+echo 'All links in docs are valid!'
+exit 0
