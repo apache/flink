07a449e2f68384007cad61c10d0dcbbe0cf89c26
Jingsong Lee <jingsonglee0@gmail.com>
2020-11-25 14:37:10 +0800

    [FLINK-20295][table][fs-connector] Table File Source lost data when reading from directories with JSON format
    This closes #14192

--- flink-formats/flink-json/src/test/java/org/apache/flink/formats/json/JsonBatchFileSystemITCase.java
+++ flink-formats/flink-json/src/test/java/org/apache/flink/formats/json/JsonBatchFileSystemITCase.java
@@ -18,13 +18,17 @@
 
 package org.apache.flink.formats.json;
 
+import org.apache.flink.table.api.TableResult;
 import org.apache.flink.table.planner.runtime.batch.sql.BatchFileSystemITCaseBase;
 import org.apache.flink.types.Row;
 import org.apache.flink.util.FileUtils;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -58,5 +62,53 @@ public class JsonBatchFileSystemITCase extends BatchFileSystemITCaseBase {
 			Arrays.asList(
 				Row.of("x5,5,1,1"),
 				Row.of("x5,5,1,1")));
+	}
+
+	@Test
+	public void bigDataTest() throws IOException {
+		int numRecords = 1000;
+		File dir = generateTestData(numRecords);
+
+		env().setParallelism(1);
+
+		String sql = String.format(
+				"CREATE TABLE bigdata_source ( " +
+						"	id INT, " +
+						"	content STRING" +
+						") PARTITIONED by (id) WITH (" +
+						"	'connector' = 'filesystem'," +
+						"	'path' = '%s'," +
+						"	'format' = 'json'" +
+						")", dir);
+		tEnv().executeSql(sql);
+		TableResult result = tEnv().executeSql("select * from bigdata_source");
+		List<String> elements = new ArrayList<>();
+		result.collect().forEachRemaining(r -> elements.add((String) r.getField(1)));
+		Assert.assertEquals(numRecords, elements.size());
+		elements.sort(String::compareTo);
+
+		List<String> expected = new ArrayList<>();
+		for (int i = 0; i < numRecords; i++) {
+			expected.add(String.valueOf(i));
+		}
+		expected.sort(String::compareTo);
+
+		Assert.assertEquals(expected, elements);
+	}
+
+	private static File generateTestData(int numRecords) throws IOException {
+		File tempDir = TEMPORARY_FOLDER.newFolder();
+
+		File root = new File(tempDir, "id=0");
+		root.mkdir();
+
+		File dataFile = new File(root, "testdata");
+		try (PrintWriter writer = new PrintWriter(dataFile)) {
+			for (int i = 0; i < numRecords; ++i) {
+				writer.println(String.format("{\"content\":\"%s\"}", i));
+			}
+		}
+
+		return tempDir;
 	}
 }
