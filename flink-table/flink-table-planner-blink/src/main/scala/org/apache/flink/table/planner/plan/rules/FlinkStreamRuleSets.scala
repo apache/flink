4cc1da96e88ddc6166181a7f83c891761db283a1
Shengkai <33114724+fsk119@users.noreply.github.com>
2020-11-26 09:56:44 +0800

    [FLINK-20314][table-planner] Add rule to remove empty FlinkLogicalCalc

--- flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/plan/rules/FlinkStreamRuleSets.scala
+++ flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/plan/rules/FlinkStreamRuleSets.scala
@@ -373,6 +373,10 @@ object FlinkStreamRuleSets {
     PythonCorrelateSplitRule.INSTANCE,
     // merge calc after calc transpose
     FlinkCalcMergeRule.INSTANCE,
+    // remove the trivial calc that is produced by PushWatermarkIntoTableSourceScanAcrossCalcRule.
+    // because [[PushWatermarkIntoTableSourceScanAcrossCalcRule]] will push the rowtime computed
+    // column into the source. After FlinkCalcMergeRule applies, it may produces a trivial calc.
+    FlinkLogicalCalcRemoveRule.INSTANCE,
     //Rule that rewrites temporal join with extracted primary key
     TemporalJoinRewriteWithUniqueKeyRule.INSTANCE,
     // Rule that splits python ScalarFunctions from java/scala ScalarFunctions.
