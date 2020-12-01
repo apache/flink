b216df852c5af44b4ea379fdaf32a3a076568e92
Zhu Zhu <reedpor@gmail.com>
2020-12-01 18:07:42 +0800

    [FLINK-17760][runtime, tests] Remove ExecutionGraphMetricsTest and add RestartTimeGaugeTest
    ExecutionGraphMetricsTest can be superseded by the newly added RestartTimeGaugeTest and DefaultSchedulerTest#jobStatusIsRestartingIfOneVertexIsWaitingForRestart().

--- flink-runtime/src/main/java/org/apache/flink/runtime/executiongraph/metrics/UpTimeGauge.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/executiongraph/metrics/UpTimeGauge.java
@@ -20,14 +20,14 @@ package org.apache.flink.runtime.executiongraph.metrics;
 
 import org.apache.flink.api.common.JobStatus;
 import org.apache.flink.metrics.Gauge;
-import org.apache.flink.runtime.executiongraph.ExecutionGraph;
+import org.apache.flink.runtime.executiongraph.JobStatusProvider;
 
 import static org.apache.flink.util.Preconditions.checkNotNull;
 
 /**
  * A gauge that returns (in milliseconds) how long a job has been running.
- * 
- * <p>For jobs that are not running any more, it returns {@value NO_LONGER_RUNNING}. 
+ *
+ * <p>For jobs that are not running any more, it returns {@value NO_LONGER_RUNNING}.
  */
 public class UpTimeGauge implements Gauge<Long> {
 
@@ -37,21 +37,21 @@ public class UpTimeGauge implements Gauge<Long> {
 
 	// ------------------------------------------------------------------------
 
-	private final ExecutionGraph eg;
+	private final JobStatusProvider jobStatusProvider;
 
-	public UpTimeGauge(ExecutionGraph executionGraph) {
-		this.eg = checkNotNull(executionGraph);
+	public UpTimeGauge(JobStatusProvider jobStatusProvider) {
+		this.jobStatusProvider = checkNotNull(jobStatusProvider);
 	}
 
 	// ------------------------------------------------------------------------
 
 	@Override
 	public Long getValue() {
-		final JobStatus status = eg.getState();
+		final JobStatus status = jobStatusProvider.getState();
 
 		if (status == JobStatus.RUNNING) {
 			// running right now - report the uptime
-			final long runningTimestamp = eg.getStatusTimestamp(JobStatus.RUNNING);
+			final long runningTimestamp = jobStatusProvider.getStatusTimestamp(JobStatus.RUNNING);
 			// we use 'Math.max' here to avoid negative timestamps when clocks change
 			return Math.max(System.currentTimeMillis() - runningTimestamp, 0);
 		}
