59ae84069313ede60cf7ad3a9d2fe1bc07c4e460
Chesnay Schepler <chesnay@apache.org>
2020-12-07 03:03:43 +0100

    [FLINK-20455][build][legal] Add jar license checker

--- tools/ci/java-ci-tools/src/main/java/org/apache/flink/tools/ci/licensecheck/LicenseChecker.java
+++ tools/ci/java-ci-tools/src/main/java/org/apache/flink/tools/ci/licensecheck/LicenseChecker.java
@@ -32,15 +32,18 @@ public class LicenseChecker {
 
 	private static final Logger LOG = LoggerFactory.getLogger(LicenseChecker.class);
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		if (args.length < 2) {
-			System.out.println("Usage: LicenseChecker <pathMavenBuildOutput> <pathFlinkRoot>");
+			System.out.println("Usage: LicenseChecker <pathMavenBuildOutput> <pathFlinkRoot> <pathFlinkDeployed>");
 			System.exit(1);
 		}
 		LOG.warn("THIS UTILITY IS ONLY CHECKING FOR COMMON LICENSING MISTAKES. A MANUAL CHECK OF THE NOTICE FILES, DEPLOYED ARTIFACTS, ETC. IS STILL NEEDED!");
 
 		NoticeFileChecker noticeChecker = new NoticeFileChecker();
 		int severeIssueCount = noticeChecker.run(new File(args[0]), Paths.get(args[1]));
+
+		JarFileChecker jarChecker = new JarFileChecker();
+		severeIssueCount += jarChecker.checkPath(Paths.get(args[2]));
 
 		if (severeIssueCount > 0) {
 			LOG.warn("Found a total of {} severe license issues", severeIssueCount);
