9048c9e3ff296366fc991fca2ce4911f0e7ca5c9
Aljoscha Krettek <aljoscha@apache.org>
2020-12-15 16:48:36 +0100

    [hotfix] Fix incorrect license headers

--- flink-connectors/flink-connector-filesystem/src/main/java/org/apache/flink/streaming/connectors/fs/AvroKeyValueSinkWriter.java
+++ flink-connectors/flink-connector-filesystem/src/main/java/org/apache/flink/streaming/connectors/fs/AvroKeyValueSinkWriter.java
@@ -1,6 +1,4 @@
-package org.apache.flink.streaming.connectors.fs;
-
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -17,6 +15,8 @@ package org.apache.flink.streaming.connectors.fs;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.flink.streaming.connectors.fs;
 
 import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
