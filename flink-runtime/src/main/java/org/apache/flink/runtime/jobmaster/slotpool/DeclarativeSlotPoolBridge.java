38a51bb1221780975b5afabb11fa4e457d74965d
Chesnay Schepler <chesnay@apache.org>
2020-12-22 13:13:29 +0100

    [hotfix][coordination] Centralize skipping declaration of unchanged requirements
    There are multiple events where resource requirements might not actually change, and so far the user of the slot pool was responsible for checking that the requirements have actually changed to prevent unnecessary RPC calls.
    Since this is something that every user should check, we might as well always do this automatically.

--- flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/slotpool/DeclarativeSlotPoolBridge.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/jobmaster/slotpool/DeclarativeSlotPoolBridge.java
@@ -167,9 +167,7 @@ public class DeclarativeSlotPoolBridge implements SlotPool {
 			}
 		}
 
-		if (!decreasedResourceRequirements.isEmpty()) {
-			declarativeSlotPool.decreaseResourceRequirementsBy(decreasedResourceRequirements);
-		}
+		declarativeSlotPool.decreaseResourceRequirementsBy(decreasedResourceRequirements);
 	}
 
 	private void clearState() {
@@ -375,9 +373,7 @@ public class DeclarativeSlotPoolBridge implements SlotPool {
 		Preconditions.checkNotNull(resourceId, "This slot pool only supports failAllocation calls coming from the TaskExecutor.");
 
 		ResourceCounter previouslyFulfilledRequirements = declarativeSlotPool.releaseSlot(allocationID, cause);
-		if (!previouslyFulfilledRequirements.isEmpty()) {
-			declarativeSlotPool.decreaseResourceRequirementsBy(previouslyFulfilledRequirements);
-		}
+		declarativeSlotPool.decreaseResourceRequirementsBy(previouslyFulfilledRequirements);
 
 		if (declarativeSlotPool.containsSlots(resourceId)) {
 			return Optional.empty();
@@ -403,9 +399,7 @@ public class DeclarativeSlotPoolBridge implements SlotPool {
 
 			if (allocationId != null) {
 				ResourceCounter previouslyFulfilledRequirement = declarativeSlotPool.freeReservedSlot(allocationId, cause, clock.relativeTimeMillis());
-				if (!previouslyFulfilledRequirement.isEmpty()) {
-					declarativeSlotPool.decreaseResourceRequirementsBy(previouslyFulfilledRequirement);
-				}
+				declarativeSlotPool.decreaseResourceRequirementsBy(previouslyFulfilledRequirement);
 			} else {
 				LOG.debug("Could not find slot which has fulfilled slot request {}. Ignoring the release operation.", slotRequestId);
 			}
