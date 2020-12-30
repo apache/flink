3f3cf26314ca6b62f81a673ce04149862ede9bd9
Chesnay Schepler <chesnay@apache.org>
2020-12-30 16:38:10 +0100

    [FLINK-20805][table][build] Apply spotless formatting

--- flink-table/flink-table-runtime-blink/src/main/java/org/apache/flink/table/runtime/generated/GeneratedNormalizedKeyComputer.java
+++ flink-table/flink-table-runtime-blink/src/main/java/org/apache/flink/table/runtime/generated/GeneratedNormalizedKeyComputer.java
@@ -18,20 +18,18 @@
 
 package org.apache.flink.table.runtime.generated;
 
-/**
- * Describes a generated {@link NormalizedKeyComputer}.
- */
+/** Describes a generated {@link NormalizedKeyComputer}. */
 public class GeneratedNormalizedKeyComputer extends GeneratedClass<NormalizedKeyComputer> {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * Creates a GeneratedNormalizedKeyComputer.
-	 *
-	 * @param className class name of the generated class.
-	 * @param code code of the generated class.
-	 */
-	public GeneratedNormalizedKeyComputer(String className, String code) {
-		super(className, code, new Object[0]);
-	}
+    /**
+     * Creates a GeneratedNormalizedKeyComputer.
+     *
+     * @param className class name of the generated class.
+     * @param code code of the generated class.
+     */
+    public GeneratedNormalizedKeyComputer(String className, String code) {
+        super(className, code, new Object[0]);
+    }
 }
