bc053b013a94926f25c91a6f09dbaaa3fad0003b
Chesnay Schepler <chesnay@apache.org>
2020-12-30 16:50:24 +0100

    [FLINK-20805][table][build] Apply spotless formatting

--- flink-table/flink-table-runtime-blink/src/main/java/org/apache/flink/table/runtime/generated/GeneratedRecordComparator.java
+++ flink-table/flink-table-runtime-blink/src/main/java/org/apache/flink/table/runtime/generated/GeneratedRecordComparator.java
@@ -18,22 +18,19 @@
 
 package org.apache.flink.table.runtime.generated;
 
-/**
- * Describes a generated {@link RecordComparator}.
- */
+/** Describes a generated {@link RecordComparator}. */
 public class GeneratedRecordComparator extends GeneratedClass<RecordComparator> {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * Creates a GeneratedRecordComparator.
-	 *
-	 * @param className class name of the generated class.
-	 * @param code code of the generated class.
-	 * @param references referenced objects of the generated class.
-	 */
-	public GeneratedRecordComparator(String className, String code, Object[] references) {
-		super(className, code, references);
-	}
-
+    /**
+     * Creates a GeneratedRecordComparator.
+     *
+     * @param className class name of the generated class.
+     * @param code code of the generated class.
+     * @param references referenced objects of the generated class.
+     */
+    public GeneratedRecordComparator(String className, String code, Object[] references) {
+        super(className, code, references);
+    }
 }
