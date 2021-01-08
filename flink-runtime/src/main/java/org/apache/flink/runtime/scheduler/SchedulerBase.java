0464532030736727fa5cd0910b0d4b6b004e4904
Till Rohrmann <trohrmann@apache.org>
2021-01-08 16:48:58 +0100

    [FLINK-20903] Remove SchedulerNG.registerJobStatusListener
    Since we can pass in the JobStatusListener into the SchedulerNG implementations,
    we no longer need the SchedulerNG.registerJobStatusListener and can remove it.
    
    This closes #14593.

--- flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/SchedulerBase.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/SchedulerBase.java
@@ -605,11 +605,6 @@ public abstract class SchedulerBase implements SchedulerNG {
     // ------------------------------------------------------------------------
 
     @Override
-    public void registerJobStatusListener(final JobStatusListener jobStatusListener) {
-        executionGraph.registerJobStatusListener(jobStatusListener);
-    }
-
-    @Override
     public final void startScheduling() {
         mainThreadExecutor.assertRunningInMainThread();
         registerJobMetrics();
