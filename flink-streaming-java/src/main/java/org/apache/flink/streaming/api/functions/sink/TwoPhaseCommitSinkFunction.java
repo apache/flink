34039396aa82232d546699f3bdb711e73e40c1e0
benjobs <benjobs@qq.com>
2021-01-14 23:11:05 +0800

    FLINK-20972 bug fix

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/functions/sink/TwoPhaseCommitSinkFunction.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/functions/sink/TwoPhaseCommitSinkFunction.java
@@ -636,7 +636,7 @@ public abstract class TwoPhaseCommitSinkFunction<IN, TXN, CONTEXT> extends RichS
         public String toString() {
             return "TransactionHolder{"
                     + "handle="
-                    + (handle == null ? null : handle.getClass().getName() + "@" + handle.hashCode())
+                    + (handle == null ? null : getClass().getName() + "@" + Integer.toHexString(hashCode()))
                     + ", transactionStartTime="
                     + transactionStartTime
                     + '}';
