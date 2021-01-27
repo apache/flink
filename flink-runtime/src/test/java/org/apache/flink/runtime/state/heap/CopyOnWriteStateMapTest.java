4ecfee10472fd12f67a0fdb9feab4e45ebfe55b6
Dawid Wysakowicz <dwysakowicz@apache.org>
2021-01-27 21:04:38 +0100

    [FLINK-21167] Make StateTable snapshots iterable
    In order to implement an iterator required by a binary unified savepoint we need a way to iterate a snapshot.

--- flink-runtime/src/test/java/org/apache/flink/runtime/state/heap/CopyOnWriteStateMapTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/state/heap/CopyOnWriteStateMapTest.java
@@ -19,10 +19,13 @@
 package org.apache.flink.runtime.state.heap;
 
 import org.apache.flink.api.common.typeutils.base.IntSerializer;
+import org.apache.flink.api.common.typeutils.base.ListSerializer;
+import org.apache.flink.api.common.typeutils.base.LongSerializer;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.runtime.state.ArrayListSerializer;
 import org.apache.flink.runtime.state.StateEntry;
+import org.apache.flink.runtime.state.StateSnapshotTransformer;
 import org.apache.flink.runtime.state.StateTransformationFunction;
 import org.apache.flink.runtime.state.internal.InternalKvState.StateIncrementalVisitor;
 import org.apache.flink.util.TestLogger;
@@ -31,12 +34,21 @@ import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.annotation.Nullable;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
+
+import static org.apache.flink.runtime.state.testutils.StateEntryMatcher.entry;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
 
 /** Test for {@link CopyOnWriteStateMap}. */
 public class CopyOnWriteStateMapTest extends TestLogger {
@@ -398,6 +410,82 @@ public class CopyOnWriteStateMapTest extends TestLogger {
         Assert.assertSame(originalState5, stateMap.get(5, 1));
     }
 
+    @Test
+    public void testIteratingOverSnapshot() {
+        ListSerializer<Integer> stateSerializer = new ListSerializer<>(IntSerializer.INSTANCE);
+        final CopyOnWriteStateMap<Integer, Integer, List<Integer>> stateMap =
+                new CopyOnWriteStateMap<>(stateSerializer);
+
+        List<Integer> originalState1 = new ArrayList<>(1);
+        List<Integer> originalState2 = new ArrayList<>(1);
+        List<Integer> originalState3 = new ArrayList<>(1);
+        List<Integer> originalState4 = new ArrayList<>(1);
+        List<Integer> originalState5 = new ArrayList<>(1);
+
+        originalState1.add(1);
+        originalState2.add(2);
+        originalState3.add(3);
+        originalState4.add(4);
+        originalState5.add(5);
+
+        stateMap.put(1, 1, originalState1);
+        stateMap.put(2, 1, originalState2);
+        stateMap.put(3, 1, originalState3);
+        stateMap.put(4, 1, originalState4);
+        stateMap.put(5, 1, originalState5);
+
+        CopyOnWriteStateMapSnapshot<Integer, Integer, List<Integer>> snapshot =
+                stateMap.stateSnapshot();
+
+        Iterator<StateEntry<Integer, Integer, List<Integer>>> iterator =
+                snapshot.getIterator(
+                        IntSerializer.INSTANCE, IntSerializer.INSTANCE, stateSerializer, null);
+        assertThat(
+                () -> iterator,
+                containsInAnyOrder(
+                        entry(1, 1, originalState1),
+                        entry(2, 1, originalState2),
+                        entry(3, 1, originalState3),
+                        entry(4, 1, originalState4),
+                        entry(5, 1, originalState5)));
+    }
+
+    @Test
+    public void testIteratingOverSnapshotWithTransform() {
+        final CopyOnWriteStateMap<Integer, Integer, Long> stateMap =
+                new CopyOnWriteStateMap<>(LongSerializer.INSTANCE);
+
+        stateMap.put(1, 1, 10L);
+        stateMap.put(2, 1, 11L);
+        stateMap.put(3, 1, 12L);
+        stateMap.put(4, 1, 13L);
+        stateMap.put(5, 1, 14L);
+
+        StateMapSnapshot<Integer, Integer, Long, ? extends StateMap<Integer, Integer, Long>>
+                snapshot = stateMap.stateSnapshot();
+
+        Iterator<StateEntry<Integer, Integer, Long>> iterator =
+                snapshot.getIterator(
+                        IntSerializer.INSTANCE,
+                        IntSerializer.INSTANCE,
+                        LongSerializer.INSTANCE,
+                        new StateSnapshotTransformer<Long>() {
+                            @Nullable
+                            @Override
+                            public Long filterOrTransform(@Nullable Long value) {
+                                if (value == 12L) {
+                                    return null;
+                                } else {
+                                    return value + 2L;
+                                }
+                            }
+                        });
+        assertThat(
+                () -> iterator,
+                containsInAnyOrder(
+                        entry(1, 1, 12L), entry(2, 1, 13L), entry(4, 1, 15L), entry(5, 1, 16L)));
+    }
+
     /** This tests that snapshot can be released correctly. */
     @Test
     public void testSnapshotRelease() {
@@ -410,16 +498,15 @@ public class CopyOnWriteStateMapTest extends TestLogger {
 
         CopyOnWriteStateMapSnapshot<Integer, Integer, Integer> snapshot = stateMap.stateSnapshot();
         Assert.assertFalse(snapshot.isReleased());
-        Assert.assertThat(
-                stateMap.getSnapshotVersions(), Matchers.contains(snapshot.getSnapshotVersion()));
+        assertThat(stateMap.getSnapshotVersions(), contains(snapshot.getSnapshotVersion()));
 
         snapshot.release();
         Assert.assertTrue(snapshot.isReleased());
-        Assert.assertThat(stateMap.getSnapshotVersions(), Matchers.empty());
+        assertThat(stateMap.getSnapshotVersions(), Matchers.empty());
 
         // verify that snapshot will release itself only once
         snapshot.release();
-        Assert.assertThat(stateMap.getSnapshotVersions(), Matchers.empty());
+        assertThat(stateMap.getSnapshotVersions(), Matchers.empty());
     }
 
     @SuppressWarnings("unchecked")
