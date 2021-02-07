817410e505f18f00622b0f190f2d674a068fb47a
V1ncentzzZ <45353183+V1ncentzzZ@users.noreply.github.com>
2021-02-07 11:36:09 +0800

    [hotfix][jdbc] Fix typo in UnsignedTypeConversionITCase
    This closes #14876 
    
    Co-authored-by: xiaozilong <xiaozilong@bigo.sg>

--- flink-connectors/flink-connector-jdbc/src/test/java/org/apache/flink/connector/jdbc/table/UnsignedTypeConversionITCase.java
+++ flink-connectors/flink-connector-jdbc/src/test/java/org/apache/flink/connector/jdbc/table/UnsignedTypeConversionITCase.java
@@ -95,7 +95,7 @@ public class UnsignedTypeConversionITCase extends AbstractTestBase {
                     }
                 }
             } catch (Exception e) {
-                logger.warn("Initialize DB fail caused by {}", e);
+                logger.warn("Initialize DB failed.", e);
                 stopDb();
             }
             if (initDbSuccess) {
@@ -106,7 +106,7 @@ public class UnsignedTypeConversionITCase extends AbstractTestBase {
         if (!initDbSuccess) {
             throw new IllegalStateException(
                     String.format(
-                            "Initialize MySQL database instance failed after %s attempts,"
+                            "Initialize MySQL database instance failed after %d attempts,"
                                     + " please open an issue.",
                             INITIALIZE_DB_MAX_RETRY));
         }
@@ -249,7 +249,7 @@ public class UnsignedTypeConversionITCase extends AbstractTestBase {
         try {
             db.stop();
         } catch (ManagedProcessException e1) {
-            logger.warn("Stop DB instance fail caused by {}", e1);
+            logger.warn("Stop DB instance failed.", e1);
         }
     }
 }
