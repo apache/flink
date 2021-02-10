tillrohrmann/incubator-flink

[hotfix] Do not pass SchedulerNG to OperatorCoordinatorHolder

flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/SchedulerBase.java
@@ -1070,7 +1070,7 @@ public abstract class SchedulerBase implements SchedulerNG {
 
     private void initializeOperatorCoordinators(ComponentMainThreadExecutor mainThreadExecutor) {
         for (OperatorCoordinatorHolder coordinatorHolder : getAllCoordinators()) {
-            coordinatorHolder.lazyInitialize(this, mainThreadExecutor);
+            coordinatorHolder.lazyInitialize(this::handleGlobalFailure, mainThreadExecutor);
         }
     }
 

apache/flink

flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/SchedulerBase.java
@@ -1050,7 +1050,7 @@ public abstract class SchedulerBase implements SchedulerNG {
         Map<OperatorID, OperatorCoordinatorHolder> coordinatorMap = new HashMap<>();
         for (ExecutionJobVertex vertex : executionGraph.getAllVertices().values()) {
             for (OperatorCoordinatorHolder holder : vertex.getOperatorCoordinators()) {
-                holder.lazyInitialize(this, mainThreadExecutor);
+                holder.lazyInitialize(this::handleGlobalFailure, mainThreadExecutor);
                 coordinatorMap.put(holder.operatorId(), holder);
             }
         }