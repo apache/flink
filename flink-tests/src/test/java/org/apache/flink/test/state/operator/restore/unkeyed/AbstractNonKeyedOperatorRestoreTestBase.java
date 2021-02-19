797dc4f6d98193dac5faa752a072efa3f53a7076
Dian Fu <dianfu@apache.org>
2021-02-19 15:06:48 +0800

    [FLINK-20536][tests] Update migration tests of AbstractNonKeyedOperatorRestoreTestBase to cover migration from 1.12

--- flink-tests/src/test/java/org/apache/flink/test/state/operator/restore/unkeyed/AbstractNonKeyedOperatorRestoreTestBase.java
+++ flink-tests/src/test/java/org/apache/flink/test/state/operator/restore/unkeyed/AbstractNonKeyedOperatorRestoreTestBase.java
@@ -55,7 +55,8 @@ public abstract class AbstractNonKeyedOperatorRestoreTestBase
                 MigrationVersion.v1_8,
                 MigrationVersion.v1_9,
                 MigrationVersion.v1_10,
-                MigrationVersion.v1_11);
+                MigrationVersion.v1_11,
+                MigrationVersion.v1_12);
     }
 
     protected AbstractNonKeyedOperatorRestoreTestBase(MigrationVersion migrationVersion) {
