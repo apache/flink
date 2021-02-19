3d0e4d28cfb79fa74c478c32b249e60b37fd718a
Dian Fu <dianfu@apache.org>
2021-02-19 17:11:53 +0800

    [FLINK-20536][tests] Update migration tests of WindowOperatorMigrationTest to cover migration from 1.12

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/runtime/operators/windowing/WindowOperatorMigrationTest.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/runtime/operators/windowing/WindowOperatorMigrationTest.java
@@ -94,7 +94,8 @@ public class WindowOperatorMigrationTest {
                 MigrationVersion.v1_8,
                 MigrationVersion.v1_9,
                 MigrationVersion.v1_10,
-                MigrationVersion.v1_11);
+                MigrationVersion.v1_11,
+                MigrationVersion.v1_12);
     }
 
     private static final TypeInformation<Tuple2<String, Integer>> STRING_INT_TUPLE =
