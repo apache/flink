ea85136ac82beb30ed7dc62b9f2537c4fecb7185
Chesnay Schepler <chesnay@apache.org>
2021-02-26 09:01:15 +0100

    [hotfix][metrics][tests] Use default scope formats

--- flink-runtime/src/test/java/org/apache/flink/runtime/metrics/util/TestingMetricRegistry.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/metrics/util/TestingMetricRegistry.java
@@ -18,6 +18,7 @@
 
 package org.apache.flink.runtime.metrics.util;
 
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.metrics.Metric;
 import org.apache.flink.runtime.metrics.MetricRegistry;
 import org.apache.flink.runtime.metrics.groups.AbstractMetricGroup;
@@ -84,7 +85,7 @@ public class TestingMetricRegistry implements MetricRegistry {
                 (ignoreMetric, ignoreMetricName, ignoreGroup) -> {};
         private TriConsumer<Metric, String, AbstractMetricGroup> unregisterConsumer =
                 (ignoreMetric, ignoreMetricName, ignoreGroup) -> {};
-        private ScopeFormats scopeFormats = null;
+        private ScopeFormats scopeFormats = ScopeFormats.fromConfig(new Configuration());
 
         private TestingMetricRegistryBuilder() {}
 
