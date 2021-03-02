94a028e61837fb2195c6d9b1448486aa7f81d6b3
Maciej Obuchowski <maciej.obuchowski@getindata.com>
2021-03-02 23:03:15 +0100

    [FLINK-21488][connectors/jdbc] Use JobID in XA global transaction ID

--- flink-connectors/flink-connector-jdbc/src/main/java/org/apache/flink/connector/jdbc/xa/SemanticXidGenerator.java
+++ flink-connectors/flink-connector-jdbc/src/main/java/org/apache/flink/connector/jdbc/xa/SemanticXidGenerator.java
@@ -18,24 +18,28 @@
 package org.apache.flink.connector.jdbc.xa;
 
 import org.apache.flink.annotation.Internal;
+import org.apache.flink.api.common.JobID;
 import org.apache.flink.api.common.functions.RuntimeContext;
 
 import javax.transaction.xa.Xid;
 
 import java.security.SecureRandom;
+import java.util.Optional;
 
 /**
  * Generates {@link Xid} from:
  *
  * <ol>
+ *   <li>To provide uniqueness over other jobs and apps, and other instances
+ *   <li>of this job, gtrid consists of
+ *   <li>job id (16 bytes)
+ *   <li>subtask index (4 bytes)
  *   <li>checkpoint id (4 bytes)
- *   <li>subtask index
- *   <li>8 random bytes to provide uniqueness across other jobs and apps (generated at startup using
- *       {@link SecureRandom})
+ *   <li>bqual consists of 4 random bytes (generated using {@link SecureRandom})
  * </ol>
  *
- * <p>Each {@link SemanticXidGenerator} instance MUST be used for only one Sink (otherwise Xids
- * could collide).
+ * <p>Each {@link SemanticXidGenerator} instance MUST be used for only one Sink (otherwise Xids will
+ * collide).
  */
 @Internal
 class SemanticXidGenerator implements XidGenerator {
@@ -46,20 +50,29 @@ class SemanticXidGenerator implements XidGenerator {
 
     private static final int FORMAT_ID = 201;
 
-    private transient byte[] gtridBuffer; // globalTransactionId = checkpoint id (long)
-    private transient byte[] bqualBuffer; // branchQualifier = task index + random bytes
+    private transient byte[]
+            gtridBuffer; // globalTransactionId = job id + task index + checkpoint id
+    private transient byte[] bqualBuffer; // branchQualifier = random bytes
 
     @Override
     public void open() {
-        bqualBuffer = getRandomBytes(Long.BYTES);
-        gtridBuffer = new byte[Long.BYTES];
+        gtridBuffer = new byte[3 * Long.BYTES];
+        bqualBuffer = getRandomBytes(Integer.BYTES);
     }
 
     @Override
     public Xid generateXid(RuntimeContext runtimeContext, long checkpointId) {
-        writeNumber(runtimeContext.getIndexOfThisSubtask(), gtridBuffer, 0);
-        // deliberately write only 4 bytes of checkpoint id and rely on random generation
-        writeNumber((int) checkpointId, gtridBuffer, Integer.BYTES);
+        Optional<JobID> jobId = runtimeContext.getJobId();
+        if (jobId.isPresent()) {
+            System.arraycopy(jobId.get().getBytes(), 0, gtridBuffer, 0, JobID.SIZE);
+        } else {
+            // fall back to RNG if jobId is unavailable for some reason
+            System.arraycopy(getRandomBytes(JobID.SIZE), 0, gtridBuffer, 0, JobID.SIZE);
+        }
+
+        writeNumber(runtimeContext.getIndexOfThisSubtask(), gtridBuffer, JobID.SIZE);
+        // deliberately write only 4 bytes of checkpoint id
+        writeNumber((int) checkpointId, gtridBuffer, JobID.SIZE + Integer.BYTES);
         // relying on arrays copying inside XidImpl constructor
         return new XidImpl(FORMAT_ID, gtridBuffer, bqualBuffer);
     }
