austince/flink-ci-flink

[FLINK-19801][task] Implement virtual channel demultiplexing in RecoveringStreamTaskNetworkInput.
The demultiplexing works in two dimensions for the following cases.
* Subtasks of the current operator have been collapsed in a round-robin fashion.
* The connected output operator has been rescaled (up and down!) and there is an overlap of channels (mostly relevant to keyed exchanges).
In both cases, records from multiple old channels are received over one new physical channel, which need to demultiplex the record to correctly restore spanning records (similar to how StreamTaskNetworkInput works).

For performance reasons, the virtual demultiplexing logic is implemented separately from StreamTaskNetworkInput, such that on after recovery, the network input is replaced with the non-recovery counter-part in all StreamInputProcessors.

flink-runtime/src/main/java/org/apache/flink/runtime/io/network/api/serialization/EventSerializer.java
@@ -30,6 +30,7 @@ import org.apache.flink.runtime.io.network.api.CheckpointBarrier;
 import org.apache.flink.runtime.io.network.api.EndOfPartitionEvent;
 import org.apache.flink.runtime.io.network.api.EndOfSuperstepEvent;
 import org.apache.flink.runtime.io.network.api.EventAnnouncement;
+import org.apache.flink.runtime.io.network.api.VirtualChannelSelector;
 import org.apache.flink.runtime.io.network.buffer.Buffer;
 import org.apache.flink.runtime.io.network.buffer.BufferConsumer;
 import org.apache.flink.runtime.io.network.buffer.FreeingBufferRecycler;
@@ -64,6 +65,8 @@ public class EventSerializer {
     private static final int END_OF_CHANNEL_STATE_EVENT = 5;
 
     private static final int ANNOUNCEMENT_EVENT = 6;
+
+    private static final int VIRTUAL_CHANNEL_SELECTOR_EVENT = 7;
 
     private static final int CHECKPOINT_TYPE_CHECKPOINT = 0;
 
@@ -105,6 +108,14 @@ public class EventSerializer {
             serializedAnnouncement.put(serializedAnnouncedEvent);
             serializedAnnouncement.flip();
             return serializedAnnouncement;
+        } else if (eventClass == VirtualChannelSelector.class) {
+            VirtualChannelSelector selector = (VirtualChannelSelector) event;
+            ByteBuffer buf = ByteBuffer.allocate(12);
+            buf.putInt(VIRTUAL_CHANNEL_SELECTOR_EVENT);
+            buf.putInt(selector.getInputSubtaskIndex());
+            buf.putInt(selector.getOutputSubtaskIndex());
+            buf.flip();
+            return buf;
         } else {
             try {
                 final DataOutputSerializer serializer = new DataOutputSerializer(128);
@@ -145,6 +156,8 @@ public class EventSerializer {
                 int sequenceNumber = buffer.getInt();
                 AbstractEvent announcedEvent = fromSerializedEvent(buffer, classLoader);
                 return new EventAnnouncement(announcedEvent, sequenceNumber);
+            } else if (type == VIRTUAL_CHANNEL_SELECTOR_EVENT) {
+                return new VirtualChannelSelector(buffer.getInt(), buffer.getInt());
             } else if (type == OTHER_EVENT) {
                 try {
                     final DataInputDeserializer deserializer = new DataInputDeserializer(buffer);

flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/tasks/OneInputStreamTask.java
@@ -37,6 +37,7 @@ import org.apache.flink.streaming.runtime.io.StreamTaskInput;
 import org.apache.flink.streaming.runtime.io.StreamTaskNetworkInput;
 import org.apache.flink.streaming.runtime.io.checkpointing.CheckpointedInputGate;
 import org.apache.flink.streaming.runtime.io.checkpointing.InputProcessorUtil;
+import org.apache.flink.streaming.runtime.io.recovery.RescalingStreamTaskNetworkInput;
 import org.apache.flink.streaming.runtime.metrics.WatermarkGauge;
 import org.apache.flink.streaming.runtime.streamrecord.LatencyMarker;
 import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
@@ -110,11 +111,11 @@ public class OneInputStreamTask<IN, OUT> extends StreamTask<OUT, OneInputStreamO
         }
         mainOperator
                 .getMetricGroup()
-                .gauge(MetricNames.IO_CURRENT_INPUT_WATERMARK, this.inputWatermarkGauge);
+                .gauge(MetricNames.IO_CURRENT_INPUT_WATERMARK, inputWatermarkGauge);
         // wrap watermark gauge since registered metrics must be unique
         getEnvironment()
                 .getMetricGroup()
-                .gauge(MetricNames.IO_CURRENT_INPUT_WATERMARK, this.inputWatermarkGauge::getValue);
+                .gauge(MetricNames.IO_CURRENT_INPUT_WATERMARK, inputWatermarkGauge::getValue);
     }
 
     private StreamTaskInput<IN> wrapWithSorted(StreamTaskInput<IN> input) {
@@ -157,8 +158,20 @@ public class OneInputStreamTask<IN, OUT> extends StreamTask<OUT, OneInputStreamO
 
         TypeSerializer<IN> inSerializer =
                 configuration.getTypeSerializerIn1(getUserCodeClassLoader());
-        return new StreamTaskNetworkInput<>(
-                inputGate, inSerializer, getEnvironment().getIOManager(), statusWatermarkValve, 0);
+
+        return RescalingStreamTaskNetworkInput.create(
+                inputGate,
+                inSerializer,
+                getEnvironment().getIOManager(),
+                statusWatermarkValve,
+                0,
+                getEnvironment().getTaskStateManager().getInputRescalingDescriptor(),
+                gateIndex ->
+                        configuration
+                                .getInPhysicalEdges(getUserCodeClassLoader())
+                                .get(gateIndex)
+                                .getPartitioner(),
+                getEnvironment().getTaskInfo());
     }
 
     /**

apache/flink

flink-runtime/src/main/java/org/apache/flink/runtime/io/network/api/serialization/EventSerializer.java
@@ -30,6 +30,7 @@ import org.apache.flink.runtime.io.network.api.CheckpointBarrier;
 import org.apache.flink.runtime.io.network.api.EndOfPartitionEvent;
 import org.apache.flink.runtime.io.network.api.EndOfSuperstepEvent;
 import org.apache.flink.runtime.io.network.api.EventAnnouncement;
+import org.apache.flink.runtime.io.network.api.SubtaskConnectionDescriptor;
 import org.apache.flink.runtime.io.network.buffer.Buffer;
 import org.apache.flink.runtime.io.network.buffer.BufferConsumer;
 import org.apache.flink.runtime.io.network.buffer.FreeingBufferRecycler;
@@ -64,6 +65,8 @@ public class EventSerializer {
     private static final int END_OF_CHANNEL_STATE_EVENT = 5;
 
     private static final int ANNOUNCEMENT_EVENT = 6;
+
+    private static final int VIRTUAL_CHANNEL_SELECTOR_EVENT = 7;
 
     private static final int CHECKPOINT_TYPE_CHECKPOINT = 0;
 
@@ -107,6 +110,14 @@ public class EventSerializer {
             serializedAnnouncement.put(serializedAnnouncedEvent);
             serializedAnnouncement.flip();
             return serializedAnnouncement;
+        } else if (eventClass == SubtaskConnectionDescriptor.class) {
+            SubtaskConnectionDescriptor selector = (SubtaskConnectionDescriptor) event;
+            ByteBuffer buf = ByteBuffer.allocate(12);
+            buf.putInt(VIRTUAL_CHANNEL_SELECTOR_EVENT);
+            buf.putInt(selector.getInputSubtaskIndex());
+            buf.putInt(selector.getOutputSubtaskIndex());
+            buf.flip();
+            return buf;
         } else {
             try {
                 final DataOutputSerializer serializer = new DataOutputSerializer(128);
@@ -147,6 +158,8 @@ public class EventSerializer {
                 int sequenceNumber = buffer.getInt();
                 AbstractEvent announcedEvent = fromSerializedEvent(buffer, classLoader);
                 return new EventAnnouncement(announcedEvent, sequenceNumber);
+            } else if (type == VIRTUAL_CHANNEL_SELECTOR_EVENT) {
+                return new SubtaskConnectionDescriptor(buffer.getInt(), buffer.getInt());
             } else if (type == OTHER_EVENT) {
                 try {
                     final DataInputDeserializer deserializer = new DataInputDeserializer(buffer);

flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/tasks/OneInputStreamTask.java
@@ -35,6 +35,7 @@ import org.apache.flink.streaming.runtime.io.PushingAsyncDataInput.DataOutput;
 import org.apache.flink.streaming.runtime.io.StreamOneInputProcessor;
 import org.apache.flink.streaming.runtime.io.StreamTaskInput;
 import org.apache.flink.streaming.runtime.io.StreamTaskNetworkInput;
+import org.apache.flink.streaming.runtime.io.StreamTaskNetworkInputFactory;
 import org.apache.flink.streaming.runtime.io.checkpointing.CheckpointedInputGate;
 import org.apache.flink.streaming.runtime.io.checkpointing.InputProcessorUtil;
 import org.apache.flink.streaming.runtime.metrics.WatermarkGauge;
@@ -110,11 +111,11 @@ public class OneInputStreamTask<IN, OUT> extends StreamTask<OUT, OneInputStreamO
         }
         mainOperator
                 .getMetricGroup()
-                .gauge(MetricNames.IO_CURRENT_INPUT_WATERMARK, this.inputWatermarkGauge);
+                .gauge(MetricNames.IO_CURRENT_INPUT_WATERMARK, inputWatermarkGauge);
         // wrap watermark gauge since registered metrics must be unique
         getEnvironment()
                 .getMetricGroup()
-                .gauge(MetricNames.IO_CURRENT_INPUT_WATERMARK, this.inputWatermarkGauge::getValue);
+                .gauge(MetricNames.IO_CURRENT_INPUT_WATERMARK, inputWatermarkGauge::getValue);
     }
 
     private StreamTaskInput<IN> wrapWithSorted(StreamTaskInput<IN> input) {
@@ -157,8 +158,20 @@ public class OneInputStreamTask<IN, OUT> extends StreamTask<OUT, OneInputStreamO
 
         TypeSerializer<IN> inSerializer =
                 configuration.getTypeSerializerIn1(getUserCodeClassLoader());
-        return new StreamTaskNetworkInput<>(
-                inputGate, inSerializer, getEnvironment().getIOManager(), statusWatermarkValve, 0);
+
+        return StreamTaskNetworkInputFactory.create(
+                inputGate,
+                inSerializer,
+                getEnvironment().getIOManager(),
+                statusWatermarkValve,
+                0,
+                getEnvironment().getTaskStateManager().getInputRescalingDescriptor(),
+                gateIndex ->
+                        configuration
+                                .getInPhysicalEdges(getUserCodeClassLoader())
+                                .get(gateIndex)
+                                .getPartitioner(),
+                getEnvironment().getTaskInfo());
     }
 
     /**