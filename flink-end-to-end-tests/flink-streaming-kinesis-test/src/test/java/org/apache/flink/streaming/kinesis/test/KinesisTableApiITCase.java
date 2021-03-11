899b0734e44859288a8df75228c1659f78076bd8
Danny Cranmer <dannycranmer@apache.org>
2021-03-11 17:33:17 +0000

    [FLINK-20042][kinesis] Moving test file to test folder. Splitting setup and test

--- flink-end-to-end-tests/flink-streaming-kinesis-test/src/test/java/org/apache/flink/streaming/kinesis/test/KinesisTableApiITCase.java
+++ flink-end-to-end-tests/flink-streaming-kinesis-test/src/test/java/org/apache/flink/streaming/kinesis/test/KinesisTableApiITCase.java
@@ -26,12 +26,13 @@ import org.apache.flink.tests.util.TestUtils;
 import org.apache.flink.tests.util.categories.TravisGroup1;
 import org.apache.flink.tests.util.flink.FlinkContainer;
 import org.apache.flink.tests.util.flink.SQLJobSubmission;
+import org.apache.flink.util.TestLogger;
 
 import org.apache.flink.shaded.guava18.com.google.common.collect.ImmutableList;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -48,13 +49,15 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
+import static org.apache.flink.streaming.connectors.kinesis.config.AWSConfigConstants.AWS_ACCESS_KEY_ID;
 import static org.apache.flink.streaming.connectors.kinesis.config.AWSConfigConstants.AWS_ENDPOINT;
+import static org.apache.flink.streaming.connectors.kinesis.config.AWSConfigConstants.AWS_SECRET_ACCESS_KEY;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /** End-to-end test for Kinesis Table API using Kinesalite. */
 @Category(value = {TravisGroup1.class})
-public class KinesisTableApiITCase {
+public class KinesisTableApiITCase extends TestLogger {
     private static final String ORDERS_STREAM = "orders";
     private static final String LARGE_ORDERS_STREAM = "large_orders";
 
@@ -65,6 +68,8 @@ public class KinesisTableApiITCase {
 
     @Rule
     public final KinesaliteContainer kinesalite = new KinesaliteContainer().withNetwork(network);
+
+    private KinesisPubsubClient kinesisClient;
 
     @Rule
     public final FlinkContainer flink =
@@ -79,8 +84,8 @@ public class KinesisTableApiITCase {
                     .withNetwork(network)
                     .dependsOn(kinesalite);
 
-    @BeforeClass
-    public static void setUp() {
+    @Before
+    public void setUp() throws Exception {
         // Required for Kinesalite.
         // Including shaded and non-shaded conf to support test running from Maven and IntelliJ
         System.setProperty("com.amazonaws.sdk.disableCertChecking", "1");
@@ -88,31 +93,33 @@ public class KinesisTableApiITCase {
         System.setProperty(
                 "org.apache.flink.kinesis.shaded.com.amazonaws.sdk.disableCertChecking", "1");
         System.setProperty("org.apache.flink.kinesis.shaded.com.amazonaws.sdk.disableCbor", "1");
+
+        Properties properties = new Properties();
+        properties.setProperty(AWS_ENDPOINT, kinesalite.getEndpointUrl());
+        properties.setProperty(AWS_ACCESS_KEY_ID, "ak");
+        properties.setProperty(AWS_SECRET_ACCESS_KEY, "sk");
+
+        kinesisClient = new KinesisPubsubClient(properties);
+        kinesisClient.createTopic(ORDERS_STREAM, 1, properties);
+        kinesisClient.createTopic(LARGE_ORDERS_STREAM, 1, properties);
     }
 
     @Test(timeout = 120_000)
     public void testTableApiSourceAndSink() throws Exception {
-        Properties properties = new Properties();
-        properties.setProperty(AWS_ENDPOINT, kinesalite.getEndpointUrl());
-
         List<Order> smallOrders = ImmutableList.of(new Order("A", 5), new Order("B", 10));
 
-        // Large orders have a quantity > 10
-        List<Order> largeOrders =
+        // filter-large-orders.sql is supposed to preserve orders with quantity > 10
+        List<Order> expected =
                 ImmutableList.of(new Order("C", 15), new Order("D", 20), new Order("E", 25));
 
-        KinesisPubsubClient client = new KinesisPubsubClient(properties);
-        client.createTopic(ORDERS_STREAM, 1, properties);
-        client.createTopic(LARGE_ORDERS_STREAM, 1, properties);
-
-        smallOrders.forEach(order -> client.sendMessage(ORDERS_STREAM, toJson(order)));
-        largeOrders.forEach(order -> client.sendMessage(ORDERS_STREAM, toJson(order)));
+        smallOrders.forEach(order -> kinesisClient.sendMessage(ORDERS_STREAM, toJson(order)));
+        expected.forEach(order -> kinesisClient.sendMessage(ORDERS_STREAM, toJson(order)));
 
         executeSqlStatements(readSqlFile("filter-large-orders.sql"));
 
-        List<Order> result = readAllOrdersFromKinesis(client);
-        assertEquals(largeOrders.size(), result.size());
-        assertTrue(result.containsAll(largeOrders));
+        List<Order> result = readAllOrdersFromKinesis(kinesisClient);
+        assertEquals(expected.size(), result.size());
+        assertTrue(result.containsAll(expected));
     }
 
     private List<Order> readAllOrdersFromKinesis(final KinesisPubsubClient client)
