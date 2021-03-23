austince/flink-ci-flink

[FLINK-21661][kinesis] Fix fetch interval for polling consumer

flink-connectors/flink-connector-kinesis/src/test/java/org/apache/flink/streaming/connectors/kinesis/internals/publisher/polling/PollingRecordPublisherTest.java
@@ -34,6 +34,7 @@ import static org.apache.flink.streaming.connectors.kinesis.internals.publisher.
 import static org.apache.flink.streaming.connectors.kinesis.model.SentinelSequenceNumber.SENTINEL_EARLIEST_SEQUENCE_NUM;
 import static org.apache.flink.streaming.connectors.kinesis.testutils.FakeKinesisBehavioursFactory.totalNumOfRecordsAfterNumOfGetRecordsCalls;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.AdditionalMatchers.gt;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
@@ -42,6 +43,8 @@ import static org.mockito.Mockito.verify;
 
 /** Tests for {@link PollingRecordPublisher}. */
 public class PollingRecordPublisherTest {
+
+    private static final long FETCH_INTERVAL_MILLIS = 500L;
 
     @Rule public ExpectedException thrown = ExpectedException.none();
 
@@ -56,6 +59,21 @@ public class PollingRecordPublisherTest {
         assertEquals(1, consumer.getRecordBatches().size());
         assertEquals(5, consumer.getRecordBatches().get(0).getDeaggregatedRecordSize());
         assertEquals(100L, consumer.getRecordBatches().get(0).getMillisBehindLatest(), 0);
+    }
+
+    @Test
+    public void testRunEmitsRunLoopTimeNanos() throws Exception {
+        PollingRecordPublisherMetricsReporter metricsReporter =
+                spy(new PollingRecordPublisherMetricsReporter(mock(MetricGroup.class)));
+
+        KinesisProxyInterface fakeKinesis = totalNumOfRecordsAfterNumOfGetRecordsCalls(5, 5, 100);
+        PollingRecordPublisher recordPublisher =
+                createPollingRecordPublisher(fakeKinesis, metricsReporter);
+
+        recordPublisher.run(new TestConsumer());
+
+        // Expect that the run loop took at least FETCH_INTERVAL_MILLIS in nanos
+        verify(metricsReporter).setRunLoopTimeNanos(gt(FETCH_INTERVAL_MILLIS * 1000));
     }
 
     @Test
@@ -136,12 +154,19 @@ public class PollingRecordPublisherTest {
         PollingRecordPublisherMetricsReporter metricsReporter =
                 new PollingRecordPublisherMetricsReporter(mock(MetricGroup.class));
 
+        return createPollingRecordPublisher(kinesis, metricsReporter);
+    }
+
+    PollingRecordPublisher createPollingRecordPublisher(
+            final KinesisProxyInterface kinesis,
+            final PollingRecordPublisherMetricsReporter metricGroupReporter)
+            throws Exception {
         return new PollingRecordPublisher(
                 StartingPosition.restartFromSequenceNumber(SENTINEL_EARLIEST_SEQUENCE_NUM.get()),
                 TestUtils.createDummyStreamShardHandle(),
-                metricsReporter,
+                metricGroupReporter,
                 kinesis,
                 10000,
-                500L);
+                FETCH_INTERVAL_MILLIS);
     }
 }

apache/flink

flink-connectors/flink-connector-kinesis/src/test/java/org/apache/flink/streaming/connectors/kinesis/internals/publisher/polling/PollingRecordPublisherTest.java
@@ -34,6 +34,7 @@ import static org.apache.flink.streaming.connectors.kinesis.internals.publisher.
 import static org.apache.flink.streaming.connectors.kinesis.model.SentinelSequenceNumber.SENTINEL_EARLIEST_SEQUENCE_NUM;
 import static org.apache.flink.streaming.connectors.kinesis.testutils.FakeKinesisBehavioursFactory.totalNumOfRecordsAfterNumOfGetRecordsCalls;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.AdditionalMatchers.geq;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
@@ -42,6 +43,8 @@ import static org.mockito.Mockito.verify;
 
 /** Tests for {@link PollingRecordPublisher}. */
 public class PollingRecordPublisherTest {
+
+    private static final long FETCH_INTERVAL_MILLIS = 500L;
 
     @Rule public ExpectedException thrown = ExpectedException.none();
 
@@ -56,6 +59,21 @@ public class PollingRecordPublisherTest {
         assertEquals(1, consumer.getRecordBatches().size());
         assertEquals(5, consumer.getRecordBatches().get(0).getDeaggregatedRecordSize());
         assertEquals(100L, consumer.getRecordBatches().get(0).getMillisBehindLatest(), 0);
+    }
+
+    @Test
+    public void testRunEmitsRunLoopTimeNanos() throws Exception {
+        PollingRecordPublisherMetricsReporter metricsReporter =
+                spy(new PollingRecordPublisherMetricsReporter(mock(MetricGroup.class)));
+
+        KinesisProxyInterface fakeKinesis = totalNumOfRecordsAfterNumOfGetRecordsCalls(5, 5, 100);
+        PollingRecordPublisher recordPublisher =
+                createPollingRecordPublisher(fakeKinesis, metricsReporter);
+
+        recordPublisher.run(new TestConsumer());
+
+        // Expect that the run loop took at least FETCH_INTERVAL_MILLIS in nanos
+        verify(metricsReporter).setRunLoopTimeNanos(geq(FETCH_INTERVAL_MILLIS * 1_000_000));
     }
 
     @Test
@@ -136,12 +154,19 @@ public class PollingRecordPublisherTest {
         PollingRecordPublisherMetricsReporter metricsReporter =
                 new PollingRecordPublisherMetricsReporter(mock(MetricGroup.class));
 
+        return createPollingRecordPublisher(kinesis, metricsReporter);
+    }
+
+    PollingRecordPublisher createPollingRecordPublisher(
+            final KinesisProxyInterface kinesis,
+            final PollingRecordPublisherMetricsReporter metricGroupReporter)
+            throws Exception {
         return new PollingRecordPublisher(
                 StartingPosition.restartFromSequenceNumber(SENTINEL_EARLIEST_SEQUENCE_NUM.get()),
                 TestUtils.createDummyStreamShardHandle(),
-                metricsReporter,
+                metricGroupReporter,
                 kinesis,
                 10000,
-                500L);
+                FETCH_INTERVAL_MILLIS);
     }
 }