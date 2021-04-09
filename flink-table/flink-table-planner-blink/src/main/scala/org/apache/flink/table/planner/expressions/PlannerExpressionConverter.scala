e33e4ae2b70ecc2f61c8bbe32e73f110fbdee7c1
Timo Walther <twalthr@apache.org>
2021-04-09 13:52:25 +0200

    [FLINK-22151][table] Update type inference of avg, sum, sum0, count, min, max, stddevPop/Samp, varPop/Samp
    This closes #15551.

--- flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/expressions/PlannerExpressionConverter.scala
+++ flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/expressions/PlannerExpressionConverter.scala
@@ -150,46 +150,6 @@ class PlannerExpressionConverter private extends ApiExpressionVisitor[PlannerExp
             assert(args.size == 1)
             DistinctAgg(args.head)
 
-          case AVG =>
-            assert(args.size == 1)
-            Avg(args.head)
-
-          case COUNT =>
-            assert(args.size == 1)
-            Count(args.head)
-
-          case MAX =>
-            assert(args.size == 1)
-            Max(args.head)
-
-          case MIN =>
-            assert(args.size == 1)
-            Min(args.head)
-
-          case SUM =>
-            assert(args.size == 1)
-            Sum(args.head)
-
-          case SUM0 =>
-            assert(args.size == 1)
-            Sum0(args.head)
-
-          case STDDEV_POP =>
-            assert(args.size == 1)
-            StddevPop(args.head)
-
-          case STDDEV_SAMP =>
-            assert(args.size == 1)
-            StddevSamp(args.head)
-
-          case VAR_POP =>
-            assert(args.size == 1)
-            VarPop(args.head)
-
-          case VAR_SAMP =>
-            assert(args.size == 1)
-            VarSamp(args.head)
-
           case COLLECT =>
             assert(args.size == 1)
             Collect(args.head)
