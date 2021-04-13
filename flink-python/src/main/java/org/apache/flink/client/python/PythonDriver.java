a6331ac6054200271e71065b471330122c3c76e9
Dian Fu <dianfu@apache.org>
2021-04-13 17:27:30 +0800

    [hotfix][python] Improve the error message in PythonDriver

--- flink-python/src/main/java/org/apache/flink/client/python/PythonDriver.java
+++ flink-python/src/main/java/org/apache/flink/client/python/PythonDriver.java
@@ -128,7 +128,7 @@ public final class PythonDriver {
             } else {
                 // throw ProgramAbortException if the caller is interested in the program plan,
                 // there is no harm to throw ProgramAbortException even if it is not the case.
-                throw new ProgramAbortException();
+                throw new ProgramAbortException(e);
             }
         } finally {
             PythonEnvUtils.setGatewayServer(null);
