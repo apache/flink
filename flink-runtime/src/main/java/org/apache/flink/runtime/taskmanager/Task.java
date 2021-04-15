a007612245797b5b466a9eecc4fff7e1027174b5
Roman Khachatryan <khachatryan.roman@gmail.com>
2021-04-15 12:58:14 +0200

    [hotfix][runtime] Log checkpoint processing delay if above threshold

--- flink-runtime/src/main/java/org/apache/flink/runtime/taskmanager/Task.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/taskmanager/Task.java
@@ -1285,7 +1285,8 @@ public class Task
 
         final AbstractInvokable invokable = this.invokable;
         final CheckpointMetaData checkpointMetaData =
-                new CheckpointMetaData(checkpointID, checkpointTimestamp);
+                new CheckpointMetaData(
+                        checkpointID, checkpointTimestamp, System.currentTimeMillis());
 
         if (executionState == ExecutionState.RUNNING && invokable != null) {
             try {
