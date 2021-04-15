e5376c2615f0b279744a92fb1b037d60834d5e22
wangfeifan <zoltar9264@163.com>
2021-04-15 14:18:53 +0800

    [FLINK-21986][State Backends] fix native memory used by RocksDB not be released timely after job restart

--- flink-state-backends/flink-statebackend-rocksdb/src/main/java/org/apache/flink/contrib/streaming/state/RocksDBOperationUtils.java
+++ flink-state-backends/flink-statebackend-rocksdb/src/main/java/org/apache/flink/contrib/streaming/state/RocksDBOperationUtils.java
@@ -248,8 +248,15 @@ public class RocksDBOperationUtils {
     public static void addColumnFamilyOptionsToCloseLater(
             List<ColumnFamilyOptions> columnFamilyOptions, ColumnFamilyHandle columnFamilyHandle) {
         try {
-            if (columnFamilyHandle != null && columnFamilyHandle.getDescriptor() != null) {
-                columnFamilyOptions.add(columnFamilyHandle.getDescriptor().getOptions());
+            // IMPORTANT NOTE: Do not call ColumnFamilyHandle#getDescriptor() just to judge if it
+            // return null and then call it again when it return is not null. That will cause
+            // task manager native memory used by RocksDB can't be released timely after job restart.
+            // The problem can find in : https://issues.apache.org/jira/browse/FLINK-21986
+            if (columnFamilyHandle != null) {
+                ColumnFamilyDescriptor columnFamilyDescriptor = columnFamilyHandle.getDescriptor();
+                if (columnFamilyDescriptor != null) {
+                    columnFamilyOptions.add(columnFamilyDescriptor.getOptions());
+                }
             }
         } catch (RocksDBException e) {
             // ignore
