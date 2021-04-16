d32ca07743c729e66656f119f057249e03e5c5e2
godfreyhe <godfreyhe@163.com>
2021-04-16 18:28:23 +0800

    [FLINK-22310][table-planner-blink] Fix the incorrect deserialization result of LogicalWindowJsonDeserializer
    This closes #15645

--- flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/plan/logical/groupWindows.scala
+++ flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/plan/logical/groupWindows.scala
@@ -20,16 +20,52 @@ package org.apache.flink.table.planner.plan.logical
 
 import org.apache.flink.table.expressions._
 import org.apache.flink.table.planner.expressions.PlannerWindowReference
+import org.apache.flink.table.planner.plan.utils.AggregateUtil.{hasTimeIntervalType, toLong}
+
+import java.time.Duration
+import java.util.Objects
 
 /**
-  * Logical super class for group windows.
-  *
-  * @param aliasAttribute window alias
-  * @param timeAttribute time field indicating event-time or processing-time
-  */
+ * Logical super class for group windows.
+ *
+ * @param aliasAttribute window alias
+ * @param timeAttribute time field indicating event-time or processing-time
+ */
 abstract class LogicalWindow(
     val aliasAttribute: PlannerWindowReference,
     val timeAttribute: FieldReferenceExpression) {
+
+  override def equals(o: Any): Boolean = {
+    if (o == null || (getClass ne o.getClass)) {
+      return false
+    }
+    val that = o.asInstanceOf[LogicalWindow]
+    Objects.equals(aliasAttribute, that.aliasAttribute) &&
+      Objects.equals(timeAttribute, that.timeAttribute)
+  }
+
+  protected def isValueLiteralExpressionEqual(
+      l1: ValueLiteralExpression, l2: ValueLiteralExpression): Boolean = {
+    if (l1 == null && l2 == null) {
+      true
+    } else if (l1 == null || l2 == null) {
+      false
+    } else {
+      if (hasTimeIntervalType(l1)) {
+        if (hasTimeIntervalType(l2)) {
+          val v1 = l1.getValueAs(classOf[Duration])
+          val v2 = l2.getValueAs(classOf[Duration])
+          v1.equals(v2) && l1.getOutputDataType.equals(l2.getOutputDataType)
+        } else {
+          false
+        }
+      } else {
+        val v1 = toLong(l1)
+        val v2 = toLong(l2)
+        v1 == v2 && l1.getOutputDataType.equals(l2.getOutputDataType)
+      }
+    }
+  }
 
   override def toString: String = getClass.getSimpleName
 }
@@ -45,6 +81,14 @@ case class TumblingGroupWindow(
   extends LogicalWindow(
     alias,
     timeField) {
+
+  override def equals(o: Any): Boolean = {
+    if (super.equals(o)) {
+      isValueLiteralExpressionEqual(size, o.asInstanceOf[TumblingGroupWindow].size)
+    } else {
+      false
+    }
+  }
 
   override def toString: String = s"TumblingGroupWindow($alias, $timeField, $size)"
 }
@@ -62,6 +106,15 @@ case class SlidingGroupWindow(
     alias,
     timeField) {
 
+  override def equals(o: Any): Boolean = {
+    if (super.equals(o)) {
+      isValueLiteralExpressionEqual(size, o.asInstanceOf[SlidingGroupWindow].size) &&
+        isValueLiteralExpressionEqual(slide, o.asInstanceOf[SlidingGroupWindow].slide)
+    } else {
+      false
+    }
+  }
+
   override def toString: String = s"SlidingGroupWindow($alias, $timeField, $size, $slide)"
 }
 
@@ -76,6 +129,14 @@ case class SessionGroupWindow(
   extends LogicalWindow(
     alias,
     timeField) {
+
+  override def equals(o: Any): Boolean = {
+    if (super.equals(o)) {
+      isValueLiteralExpressionEqual(gap, o.asInstanceOf[SessionGroupWindow].gap)
+    } else {
+      false
+    }
+  }
 
   override def toString: String = s"SessionGroupWindow($alias, $timeField, $gap)"
 }
