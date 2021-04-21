ed5ca59f974c0499d9a0a4ef36580dafd5081c89
Chesnay Schepler <chesnay@apache.org>
2021-04-21 15:10:24 +0200

    [FLINK-20723][tests] Allow retries to be defined per class

--- flink-test-utils-parent/flink-test-utils-junit/src/test/java/org/apache/flink/testutils/junit/RetryRuleTest.java
+++ flink-test-utils-parent/flink-test-utils-junit/src/test/java/org/apache/flink/testutils/junit/RetryRuleTest.java
@@ -30,10 +30,11 @@ import static org.junit.Assert.assertThat;
 
 /** Tests for the {@link RetryRule}. */
 public class RetryRuleTest extends TestLogger {
+    private static final RetryRule RETRY_RULE = new RetryRule();
 
     @Test
     public void testExpectedExceptionIgnored() throws Throwable {
-        final RetryRule retryRule = new RetryRule();
+        final int numEvaluationsToFail = 1;
 
         final Description testDescription =
                 Description.createTestDescription(
@@ -43,21 +44,97 @@ public class RetryRuleTest extends TestLogger {
                                 .getMethod("test")
                                 .getAnnotations());
 
-        final TestStatement statement = new TestStatement(1);
+        final TestStatement statement = new TestStatement(numEvaluationsToFail);
 
         try {
-            retryRule.apply(statement, testDescription).evaluate();
+            RETRY_RULE.apply(statement, testDescription).evaluate();
             Assert.fail("Should have failed.");
         } catch (RuntimeException expected) {
         }
 
-        assertThat(statement.getNumEvaluations(), is(1));
+        assertThat(statement.getNumEvaluations(), is(numEvaluationsToFail));
     }
 
     @Ignore // we don't want to actually this run as a test
     private static class TestClassWithTestExpectingRuntimeException {
         @RetryOnFailure(times = 2)
         @Test(expected = RuntimeException.class)
+        public void test() {}
+    }
+
+    @Test
+    public void testNoAnnotationResultsInZeroRetries() throws Throwable {
+        final int numEvaluationsToFail = 1;
+
+        final Description testDescription =
+                Description.createTestDescription(
+                        TestClassWithoutAnnotation.class,
+                        "test",
+                        TestClassWithAnnotation.class.getMethod("test").getAnnotations());
+
+        final TestStatement statement = new TestStatement(numEvaluationsToFail);
+
+        try {
+            RETRY_RULE.apply(statement, testDescription).evaluate();
+            Assert.fail("Should have failed.");
+        } catch (RuntimeException expected) {
+        }
+
+        assertThat(statement.getNumEvaluations(), is(numEvaluationsToFail));
+    }
+
+    @Ignore // we don't want to actually this run as a test
+    private static class TestClassWithoutAnnotation {
+        @Test
+        public void test() {}
+    }
+
+    @Test
+    public void testAnnotationOnClassUsedAsFallback() throws Throwable {
+        final int numEvaluationsToFail = 1;
+
+        final Description testDescription =
+                Description.createTestDescription(
+                        TestClassWithAnnotation.class,
+                        "test",
+                        TestClassWithAnnotation.class.getMethod("test").getAnnotations());
+
+        final TestStatement statement = new TestStatement(numEvaluationsToFail);
+
+        RETRY_RULE.apply(statement, testDescription).evaluate();
+
+        assertThat(statement.getNumEvaluations(), is(numEvaluationsToFail + 1));
+    }
+
+    @Ignore // we don't want to actually this run as a test
+    @RetryOnFailure(times = 1)
+    private static class TestClassWithAnnotation {
+        @Test
+        public void test() {}
+    }
+
+    @Test
+    public void testAnnotationOnMethodTakesPrecedence() throws Throwable {
+        final int numEvaluationsToFail = 2;
+
+        final Description testDescription =
+                Description.createTestDescription(
+                        TestClassWithAnnotationOnMethod.class,
+                        "test",
+                        TestClassWithAnnotationOnMethod.class.getMethod("test").getAnnotations());
+
+        final TestStatement statement = new TestStatement(numEvaluationsToFail);
+
+        RETRY_RULE.apply(statement, testDescription).evaluate();
+
+        assertThat(statement.getNumEvaluations(), is(numEvaluationsToFail + 1));
+    }
+
+    @Ignore // we don't want to actually this run as a test
+    @RetryOnFailure(times = 1)
+    private static class TestClassWithAnnotationOnMethod {
+        @RetryOnFailure(times = 2)
+        @Test
         public void test() {}
     }
 
