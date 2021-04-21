28eea07cea69f1d7bf0d2062e65e08f7671b8ebc
Dong Lin <lindong28@gmail.com>
2021-04-21 18:46:46 +0800

    [FLINK-22085][tests] Update TestUtils::tryExecute() to cancel the job after execution failure.
    This closes #15713

--- flink-tests/src/test/java/org/apache/flink/test/util/TestUtils.java
+++ flink-tests/src/test/java/org/apache/flink/test/util/TestUtils.java
@@ -18,41 +18,53 @@
 
 package org.apache.flink.test.util;
 
-import org.apache.flink.api.common.JobExecutionResult;
 import org.apache.flink.api.common.JobID;
 import org.apache.flink.client.ClientUtils;
 import org.apache.flink.client.program.ClusterClient;
-import org.apache.flink.client.program.ProgramInvocationException;
-import org.apache.flink.runtime.client.JobExecutionException;
+import org.apache.flink.core.execution.JobClient;
 import org.apache.flink.runtime.client.JobInitializationException;
 import org.apache.flink.runtime.jobgraph.JobGraph;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.graph.StreamGraph;
+import org.apache.flink.util.ExceptionUtils;
+
+import java.util.Optional;
 
 import static org.junit.Assert.fail;
 
 /** Test utilities. */
 public class TestUtils {
 
-    public static JobExecutionResult tryExecute(StreamExecutionEnvironment see, String name)
-            throws Exception {
+    /**
+     * Execute the job and wait for the job result synchronously.
+     *
+     * @throws Exception If executing the environment throws an exception which does not have {@link
+     *     SuccessException} as a cause.
+     */
+    public static void tryExecute(StreamExecutionEnvironment see, String name) throws Exception {
+        JobClient jobClient = null;
         try {
-            return see.execute(name);
-        } catch (ProgramInvocationException | JobExecutionException root) {
-            Throwable cause = root.getCause();
-
-            // search for nested SuccessExceptions
-            int depth = 0;
-            while (!(cause instanceof SuccessException)) {
-                if (cause == null || depth++ == 20) {
-                    root.printStackTrace();
-                    fail("Test failed: " + root.getMessage());
-                } else {
-                    cause = cause.getCause();
+            StreamGraph graph = see.getStreamGraph(name);
+            jobClient = see.executeAsync(graph);
+            jobClient.getJobExecutionResult().get();
+        } catch (Throwable root) {
+            if (jobClient != null) {
+                try {
+                    jobClient.cancel().get();
+                } catch (Exception e) {
+                    // Exception could be thrown if the job has already finished.
+                    // Ignore the exception.
                 }
             }
-        }
 
-        return null;
+            Optional<SuccessException> successAsCause =
+                    ExceptionUtils.findThrowable(root, SuccessException.class);
+
+            if (!successAsCause.isPresent()) {
+                root.printStackTrace();
+                fail("Test failed: " + root.getMessage());
+            }
+        }
     }
 
     public static void submitJobAndWaitForResult(
