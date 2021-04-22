0d29b23f892714e4936b8af2f896e3040ddc9e89
dbgp2021 <80257284+dbgp2021@users.noreply.github.com>
2021-04-22 21:23:56 +0800

    [FLINK-22000][io] Set a default character set in InputStreamReader

--- flink-python/src/main/java/org/apache/flink/client/python/PythonDriver.java
+++ flink-python/src/main/java/org/apache/flink/client/python/PythonDriver.java
@@ -30,6 +30,7 @@ import py4j.GatewayServer;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -101,7 +102,9 @@ public final class PythonDriver {
             Runtime.getRuntime().addShutdownHook(shutdownHook);
 
             BufferedReader in =
-                    new BufferedReader(new InputStreamReader(pythonProcess.getInputStream()));
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    pythonProcess.getInputStream(), StandardCharsets.UTF_8));
             LOG.info(
                     "--------------------------- Python Process Started --------------------------");
             // print the python process output to stdout and log file
