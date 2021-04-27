dd7352f8f7981ea5af5ea95b36ce28514bdbfe7c
Yik San Chan <evan.chanyiksan@gmail.com>
2021-04-27 17:34:03 +0800

    [hotfix][python][docs] Fix python.archives docs
    This closes #15783.

--- flink-clients/src/main/java/org/apache/flink/client/cli/CliFrontendParser.java
+++ flink-clients/src/main/java/org/apache/flink/client/cli/CliFrontendParser.java
@@ -224,7 +224,7 @@ public class CliFrontendParser {
                     "Add python archive files for job. The archive files will be extracted to the working directory "
                             + "of python UDF worker. Currently only zip-format is supported. For each archive file, a target directory "
                             + "be specified. If the target directory name is specified, the archive file will be extracted to a "
-                            + "name can directory with the specified name. Otherwise, the archive file will be extracted to a "
+                            + "directory with the specified name. Otherwise, the archive file will be extracted to a "
                             + "directory with the same name of the archive file. The files uploaded via this option are accessible "
                             + "via relative path. '#' could be used as the separator of the archive file path and the target directory "
                             + "name. Comma (',') could be used as the separator to specify multiple archive files. "
