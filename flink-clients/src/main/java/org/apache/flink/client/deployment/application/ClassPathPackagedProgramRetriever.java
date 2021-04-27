6314a9c8dd16b17b864b3ce38e5ede1cc96c9105
se.royruan <ruan.xin@immomo.com>
2021-04-27 19:44:48 +0800

    configuration

--- flink-clients/src/main/java/org/apache/flink/client/deployment/application/ClassPathPackagedProgramRetriever.java
+++ flink-clients/src/main/java/org/apache/flink/client/deployment/application/ClassPathPackagedProgramRetriever.java
@@ -24,6 +24,7 @@ import org.apache.flink.client.program.PackagedProgram;
 import org.apache.flink.client.program.PackagedProgramRetriever;
 import org.apache.flink.client.program.PackagedProgramUtils;
 import org.apache.flink.client.program.ProgramInvocationException;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.util.ExceptionUtils;
 import org.apache.flink.util.FileUtils;
 import org.apache.flink.util.FlinkException;
@@ -76,12 +77,15 @@ public class ClassPathPackagedProgramRetriever implements PackagedProgramRetriev
 
     @Nullable private final File jarFile;
 
+    @Nonnull private final Configuration configuration;
+
     private ClassPathPackagedProgramRetriever(
             @Nonnull String[] programArguments,
             @Nullable String jobClassName,
             @Nonnull Supplier<Iterable<File>> jarsOnClassPath,
             @Nullable File userLibDirectory,
-            @Nullable File jarFile)
+            @Nullable File jarFile,
+            @Nonnull Configuration configuration)
             throws IOException {
         this.userLibDirectory = userLibDirectory;
         this.programArguments = requireNonNull(programArguments, "programArguments");
@@ -89,6 +93,7 @@ public class ClassPathPackagedProgramRetriever implements PackagedProgramRetriev
         this.jarsOnClassPath = requireNonNull(jarsOnClassPath);
         this.userClassPaths = discoverUserClassPaths(userLibDirectory);
         this.jarFile = jarFile;
+        this.configuration = configuration;
     }
 
     private Collection<URL> discoverUserClassPaths(@Nullable File jobDir) throws IOException {
@@ -120,6 +125,7 @@ public class ClassPathPackagedProgramRetriever implements PackagedProgramRetriev
                         .setArguments(programArguments)
                         .setJarFile(pythonJarFile)
                         .setEntryPointClassName(pythonJobClassName)
+                        .setConfiguration(configuration)
                         .build();
             }
 
@@ -129,6 +135,7 @@ public class ClassPathPackagedProgramRetriever implements PackagedProgramRetriev
                         .setArguments(programArguments)
                         .setJarFile(jarFile)
                         .setEntryPointClassName(jobClassName)
+                        .setConfiguration(configuration)
                         .build();
             }
 
@@ -137,6 +144,7 @@ public class ClassPathPackagedProgramRetriever implements PackagedProgramRetriev
                     .setUserClassPaths(new ArrayList<>(userClassPaths))
                     .setEntryPointClassName(entryClass)
                     .setArguments(programArguments)
+                    .setConfiguration(configuration)
                     .build();
         } catch (ProgramInvocationException e) {
             throw new FlinkException("Could not load the provided entrypoint class.", e);
@@ -252,6 +260,8 @@ public class ClassPathPackagedProgramRetriever implements PackagedProgramRetriev
 
         private File jarFile;
 
+        private Configuration configuration = new Configuration();
+
         private Builder(String[] programArguments) {
             this.programArguments = requireNonNull(programArguments);
         }
@@ -276,9 +286,14 @@ public class ClassPathPackagedProgramRetriever implements PackagedProgramRetriev
             return this;
         }
 
+        public Builder setConfiguration(Configuration configuration) {
+            this.configuration = configuration;
+            return this;
+        }
+
         public ClassPathPackagedProgramRetriever build() throws IOException {
             return new ClassPathPackagedProgramRetriever(
-                    programArguments, jobClassName, jarsOnClassPath, userLibDirectory, jarFile);
+                    programArguments, jobClassName, jarsOnClassPath, userLibDirectory, jarFile, configuration);
         }
     }
 
