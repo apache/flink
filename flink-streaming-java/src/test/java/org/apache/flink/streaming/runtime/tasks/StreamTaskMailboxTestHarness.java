0790768167f1a43d90df89ca4ceb2248841d91d3
Arvid Heise <arvid@ververica.com>
2021-05-05 16:56:00 +0200

    [FLINK-22573][datastream] Fix AsyncIO calls timeout on completed element.
    As long as the mailbox is blocked, timers are not cancelled, such that a completed element might still get a timeout.
    The fix is to check the completed flag when the timer triggers.

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/runtime/tasks/StreamTaskMailboxTestHarness.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/runtime/tasks/StreamTaskMailboxTestHarness.java
@@ -68,6 +68,10 @@ public class StreamTaskMailboxTestHarness<OUT> implements AutoCloseable {
         return streamTask;
     }
 
+    public TimerService getTimerService() {
+        return streamTask.getTimerService();
+    }
+
     /**
      * Get all the output from the task. This contains StreamRecords and Events interleaved. Use
      * {@link
