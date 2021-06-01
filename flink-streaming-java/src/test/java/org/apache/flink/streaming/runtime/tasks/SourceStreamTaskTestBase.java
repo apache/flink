d910337a65d5b8bebec51519f417fb1836a05bd3
Piotr Nowojski <piotr.nowojski@gmail.com>
2021-06-01 15:16:07 +0200

    [FLINK-22833][metrics] Set checkpointStartDelay in checkpoint metrics for SourceTasks
    This commit is fixing a bug of checkpointStartDelay being not set in
    CheckpointMetrics of SourceStreamTask and SourceOperatorStreamTask.

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/runtime/tasks/SourceStreamTaskTestBase.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/runtime/tasks/SourceStreamTaskTestBase.java
@@ -18,6 +18,7 @@
 
 package org.apache.flink.streaming.runtime.tasks;
 
+import org.apache.flink.core.testutils.OneShotLatch;
 import org.apache.flink.metrics.Gauge;
 import org.apache.flink.metrics.Metric;
 import org.apache.flink.runtime.checkpoint.CheckpointMetaData;
@@ -25,8 +26,11 @@ import org.apache.flink.runtime.checkpoint.CheckpointOptions;
 import org.apache.flink.runtime.execution.Environment;
 import org.apache.flink.runtime.metrics.MetricNames;
 import org.apache.flink.runtime.metrics.groups.TaskMetricGroup;
+import org.apache.flink.runtime.taskmanager.TestCheckpointResponder;
 import org.apache.flink.streaming.api.operators.StreamOperatorFactory;
 import org.apache.flink.util.function.FunctionWithException;
+
+import org.apache.flink.shaded.guava18.com.google.common.collect.Iterables;
 
 import org.hamcrest.Matcher;
 
@@ -66,6 +70,9 @@ public class SourceStreamTaskTestBase {
                             new CheckpointMetaData(1L, System.currentTimeMillis()),
                             CheckpointOptions.forCheckpointWithDefaultLocation());
 
+            OneShotLatch checkpointAcknowledgeLatch = new OneShotLatch();
+            harness.getCheckpointResponder().setAcknowledgeLatch(checkpointAcknowledgeLatch);
+
             assertFalse(triggerFuture.isDone());
             Thread.sleep(sleepTime);
             while (!triggerFuture.isDone()) {
@@ -78,6 +85,14 @@ public class SourceStreamTaskTestBase {
                     greaterThanOrEqualTo(sleepTime * 1_000_000));
             Gauge<Double> busyTimeGauge = (Gauge<Double>) metrics.get(MetricNames.TASK_BUSY_TIME);
             assertThat(busyTimeGauge.getValue(), busyTimeMatcher);
+
+            checkpointAcknowledgeLatch.await();
+            TestCheckpointResponder.AcknowledgeReport acknowledgeReport =
+                    Iterables.getOnlyElement(
+                            harness.getCheckpointResponder().getAcknowledgeReports());
+            assertThat(
+                    acknowledgeReport.getCheckpointMetrics().getCheckpointStartDelayNanos(),
+                    greaterThanOrEqualTo(sleepTime * 1_000_000));
         }
     }
 }
