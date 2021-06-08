ed6b33d487bccd9fd96607a3fe681ead1912d365
Till Rohrmann <trohrmann@apache.org>
2021-06-08 12:52:57 +0200

    [hotfix] Replace deprecated import org.junit.Assert.assertThat with org.hamcrest.MatcherAssert.assertThat

--- flink-runtime/src/test/java/org/apache/flink/runtime/entrypoint/ClusterEntrypointTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/entrypoint/ClusterEntrypointTest.java
@@ -65,7 +65,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.junit.Assume.assumeTrue;
