5fe92275f875d50f14d80f4ddecbea85e942db8d
Yuri Gusev <yura.gusev@gmail.com>
2021-06-13 21:59:33 +0200

    [FLINK-16504] [dynamodb connector] add interface for DynamoDb producer

---
+++ flink-connectors/flink-connector-dynamodb/src/main/java/org/apache/flink/streaming/connectors/dynamodb/DynamoDbProducer.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.connectors.dynamodb;
+
+import org.apache.flink.annotation.PublicEvolving;
+
+import software.amazon.awssdk.services.dynamodb.model.DeleteItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.UpdateItemRequest;
+
+/** TODO. */
+@PublicEvolving
+public interface DynamoDbProducer {
+
+    /** Tear-down the producer. */
+    void close() throws Exception;
+
+    /** Get outstanding records in the in-memory queue. */
+    long getOutstandingRecordsCount();
+
+    /** Flush outstanding records in the producer. */
+    void flush() throws Exception;
+
+    /** Produce to DynamoDb. */
+    void produce(PutItemRequest request);
+
+    /** Produce to DynamoDb. */
+    void produce(DeleteItemRequest request);
+
+    /** Produce to DynamoDb. */
+    void produce(UpdateItemRequest request);
+}
