59578126533484923effd91e2b48a39dda6ffc1a
Jingsong Lee <jingsonglee0@gmail.com>
2021-06-21 15:30:32 +0800

    [FLINK-22901][table] Introduce getUpsertKeys in FlinkRelMetadataQuery
    This closes #16096

--- flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/plan/metadata/FlinkDefaultRelMetadataProvider.scala
+++ flink-table/flink-table-planner-blink/src/main/scala/org/apache/flink/table/planner/plan/metadata/FlinkDefaultRelMetadataProvider.scala
@@ -40,6 +40,7 @@ object FlinkDefaultRelMetadataProvider {
       FlinkRelMdPopulationSize.SOURCE,
       FlinkRelMdColumnUniqueness.SOURCE,
       FlinkRelMdUniqueKeys.SOURCE,
+      FlinkRelMdUpsertKeys.SOURCE,
       FlinkRelMdUniqueGroups.SOURCE,
       FlinkRelMdModifiedMonotonicity.SOURCE,
       RelMdColumnOrigins.SOURCE,
