a1d1c5c5bf3666fde85456d57e2ebcc440645af5
Till Rohrmann <trohrmann@apache.org>
2021-07-01 14:43:17 +0200

    [hotfix][test] Replace junit.Assert.assertThat with hamcrest.MatcherAssert.assertThat in RemoteAkkaRpcServiceTest

--- flink-rpc/flink-rpc-akka/src/test/java/org/apache/flink/runtime/rpc/akka/RemoteAkkaRpcActorTest.java
+++ flink-rpc/flink-rpc-akka/src/test/java/org/apache/flink/runtime/rpc/akka/RemoteAkkaRpcActorTest.java
@@ -35,10 +35,10 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 /** Tests for remote AkkaRpcActors. */
