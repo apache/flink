0ff6ceb39e04ab0a750c63a41a8171b4f84498d3
Roman Khachatryan <khachatryan.roman@gmail.com>
2021-07-12 19:44:56 +0200

    [FLINK-21353][state/changelog] Implement batching

--- flink-dstl/flink-dstl-dfs/src/main/java/org/apache/flink/changelog/fs/FsStateChangelogOptions.java
+++ flink-dstl/flink-dstl-dfs/src/main/java/org/apache/flink/changelog/fs/FsStateChangelogOptions.java
@@ -22,19 +22,24 @@ import org.apache.flink.configuration.ConfigOption;
 import org.apache.flink.configuration.ConfigOptions;
 import org.apache.flink.configuration.MemorySize;
 
+import java.time.Duration;
+
 /** {@link ConfigOptions} for {@link FsStateChangelogStorage}. */
 @Experimental
 public class FsStateChangelogOptions {
+
     public static final ConfigOption<String> BASE_PATH =
             ConfigOptions.key("dstl.dfs.base-path")
                     .stringType()
                     .noDefaultValue()
                     .withDescription("Base path to store changelog files.");
+
     public static final ConfigOption<Boolean> COMPRESSION_ENABLED =
             ConfigOptions.key("dstl.dfs.compression.enabled")
                     .booleanType()
                     .defaultValue(false)
                     .withDescription("Whether to enable compression when serializing changelog.");
+
     public static final ConfigOption<MemorySize> PREEMPTIVE_PERSIST_THRESHOLD =
             ConfigOptions.key("dstl.dfs.preemptive-persist-threshold")
                     .memoryType()
@@ -44,9 +49,47 @@ public class FsStateChangelogOptions {
                                     + "beyond which they are persisted pre-emptively without waiting for a checkpoint. "
                                     + " Improves checkpointing time by allowing quasi-continuous uploading of state changes "
                                     + "(as opposed to uploading all accumulated changes on checkpoint).");
+
+    public static final ConfigOption<Duration> PERSIST_DELAY =
+            ConfigOptions.key("dstl.dfs.batch.persist-delay")
+                    .durationType()
+                    .defaultValue(Duration.ofMillis(10))
+                    .withDescription(
+                            "Delay before persisting changelog after receiving persist request (on checkpoint). "
+                                    + "Minimizes the number of files and requests "
+                                    + "if multiple operators (backends) or sub-tasks are using the same store. "
+                                    + "Correspondingly increases checkpoint time (async phase).");
+
+    public static final ConfigOption<MemorySize> PERSIST_SIZE_THRESHOLD =
+            ConfigOptions.key("dstl.dfs.batch.persist-size-threshold")
+                    .memoryType()
+                    .defaultValue(MemorySize.parse("10Mb"))
+                    .withDescription(
+                            "Size threshold for state changes that were requested to be persisted but are waiting for "
+                                    + PERSIST_DELAY.key()
+                                    + " (from all operators). "
+                                    + ". Once reached, accumulated changes are persisted immediately. "
+                                    + "This is different from "
+                                    + PREEMPTIVE_PERSIST_THRESHOLD.key()
+                                    + " as it happens AFTER the checkpoint and potentially for state changes of multiple operators.");
+
     public static final ConfigOption<MemorySize> UPLOAD_BUFFER_SIZE =
             ConfigOptions.key("dstl.dfs.upload.buffer-size")
                     .memoryType()
                     .defaultValue(MemorySize.parse("1Mb"))
                     .withDescription("Buffer size used when uploading change sets");
+
+    public static final ConfigOption<Integer> NUM_UPLOAD_THREADS =
+            ConfigOptions.key("dstl.dfs.upload.num-threads")
+                    .intType()
+                    .defaultValue(5)
+                    .withDescription("Number of threads to use for upload.");
+
+    public static final ConfigOption<MemorySize> IN_FLIGHT_DATA_LIMIT =
+            ConfigOptions.key("dstl.dfs.upload.max-in-flight")
+                    .memoryType()
+                    .defaultValue(MemorySize.parse("100Mb"))
+                    .withDescription(
+                            "Max amount of data allowed to be in-flight. "
+                                    + "Upon reaching this limit the task will fail");
 }
