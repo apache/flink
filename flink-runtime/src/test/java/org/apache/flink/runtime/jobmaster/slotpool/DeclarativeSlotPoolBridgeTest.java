2239df62bc3a08a7cf8c2915030d05ea293491ea
Till Rohrmann <trohrmann@apache.org>
2021-07-13 16:52:25 +0200

    [hotfix][tests] Use MatcherAssert.assertThat in DeclarativeSlotPoolBridgeTest

--- flink-runtime/src/test/java/org/apache/flink/runtime/jobmaster/slotpool/DeclarativeSlotPoolBridgeTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/jobmaster/slotpool/DeclarativeSlotPoolBridgeTest.java
@@ -51,7 +51,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.fail;
 
 /** Tests for the {@link DeclarativeSlotPoolBridge}. */
