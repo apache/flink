c96ba4891fd01d91b9afc609a8cf88678351825d
zhao_wei_nan <326747337@qq.com>
2021-07-19 22:42:25 +0800

    [FLINK-20562]support ExplainDetails for EXPLAIN sytnax

--- flink-table/flink-table-planner/src/test/scala/org/apache/flink/table/api/TableEnvironmentTest.scala
+++ flink-table/flink-table-planner/src/test/scala/org/apache/flink/table/api/TableEnvironmentTest.scala
@@ -1412,28 +1412,6 @@ class TableEnvironmentTest {
   }
 
   @Test
-  def testTableExplain(): Unit = {
-    val createTableStmt =
-      """
-        |CREATE TABLE MyTable (
-        |  a bigint,
-        |  b int,
-        |  c varchar
-        |) with (
-        |  'connector' = 'COLLECTION',
-        |  'is-bounded' = 'false'
-        |)
-      """.stripMargin
-    val tableResult1 = tableEnv.executeSql(createTableStmt)
-    assertEquals(ResultKind.SUCCESS, tableResult1.getResultKind)
-
-    val actual = tableEnv.sqlQuery("select * from MyTable where a > 10")
-      .explain(ExplainDetail.CHANGELOG_MODE)
-    val expected = TableTestUtil.readFromResource("/explain/testExplainSqlWithSelect.out")
-    assertEquals(replaceStageId(expected), replaceStageId(actual))
-  }
-
-  @Test
   def testExecuteSqlWithExplainDetailsSelect(): Unit = {
     val createTableStmt =
       """
