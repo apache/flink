79a2e298151fe0ef04191ed4b8d5b16b3a1569e4
martijnvisser <martijn@2symbols.com>
2021-07-28 09:24:08 +0200

    [FLINK-19929] Upgrade Kinesis dependencies

--- flink-end-to-end-tests/test-scripts/test_sql_client.sh
+++ flink-end-to-end-tests/test-scripts/test_sql_client.sh
@@ -61,7 +61,8 @@ for SQL_JAR in $SQL_JARS_DIR/*.jar; do
         ! [[ $EXTRACTED_FILE = "$EXTRACTED_JAR/org/apache/avro"* ]] && \
         # Following required by amazon-kinesis-producer in flink-connector-kinesis
         ! [[ $EXTRACTED_FILE = "$EXTRACTED_JAR/amazon-kinesis-producer-native-binaries"* ]] && \
-        ! [[ $EXTRACTED_FILE = "$EXTRACTED_JAR/cacerts"* ]] ; then
+        ! [[ $EXTRACTED_FILE = "$EXTRACTED_JAR/cacerts"* ]] && \
+        ! [[ $EXTRACTED_FILE = "$EXTRACTED_JAR/google"* ]] ; then
       echo "Bad file in JAR: $EXTRACTED_FILE"
       exit 1
     fi
