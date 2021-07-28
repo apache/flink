791474be4c1f3199a77a72489d953b9b4195db53
Dawid Wysakowicz <dwysakowicz@apache.org>
2021-07-28 13:11:03 +0200

    [FLINK-23408] Add hasReceivedEndOfData to InputGates
    This commits introduces a method for checking if an InputGate received EndOfData on all underlying channels. It implements a similar logic to EndOfPartitionEvent and corresponding isFinished method.

--- flink-runtime/src/main/java/org/apache/flink/runtime/taskmanager/InputGateWithMetrics.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/taskmanager/InputGateWithMetrics.java
@@ -108,6 +108,11 @@ public class InputGateWithMetrics extends IndexedInputGate {
     }
 
     @Override
+    public boolean hasReceivedEndOfData() {
+        return inputGate.hasReceivedEndOfData();
+    }
+
+    @Override
     public void setup() throws IOException {
         inputGate.setup();
     }
