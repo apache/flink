14bdf8acfaaebbd4736a449a380e8c41569b0fb5
yulei0824 <yulei0824@163.com>
2021-08-04 15:01:23 +0800

    [FLINK-23539][metrics][influxdb] Filter '\n' in tags

--- flink-metrics/flink-metrics-influxdb/src/main/java/org/apache/flink/metrics/influxdb/MeasurementInfoProvider.java
+++ flink-metrics/flink-metrics-influxdb/src/main/java/org/apache/flink/metrics/influxdb/MeasurementInfoProvider.java
@@ -30,6 +30,7 @@ import java.util.regex.Pattern;
 
 class MeasurementInfoProvider implements MetricInfoProvider<MeasurementInfo> {
     @VisibleForTesting static final char SCOPE_SEPARATOR = '_';
+    private static final String POINT_DELIMITER = "\n";
 
     private static final CharacterFilter CHARACTER_FILTER =
             new CharacterFilter() {
@@ -53,7 +54,9 @@ class MeasurementInfoProvider implements MetricInfoProvider<MeasurementInfo> {
         Map<String, String> tags = new HashMap<>();
         for (Map.Entry<String, String> variable : group.getAllVariables().entrySet()) {
             String name = variable.getKey();
-            tags.put(name.substring(1, name.length() - 1), variable.getValue());
+            tags.put(
+                    normalize(name.substring(1, name.length() - 1)),
+                    normalize(variable.getValue()));
         }
         return tags;
     }
@@ -65,5 +68,9 @@ class MeasurementInfoProvider implements MetricInfoProvider<MeasurementInfo> {
     private static String getLogicalScope(MetricGroup group) {
         return ((FrontMetricGroup<AbstractMetricGroup<?>>) group)
                 .getLogicalScope(CHARACTER_FILTER, SCOPE_SEPARATOR);
+    }
+
+    private static String normalize(String value) {
+        return value.replace(POINT_DELIMITER, "");
     }
 }
