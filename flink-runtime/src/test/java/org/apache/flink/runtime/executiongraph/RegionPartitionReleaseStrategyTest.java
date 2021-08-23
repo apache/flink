5a6d9cda75e0c5f9ad8216c812ae51a855650c05
Thesharing <cyprestar@outlook.com>
2021-08-23 17:33:45 +0800

    [hotfix] Replace deprecated Assert.assertThat

--- flink-runtime/src/test/java/org/apache/flink/runtime/executiongraph/RegionPartitionReleaseStrategyTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/executiongraph/RegionPartitionReleaseStrategyTest.java
@@ -33,10 +33,10 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 
 /** Tests for {@link RegionPartitionReleaseStrategy}. */
 public class RegionPartitionReleaseStrategyTest extends TestLogger {
