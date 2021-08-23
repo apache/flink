fdc2fb26622254b57f37fcfd405db2cbe0c71213
Qingsheng Ren <renqschn@gmail.com>
2021-08-23 12:13:32 +0800

    [hotfix][connector/common] Expose splitFinishedHook in SplitFetcher for fine-grained testing
    (cherry picked from commit 754b744c2a3d4a15c2197a2df4f92c19bd3e33b7)

--- flink-connectors/flink-connector-base/src/test/java/org/apache/flink/connector/base/source/reader/fetcher/SplitFetcherTest.java
+++ flink-connectors/flink-connector-base/src/test/java/org/apache/flink/connector/base/source/reader/fetcher/SplitFetcherTest.java
@@ -179,7 +179,8 @@ public class SplitFetcherTest {
                         elementQueue,
                         new MockSplitReader(2, true),
                         ExceptionUtils::rethrow,
-                        () -> {});
+                        () -> {},
+                        (ignore) -> {});
 
         // Prepare the splits.
         List<MockSourceSplit> splits = new ArrayList<>();
@@ -268,7 +269,8 @@ public class SplitFetcherTest {
     private static <E> SplitFetcher<E, TestingSourceSplit> createFetcher(
             final SplitReader<E, TestingSourceSplit> reader,
             final FutureCompletingBlockingQueue<RecordsWithSplitIds<E>> queue) {
-        return new SplitFetcher<>(0, queue, reader, ExceptionUtils::rethrow, () -> {});
+        return new SplitFetcher<>(
+                0, queue, reader, ExceptionUtils::rethrow, () -> {}, (ignore) -> {});
     }
 
     private static <E> SplitFetcher<E, TestingSourceSplit> createFetcherWithSplit(
