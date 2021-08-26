cfe9519a8cf4220b48dc327c95d568dfb121a82f
Dian Fu <dianfu@apache.org>
2021-08-26 19:25:53 +0800

    [FLINK-23994][python] Handle properly for null values in ArrayDataSerializer and MapDataSerializer
    This closes #16998.

--- flink-python/src/main/java/org/apache/flink/table/runtime/typeutils/serializers/python/ArrayDataSerializer.java
+++ flink-python/src/main/java/org/apache/flink/table/runtime/typeutils/serializers/python/ArrayDataSerializer.java
@@ -54,12 +54,15 @@ public class ArrayDataSerializer
 
     private final int elementSize;
 
+    private final BinaryArrayWriter.NullSetter nullSetter;
+
     public ArrayDataSerializer(LogicalType eleType, TypeSerializer elementTypeSerializer) {
         super(eleType);
         this.elementType = eleType;
         this.elementTypeSerializer = elementTypeSerializer;
         this.elementSize = BinaryArrayData.calculateFixLengthPartSize(this.elementType);
         this.elementGetter = ArrayData.createElementGetter(elementType);
+        this.nullSetter = BinaryArrayWriter.createNullSetter(eleType);
     }
 
     @Override
@@ -99,7 +102,7 @@ public class ArrayDataSerializer
                 Object element = elementTypeSerializer.deserialize(source);
                 BinaryWriter.write(writer, i, element, elementType, elementTypeSerializer);
             } else {
-                writer.setNullAt(i);
+                nullSetter.setNull(writer, i);
             }
         }
         writer.complete();
