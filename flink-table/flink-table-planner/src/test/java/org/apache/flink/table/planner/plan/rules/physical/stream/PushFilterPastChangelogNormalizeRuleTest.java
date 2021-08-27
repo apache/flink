5c96e5b985ecce6b39bb3913470f11a8e3a2ad83
Ingo Bürk <ingo.buerk@tngtech.com>
2021-08-27 12:21:22 +0200

    [FLINK-23663][table-planner] Push primary key filters through ChangelogNormalize
    This closes #17011.

---
+++ flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/plan/rules/physical/stream/PushFilterPastChangelogNormalizeRuleTest.java
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.planner.plan.rules.physical.stream;
+
+import org.apache.flink.table.api.Schema;
+import org.apache.flink.table.api.TableConfig;
+import org.apache.flink.table.api.TableDescriptor;
+import org.apache.flink.table.connector.ChangelogMode;
+import org.apache.flink.table.planner.factories.TableFactoryHarness;
+import org.apache.flink.table.planner.utils.StreamTableTestUtil;
+import org.apache.flink.table.planner.utils.TableTestBase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.apache.flink.table.api.DataTypes.BIGINT;
+import static org.apache.flink.table.api.DataTypes.INT;
+import static org.apache.flink.table.api.DataTypes.STRING;
+
+/** Tests for {@link PushFilterPastChangelogNormalizeRule}. */
+public class PushFilterPastChangelogNormalizeRuleTest extends TableTestBase {
+
+    private StreamTableTestUtil util;
+
+    @Before
+    public void before() {
+        util = streamTestUtil(TableConfig.getDefault());
+    }
+
+    @Test
+    public void testWithSinglePrimaryKeyFilter() {
+        final TableDescriptor sourceDescriptor =
+                TableFactoryHarness.newBuilder()
+                        .schema(
+                                Schema.newBuilder()
+                                        .column("f0", STRING())
+                                        .column("f1", INT().notNull())
+                                        .primaryKey("f1")
+                                        .build())
+                        .unboundedScanSource(ChangelogMode.upsert())
+                        .build();
+
+        util.tableEnv().createTable("T", sourceDescriptor);
+        util.verifyRelPlan("SELECT * FROM T WHERE f1 < 1");
+    }
+
+    @Test
+    public void testWithMultipleFilters() {
+        final TableDescriptor sourceDescriptor =
+                TableFactoryHarness.newBuilder()
+                        .schema(
+                                Schema.newBuilder()
+                                        .column("f0", STRING())
+                                        .column("f1", INT().notNull())
+                                        .column("f2", STRING())
+                                        .primaryKey("f1")
+                                        .build())
+                        .unboundedScanSource(ChangelogMode.upsert())
+                        .build();
+
+        util.tableEnv().createTable("T", sourceDescriptor);
+
+        // Only the first filter (f1 < 10) can be pushed
+        util.verifyRelPlan(
+                "SELECT f1, SUM(f1) AS `sum` FROM T WHERE f1 < 10 AND (f1 > 3 OR f2 IS NULL) GROUP BY f1");
+    }
+
+    @Test
+    public void testWithMultiplePrimaryKeyColumns() {
+        final TableDescriptor sourceDescriptor =
+                TableFactoryHarness.newBuilder()
+                        .schema(
+                                Schema.newBuilder()
+                                        .column("f0", STRING())
+                                        .column("f1", INT().notNull())
+                                        .column("f2", BIGINT().notNull())
+                                        .primaryKey("f1", "f2")
+                                        .build())
+                        .unboundedScanSource(ChangelogMode.upsert())
+                        .build();
+
+        util.tableEnv().createTable("T", sourceDescriptor);
+        util.verifyRelPlan("SELECT f0, f1 FROM T WHERE (f1 < 1 OR f2 > 10) AND f0 IS NOT NULL");
+    }
+}
