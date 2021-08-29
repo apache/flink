68427b94e51ac683029dffff04cefd75b40d6817
kevin.cyj <kevin.cyj@alibaba-inc.com>
2021-08-29 12:21:54 +0800

    [FLINK-24035][network] Guarantee that the LocalBufferPool is initialized with one buffer
    Previously, The buffer listeners are not notified when the the local buffer pool receives available notification from the global pool. This may cause potential deadlock issue:
    
    1. A LocalBufferPool is created but there is no available buffers in the global NetworkBufferPool.
    2. The LocalBufferPool registers an available buffer listener to the global NetworkBufferPool.
    3. The BufferManager requests buffers from the LocalBufferPool but no buffer is available. As a result, it registers an available buffer listener to the LocalBufferPool.
    4. A buffer is recycled to the global NetworkBufferPool and the LocalBufferPool is notified about the available buffer.
    5. The LocalBufferPool requests the available buffer from the global NetworkBufferPool but the registered available buffer listener of BufferManager is not notified and it can never get a chance to be notified so deadlock occurs.
    
    This patch fixes this issue by guaranteeing that the LocalBufferPool is initialized with one buffer. With this piece of guaranteed buffer, the LocalBufferPool will be in the healthy buffer request and recycle loop. Note: We are not fixing this issue by notifying the buffer listeners when the local buffer pool receives available notification from the global pool because of the potential deadlock.

--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/buffer/LocalBufferPool.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/buffer/LocalBufferPool.java
@@ -27,6 +27,7 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.GuardedBy;
 
+import java.io.IOException;
 import java.util.ArrayDeque;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -132,7 +133,8 @@ class LocalBufferPool implements BufferPool {
      * @param networkBufferPool global network buffer pool to get buffers from
      * @param numberOfRequiredMemorySegments minimum number of network buffers
      */
-    LocalBufferPool(NetworkBufferPool networkBufferPool, int numberOfRequiredMemorySegments) {
+    LocalBufferPool(NetworkBufferPool networkBufferPool, int numberOfRequiredMemorySegments)
+            throws IOException {
         this(
                 networkBufferPool,
                 numberOfRequiredMemorySegments,
@@ -152,7 +154,8 @@ class LocalBufferPool implements BufferPool {
     LocalBufferPool(
             NetworkBufferPool networkBufferPool,
             int numberOfRequiredMemorySegments,
-            int maxNumberOfMemorySegments) {
+            int maxNumberOfMemorySegments)
+            throws Exception {
         this(
                 networkBufferPool,
                 numberOfRequiredMemorySegments,
@@ -176,7 +179,8 @@ class LocalBufferPool implements BufferPool {
             int numberOfRequiredMemorySegments,
             int maxNumberOfMemorySegments,
             int numberOfSubpartitions,
-            int maxBuffersPerChannel) {
+            int maxBuffersPerChannel)
+            throws IOException {
         checkArgument(
                 numberOfRequiredMemorySegments > 0,
                 "Required number of memory segments (%s) should be larger than 0.",
@@ -215,9 +219,13 @@ class LocalBufferPool implements BufferPool {
         // Lock is only taken, because #checkAvailability asserts it. It's a small penalty for
         // thread safety.
         synchronized (this.availableMemorySegments) {
-            if (checkAvailability()) {
-                availabilityHelper.resetAvailable();
-            }
+            // Make sure that this buffer pool always has one buffer on initialization. For input
+            // side, it guarantees that the buffer listeners can get floating buffers properly and
+            // no deadlock will occur (see FLINK-24035 for more information). For output side, it
+            // means all result partitions will be available and ready for output on initialization.
+            availableMemorySegments.add(networkBufferPool.requestMemorySegmentBlocking());
+            ++numberOfRequestedMemorySegments;
+            availabilityHelper.resetAvailable();
 
             checkConsistentAvailability();
         }
