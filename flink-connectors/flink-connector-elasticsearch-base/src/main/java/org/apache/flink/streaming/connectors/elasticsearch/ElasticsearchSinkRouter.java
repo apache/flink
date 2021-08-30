9365765f58ec7b6c7aac396f279e470b883c7eb2
Rion Williams <rionmonster@gmail.com>
2021-08-30 13:19:11 -0500

    [FLINK-23977] Updated documentation / comments for ElasticsearchSinkRouter to explain usage

--- flink-connectors/flink-connector-elasticsearch-base/src/main/java/org/apache/flink/streaming/connectors/elasticsearch/ElasticsearchSinkRouter.java
+++ flink-connectors/flink-connector-elasticsearch-base/src/main/java/org/apache/flink/streaming/connectors/elasticsearch/ElasticsearchSinkRouter.java
@@ -5,15 +5,16 @@ package org.apache.flink.streaming.connectors.elasticsearch;
  * be consumed by an {@link DynamicElasticsearchSink} instance.
  */
 public interface ElasticsearchSinkRouter<
-            ElementT, RouteT, SinkT extends ElasticsearchSinkBase<ElementT, ?>> {
+        ElementT, RouteT, SinkT extends ElasticsearchSinkBase<ElementT, ?>> {
     /**
-     * Generate a deterministically unique identifier from an {@link ElementT} instance, which
-     * is used to initialize and cache the {@link SinkT} defined in the router.
-     * */
+     * Generate a deterministically unique identifier from an {@link ElementT} instance, which is
+     * used to initialize and cache the {@link SinkT} defined in the router.
+     */
     RouteT getRoute(ElementT element);
+
     /**
      * Generate a {@link SinkT} instance from an {@link ElementT} instance that will associate all
      * Elasticsearch requests for the {@link RouteT} defined in the router.
-     * */
+     */
     SinkT createSink(RouteT cacheKey, ElementT element);
 }
