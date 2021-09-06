c0f2d568369a2bda6c3f56c890f3597424ac1551
Qingsheng Ren <renqschn@gmail.com>
2021-09-06 16:32:09 +0800

    [FLINK-22198][connector/kafka] Disable log deletion on Kafka broker in Kafka table connector tests

--- flink-connectors/flink-connector-kafka/src/test/java/org/apache/flink/streaming/connectors/kafka/table/KafkaTableITCase.java
+++ flink-connectors/flink-connector-kafka/src/test/java/org/apache/flink/streaming/connectors/kafka/table/KafkaTableITCase.java
@@ -25,6 +25,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.connectors.kafka.KafkaTestBase;
 import org.apache.flink.streaming.connectors.kafka.KafkaTestBaseWithFlink;
+import org.apache.flink.streaming.connectors.kafka.KafkaTestEnvironment;
 import org.apache.flink.streaming.connectors.kafka.partitioner.FlinkKafkaPartitioner;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.TableResult;
@@ -35,6 +36,7 @@ import org.apache.flink.test.util.SuccessException;
 import org.apache.flink.types.Row;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -48,6 +50,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -87,6 +90,22 @@ public class KafkaTableITCase extends KafkaTestBaseWithFlink {
 
     protected StreamExecutionEnvironment env;
     protected StreamTableEnvironment tEnv;
+
+    @BeforeClass
+    public static void prepare() throws Exception {
+        LOG.info("-------------------------------------------------------------------------");
+        LOG.info("    Starting KafkaTableITCase ");
+        LOG.info("-------------------------------------------------------------------------");
+
+        Properties serverProperties = new Properties();
+        serverProperties.put("log.retention.ms", Integer.toString(-1));
+        startClusters(
+                KafkaTestEnvironment.createConfig()
+                        .setKafkaServersNumber(NUMBER_OF_KAFKA_SERVERS)
+                        .setSecureMode(false)
+                        .setHideKafkaBehindProxy(false)
+                        .setKafkaServerProperties(serverProperties));
+    }
 
     @Before
     public void setup() {
