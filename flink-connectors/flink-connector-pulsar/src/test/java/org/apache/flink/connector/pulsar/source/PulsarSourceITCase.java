4e68c4559619676995984565315411af8598af2e
Yufan Sheng <yufan@streamnative.io>
2021-09-08 23:13:06 +0800

    [FLINK-23944][connector/pulsar] Enable PulsarSourceITCase.testTaskManagerFailure after test framework was fixed.
    This closes #17201

--- flink-connectors/flink-connector-pulsar/src/test/java/org/apache/flink/connector/pulsar/source/PulsarSourceITCase.java
+++ flink-connectors/flink-connector-pulsar/src/test/java/org/apache/flink/connector/pulsar/source/PulsarSourceITCase.java
@@ -23,16 +23,11 @@ import org.apache.flink.connector.pulsar.testutils.PulsarTestEnvironment;
 import org.apache.flink.connector.pulsar.testutils.cases.MultipleTopicConsumingContext;
 import org.apache.flink.connector.pulsar.testutils.cases.SingleTopicConsumingContext;
 import org.apache.flink.connector.pulsar.testutils.runtime.PulsarRuntime;
-import org.apache.flink.connectors.test.common.environment.ClusterControllable;
 import org.apache.flink.connectors.test.common.environment.MiniClusterTestEnvironment;
-import org.apache.flink.connectors.test.common.environment.TestEnvironment;
-import org.apache.flink.connectors.test.common.external.ExternalContext;
 import org.apache.flink.connectors.test.common.junit.annotations.ExternalContextFactory;
 import org.apache.flink.connectors.test.common.junit.annotations.ExternalSystem;
 import org.apache.flink.connectors.test.common.junit.annotations.TestEnv;
 import org.apache.flink.connectors.test.common.testsuites.SourceTestSuiteBase;
-
-import org.junit.jupiter.api.Disabled;
 
 /** Unite test class for {@link PulsarSource}. */
 @SuppressWarnings("unused")
@@ -53,14 +48,4 @@ class PulsarSourceITCase extends SourceTestSuiteBase<String> {
     @ExternalContextFactory
     PulsarTestContextFactory<String, MultipleTopicConsumingContext> multipleTopic =
             new PulsarTestContextFactory<>(pulsar, MultipleTopicConsumingContext::new);
-
-    @Disabled
-    @Override
-    public void testTaskManagerFailure(
-            TestEnvironment testEnv,
-            ExternalContext<String> externalContext,
-            ClusterControllable controller)
-            throws Exception {
-        super.testTaskManagerFailure(testEnv, externalContext, controller);
-    }
 }
