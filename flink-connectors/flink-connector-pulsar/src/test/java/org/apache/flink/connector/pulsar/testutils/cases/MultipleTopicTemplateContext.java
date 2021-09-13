2b5acc096443e48a72afadcd180bfe62c1ea6f2f
syhily <syhily@gmail.com>
2021-09-13 21:52:31 +0800

    [FLINK-23969][connector/pulsar] Create e2e tests for pulsar connector.

--- flink-connectors/flink-connector-pulsar/src/test/java/org/apache/flink/connector/pulsar/testutils/cases/MultipleTopicTemplateContext.java
+++ flink-connectors/flink-connector-pulsar/src/test/java/org/apache/flink/connector/pulsar/testutils/cases/MultipleTopicTemplateContext.java
@@ -29,38 +29,33 @@ import org.apache.flink.connector.pulsar.testutils.PulsarTestContext;
 import org.apache.flink.connector.pulsar.testutils.PulsarTestEnvironment;
 import org.apache.flink.connectors.test.common.external.SourceSplitDataWriter;
 
+import org.apache.pulsar.client.api.RegexSubscriptionMode;
+import org.apache.pulsar.client.api.SubscriptionType;
+
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ThreadLocalRandom;
 
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.apache.flink.connector.pulsar.source.reader.deserializer.PulsarDeserializationSchema.pulsarSchema;
 import static org.apache.pulsar.client.api.Schema.STRING;
-import static org.apache.pulsar.client.api.SubscriptionType.Exclusive;
 
 /**
- * A Pulsar external context that will create only one topic and use partitions in that topic as
+ * Pulsar external context template that will create multiple topics with only one partitions as
  * source splits.
  */
-public class SingleTopicConsumingContext extends PulsarTestContext<String> {
-    private static final long serialVersionUID = 2754642285356345741L;
+public abstract class MultipleTopicTemplateContext extends PulsarTestContext<String> {
+    private static final long serialVersionUID = 7333807392445848344L;
 
-    private static final String TOPIC_NAME_PREFIX = "pulsar-single-topic";
-    private final String topicName;
-    private final Map<Integer, SourceSplitDataWriter<String>> partitionToSplitWriter =
+    private int numTopics = 0;
+
+    private final String topicPattern = "pulsar-multiple-topic-[0-9]+-" + randomAlphabetic(8);
+
+    private final Map<String, SourceSplitDataWriter<String>> topicNameToSplitWriters =
             new HashMap<>();
 
-    private int numSplits = 0;
-
-    public SingleTopicConsumingContext(PulsarTestEnvironment environment) {
+    public MultipleTopicTemplateContext(PulsarTestEnvironment environment) {
         super(environment);
-        this.topicName =
-                TOPIC_NAME_PREFIX + "-" + ThreadLocalRandom.current().nextLong(Long.MAX_VALUE);
-    }
-
-    @Override
-    protected String displayName() {
-        return "consuming message on single topic";
     }
 
     @Override
@@ -68,11 +63,11 @@ public class SingleTopicConsumingContext extends PulsarTestContext<String> {
         PulsarSourceBuilder<String> builder =
                 PulsarSource.builder()
                         .setDeserializationSchema(pulsarSchema(STRING))
-                        .setServiceUrl(operator.serviceUrl())
-                        .setAdminUrl(operator.adminUrl())
-                        .setTopics(topicName)
-                        .setSubscriptionType(Exclusive)
-                        .setSubscriptionName("pulsar-single-topic");
+                        .setServiceUrl(serviceUrl())
+                        .setAdminUrl(adminUrl())
+                        .setTopicPattern(topicPattern, RegexSubscriptionMode.AllTopics)
+                        .setSubscriptionType(subscriptionType())
+                        .setSubscriptionName(subscriptionName());
         if (boundedness == Boundedness.BOUNDED) {
             // Using latest stop cursor for making sure the source could be stopped.
             // This is required for SourceTestSuiteBase.
@@ -84,18 +79,14 @@ public class SingleTopicConsumingContext extends PulsarTestContext<String> {
 
     @Override
     public SourceSplitDataWriter<String> createSourceSplitDataWriter() {
-        if (numSplits == 0) {
-            // Create the topic first.
-            operator.createTopic(topicName, 1);
-            numSplits++;
-        } else {
-            numSplits++;
-            operator.increaseTopicPartitions(topicName, numSplits);
-        }
+        String topicName = topicPattern.replace("[0-9]+", String.valueOf(numTopics));
+        operator.createTopic(topicName, 1);
 
-        String partitionName = TopicNameUtils.topicNameWithPartition(topicName, numSplits - 1);
+        String partitionName = TopicNameUtils.topicNameWithPartition(topicName, 0);
         PulsarPartitionDataWriter writer = new PulsarPartitionDataWriter(operator, partitionName);
-        partitionToSplitWriter.put(numSplits - 1, writer);
+
+        topicNameToSplitWriters.put(partitionName, writer);
+        numTopics++;
 
         return writer;
     }
@@ -107,11 +98,22 @@ public class SingleTopicConsumingContext extends PulsarTestContext<String> {
 
     @Override
     public void close() throws Exception {
-        // Close writer.
-        for (SourceSplitDataWriter<String> writer : partitionToSplitWriter.values()) {
+        for (SourceSplitDataWriter<String> writer : topicNameToSplitWriters.values()) {
             writer.close();
         }
 
-        partitionToSplitWriter.clear();
+        topicNameToSplitWriters.clear();
+    }
+
+    protected abstract String subscriptionName();
+
+    protected abstract SubscriptionType subscriptionType();
+
+    protected String serviceUrl() {
+        return operator.serviceUrl();
+    }
+
+    protected String adminUrl() {
+        return operator.adminUrl();
     }
 }
--- flink-end-to-end-tests/flink-end-to-end-tests-pulsar/src/test/java/org/apache/flink/tests/util/pulsar/common/KeyedPulsarPartitionDataWriter.java
+++ flink-end-to-end-tests/flink-end-to-end-tests-pulsar/src/test/java/org/apache/flink/tests/util/pulsar/common/KeyedPulsarPartitionDataWriter.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.apache.flink.connector.pulsar.testutils;
+package org.apache.flink.tests.util.pulsar.common;
 
 import org.apache.flink.connector.pulsar.testutils.runtime.PulsarRuntimeOperator;
 import org.apache.flink.connectors.test.common.external.SourceSplitDataWriter;
@@ -24,21 +24,35 @@ import org.apache.flink.connectors.test.common.external.SourceSplitDataWriter;
 import org.apache.pulsar.client.api.Schema;
 
 import java.util.Collection;
+import java.util.List;
 
-/** Source split data writer for writing test data into a Pulsar topic partition. */
-public class PulsarPartitionDataWriter implements SourceSplitDataWriter<String> {
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Source split data writer for writing test data into a Pulsar topic partition. It will write the
+ * message with two keys.
+ */
+public class KeyedPulsarPartitionDataWriter implements SourceSplitDataWriter<String> {
 
     private final PulsarRuntimeOperator operator;
     private final String fullTopicName;
+    private final String key1;
+    private final String key2;
 
-    public PulsarPartitionDataWriter(PulsarRuntimeOperator operator, String fullTopicName) {
+    public KeyedPulsarPartitionDataWriter(
+            PulsarRuntimeOperator operator, String fullTopicName, String key1, String key2) {
         this.operator = operator;
         this.fullTopicName = fullTopicName;
+        this.key1 = key1;
+        this.key2 = key2;
     }
 
     @Override
     public void writeRecords(Collection<String> records) {
-        operator.sendMessages(fullTopicName, Schema.STRING, records);
+        operator.sendMessages(fullTopicName, Schema.STRING, key1, records);
+
+        List<String> newRecords = records.stream().map(a -> a + key1).collect(toList());
+        operator.sendMessages(fullTopicName, Schema.STRING, key2, newRecords);
     }
 
     @Override
