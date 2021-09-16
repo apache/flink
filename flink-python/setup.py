050f025f5857bce608545c9bf5148ab01511346c
huangxingbo <hxbks2ks@gmail.com>
2021-09-16 15:22:11 +0800

    [FLINK-24305][python] Limit the protobuf version<3.18
    This closes #17298.

--- flink-python/setup.py
+++ flink-python/setup.py
@@ -312,7 +312,7 @@ try:
                           'cloudpickle==1.2.2', 'avro-python3>=1.8.1,!=1.9.2,<1.10.0',
                           'pandas>=1.0,<1.2.0', 'pyarrow>=0.15.1,<3.0.0',
                           'pytz>=2018.3', 'numpy>=1.14.3,<1.20', 'fastavro>=0.21.4,<0.24',
-                          'requests>=2.26.0',
+                          'requests>=2.26.0', 'protobuf<3.18',
                           apache_flink_libraries_dependency],
         cmdclass={'build_ext': build_ext},
         tests_require=['pytest==4.4.1'],
