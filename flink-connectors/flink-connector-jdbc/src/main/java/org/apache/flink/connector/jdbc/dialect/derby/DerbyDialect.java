412b49a0fa4756bc6a663782eec3252411bcbd5e
sjwiesman <sjwiesman@gmail.com>
2021-09-16 12:35:11 -0500

    [FLINK-24253][jdbc] move default implementations to AbstractDialect
    This closes #17264

--- flink-connectors/flink-connector-jdbc/src/main/java/org/apache/flink/connector/jdbc/dialect/derby/DerbyDialect.java
+++ flink-connectors/flink-connector-jdbc/src/main/java/org/apache/flink/connector/jdbc/dialect/derby/DerbyDialect.java
@@ -24,9 +24,9 @@ import org.apache.flink.connector.jdbc.internal.converter.DerbyRowConverter;
 import org.apache.flink.table.types.logical.LogicalTypeRoot;
 import org.apache.flink.table.types.logical.RowType;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.EnumSet;
 import java.util.Optional;
+import java.util.Set;
 
 class DerbyDialect extends AbstractDialect {
 
@@ -68,48 +68,43 @@ class DerbyDialect extends AbstractDialect {
     }
 
     @Override
-    public int maxDecimalPrecision() {
-        return MAX_DECIMAL_PRECISION;
+    public Optional<String> getUpsertStatement(
+            String tableName, String[] fieldNames, String[] uniqueKeyFields) {
+        return Optional.empty();
     }
 
     @Override
-    public int minDecimalPrecision() {
-        return MIN_DECIMAL_PRECISION;
+    public Optional<Range> decimalPrecisionRange() {
+        return Optional.of(Range.of(MIN_DECIMAL_PRECISION, MAX_DECIMAL_PRECISION));
     }
 
     @Override
-    public int maxTimestampPrecision() {
-        return MAX_TIMESTAMP_PRECISION;
+    public Optional<Range> timestampPrecisionRange() {
+        return Optional.of(Range.of(MIN_TIMESTAMP_PRECISION, MAX_TIMESTAMP_PRECISION));
     }
 
     @Override
-    public int minTimestampPrecision() {
-        return MIN_TIMESTAMP_PRECISION;
-    }
-
-    @Override
-    public List<LogicalTypeRoot> unsupportedTypes() {
+    public Set<LogicalTypeRoot> supportedTypes() {
         // The data types used in Derby are list at
         // http://db.apache.org/derby/docs/10.14/ref/crefsqlj31068.html
 
         // TODO: We can't convert BINARY data type to
         //  PrimitiveArrayTypeInfo.BYTE_PRIMITIVE_ARRAY_TYPE_INFO in
         // LegacyTypeInfoDataTypeConverter.
-        return Arrays.asList(
-                LogicalTypeRoot.BINARY,
-                LogicalTypeRoot.TIMESTAMP_WITH_LOCAL_TIME_ZONE,
-                LogicalTypeRoot.TIMESTAMP_WITH_TIME_ZONE,
-                LogicalTypeRoot.INTERVAL_YEAR_MONTH,
-                LogicalTypeRoot.INTERVAL_DAY_TIME,
-                LogicalTypeRoot.ARRAY,
-                LogicalTypeRoot.MULTISET,
-                LogicalTypeRoot.MAP,
-                LogicalTypeRoot.ROW,
-                LogicalTypeRoot.DISTINCT_TYPE,
-                LogicalTypeRoot.STRUCTURED_TYPE,
-                LogicalTypeRoot.NULL,
-                LogicalTypeRoot.RAW,
-                LogicalTypeRoot.SYMBOL,
-                LogicalTypeRoot.UNRESOLVED);
+        return EnumSet.of(
+                LogicalTypeRoot.CHAR,
+                LogicalTypeRoot.VARCHAR,
+                LogicalTypeRoot.BOOLEAN,
+                LogicalTypeRoot.VARBINARY,
+                LogicalTypeRoot.DECIMAL,
+                LogicalTypeRoot.TINYINT,
+                LogicalTypeRoot.SMALLINT,
+                LogicalTypeRoot.INTEGER,
+                LogicalTypeRoot.BIGINT,
+                LogicalTypeRoot.FLOAT,
+                LogicalTypeRoot.DOUBLE,
+                LogicalTypeRoot.DATE,
+                LogicalTypeRoot.TIME_WITHOUT_TIME_ZONE,
+                LogicalTypeRoot.TIMESTAMP_WITHOUT_TIME_ZONE);
     }
 }
