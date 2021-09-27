c7865562890ec8e1b41e83dbb5b2463ff5bbf9c8
Nick <nick.burkard@gmail.com>
2021-09-27 10:16:16 -0400

    undo clojure changes, jepsen seems old & unused

--- flink-jepsen/src/jepsen/flink/flink.clj
+++ flink-jepsen/src/jepsen/flink/flink.clj
@@ -32,9 +32,9 @@
              [kafka :as kafka]
              [nemesis :as fn]]))
 
-(def default-flink-dist-url "https://archive.apache.org/dist/flink/flink-1.6.0/flink-1.6.0-bin-hadoop28-scala_2.12.tgz")
+(def default-flink-dist-url "https://archive.apache.org/dist/flink/flink-1.6.0/flink-1.6.0-bin-hadoop28-scala_2.11.tgz")
 (def hadoop-dist-url "https://archive.apache.org/dist/hadoop/common/hadoop-2.8.3/hadoop-2.8.3.tar.gz")
-(def kafka-dist-url "http://mirror.funkfreundelandshut.de/apache/kafka/2.2.2/kafka_2.12-2.2.2.tgz")
+(def kafka-dist-url "http://mirror.funkfreundelandshut.de/apache/kafka/2.2.2/kafka_2.11-2.2.2.tgz")
 (def deb-zookeeper-package "3.4.9-3+deb9u2")
 
 (def dbs
