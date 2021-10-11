fcac54ef9400ce97f10b1a1f190d523311ef9973
Dawid Wysakowicz <dwysakowicz@apache.org>
2021-10-11 09:55:54 +0200

    Regenerate migration files for ContinuousFileProcessingMigrationTest

--- flink-fs-tests/src/test/java/org/apache/flink/hdfstests/ContinuousFileProcessingMigrationTest.java
+++ flink-fs-tests/src/test/java/org/apache/flink/hdfstests/ContinuousFileProcessingMigrationTest.java
@@ -83,7 +83,8 @@ public class ContinuousFileProcessingMigrationTest {
                 Tuple2.of(MigrationVersion.v1_10, 1594559333000L),
                 Tuple2.of(MigrationVersion.v1_11, 1594561663000L),
                 Tuple2.of(MigrationVersion.v1_12, 1613720148000L),
-                Tuple2.of(MigrationVersion.v1_13, 1627550216000L));
+                Tuple2.of(MigrationVersion.v1_13, 1627550216000L),
+                Tuple2.of(MigrationVersion.v1_14, 1633938795000L));
     }
 
     /**
