798aed05ba32825187ff935843b01ad94ebd8234
lincoln lee <lincoln.86xy@gmail.com>
2021-10-28 15:52:48 +0800

    [FLINK-24654][table] Fix NPE on RetractableTopNFunction when some records were cleared by state ttl
    This closes #17570

--- flink-table/flink-table-runtime/src/main/java/org/apache/flink/table/runtime/operators/rank/RetractableTopNFunction.java
+++ flink-table/flink-table-runtime/src/main/java/org/apache/flink/table/runtime/operators/rank/RetractableTopNFunction.java
@@ -224,6 +224,18 @@ public class RetractableTopNFunction extends AbstractTopNFunction {
 
     // ------------- ROW_NUMBER-------------------------------
 
+    private void processStateStaled(Iterator<Map.Entry<RowData, Long>> sortedMapIterator)
+            throws RuntimeException {
+        // Skip the data if it's state is cleared because of state ttl.
+        if (lenient) {
+            // Sync with dataState
+            sortedMapIterator.remove();
+            LOG.warn(STATE_CLEARED_WARN_MSG);
+        } else {
+            throw new RuntimeException(STATE_CLEARED_WARN_MSG);
+        }
+    }
+
     private void emitRecordsWithRowNumber(
             SortedMap<RowData, Long> sortedMap,
             RowData sortKey,
@@ -244,12 +256,7 @@ public class RetractableTopNFunction extends AbstractTopNFunction {
             } else if (findsSortKey) {
                 List<RowData> inputs = dataState.get(key);
                 if (inputs == null) {
-                    // Skip the data if it's state is cleared because of state ttl.
-                    if (lenient) {
-                        LOG.warn(STATE_CLEARED_WARN_MSG);
-                    } else {
-                        throw new RuntimeException(STATE_CLEARED_WARN_MSG);
-                    }
+                    processStateStaled(iterator);
                 } else {
                     int i = 0;
                     while (i < inputs.size() && isInRankEnd(currentRank)) {
@@ -294,12 +301,7 @@ public class RetractableTopNFunction extends AbstractTopNFunction {
             } else if (findsSortKey) {
                 List<RowData> inputs = dataState.get(key);
                 if (inputs == null) {
-                    // Skip the data if it's state is cleared because of state ttl.
-                    if (lenient) {
-                        LOG.warn(STATE_CLEARED_WARN_MSG);
-                    } else {
-                        throw new RuntimeException(STATE_CLEARED_WARN_MSG);
-                    }
+                    processStateStaled(iterator);
                 } else {
                     long count = entry.getValue();
                     // gets the rank of last record with same sortKey
@@ -346,12 +348,7 @@ public class RetractableTopNFunction extends AbstractTopNFunction {
             if (!findsSortKey && key.equals(sortKey)) {
                 List<RowData> inputs = dataState.get(key);
                 if (inputs == null) {
-                    // Skip the data if it's state is cleared because of state ttl.
-                    if (lenient) {
-                        LOG.warn(STATE_CLEARED_WARN_MSG);
-                    } else {
-                        throw new RuntimeException(STATE_CLEARED_WARN_MSG);
-                    }
+                    processStateStaled(iterator);
                 } else {
                     Iterator<RowData> inputIter = inputs.iterator();
                     while (inputIter.hasNext() && isInRankEnd(currentRank)) {
@@ -375,14 +372,18 @@ public class RetractableTopNFunction extends AbstractTopNFunction {
                 }
             } else if (findsSortKey) {
                 List<RowData> inputs = dataState.get(key);
-                int i = 0;
-                while (i < inputs.size() && isInRankEnd(currentRank)) {
-                    RowData currentRow = inputs.get(i);
-                    collectUpdateBefore(out, prevRow, currentRank);
-                    collectUpdateAfter(out, currentRow, currentRank);
-                    prevRow = currentRow;
-                    currentRank += 1;
-                    i++;
+                if (inputs == null) {
+                    processStateStaled(iterator);
+                } else {
+                    int i = 0;
+                    while (i < inputs.size() && isInRankEnd(currentRank)) {
+                        RowData currentRow = inputs.get(i);
+                        collectUpdateBefore(out, prevRow, currentRank);
+                        collectUpdateAfter(out, currentRow, currentRank);
+                        prevRow = currentRow;
+                        currentRank += 1;
+                        i++;
+                    }
                 }
             } else {
                 currentRank += entry.getValue();
@@ -417,12 +418,7 @@ public class RetractableTopNFunction extends AbstractTopNFunction {
             if (!findsSortKey && key.equals(sortKey)) {
                 List<RowData> inputs = dataState.get(key);
                 if (inputs == null) {
-                    // Skip the data if it's state is cleared because of state ttl.
-                    if (lenient) {
-                        LOG.warn(STATE_CLEARED_WARN_MSG);
-                    } else {
-                        throw new RuntimeException(STATE_CLEARED_WARN_MSG);
-                    }
+                    processStateStaled(iterator);
                 } else {
                     Iterator<RowData> inputIter = inputs.iterator();
                     while (inputIter.hasNext() && isInRankEnd(nextRank)) {
@@ -455,9 +451,13 @@ public class RetractableTopNFunction extends AbstractTopNFunction {
                     // sends the record if there is a record recently upgrades to Top-N
                     int index = Long.valueOf(rankEnd - nextRank).intValue();
                     List<RowData> inputs = dataState.get(key);
-                    RowData toAdd = inputs.get(index);
-                    collectInsert(out, toAdd);
-                    break;
+                    if (inputs == null) {
+                        processStateStaled(iterator);
+                    } else {
+                        RowData toAdd = inputs.get(index);
+                        collectInsert(out, toAdd);
+                        break;
+                    }
                 }
             } else {
                 nextRank += entry.getValue();
