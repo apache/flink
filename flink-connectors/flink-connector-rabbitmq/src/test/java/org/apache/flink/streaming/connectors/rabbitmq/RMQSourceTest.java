c3683b982d7aebe2bbdd8cf8c0800fed0c180b61
Michal Ciesielczyk <michal.ciesielczyk@deep.bi>
2021-11-08 01:14:35 +0100

    [FLINK-23696][connectors/rabbitmq] Fix RMQSourceTest.testRedeliveredSessionIDsAck test

--- flink-connectors/flink-connector-rabbitmq/src/test/java/org/apache/flink/streaming/connectors/rabbitmq/RMQSourceTest.java
+++ flink-connectors/flink-connector-rabbitmq/src/test/java/org/apache/flink/streaming/connectors/rabbitmq/RMQSourceTest.java
@@ -116,6 +116,7 @@ public class RMQSourceTest {
         source.initializeState(mockContext);
         source.open(config);
 
+        DummySourceContext.numElementsCollected = 0;
         messageId = 0;
         generateCorrelationIds = true;
 
@@ -388,12 +389,14 @@ public class RMQSourceTest {
             testHarness.snapshot(snapshotId, System.currentTimeMillis());
             source.notifyCheckpointComplete(snapshotId);
             lastMessageId = messageId;
+
+            // check if all the messages are being collected and acknowledged
+            long totalNumberOfAcks = numMsgRedelivered + lastMessageId;
+            assertEquals(lastMessageId, DummySourceContext.numElementsCollected);
+            assertEquals(totalNumberOfAcks, ((RMQTestSource) source).addIdCalls);
         }
 
-        // check if all the messages are being acknowledged
-        long totalNumberOfAcks = numMsgRedelivered + lastMessageId;
-        assertEquals(lastMessageId, DummySourceContext.numElementsCollected);
-        assertEquals(totalNumberOfAcks, ((RMQTestSource) source).addIdCalls);
+        // check if all the acks are being sent
         Mockito.verify(source.channel, Mockito.times((int) lastMessageId))
                 .basicAck(Mockito.anyLong(), Mockito.eq(false));
         Mockito.verify(source.channel, Mockito.times((int) numMsgRedelivered))
