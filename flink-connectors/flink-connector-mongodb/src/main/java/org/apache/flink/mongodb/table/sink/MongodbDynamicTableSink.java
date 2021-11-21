0418859c5dd1fe37b750ea06b8a1a01cc831f0e2
shihong90 <2572805166@qq.com>
2021-11-21 22:42:34 +0800

    [FLINK-6573][Connectors/Common ] Flink MongoDB Connector

--- flink-connectors/flink-connector-mongodb/src/main/java/org/apache/flink/mongodb/table/sink/MongodbDynamicTableSink.java
+++ flink-connectors/flink-connector-mongodb/src/main/java/org/apache/flink/mongodb/table/sink/MongodbDynamicTableSink.java
@@ -16,13 +16,11 @@ public class MongodbDynamicTableSink implements DynamicTableSink {
 
     @Override
     public ChangelogMode getChangelogMode(ChangelogMode changelogMode) {
-        // I、-U、+U、D
         return ChangelogMode.insertOnly();
     }
 
     @Override
     public SinkRuntimeProvider getSinkRuntimeProvider(Context context) {
-        // 初始化数据结构转换器，可以将二进制的数据转换成flink可操作的Row
         DataStructureConverter converter = context.createDataStructureConverter(this.tableSchema.toRowDataType());
         return SinkFunctionProvider.of(new MongodbUpsertSinkFunction(this.mongodbSinkConf, this.tableSchema.getFieldNames(), converter));
     }
