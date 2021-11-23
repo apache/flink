45ac29fdd23a851dfcf47e70d9db5710a19d2af8
jakevin <30525741+jackwener@users.noreply.github.com>
2021-11-23 17:32:19 +0800

    add a space according to review

--- flink-streaming-scala/src/main/scala/org/apache/flink/streaming/api/scala/DataStream.scala
+++ flink-streaming-scala/src/main/scala/org/apache/flink/streaming/api/scala/DataStream.scala
@@ -141,7 +141,7 @@ class DataStream[T](stream: JavaStream[T]) {
       case ds: SingleOutputStreamOperator[T] => ds.setMaxParallelism(maxParallelism)
       case _ =>
         throw new UnsupportedOperationException("Operator " + stream + " cannot set the maximum" +
-                                                  "parallelism")
+                                                  " parallelism")
     }
 
     this
