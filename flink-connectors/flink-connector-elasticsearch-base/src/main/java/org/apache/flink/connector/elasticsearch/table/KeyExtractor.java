db10a536ed3ef6b258b788d22ca01af6fe5634e1
Fabian Paul <fabianpaul@ververica.com>
2021-11-26 11:42:36 +0100

    [FLINK-24596][core] Introduce SerializableFunction and unify usages

--- flink-connectors/flink-connector-elasticsearch-base/src/main/java/org/apache/flink/connector/elasticsearch/table/KeyExtractor.java
+++ flink-connectors/flink-connector-elasticsearch-base/src/main/java/org/apache/flink/connector/elasticsearch/table/KeyExtractor.java
@@ -23,6 +23,7 @@ import org.apache.flink.table.api.TableColumn;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.types.logical.DistinctType;
 import org.apache.flink.table.types.logical.LogicalType;
+import org.apache.flink.util.function.SerializableFunction;
 
 import java.io.Serializable;
 import java.time.Duration;
