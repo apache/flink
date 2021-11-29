f0318293cb6506897097c6d48abc5e546d469953
Till Rohrmann <trohrmann@apache.org>
2021-11-29 16:40:53 +0100

    [hotfix] Use Hamcrest assertThat in FileUtilsTest

--- flink-core/src/test/java/org/apache/flink/util/FileUtilsTest.java
+++ flink-core/src/test/java/org/apache/flink/util/FileUtilsTest.java
@@ -49,13 +49,13 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.junit.Assume.assumeFalse;
