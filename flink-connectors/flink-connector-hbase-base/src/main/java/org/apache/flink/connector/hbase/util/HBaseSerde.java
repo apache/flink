970c783aac9f0276544229ae6b5d321a595317b8
Bruce <wenjie.wang01@liulishuo.com>
2021-12-15 20:19:46 +0800

    [FLINK-25330][Connectors/HBase] delete all versions of hbase data when flink retracts data.

--- flink-connectors/flink-connector-hbase-base/src/main/java/org/apache/flink/connector/hbase/util/HBaseSerde.java
+++ flink-connectors/flink-connector-hbase-base/src/main/java/org/apache/flink/connector/hbase/util/HBaseSerde.java
@@ -174,7 +174,7 @@ public class HBaseSerde {
                 for (int q = 0; q < this.qualifiers[f].length; q++) {
                     // get quantifier key
                     byte[] qualifier = qualifiers[f][q];
-                    delete.addColumn(familyKey, qualifier);
+                    delete.addColumns(familyKey, qualifier);
                 }
             }
         }
