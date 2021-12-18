c87817ded7f92df4066277a0e944563315541c22
Yubin Li <lixin58688@163.com>
2021-12-18 14:13:20 +0800

    [FLINK-25369][table] CatalogManager provide right tables or views in given catalog database name

--- flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/catalog/CatalogITCase.java
+++ flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/catalog/CatalogITCase.java
@@ -19,11 +19,22 @@
 package org.apache.flink.table.planner.catalog;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Schema;
 import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.catalog.Catalog;
+import org.apache.flink.table.catalog.CatalogDatabaseImpl;
+import org.apache.flink.table.catalog.CatalogManager;
+import org.apache.flink.table.catalog.CatalogTable;
 import org.apache.flink.table.catalog.GenericInMemoryCatalog;
 import org.apache.flink.table.catalog.GenericInMemoryCatalogFactoryOptions;
+import org.apache.flink.table.catalog.ObjectPath;
+import org.apache.flink.table.catalog.exceptions.DatabaseAlreadyExistException;
+import org.apache.flink.table.catalog.exceptions.DatabaseNotExistException;
+import org.apache.flink.table.catalog.exceptions.TableAlreadyExistException;
+import org.apache.flink.table.utils.CatalogManagerMocks;
 import org.apache.flink.testutils.ClassLoaderUtils;
 import org.apache.flink.util.TemporaryClassLoaderContext;
 
@@ -32,7 +43,13 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -167,6 +184,52 @@ public class CatalogITCase {
 
             assertTrue(tableEnvironment.getCatalog("cat").isPresent());
         }
+    }
+
+    @Test
+    public void testGetTablesFromGivenCatalogDatabase()
+            throws TableAlreadyExistException, DatabaseNotExistException,
+                    DatabaseAlreadyExistException {
+        final Catalog c1 = new GenericInMemoryCatalog("c1", "default");
+        final Catalog c2 = new GenericInMemoryCatalog("c2", "d2");
+
+        final CatalogManager catalogManager =
+                CatalogManagerMocks.preparedCatalogManager().defaultCatalog("c2", c2).build();
+        catalogManager.registerCatalog("c1", c1);
+        c1.createDatabase(
+                "d1",
+                new CatalogDatabaseImpl(
+                        new HashMap<String, String>() {
+                            {
+                                put("k1", "v1");
+                            }
+                        },
+                        ""),
+                true);
+        final ObjectPath path1 = new ObjectPath("d1", "t1");
+        final ObjectPath path2 = new ObjectPath(catalogManager.getCurrentDatabase(), "t2");
+
+        final Schema tableSchema =
+                Schema.newBuilder()
+                        .column("a", DataTypes.BIGINT())
+                        .column("b", DataTypes.VARCHAR(Integer.MAX_VALUE))
+                        .column("c", DataTypes.INT())
+                        .column("d", DataTypes.VARCHAR(Integer.MAX_VALUE))
+                        .build();
+        Map<String, String> options = new HashMap<>();
+        options.put("connector", "COLLECTION");
+        final CatalogTable catalogTable =
+                CatalogTable.of(tableSchema, "", new ArrayList<>(), options);
+        c1.createTable(path1, catalogTable, true);
+        c2.createTable(path2, catalogTable, true);
+        assertEquals(catalogManager.getCurrentCatalog(), "c2");
+        assertEquals(catalogManager.getCurrentDatabase(), "d2");
+        Set<String> res = new HashSet<>();
+        res.add("t2");
+        assertEquals(catalogManager.listTables(), res);
+        res.clear();
+        res.add("t1");
+        assertEquals(catalogManager.listTables("c1", "d1"), res);
     }
 
     private TableEnvironment getTableEnvironment() {
