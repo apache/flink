cfeaff195dd19f7a9544f927e7f99764ebbe4431
Matthias Pohl <matthias@ververica.com>
2021-12-30 15:14:36 +0100

    [hotfix] Migrates ApplicationStatusTest to JUnit5 and AssertJ

--- flink-runtime/src/test/java/org/apache/flink/runtime/clusterframework/ApplicationStatusTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/clusterframework/ApplicationStatusTest.java
@@ -18,41 +18,39 @@
 
 package org.apache.flink.runtime.clusterframework;
 
-import org.apache.flink.util.TestLogger;
+import org.apache.flink.util.TestLoggerExtension;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /** Tests for the {@link ApplicationStatus}. */
-public class ApplicationStatusTest extends TestLogger {
+@ExtendWith(TestLoggerExtension.class)
+public class ApplicationStatusTest {
 
     private static final int SUCCESS_EXIT_CODE = 0;
 
     @Test
     public void succeededStatusMapsToSuccessExitCode() {
         int exitCode = ApplicationStatus.SUCCEEDED.processExitCode();
-        assertThat(exitCode, is(equalTo(SUCCESS_EXIT_CODE)));
+        assertThat(exitCode).isEqualTo(SUCCESS_EXIT_CODE);
     }
 
     @Test
     public void cancelledStatusMapsToSuccessExitCode() {
         int exitCode = ApplicationStatus.CANCELED.processExitCode();
-        assertThat(exitCode, is(equalTo(SUCCESS_EXIT_CODE)));
+        assertThat(exitCode).isEqualTo(SUCCESS_EXIT_CODE);
     }
 
     @Test
     public void notSucceededNorCancelledStatusMapsToNonSuccessExitCode() {
         Iterable<Integer> exitCodes = exitCodes(notSucceededNorCancelledStatus());
-        assertThat(exitCodes, not(contains(SUCCESS_EXIT_CODE)));
+        assertThat(exitCodes).doesNotContain(SUCCESS_EXIT_CODE);
     }
 
     private static Iterable<Integer> exitCodes(Iterable<ApplicationStatus> statuses) {
