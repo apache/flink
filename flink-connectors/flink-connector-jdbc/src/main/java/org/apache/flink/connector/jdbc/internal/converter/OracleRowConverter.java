4ce39084fb4cbb9d79739d485324bc032bb7e4d3
Joao Boto <jboto@idealista.com>
2021-12-30 20:04:15 +0100

    [FLINK-14100][connectors] Add Oracle dialect - Add timestamps

--- flink-connectors/flink-connector-jdbc/src/main/java/org/apache/flink/connector/jdbc/internal/converter/OracleRowConverter.java
+++ flink-connectors/flink-connector-jdbc/src/main/java/org/apache/flink/connector/jdbc/internal/converter/OracleRowConverter.java
@@ -18,6 +18,12 @@
 
 package org.apache.flink.connector.jdbc.internal.converter;
 
+import oracle.jdbc.internal.OracleClob;
+
+import oracle.sql.TIMESTAMPLTZ;
+
+import oracle.sql.TIMESTAMPTZ;
+
 import org.apache.flink.connector.jdbc.converter.AbstractJdbcRowConverter;
 import org.apache.flink.table.data.DecimalData;
 import org.apache.flink.table.data.StringData;
@@ -33,12 +39,17 @@ import oracle.sql.DATE;
 import oracle.sql.NUMBER;
 import oracle.sql.RAW;
 import oracle.sql.TIMESTAMP;
+import oracle.sql.CLOB;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.sql.Date;
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 /**
  * Runtime converter that responsible to convert between JDBC object and Flink internal object for
@@ -123,27 +134,50 @@ public class OracleRowConverter extends AbstractJdbcRowConverter {
                                         (((DATE) val).timeValue().toLocalTime().toNanoOfDay()
                                                 / 1_000_000L)
                                 : (int) (((Time) val).toLocalTime().toNanoOfDay() / 1_000_000L);
-            case TIMESTAMP_WITHOUT_TIME_ZONE:
-                return val ->
-                        val instanceof TIMESTAMP
-                                ? TimestampData.fromTimestamp(((TIMESTAMP) val).timestampValue())
-                                : TimestampData.fromTimestamp((Timestamp) val);
             case CHAR:
             case VARCHAR:
                 return val ->
                         (val instanceof CHAR)
                                 ? StringData.fromString(((CHAR) val).getString())
-                                : StringData.fromString((String) val);
+                                : (val instanceof OracleClob)
+                                    ? StringData.fromString(((OracleClob) val).stringValue())
+                                    : StringData.fromString((String) val);
             case BINARY:
             case VARBINARY:
+            case RAW:
                 return val -> (val instanceof RAW) ? ((RAW) val).getBytes() : val;
+            case TIMESTAMP_WITHOUT_TIME_ZONE:
+                return val ->
+                        val instanceof TIMESTAMP
+                                ? TimestampData.fromTimestamp(((TIMESTAMP) val).timestampValue())
+                                : TimestampData.fromTimestamp((Timestamp) val);
             case TIMESTAMP_WITH_TIME_ZONE:
+                return val -> {
+                    if (val instanceof TIMESTAMPTZ) {
+                        final TIMESTAMPTZ ts = (TIMESTAMPTZ) val;
+                        final ZonedDateTime zdt = ZonedDateTime
+                                .ofInstant(ts.timestampValue().toInstant(), ts.getTimeZone().toZoneId());
+                        return TimestampData.fromLocalDateTime(zdt.toLocalDateTime());
+                    } else {
+                        return TimestampData.fromTimestamp((Timestamp) val);
+                    }
+                };
             case TIMESTAMP_WITH_LOCAL_TIME_ZONE:
+                return val -> {
+                    if (val instanceof TIMESTAMPLTZ) {
+                        final TIMESTAMPLTZ ts = (TIMESTAMPLTZ) val;
+                        final ZonedDateTime zdt = ZonedDateTime
+                                .ofInstant(ts.timestampValue().toInstant(), ZoneId.systemDefault())
+                                .withZoneSameInstant(ZoneOffset.UTC);
+                        return TimestampData.fromLocalDateTime(zdt.toLocalDateTime());
+                    } else {
+                        return TimestampData.fromTimestamp((Timestamp) val);
+                    }
+                };
             case ARRAY:
             case ROW:
             case MAP:
             case MULTISET:
-            case RAW:
             default:
                 return super.createInternalConverter(type);
         }
