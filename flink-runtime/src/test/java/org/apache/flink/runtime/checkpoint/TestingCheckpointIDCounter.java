440f93fb4599bf8b564544d6590bb7af12d7b9e6
Matthias Pohl <matthias@ververica.com>
2022-01-13 13:40:59 +0100

    [FLINK-25955][runtime] Makes TestingCheckpointIDCounter and TestingCompletedCheckpointStore more generic
    We need to extend the TestingCheckpointIDCounter to enable
    triggering an Exception in the shutdown process on execution.
    This selective requirement could have been made with less changes.
    Instead, I decided to provide the most generic Testing*
    implementation offering a proper TestingCheckpointIDCounter
    implementation which makes it more future-proof.

--- flink-runtime/src/test/java/org/apache/flink/runtime/checkpoint/TestingCheckpointIDCounter.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/checkpoint/TestingCheckpointIDCounter.java
@@ -20,36 +20,109 @@ package org.apache.flink.runtime.checkpoint;
 import org.apache.flink.api.common.JobStatus;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /** Test {@link CheckpointIDCounter} implementation for testing the shutdown behavior. */
 public final class TestingCheckpointIDCounter implements CheckpointIDCounter {
 
-    private final CompletableFuture<JobStatus> shutdownStatus;
+    private final Runnable startRunnable;
+    private final Consumer<JobStatus> shutdownConsumer;
+    private final Supplier<Integer> getAndIncrementSupplier;
+    private final Supplier<Integer> getSupplier;
+    private final Consumer<Long> setCountConsumer;
 
-    public TestingCheckpointIDCounter(CompletableFuture<JobStatus> shutdownStatus) {
-        this.shutdownStatus = shutdownStatus;
+    public static TestingCheckpointIDCounter createStoreWithShutdownCheckAndNoStartAction(
+            CompletableFuture<JobStatus> shutdownFuture) {
+        return TestingCheckpointIDCounter.builder()
+                .withStartRunnable(() -> {})
+                .withShutdownConsumer(shutdownFuture::complete)
+                .build();
+    }
+
+    private TestingCheckpointIDCounter(
+            Runnable startRunnable,
+            Consumer<JobStatus> shutdownConsumer,
+            Supplier<Integer> getAndIncrementSupplier,
+            Supplier<Integer> getSupplier,
+            Consumer<Long> setCountConsumer) {
+        this.startRunnable = startRunnable;
+        this.shutdownConsumer = shutdownConsumer;
+        this.getAndIncrementSupplier = getAndIncrementSupplier;
+        this.getSupplier = getSupplier;
+        this.setCountConsumer = setCountConsumer;
     }
 
     @Override
-    public void start() {}
+    public void start() {
+        startRunnable.run();
+    }
 
     @Override
     public void shutdown(JobStatus jobStatus) {
-        shutdownStatus.complete(jobStatus);
+        shutdownConsumer.accept(jobStatus);
     }
 
     @Override
     public long getAndIncrement() {
-        throw new UnsupportedOperationException("Not implemented.");
+        return getAndIncrementSupplier.get();
     }
 
     @Override
     public long get() {
-        throw new UnsupportedOperationException("Not implemented.");
+        return getSupplier.get();
     }
 
     @Override
     public void setCount(long newId) {
-        throw new UnsupportedOperationException("Not implemented.");
+        setCountConsumer.accept(newId);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /** {@code Builder} for creating {@code TestingCheckpointIDCounter} instances. */
+    public static class Builder {
+
+        private Runnable startRunnable;
+        private Consumer<JobStatus> shutdownConsumer;
+        private Supplier<Integer> getAndIncrementSupplier;
+        private Supplier<Integer> getSupplier;
+        private Consumer<Long> setCountConsumer;
+
+        public Builder withStartRunnable(Runnable startRunnable) {
+            this.startRunnable = startRunnable;
+            return this;
+        }
+
+        public Builder withShutdownConsumer(Consumer<JobStatus> shutdownConsumer) {
+            this.shutdownConsumer = shutdownConsumer;
+            return this;
+        }
+
+        public Builder withGetAndIncrementSupplier(Supplier<Integer> getAndIncrementSupplier) {
+            this.getAndIncrementSupplier = getAndIncrementSupplier;
+            return this;
+        }
+
+        public Builder withGetSupplier(Supplier<Integer> getSupplier) {
+            this.getSupplier = getSupplier;
+            return this;
+        }
+
+        public Builder withSetCountConsumer(Consumer<Long> setCountConsumer) {
+            this.setCountConsumer = setCountConsumer;
+            return this;
+        }
+
+        public TestingCheckpointIDCounter build() {
+            return new TestingCheckpointIDCounter(
+                    startRunnable,
+                    shutdownConsumer,
+                    getAndIncrementSupplier,
+                    getSupplier,
+                    setCountConsumer);
+        }
     }
 }
