f8d00a2f8acf62df41f181ac867e3a57820972eb
Yuxin Tan <tanyuxinwork@gmail.com>
2022-01-14 17:46:23 +0800

    [FLINK-25654][network] Remove the redundant locks in SortMergeResultPartition and PartitionSortedBuffer.
    After FLINK-2372, the task canceler will never call the close method of ResultPartition, which can reduce some race conditions and simplify the code. This PR aims to remove some redundant locks in SortMergeResultPartition and PartitionSortedBuffer.
    
    This closes #18364.

--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/SortMergeResultPartition.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/SortMergeResultPartition.java
@@ -78,10 +78,8 @@ public class SortMergeResultPartition extends ResultPartition {
     private PartitionedFile resultFile;
 
     /** Buffers cut from the network buffer pool for data writing. */
-    @GuardedBy("lock")
     private final List<MemorySegment> writeSegments = new ArrayList<>();
 
-    @GuardedBy("lock")
     private boolean hasNotifiedEndOfUserRecords;
 
     /** Size of network buffer and write buffer. */
@@ -173,16 +171,14 @@ public class SortMergeResultPartition extends ResultPartition {
         }
         numBuffersForSort = numRequiredBuffer - numWriteBuffers;
 
-        synchronized (lock) {
-            try {
-                for (int i = 0; i < numWriteBuffers; ++i) {
-                    MemorySegment segment = bufferPool.requestMemorySegmentBlocking();
-                    writeSegments.add(segment);
-                }
-            } catch (InterruptedException exception) {
-                // the setup method does not allow InterruptedException
-                throw new IOException(exception);
+        try {
+            for (int i = 0; i < numWriteBuffers; ++i) {
+                MemorySegment segment = bufferPool.requestMemorySegmentBlocking();
+                writeSegments.add(segment);
             }
+        } catch (InterruptedException exception) {
+            // the setup method does not allow InterruptedException
+            throw new IOException(exception);
         }
 
         LOG.info(
@@ -276,7 +272,6 @@ public class SortMergeResultPartition extends ResultPartition {
 
         unicastSortBuffer =
                 new PartitionSortedBuffer(
-                        lock,
                         bufferPool,
                         numSubpartitions,
                         networkBufferSize,
@@ -294,7 +289,6 @@ public class SortMergeResultPartition extends ResultPartition {
 
         broadcastSortBuffer =
                 new PartitionSortedBuffer(
-                        lock,
                         bufferPool,
                         numSubpartitions,
                         networkBufferSize,
@@ -343,10 +337,8 @@ public class SortMergeResultPartition extends ResultPartition {
     }
 
     private Queue<MemorySegment> getWriteSegments() {
-        synchronized (lock) {
-            checkState(!writeSegments.isEmpty(), "Task has been canceled.");
-            return new ArrayDeque<>(writeSegments);
-        }
+        checkState(!writeSegments.isEmpty(), "Task has been canceled.");
+        return new ArrayDeque<>(writeSegments);
     }
 
     private BufferWithChannel compressBufferIfPossible(BufferWithChannel bufferWithChannel) {
@@ -398,11 +390,9 @@ public class SortMergeResultPartition extends ResultPartition {
 
     @Override
     public void notifyEndOfData() throws IOException {
-        synchronized (lock) {
-            if (!hasNotifiedEndOfUserRecords) {
-                broadcastEvent(EndOfData.INSTANCE, false);
-                hasNotifiedEndOfUserRecords = true;
-            }
+        if (!hasNotifiedEndOfUserRecords) {
+            broadcastEvent(EndOfData.INSTANCE, false);
+            hasNotifiedEndOfUserRecords = true;
         }
     }
 
@@ -425,13 +415,11 @@ public class SortMergeResultPartition extends ResultPartition {
     }
 
     private void releaseWriteBuffers() {
-        synchronized (lock) {
-            if (bufferPool != null) {
-                for (MemorySegment segment : writeSegments) {
-                    bufferPool.recycle(segment);
-                }
-                writeSegments.clear();
+        if (bufferPool != null) {
+            for (MemorySegment segment : writeSegments) {
+                bufferPool.recycle(segment);
             }
+            writeSegments.clear();
         }
     }
 
