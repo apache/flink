5e680fa661f57a276e268ce9d189495b6700d1bc
Roman Khachatryan <khachatryan.roman@gmail.com>
2022-01-18 15:38:20 +0100

    [FLINK-25399][tests] Use FS-based changelog in PartiallyFinishedSourcesITCase

--- flink-tests/src/test/java/org/apache/flink/runtime/operators/lifecycle/PartiallyFinishedSourcesITCase.java
+++ flink-tests/src/test/java/org/apache/flink/runtime/operators/lifecycle/PartiallyFinishedSourcesITCase.java
@@ -47,7 +47,10 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static java.util.stream.StreamSupport.stream;
 import static org.apache.flink.api.common.restartstrategy.RestartStrategies.fixedDelayRestart;
+import static org.apache.flink.changelog.fs.FsStateChangelogOptions.BASE_PATH;
+import static org.apache.flink.changelog.fs.FsStateChangelogStorageFactory.IDENTIFIER;
 import static org.apache.flink.configuration.JobManagerOptions.EXECUTION_FAILOVER_STRATEGY;
+import static org.apache.flink.configuration.StateChangelogOptions.STATE_CHANGE_LOG_STORAGE;
 import static org.apache.flink.runtime.operators.lifecycle.command.TestCommand.FINISH_SOURCES;
 import static org.apache.flink.runtime.operators.lifecycle.command.TestCommandDispatcher.TestCommandScope.ALL_SUBTASKS;
 import static org.apache.flink.runtime.operators.lifecycle.command.TestCommandDispatcher.TestCommandScope.SINGLE_SUBTASK;
@@ -82,6 +85,14 @@ public class PartiallyFinishedSourcesITCase extends TestLogger {
         // - "region" is currently the default
         // - "full" is enforced by Adaptive/Reactive scheduler (even when parameterized)
         configuration.set(EXECUTION_FAILOVER_STRATEGY, failoverStrategy);
+
+        // If changelog backend is enabled then this test might run too slow with in-memory
+        // implementation - use fs-based instead.
+        // The randomization currently happens on the job level (environment); while this factory
+        // can only be set on the cluster level; so we do it unconditionally here.
+        configuration.setString(STATE_CHANGE_LOG_STORAGE, IDENTIFIER);
+        configuration.setString(BASE_PATH, TEMPORARY_FOLDER.newFolder().getAbsolutePath());
+
         miniClusterResource =
                 new MiniClusterWithClientResource(
                         new MiniClusterResourceConfiguration.Builder()
