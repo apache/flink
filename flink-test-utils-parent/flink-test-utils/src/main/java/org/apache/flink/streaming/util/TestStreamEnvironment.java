950ba3ef64504bf5bd295fa3ee0a921eb809a7a3
Roman Khachatryan <khachatryan.roman@gmail.com>
2022-01-28 00:53:06 +0100

    [FLINK-23559][tests] Randomize periodic materialisation interval in tests

--- flink-test-utils-parent/flink-test-utils/src/main/java/org/apache/flink/streaming/util/TestStreamEnvironment.java
+++ flink-test-utils-parent/flink-test-utils/src/main/java/org/apache/flink/streaming/util/TestStreamEnvironment.java
@@ -131,6 +131,15 @@ public class TestStreamEnvironment extends StreamExecutionEnvironment {
                 boolean enabled =
                         randomize(conf, StateChangelogOptions.ENABLE_STATE_CHANGE_LOG, true, false);
                 if (enabled) {
+                    randomize(
+                            conf,
+                            StateChangelogOptions.PERIODIC_MATERIALIZATION_INTERVAL,
+                            Duration.ofMillis(100),
+                            Duration.ofMillis(500),
+                            Duration.ofSeconds(1),
+                            Duration.ofSeconds(5),
+                            Duration.ofSeconds(
+                                    Long.MAX_VALUE / 1000 /* max allowed by Duration.toMillis */));
                     miniCluster.overrideRestoreModeForRandomizedChangelogStateBackend();
                 }
             }
