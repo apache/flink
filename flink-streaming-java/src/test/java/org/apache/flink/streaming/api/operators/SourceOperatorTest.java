6a96c10452c81a79f967b2a2607468b440a95c1a
Piotr Nowojski <piotr.nowojski@gmail.com>
2022-02-01 14:25:29 +0100

    [hotfix][test] Fix SourceOperatorTest#testSameAvailabilityFuture
    Previously this test was returning always CompletedFuture, which technically speaking
    should have been converted to AVAILABLE. And it also wasn't testing the important
    behaviour, that when being unavailable for long period of time, getAvailableFuture
    shouldn't be producing new CompletableFuture per each call.

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/api/operators/SourceOperatorTest.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/api/operators/SourceOperatorTest.java
@@ -22,7 +22,6 @@ import org.apache.flink.api.connector.source.SourceEvent;
 import org.apache.flink.api.connector.source.mocks.MockSourceReader;
 import org.apache.flink.api.connector.source.mocks.MockSourceSplit;
 import org.apache.flink.api.connector.source.mocks.MockSourceSplitSerializer;
-import org.apache.flink.runtime.io.AvailabilityProvider;
 import org.apache.flink.runtime.operators.coordination.MockOperatorEventGateway;
 import org.apache.flink.runtime.operators.coordination.OperatorEvent;
 import org.apache.flink.runtime.source.event.AddSplitEvent;
@@ -45,9 +44,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -194,13 +190,5 @@ public class SourceOperatorTest {
         operator.snapshotState(new StateSnapshotContextSynchronousImpl(100L, 100L));
         operator.notifyCheckpointAborted(100L);
         assertEquals(100L, (long) mockSourceReader.getAbortedCheckpoints().get(0));
-    }
-
-    @Test
-    public void testSameAvailabilityFuture() {
-        final CompletableFuture<?> initialFuture = operator.getAvailableFuture();
-        final CompletableFuture<?> secondFuture = operator.getAvailableFuture();
-        assertThat(initialFuture, not(sameInstance(AvailabilityProvider.AVAILABLE)));
-        assertThat(secondFuture, sameInstance(initialFuture));
     }
 }
