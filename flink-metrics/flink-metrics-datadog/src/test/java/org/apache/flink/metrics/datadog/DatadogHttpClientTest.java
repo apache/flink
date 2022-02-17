3fef5d05d3971a5ac0b0381975a54bcb6b8f1cb7
Chesnay Schepler <chesnay@apache.org>
2022-02-17 16:10:50 +0100

    [FLINK-26229][datadog] Migrate tests to JUnit5

--- flink-metrics/flink-metrics-datadog/src/test/java/org/apache/flink/metrics/datadog/DatadogHttpClientTest.java
+++ flink-metrics/flink-metrics-datadog/src/test/java/org/apache/flink/metrics/datadog/DatadogHttpClientTest.java
@@ -27,7 +27,7 @@ import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.MapperFeature;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.net.InetSocketAddress;
 import java.net.Proxy;
@@ -35,11 +35,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /** Tests for the DatadogHttpClient. */
 public class DatadogHttpClientTest {
@@ -59,14 +56,16 @@ public class DatadogHttpClientTest {
 
     private static final long MOCKED_SYSTEM_MILLIS = 123L;
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testClientWithEmptyKey() {
-        new DatadogHttpClient("", null, 123, DataCenter.US, false);
+        assertThatThrownBy(() -> new DatadogHttpClient("", null, 123, DataCenter.US, false))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testClientWithNullKey() {
-        new DatadogHttpClient(null, null, 123, DataCenter.US, false);
+        assertThatThrownBy(() -> new DatadogHttpClient(null, null, 123, DataCenter.US, false))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -81,12 +80,12 @@ public class DatadogHttpClientTest {
         DatadogHttpClient client =
                 new DatadogHttpClient("anApiKey", "localhost", 123, DataCenter.US, false);
 
-        assertTrue(client.getProxy().address() instanceof InetSocketAddress);
+        assertThat(client.getProxy().address()).isInstanceOf(InetSocketAddress.class);
 
         InetSocketAddress proxyAddress = (InetSocketAddress) client.getProxy().address();
 
-        assertEquals(123, proxyAddress.getPort());
-        assertEquals("localhost", proxyAddress.getHostString());
+        assertThat(proxyAddress.getPort()).isEqualTo(123);
+        assertThat(proxyAddress.getHostString()).isEqualTo("localhost");
     }
 
     @Test
@@ -179,20 +178,17 @@ public class DatadogHttpClientTest {
             final MetricAssertion metricAssertion = metricAssertions[i];
 
             if (metricAssertion.expectHost) {
-                assertThat(parsedJson.get(DMetric.FIELD_NAME_HOST).asText(), is(HOST));
+                assertThat(parsedJson.get(DMetric.FIELD_NAME_HOST).asText()).isEqualTo(HOST);
             } else {
-                assertThat(parsedJson.get(DMetric.FIELD_NAME_HOST), nullValue());
+                assertThat(parsedJson.get(DMetric.FIELD_NAME_HOST)).isNull();
             }
-            assertThat(
-                    parsedJson.get(DMetric.FIELD_NAME_METRIC).asText(),
-                    is(METRIC + metricAssertion.metricNameSuffix));
-            assertThat(
-                    parsedJson.get(DMetric.FIELD_NAME_TYPE).asText(),
-                    is(metricAssertion.expectedType.name()));
-            assertThat(
-                    parsedJson.get(DMetric.FIELD_NAME_POINTS).toString(),
-                    is(String.format("[[123,%s]]", metricAssertion.expectedValue)));
-            assertThat(parsedJson.get(DMetric.FIELD_NAME_TAGS).toString(), is(TAGS_AS_JSON));
+            assertThat(parsedJson.get(DMetric.FIELD_NAME_METRIC).asText())
+                    .isEqualTo(METRIC + metricAssertion.metricNameSuffix);
+            assertThat(parsedJson.get(DMetric.FIELD_NAME_TYPE).asText())
+                    .isEqualTo(metricAssertion.expectedType.name());
+            assertThat(parsedJson.get(DMetric.FIELD_NAME_POINTS).toString())
+                    .isEqualTo(String.format("[[123,%s]]", metricAssertion.expectedValue));
+            assertThat(parsedJson.get(DMetric.FIELD_NAME_TAGS).toString()).isEqualTo(TAGS_AS_JSON);
         }
     }
 
