bc7a8b64a529bbeb7c1fb69beb2addbad85d6d39
Anton Kalashnikov <kaa.dev@yandex.ru>
2022-02-21 16:37:58 +0100

    [hotfix][runtime] CodeStyle correction for NetworkBufferPoolTest

--- flink-runtime/src/test/java/org/apache/flink/runtime/io/network/buffer/NetworkBufferPoolTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/io/network/buffer/NetworkBufferPoolTest.java
@@ -23,10 +23,7 @@ import org.apache.flink.core.testutils.CheckedThread;
 import org.apache.flink.core.testutils.OneShotLatch;
 import org.apache.flink.util.TestLogger;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.junit.rules.Timeout;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -40,13 +37,13 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.apache.flink.util.Preconditions.checkNotNull;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.hamcrest.core.IsNot.not;
@@ -55,17 +52,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 /** Tests for {@link NetworkBufferPool}. */
 public class NetworkBufferPoolTest extends TestLogger {
-
-    @Rule public ExpectedException expectedException = ExpectedException.none();
-
-    @Rule public Timeout timeout = new Timeout(10, TimeUnit.SECONDS);
 
     @Test
     public void testCreatePoolAfterDestroy() {
@@ -434,10 +426,9 @@ public class NetworkBufferPoolTest extends TestLogger {
 
         segment.free();
 
-        expectedException.expect(IllegalStateException.class);
-        expectedException.expectMessage("destroyed");
         try {
-            asyncRequest.sync();
+            Exception ex = assertThrows(IllegalStateException.class, asyncRequest::sync);
+            assertTrue(ex.getMessage().contains("destroyed"));
         } finally {
             globalPool.destroy();
         }
@@ -518,11 +509,9 @@ public class NetworkBufferPoolTest extends TestLogger {
 
         asyncRequest.start();
 
-        expectedException.expect(IOException.class);
-        expectedException.expectMessage("Timeout");
-
         try {
-            asyncRequest.sync();
+            Exception ex = assertThrows(IOException.class, asyncRequest::sync);
+            assertTrue(ex.getMessage().contains("Timeout"));
         } finally {
             globalPool.destroy();
         }
@@ -683,7 +672,7 @@ public class NetworkBufferPoolTest extends TestLogger {
             // wait until all available buffers are requested
             while (segmentsRequested.size() + segments.size() + exclusiveSegments.size()
                     < numBuffers) {
-                Thread.sleep(100);
+                Thread.sleep(10);
                 assertNull(cause.get());
             }
 
