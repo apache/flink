2b12c60dc4b735c3a358fe3f9b362253e674651a
Paul Lin <paullin3280@gmail.com>
2022-03-05 11:36:24 +0800

    [FLINK-18592] Fix broken oracle statement tests

--- flink-connectors/flink-connector-jdbc/src/test/java/org/apache/flink/connector/jdbc/dialect/oracle/OraclePreparedStatementTest.java
+++ flink-connectors/flink-connector-jdbc/src/test/java/org/apache/flink/connector/jdbc/dialect/oracle/OraclePreparedStatementTest.java
@@ -46,7 +46,7 @@ public class OraclePreparedStatementTest {
         String insertStmt = dialect.getInsertIntoStatement(tableName, fieldNames);
         assertEquals(
                 "INSERT INTO tbl(id, name, email, ts, field1, field_2, __field_3__) "
-                        + "VALUES (:id, :name, :email, :ts, :field1, :field_2, :__field_3__)",
+                        + "VALUES (:{id}, :{name}, :{email}, :{ts}, :{field1}, :{field_2}, :{__field_3__})",
                 insertStmt);
         NamedStatementMatcher.parsedSql(
                         "INSERT INTO tbl(id, name, email, ts, field1, field_2, __field_3__) "
@@ -64,7 +64,7 @@ public class OraclePreparedStatementTest {
     @Test
     public void testDeleteStatement() {
         String deleteStmt = dialect.getDeleteStatement(tableName, keyFields);
-        assertEquals("DELETE FROM tbl WHERE id = :id AND __field_3__ = :__field_3__", deleteStmt);
+        assertEquals("DELETE FROM tbl WHERE id = :{id} AND __field_3__ = :{__field_3__}", deleteStmt);
         NamedStatementMatcher.parsedSql("DELETE FROM tbl WHERE id = ? AND __field_3__ = ?")
                 .parameter("id", singletonList(1))
                 .parameter("__field_3__", singletonList(2))
@@ -75,7 +75,7 @@ public class OraclePreparedStatementTest {
     public void testRowExistsStatement() {
         String rowExistStmt = dialect.getRowExistsStatement(tableName, keyFields);
         assertEquals(
-                "SELECT 1 FROM tbl WHERE id = :id AND __field_3__ = :__field_3__", rowExistStmt);
+                "SELECT 1 FROM tbl WHERE id = :{id} AND __field_3__ = :{__field_3__}", rowExistStmt);
         NamedStatementMatcher.parsedSql("SELECT 1 FROM tbl WHERE id = ? AND __field_3__ = ?")
                 .parameter("id", singletonList(1))
                 .parameter("__field_3__", singletonList(2))
@@ -86,9 +86,9 @@ public class OraclePreparedStatementTest {
     public void testUpdateStatement() {
         String updateStmt = dialect.getUpdateStatement(tableName, fieldNames, keyFields);
         assertEquals(
-                "UPDATE tbl SET id = :id, name = :name, email = :email, ts = :ts, "
-                        + "field1 = :field1, field_2 = :field_2, __field_3__ = :__field_3__ "
-                        + "WHERE id = :id AND __field_3__ = :__field_3__",
+                "UPDATE tbl SET id = :{id}, name = :{name}, email = :{email}, ts = :{ts}, "
+                        + "field1 = :{field1}, field_2 = :{field_2}, __field_3__ = :{__field_3__} "
+                        + "WHERE id = :{id} AND __field_3__ = :{__field_3__}",
                 updateStmt);
         NamedStatementMatcher.parsedSql(
                         "UPDATE tbl SET id = ?, name = ?, email = ?, ts = ?, field1 = ?, "
@@ -108,7 +108,7 @@ public class OraclePreparedStatementTest {
         String upsertStmt = dialect.getUpsertStatement(tableName, fieldNames, keyFields).get();
         assertEquals(
                 " MERGE INTO tbl t "
-                        + " USING (SELECT :id id, :name name, :email email, :ts ts, :field1 field1, :field_2 field_2, :__field_3__ __field_3__ FROM DUAL) s "
+                        + " USING (SELECT :{id} id, :{name} name, :{email} email, :{ts} ts, :{field1} field1, :{field_2} field_2, :{__field_3__} __field_3__ FROM DUAL) s "
                         + " ON (t.id=s.id and t.__field_3__=s.__field_3__) "
                         + " WHEN MATCHED THEN UPDATE SET t.name=s.name, t.email=s.email, t.ts=s.ts, t.field1=s.field1, t.field_2=s.field_2"
                         + " WHEN NOT MATCHED THEN INSERT (id, name, email, ts, field1, field_2, __field_3__)"
@@ -136,7 +136,7 @@ public class OraclePreparedStatementTest {
         String selectStmt = dialect.getSelectFromStatement(tableName, fieldNames, keyFields);
         assertEquals(
                 "SELECT id, name, email, ts, field1, field_2, __field_3__ FROM tbl "
-                        + "WHERE id = :id AND __field_3__ = :__field_3__",
+                        + "WHERE id = :{id} AND __field_3__ = :{__field_3__}",
                 selectStmt);
         NamedStatementMatcher.parsedSql(
                         "SELECT id, name, email, ts, field1, field_2, __field_3__ FROM tbl "
