a54c11040dd0103ffce5146a09a2ab6103134906
Juntao Hu <maybachhjt@gmail.com>
2022-03-08 17:18:57 +0800

    [FLINK-26536][python] Fix RemoteKeyedStateBackend#merge_namespaces to handle properly for the timerservice
    This closes #19008.

--- flink-python/pyflink/fn_execution/state_impl.py
+++ flink-python/pyflink/fn_execution/state_impl.py
@@ -1155,13 +1155,14 @@ class RemoteKeyedStateBackend(object):
             self._map_state_handler.clear(self._clear_iterator_mark)
 
     def merge_namespaces(self, state: SynchronousMergingRuntimeState, target, sources, ttl_config):
+        for source in sources:
+            state.set_current_namespace(source)
+            self.commit_internal_state(state.get_internal_state())
         state.set_current_namespace(target)
         self.commit_internal_state(state.get_internal_state())
         encoded_target_namespace = self._encode_namespace(target)
-        encoded_namespaces = [encoded_target_namespace]
-        for source in sources:
-            encoded_namespaces.append(self._encode_namespace(source))
-        self.clear_state_cache(state, encoded_namespaces)
+        encoded_namespaces = [self._encode_namespace(source) for source in sources]
+        self.clear_state_cache(state, [encoded_target_namespace] + encoded_namespaces)
 
         state_key = self.get_bag_state_key(
             state.name, self._encoded_current_key, encoded_target_namespace, ttl_config)
