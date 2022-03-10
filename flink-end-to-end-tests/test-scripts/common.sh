e981675f5dfe6d561eead5e151a750ea042eaf2c
Anton Kalashnikov <kaa.dev@yandex.ru>
2022-03-10 14:24:21 +0100

    [FLINK-26501][e2e-tests] Added new exception to white list since the failed checkpoint exception is always logging on the task manager site now

--- flink-end-to-end-tests/test-scripts/common.sh
+++ flink-end-to-end-tests/test-scripts/common.sh
@@ -429,6 +429,7 @@ function check_logs_for_exceptions {
    | grep -v "org.apache.flink.runtime.JobException: Recovery is suppressed" \
    | grep -v "WARN  akka.remote.ReliableDeliverySupervisor" \
    | grep -v "RecipientUnreachableException" \
+   | grep -v "SerializedCheckpointException.unwrap" \
    | grep -ic "exception" || true)
   if [[ ${exception_count} -gt 0 ]]; then
     echo "Found exception in log files; printing first 500 lines; see full logs for details:"
