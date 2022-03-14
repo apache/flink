9c77f13e587d7cb52469cac934c6aaec28dcd17d
Alexander Fedulov <1492164+afedulov@users.noreply.github.com>
2022-03-14 16:52:19 +0100

    [FLINK-25927][connectors][build] Consistent flink-connector-base usage
    flink-connector-base was previously inconsistently used in connectors (directly shaded in some and transitively pulled in via flink-connector-files which was itself shaded in the table uber jar). FLINK-24687 moved flink-connector-files out from the flink-table uber jar. This commit implements a combined approach for ensuring a smooth transition for both Flink users and for external connector developers, as outlined in this:
    - all internal Flink connectors that depend on flink-connector-base now shade and relocate it
    - for compatibility, until external developers implement the same change, flink-connector-base is also included into flink-dist

--- tools/ci/shade.sh
+++ tools/ci/shade.sh
@@ -153,7 +153,7 @@ check_shaded_artifacts_connector_elasticsearch() {
 	VARIANT=$1
 	find flink-connectors/flink-connector-elasticsearch${VARIANT}/target/flink-connector-elasticsearch${VARIANT}*.jar ! -name "*-tests.jar" -exec jar tf {} \; > allClasses
 
-	UNSHADED_CLASSES=`cat allClasses | grep -v -e '^META-INF' -e '^assets' -e "^org/apache/flink/connector/elasticsearch/" -e "^org/apache/flink/streaming/connectors/elasticsearch/" -e "^org/apache/flink/streaming/connectors/elasticsearch${VARIANT}/" -e "^org/apache/flink/table/descriptors/" -e "^org/elasticsearch/" | grep '\.class$'`
+	UNSHADED_CLASSES=`cat allClasses | grep -v -e '^META-INF' -e '^assets' -e "^org/apache/flink/connector/elasticsearch/" -e "^org/apache/flink/streaming/connectors/elasticsearch/" -e "^org/apache/flink/streaming/connectors/elasticsearch${VARIANT}/" -e "^org/apache/flink/connector/elasticsearch${VARIANT}/shaded/" -e "^org/apache/flink/table/descriptors/" -e "^org/elasticsearch/" | grep '\.class$'`
 	if [ "$?" = "0" ]; then
 		echo "=============================================================================="
 		echo "Detected unshaded dependencies in flink-connector-elasticsearch${VARIANT}'s fat jar:"
@@ -173,3 +173,62 @@ check_shaded_artifacts_connector_elasticsearch() {
 
 	return 0
 }
+
+check_one_per_package() {
+    read foo
+	if [ $foo -gt 1 ]
+	then
+		echo "ERROR - CHECK FAILED: $1 is shaded multiple times!"
+		exit 1
+	else
+		echo "OK"
+	fi
+}
+
+check_relocated() {
+    read foo
+	if [ $foo -ne 0 ]
+	then
+		echo "ERROR - CHECK FAILED: found $1 classes that where not relocated!"
+		exit 1
+	else
+		echo "OK"
+	fi
+}
+
+check_one_per_package_file_connector_base() {
+  echo "Checking that flink-connector-base is included only once:"
+  echo "__________________________________________________________________________"
+
+  CONNECTOR_JARS=$(find flink-connectors -type f -name '*.jar' | grep -vE "original|connector-hive" | grep -v '\-test');
+  EXIT_CODE=0
+
+  for i in $CONNECTOR_JARS;
+    do
+      echo -n "- $i: ";
+      jar tf $i | grep 'org/apache/flink/connector/base/source/reader/RecordEmitter' | wc -l | check_one_per_package "flink-connector-base";
+      EXIT_CODE=$((EXIT_CODE+$?))
+    done;
+    return $EXIT_CODE;
+}
+
+check_relocated_file_connector_base() {
+  echo -e "\n\n"
+  echo "Checking that flink-connector-base is relocated:"
+  echo "__________________________________________________________________________"
+
+  CONNECTOR_JARS=$(find flink-connectors -type f -name '*.jar' | \
+    grep -v original | grep -v '\-test' | grep -v 'flink-connectors/flink-connector-base');
+
+  EXIT_CODE=0
+  for i in $CONNECTOR_JARS;
+    do
+      echo -n "- $i: ";
+      jar tf $i | grep '^org/apache/flink/connector/base/source/reader/RecordEmitter' | wc -l | check_relocated "flink-connector-base";
+      EXIT_CODE=$((EXIT_CODE+$?))
+    done;
+  return $EXIT_CODE;
+}
+
+
+
