891ec9dfb05fa05c91cc4e9fd0108b56d8d7a3db
zhangjingcun <cun8cun8@163.com>
2022-03-15 11:23:13 +0800

    [FLINK-26607][python] Correct the MAX_LONG_VALUE/MIN_LONG_VALUE in several places
    This closes #19089.

---
+++ flink-python/pyflink/common/constants.py
@@ -0,0 +1,26 @@
+################################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+"""
+A constant holding the maximum value a long can have, 2^63 – 1.
+"""
+MAX_LONG_VALUE = 0x7fffffffffffffff
+
+"""
+A constant holding the minimum value a long can have, -2^63
+"""
+MIN_LONG_VALUE = - MAX_LONG_VALUE - 1
