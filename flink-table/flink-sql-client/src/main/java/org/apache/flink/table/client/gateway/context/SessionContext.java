1260da9994c4c5839914e1982af4d584332ba5fa
Paul Lin <paullin3280@gmail.com>
2022-03-17 19:22:56 +0800

    [FLINK-26618] Fix pipeline.jars not aligned with remove-jar statement

--- flink-table/flink-sql-client/src/main/java/org/apache/flink/table/client/gateway/context/SessionContext.java
+++ flink-table/flink-sql-client/src/main/java/org/apache/flink/table/client/gateway/context/SessionContext.java
@@ -262,6 +262,9 @@ public class SessionContext {
         }
 
         Set<URL> newDependencies = new HashSet<>(dependencies);
+        // merge the jars in config with the jars maintained in session
+        Set<URL> jarsInConfig = getJarsInConfig();
+        newDependencies.addAll(jarsInConfig);
         newDependencies.add(jarURL);
         updateClassLoaderAndDependencies(newDependencies);
 
@@ -280,6 +283,9 @@ public class SessionContext {
         }
 
         Set<URL> newDependencies = new HashSet<>(dependencies);
+        // merge the jars in config with the jars maintained in session
+        Set<URL> jarsInConfig = getJarsInConfig();
+        newDependencies.addAll(jarsInConfig);
         newDependencies.remove(jarURL);
         updateClassLoaderAndDependencies(newDependencies);
 
@@ -339,7 +345,7 @@ public class SessionContext {
         ConfigUtils.encodeCollectionToConfig(
                 sessionConfiguration,
                 PipelineOptions.JARS,
-                new ArrayList<>(jarsInConfig),
+                new ArrayList<>(newDependencies),
                 URL::toString);
 
         // TODO: update the the classloader in CatalogManager.
@@ -373,5 +379,19 @@ public class SessionContext {
                     String.format("Failed to get the jar file with specified path: %s", jarPath),
                     e);
         }
+    }
+
+    private Set<URL> getJarsInConfig() {
+        Set<URL> jarsInConfig;
+        try {
+            jarsInConfig =
+                    new HashSet<>(
+                            ConfigUtils.decodeListFromConfig(
+                                    sessionConfiguration, PipelineOptions.JARS, URL::new));
+        } catch (MalformedURLException e) {
+            throw new SqlExecutionException(
+                    "Failed to parse the option `pipeline.jars` in configuration.", e);
+        }
+        return jarsInConfig;
     }
 }
