0b09d340e26550cc044b8dc91b30f6e59e8d8cf0
Alexander Fedulov <1492164+afedulov@users.noreply.github.com>
2022-03-17 23:08:57 +0100

    [FLINK-26716][test][connectors/jdbc] Migrate PostgreSQL tests to Testcontainers

--- flink-test-utils-parent/flink-test-utils-junit/src/main/java/org/apache/flink/util/DockerImageVersions.java
+++ flink-test-utils-parent/flink-test-utils-junit/src/main/java/org/apache/flink/util/DockerImageVersions.java
@@ -49,4 +49,6 @@ public class DockerImageVersions {
     public static final String MINIO = "minio/minio:RELEASE.2022-02-07T08-17-33Z";
 
     public static final String ZOOKEEPER = "zookeeper:3.4.14";
+
+    public static final String POSTGRES = "postgres:9.6.12";
 }
