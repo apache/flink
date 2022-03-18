f44982b719457af56a03f7ae39e9bbf271fa2de4
Chesnay Schepler <chesnay@apache.org>
2022-03-18 15:06:19 +0100

    [FLINK-25904][metrics] Lazily initialize Percentile
    Percentile serialization doesn't work properly (see MATH-1642), so instead of serialize the data array and lazily initialize the Percentile as needed.

--- flink-runtime/src/test/java/org/apache/flink/runtime/metrics/DescriptiveStatisticsHistogramTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/metrics/DescriptiveStatisticsHistogramTest.java
@@ -19,8 +19,14 @@
 package org.apache.flink.runtime.metrics;
 
 import org.apache.flink.metrics.AbstractHistogramTest;
+import org.apache.flink.util.InstantiationUtil;
+import org.apache.flink.util.function.FunctionWithException;
 
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * Tests for {@link DescriptiveStatisticsHistogram} and {@link
@@ -28,10 +34,59 @@ import org.junit.Test;
  */
 public class DescriptiveStatisticsHistogramTest extends AbstractHistogramTest {
 
+    private static final double[] DATA = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
     /** Tests the histogram functionality of the DropwizardHistogramWrapper. */
     @Test
     public void testDescriptiveHistogram() {
         int size = 10;
         testHistogram(size, new DescriptiveStatisticsHistogram(size));
+    }
+
+    /** Tests our workaround for https://issues.apache.org/jira/browse/MATH-1642. */
+    @Test
+    public void testSerialization() throws Exception {
+        testDuplication(
+                original -> {
+                    final byte[] bytes = InstantiationUtil.serializeObject(original);
+                    return (DescriptiveStatisticsHistogramStatistics.CommonMetricsSnapshot)
+                            InstantiationUtil.deserializeObject(bytes, getClass().getClassLoader());
+                });
+    }
+
+    @Test
+    public void testCopy() throws Exception {
+        testDuplication(DescriptiveStatisticsHistogramStatistics.CommonMetricsSnapshot::copy);
+    }
+
+    private static void testDuplication(
+            FunctionWithException<
+                            DescriptiveStatisticsHistogramStatistics.CommonMetricsSnapshot,
+                            DescriptiveStatisticsHistogramStatistics.CommonMetricsSnapshot,
+                            Exception>
+                    duplicator)
+            throws Exception {
+
+        DescriptiveStatisticsHistogramStatistics.CommonMetricsSnapshot original =
+                new DescriptiveStatisticsHistogramStatistics.CommonMetricsSnapshot();
+        original.evaluate(DATA);
+
+        assertOperations(original);
+
+        final DescriptiveStatisticsHistogramStatistics.CommonMetricsSnapshot copy =
+                duplicator.apply(original);
+
+        assertOperations(copy);
+    }
+
+    private static void assertOperations(
+            DescriptiveStatisticsHistogramStatistics.CommonMetricsSnapshot statistics) {
+        assertThat(statistics.getPercentile(0.5), equalTo(1.0));
+        assertThat(statistics.getCount(), equalTo(9L));
+        assertThat(statistics.getMin(), equalTo(1.0));
+        assertThat(statistics.getMax(), equalTo(9.0));
+        assertThat(statistics.getMean(), equalTo(5.0));
+        assertThat(statistics.getStandardDeviation(), closeTo(2.7, 0.5));
+        assertThat(statistics.getValues(), equalTo(DATA));
     }
 }
