d47af79f7ad58d4c31fa1b392a72466e3008332e
Ryan Skraba <rskraba@apache.org>
2022-03-18 18:02:12 +0100

    [FLINK-26736][tests][JUnit5 migration] Module:flink-avro-confluent-registry

--- flink-formats/flink-avro-confluent-registry/src/test/java/org/apache/flink/formats/avro/registry/confluent/RegistryAvroRowDataSeDeSchemaTest.java
+++ flink-formats/flink-avro-confluent-registry/src/test/java/org/apache/flink/formats/avro/registry/confluent/RegistryAvroRowDataSeDeSchemaTest.java
@@ -39,29 +39,23 @@ import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.Random;
 
-import static org.apache.flink.core.testutils.FlinkMatchers.containsCause;
 import static org.apache.flink.formats.avro.utils.AvroTestUtils.writeRecord;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * Tests for {@link AvroRowDataDeserializationSchema} and {@link AvroRowDataSerializationSchema} for
  * schema registry avro.
  */
-public class RegistryAvroRowDataSeDeSchemaTest {
+class RegistryAvroRowDataSeDeSchemaTest {
     private static final Schema ADDRESS_SCHEMA = Address.getClassSchema();
 
     private static final Schema ADDRESS_SCHEMA_COMPATIBLE =
@@ -83,45 +77,43 @@ public class RegistryAvroRowDataSeDeSchemaTest {
 
     private Address address;
 
-    @Rule public ExpectedException expectedEx = ExpectedException.none();
-
-    @BeforeClass
-    public static void beforeClass() {
+    @BeforeAll
+    static void beforeClass() {
         client = new MockSchemaRegistryClient();
     }
 
-    @Before
-    public void before() {
+    @BeforeEach
+    void before() {
         this.address = TestDataGenerator.generateRandomAddress(new Random());
     }
 
-    @After
-    public void after() throws IOException, RestClientException {
+    @AfterEach
+    void after() throws IOException, RestClientException {
         client.deleteSubject(SUBJECT);
     }
 
     @Test
-    public void testRowDataWriteReadWithFullSchema() throws Exception {
+    void testRowDataWriteReadWithFullSchema() throws Exception {
         testRowDataWriteReadWithSchema(ADDRESS_SCHEMA);
     }
 
     @Test
-    public void testRowDataWriteReadWithCompatibleSchema() throws Exception {
+    void testRowDataWriteReadWithCompatibleSchema() throws Exception {
         testRowDataWriteReadWithSchema(ADDRESS_SCHEMA_COMPATIBLE);
         // Validates new schema has been registered.
-        assertThat(client.getAllVersions(SUBJECT).size(), is(1));
+        assertThat(client.getAllVersions(SUBJECT)).hasSize(1);
     }
 
     @Test
-    public void testRowDataWriteReadWithPreRegisteredSchema() throws Exception {
+    void testRowDataWriteReadWithPreRegisteredSchema() throws Exception {
         client.register(SUBJECT, ADDRESS_SCHEMA);
         testRowDataWriteReadWithSchema(ADDRESS_SCHEMA);
         // Validates it does not produce new schema.
-        assertThat(client.getAllVersions(SUBJECT).size(), is(1));
+        assertThat(client.getAllVersions(SUBJECT)).hasSize(1);
     }
 
     @Test
-    public void testRowDataReadWithNonRegistryAvro() throws Exception {
+    void testRowDataReadWithNonRegistryAvro() throws Exception {
         DataType dataType = AvroSchemaConverter.convertToDataType(ADDRESS_SCHEMA.toString());
         RowType rowType = (RowType) dataType.getLogicalType();
 
@@ -132,10 +124,9 @@ public class RegistryAvroRowDataSeDeSchemaTest {
 
         client.register(SUBJECT, ADDRESS_SCHEMA);
         byte[] oriBytes = writeRecord(address, ADDRESS_SCHEMA);
-        expectedEx.expect(IOException.class);
-        expectedEx.expect(
-                containsCause(new IOException("Unknown data format. Magic number does not match")));
-        deserializer.deserialize(oriBytes);
+        assertThatThrownBy(() -> deserializer.deserialize(oriBytes))
+                .isInstanceOf(IOException.class)
+                .hasCause(new IOException("Unknown data format. Magic number does not match"));
     }
 
     private void testRowDataWriteReadWithSchema(Schema schema) throws Exception {
@@ -150,18 +141,18 @@ public class RegistryAvroRowDataSeDeSchemaTest {
         serializer.open(null);
         deserializer.open(null);
 
-        assertNull(deserializer.deserialize(null));
+        assertThat(deserializer.deserialize(null)).isNull();
 
         RowData oriData = address2RowData(address);
         byte[] serialized = serializer.serialize(oriData);
         RowData rowData = deserializer.deserialize(serialized);
-        assertThat(rowData.getArity(), equalTo(schema.getFields().size()));
-        assertEquals(address.getNum(), rowData.getInt(0));
-        assertEquals(address.getStreet(), rowData.getString(1).toString());
+        assertThat(rowData.getArity()).isEqualTo(schema.getFields().size());
+        assertThat(rowData.getInt(0)).isEqualTo(address.getNum());
+        assertThat(rowData.getString(1).toString()).isEqualTo(address.getStreet());
         if (schema != ADDRESS_SCHEMA_COMPATIBLE) {
-            assertEquals(address.getCity(), rowData.getString(2).toString());
-            assertEquals(address.getState(), rowData.getString(3).toString());
-            assertEquals(address.getZip(), rowData.getString(4).toString());
+            assertThat(rowData.getString(2).toString()).isEqualTo(address.getCity());
+            assertThat(rowData.getString(3).toString()).isEqualTo(address.getState());
+            assertThat(rowData.getString(4).toString()).isEqualTo(address.getZip());
         }
     }
 
