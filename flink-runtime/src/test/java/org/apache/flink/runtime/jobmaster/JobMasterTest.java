a3a1202a75c952b17677cb91984d03789e29c014
Matthias Pohl <matthias@ververica.com>
2022-03-22 10:01:04 +0100

    [FLINK-26798][runtime] Hardens test against unexpected heartbeat
    testJobFailureWhenTaskExecutorHeartbeatTimeout failed due to a heartbeat
    being processed during the test. The missing payload of the test
    implementation caused an unexpected error after the JobGraph was already
    running (and therefore, expected some executions being present in the
    ExecutionDeploymentReport of the TaskExecutorToJobManagerHeartbeatPayload.
    
    The heartbeatFromTaskManager call is not necessary in this test because
    we're simulating a timeout of the heartbeat, anyway. Removing this call
    fixes the issue.

--- flink-runtime/src/test/java/org/apache/flink/runtime/jobmaster/JobMasterTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/jobmaster/JobMasterTest.java
@@ -97,7 +97,6 @@ import org.apache.flink.runtime.scheduler.TestingSchedulerNGFactory;
 import org.apache.flink.runtime.state.CompletedCheckpointStorageLocation;
 import org.apache.flink.runtime.state.StreamStateHandle;
 import org.apache.flink.runtime.taskexecutor.TaskExecutorGateway;
-import org.apache.flink.runtime.taskexecutor.TaskExecutorToJobManagerHeartbeatPayload;
 import org.apache.flink.runtime.taskexecutor.TestingTaskExecutorGateway;
 import org.apache.flink.runtime.taskexecutor.TestingTaskExecutorGatewayBuilder;
 import org.apache.flink.runtime.taskexecutor.slot.SlotOffer;
@@ -154,7 +153,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -1746,9 +1744,7 @@ public class JobMasterTest extends TestLogger {
                 (localTaskManagerLocation, jobMasterGateway) ->
                         jobMasterGateway.disconnectTaskManager(
                                 localTaskManagerLocation.getResourceID(),
-                                new FlinkException("Test disconnectTaskManager exception.")),
-                (jobMasterGateway, resourceID) ->
-                        (ignoredA, ignoredB) -> FutureUtils.completedVoidFuture());
+                                new FlinkException("Test disconnectTaskManager exception.")));
     }
 
     @Test
@@ -1760,14 +1756,7 @@ public class JobMasterTest extends TestLogger {
                 testingHeartbeatService,
                 (localTaskManagerLocation, jobMasterGateway) ->
                         testingHeartbeatService.triggerHeartbeatTimeout(
-                                jmResourceId, localTaskManagerLocation.getResourceID()),
-                (jobMasterGateway, taskManagerResourceId) ->
-                        (resourceId, ignored) -> {
-                            jobMasterGateway.heartbeatFromTaskManager(
-                                    taskManagerResourceId,
-                                    TaskExecutorToJobManagerHeartbeatPayload.empty());
-                            return FutureUtils.completedVoidFuture();
-                        });
+                                jmResourceId, localTaskManagerLocation.getResourceID()));
     }
 
     /**
@@ -1979,12 +1968,7 @@ public class JobMasterTest extends TestLogger {
 
     private void runJobFailureWhenTaskExecutorTerminatesTest(
             HeartbeatServices heartbeatServices,
-            BiConsumer<LocalUnresolvedTaskManagerLocation, JobMasterGateway> jobReachedRunningState,
-            BiFunction<
-                            JobMasterGateway,
-                            ResourceID,
-                            BiFunction<ResourceID, AllocatedSlotReport, CompletableFuture<Void>>>
-                    heartbeatConsumerFunction)
+            BiConsumer<LocalUnresolvedTaskManagerLocation, JobMasterGateway> jobReachedRunningState)
             throws Exception {
         final JobGraph jobGraph = JobGraphTestUtils.singleNoOpJobGraph();
         final JobMasterBuilder.TestingOnCompletionActions onCompletionActions =
@@ -2016,10 +2000,6 @@ public class JobMasterTest extends TestLogger {
                                                 taskDeploymentDescriptor.getExecutionAttemptId());
                                         return CompletableFuture.completedFuture(Acknowledge.get());
                                     })
-                            .setHeartbeatJobManagerFunction(
-                                    heartbeatConsumerFunction.apply(
-                                            jobMasterGateway,
-                                            taskManagerUnresolvedLocation.getResourceID()))
                             .createTestingTaskExecutorGateway();
 
             final Collection<SlotOffer> slotOffers =
