2049f849a130e738759001c5a6b9d85834da08d0
shizhengchao <shizhengchao@outlook.com>
2022-03-29 20:11:21 +0800

    [FLINK-26033][flink-connector-kafka]Fix the problem that robin does not take effect due to upgrading kafka client to 2.4.1 since Flink1.11

---
+++ flink-connectors/flink-connector-kafka/src/test/java/org/apache/flink/streaming/connectors/kafka/FlinkRoundRobinPartitionerTest.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.streaming.connectors.kafka;
+
+import org.apache.flink.streaming.connectors.kafka.partitioner.FlinkRoundRobinPartitioner;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/** Tests for the {@link FlinkRoundRobinPartitioner}. */
+public class FlinkRoundRobinPartitionerTest {
+
+    @Test
+    public void testRoundRobin() {
+        FlinkRoundRobinPartitioner<String> part = new FlinkRoundRobinPartitioner<>();
+
+        int[] partitions = new int[] {0, 1, 2, 3, 4};
+        String topic = "topic1";
+        part.open(0, 2);
+
+        assertThat(part.partition("abc1", null, null, topic, partitions)).isEqualTo(0);
+        assertThat(part.partition("abc2", null, null, topic, partitions)).isEqualTo(1);
+        assertThat(part.partition("abc3", null, null, topic, partitions)).isEqualTo(2);
+        assertThat(part.partition("abc4", null, null, topic, partitions)).isEqualTo(3);
+        assertThat(part.partition("abc5", null, null, topic, partitions)).isEqualTo(4);
+
+        part.open(1, 2);
+        assertThat(part.partition("abc1", null, null, topic, partitions)).isEqualTo(0);
+        assertThat(part.partition("abc2", null, null, topic, partitions)).isEqualTo(1);
+        assertThat(part.partition("abc3", null, null, topic, partitions)).isEqualTo(2);
+        assertThat(part.partition("abc4", null, null, topic, partitions)).isEqualTo(3);
+        assertThat(part.partition("abc5", null, null, topic, partitions)).isEqualTo(4);
+    }
+}
