215613e0b04418ad1ef9fbf98a1bffff53fa02f6
MartijnVisser <martijnvisser@apache.org>
2022-04-05 19:47:08 +0200

    [FLINK-27063][Connectors/Hive] Upgrade Hive 2.3 connector from 2.3.6 to 2.3.9

--- flink-connectors/flink-connector-hive/src/test/java/org/apache/flink/connectors/hive/HiveRunnerShimLoader.java
+++ flink-connectors/flink-connector-hive/src/test/java/org/apache/flink/connectors/hive/HiveRunnerShimLoader.java
@@ -44,6 +44,8 @@ public class HiveRunnerShimLoader {
                         case HiveShimLoader.HIVE_VERSION_V2_3_5:
                         case HiveShimLoader.HIVE_VERSION_V2_3_6:
                         case HiveShimLoader.HIVE_VERSION_V2_3_7:
+                        case HiveShimLoader.HIVE_VERSION_V2_3_8:
+                        case HiveShimLoader.HIVE_VERSION_V2_3_9:
                         case HiveShimLoader.HIVE_VERSION_V3_1_0:
                         case HiveShimLoader.HIVE_VERSION_V3_1_1:
                         case HiveShimLoader.HIVE_VERSION_V3_1_2:
