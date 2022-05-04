0bfb1cb55bfce0145c5a96c7356e5e401993d108
Fabian Paul <fabianpaul@ververica.com>
2022-05-04 10:50:49 +0200

    [FLINK-27486][core] Mark documentation formatting tools as PublicEvolving

--- flink-core/src/main/java/org/apache/flink/configuration/description/InlineElement.java
+++ flink-core/src/main/java/org/apache/flink/configuration/description/InlineElement.java
@@ -18,5 +18,8 @@
 
 package org.apache.flink.configuration.description;
 
+import org.apache.flink.annotation.PublicEvolving;
+
 /** Part of description that represents an element inside a block e.g. a link. */
+@PublicEvolving
 public interface InlineElement extends DescriptionElement {}
