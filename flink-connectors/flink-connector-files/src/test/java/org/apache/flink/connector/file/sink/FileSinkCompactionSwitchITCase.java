a4cbcbdc176ba86d4e8b2b89035ed1b53e8a7475
Sergey Nuyanzin <snuyanzin@gmail.com>
2022-05-18 00:45:17 +0200

    [FLINK-27607][tests] apply spotless

--- flink-connectors/flink-connector-files/src/test/java/org/apache/flink/connector/file/sink/FileSinkCompactionSwitchITCase.java
+++ flink-connectors/flink-connector-files/src/test/java/org/apache/flink/connector/file/sink/FileSinkCompactionSwitchITCase.java
@@ -106,7 +106,8 @@ public class FileSinkCompactionSwitchITCase {
 
     @TempDir private static java.nio.file.Path tmpDir;
 
-    @RegisterExtension private final SharedObjectsExtension sharedObjects = SharedObjectsExtension.create();
+    @RegisterExtension
+    private final SharedObjectsExtension sharedObjects = SharedObjectsExtension.create();
 
     private static final Map<String, CountDownLatch> LATCH_MAP = new ConcurrentHashMap<>();
 
