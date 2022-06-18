0b06f6cb1b6088c62ba2a3b2b7389ad2d735aaa9
wanglin <15551101192@163.com>
2022-06-18 10:20:20 +0800

    little test

---
+++ flink-table/flink-table-runtime/src/main/java/org/apache/flink/table/runtime/util/LinUtil.java
@@ -0,0 +1,14 @@
+package org.apache.flink.table.runtime.util;
+
+/**
+ * Author: 一土木木
+ * Date: 2022/6/18 10:19
+ * Description: LinUtil
+ */
+public class LinUtil {
+
+    public static String authorName() {
+        return "StephenLin";
+    }
+
+}
