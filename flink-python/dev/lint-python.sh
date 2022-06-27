d716b4376f4e6328be9ba3875df796478280a6d1
huangxingbo <hxb@apache.org>
2022-06-27 14:16:47 +0800

    [FLINK-28195][python] Annotate Python3.6 as deprecated in PyFlink 1.16

--- flink-python/dev/lint-python.sh
+++ flink-python/dev/lint-python.sh
@@ -228,7 +228,7 @@ function install_py_env() {
     if [[ ${BUILD_REASON} = 'IndividualCI' ]]; then
         py_env=("3.9")
     else
-        py_env=("3.6" "3.7" "3.8" "3.9")
+        py_env=("3.7" "3.8" "3.9")
     fi
     for ((i=0;i<${#py_env[@]};i++)) do
         if [ -d "$CURRENT_DIR/.conda/envs/${py_env[i]}" ]; then
@@ -403,7 +403,7 @@ function install_environment() {
     fi
 
     # step-3 install python environment which includes
-    # 3.6 3.7 3.8 3.9
+    # 3.7 3.8 3.9
     if [ $STEP -lt 3 ] && [ `need_install_component "py_env"` = true ]; then
         print_function "STEP" "installing python environment..."
         install_py_env
@@ -778,7 +778,7 @@ usage: $0 [options]
 -l          list all checks supported.
 Examples:
   ./lint-python -s basic        =>  install environment with basic components.
-  ./lint-python -s py_env       =>  install environment with python env(3.6,3.7,3.8,3.9).
+  ./lint-python -s py_env       =>  install environment with python env(3.7,3.8,3.9).
   ./lint-python -s all          =>  install environment with all components such as python env,tox,flake8,sphinx,mypy etc.
   ./lint-python -s tox,flake8   =>  install environment with tox,flake8.
   ./lint-python -s tox -f       =>  reinstall environment with tox.
