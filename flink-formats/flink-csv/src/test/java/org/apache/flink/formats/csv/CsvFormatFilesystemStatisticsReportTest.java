5f2d088a2713ced5c6ce072db92f4378f73bc739
zhengyunhong.zyh <337361684@qq.com>
2022-06-27 19:58:01 +0800

    [FLINK-27988][table-planner] Let HiveTableSource extend from SupportStatisticsReport
    This closes #20084

--- flink-formats/flink-csv/src/test/java/org/apache/flink/formats/csv/CsvFormatFilesystemStatisticsReportTest.java
+++ flink-formats/flink-csv/src/test/java/org/apache/flink/formats/csv/CsvFormatFilesystemStatisticsReportTest.java
@@ -18,90 +18,36 @@
 
 package org.apache.flink.formats.csv;
 
-import org.apache.flink.table.api.TableConfig;
-import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.plan.stats.TableStats;
-import org.apache.flink.table.planner.plan.schema.TableSourceTable;
 import org.apache.flink.table.planner.plan.stats.FlinkStatistic;
-import org.apache.flink.table.planner.utils.BatchTableTestUtil;
-import org.apache.flink.table.planner.utils.TableTestBase;
-import org.apache.flink.table.planner.utils.TableTestUtil;
-import org.apache.flink.util.Preconditions;
+import org.apache.flink.table.types.DataType;
 
-import org.apache.calcite.rel.RelNode;
-import org.apache.calcite.rel.RelVisitor;
-import org.apache.calcite.rel.core.TableScan;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Test for statistics functionality in {@link CsvFormatFactory} in the case of file system source.
  */
-public class CsvFormatFilesystemStatisticsReportTest extends TableTestBase {
-    private BatchTableTestUtil util;
-    private TableEnvironment tEnv;
-    @TempDir private static File path;
+public class CsvFormatFilesystemStatisticsReportTest extends CsvFormatStatisticsReportTest {
 
     @BeforeEach
-    public void setup() throws IOException {
-        util = batchTestUtil(TableConfig.getDefault());
-        tEnv = util.getTableEnv();
-        String pathName = writeData(path, Arrays.asList("1,1,hi", "2,1,hello", "3,2,hello world"));
-
-        String ddl =
-                String.format(
-                        "CREATE TABLE sourceTable (\n"
-                                + "  a bigint,\n"
-                                + "  b int,\n"
-                                + "  c varchar\n"
-                                + ") with (\n"
-                                + " 'connector' = 'filesystem',"
-                                + " 'format' = 'csv',"
-                                + " 'path' = '%s')",
-                        pathName);
-        tEnv.executeSql(ddl);
+    public void setup(@TempDir File file) throws Exception {
+        super.setup(file);
     }
 
     @Test
-    public void testCsvFileSystemStatisticsReport() {
+    public void testCsvFileSystemStatisticsReport()
+            throws ExecutionException, InterruptedException {
+        // insert data and get statistics by get plan.
+        DataType dataType = tEnv.from("sourceTable").getResolvedSchema().toPhysicalRowDataType();
+        tEnv.fromValues(dataType, getData()).executeInsert("sourceTable").await();
         FlinkStatistic statistic = getStatisticsFromOptimizedPlan("select * from sourceTable");
         assertThat(statistic.getTableStats()).isEqualTo(new TableStats(3));
-    }
-
-    private String writeData(File path, List<String> data) throws IOException {
-        String file = path.getAbsolutePath() + "/00-00.tmp";
-        Files.write(new File(file).toPath(), String.join("\n", data).getBytes());
-        return file;
-    }
-
-    private FlinkStatistic getStatisticsFromOptimizedPlan(String sql) {
-        RelNode relNode = TableTestUtil.toRelNode(tEnv.sqlQuery(sql));
-        RelNode optimized = util.getPlanner().optimize(relNode);
-        FlinkStatisticVisitor visitor = new FlinkStatisticVisitor();
-        visitor.go(optimized);
-        return visitor.result;
-    }
-
-    private static class FlinkStatisticVisitor extends RelVisitor {
-        private FlinkStatistic result = null;
-
-        @Override
-        public void visit(RelNode node, int ordinal, RelNode parent) {
-            if (node instanceof TableScan) {
-                Preconditions.checkArgument(result == null);
-                TableSourceTable table = (TableSourceTable) node.getTable();
-                result = table.getStatistic();
-            }
-            super.visit(node, ordinal, parent);
-        }
     }
 }
