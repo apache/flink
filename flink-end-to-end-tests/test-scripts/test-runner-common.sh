35e43a97c80965f4db6436b0b70958c83688c1c9
MartijnVisser <martijnvisser@apache.org>
2022-06-30 14:19:44 +0200

    [FLINK-28329][Tests] Output the top 15 directories in terms of used disk space
    (cherry picked from commit a33970455712b9d5a67cc66bb049d94ad4c4b543)

--- flink-end-to-end-tests/test-scripts/test-runner-common.sh
+++ flink-end-to-end-tests/test-scripts/test-runner-common.sh
@@ -109,7 +109,10 @@ function log_environment_info {
     jps
 
     echo "Disk information"
-    df -hH
+    df -h
+
+    echo "##[group]Top 15 biggest directories in terms of used disk space"
+    du -a . | sort -n -r | head -n 15
 
     if sudo -n true 2>/dev/null; then
       echo "Allocated ports"
