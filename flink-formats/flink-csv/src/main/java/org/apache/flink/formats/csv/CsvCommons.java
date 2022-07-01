09747f999e54b7921e8c12c944b941b0777be48f
fengjiankun <fengjiankun@360.cn>
2022-07-01 17:12:20 +0800

    [FLINK-26270][Formats][CSV] Flink SQL write data to kafka by CSV format , whether decimal type was converted to scientific notation. This closes #20127

--- flink-formats/flink-csv/src/main/java/org/apache/flink/formats/csv/CsvCommons.java
+++ flink-formats/flink-csv/src/main/java/org/apache/flink/formats/csv/CsvCommons.java
@@ -35,6 +35,7 @@ import static org.apache.flink.formats.csv.CsvFormatOptions.FIELD_DELIMITER;
 import static org.apache.flink.formats.csv.CsvFormatOptions.IGNORE_PARSE_ERRORS;
 import static org.apache.flink.formats.csv.CsvFormatOptions.NULL_LITERAL;
 import static org.apache.flink.formats.csv.CsvFormatOptions.QUOTE_CHARACTER;
+import static org.apache.flink.formats.csv.CsvFormatOptions.WRITE_BIGDECIMAL_IN_SCIENTIFIC_NOTATION;
 
 /** A class with common CSV format constants and utility methods. */
 class CsvCommons {
@@ -100,6 +101,7 @@ class CsvCommons {
         options.add(ARRAY_ELEMENT_DELIMITER);
         options.add(ESCAPE_CHARACTER);
         options.add(NULL_LITERAL);
+        options.add(WRITE_BIGDECIMAL_IN_SCIENTIFIC_NOTATION);
         return options;
     }
 
@@ -112,6 +114,7 @@ class CsvCommons {
         options.add(ARRAY_ELEMENT_DELIMITER);
         options.add(ESCAPE_CHARACTER);
         options.add(NULL_LITERAL);
+        options.add(WRITE_BIGDECIMAL_IN_SCIENTIFIC_NOTATION);
         return options;
     }
 }
