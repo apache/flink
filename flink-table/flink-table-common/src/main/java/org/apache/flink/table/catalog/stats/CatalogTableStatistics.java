38aaac9b07ce9c64b5b7b9c5e4c133534f2bcc4e
Jing Ge <gejing@gmail.com>
2022-07-02 02:15:32 +0200

    [FLINK-28778][SQL/API] add new EMPTY CatalogTableStatistics

--- flink-table/flink-table-common/src/main/java/org/apache/flink/table/catalog/stats/CatalogTableStatistics.java
+++ flink-table/flink-table-common/src/main/java/org/apache/flink/table/catalog/stats/CatalogTableStatistics.java
@@ -27,7 +27,10 @@ import java.util.Objects;
 /** Statistics for a non-partitioned table or a partition of a partitioned table. */
 @PublicEvolving
 public class CatalogTableStatistics {
+
     public static final CatalogTableStatistics UNKNOWN = new CatalogTableStatistics(-1, -1, -1, -1);
+
+    public static final CatalogTableStatistics EMPTY = new CatalogTableStatistics(0, 0, 0, 0);
 
     /** The number of rows in the table or partition. */
     private final long rowCount;
