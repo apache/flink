a1f8e144dd67b7fd3b43ee3eb842650733251d09
Yufan Sheng <yufan@streamnative.io>
2022-07-05 02:48:18 +0800

    [FLINK-27199][Connector/Pulsar] Bump the pulsar-client-all to latest 2.10.0.

--- flink-connectors/flink-connector-pulsar/src/main/java/org/apache/flink/connector/pulsar/source/enumerator/cursor/start/MessageIdStartCursor.java
+++ flink-connectors/flink-connector-pulsar/src/main/java/org/apache/flink/connector/pulsar/source/enumerator/cursor/start/MessageIdStartCursor.java
@@ -27,7 +27,7 @@ import org.apache.pulsar.client.impl.MessageIdImpl;
 
 import static org.apache.flink.util.Preconditions.checkState;
 
-/** This cursor would left pulsar start consuming from a specific message id. */
+/** This cursor would leave pulsar start consuming from a specific message id. */
 public class MessageIdStartCursor implements StartCursor {
     private static final long serialVersionUID = -8057345435887170111L;
 
