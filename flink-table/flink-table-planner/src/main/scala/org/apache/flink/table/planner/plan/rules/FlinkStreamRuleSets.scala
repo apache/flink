dc9b621d488b3eab296c3362e754dd2d8bb4e97c
trushev <trushev.alex@gmail.com>
2022-07-15 19:53:08 +0700

    fix comment

--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/plan/rules/FlinkStreamRuleSets.scala
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/plan/rules/FlinkStreamRuleSets.scala
@@ -137,7 +137,7 @@ object FlinkStreamRuleSets {
     CoreRules.FILTER_INTO_JOIN,
     // push filter into the children of a join
     CoreRules.JOIN_CONDITION_PUSH,
-    // push transitive predicates into the children of a join
+    // push filter through an aggregation
     CoreRules.FILTER_AGGREGATE_TRANSPOSE,
     // push a filter past a project
     CoreRules.FILTER_PROJECT_TRANSPOSE,
