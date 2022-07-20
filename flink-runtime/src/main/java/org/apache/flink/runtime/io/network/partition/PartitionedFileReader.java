46a706b77b69270af1f441148020f067710ec5ac
Yuxin Tan <tanyuxinwork@gmail.com>
2022-07-20 15:20:07 +0800

    [FLINK-28519][network] Fix the bug that SortMergeResultPartitionReadScheduler may not read data sequentially
    This closes #20320

--- flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/PartitionedFileReader.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/io/network/partition/PartitionedFileReader.java
@@ -66,7 +66,8 @@ class PartitionedFileReader {
             PartitionedFile partitionedFile,
             int targetSubpartition,
             FileChannel dataFileChannel,
-            FileChannel indexFileChannel) {
+            FileChannel indexFileChannel)
+            throws IOException {
         checkArgument(checkNotNull(dataFileChannel).isOpen(), "Data file channel must be opened.");
         checkArgument(
                 checkNotNull(indexFileChannel).isOpen(), "Index file channel must be opened.");
@@ -78,6 +79,7 @@ class PartitionedFileReader {
 
         this.indexEntryBuf = ByteBuffer.allocateDirect(PartitionedFile.INDEX_ENTRY_SIZE);
         BufferReaderWriterUtil.configureByteBuffer(indexEntryBuf);
+        moveToNextReadableRegion();
     }
 
     private void moveToNextReadableRegion() throws IOException {
