328007f0b9a3e4da31b20e75b94d9c339b168af0
Chesnay Schepler <chesnay@apache.org>
2022-08-05 13:45:18 +0200

    [FLINK-28621] Enable Date/Time&Optional support for all mappers

--- flink-core/src/main/java/org/apache/flink/util/jackson/JacksonMapperFactory.java
+++ flink-core/src/main/java/org/apache/flink/util/jackson/JacksonMapperFactory.java
@@ -20,7 +20,10 @@ package org.apache.flink.util.jackson;
 import org.apache.flink.annotation.Experimental;
 
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.SerializationFeature;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 /** Factory for Jackson mappers. */
 @Experimental
@@ -28,12 +31,21 @@ public final class JacksonMapperFactory {
 
     public static ObjectMapper createObjectMapper() {
         final ObjectMapper objectMapper = new ObjectMapper();
+        registerModules(objectMapper);
         return objectMapper;
     }
 
     public static CsvMapper createCsvMapper() {
         final CsvMapper csvMapper = new CsvMapper();
+        registerModules(csvMapper);
         return csvMapper;
+    }
+
+    private static void registerModules(ObjectMapper mapper) {
+        mapper.registerModule(new JavaTimeModule())
+                .registerModule(new Jdk8Module().configureAbsentsAsNulls(true))
+                .disable(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 
     private JacksonMapperFactory() {}
