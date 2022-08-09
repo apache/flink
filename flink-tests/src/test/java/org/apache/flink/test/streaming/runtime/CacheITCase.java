208f08b406a7fd48890cda16d317a30ee892a2e7
sxnan <suxuannan95@gmail.com>
2022-08-09 16:41:50 +0800

    [FLINK-28860][tests] Disable CacheITCase temporarily
    This closes #20512.

--- flink-tests/src/test/java/org/apache/flink/test/streaming/runtime/CacheITCase.java
+++ flink-tests/src/test/java/org/apache/flink/test/streaming/runtime/CacheITCase.java
@@ -48,6 +48,7 @@ import org.apache.flink.util.OutputTag;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -65,6 +66,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /** Test datastream cache. */
+@Disabled
 public class CacheITCase extends AbstractTestBase {
 
     private StreamExecutionEnvironment env;
