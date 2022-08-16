0195b2ce4364c87a69ca7787b8c7b57986efb360
Yufan Sheng <yufan@streamnative.io>
2022-08-17 07:02:53 +0800

    [FLINK-28934][Connector/pulsar] Fix split assignment in different Pulsar subscriptions.

--- flink-connectors/flink-connector-pulsar/src/test/java/org/apache/flink/connector/pulsar/source/enumerator/PulsarSourceEnumStateSerializerTest.java
+++ flink-connectors/flink-connector-pulsar/src/test/java/org/apache/flink/connector/pulsar/source/enumerator/PulsarSourceEnumStateSerializerTest.java
@@ -18,23 +18,17 @@
 
 package org.apache.flink.connector.pulsar.source.enumerator;
 
-import org.apache.flink.connector.pulsar.source.enumerator.cursor.StopCursor;
 import org.apache.flink.connector.pulsar.source.enumerator.topic.TopicPartition;
 import org.apache.flink.connector.pulsar.source.enumerator.topic.TopicRange;
-import org.apache.flink.connector.pulsar.source.split.PulsarPartitionSplit;
 
-import org.apache.flink.shaded.guava30.com.google.common.collect.ImmutableMap;
 import org.apache.flink.shaded.guava30.com.google.common.collect.Sets;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-import java.util.Map;
 import java.util.Set;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.apache.flink.connector.pulsar.source.enumerator.PulsarSourceEnumStateSerializer.INSTANCE;
-import static org.apache.flink.connector.pulsar.source.enumerator.cursor.MessageIdUtils.newMessageId;
 import static org.apache.flink.connector.pulsar.source.enumerator.topic.TopicRange.createFullRange;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
@@ -48,29 +42,13 @@ class PulsarSourceEnumStateSerializerTest {
                 Sets.newHashSet(
                         new TopicPartition(randomAlphabetic(10), 2, new TopicRange(1, 30)),
                         new TopicPartition(randomAlphabetic(10), 1, createFullRange()));
-        Set<PulsarPartitionSplit> splits =
-                Collections.singleton(
-                        new PulsarPartitionSplit(
-                                new TopicPartition(randomAlphabetic(10), 10, createFullRange()),
-                                StopCursor.defaultStopCursor(),
-                                newMessageId(100L, 23L, 44),
-                                null));
-        Map<Integer, Set<PulsarPartitionSplit>> shared = Collections.singletonMap(5, splits);
-        Map<Integer, Set<String>> mapping =
-                ImmutableMap.of(
-                        1, Sets.newHashSet(randomAlphabetic(10), randomAlphabetic(10)),
-                        2, Sets.newHashSet(randomAlphabetic(10), randomAlphabetic(10)));
 
-        PulsarSourceEnumState state =
-                new PulsarSourceEnumState(partitions, splits, shared, mapping, true);
+        PulsarSourceEnumState state = new PulsarSourceEnumState(partitions);
 
         byte[] bytes = INSTANCE.serialize(state);
         PulsarSourceEnumState state1 = INSTANCE.deserialize(INSTANCE.getVersion(), bytes);
 
         assertEquals(state.getAppendedPartitions(), state1.getAppendedPartitions());
-        assertEquals(state.getPendingPartitionSplits(), state1.getPendingPartitionSplits());
-        assertEquals(state.getReaderAssignedSplits(), state1.getReaderAssignedSplits());
-        assertEquals(state.isInitialized(), state1.isInitialized());
 
         assertNotSame(state, state1);
     }
