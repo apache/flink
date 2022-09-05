386a6d4bfabc7cfc259c79d4862a334e3676f747
Weijie Guo <reswqa@163.com>
2022-09-05 17:49:04 +0800

    [FLINK-29101] ignore non canBePipelined consumedPartitionGroup in getAllSchedulableRegions.

--- flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/strategy/PipelinedRegionSchedulingStrategy.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/strategy/PipelinedRegionSchedulingStrategy.java
@@ -250,6 +250,11 @@ public class PipelinedRegionSchedulingStrategy implements SchedulingStrategy {
                         .getOrDefault(currentRegion, Collections.emptySet())
                         .forEach(
                                 (consumedPartitionGroup) -> {
+                                    if (!consumedPartitionGroup
+                                            .getResultPartitionType()
+                                            .canBePipelinedConsumed()) {
+                                        return;
+                                    }
                                     // If this group has been visited, there is no need
                                     // to repeat the determination.
                                     if (visitedConsumedPartitionGroups.contains(
