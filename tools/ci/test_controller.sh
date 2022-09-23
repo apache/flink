2499a46c91dec66b666b8df59bfb77a32f457e48
Chesnay Schepler <chesnay@apache.org>
2022-09-23 16:49:29 +0200

    Update test_controller.sh

--- tools/ci/test_controller.sh
+++ tools/ci/test_controller.sh
@@ -105,7 +105,7 @@ if [ $STAGE == $STAGE_PYTHON ]; then
 	run_with_watchdog "./flink-python/dev/lint-python.sh" $CALLBACK_ON_TIMEOUT
 	EXIT_CODE=$?
 else
-	MVN_TEST_OPTIONS="-Dflink.tests.with-openssl -Dflink.tests.check-segment-multiple-free -Darchunit.freeze.store.default.allowStoreUpdate=false -Dakka.rpc.force-invocation-serialization"
+	MVN_TEST_OPTIONS="-Dtest=HDFSTest -Dflink.tests.with-openssl -Dflink.tests.check-segment-multiple-free -Darchunit.freeze.store.default.allowStoreUpdate=false -Dakka.rpc.force-invocation-serialization"
 	if [ $STAGE = $STAGE_FINEGRAINED_RESOURCE_MANAGEMENT ]; then
 		if [[ ${PROFILE} == *"enable-adaptive-scheduler"* ]]; then
 			echo "Skipping fine grained resource management test stage in adaptive scheduler job"
