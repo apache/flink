3366ecff5109a8ba5c64a4b2b603f319d0bc3750
Chesnay Schepler <chesnay@apache.org>
2022-09-28 13:15:32 +0200

    [FLINK-29455] Introduce OperatorIdentifier

---
+++ flink-libraries/flink-state-processing-api/src/main/java/org/apache/flink/state/api/OperatorIdentifier.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.state.api;
+
+import org.apache.flink.annotation.Internal;
+import org.apache.flink.runtime.jobgraph.OperatorID;
+import org.apache.flink.state.api.runtime.OperatorIDGenerator;
+import org.apache.flink.util.Preconditions;
+import org.apache.flink.util.StringUtils;
+
+import javax.annotation.Nullable;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.Optional;
+
+/** Identifies an operator, either based on a {@code uid} or {@code uidHash}. */
+@Internal
+public class OperatorIdentifier implements Serializable {
+    private final OperatorID operatorId;
+    // this is only used for logging purposes
+    @Nullable private final String uid;
+
+    private OperatorIdentifier(OperatorID operatorId, @Nullable String uid) {
+        this.operatorId = operatorId;
+        this.uid = uid;
+    }
+
+    public static OperatorIdentifier forUidHash(String uidHash) {
+        Preconditions.checkNotNull(uidHash);
+        return new OperatorIdentifier(new OperatorID(StringUtils.hexStringToByte(uidHash)), null);
+    }
+
+    public static OperatorIdentifier forUid(String uid) {
+        Preconditions.checkNotNull(uid);
+        return new OperatorIdentifier(OperatorIDGenerator.fromUid(uid), uid);
+    }
+
+    public Optional<String> getUid() {
+        return Optional.ofNullable(uid);
+    }
+
+    public OperatorID getOperatorId() {
+        return operatorId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OperatorIdentifier that = (OperatorIdentifier) o;
+        return Objects.equals(operatorId, that.operatorId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(operatorId);
+    }
+}
