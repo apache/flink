d974697262244e54d640b6bee6d9b5d87c5aa8a3
yuzelin <747884505@qq.com>
2022-10-04 04:22:38 +0800

    apply review comment.

--- flink-table/flink-sql-client/src/main/java/org/apache/flink/table/client/cli/parser/ClientParser.java
+++ flink-table/flink-sql-client/src/main/java/org/apache/flink/table/client/cli/parser/ClientParser.java
@@ -18,7 +18,6 @@
 
 package org.apache.flink.table.client.cli.parser;
 
-import org.apache.flink.sql.parser.impl.FlinkSqlParserImplConstants;
 import org.apache.flink.sql.parser.impl.FlinkSqlParserImplTokenManager;
 import org.apache.flink.sql.parser.impl.SimpleCharStream;
 import org.apache.flink.sql.parser.impl.Token;
@@ -26,18 +25,31 @@ import org.apache.flink.table.api.SqlParserEOFException;
 import org.apache.flink.table.client.gateway.SqlExecutionException;
 import org.apache.flink.table.operations.Operation;
 
-import javax.annotation.Nonnull;
-
 import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Optional;
+
+import static org.apache.flink.sql.parser.impl.FlinkSqlParserImplConstants.BEGIN;
+import static org.apache.flink.sql.parser.impl.FlinkSqlParserImplConstants.COMPILE;
+import static org.apache.flink.sql.parser.impl.FlinkSqlParserImplConstants.CREATE;
+import static org.apache.flink.sql.parser.impl.FlinkSqlParserImplConstants.END;
+import static org.apache.flink.sql.parser.impl.FlinkSqlParserImplConstants.EOF;
+import static org.apache.flink.sql.parser.impl.FlinkSqlParserImplConstants.EXECUTE;
+import static org.apache.flink.sql.parser.impl.FlinkSqlParserImplConstants.EXPLAIN;
+import static org.apache.flink.sql.parser.impl.FlinkSqlParserImplConstants.IDENTIFIER;
+import static org.apache.flink.sql.parser.impl.FlinkSqlParserImplConstants.SEMICOLON;
+import static org.apache.flink.sql.parser.impl.FlinkSqlParserImplConstants.SET;
+import static org.apache.flink.sql.parser.impl.FlinkSqlParserImplConstants.SHOW;
+import static org.apache.flink.sql.parser.impl.FlinkSqlParserImplConstants.STATEMENT;
 
 /**
  * ClientParser use {@link FlinkSqlParserImplTokenManager} to do lexical analysis. It cannot
- * recognize special hive keywords yet.
+ * recognize special hive keywords yet because Hive has a slightly different vocabulary compared to
+ * Flink's, which causes the ClientParser misunderstanding some Hive's keywords to IDENTIFIER. But
+ * the ClientParser is only responsible to check whether the statement is completed or not and only
+ * cares about a few statements. So it's acceptable to tolerate the inaccuracy here.
  */
-public class ClientParser implements SqlCommandParser, FlinkSqlParserImplConstants {
+public class ClientParser implements SqlCommandParser {
 
     /** A dumb implementation. TODO: remove this after unifying the SqlMultiLineParser. */
     @Override
@@ -45,34 +57,92 @@ public class ClientParser implements SqlCommandParser, FlinkSqlParserImplConstan
         return Optional.empty();
     }
 
-    public Optional<StatementType> parseStatement(@Nonnull String statement)
-            throws SqlExecutionException {
-        String trimmedStatement = statement.trim();
-        FlinkSqlParserImplTokenManager tokenManager =
-                new FlinkSqlParserImplTokenManager(
-                        new SimpleCharStream(new StringReader(trimmedStatement)));
-        // this means to switch to "BACK TICK IDENTIFIER" state to support '`' in Flink SQL
-        tokenManager.SwitchTo(2);
-        List<Token> tokens = new ArrayList<>();
-        Token token = tokenManager.getNextToken();
-        while (token.kind != EOF) {
-            tokens.add(token);
-            token = tokenManager.getNextToken();
+    public Optional<StatementType> parseStatement(String statement) throws SqlExecutionException {
+
+        return getStatementType(new TokenIterator(statement.trim()));
+    }
+
+    private static class TokenIterator implements Iterator<Token> {
+
+        private final FlinkSqlParserImplTokenManager tokenManager;
+        private Token currentToken;
+
+        public TokenIterator(String statement) {
+            tokenManager =
+                    new FlinkSqlParserImplTokenManager(
+                            new SimpleCharStream(new StringReader(statement)));
+            // means to switch to "BACK QUOTED IDENTIFIER" state to support '`xxx`' in Flink SQL
+            tokenManager.SwitchTo(2);
+            this.currentToken = tokenManager.getNextToken();
+            // this will make all tokens link together
+            checkIncompleteStatement();
         }
 
-        if (tokens.size() == 0 || tokens.get(tokens.size() - 1).kind != SEMICOLON) {
-            // throw this to notify the terminal to continue reading input
-            throw new SqlExecutionException("", new SqlParserEOFException(""));
+        @Override
+        public boolean hasNext() {
+            return currentToken.kind != EOF;
         }
 
-        return getStatementType(tokens);
+        @Override
+        public Token next() {
+            Token before = currentToken;
+            currentToken = before.next;
+            return before;
+        }
+
+        boolean nextTokenMatched(int kind) {
+            return hasNext() && next().kind == kind;
+        }
+
+        /**
+         * check these follow special cases: 1. "". 2. "COMPILE/EXECUTE STATEMENT SET BEGIN\n INSERT
+         * xxx;". 3. "EXPLAIN COMPILE/EXECUTE STATEMENT SET BEGIN\n INSERT xxx;". 4. "EXPLAIN BEGIN
+         * STATEMENT SET;". 5. string not ended with ';'.
+         */
+        void checkIncompleteStatement() {
+            // case 1
+            if (currentToken.kind == EOF) {
+                continueReadInput();
+            }
+
+            Token head = currentToken, tail = tokenManager.getNextToken();
+
+            // case 2, 3 and 4
+            boolean setNotEnded =
+                    (currentToken.kind == COMPILE || currentToken.kind == EXECUTE)
+                            || (currentToken.kind == EXPLAIN
+                                    && (tail.kind == COMPILE
+                                            || tail.kind == EXECUTE
+                                            || tail.kind == BEGIN));
+
+            // case 5
+            boolean statementNotEnded = head.kind != SEMICOLON && tail.kind != SEMICOLON;
+
+            while (tail.kind != EOF) {
+                head.next = tail;
+                head = tail;
+                // update setEnded
+                if (tail.kind == END) {
+                    setNotEnded = false;
+                }
+                // update endedWithSemicolon
+                statementNotEnded = tail.kind != SEMICOLON;
+                tail = tokenManager.getNextToken();
+            }
+
+            // hasNext() needs an EOF tail token
+            head.next = tail;
+
+            if (setNotEnded || statementNotEnded) {
+                continueReadInput();
+            }
+        }
     }
 
     // ---------------------------------------------------------------------------------------------
-    private Optional<StatementType> getStatementType(List<Token> tokens) {
-        Token firstToken = tokens.get(0);
-
-        if (firstToken.kind == SEMICOLON) {
+    private Optional<StatementType> getStatementType(TokenIterator tokens) {
+        Token firstToken = tokens.next();
+        if (firstToken.kind == SEMICOLON && !tokens.hasNext()) {
             return Optional.empty();
         }
 
@@ -80,16 +150,20 @@ public class ClientParser implements SqlCommandParser, FlinkSqlParserImplConstan
             // it means the token is not a reserved keyword, potentially a client command
             return getPotentialCommandType(firstToken.image);
         } else if (firstToken.kind == EXPLAIN) {
-            return getPotentialExplainType(tokens);
+            return Optional.of(StatementType.EXPLAIN);
         } else if (firstToken.kind == SHOW) {
-            return getPotentialShowCreateType(tokens);
+            return Optional.of(
+                    tokens.nextTokenMatched(CREATE)
+                            ? StatementType.SHOW_CREATE
+                            : StatementType.OTHER);
         } else if (firstToken.kind == BEGIN) {
-            return getPotentialBeginStatementSetType(tokens);
+            return Optional.of(
+                    tokens.nextTokenMatched(STATEMENT) && tokens.nextTokenMatched(SET)
+                            ? StatementType.BEGIN_STATEMENT_SET
+                            : StatementType.OTHER);
         } else if (firstToken.kind == END) {
-            // an 'END;' statement should have exactly 2 tokens
-            return tokens.size() == 2
-                    ? Optional.of(StatementType.END)
-                    : Optional.of(StatementType.OTHER);
+            return Optional.of(
+                    tokens.nextTokenMatched(SEMICOLON) ? StatementType.END : StatementType.OTHER);
         } else {
             return Optional.of(StatementType.OTHER);
         }
@@ -109,38 +183,10 @@ public class ClientParser implements SqlCommandParser, FlinkSqlParserImplConstan
         }
     }
 
-    private Optional<StatementType> getPotentialExplainType(List<Token> tokens) {
-        Token secondToken = tokens.get(1), potentialEnd = tokens.get(tokens.size() - 2);
-        if ((secondToken.kind == EXECUTE || secondToken.kind == BEGIN)
-                && potentialEnd.kind != END) {
-            throw new SqlExecutionException("", new SqlParserEOFException(""));
-        }
-        return Optional.of(StatementType.EXPLAIN);
-    }
-
-    private Optional<StatementType> getPotentialShowCreateType(List<Token> tokens) {
-        // obviously a 'SHOW CREATE TABLE/VIEW' statement has more than 3 tokens
-        if (tokens.size() < 3) {
-            return Optional.of(StatementType.OTHER);
-        }
-        Token secondToken = tokens.get(1), thirdToken = tokens.get(2);
-        if (secondToken.kind == CREATE && (thirdToken.kind == TABLE || thirdToken.kind == VIEW)) {
-            return Optional.of(StatementType.SHOW_CREATE);
-        } else {
-            return Optional.of(StatementType.OTHER);
-        }
-    }
-
-    private Optional<StatementType> getPotentialBeginStatementSetType(List<Token> tokens) {
-        // obviously a 'BEGIN STATEMENT SET' statement has more than 3 tokens
-        if (tokens.size() < 3) {
-            return Optional.of(StatementType.OTHER);
-        }
-        Token secondToken = tokens.get(1), thirdToken = tokens.get(2);
-        if (secondToken.kind == STATEMENT && thirdToken.kind == SET) {
-            return Optional.of(StatementType.BEGIN_STATEMENT_SET);
-        } else {
-            return Optional.of(StatementType.OTHER);
-        }
+    private static void continueReadInput() {
+        // throw this to notify the terminal to continue reading input
+        throw new SqlExecutionException(
+                "The SQL statement is incomplete.",
+                new SqlParserEOFException("The SQL statement is incomplete."));
     }
 }
