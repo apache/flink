628b73c6069498ca7cb8db5e1473dea9978d7111
Sergey Nuyanzin <snuyanzin@gmail.com>
2022-10-07 13:33:05 +0200

    [hotfix] Remove unused imports, remove duplicate word 'with' from javadoc

--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/plan/nodes/logical/FlinkLogicalSink.scala
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/plan/nodes/logical/FlinkLogicalSink.scala
@@ -17,7 +17,7 @@
  */
 package org.apache.flink.table.planner.plan.nodes.logical
 
-import org.apache.flink.table.catalog.{CatalogTable, ContextResolvedTable, ObjectIdentifier, ResolvedCatalogTable}
+import org.apache.flink.table.catalog.ContextResolvedTable
 import org.apache.flink.table.connector.sink.DynamicTableSink
 import org.apache.flink.table.planner.plan.abilities.sink.SinkAbilitySpec
 import org.apache.flink.table.planner.plan.nodes.FlinkConventions
