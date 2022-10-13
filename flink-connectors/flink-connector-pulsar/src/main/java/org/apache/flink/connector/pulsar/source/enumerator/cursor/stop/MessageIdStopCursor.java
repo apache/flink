0991c782d764b1b1e027430cb3ade9786905a871
Yufan Sheng <yufan@streamnative.io>
2022-10-13 20:03:12 +0800

    Fix wrong batch size assertion.

--- flink-connectors/flink-connector-pulsar/src/main/java/org/apache/flink/connector/pulsar/source/enumerator/cursor/stop/MessageIdStopCursor.java
+++ flink-connectors/flink-connector-pulsar/src/main/java/org/apache/flink/connector/pulsar/source/enumerator/cursor/stop/MessageIdStopCursor.java
@@ -22,7 +22,6 @@ import org.apache.flink.connector.pulsar.source.enumerator.cursor.StopCursor;
 
 import org.apache.pulsar.client.api.Message;
 import org.apache.pulsar.client.api.MessageId;
-import org.apache.pulsar.client.impl.MessageIdImpl;
 
 import static org.apache.flink.connector.pulsar.source.enumerator.cursor.MessageIdUtils.unwrapMessageId;
 import static org.apache.flink.util.Preconditions.checkArgument;
@@ -41,13 +40,10 @@ public class MessageIdStopCursor implements StopCursor {
     private final boolean inclusive;
 
     public MessageIdStopCursor(MessageId messageId, boolean inclusive) {
-        MessageIdImpl idImpl = unwrapMessageId(messageId);
-        checkArgument(!earliest.equals(idImpl), "MessageId.earliest is not supported.");
-        checkArgument(
-                !latest.equals(idImpl),
-                "MessageId.latest is not supported, use LatestMessageStopCursor instead.");
+        checkArgument(!earliest.equals(messageId), "MessageId.earliest is not supported.");
+        checkArgument(!latest.equals(messageId), "Use LatestMessageStopCursor instead.");
 
-        this.messageId = idImpl;
+        this.messageId = unwrapMessageId(messageId);
         this.inclusive = inclusive;
     }
 
