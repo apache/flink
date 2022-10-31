37d0dd7b2252f0af724b57d66a70922f15dbfb62
Sergey Nuyanzin <snuyanzin@gmail.com>
2022-10-31 13:02:38 +0100

    Revert deletion of VoidNamespace#readResolve

--- flink-runtime/src/main/java/org/apache/flink/runtime/state/VoidNamespace.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/state/VoidNamespace.java
@@ -18,6 +18,8 @@
 
 package org.apache.flink.runtime.state;
 
+import java.io.ObjectStreamException;
+
 /** Singleton placeholder class for state without a namespace. */
 public final class VoidNamespace {
 
@@ -53,5 +55,14 @@ public final class VoidNamespace {
     @Override
     public String toString() {
         return getClass().getSimpleName();
+    }
+
+    // ------------------------------------------------------------------------
+    //  Singleton serialization
+    // ------------------------------------------------------------------------
+
+    // make sure that we preserve the singleton properly on serialization
+    private Object readResolve() throws ObjectStreamException {
+        return INSTANCE;
     }
 }
