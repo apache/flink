efee1392fd5936a99f8e21967875933aa6aa65e0
Weihua Hu <huweihua.ckl@gmail.com>
2022-11-10 17:19:27 +0800

    [FLINK-27341][runtime] Remove LOOPBACK from TaskManager findConnectingAddress.
    If JobManager and TaskManager run in same host, the previous logic will
    use LOOPBACK interface for TaskManager, thus other TaskManagers in
    different host can not connect with the TaskManager.

--- flink-runtime/src/main/java/org/apache/flink/runtime/net/ConnectionUtils.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/net/ConnectionUtils.java
@@ -58,7 +58,6 @@ public class ConnectionUtils {
      * state failed to determine the address.
      */
     private enum AddressDetectionState {
-        LOOPBACK(100),
         /** Connect from interface returned by InetAddress.getLocalHost(). * */
         LOCAL_HOST(200),
         /** Detect own IP address based on the target IP address. Look for common prefix */
@@ -116,7 +115,6 @@ public class ConnectionUtils {
         final List<AddressDetectionState> strategies =
                 Collections.unmodifiableList(
                         Arrays.asList(
-                                AddressDetectionState.LOOPBACK,
                                 AddressDetectionState.LOCAL_HOST,
                                 AddressDetectionState.ADDRESS,
                                 AddressDetectionState.FAST_CONNECT,
@@ -227,18 +225,6 @@ public class ConnectionUtils {
     private static InetAddress findAddressUsingStrategy(
             AddressDetectionState strategy, InetSocketAddress targetAddress, boolean logging)
             throws IOException {
-        if (strategy == AddressDetectionState.LOOPBACK) {
-            InetAddress loopback = InetAddress.getLoopbackAddress();
-
-            if (tryToConnect(loopback, targetAddress, strategy.getTimeout(), logging)) {
-                LOG.debug(
-                        "Using InetAddress.getLoopbackAddress() immediately for connecting address");
-                return loopback;
-            } else {
-                return null;
-            }
-        }
-
         // try LOCAL_HOST strategy independent of the network interfaces
         if (strategy == AddressDetectionState.LOCAL_HOST) {
             InetAddress localhostName;
@@ -446,7 +432,7 @@ public class ConnectionUtils {
                     }
 
                     if (targetAddress != null) {
-                        AddressDetectionState strategy = AddressDetectionState.LOOPBACK;
+                        AddressDetectionState strategy = AddressDetectionState.LOCAL_HOST;
 
                         boolean logging = elapsedTimeMillis >= startLoggingAfter.toMillis();
                         if (logging) {
@@ -463,9 +449,6 @@ public class ConnectionUtils {
 
                             // pick the next strategy
                             switch (strategy) {
-                                case LOOPBACK:
-                                    strategy = AddressDetectionState.LOCAL_HOST;
-                                    break;
                                 case LOCAL_HOST:
                                     strategy = AddressDetectionState.ADDRESS;
                                     break;
