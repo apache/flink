0b816680e51283d261eb7b7bce560dd1641691ce
Zakelly <zakelly.lan@gmail.com>
2022-11-21 20:43:01 +0800

    document the parallelism setting part

--- flink-connectors/flink-connector-kafka/src/main/java/org/apache/flink/streaming/connectors/kafka/shuffle/FlinkKafkaShuffle.java
+++ flink-connectors/flink-connector-kafka/src/main/java/org/apache/flink/streaming/connectors/kafka/shuffle/FlinkKafkaShuffle.java
@@ -342,6 +342,8 @@ public class FlinkKafkaShuffle {
                 "Missing partition number for Kafka Shuffle");
         int numberOfPartitions =
                 PropertiesUtil.getInt(kafkaProperties, PARTITION_NUMBER, Integer.MIN_VALUE);
+        // Set the parallelism / max parallelism of the keyed stream in consumer side as the number
+        // of kafka partitions
         DataStream<T> outputDataStream =
                 env.addSource(kafkaConsumer)
                         .setParallelism(numberOfPartitions)
