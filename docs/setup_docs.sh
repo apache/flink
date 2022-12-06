c6e824c955b22c9445e847c113170ef200e76ba3
Chesnay Schepler <chesnay@apache.org>
2022-12-06 11:27:10 +0100

    [hotfix] Remove out-dated comment

--- docs/setup_docs.sh
+++ docs/setup_docs.sh
@@ -51,8 +51,6 @@ rm -rf tmp
 mkdir tmp
 cd tmp
 
-# Since there's no documentation yet available for a release branch,
-# we only get the documentation from the main branch
 integrate_connector_docs elasticsearch v3.0.0
 integrate_connector_docs aws v3.0.0
 
