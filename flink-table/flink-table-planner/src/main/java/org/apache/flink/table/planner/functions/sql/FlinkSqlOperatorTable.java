ba991ea3b2228ef378aa9d6467a29a640925883d
shuiqiangchen <acqua.csq@gmail.com>
2022-12-07 16:35:37 +0800

    [FLINK-28882] Changed the return type of ENCODE from BINARY to VARBINARY

--- flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/functions/sql/FlinkSqlOperatorTable.java
+++ flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/functions/sql/FlinkSqlOperatorTable.java
@@ -818,7 +818,7 @@ public class FlinkSqlOperatorTable extends ReflectiveSqlOperatorTable {
                     "ENCODE",
                     SqlKind.OTHER_FUNCTION,
                     ReturnTypes.cascade(
-                            ReturnTypes.explicit(SqlTypeName.BINARY),
+                            ReturnTypes.explicit(SqlTypeName.VARBINARY),
                             SqlTypeTransforms.FORCE_NULLABLE),
                     null,
                     OperandTypes.family(SqlTypeFamily.STRING, SqlTypeFamily.STRING),
