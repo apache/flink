ad933adc5ee69f2234c0ba44b2c67f0be32c79f9
sunxia <xingbe528@gmail.com>
2023-01-08 23:40:22 +0800

    [FLINK-30480][runtime] Add benchmarks for adaptive batch scheduler.

--- flink-runtime/src/test/java/org/apache/flink/runtime/scheduler/benchmark/JobConfiguration.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/scheduler/benchmark/JobConfiguration.java
@@ -19,13 +19,15 @@
 package org.apache.flink.runtime.scheduler.benchmark;
 
 import org.apache.flink.api.common.ExecutionMode;
+import org.apache.flink.configuration.JobManagerOptions.HybridPartitionDataConsumeConstraint;
 import org.apache.flink.runtime.io.network.partition.ResultPartitionType;
 import org.apache.flink.runtime.jobgraph.DistributionPattern;
 import org.apache.flink.runtime.jobgraph.JobType;
 
 /**
  * {@link JobConfiguration} contains the configuration of a STREAMING/BATCH job. It concludes {@link
- * DistributionPattern}, {@link ResultPartitionType}, {@link JobType}, {@link ExecutionMode}.
+ * DistributionPattern}, {@link ResultPartitionType}, {@link JobType}, {@link ExecutionMode}, {@link
+ * HybridPartitionDataConsumeConstraint}.
  */
 public enum JobConfiguration {
     STREAMING(
@@ -44,6 +46,33 @@ public enum JobConfiguration {
             4000,
             false),
 
+    BATCH_HYBRID_DEFAULT(
+            DistributionPattern.ALL_TO_ALL,
+            ResultPartitionType.HYBRID_FULL,
+            JobType.BATCH,
+            ExecutionMode.BATCH,
+            HybridPartitionDataConsumeConstraint.UNFINISHED_PRODUCERS,
+            4000,
+            false),
+
+    BATCH_HYBRID_PARTIAL_FINISHED(
+            DistributionPattern.ALL_TO_ALL,
+            ResultPartitionType.HYBRID_FULL,
+            JobType.BATCH,
+            ExecutionMode.BATCH,
+            HybridPartitionDataConsumeConstraint.ONLY_FINISHED_PRODUCERS,
+            4000,
+            false),
+
+    BATCH_HYBRID_ALL_FINISHED(
+            DistributionPattern.ALL_TO_ALL,
+            ResultPartitionType.HYBRID_FULL,
+            JobType.BATCH,
+            ExecutionMode.BATCH,
+            HybridPartitionDataConsumeConstraint.ALL_PRODUCERS_FINISHED,
+            4000,
+            false),
+
     STREAMING_TEST(
             DistributionPattern.ALL_TO_ALL,
             ResultPartitionType.PIPELINED,
@@ -57,6 +86,33 @@ public enum JobConfiguration {
             ResultPartitionType.BLOCKING,
             JobType.BATCH,
             ExecutionMode.BATCH,
+            10,
+            false),
+
+    BATCH_HYBRID_DEFAULT_TEST(
+            DistributionPattern.ALL_TO_ALL,
+            ResultPartitionType.HYBRID_FULL,
+            JobType.BATCH,
+            ExecutionMode.BATCH,
+            HybridPartitionDataConsumeConstraint.UNFINISHED_PRODUCERS,
+            10,
+            false),
+
+    BATCH_HYBRID_PARTIAL_FINISHED_TEST(
+            DistributionPattern.ALL_TO_ALL,
+            ResultPartitionType.HYBRID_FULL,
+            JobType.BATCH,
+            ExecutionMode.BATCH,
+            HybridPartitionDataConsumeConstraint.ONLY_FINISHED_PRODUCERS,
+            10,
+            false),
+
+    BATCH_HYBRID_ALL_FINISHED_TEST(
+            DistributionPattern.ALL_TO_ALL,
+            ResultPartitionType.HYBRID_FULL,
+            JobType.BATCH,
+            ExecutionMode.BATCH,
+            HybridPartitionDataConsumeConstraint.ALL_PRODUCERS_FINISHED,
             10,
             false),
 
@@ -98,6 +154,7 @@ public enum JobConfiguration {
     private final JobType jobType;
     private final ExecutionMode executionMode;
     private final boolean evenlySpreadOutSlots;
+    private final HybridPartitionDataConsumeConstraint hybridPartitionDataConsumeConstraint;
 
     JobConfiguration(
             DistributionPattern distributionPattern,
@@ -106,10 +163,29 @@ public enum JobConfiguration {
             ExecutionMode executionMode,
             int parallelism,
             boolean evenlySpreadOutSlots) {
+        this(
+                distributionPattern,
+                resultPartitionType,
+                jobType,
+                executionMode,
+                HybridPartitionDataConsumeConstraint.UNFINISHED_PRODUCERS,
+                parallelism,
+                evenlySpreadOutSlots);
+    }
+
+    JobConfiguration(
+            DistributionPattern distributionPattern,
+            ResultPartitionType resultPartitionType,
+            JobType jobType,
+            ExecutionMode executionMode,
+            HybridPartitionDataConsumeConstraint hybridPartitionDataConsumeConstraint,
+            int parallelism,
+            boolean evenlySpreadOutSlots) {
         this.distributionPattern = distributionPattern;
         this.resultPartitionType = resultPartitionType;
         this.jobType = jobType;
         this.executionMode = executionMode;
+        this.hybridPartitionDataConsumeConstraint = hybridPartitionDataConsumeConstraint;
         this.parallelism = parallelism;
         this.evenlySpreadOutSlots = evenlySpreadOutSlots;
     }
@@ -132,6 +208,10 @@ public enum JobConfiguration {
 
     public ExecutionMode getExecutionMode() {
         return executionMode;
+    }
+
+    public HybridPartitionDataConsumeConstraint getHybridPartitionDataConsumeConstraint() {
+        return hybridPartitionDataConsumeConstraint;
     }
 
     public boolean isEvenlySpreadOutSlots() {
