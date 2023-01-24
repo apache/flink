b342d60e3978d4448b428e31a16a848b4be1e441
Hangxiang Yu <masteryhx@gmail.com>
2023-01-24 10:40:07 +0800

    [FLINK-30613][serializer] Migrate StateSerializerSnapshot to implement new method of resolving schema compatibility

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/functions/sink/TwoPhaseCommitSinkFunction.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/functions/sink/TwoPhaseCommitSinkFunction.java
@@ -27,6 +27,7 @@ import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeutils.CompositeTypeSerializerSnapshot;
 import org.apache.flink.api.common.typeutils.TypeSerializer;
+import org.apache.flink.api.common.typeutils.TypeSerializerSnapshot;
 import org.apache.flink.core.memory.DataInputView;
 import org.apache.flink.core.memory.DataOutputView;
 import org.apache.flink.runtime.state.FunctionInitializationContext;
@@ -923,8 +924,14 @@ public abstract class TwoPhaseCommitSinkFunction<IN, TXN, CONTEXT> extends RichS
 
         @Override
         protected OuterSchemaCompatibility resolveOuterSchemaCompatibility(
-                StateSerializer<TXN, CONTEXT> newSerializer) {
-            if (supportsNullTransaction != newSerializer.supportNullPendingTransaction) {
+                TypeSerializerSnapshot<State<TXN, CONTEXT>> oldSerializerSnapshot) {
+            if (!(oldSerializerSnapshot instanceof StateSerializerSnapshot)) {
+                return OuterSchemaCompatibility.INCOMPATIBLE;
+            }
+
+            StateSerializerSnapshot<TXN, CONTEXT> oldStateSerializerSnapshot =
+                    (StateSerializerSnapshot<TXN, CONTEXT>) oldSerializerSnapshot;
+            if (supportsNullTransaction != oldStateSerializerSnapshot.supportsNullTransaction) {
                 return OuterSchemaCompatibility.COMPATIBLE_AFTER_MIGRATION;
             }
 
