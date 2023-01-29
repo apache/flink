550d0c74e80f26e9f906c731d7cd5ca91f1426ad
zhengyunhong.zyh <337361684@qq.com>
2023-01-29 21:48:59 +0800

    add comments

--- flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/runtime/stream/sql/join/JoinReorderITCase.java
+++ flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/runtime/stream/sql/join/JoinReorderITCase.java
@@ -19,6 +19,7 @@
 package org.apache.flink.table.planner.runtime.stream.sql.join;
 
 import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.configuration.TaskManagerOptions;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.table.api.EnvironmentSettings;
@@ -49,6 +50,9 @@ public class JoinReorderITCase extends JoinReorderITCaseBase {
     @BeforeEach
     public void before() throws Exception {
         super.before();
+        // This conf is aims to fix IOException due to timeout occurring while requesting exclusive
+        // NetworkBuffer (see https://issues.apache.org/jira/browse/FLINK-30727).
+        tEnv.getConfig().getConfiguration().set(TaskManagerOptions.NETWORK_MEMORY_FRACTION, 0.4f);
     }
 
     @AfterEach
