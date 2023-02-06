582ea29f1713fa799af1cf9660dd146ff0461e83
Chesnay Schepler <chesnay@apache.org>
2023-02-06 10:35:43 +0100

    [FLINK-30905][docs] Upgrade Hugo to 0.110

--- .github/workflows/docs.sh
+++ .github/workflows/docs.sh
@@ -23,8 +23,8 @@ java -version
 javadoc -J-version
 
 # setup hugo
-HUGO_REPO=https://github.com/gohugoio/hugo/releases/download/v0.80.0/hugo_extended_0.80.0_Linux-64bit.tar.gz
-HUGO_ARTIFACT=hugo_extended_0.80.0_Linux-64bit.tar.gz
+HUGO_REPO=https://github.com/gohugoio/hugo/releases/download/v0.110.0/hugo_extended_0.110.0_Linux-64bit.tar.gz
+HUGO_ARTIFACT=hugo_extended_0.110.0_Linux-64bit.tar.gz
 if ! curl --fail -OL $HUGO_REPO ; then
 	echo "Failed to download Hugo binary"
 	exit 1
