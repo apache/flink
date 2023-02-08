3a1d6bea571934194da00a6fe51c0fc33406f182
Sergei Tsarev <sergei@fentik.com>
2023-02-08 20:52:44 +0000

    parametrize flink version

--- fentik-build.sh
+++ fentik-build.sh
@@ -7,7 +7,8 @@
 set -ex
 
 FLINK_DIR=build-target
-FLINK_BASE=flink-1.16.0
+FLINK_VER=1.16.0
+FLINK_BASE=flink-${FLINK_VER}
 LIB_DIR="$FLINK_DIR/lib/"
 OPT_DIR="$FLINK_DIR/opt/"
 GIT_SHA=$(git log -n 1 --format="%H" .)
@@ -16,19 +17,19 @@ mvn install -DskipTests -Dfast -f fentik-udf
 mvn install -DskipTests -Dfast
 
 # Copy some required libraries that are not part of the core Flink distribution.
-cp ./flink-connectors/flink-sql-connector-kafka/target/flink-sql-connector-kafka-1.16.0.jar $LIB_DIR
-cp ./flink-table/flink-table-runtime/target/flink-table-runtime-1.16.0.jar  $LIB_DIR
-cp ./flink-metrics/flink-metrics-prometheus/target/flink-metrics-prometheus-1.16.0.jar $LIB_DIR
+cp ./flink-connectors/flink-sql-connector-kafka/target/flink-sql-connector-kafka-${FLINK_VER}.jar $LIB_DIR
+cp ./flink-table/flink-table-runtime/target/flink-table-runtime-${FLINK_VER}.jar  $LIB_DIR
+cp ./flink-metrics/flink-metrics-prometheus/target/flink-metrics-prometheus-${FLINK_VER}.jar $LIB_DIR
 
 # Move flink-connector-hive jar, since it conflicts with flink-connector-files.
-mv $LIB_DIR/flink-connector-hive-1.16.0.jar $OPT_DIR
+# mv $LIB_DIR/flink-connector-hive-${FLINK_VER}.jar $OPT_DIR
 
 aws s3 cp s3://dev-dataflo/ops/ec2/flink-lib/libfb303-0.9.3.jar $LIB_DIR
 aws s3 cp s3://dev-dataflo/ops/ec2/flink-lib/hive-exec-3.1.2.jar $LIB_DIR
 aws s3 cp s3://dev-dataflo/ops/ec2/fentik-rescale-savepoint-0.1.0.jar $OPT_DIR
 
 mkdir -p $FLINK_DIR/plugins/s3-fs-presto
-cp ./flink-filesystems/flink-s3-fs-presto/target/flink-s3-fs-presto-1.16.0.jar $FLINK_DIR/plugins/s3-fs-presto/
+cp ./flink-filesystems/flink-s3-fs-presto/target/flink-s3-fs-presto-${FLINK_VER}.jar $FLINK_DIR/plugins/s3-fs-presto/
 
 if [ "$1" == "--package" ]; then
     if test -n "$(git status --porcelain=v1 2>/dev/null)"; then
