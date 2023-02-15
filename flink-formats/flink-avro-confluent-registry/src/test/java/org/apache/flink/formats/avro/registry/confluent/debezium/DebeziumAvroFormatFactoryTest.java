864caf9958f2ffe4732a1e1e47bea6a0ae215f4a
Fruzsina Nagy <fruzsifruzsifruzsi@gmail.com>
2023-02-15 11:41:10 +0100

    [FLINK-31085] Add schema option to confluent registry avro formats

--- flink-formats/flink-avro-confluent-registry/src/test/java/org/apache/flink/formats/avro/registry/confluent/debezium/DebeziumAvroFormatFactoryTest.java
+++ flink-formats/flink-avro-confluent-registry/src/test/java/org/apache/flink/formats/avro/registry/confluent/debezium/DebeziumAvroFormatFactoryTest.java
@@ -32,6 +32,7 @@ import org.apache.flink.table.runtime.connector.source.ScanRuntimeProviderContex
 import org.apache.flink.table.runtime.typeutils.InternalTypeInfo;
 import org.apache.flink.table.types.logical.RowType;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -40,6 +41,8 @@ import java.util.Map;
 import static org.apache.flink.table.factories.utils.FactoryMocks.createTableSink;
 import static org.apache.flink.table.factories.utils.FactoryMocks.createTableSource;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /** Tests for {@link DebeziumAvroFormatFactory}. */
 class DebeziumAvroFormatFactoryTest {
@@ -50,6 +53,68 @@ class DebeziumAvroFormatFactoryTest {
                     Column.physical("b", DataTypes.INT()),
                     Column.physical("c", DataTypes.BOOLEAN()));
 
+    private static final String RECORD_SCHEMA =
+            "    {\n"
+                    + "        \"type\": \"record\",\n"
+                    + "        \"name\": \"test\",\n"
+                    + "        \"fields\": [\n"
+                    + "            {\n"
+                    + "                \"name\": \"before\",\n"
+                    + "                \"type\": [\n"
+                    + "                    \"null\",\n"
+                    + "                    {\n"
+                    + "                        \"type\": \"record\",\n"
+                    + "                        \"name\": \"testSchema\",\n"
+                    + "                        \"fields\": [\n"
+                    + "                            {\n"
+                    + "                                \"name\": \"a\",\n"
+                    + "                                \"type\": [\n"
+                    + "                                    \"null\",\n"
+                    + "                                    \"string\"\n"
+                    + "                                ],\n"
+                    + "                                \"default\": null\n"
+                    + "                            },\n"
+                    + "                            {\n"
+                    + "                                \"name\": \"b\",\n"
+                    + "                                \"type\": [\n"
+                    + "                                    \"null\",\n"
+                    + "                                    \"int\"\n"
+                    + "                                ],\n"
+                    + "                                \"default\": null\n"
+                    + "                            },\n"
+                    + "                            {\n"
+                    + "                                \"name\": \"c\",\n"
+                    + "                                \"type\": [\n"
+                    + "                                    \"null\",\n"
+                    + "                                    \"boolean\"\n"
+                    + "                                ],\n"
+                    + "                                \"default\": null\n"
+                    + "                            }\n"
+                    + "                        ]\n"
+                    + "                    }\n"
+                    + "                ],\n"
+                    + "                \"default\": null\n"
+                    + "            },\n"
+                    + "            {\n"
+                    + "                \"name\": \"after\",\n"
+                    + "                \"type\": [\n"
+                    + "                    \"null\",\n"
+                    + "                    \"testSchema\"\n"
+                    + "                ],\n"
+                    + "                \"default\": null\n"
+                    + "            },\n"
+                    + "            {\n"
+                    + "                \"name\": \"op\",\n"
+                    + "                \"type\": [\n"
+                    + "                    \"null\",\n"
+                    + "                    \"string\"\n"
+                    + "                ],\n"
+                    + "                \"default\": null\n"
+                    + "            }\n"
+                    + "        ]\n"
+                    + "    }\n";
+    private static final String AVRO_SCHEMA = "[\n\"null\",\n" + RECORD_SCHEMA + "]";
+
     private static final RowType ROW_TYPE =
             (RowType) SCHEMA.toPhysicalRowDataType().getLogicalType();
 
@@ -59,22 +124,91 @@ class DebeziumAvroFormatFactoryTest {
     @Test
     void testSeDeSchema() {
         final Map<String, String> options = getAllOptions();
-
-        final Map<String, String> registryConfigs = new HashMap<>();
-        registryConfigs.put("basic.auth.user.info", "something1");
-        registryConfigs.put("basic.auth.credentials.source", "something2");
+        final Map<String, String> registryConfigs = getRegistryConfigs();
 
         DebeziumAvroDeserializationSchema expectedDeser =
                 new DebeziumAvroDeserializationSchema(
-                        ROW_TYPE, InternalTypeInfo.of(ROW_TYPE), REGISTRY_URL, registryConfigs);
+                        ROW_TYPE,
+                        InternalTypeInfo.of(ROW_TYPE),
+                        REGISTRY_URL,
+                        null,
+                        registryConfigs);
+        DeserializationSchema<RowData> actualDeser = createDeserializationSchema(options);
+        assertEquals(expectedDeser, actualDeser);
+
+        DebeziumAvroSerializationSchema expectedSer =
+                new DebeziumAvroSerializationSchema(
+                        ROW_TYPE, REGISTRY_URL, SUBJECT, null, registryConfigs);
+        SerializationSchema<RowData> actualSer = createSerializationSchema(options);
+        assertEquals(expectedSer, actualSer);
+    }
+
+    @Test
+    public void testSeDeSchemaWithSchemaOption() {
+        final Map<String, String> options = getAllOptions();
+        options.put("debezium-avro-confluent.schema", AVRO_SCHEMA);
+
+        final Map<String, String> registryConfigs = getRegistryConfigs();
+
+        DebeziumAvroDeserializationSchema expectedDeser =
+                new DebeziumAvroDeserializationSchema(
+                        ROW_TYPE,
+                        InternalTypeInfo.of(ROW_TYPE),
+                        REGISTRY_URL,
+                        AVRO_SCHEMA,
+                        registryConfigs);
         DeserializationSchema<RowData> actualDeser = createDeserializationSchema(options);
         assertThat(actualDeser).isEqualTo(expectedDeser);
 
         DebeziumAvroSerializationSchema expectedSer =
                 new DebeziumAvroSerializationSchema(
-                        ROW_TYPE, REGISTRY_URL, SUBJECT, registryConfigs);
+                        ROW_TYPE, REGISTRY_URL, SUBJECT, AVRO_SCHEMA, registryConfigs);
         SerializationSchema<RowData> actualSer = createSerializationSchema(options);
         assertThat(actualSer).isEqualTo(expectedSer);
+    }
+
+    @Test
+    public void testSeDeSchemaWithInvalidSchemaOption() {
+        final Map<String, String> options = getAllOptions();
+        options.put("debezium-avro-confluent.schema", RECORD_SCHEMA);
+
+        assertThrows(IllegalArgumentException.class, () -> createDeserializationSchema(options));
+        assertThrows(IllegalArgumentException.class, () -> createSerializationSchema(options));
+
+        String basicSchema = "[ \"null\", \"string\" ]";
+        options.put("debezium-avro-confluent.schema", basicSchema);
+        assertThrows(IllegalArgumentException.class, () -> createDeserializationSchema(options));
+        assertThrows(IllegalArgumentException.class, () -> createSerializationSchema(options));
+
+        String invalidSchema =
+                "[\"null\", "
+                        + "{ \"type\" : \"record\","
+                        + "\"name\" : \"debezium\","
+                        + "\"fields\": [{\n"
+                        + "        \"default\": null,\n"
+                        + "        \"name\": \"op\",\n"
+                        + "        \"type\": [\n"
+                        + "          \"null\",\n"
+                        + "          \"string\"\n"
+                        + "        ]\n"
+                        + "      }]"
+                        + "}]";
+        options.put("debezium-avro-confluent.schema", invalidSchema);
+        assertThrows(IllegalArgumentException.class, () -> createDeserializationSchema(options));
+        assertThrows(IllegalArgumentException.class, () -> createSerializationSchema(options));
+
+        String invalidRecordSchema = AVRO_SCHEMA.replace("int", "string");
+        options.put("debezium-avro-confluent.schema", invalidRecordSchema);
+        assertThrows(IllegalArgumentException.class, () -> createDeserializationSchema(options));
+        assertThrows(IllegalArgumentException.class, () -> createSerializationSchema(options));
+    }
+
+    @NotNull
+    private Map<String, String> getRegistryConfigs() {
+        final Map<String, String> registryConfigs = new HashMap<>();
+        registryConfigs.put("basic.auth.user.info", "something1");
+        registryConfigs.put("basic.auth.credentials.source", "something2");
+        return registryConfigs;
     }
 
     private Map<String, String> getAllOptions() {
