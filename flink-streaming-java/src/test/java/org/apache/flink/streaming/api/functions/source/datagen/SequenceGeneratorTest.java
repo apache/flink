bf205b50c0dbfbb394c24ede581a51ebc196e23a
xzw <xuzhiwen@deepexi.com>
2023-02-28 10:59:33 +0800

    [FLINK-31084][connectors/dataGen] Add default value for dataGen sequence mode and increase SequenceGenerator test class

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/api/functions/source/datagen/SequenceGeneratorTest.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/api/functions/source/datagen/SequenceGeneratorTest.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.flink.streaming.api.functions.source.datagen;
 
 import org.assertj.core.api.Assertions;
