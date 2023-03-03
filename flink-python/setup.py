38679f8ed4eb32c1ac65e57543d5822ed9faf6e2
Akhil Gupta <akhilg@fentik.com>
2023-03-03 13:06:05 -0800

    Fix python-flink dependencies.

--- flink-python/setup.py
+++ flink-python/setup.py
@@ -308,18 +308,15 @@ try:
         'pyflink.examples': ['*.py', '*/*.py'],
         'pyflink.bin': ['*']}
 
-    install_requires = ['py4j==0.10.9.5', 'python-dateutil==2.8.0', 'apache-beam==2.38.0',
-                        'cloudpickle==2.1.0', 'avro-python3>=1.8.1,!=1.9.2,<1.10.0',
+
+    install_requires = ['py4j==0.10.9.7', 'python-dateutil>=2.8.0,<3', 'apache-beam==2.43.0',
+                        'cloudpickle==2.2.0', 'avro-python3>=1.8.1,!=1.9.2,<1.10.0',
                         'pytz>=2018.3', 'fastavro>=1.1.0,<1.4.8', 'requests>=2.26.0',
                         'protobuf>=3.19.0,<=3.21',
-                        'numpy>=1.14.3,<1.20; python_full_version < "3.7"',
-                        'numpy>=1.21.4,<1.22.0; python_full_version >= "3.7"',
-                        'pandas>=1.0,<1.2.0; python_full_version < "3.7"',
-                        'pandas>=1.3.0,<1.4.0; python_full_version >= "3.7"',
-                        'pyarrow>=0.15.1,<7.0.0; python_full_version < "3.7"',
-                        'pyarrow>=5.0.0,<9.0.0; python_full_version >= "3.7"',
-                        'pemja==0.2.6;'
-                        'python_full_version >= "3.7" and platform_system != "Windows"',
+                        'numpy>=1.21.4,<1.22.0',
+                        'pandas>=1.3.0,<1.4.0',
+                        'pyarrow>=5.0.0,<9.0.0',
+                        'pemja==0.3.0;platform_system != "Windows"',
                         'httplib2>=0.19.0,<=0.20.4', apache_flink_libraries_dependency]
 
     setup(
