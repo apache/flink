d39ee9d2f104371ebeb459325d721a0e3fc5eebf
xuzifu666 <1206332514@qq.com>
2023-03-06 09:39:00 +0800

    Update flink-runtime/src/main/java/org/apache/flink/runtime/zookeeper/ZooKeeperStateHandleStore.java
    Co-authored-by: Roman <khachatryan.roman@gmail.com>

--- flink-runtime/src/main/java/org/apache/flink/runtime/zookeeper/ZooKeeperStateHandleStore.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/zookeeper/ZooKeeperStateHandleStore.java
@@ -362,9 +362,8 @@ public class ZooKeeperStateHandleStore<T extends Serializable>
             } else {
                 try {
                     return client.getChildren().forPath(path);
-                } catch (KeeperException.NoNodeException ignored) {
-                    // Concurrent deletion, retry
-					LOG.warn("NoNodeException happend, and retrying again.");
+                } catch (KeeperException.NoNodeException e) {
+					LOG.debug("Unable to get all handles, retrying (ZNode was likely deleted concurrently: {})", e.getMessage());
                 }
             }
         }
