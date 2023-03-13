181640f4e0a42e00c00c5f5b62fe61dba9448d2a
Akhil Gupta <akhilg@fentik.com>
2023-03-13 13:27:11 -0700

    Try using an updated version of apache-beam

--- flink-python/setup.py
+++ flink-python/setup.py
@@ -311,7 +311,7 @@ try:
 
     # (akhilg): Set the dependency of py4j from 0.10.9.7 to 0.10.9.5 to be compatible with pyspark==3.3 dependency
     # (akhilg): Set the dependency on numpy to include 1.22.0 to handle a security issue.
-    install_requires = ['py4j==0.10.9.5', 'python-dateutil>=2.8.0,<3', 'apache-beam==2.43.0',
+    install_requires = ['py4j==0.10.9.5', 'python-dateutil>=2.8.0,<3', 'apache-beam==2.45.0',
                         'cloudpickle==2.2.0', 'avro-python3>=1.8.1,!=1.9.2,<1.10.0',
                         'pytz>=2018.3', 'fastavro>=1.1.0,<1.4.8', 'requests>=2.26.0',
                         'protobuf>=3.19.0,<=3.21',
