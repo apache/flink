6daa5055747066d8740479c4603620fc8afd1065
Sergey Nuyanzin <snuyanzin@gmail.com>
2023-03-22 20:41:25 +0100

    [FLINK-24909][Table SQL/Client] Address feedback

--- flink-table/flink-sql-client/src/main/java/org/apache/flink/table/client/cli/parser/SqlClientSyntaxHighlighter.java
+++ flink-table/flink-sql-client/src/main/java/org/apache/flink/table/client/cli/parser/SqlClientSyntaxHighlighter.java
@@ -47,25 +47,25 @@ import static org.apache.flink.table.client.cli.CliClient.COLOR_SCHEMA_VAR;
 /** Sql Client syntax highlighter. */
 public class SqlClientSyntaxHighlighter extends DefaultHighlighter {
     private static final Logger LOG = LoggerFactory.getLogger(SqlClientSyntaxHighlighter.class);
-    private static Set<String> FLINK_KEYWORD_SET;
-    private static Set<Character> FLINK_KEYWORD_CHARACTER_SET;
+    private static Set<String> flinkKeywordSet;
+    private static Set<Character> flinkKeywordCharacterSet;
 
     static {
         try (InputStream is =
                 SqlClientSyntaxHighlighter.class.getResourceAsStream("/keywords.properties")) {
             Properties props = new Properties();
             props.load(is);
-            FLINK_KEYWORD_SET =
+            flinkKeywordSet =
                     Collections.unmodifiableSet(
                             Arrays.stream(props.get("default").toString().split(";"))
                                     .collect(Collectors.toSet()));
-            FLINK_KEYWORD_CHARACTER_SET =
-                    FLINK_KEYWORD_SET.stream()
+            flinkKeywordCharacterSet =
+                    flinkKeywordSet.stream()
                             .flatMap(t -> t.chars().mapToObj(c -> (char) c))
                             .collect(Collectors.toSet());
         } catch (IOException e) {
             LOG.error("Exception: ", e);
-            FLINK_KEYWORD_SET = Collections.emptySet();
+            flinkKeywordSet = Collections.emptySet();
         }
     }
 
@@ -95,61 +95,37 @@ public class SqlClientSyntaxHighlighter extends DefaultHighlighter {
 
     static AttributedString getHighlightedOutput(
             String buffer, SyntaxHighlightStyle style, SqlDialect dialect) {
-        final Set<Character> stateStartSymbols =
-                Arrays.stream(State.values())
-                        .map(t -> t.getStart(dialect).charAt(0))
-                        .collect(Collectors.toSet());
         final AttributedStringBuilder highlightedOutput = new AttributedStringBuilder();
         State currentParseState = null;
-        int counter = 0;
         StringBuilder word = new StringBuilder();
         for (int i = 0; i < buffer.length(); i++) {
-            final char c = buffer.charAt(i);
+            final char currentChar = buffer.charAt(i);
             if (currentParseState == null) {
-                if (stateStartSymbols.contains(c)) {
-                    for (State s : State.STATE_LIST) {
-                        final String stateStart = s.getStart(dialect);
-                        if (buffer.regionMatches(i, stateStart, 0, stateStart.length())) {
-                            handleWord(
-                                    word,
-                                    highlightedOutput,
-                                    currentParseState,
-                                    style,
-                                    true,
-                                    dialect);
-                            s.getStyleSetter().accept(highlightedOutput, style);
-                            highlightedOutput.append(stateStart);
-                            counter++;
-                            currentParseState = s;
-                            i += stateStart.length() - 1;
-                            break;
-                        }
-                    }
-                }
+                currentParseState = State.computeStateAt(buffer, i, dialect);
                 if (currentParseState == null) {
-                    if (!FLINK_KEYWORD_CHARACTER_SET.contains(Character.toUpperCase(c))) {
-                        handleWord(
-                                word, highlightedOutput, currentParseState, style, true, dialect);
-                        highlightedOutput.append(c);
+                    if (!flinkKeywordCharacterSet.contains(Character.toUpperCase(currentChar))) {
+                        handleWord(word, highlightedOutput, currentParseState, style, true);
+                        highlightedOutput.append(currentChar);
                     } else {
-                        word.append(c);
+                        word.append(currentChar);
                     }
+                } else {
+                    handleWord(word, highlightedOutput, null, style, true);
+                    currentParseState.getStyleSetter().accept(highlightedOutput, style);
+                    highlightedOutput.append(currentParseState.getStart());
+                    i += currentParseState.getStart().length() - 1;
                 }
             } else {
-                word.append(c);
-                final String stateEnd = currentParseState.getEnd(dialect);
+                word.append(currentChar);
+                final String stateEnd = currentParseState.getEnd();
                 if (buffer.regionMatches(i, stateEnd, 0, stateEnd.length())) {
-                    counter--;
-                    if (counter == 0) {
-                        handleWord(
-                                word, highlightedOutput, currentParseState, style, true, dialect);
-                        i += stateEnd.length() - 1;
-                        currentParseState = null;
-                    }
+                    handleWord(word, highlightedOutput, currentParseState, style, true);
+                    i += stateEnd.length() - 1;
+                    currentParseState = null;
                 }
             }
         }
-        handleWord(word, highlightedOutput, currentParseState, style, false, dialect);
+        handleWord(word, highlightedOutput, currentParseState, style, false);
         return highlightedOutput.toAttributedString();
     }
 
@@ -158,11 +134,10 @@ public class SqlClientSyntaxHighlighter extends DefaultHighlighter {
             AttributedStringBuilder sb,
             State currentState,
             SyntaxHighlightStyle style,
-            boolean turnOffHighlight,
-            SqlDialect dialect) {
+            boolean turnOffHighlight) {
         final String wordStr = word.toString();
         if (currentState == null) {
-            if (FLINK_KEYWORD_SET.contains(wordStr.toUpperCase(Locale.ROOT))) {
+            if (flinkKeywordSet.contains(wordStr.toUpperCase(Locale.ROOT))) {
                 sb.style(style.getKeywordStyle());
             } else {
                 sb.style(style.getDefaultStyle());
@@ -170,7 +145,7 @@ public class SqlClientSyntaxHighlighter extends DefaultHighlighter {
             sb.append(wordStr);
         } else if (turnOffHighlight) {
             sb.append(wordStr);
-            final String stateEnd = currentState.getEnd(dialect);
+            final String stateEnd = currentState.getEnd();
             if (stateEnd.length() > 1) {
                 sb.append(stateEnd.substring(1));
             }
@@ -181,36 +156,51 @@ public class SqlClientSyntaxHighlighter extends DefaultHighlighter {
         sb.style(style.getDefaultStyle());
     }
 
-    /** State of parser while preparing highlighted output. */
+    /**
+     * State of parser while preparing highlighted output. This class represents a state machine.
+     *
+     * <pre>
+     *      MultiLine Comment                Single Line Comment
+     *           |                                    |
+     *   (&#47;*,*&#47;) |                                    | (--, \n)
+     *           *------------Default-----------------*
+     *                        |    |
+     *                        |    |
+     *           *------------*    *------------------*
+     *  (&#47;*+,*&#47;) |            |    |                  | (', ')
+     *           |            |    |                  |
+     *         Hint           |    |                String
+     *                        |    |
+     *                        |    |
+     *           *------------*    *------------------*
+     *    (&quot;, &quot;) |                                    | (`, `)
+     *           |                                    |
+     *     Hive Identifier                  Flink Default Identifier
+     * </pre>
+     */
     private enum State {
-        QUOTED(
-                1,
-                (dialect) -> "'",
-                (dialect) -> "'",
-                (asb, style) -> asb.style(style.getQuotedStyle())),
+        QUOTED(1, "'", "'", dialect -> true, (asb, style) -> asb.style(style.getQuotedStyle())),
         SQL_QUOTED_IDENTIFIER(
                 2,
-                (dialect) -> dialect == SqlDialect.HIVE ? "\"" : "`",
-                (dialect) -> dialect == SqlDialect.HIVE ? "\"" : "`",
+                "`",
+                "`",
+                (dialect) -> dialect == SqlDialect.DEFAULT || dialect == null,
+                (asb, style) -> asb.style(style.getSqlIdentifierStyle())),
+        HIVE_SQL_QUOTED_IDENTIFIER(
+                2,
+                "\"",
+                "\"",
+                (dialect) -> dialect == SqlDialect.HIVE,
                 (asb, style) -> asb.style(style.getSqlIdentifierStyle())),
         ONE_LINE_COMMENTED(
-                3,
-                (dialect) -> "--",
-                (dialect) -> "\n",
-                (asb, style) -> asb.style(style.getCommentStyle())),
+                3, "--", "\n", dialect -> true, (asb, style) -> asb.style(style.getCommentStyle())),
         MULTILINE_COMMENTED(
-                5,
-                (dialect) -> "/*",
-                (dialect) -> "*/",
-                (asb, style) -> asb.style(style.getCommentStyle())),
-        HINTED(
-                4,
-                (dialect) -> "/*+",
-                (dialect) -> "*/",
-                (asb, style) -> asb.style(style.getHintStyle()));
+                5, "/*", "*/", dialect -> true, (asb, style) -> asb.style(style.getCommentStyle())),
+        HINTED(4, "/*+", "*/", dialect -> true, (asb, style) -> asb.style(style.getHintStyle()));
 
-        private final Function<SqlDialect, String> start;
-        private final Function<SqlDialect, String> end;
+        private final String start;
+        private final String end;
+        private final Function<SqlDialect, Boolean> condition;
 
         private final int order;
 
@@ -220,15 +210,21 @@ public class SqlClientSyntaxHighlighter extends DefaultHighlighter {
                 Arrays.stream(State.values())
                         .sorted(Comparator.comparingInt(o -> o.order))
                         .collect(Collectors.toList());
+        private static final Set<Character> STATE_START_SYMBOLS =
+                Arrays.stream(State.values())
+                        .map(t -> t.start.charAt(0))
+                        .collect(Collectors.toSet());
 
         State(
                 int order,
-                Function<SqlDialect, String> start,
-                Function<SqlDialect, String> end,
+                String start,
+                String end,
+                Function<SqlDialect, Boolean> condition,
                 BiConsumer<AttributedStringBuilder, SyntaxHighlightStyle> styleSetter) {
             this.start = start;
             this.end = end;
             this.order = order;
+            this.condition = condition;
             this.styleSetter = styleSetter;
         }
 
@@ -236,12 +232,26 @@ public class SqlClientSyntaxHighlighter extends DefaultHighlighter {
             return styleSetter;
         }
 
-        public String getStart(SqlDialect dialect) {
-            return start.apply(dialect);
+        public String getStart() {
+            return start;
         }
 
-        public String getEnd(SqlDialect dialect) {
-            return end.apply(dialect);
+        public String getEnd() {
+            return end;
+        }
+
+        public static State computeStateAt(String input, int pos, SqlDialect dialect) {
+            final char currentChar = input.charAt(pos);
+            if (!STATE_START_SYMBOLS.contains(currentChar)) {
+                return null;
+            }
+            for (State state : STATE_LIST) {
+                if (state.condition.apply(dialect)
+                        && state.start.regionMatches(0, input, pos, state.start.length())) {
+                    return state;
+                }
+            }
+            return null;
         }
     }
 }
