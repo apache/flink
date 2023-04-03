34a48cd7c46a3a5d33a5f5dc9c0eabab79ca3d58
Danny Cranmer <dannycranmer@apache.org>
2023-04-03 19:16:09 +0100

    [hotfix][Connectors/AWS][docs] Updating AWS docs branch to fix build

--- docs/setup_docs.sh
+++ docs/setup_docs.sh
@@ -44,7 +44,7 @@ mkdir tmp
 cd tmp
 
 integrate_connector_docs elasticsearch v3.0.0
-integrate_connector_docs aws v4.1.0
+integrate_connector_docs aws v4.1.0-docs
 integrate_connector_docs cassandra v3.0.0
 integrate_connector_docs pulsar main
 integrate_connector_docs jdbc v3.0.0
