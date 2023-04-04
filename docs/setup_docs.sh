b1b87252a0809fb4f25e9b182eb93611b7a918ed
Weijie Guo <reswqa@163.com>
2023-04-04 15:16:18 +0800

    [FLINK-31704][Connectors/Pulsar] Pulsar docs should be pulled from dedicated branch

--- docs/setup_docs.sh
+++ docs/setup_docs.sh
@@ -46,7 +46,7 @@ cd tmp
 integrate_connector_docs elasticsearch v3.0.0-docs
 integrate_connector_docs aws v4.1.0-docs
 integrate_connector_docs cassandra v3.0.0
-integrate_connector_docs pulsar main
+integrate_connector_docs pulsar v3.0.0-docs
 integrate_connector_docs jdbc v3.0.0
 integrate_connector_docs rabbitmq v3.0.0
 integrate_connector_docs gcp-pubsub v3.0.0
