24350b630bad0e3f2cf958f412b9d4dcc1c761b6
Shammon FY <zjureel@gmail.com>
2023-04-19 08:54:44 +0800

    [FLINK-31543][jdbc-driver] Add License

--- flink-table/flink-sql-jdbc-driver/src/test/java/org/apache/flink/table/jdbc/FlinkJdbcDriverTestBase.java
+++ flink-table/flink-sql-jdbc-driver/src/test/java/org/apache/flink/table/jdbc/FlinkJdbcDriverTestBase.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.table.jdbc;
 
 import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration;
@@ -11,6 +29,7 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.util.Properties;
 
+/** Base test for flink jdbc driver which will launch flink cluster and sql gatewa. */
 public class FlinkJdbcDriverTestBase {
     @RegisterExtension
     @Order(1)
