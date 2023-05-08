7d813376e905dee04041ada03cb4f3b8b33280fc
nateab <nbeshada+git@confluent.io>
2023-05-08 06:56:43 -0700

    [FLINK-31660][table] Bundle json-path in table-planner

--- flink-table/flink-table-planner-loader/src/main/java/org/apache/flink/table/planner/loader/PlannerModule.java
+++ flink-table/flink-table-planner-loader/src/main/java/org/apache/flink/table/planner/loader/PlannerModule.java
@@ -82,8 +82,6 @@ class PlannerModule {
 
         KNOWN_MODULE_ASSOCIATIONS.put("org.codehaus.janino", "flink-table-runtime");
         KNOWN_MODULE_ASSOCIATIONS.put("org.codehaus.commons", "flink-table-runtime");
-        KNOWN_MODULE_ASSOCIATIONS.put(
-                "org.apache.flink.table.shaded.com.jayway", "flink-table-runtime");
     }
 
     private final ClassLoader submoduleClassLoader;
