6df09d833ca71302d7f5d2cbac7601a207cc1bb7
Chesnay Schepler <chesnay@apache.org>
2023-06-15 12:51:19 +0200

    [FLINK-32336][tests] PartitionITCase#ComparablePojo now public

--- flink-tests/src/test/java/org/apache/flink/test/operators/PartitionITCase.java
+++ flink-tests/src/test/java/org/apache/flink/test/operators/PartitionITCase.java
@@ -718,7 +718,8 @@ public class PartitionITCase extends MultipleProgramsTestBase {
         }
     }
 
-    private static class ComparablePojo implements Comparable<ComparablePojo> {
+    /** A comparable POJO. */
+    public static class ComparablePojo implements Comparable<ComparablePojo> {
         private Long first;
         private Long second;
 
@@ -774,6 +775,9 @@ public class PartitionITCase extends MultipleProgramsTestBase {
         @Override
         public void mapPartition(Iterable<T> values, Collector<Tuple2<T, T>> out) throws Exception {
             Iterator<T> itr = values.iterator();
+            if (!itr.hasNext()) {
+                return;
+            }
             T min = itr.next();
             T max = min;
             T value;
