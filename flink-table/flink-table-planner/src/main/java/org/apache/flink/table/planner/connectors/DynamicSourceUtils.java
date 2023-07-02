b772261126e5fbf35dde451144c4d513ebf7f531
lincoln lee <lincoln.86xy@gmail.com>
2023-07-02 08:40:29 +0800

    fix tests

--- flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/connectors/DynamicSourceUtils.java
+++ flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/connectors/DynamicSourceUtils.java
@@ -487,7 +487,7 @@ public final class DynamicSourceUtils {
         final boolean hasUpdateBefore =
                 hasChangelogMode && changelogMode.contains(RowKind.UPDATE_BEFORE);
         final boolean hasUpdateAfter =
-                hasUpdateBefore && changelogMode.contains(RowKind.UPDATE_AFTER);
+                hasChangelogMode && changelogMode.contains(RowKind.UPDATE_AFTER);
         if (!hasUpdateBefore && hasUpdateAfter) {
             // only UPDATE_AFTER
             if (!schema.getPrimaryKey().isPresent()) {
@@ -506,7 +506,7 @@ public final class DynamicSourceUtils {
                             tableDebugName,
                             ScanTableSource.class.getSimpleName(),
                             scanSource.getClass().getName()));
-        } else if (hasUpdateBefore && !changelogMode.containsOnly(RowKind.INSERT)) {
+        } else if (hasChangelogMode && !changelogMode.containsOnly(RowKind.INSERT)) {
             // CDC mode (non-upsert mode and non-insert-only mode)
             final boolean changeEventsDuplicate =
                     config.get(ExecutionConfigOptions.TABLE_EXEC_SOURCE_CDC_EVENTS_DUPLICATE);
