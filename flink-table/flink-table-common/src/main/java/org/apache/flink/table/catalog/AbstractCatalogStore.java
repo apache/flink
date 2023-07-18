9e7ff20c82d21978a1dcec8c164bef85004d25ea
Feng Jin <jinfeng1995@gmail.com>
2023-07-18 20:33:02 +0800

    make isOpen and checkOpenState protected

--- flink-table/flink-table-common/src/main/java/org/apache/flink/table/catalog/AbstractCatalogStore.java
+++ flink-table/flink-table-common/src/main/java/org/apache/flink/table/catalog/AbstractCatalogStore.java
@@ -23,7 +23,8 @@ import org.apache.flink.util.Preconditions;
 /** The AbstractCatalogStore class is an abstract base class for implementing a catalog store. */
 public abstract class AbstractCatalogStore implements CatalogStore {
 
-    private boolean isOpen;
+    /** Catalog store state. */
+    protected boolean isOpen;
 
     /** Opens the catalog store. */
     @Override
@@ -38,20 +39,11 @@ public abstract class AbstractCatalogStore implements CatalogStore {
     }
 
     /**
-     * Returns whether the catalog store is currently open.
-     *
-     * @return true if the store is open, false otherwise
-     */
-    public boolean isOpen() {
-        return isOpen;
-    }
-
-    /**
      * Checks whether the catalog store is currently open.
      *
      * @throws IllegalStateException if the store is closed
      */
-    public void checkOpenState() {
-        Preconditions.checkState(isOpen(), "Catalog store is not open.");
+    protected void checkOpenState() {
+        Preconditions.checkState(isOpen, "Catalog store is not open.");
     }
 }
