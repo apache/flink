104e583ed5f67fbd9816cefe4cc75d92d8208135
ShammonFY <zjureel@gmail.com>
2023-07-19 16:40:57 +0800

    [FLINK-32617][jdbc-driver] Return true or false for most methods in database meta data

--- flink-table/flink-sql-jdbc-driver/src/main/java/org/apache/flink/table/jdbc/BaseDatabaseMetaData.java
+++ flink-table/flink-sql-jdbc-driver/src/main/java/org/apache/flink/table/jdbc/BaseDatabaseMetaData.java
@@ -33,78 +33,6 @@ public abstract class BaseDatabaseMetaData implements DatabaseMetaData {
     }
 
     @Override
-    public boolean nullsAreSortedHigh() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#nullsAreSortedHigh is not supported");
-    }
-
-    @Override
-    public boolean nullsAreSortedLow() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#nullsAreSortedLow is not supported");
-    }
-
-    @Override
-    public boolean nullsAreSortedAtStart() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#nullsAreSortedAtStart is not supported");
-    }
-
-    @Override
-    public boolean nullsAreSortedAtEnd() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#nullsAreSortedAtEnd is not supported");
-    }
-
-    @Override
-    public boolean supportsMixedCaseIdentifiers() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsMixedCaseIdentifiers is not supported");
-    }
-
-    @Override
-    public boolean storesUpperCaseIdentifiers() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#storesUpperCaseIdentifiers is not supported");
-    }
-
-    @Override
-    public boolean storesLowerCaseIdentifiers() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#storesLowerCaseIdentifiers is not supported");
-    }
-
-    @Override
-    public boolean storesMixedCaseIdentifiers() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#storesMixedCaseIdentifiers is not supported");
-    }
-
-    @Override
-    public boolean supportsMixedCaseQuotedIdentifiers() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsMixedCaseQuotedIdentifiers is not supported");
-    }
-
-    @Override
-    public boolean storesUpperCaseQuotedIdentifiers() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#storesUpperCaseQuotedIdentifiers is not supported");
-    }
-
-    @Override
-    public boolean storesLowerCaseQuotedIdentifiers() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#storesLowerCaseQuotedIdentifiers is not supported");
-    }
-
-    @Override
-    public boolean storesMixedCaseQuotedIdentifiers() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#storesMixedCaseQuotedIdentifiers is not supported");
-    }
-
-    @Override
     public String getSQLKeywords() throws SQLException {
         throw new SQLFeatureNotSupportedException(
                 "FlinkDatabaseMetaData#getSQLKeywords is not supported");
@@ -141,177 +69,15 @@ public abstract class BaseDatabaseMetaData implements DatabaseMetaData {
     }
 
     @Override
-    public boolean nullPlusNonNullIsNull() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#nullPlusNonNullIsNull is not supported");
-    }
-
-    @Override
-    public boolean supportsConvert() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsConvert is not supported");
-    }
-
-    @Override
-    public boolean supportsConvert(int fromType, int toType) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsConvert is not supported");
-    }
-
-    @Override
-    public boolean supportsMultipleResultSets() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsMultipleResultSets is not supported");
-    }
-
-    @Override
-    public boolean supportsMultipleTransactions() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsMultipleTransactions is not supported");
-    }
-
-    @Override
-    public boolean supportsMinimumSQLGrammar() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsMinimumSQLGrammar is not supported");
-    }
-
-    @Override
-    public boolean supportsCoreSQLGrammar() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsCoreSQLGrammar is not supported");
-    }
-
-    @Override
-    public boolean supportsExtendedSQLGrammar() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsExtendedSQLGrammar is not supported");
-    }
-
-    @Override
-    public boolean supportsANSI92EntryLevelSQL() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsANSI92EntryLevelSQL is not supported");
-    }
-
-    @Override
-    public boolean supportsANSI92IntermediateSQL() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsANSI92IntermediateSQL is not supported");
-    }
-
-    @Override
-    public boolean supportsANSI92FullSQL() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsANSI92FullSQL is not supported");
-    }
-
-    @Override
-    public boolean supportsIntegrityEnhancementFacility() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsIntegrityEnhancementFacility is not supported");
-    }
-
-    @Override
     public String getProcedureTerm() throws SQLException {
         throw new SQLFeatureNotSupportedException(
                 "FlinkDatabaseMetaData#getProcedureTerm is not supported");
     }
 
     @Override
-    public boolean isCatalogAtStart() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#isCatalogAtStart is not supported");
-    }
-
-    @Override
     public String getCatalogSeparator() throws SQLException {
         throw new SQLFeatureNotSupportedException(
                 "FlinkDatabaseMetaData#getCatalogSeparator is not supported");
-    }
-
-    @Override
-    public boolean supportsSchemasInProcedureCalls() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsSchemasInProcedureCalls is not supported");
-    }
-
-    @Override
-    public boolean supportsSchemasInIndexDefinitions() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsSchemasInIndexDefinitions is not supported");
-    }
-
-    @Override
-    public boolean supportsSchemasInPrivilegeDefinitions() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsSchemasInPrivilegeDefinitions is not supported");
-    }
-
-    @Override
-    public boolean supportsCatalogsInProcedureCalls() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsCatalogsInProcedureCalls is not supported");
-    }
-
-    @Override
-    public boolean supportsCatalogsInIndexDefinitions() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsCatalogsInIndexDefinitions is not supported");
-    }
-
-    @Override
-    public boolean supportsCatalogsInPrivilegeDefinitions() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsCatalogsInPrivilegeDefinitions is not supported");
-    }
-
-    @Override
-    public boolean supportsPositionedDelete() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsPositionedDelete is not supported");
-    }
-
-    @Override
-    public boolean supportsPositionedUpdate() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsPositionedUpdate is not supported");
-    }
-
-    @Override
-    public boolean supportsSelectForUpdate() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsSelectForUpdate is not supported");
-    }
-
-    @Override
-    public boolean supportsStoredProcedures() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsStoredProcedures is not supported");
-    }
-
-    @Override
-    public boolean supportsOpenCursorsAcrossCommit() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsOpenCursorsAcrossCommit is not supported");
-    }
-
-    @Override
-    public boolean supportsOpenCursorsAcrossRollback() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsOpenCursorsAcrossRollback is not supported");
-    }
-
-    @Override
-    public boolean supportsOpenStatementsAcrossCommit() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsOpenStatementsAcrossCommit is not supported");
-    }
-
-    @Override
-    public boolean supportsOpenStatementsAcrossRollback() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsOpenStatementsAcrossRollback is not supported");
     }
 
     @Override
@@ -405,12 +171,6 @@ public abstract class BaseDatabaseMetaData implements DatabaseMetaData {
     }
 
     @Override
-    public boolean doesMaxRowSizeIncludeBlobs() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#doesMaxRowSizeIncludeBlobs is not supported");
-    }
-
-    @Override
     public int getMaxStatementLength() throws SQLException {
         throw new SQLFeatureNotSupportedException(
                 "FlinkDatabaseMetaData#getMaxStatementLength is not supported");
@@ -444,42 +204,6 @@ public abstract class BaseDatabaseMetaData implements DatabaseMetaData {
     public int getDefaultTransactionIsolation() throws SQLException {
         throw new SQLFeatureNotSupportedException(
                 "FlinkDatabaseMetaData#getDefaultTransactionIsolation is not supported");
-    }
-
-    @Override
-    public boolean supportsTransactions() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsTransactions is not supported");
-    }
-
-    @Override
-    public boolean supportsTransactionIsolationLevel(int level) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsTransactionIsolationLevel is not supported");
-    }
-
-    @Override
-    public boolean supportsDataDefinitionAndDataManipulationTransactions() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsDataDefinitionAndDataManipulationTransactions is not supported");
-    }
-
-    @Override
-    public boolean supportsDataManipulationTransactionsOnly() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsDataManipulationTransactionsOnly is not supported");
-    }
-
-    @Override
-    public boolean dataDefinitionCausesTransactionCommit() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#dataDefinitionCausesTransactionCommit is not supported");
-    }
-
-    @Override
-    public boolean dataDefinitionIgnoredInTransactions() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#dataDefinitionIgnoredInTransactions is not supported");
     }
 
     @Override
@@ -572,100 +296,10 @@ public abstract class BaseDatabaseMetaData implements DatabaseMetaData {
     }
 
     @Override
-    public boolean supportsResultSetConcurrency(int type, int concurrency) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsResultSetConcurrency is not supported");
-    }
-
-    @Override
-    public boolean ownUpdatesAreVisible(int type) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#ownUpdatesAreVisible is not supported");
-    }
-
-    @Override
-    public boolean ownDeletesAreVisible(int type) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#ownDeletesAreVisible is not supported");
-    }
-
-    @Override
-    public boolean ownInsertsAreVisible(int type) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#ownInsertsAreVisible is not supported");
-    }
-
-    @Override
-    public boolean othersUpdatesAreVisible(int type) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#othersUpdatesAreVisible is not supported");
-    }
-
-    @Override
-    public boolean othersDeletesAreVisible(int type) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#othersDeletesAreVisible is not supported");
-    }
-
-    @Override
-    public boolean othersInsertsAreVisible(int type) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#othersInsertsAreVisible is not supported");
-    }
-
-    @Override
-    public boolean updatesAreDetected(int type) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#updatesAreDetected is not supported");
-    }
-
-    @Override
-    public boolean deletesAreDetected(int type) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#deletesAreDetected is not supported");
-    }
-
-    @Override
-    public boolean insertsAreDetected(int type) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#insertsAreDetected is not supported");
-    }
-
-    @Override
-    public boolean supportsBatchUpdates() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsBatchUpdates is not supported");
-    }
-
-    @Override
     public ResultSet getUDTs(
             String catalog, String schemaPattern, String typeNamePattern, int[] types)
             throws SQLException {
         throw new SQLFeatureNotSupportedException("FlinkDatabaseMetaData#getUDTs is not supported");
-    }
-
-    @Override
-    public boolean supportsSavepoints() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsSavepoints is not supported");
-    }
-
-    @Override
-    public boolean supportsNamedParameters() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsNamedParameters is not supported");
-    }
-
-    @Override
-    public boolean supportsMultipleOpenResults() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsMultipleOpenResults is not supported");
-    }
-
-    @Override
-    public boolean supportsGetGeneratedKeys() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsGetGeneratedKeys is not supported");
     }
 
     @Override
@@ -694,12 +328,6 @@ public abstract class BaseDatabaseMetaData implements DatabaseMetaData {
     }
 
     @Override
-    public boolean supportsResultSetHoldability(int holdability) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsResultSetHoldability is not supported");
-    }
-
-    @Override
     public int getResultSetHoldability() throws SQLException {
         throw new SQLFeatureNotSupportedException(
                 "FlinkDatabaseMetaData#getResultSetHoldability is not supported");
@@ -712,33 +340,9 @@ public abstract class BaseDatabaseMetaData implements DatabaseMetaData {
     }
 
     @Override
-    public boolean locatorsUpdateCopy() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#locatorsUpdateCopy is not supported");
-    }
-
-    @Override
-    public boolean supportsStatementPooling() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsStatementPooling is not supported");
-    }
-
-    @Override
     public RowIdLifetime getRowIdLifetime() throws SQLException {
         throw new SQLFeatureNotSupportedException(
                 "FlinkDatabaseMetaData#getRowIdLifetime is not supported");
-    }
-
-    @Override
-    public boolean supportsStoredFunctionsUsingCallSyntax() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#supportsStoredFunctionsUsingCallSyntax is not supported");
-    }
-
-    @Override
-    public boolean autoCommitFailureClosesAllResultSets() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#autoCommitFailureClosesAllResultSets is not supported");
     }
 
     @Override
@@ -774,19 +378,7 @@ public abstract class BaseDatabaseMetaData implements DatabaseMetaData {
     }
 
     @Override
-    public boolean generatedKeyAlwaysReturned() throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#generatedKeyAlwaysReturned is not supported");
-    }
-
-    @Override
     public <T> T unwrap(Class<T> iface) throws SQLException {
         throw new SQLFeatureNotSupportedException("FlinkDatabaseMetaData#unwrap is not supported");
-    }
-
-    @Override
-    public boolean isWrapperFor(Class<?> iface) throws SQLException {
-        throw new SQLFeatureNotSupportedException(
-                "FlinkDatabaseMetaData#isWrapperFor is not supported");
     }
 }
