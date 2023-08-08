9060a8739c325ec1e71e11644775e9052b391172
WangJiangZhou <1634846162@qq.com>
2023-08-08 13:46:07 +0800

    wjz

---
+++ flink-annotations/src/main/java/a.java
@@ -0,0 +1,11 @@
+/**
+ * @name: a
+ * @author: JZWang
+ * @date: 2023/8/8 13:45
+ * @description:
+ */
+public class a {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+    }
+}
