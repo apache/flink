0ee75e02cfa916485fe69d4c53062b955c0aa417
JunRuiLee <jrlee.ljr@gmail.com>
2023-08-15 15:22:07 +0800

    [FLINK-32788][runtime] Support the handling of exception when slow task detector notify slow tasks.

--- flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/slowtaskdetector/ExecutionTimeBasedSlowTaskDetector.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/slowtaskdetector/ExecutionTimeBasedSlowTaskDetector.java
@@ -29,7 +29,9 @@ import org.apache.flink.runtime.executiongraph.ExecutionAttemptID;
 import org.apache.flink.runtime.executiongraph.ExecutionGraph;
 import org.apache.flink.runtime.executiongraph.ExecutionJobVertex;
 import org.apache.flink.runtime.executiongraph.ExecutionVertex;
+import org.apache.flink.runtime.rpc.FatalErrorHandler;
 import org.apache.flink.runtime.scheduler.strategy.ExecutionVertexID;
+import org.apache.flink.util.FatalExitExceptionHandler;
 import org.apache.flink.util.IterableUtils;
 
 import java.util.Arrays;
@@ -43,6 +45,7 @@ import java.util.stream.Collectors;
 
 import static org.apache.flink.runtime.executiongraph.ExecutionVertex.NUM_BYTES_UNKNOWN;
 import static org.apache.flink.util.Preconditions.checkArgument;
+import static org.apache.flink.util.Preconditions.checkNotNull;
 import static org.apache.flink.util.Preconditions.checkState;
 
 /** The slow task detector which detects slow tasks based on their execution time. */
@@ -57,6 +60,11 @@ public class ExecutionTimeBasedSlowTaskDetector implements SlowTaskDetector {
     private final double baselineMultiplier;
 
     private ScheduledFuture<?> scheduledDetectionFuture;
+
+    private FatalErrorHandler fatalErrorHandler =
+            throwable ->
+                    FatalExitExceptionHandler.INSTANCE.uncaughtException(
+                            Thread.currentThread(), throwable);
 
     public ExecutionTimeBasedSlowTaskDetector(Configuration configuration) {
         this.checkIntervalMillis =
@@ -94,6 +102,13 @@ public class ExecutionTimeBasedSlowTaskDetector implements SlowTaskDetector {
                 this.baselineMultiplier);
     }
 
+    @VisibleForTesting
+    ExecutionTimeBasedSlowTaskDetector(
+            Configuration configuration, FatalErrorHandler fatalErrorHandler) {
+        this(configuration);
+        this.fatalErrorHandler = checkNotNull(fatalErrorHandler);
+    }
+
     @Override
     public void start(
             final ExecutionGraph executionGraph,
@@ -110,7 +125,11 @@ public class ExecutionTimeBasedSlowTaskDetector implements SlowTaskDetector {
         this.scheduledDetectionFuture =
                 mainThreadExecutor.schedule(
                         () -> {
-                            listener.notifySlowTasks(findSlowTasks(executionGraph));
+                            try {
+                                listener.notifySlowTasks(findSlowTasks(executionGraph));
+                            } catch (Throwable throwable) {
+                                fatalErrorHandler.onFatalError(throwable);
+                            }
                             scheduleTask(executionGraph, listener, mainThreadExecutor);
                         },
                         checkIntervalMillis,
@@ -266,6 +285,11 @@ public class ExecutionTimeBasedSlowTaskDetector implements SlowTaskDetector {
         if (scheduledDetectionFuture != null) {
             scheduledDetectionFuture.cancel(false);
         }
+    }
+
+    @VisibleForTesting
+    ScheduledFuture<?> getScheduledDetectionFuture() {
+        return scheduledDetectionFuture;
     }
 
     /** This class defines the execution time and input bytes for an execution. */
