8a314a19e9d36d424ca2935e7a7c3c256c2b1dab
Shengkai <1059623455@qq.com>
2023-08-23 14:43:53 +0800

    [FLINK-32731][e2e] Add retry mechanism when fails to start the namenode

--- flink-end-to-end-tests/flink-sql-gateway-test/src/test/java/org/apache/flink/table/gateway/containers/HiveContainer.java
+++ flink-end-to-end-tests/flink-sql-gateway-test/src/test/java/org/apache/flink/table/gateway/containers/HiveContainer.java
@@ -20,6 +20,11 @@ package org.apache.flink.table.gateway.containers;
 import org.apache.flink.test.parameters.ParameterProperty;
 import org.apache.flink.util.DockerImageVersions;
 
+import com.github.dockerjava.api.command.InspectContainerResponse;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.junit.runner.Description;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,6 +44,7 @@ public class HiveContainer extends GenericContainer<HiveContainer> {
 
     public static final String HOST_NAME = "hadoop-master";
     public static final int HIVE_METASTORE_PORT = 9083;
+    private static final int NAME_NODE_WEB_PORT = 50070;
 
     // Detailed log paths are from
     // https://github.com/prestodb/docker-images/tree/master/prestodb/hdp2.6-hive/files/etc/supervisord.d
@@ -53,7 +59,9 @@ public class HiveContainer extends GenericContainer<HiveContainer> {
     public HiveContainer() {
         super(DockerImageName.parse(DockerImageVersions.HIVE2));
         withExtraHost(HOST_NAME, "127.0.0.1");
+        withStartupAttempts(3);
         addExposedPort(HIVE_METASTORE_PORT);
+        addExposedPort(NAME_NODE_WEB_PORT);
     }
 
     @Override
@@ -61,6 +69,28 @@ public class HiveContainer extends GenericContainer<HiveContainer> {
         super.doStart();
         if (LOG.isInfoEnabled()) {
             followOutput(new Slf4jLogConsumer(LOG));
+        }
+    }
+
+    @Override
+    protected void containerIsStarted(InspectContainerResponse containerInfo) {
+        super.containerIsStarted(containerInfo);
+        final Request request =
+                new Request.Builder()
+                        .post(new FormBody.Builder().build())
+                        .url(
+                                String.format(
+                                        "http://127.0.0.1:%s", getMappedPort(NAME_NODE_WEB_PORT)))
+                        .build();
+        OkHttpClient client = new OkHttpClient();
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new RuntimeException(
+                        String.format(
+                                "The rest request is not successful: %s", response.message()));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
