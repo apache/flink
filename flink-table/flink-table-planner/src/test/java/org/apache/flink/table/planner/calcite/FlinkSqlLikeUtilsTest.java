af83abeec86c006ef00fe1f627ef517d4b24d5fa
yeming <xishuaidelin@gmail.com>
2023-08-25 14:23:35 +0800

    [FLINK-32824][table] format.

--- flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/calcite/FlinkSqlLikeUtilsTest.java
+++ flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/calcite/FlinkSqlLikeUtilsTest.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.flink.table.planner.calcite;
 
 import org.apache.flink.table.functions.SqlLikeUtils;
