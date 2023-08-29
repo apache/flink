949840fe6536a5fea11365bf2fec61b2fbf6f640
Akira Ajisaka <akiraaj@amazon.com>
2023-08-29 16:03:28 +0900

    Use Python 3.10 miniconda

--- docs/static/downloads/setup-pyflink-virtual-env.sh
+++ docs/static/downloads/setup-pyflink-virtual-env.sh
@@ -16,11 +16,11 @@
 set -e
 # download miniconda.sh
 if [[ `uname -s` == "Darwin" && `uname -m` == "arm64" ]]; then
-    wget "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh" -O "miniconda.sh"
+    wget "https://repo.anaconda.com/miniconda/Miniconda3-py310_23.5.2-0-MacOSX-arm64.sh" -O "miniconda.sh"
 elif [[ `uname -s` == "Darwin" ]]; then
-    wget "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh" -O "miniconda.sh"
+    wget "https://repo.anaconda.com/miniconda/Miniconda3-py310_23.5.2-0-MacOSX-x86_64.sh" -O "miniconda.sh"
 else
-    wget "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" -O "miniconda.sh"
+    wget "https://repo.anaconda.com/miniconda/Miniconda3-py310_23.5.2-0-Linux-x86_64.sh" -O "miniconda.sh"
 fi
 
 # add the execution permission
