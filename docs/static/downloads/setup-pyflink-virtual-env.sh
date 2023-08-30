8bc7281f4782188248d5a93109ed6189c7167533
Akira Ajisaka <akiraaj@amazon.com>
2023-08-30 14:50:01 +0900

    Simplify if statements

--- docs/static/downloads/setup-pyflink-virtual-env.sh
+++ docs/static/downloads/setup-pyflink-virtual-env.sh
@@ -15,10 +15,12 @@
 # limitations under the License.
 set -e
 # download miniconda.sh
-if [[ `uname -s` == "Darwin" && `uname -m` == "arm64" ]]; then
-    wget "https://repo.anaconda.com/miniconda/Miniconda3-py310_23.5.2-0-MacOSX-arm64.sh" -O "miniconda.sh"
-elif [[ `uname -s` == "Darwin" ]]; then
-    wget "https://repo.anaconda.com/miniconda/Miniconda3-py310_23.5.2-0-MacOSX-x86_64.sh" -O "miniconda.sh"
+if [[ `uname -s` == "Darwin" ]]; then
+    if [[ `uname -m` == "arm64" ]]; then
+        wget "https://repo.anaconda.com/miniconda/Miniconda3-py310_23.5.2-0-MacOSX-arm64.sh" -O "miniconda.sh"
+    else
+        wget "https://repo.anaconda.com/miniconda/Miniconda3-py310_23.5.2-0-MacOSX-x86_64.sh" -O "miniconda.sh"
+    fi
 else
     wget "https://repo.anaconda.com/miniconda/Miniconda3-py310_23.5.2-0-Linux-x86_64.sh" -O "miniconda.sh"
 fi
