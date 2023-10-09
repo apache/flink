53ece12c25579497338ed59a7aebe70f2b3d9ed6
Dawid Wysakowicz <dwysakowicz@apache.org>
2023-10-09 16:39:51 +0200

    [FLINK-33223] MATCH_RECOGNIZE AFTER MATCH clause can not be deserialised from a compiled plan

--- flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/plan/nodes/exec/stream/MatchRecognizeJsonPlanTest.java
+++ flink-table/flink-table-planner/src/test/java/org/apache/flink/table/planner/plan/nodes/exec/stream/MatchRecognizeJsonPlanTest.java
@@ -76,4 +76,70 @@ public class MatchRecognizeJsonPlanTest extends TableTestBase {
                         + "     ) AS T";
         util.verifyJsonPlan(sql);
     }
+
+    @Test
+    public void testSkipToLast() {
+        doTestAfterMatch("AFTER MATCH SKIP TO LAST B");
+    }
+
+    @Test
+    public void testSkipToFirst() {
+        doTestAfterMatch("AFTER MATCH SKIP TO FIRST B");
+    }
+
+    @Test
+    public void testSkipPastLastRow() {
+        doTestAfterMatch("AFTER MATCH SKIP PAST LAST ROW");
+    }
+
+    @Test
+    public void testSkipToNextRow() {
+        doTestAfterMatch("AFTER MATCH SKIP TO NEXT ROW");
+    }
+
+    private void doTestAfterMatch(Object afterClause) {
+        String srcTableDdl =
+                "CREATE TABLE MyTable (\n"
+                        + "  vehicle_id bigint,\n"
+                        + "  engine_temperature int,\n"
+                        + "  rowtime timestamp_ltz(3),"
+                        + "  WATERMARK FOR rowtime AS SOURCE_WATERMARK()\n"
+                        + ") with (\n"
+                        + "  'connector' = 'values',\n"
+                        + "  'bounded' = 'false')";
+        tEnv.executeSql(srcTableDdl);
+        String sinkTableDdl =
+                "CREATE TABLE MySink (\n"
+                        + "  vehicle_id bigint,\n"
+                        + "  startTime timestamp_ltz(3),\n"
+                        + "  endTime timestamp_ltz(3),\n"
+                        + "  Initial_Temp int,\n"
+                        + "  Final_Temp int\n"
+                        + ") with (\n"
+                        + "  'connector' = 'values',\n"
+                        + "  'sink-insert-only' = 'false',\n"
+                        + "  'table-sink-class' = 'DEFAULT')";
+        tEnv.executeSql(sinkTableDdl);
+
+        String sql =
+                "insert into MySink"
+                        + " SELECT * FROM\n"
+                        + " MyTable\n"
+                        + "   MATCH_RECOGNIZE(\n"
+                        + "   PARTITION BY vehicle_id\n"
+                        + "   ORDER BY `rowtime`\n"
+                        + "   MEASURES \n"
+                        + "       FIRST(A.`rowtime`) as startTime,\n"
+                        + "       LAST(A.`rowtime`) as endTime,\n"
+                        + "       FIRST(A.engine_temperature) as Initial_Temp,\n"
+                        + "       LAST(A.engine_temperature) as Final_Temp\n"
+                        + "   ONE ROW PER MATCH\n"
+                        + "   %s\n"
+                        + "   PATTERN (A+ B)\n"
+                        + "   DEFINE\n"
+                        + "       A as LAST(A.engine_temperature,1) is NULL OR A.engine_temperature > LAST(A.engine_temperature,1),\n"
+                        + "       B as B.engine_temperature < LAST(A.engine_temperature)\n"
+                        + "   )MR;";
+        util.verifyJsonPlan(String.format(sql, afterClause));
+    }
 }
