f1daffb7ffa5366866f22b0dd742ebcf1a3454e1
yu <13485876233>
2023-10-09 13:40:48 +0800

    FLINK-30893 Replace not specifying a driver in Minikube by something else

--- flink-end-to-end-tests/test-scripts/common_kubernetes.sh
+++ flink-end-to-end-tests/test-scripts/common_kubernetes.sh
@@ -24,7 +24,7 @@ CONTAINER_SCRIPTS=${END_TO_END_DIR}/test-scripts/container-scripts
 MINIKUBE_START_RETRIES=3
 MINIKUBE_START_BACKOFF=5
 RESULT_HASH="e682ec6622b5e83f2eb614617d5ab2cf"
-MINIKUBE_VERSION="v1.28.0"
+NO_SPECIFY_MINIKUBE_VERSION=false
 
 NON_LINUX_ENV_NOTE="****** Please start/stop minikube manually in non-linux environment. ******"
 
@@ -35,6 +35,7 @@ function setup_kubernetes_for_linux {
     else
         local arch='amd64'
     fi
+
     # Download kubectl, which is a requirement for using minikube.
     if ! [ -x "$(command -v kubectl)" ]; then
         echo "Installing kubectl ..."
@@ -43,22 +44,39 @@ function setup_kubernetes_for_linux {
             chmod +x kubectl && sudo mv kubectl /usr/local/bin/
     fi
 
-    # Download minikube when it is not installed beforehand.
-    if [ -x "$(command -v minikube)" ] && [[ "$(minikube version | grep -c $MINIKUBE_VERSION)" == "0" ]]; then
-      echo "Removing any already installed minikube binaries ..."
-      sudo rm "$(which minikube)"
-    fi
+    if [[ ${NO_SPECIFY_MINIKUBE_VERSION} == false ]]; then
+        MINIKUBE_VERSION="v1.28.0"
+        # Download minikube when it is not installed beforehand.
+        if [ -x "$(command -v minikube)" ] && [[ "$(minikube version | grep -c $MINIKUBE_VERSION)" == "0" ]]; then
+          remove_old_minikube_version
+        fi
 
-    if ! [ -x "$(command -v minikube)" ]; then
-      echo "Installing minikube $MINIKUBE_VERSION ..."
-      curl -Lo minikube https://storage.googleapis.com/minikube/releases/$MINIKUBE_VERSION/minikube-linux-$arch && \
-          chmod +x minikube && sudo mv minikube /usr/bin/minikube
-    fi
+        if ! [ -x "$(command -v minikube)" ]; then
+          echo "Installing minikube $MINIKUBE_VERSION ..."
+          curl -Lo minikube https://storage.googleapis.com/minikube/releases/$MINIKUBE_VERSION/minikube-linux-$arch && \
+              chmod +x minikube && sudo mv minikube /usr/bin/minikube
+        fi
+    else
+        # Download minikube when it is not installed beforehand.
+        if [ -x "$(command -v minikube)" ]; then
+            minikubeVersionArray=(`minikube update-check | awk '{ print $2 }'`)
+            if [ ${#minikubeVersionArray[@]} == 2 ];then
+                CurrentVersion=${minikubeVersionArray[0]}
+                LatestVersion=${minikubeVersionArray[1]}
+                echo "minikube latest version $LatestVersion ..."
+                if [[ ${CurrentVersion} != ${LatestVersion} ]];then
+                    remove_old_minikube_version
+                fi
+            else
+                remove_old_minikube_version
+            fi
+        fi
 
-    if ! [ -x "$(command -v minikube)" ]; then
-      echo "Installing latest minikube ..."
-      curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-$arch && \
-          chmod +x minikube && sudo mv minikube /usr/bin/minikube
+        if ! [ -x "$(command -v minikube)" ]; then
+          echo "Installing latest version minikube ..."
+          curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-$arch && \
+              chmod +x minikube && sudo mv minikube /usr/bin/minikube
+        fi
     fi
 
     # conntrack is required for minikube 1.9 and later
@@ -263,6 +281,11 @@ function get_host_machine_address {
     else
         echo $(hostname --ip-address)
     fi
+}
+
+function remove_old_minikube_version {
+    echo "Removing any already installed minikube binaries ..."
+    sudo rm "$(which minikube)"
 }
 
 on_exit cleanup
