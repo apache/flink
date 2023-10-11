b14411e7062c235cb28582a37e37decc7c425476
Zhanghao Chen <m1a2@outlook.com>
2023-10-11 17:27:41 +0800

    [FLINK-33166][core][config] Make root logger level configurable (#23476)
    * [FLINK-33166][core][config] Make root logger level configurable

--- flink-core/src/main/java/org/apache/flink/configuration/CoreOptions.java
+++ flink-core/src/main/java/org/apache/flink/configuration/CoreOptions.java
@@ -81,7 +81,7 @@ public class CoreOptions {
      *
      * <p>It is important that all classes whose objects move between Flink's runtime and any user
      * code (including Flink connectors that run as part of the user code) are covered by these
-     * patterns here. Otherwise it is be possible that the Flink runtime and the user code load two
+     * patterns here. Otherwise, it is possible that the Flink runtime and the user code load two
      * different copies of a class through the different class loaders. That leads to errors like "X
      * cannot be cast to X" exceptions, where both class names are equal, or "X cannot be assigned
      * to Y", where X should be a subclass of Y.
@@ -273,7 +273,7 @@ public class CoreOptions {
                                     .build());
 
     /**
-     * This options is here only for documentation generation, it is only evaluated in the shell
+     * This option is here only for documentation generation, it is only evaluated in the shell
      * scripts.
      */
     @SuppressWarnings("unused")
@@ -297,7 +297,7 @@ public class CoreOptions {
                             "Defines the directory where the flink-<host>-<process>.pid files are saved.");
 
     /**
-     * This options is here only for documentation generation, it is only evaluated in the shell
+     * This option is here only for documentation generation, it is only evaluated in the shell
      * scripts.
      */
     @SuppressWarnings("unused")
@@ -306,6 +306,17 @@ public class CoreOptions {
                     .intType()
                     .defaultValue(10)
                     .withDescription("The maximum number of old log files to keep.");
+
+    /**
+     * This option is here only for documentation generation, it is only evaluated in the shell
+     * scripts.
+     */
+    @SuppressWarnings("unused")
+    public static final ConfigOption<String> FLINK_LOG_LEVEL =
+            ConfigOptions.key("env.log.level")
+                    .stringType()
+                    .defaultValue("INFO")
+                    .withDescription("Defines the level of the root logger.");
 
     /**
      * This option is here only for documentation generation, it is only evaluated in the shell
@@ -322,7 +333,7 @@ public class CoreOptions {
                                     + "Note that redirected files do not support rolling rotate.");
 
     /**
-     * This options is here only for documentation generation, it is only evaluated in the shell
+     * This option is here only for documentation generation, it is only evaluated in the shell
      * scripts.
      */
     @SuppressWarnings("unused")
@@ -336,7 +347,7 @@ public class CoreOptions {
                                     + " stop-zookeeper-quorum.sh).");
 
     /**
-     * This options is here only for documentation generation, it is only evaluated in the shell
+     * This option is here only for documentation generation, it is only evaluated in the shell
      * scripts.
      */
     @SuppressWarnings("unused")
@@ -349,7 +360,7 @@ public class CoreOptions {
                                     + " configuration. You can also set it via environment variable.");
 
     /**
-     * This options is here only for documentation generation, it is only evaluated in the shell
+     * This option is here only for documentation generation, it is only evaluated in the shell
      * scripts.
      */
     @SuppressWarnings("unused")
@@ -362,7 +373,7 @@ public class CoreOptions {
                                     + " set it via environment variable.");
 
     /**
-     * This options is here only for documentation generation, it is only evaluated in the shell
+     * This option is here only for documentation generation, it is only evaluated in the shell
      * scripts.
      */
     @SuppressWarnings("unused")
