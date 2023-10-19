265d81741654ff70836485e8e8d31ce33a87e960
zoudan <zoudan@bytedance.com>
2023-10-19 17:48:05 +0800

    [FLINK-33313][table] Fix RexNodeExtractor#extractConjunctiveConditions throws an Exception when handle binary literal
    Close apache/flink#23551

--- flink-table/flink-table-planner/src/test/scala/org/apache/flink/table/planner/plan/utils/RexNodeRewriterTest.scala
+++ flink-table/flink-table-planner/src/test/scala/org/apache/flink/table/planner/plan/utils/RexNodeRewriterTest.scala
@@ -39,12 +39,13 @@ class RexNodeRewriterTest extends RexNodeTestBase {
           "$2",
           "$3",
           "$4",
+          "$5",
           "*($t2, $t3)",
           "100",
-          "<($t5, $t6)",
+          "<($t6, $t7)",
           "6",
-          ">($t1, $t8)",
-          "AND($t7, $t9)")))
+          ">($t1, $t9)",
+          "AND($t8, $t10)")))
 
     // use amount, id, price fields to create a new RexProgram
     val usedFields = Array(2, 3, 1)
