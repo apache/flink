f67849812443f3d41c0d7b664f79240d601f0258
yu <13485876233>
2023-10-20 14:34:40 +0800

    FLINK-30893 Replace not specifying a driver in Minikube by something

--- flink-end-to-end-tests/test-scripts/common_kubernetes.sh
+++ flink-end-to-end-tests/test-scripts/common_kubernetes.sh
@@ -54,7 +54,7 @@ function setup_kubernetes_for_linux {
           chmod +x minikube && sudo mv minikube /usr/bin/minikube
     fi
 
-    # Install the latest version, avoid none driver of minikube in the future
+    # Install the latest version, avoid none driver of minikube
     if ! [ -x "$(command -v minikube)" ]; then
       echo "Installing latest version minikube ..."
       curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-$arch && \
