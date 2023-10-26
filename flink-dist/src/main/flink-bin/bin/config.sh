03ebfeff9a60c9f0ed4d67c5799edfa2fd3e8aac
Sergey Nuyanzin <snuyanzin@gmail.com>
2023-10-26 10:07:20 +0200

    use condition if jdk version > 17

--- flink-dist/src/main/flink-bin/bin/config.sh
+++ flink-dist/src/main/flink-bin/bin/config.sh
@@ -336,7 +336,7 @@ if [ -z "${FLINK_ENV_JAVA_OPTS}" ]; then
     FLINK_ENV_JAVA_OPTS="-XX:+IgnoreUnrecognizedVMOptions $( echo "${FLINK_ENV_JAVA_OPTS}" | sed -e 's/^"//'  -e 's/"$//' )"
 
     JAVA_SPEC_VERSION=`"${JAVA_RUN}" -XshowSettings:properties 2>&1 | grep "java.specification.version" | cut -d "=" -f 2 |  tr -d '[:space:]'`
-    if (( $(("$JAVA_SPEC_VERSION > 17" | bc -l)) )); then
+    if [[ $(echo "$JAVA_SPEC_VERSION > 17" | bc )  == "1"  ]]; then
       # set security manager property to allow calls to System.setSecurityManager() at runtime
       FLINK_ENV_JAVA_OPTS="$FLINK_ENV_JAVA_OPTS -Djava.security.manager=allow"
     fi
