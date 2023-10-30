f5619d34430c7622fa5304ae86839ce5a564bd10
Dawid Wysakowicz <dwysakowicz@apache.org>
2023-10-30 13:45:20 +0100

    [FLINK-18286] Implement type inference for functions on composite types

--- flink-table/flink-table-common/src/main/java/org/apache/flink/table/types/inference/strategies/SpecificTypeStrategies.java
+++ flink-table/flink-table-common/src/main/java/org/apache/flink/table/types/inference/strategies/SpecificTypeStrategies.java
@@ -59,12 +59,9 @@ public final class SpecificTypeStrategies {
     public static final TypeStrategy ARRAY = new ArrayTypeStrategy();
 
     /** Type strategy specific for array element. */
-    public static final TypeStrategy ARRAY_ELEMENT =
-            callContext ->
-                    Optional.of(
-                            ((CollectionDataType) callContext.getArgumentDataTypes().get(0))
-                                    .getElementDataType());
+    public static final TypeStrategy ARRAY_ELEMENT = new ArrayElementTypeStrategy();
 
+    public static final TypeStrategy ITEM_AT = new ItemAtTypeStrategy();
     /** See {@link GetTypeStrategy}. */
     public static final TypeStrategy GET = new GetTypeStrategy();
 
