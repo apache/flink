2779239ff7d889f0a15dd837609dbb900b9bc693
fengjiajie <fengjiajie@sensorsdata.cn>
2023-11-06 14:21:40 +0800

    [FLINK-33171][table-planner] Consistent implicit type coercion support for equal and non-equal comparisons for codegen
    This closes #23478

--- flink-table/flink-table-planner/src/test/scala/org/apache/flink/table/planner/expressions/utils/ExpressionTestBase.scala
+++ flink-table/flink-table-planner/src/test/scala/org/apache/flink/table/planner/expressions/utils/ExpressionTestBase.scala
@@ -258,6 +258,7 @@ abstract class ExpressionTestBase(isStreaming: Boolean = true) {
       val converter = DataStructureConverters
         .getConverter(resolvedDataType)
         .asInstanceOf[DataStructureConverter[RowData, Row]]
+      converter.open(getClass.getClassLoader)
       converter.toInternalOrNull(testData)
     }
     try {
