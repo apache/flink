8f96fb6d18ec6168e3b0d86cfbade578a044bdc4
Sergey Nuyanzin <snuyanzin@gmail.com>
2023-11-27 01:37:55 +0100

    [FLINK-33664][ci] Setup cron build for java 21

--- flink-end-to-end-tests/test-scripts/common_docker.sh
+++ flink-end-to-end-tests/test-scripts/common_docker.sh
@@ -57,6 +57,9 @@ function build_image() {
     if [[ ${PROFILE} == *"jdk17"* ]]; then
         java_version=17
     fi
+    if [[ ${PROFILE} == *"jdk21"* ]]; then
+        java_version=21
+    fi
 
     cd flink-docker
     ./add-custom.sh -u ${file_server_address}:9999/flink.tgz -n ${image_name} -j ${java_version}
