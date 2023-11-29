b3b7240cc34e552273b26d8090d45e492474c9ea
Jiabao Sun <jiabao.sun@xtransfer.cn>
2023-11-29 09:48:34 +0800

    [FLINK-31339][table][tests] Comparing with materialized result when mini-batch enabled to fix unstable sql e2e tests
    This closes #23816.

--- flink-end-to-end-tests/flink-end-to-end-tests-sql/src/test/java/org/apache/flink/table/sql/codegen/SqlITCaseBase.java
+++ flink-end-to-end-tests/flink-end-to-end-tests-sql/src/test/java/org/apache/flink/table/sql/codegen/SqlITCaseBase.java
@@ -18,13 +18,24 @@
 
 package org.apache.flink.table.sql.codegen;
 
+import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.api.common.time.Deadline;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.connector.testutils.source.deserialization.TestingDeserializationContext;
+import org.apache.flink.formats.common.TimestampFormat;
+import org.apache.flink.formats.json.debezium.DebeziumJsonDeserializationSchema;
+import org.apache.flink.table.catalog.ResolvedSchema;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.data.conversion.RowRowConverter;
+import org.apache.flink.table.runtime.typeutils.InternalTypeInfo;
 import org.apache.flink.test.resources.ResourceTestUtils;
 import org.apache.flink.tests.util.flink.ClusterController;
 import org.apache.flink.tests.util.flink.FlinkResource;
 import org.apache.flink.tests.util.flink.FlinkResourceSetup;
 import org.apache.flink.tests.util.flink.LocalStandaloneFlinkResourceFactory;
+import org.apache.flink.types.Row;
+import org.apache.flink.types.RowKind;
+import org.apache.flink.util.Collector;
 import org.apache.flink.util.TestLogger;
 
 import org.junit.Before;
@@ -50,8 +61,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /** Base class for sql ITCase. */
@@ -103,7 +117,26 @@ public abstract class SqlITCaseBase extends TestLogger {
         runAndCheckSQL(sqlPath, Collections.singletonMap(result, resultItems));
     }
 
+    public void runAndCheckSQL(
+            String sqlPath,
+            List<String> resultItems,
+            Function<List<String>, List<String>> formatter)
+            throws Exception {
+        runAndCheckSQL(
+                sqlPath,
+                Collections.singletonMap(result, resultItems),
+                Collections.singletonMap(result, formatter));
+    }
+
     public void runAndCheckSQL(String sqlPath, Map<Path, List<String>> resultItems)
+            throws Exception {
+        runAndCheckSQL(sqlPath, resultItems, Collections.emptyMap());
+    }
+
+    public void runAndCheckSQL(
+            String sqlPath,
+            Map<Path, List<String>> resultItems,
+            Map<Path, Function<List<String>, List<String>>> formatters)
             throws Exception {
         try (ClusterController clusterController = flink.startCluster(1)) {
             List<String> sqlLines = initializeSqlLines(sqlPath);
@@ -113,7 +146,10 @@ public abstract class SqlITCaseBase extends TestLogger {
             // Wait until all the results flushed to the json file.
             LOG.info("Verify the result.");
             for (Map.Entry<Path, List<String>> entry : resultItems.entrySet()) {
-                checkResultFile(entry.getKey(), entry.getValue());
+                checkResultFile(
+                        entry.getKey(),
+                        entry.getValue(),
+                        formatters.getOrDefault(entry.getKey(), this::formatRawResult));
             }
             LOG.info("The SQL client test run successfully.");
         }
@@ -147,22 +183,27 @@ public abstract class SqlITCaseBase extends TestLogger {
         return result;
     }
 
-    private static void checkResultFile(Path resultPath, List<String> expectedItems)
+    private static void checkResultFile(
+            Path resultPath,
+            List<String> expectedItems,
+            Function<List<String>, List<String>> resultFormatter)
             throws Exception {
         boolean success = false;
         final Deadline deadline = Deadline.fromNow(Duration.ofSeconds(20));
         List<String> lines = null;
-        int resultSize = expectedItems.size();
         while (deadline.hasTimeLeft()) {
             if (Files.exists(resultPath)) {
                 lines = readResultFiles(resultPath);
-                if (lines.size() == resultSize) {
+                try {
+                    List<String> actual = resultFormatter.apply(lines);
+                    assertThat(actual)
+                            .hasSameSizeAs(expectedItems)
+                            .containsExactlyInAnyOrderElementsOf(expectedItems);
                     success = true;
-                    assertThat(lines).hasSameElementsAs(expectedItems);
                     break;
-                } else {
+                } catch (AssertionError e) {
                     LOG.info(
-                            "The target result {} does not contain enough records, current {} records, left time: {}s",
+                            "The target result {} does not match expected records, current {} records, left time: {}s",
                             resultPath,
                             lines.size(),
                             deadline.timeLeft().getSeconds());
@@ -178,7 +219,7 @@ public abstract class SqlITCaseBase extends TestLogger {
                         "Did not get expected results before timeout, actual result: %s.", lines));
     }
 
-    private static List<String> readResultFiles(Path path) throws IOException {
+    private static List<String> readResultFiles(Path path) throws Exception {
         File filePath = path.toFile();
         // list all the non-hidden files
         File[] files = filePath.listFiles((dir, name) -> !name.startsWith("."));
@@ -189,5 +230,91 @@ public abstract class SqlITCaseBase extends TestLogger {
             }
         }
         return result;
+    }
+
+    /**
+     * The raw data read from the file system can be mapped and transformed. For example, subclasses
+     * can override this method to obtain the final result after materialization.
+     *
+     * <pre>{@code
+     * @Override
+     * protected List<String> formatRawResult(List<String> rawResults) {
+     *     return convertToMaterializedResult(rawResults, schema, deserializationSchema);
+     * }
+     * }</pre>
+     */
+    protected List<String> formatRawResult(List<String> rawResults) {
+        return rawResults;
+    }
+
+    protected static List<String> convertToMaterializedResult(
+            List<String> rawResults,
+            ResolvedSchema schema,
+            DeserializationSchema<RowData> deserializationSchema) {
+        DataCollector collector = new DataCollector();
+        try {
+            deserializationSchema.open(new TestingDeserializationContext());
+            for (String rawResult : rawResults) {
+                deserializationSchema.deserialize(rawResult.getBytes(), collector);
+            }
+        } catch (Exception e) {
+            fail("deserialize error: ", e);
+        }
+
+        RowRowConverter converter = RowRowConverter.create(schema.toPhysicalRowDataType());
+        Map<Row, Row> upsertResult = new HashMap<>();
+
+        for (RowData rowData : collector.dataList) {
+            RowKind kind = rowData.getRowKind();
+
+            Row row = converter.toExternal(rowData);
+            assertThat(row).isNotNull();
+
+            Row key = Row.project(row, schema.getPrimaryKeyIndexes());
+            key.setKind(RowKind.INSERT);
+
+            if (kind == RowKind.INSERT || kind == RowKind.UPDATE_AFTER) {
+                Row upsertRow = Row.copy(row);
+                upsertRow.setKind(RowKind.INSERT);
+                upsertResult.put(key, upsertRow);
+            } else {
+                Row oldValue = upsertResult.remove(key);
+                if (oldValue == null) {
+                    throw new RuntimeException(
+                            "Tried to delete a value that wasn't inserted first. "
+                                    + "This is probably an incorrectly implemented test.");
+                }
+            }
+        }
+
+        return upsertResult.values().stream().map(Row::toString).collect(Collectors.toList());
+    }
+
+    /**
+     * Create a DebeziumJsonDeserializationSchema using the given {@link ResolvedSchema} to convert
+     * debezium-json formatted record into {@link RowData}.
+     */
+    protected static DebeziumJsonDeserializationSchema createDebeziumDeserializationSchema(
+            ResolvedSchema schema) {
+        return new DebeziumJsonDeserializationSchema(
+                schema.toPhysicalRowDataType(),
+                Collections.emptyList(),
+                InternalTypeInfo.of(schema.toPhysicalRowDataType().getLogicalType()),
+                false,
+                true,
+                TimestampFormat.ISO_8601);
+    }
+
+    private static class DataCollector implements Collector<RowData> {
+
+        private final List<RowData> dataList = new ArrayList<>();
+
+        @Override
+        public void collect(RowData record) {
+            dataList.add(record);
+        }
+
+        @Override
+        public void close() {}
     }
 }
