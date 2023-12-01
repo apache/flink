2dd1076dbbf0f47e4b5d15e67e0265679397f93e
Rui Fan <1996fanrui@gmail.com>
2023-12-01 16:04:36 +0800

    [FLINK-33693][checkpoint] Force aligned barrier works with timeoutable aligned checkpoint barrier

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/io/RecordWriterOutput.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/runtime/io/RecordWriterOutput.java
@@ -149,9 +149,7 @@ public class RecordWriterOutput<OUT> implements WatermarkGaugeExposingOutput<Str
     }
 
     public void broadcastEvent(AbstractEvent event, boolean isPriorityEvent) throws IOException {
-        if (isPriorityEvent
-                && event instanceof CheckpointBarrier
-                && !supportsUnalignedCheckpoints) {
+        if (event instanceof CheckpointBarrier && !supportsUnalignedCheckpoints) {
             final CheckpointBarrier barrier = (CheckpointBarrier) event;
             event = barrier.withOptions(barrier.getCheckpointOptions().withUnalignedUnsupported());
             isPriorityEvent = false;
