85edd784fc72c1784849e2b122cbf3215f89817c
Matthias Pohl <matthias.pohl@aiven.io>
2023-12-10 13:36:57 +0100

    [FLINK-33914][ci] Introduces a basic GitHub Actions workflow
    - Adds basic CI workflow
    - Adds composite workflow for starting Flink's CI stages
    - Updates CI helper scripts to support GitHub Actions
    - Adds composite workflow for doing pre-compile checks
    - Adds custom action to enable maven-utils.sh in workflows
    - Adds custom action for initializing a job

--- tools/azure-pipelines/unpack_build_artifact.sh
+++ tools/azure-pipelines/unpack_build_artifact.sh
@@ -24,7 +24,14 @@ if ! [ -e $FLINK_ARTIFACT_DIR ]; then
 fi
 
 echo "Merging cache"
-cp -RT "$FLINK_ARTIFACT_DIR" "."
+if [ -z "${FLINK_ARTIFACT_FILENAME}" ]; then
+  # for Azure Pipelines
+  cp -RT "$FLINK_ARTIFACT_DIR" "."
+else
+  # for GitHub Actions
+  echo "Extract build artifacts ${FLINK_ARTIFACT_DIR}/${FLINK_ARTIFACT_FILENAME} into local directory."
+  tar -xzf "${FLINK_ARTIFACT_DIR}/${FLINK_ARTIFACT_FILENAME}"
+fi
 
 echo "Adjusting timestamps"
 # adjust timestamps of proto file to avoid re-generation
