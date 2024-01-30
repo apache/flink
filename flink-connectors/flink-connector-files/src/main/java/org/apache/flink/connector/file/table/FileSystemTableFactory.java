6da082a35f03e0f45782d94f70969b3ac271e46a
Nathanael Ji <nji@lyft.com>
2024-01-30 06:14:35 -0800

    Revert "printing factories and classpaths"
    This reverts commit b3d59e20dac722a244cc9b847f9982309624dce4.

--- flink-connectors/flink-connector-files/src/main/java/org/apache/flink/connector/file/table/FileSystemTableFactory.java
+++ flink-connectors/flink-connector-files/src/main/java/org/apache/flink/connector/file/table/FileSystemTableFactory.java
@@ -200,22 +200,10 @@ public class FileSystemTableFactory implements DynamicTableSourceFactory, Dynami
                             FactoryUtil.FORMAT.key()));
         }
 
-        String classpath = System.getProperty("java.class.path");
-        String[] classpathEntries = classpath.split(System.getProperty("path.separator"));
-
-        System.out.println("Classpath entries:");
-        for (String entry : classpathEntries) {
-            System.out.println(entry);
-        }
-
-        System.out.println("Factories:");
         final List<Factory> factories = new LinkedList<>();
         ServiceLoader.load(Factory.class, context.getClassLoader())
                 .iterator()
-                .forEachRemaining(factory -> {
-                    System.out.println("Factory Name: " + factory.getClass().getName());
-                    factories.add(factory);
-                });
+                .forEachRemaining(factories::add);
 
         final List<Factory> foundFactories =
                 factories.stream()
