6b470ee416a1283a4aea143fc923d17ce54cfcf9
Sergey Nuyanzin <snuyanzin@gmail.com>
2024-01-31 18:01:59 +0100

    [FLINK-31362][table] Update Calcite classes

--- flink-table/flink-table-planner/src/main/java/org/apache/calcite/tools/RelBuilder.java
+++ flink-table/flink-table-planner/src/main/java/org/apache/calcite/tools/RelBuilder.java
@@ -106,6 +106,7 @@ import org.apache.calcite.sql.type.SqlTypeName;
 import org.apache.calcite.sql.type.TableFunctionReturnTypeInference;
 import org.apache.calcite.sql.validate.SqlValidatorUtil;
 import org.apache.calcite.sql2rel.SqlToRelConverter;
+import org.apache.calcite.util.DateString;
 import org.apache.calcite.util.Holder;
 import org.apache.calcite.util.ImmutableBitSet;
 import org.apache.calcite.util.ImmutableIntList;
@@ -156,7 +157,7 @@ import static org.apache.calcite.util.Static.RESOURCE;
  * <p>FLINK modifications are at lines
  *
  * <ol>
- *   <li>Should be removed after fix of FLINK-29804: Lines 2945 ~ 2948
+ *   <li>Should be removed after fix of FLINK-29804: Lines 3000 ~ 3003
  * </ol>
  */
 @Value.Enclosing
@@ -470,6 +471,8 @@ public class RelBuilder {
         } else if (value instanceof Enum) {
             return rexBuilder.makeLiteral(
                     value, getTypeFactory().createSqlType(SqlTypeName.SYMBOL));
+        } else if (value instanceof DateString) {
+            return rexBuilder.makeDateLiteral((DateString) value);
         } else {
             throw new IllegalArgumentException(
                     "cannot convert " + value + " (" + value.getClass() + ") to a constant");
@@ -1182,7 +1185,7 @@ public class RelBuilder {
      *
      * @see #project
      */
-    public RexNode alias(RexNode expr, @Nullable String alias) {
+    public RexNode alias(RexNode expr, String alias) {
         final RexNode aliasLiteral = literal(alias);
         switch (expr.getKind()) {
             case AS:
@@ -1988,7 +1991,25 @@ public class RelBuilder {
             Iterable<? extends RexNode> nodes,
             Iterable<? extends @Nullable String> fieldNames,
             boolean force) {
-        return project_(nodes, fieldNames, ImmutableList.of(), force);
+        return project(nodes, fieldNames, force, ImmutableSet.of());
+    }
+
+    /**
+     * The same with {@link #project(Iterable, Iterable, boolean)}, with additional variablesSet
+     * param.
+     *
+     * @param nodes Expressions
+     * @param fieldNames Suggested field names
+     * @param force create project even if it is identity
+     * @param variablesSet Correlating variables that are set when reading a row from the input, and
+     *     which may be referenced from the projection expressions
+     */
+    public RelBuilder project(
+            Iterable<? extends RexNode> nodes,
+            Iterable<? extends @Nullable String> fieldNames,
+            boolean force,
+            Iterable<CorrelationId> variablesSet) {
+        return project_(nodes, fieldNames, ImmutableList.of(), force, variablesSet);
     }
 
     /** Creates a {@link Project} of all original fields, plus the given expressions. */
@@ -1997,7 +2018,7 @@ public class RelBuilder {
     }
 
     /** Creates a {@link Project} of all original fields, plus the given list of expressions. */
-    public RelBuilder projectPlus(Iterable<RexNode> nodes) {
+    public RelBuilder projectPlus(Iterable<? extends RexNode> nodes) {
         return project(Iterables.concat(fields(), nodes));
     }
 
@@ -2061,10 +2082,12 @@ public class RelBuilder {
             Iterable<? extends RexNode> nodes,
             Iterable<? extends @Nullable String> fieldNames,
             Iterable<RelHint> hints,
-            boolean force) {
+            boolean force,
+            Iterable<CorrelationId> variablesSet) {
         final Frame frame = requireNonNull(peek_(), "frame stack is empty");
         final RelDataType inputRowType = frame.rel.getRowType();
         final List<RexNode> nodeList = Lists.newArrayList(nodes);
+        final Set<CorrelationId> variables = ImmutableSet.copyOf(variablesSet);
 
         // Perform a quick check for identity. We'll do a deeper check
         // later when we've derived column names.
@@ -2077,8 +2100,9 @@ public class RelBuilder {
             fieldNameList.add(null);
         }
 
+        // Do not merge projection when top projection has correlation variables
         bloat:
-        if (frame.rel instanceof Project && config.bloat() >= 0) {
+        if (frame.rel instanceof Project && config.bloat() >= 0 && variables.isEmpty()) {
             final Project project = (Project) frame.rel;
             // Populate field names. If the upper expression is an input ref and does
             // not have a recommended name, use the name of the underlying field.
@@ -2123,7 +2147,13 @@ public class RelBuilder {
             final ImmutableSet.Builder<RelHint> mergedHints = ImmutableSet.builder();
             mergedHints.addAll(project.getHints());
             mergedHints.addAll(hints);
-            return project_(newNodes, fieldNameList, mergedHints.build(), force);
+            // Keep bottom projection's variablesSet.
+            return project_(
+                    newNodes,
+                    fieldNameList,
+                    mergedHints.build(),
+                    force,
+                    ImmutableSet.copyOf(project.getVariablesSet()));
         }
 
         // Simplify expressions.
@@ -2209,7 +2239,8 @@ public class RelBuilder {
                         frame.rel,
                         ImmutableList.copyOf(hints),
                         ImmutableList.copyOf(nodeList),
-                        fieldNameList);
+                        fieldNameList,
+                        variables);
         stack.pop();
         stack.push(new Frame(project, fields.build()));
         return this;
@@ -2233,6 +2264,30 @@ public class RelBuilder {
             Iterable<? extends RexNode> nodes,
             @Nullable Iterable<? extends @Nullable String> fieldNames,
             boolean force) {
+        return projectNamed(nodes, fieldNames, force, ImmutableSet.of());
+    }
+
+    /**
+     * Creates a {@link Project} of the given expressions and field names, and optionally
+     * optimizing.
+     *
+     * <p>If {@code fieldNames} is null, or if a particular entry in {@code fieldNames} is null,
+     * derives field names from the input expressions.
+     *
+     * <p>If {@code force} is false, and the input is a {@code Project}, and the expressions make
+     * the trivial projection ($0, $1, ...), modifies the input.
+     *
+     * @param nodes Expressions
+     * @param fieldNames Suggested field names, or null to generate
+     * @param force Whether to create a renaming Project if the projections are trivial
+     * @param variablesSet Correlating variables that are set when reading a row from the input, and
+     *     which may be referenced from the projection expressions
+     */
+    public RelBuilder projectNamed(
+            Iterable<? extends RexNode> nodes,
+            @Nullable Iterable<? extends @Nullable String> fieldNames,
+            boolean force,
+            Iterable<CorrelationId> variablesSet) {
         @SuppressWarnings("unchecked")
         final List<? extends RexNode> nodeList =
                 nodes instanceof List ? (List) nodes : ImmutableList.copyOf(nodes);
@@ -2278,7 +2333,7 @@ public class RelBuilder {
                 stack.push(new Frame(newValues, frame.fields));
             }
         } else {
-            project(nodeList, rowType.getFieldNames(), force);
+            project(nodeList, rowType.getFieldNames(), force, variablesSet);
         }
         return this;
     }
@@ -3474,7 +3529,8 @@ public class RelBuilder {
                                         sort,
                                         project.getHints(),
                                         project.getProjects(),
-                                        Pair.right(project.getNamedProjects())));
+                                        Pair.right(project.getNamedProjects()),
+                                        project.getVariablesSet()));
                         return this;
                     }
                 }
