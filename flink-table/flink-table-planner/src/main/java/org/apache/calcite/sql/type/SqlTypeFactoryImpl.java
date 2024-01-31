4d9c7894e921ee9ccc8b8058bb4d271aff24d805
Sergey Nuyanzin <snuyanzin@gmail.com>
2024-01-31 22:25:20 +0100

    [FLINK-31362][table] Adopt inferring constants from predicates with IS NOT DISTINCT FROM operator

--- flink-table/flink-table-planner/src/main/java/org/apache/calcite/sql/type/SqlTypeFactoryImpl.java
+++ flink-table/flink-table-planner/src/main/java/org/apache/calcite/sql/type/SqlTypeFactoryImpl.java
@@ -38,8 +38,9 @@ import static java.util.Objects.requireNonNull;
  * <p>FLINK modifications are at lines
  *
  * <ol>
- *   <li>Should be removed after fixing CALCITE-6342: Lines 475-485
- *   <li>Should be removed after fix of FLINK-31350: Lines 552 ~ 564.
+ *   <li>Should be removed after fixing CALCITE-6342: Lines 100-102
+ *   <li>Should be removed after fixing CALCITE-6342: Lines 482-494
+ *   <li>Should be removed after fix of FLINK-31350: Lines 561-573.
  * </ol>
  */
 public class SqlTypeFactoryImpl extends RelDataTypeFactoryImpl {
