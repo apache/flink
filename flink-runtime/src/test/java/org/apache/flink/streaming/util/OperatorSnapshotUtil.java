617921f18b36385b87fa033c23971cd2bd00e79e
wangfeifan <zoltar9264@163.com>
2024-02-01 17:04:55 +0800

    [FLINK-33734][checkpointing] Extract interface ChannelStateHandleSerializer

--- flink-runtime/src/test/java/org/apache/flink/streaming/util/OperatorSnapshotUtil.java
+++ flink-runtime/src/test/java/org/apache/flink/streaming/util/OperatorSnapshotUtil.java
@@ -104,7 +104,7 @@ public class OperatorSnapshotUtil {
             Collection<InputStateHandle> inputChannelStateHandles = state.getInputChannelState();
             dos.writeInt(inputChannelStateHandles.size());
             for (InputStateHandle inputChannelStateHandle : inputChannelStateHandles) {
-                MetadataV3Serializer.INSTANCE.serializeInputChannelStateHandle(
+                MetadataV3Serializer.INSTANCE.serializeInputStateHandle(
                         inputChannelStateHandle, dos);
             }
 
@@ -112,7 +112,7 @@ public class OperatorSnapshotUtil {
                     state.getResultSubpartitionState();
             dos.writeInt(inputChannelStateHandles.size());
             for (OutputStateHandle resultSubpartitionStateHandle : resultSubpartitionStateHandles) {
-                MetadataV3Serializer.INSTANCE.serializeResultSubpartitionStateHandle(
+                MetadataV3Serializer.INSTANCE.serializeOutputStateHandle(
                         resultSubpartitionStateHandle, dos);
             }
 
@@ -177,13 +177,12 @@ public class OperatorSnapshotUtil {
 
             final StateObjectCollection<InputStateHandle> inputChannelStateHandles =
                     v == MetadataV3Serializer.VERSION
-                            ? MetadataV3Serializer.deserializeInputChannelStateHandle(dis)
+                            ? MetadataV3Serializer.deserializeInputStateHandle(dis)
                             : StateObjectCollection.empty();
 
             final StateObjectCollection<OutputStateHandle> resultSubpartitionStateHandles =
                     v == MetadataV3Serializer.VERSION
-                            ? MetadataV3Serializer.INSTANCE
-                                    .deserializeResultSubpartitionStateHandle(dis)
+                            ? MetadataV3Serializer.INSTANCE.deserializeOutputStateHandle(dis)
                             : StateObjectCollection.empty();
 
             return OperatorSubtaskState.builder()
