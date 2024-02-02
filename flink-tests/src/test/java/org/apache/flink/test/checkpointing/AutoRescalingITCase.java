32ac0825ce3f6b0af99884f1af80de90e0ffb992
Rui Fan <1996fanrui@gmail.com>
2024-02-02 13:08:01 +0800

    [FLINK-34336][test] Fix the bug that AutoRescalingITCase may hang sometimes

--- flink-tests/src/test/java/org/apache/flink/test/checkpointing/AutoRescalingITCase.java
+++ flink-tests/src/test/java/org/apache/flink/test/checkpointing/AutoRescalingITCase.java
@@ -337,7 +337,7 @@ public class AutoRescalingITCase extends TestLogger {
 
             restClusterClient.updateJobResourceRequirements(jobID, builder.build()).join();
 
-            waitForRunningTasks(restClusterClient, jobID, parallelism2);
+            waitForRunningTasks(restClusterClient, jobID, 2 * parallelism2);
             waitForAvailableSlots(restClusterClient, totalSlots - parallelism2);
 
             StateSourceBase.canFinishLatch.countDown();
@@ -427,7 +427,8 @@ public class AutoRescalingITCase extends TestLogger {
 
             restClusterClient.updateJobResourceRequirements(jobID, builder.build()).join();
 
-            waitForRunningTasks(restClusterClient, jobID, parallelism2);
+            // Source is parallelism, the flatMapper & Sink is parallelism2
+            waitForRunningTasks(restClusterClient, jobID, parallelism + parallelism2);
             waitForAvailableSlots(restClusterClient, totalSlots - parallelism2);
 
             SubtaskIndexSource.SOURCE_LATCH.trigger();
