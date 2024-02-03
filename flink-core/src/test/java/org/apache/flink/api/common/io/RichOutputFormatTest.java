4c3f0a81a46e3fd975991f165a4fdf5097507391
gongzhongqiang <gongzhongqiang@gigacloudtech.com>
2024-02-03 17:33:09 +0800

    address comment

--- flink-core/src/test/java/org/apache/flink/api/common/io/RichOutputFormatTest.java
+++ flink-core/src/test/java/org/apache/flink/api/common/io/RichOutputFormatTest.java
@@ -48,13 +48,9 @@ class RichOutputFormatTest {
                         new HashMap<>(),
                         UnregisteredMetricsGroup.createOperatorMetricGroup()));
 
-        assertThat(1)
-                .isEqualTo(inputFormat.getRuntimeContext().getTaskInfo().getIndexOfThisSubtask());
-        assertThat(3)
-                .isEqualTo(
-                        inputFormat
-                                .getRuntimeContext()
-                                .getTaskInfo()
-                                .getNumberOfParallelSubtasks());
+        assertThat(inputFormat.getRuntimeContext().getTaskInfo().getIndexOfThisSubtask())
+                .isEqualTo(1);
+        assertThat(inputFormat.getRuntimeContext().getTaskInfo().getNumberOfParallelSubtasks())
+                .isEqualTo(3);
     }
 }
