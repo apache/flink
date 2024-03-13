ca050281002fd60e6c1f97ed85243e1e2890f78b
Robert Young <robeyoun@redhat.com>
2024-03-14 11:45:43 +1300

    [FLINK-34569][e2e] Remove jq containers after user
    Why:
    A large pile of exited jq containers were left in docker after
    an operation was retried repeatedly.
    
    Signed-off-by: Robert Young <robeyoun@redhat.com>

--- flink-end-to-end-tests/test-scripts/common_s3_operations.sh
+++ flink-end-to-end-tests/test-scripts/common_s3_operations.sh
@@ -145,7 +145,7 @@ function s3_get_number_of_lines_by_prefix() {
 
   # find all files that have the given prefix
   parts=$(aws_cli s3api list-objects --bucket "$IT_CASE_S3_BUCKET" --prefix "$1" |
-    docker run -i stedolan/jq -r '[.Contents[].Key] | join(" ")')
+    docker run -i --rm stedolan/jq -r '[.Contents[].Key] | join(" ")')
 
   # in parallel (N tasks), query the number of lines, store result in a file named lines
   N=10
