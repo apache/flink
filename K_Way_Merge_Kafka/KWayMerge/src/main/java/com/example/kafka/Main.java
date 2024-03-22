7bf4e03407d6998cc4dbfeed9eaedbd719036a1a
sarthaksingh94 <singh94@illinois.edu>
2024-03-21 21:54:44 -0500

    small bug comment

--- K_Way_Merge_Kafka/KWayMerge/src/main/java/com/example/kafka/Main.java
+++ K_Way_Merge_Kafka/KWayMerge/src/main/java/com/example/kafka/Main.java
@@ -3,6 +3,8 @@ package com.example.kafka;
 public class Main {
 
     public static void main( String[] args ) {
+        // Weird bug - the first time you launch the cluster and run this code it polls forever and cant find any new events. Once you stop the code and try again it works
+        // Needs to be fixed in the future but for now I will leave it due to time contraints.
         KafkaTestProducer ktp = new KafkaTestProducer("localhost:9092", 3);
         KafkaMergeConsumer kmc = new KafkaMergeConsumer("localhost:9092",3);
 
