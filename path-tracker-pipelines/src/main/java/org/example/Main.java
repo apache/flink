5ddac282870ccd343de5783040bd18db72b11046
Robertation <67719835+Robertation256@users.noreply.github.com>
2024-03-23 18:19:10 -0500

    Add in topic creation

--- path-tracker-pipelines/src/main/java/org/example/Main.java
+++ path-tracker-pipelines/src/main/java/org/example/Main.java
@@ -18,20 +18,77 @@
 
 package org.example;
 
-import org.apache.flink.configuration.Configuration;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.streaming.api.environment.PathAnalyzer;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.v2.DiscardingSink;
 import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.DescribeTopicsResult;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.TopicDescription;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.TopicPartitionInfo;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import javax.xml.crypto.Data;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
+
+    // topic for queues
+    private static String OUTPUT_TOPIC = "test-output-topic";
     public static void main(String[] args) throws  Exception{
 
+        KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"));
+        kafka.start();
 
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(new Configuration());
+        Properties prop = new Properties();
+        prop.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
 
-        DataRecord r1 = new DataRecord(1);
-        DataRecord r2 = new DataRecord(2);
+        AdminClient adminClient = KafkaAdminClient.create(prop);
+
+        StreamExecutionEnvironment env = createPipeline(kafka.getBootstrapServers());
+
+        int pathNum = PathAnalyzer.computePathNum(env);
+
+        NewTopic newTopic = new NewTopic(OUTPUT_TOPIC, pathNum, (short) 1);
+        adminClient.createTopics(Collections.singleton(newTopic));
+
+
+        env.execute();
+        
+
+    }
+
+    private static StreamExecutionEnvironment createPipeline(String kafkaBootstrapServer) {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        KafkaSink<String> kafkaSink = KafkaSink.<String>builder()
+                .setBootstrapServers(kafkaBootstrapServer)
+                .setRecordSerializer(KafkaRecordSerializationSchema.builder()
+                        .setTopic(OUTPUT_TOPIC)
+                        .setValueSerializationSchema(new SimpleStringSchema())
+                        .build()
+                )
+                .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+                .build();
+
         env.addSource(new TestDataSource(100)).setParallelism(1)
                 // filter out multiples of 7
                 .filter((DataRecord x) -> { return x.getValue()%7 != 0; }).setParallelism(3)
@@ -40,10 +97,13 @@ public class Main {
                 .map((DataRecord x) -> {x.setValue(x.getValue()*2); return x; }).setParallelism(4)
                 .keyBy(DataRecord::getValue)
                 // square it
-                .map((DataRecord x) -> {return x.getValue()*x.getValue(); }).setParallelism(2)
-                .print();
+                .map((DataRecord x) -> {int newValue = x.getValue()*x.getValue(); x.setValue(newValue); return String.format("%d-%d", x.getSequenceId(), newValue); }).setParallelism(2)
+                .sinkTo(kafkaSink).setParallelism(1);
 
-        env.execute();
+
+
+        return env;
+
     }
 }
 
