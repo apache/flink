a91ed38e65dfdb03f5944b8066f63c6b7b44afca
Zhanghao Chen <m1a2@outlook.com>
2024-04-09 12:52:14 +0800

    [FLINK-35068][core][type] Introduce built-in serialization support for java.util.Set
    (cherry picked from commit e1b45684394541ee290a3d81cc59a85623396c42)

--- flink-core/src/main/java/org/apache/flink/api/java/typeutils/TypeExtractor.java
+++ flink-core/src/main/java/org/apache/flink/api/java/typeutils/TypeExtractor.java
@@ -73,6 +73,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.apache.flink.api.java.typeutils.TypeExtractionUtils.checkAndExtractLambda;
 import static org.apache.flink.api.java.typeutils.TypeExtractionUtils.getAllDeclaredMethods;
@@ -1993,6 +1994,10 @@ public class TypeExtractor {
                     Class<?> elementClass = (Class<?>) actualTypeArguments[0];
                     TypeInformation<?> elementTypeInfo = createTypeInfo(elementClass);
                     return (TypeInformation<OUT>) Types.LIST(elementTypeInfo);
+                } else if (clazz.isAssignableFrom(Set.class)) {
+                    Class<?> elementClass = (Class<?>) actualTypeArguments[0];
+                    TypeInformation<?> elementTypeInfo = createTypeInfo(elementClass);
+                    return (TypeInformation<OUT>) Types.SET(elementTypeInfo);
                 }
             }
         }
