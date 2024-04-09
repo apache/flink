b1165a89edb9857754e283c6afd7983a34acd465
Sergey Nuyanzin <snuyanzin@gmail.com>
2024-04-09 20:02:01 +0200

    [FLINK-28693][table] Janino compile failed because of the generated code refers a class in table-planner
    Co-authored-by: Xuyang <xyzhong188@163.com>

--- flink-end-to-end-tests/flink-end-to-end-tests-sql/src/test/java/org/apache/flink/table/sql/UsingRemoteJarITCase.java
+++ flink-end-to-end-tests/flink-end-to-end-tests-sql/src/test/java/org/apache/flink/table/sql/UsingRemoteJarITCase.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.apache.flink.table.sql.codegen;
+package org.apache.flink.table.sql;
 
 import org.apache.flink.formats.json.debezium.DebeziumJsonDeserializationSchema;
 import org.apache.flink.table.api.DataTypes;
