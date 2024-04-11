ce5e9b39afd8278d46c3b5b0bf7175e692c232f7
Lorenzo Affetti <lorenzo.affetti@ververica.com>
2024-04-11 15:55:06 +0200

    [FLINK-20398][test] solve deprecation in BatchSQLTest source table creation

--- flink-end-to-end-tests/flink-batch-sql-test/src/test/java/org/apache/flink/sql/tests/BatchSQLTest.java
+++ flink-end-to-end-tests/flink-batch-sql-test/src/test/java/org/apache/flink/sql/tests/BatchSQLTest.java
@@ -19,25 +19,19 @@
 package org.apache.flink.sql.tests;
 
 import org.apache.flink.api.common.BatchShuffleMode;
-import org.apache.flink.api.common.io.InputFormat;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.api.common.typeinfo.Types;
-import org.apache.flink.api.java.io.IteratorInputFormat;
 import org.apache.flink.configuration.ExecutionOptions;
 import org.apache.flink.core.execution.JobClient;
+import org.apache.flink.formats.csv.CsvFormatOptions;
 import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration;
 import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.FormatDescriptor;
+import org.apache.flink.table.api.Schema;
+import org.apache.flink.table.api.TableDescriptor;
 import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.TableResult;
-import org.apache.flink.table.api.TableSchema;
-import org.apache.flink.table.api.internal.TableEnvironmentInternal;
-import org.apache.flink.table.sinks.CsvTableSink;
-import org.apache.flink.table.sources.InputFormatTableSource;
-import org.apache.flink.table.types.DataType;
 import org.apache.flink.test.junit5.MiniClusterExtension;
 import org.apache.flink.test.resources.ResourceTestUtils;
-import org.apache.flink.types.Row;
 import org.apache.flink.util.TestLoggerExtension;
 
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -48,15 +42,14 @@ import org.junit.jupiter.params.provider.EnumSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.Serializable;
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.UUID;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -82,27 +75,51 @@ class BatchSQLTest {
                 "ALL_EXCHANGES_BLOCKING",
                 "ALL_EXCHANGES_HYBRID_FULL",
                 "ALL_EXCHANGES_HYBRID_SELECTIVE"
+                // Only above shuffle modes are supported by the adaptive batch scheduler
+                // , "ALL_EXCHANGES_PIPELINE"
             })
     public void testBatchSQL(BatchShuffleMode shuffleMode, @TempDir Path tmpDir) throws Exception {
-        final Path resultFile = tmpDir.resolve(String.format("result-%s", UUID.randomUUID()));
-        LOG.info("Results for this test will be stored at: {}", resultFile);
+        LOG.info("Results for this test will be stored at: {}", tmpDir);
 
         String sqlStatement = new String(Files.readAllBytes(sqlPath));
 
         TableEnvironment tEnv = TableEnvironment.create(EnvironmentSettings.inBatchMode());
         tEnv.getConfig().set(ExecutionOptions.BATCH_SHUFFLE_MODE, shuffleMode);
 
-        ((TableEnvironmentInternal) tEnv)
-                .registerTableSourceInternal("table1", new GeneratorTableSource(10, 100, 60, 0));
-        ((TableEnvironmentInternal) tEnv)
-                .registerTableSourceInternal("table2", new GeneratorTableSource(5, 0.2f, 60, 5));
-        ((TableEnvironmentInternal) tEnv)
-                .registerTableSinkInternal(
-                        "sinkTable",
-                        new CsvTableSink(resultFile.toString())
-                                .configure(
-                                        new String[] {"f0", "f1"},
-                                        new TypeInformation[] {Types.INT, Types.SQL_TIMESTAMP}));
+        tEnv.createTable(
+                "table1",
+                TableDescriptor.forConnector(GeneratorTableSourceFactory.CONNECTOR_ID)
+                        .schema(GeneratorTableSourceFactory.getSchema())
+                        .option(GeneratorTableSourceFactory.NUM_KEYS, 10)
+                        .option(GeneratorTableSourceFactory.ROWS_PER_KEY_PER_SECOND, 100f)
+                        .option(GeneratorTableSourceFactory.DURATION_SECONDS, 60)
+                        .option(GeneratorTableSourceFactory.OFFSET_SECONDS, 0)
+                        .build());
+        tEnv.createTable(
+                "table2",
+                TableDescriptor.forConnector(GeneratorTableSourceFactory.CONNECTOR_ID)
+                        .schema(GeneratorTableSourceFactory.getSchema())
+                        .option(GeneratorTableSourceFactory.NUM_KEYS, 5)
+                        .option(GeneratorTableSourceFactory.ROWS_PER_KEY_PER_SECOND, 0.2f)
+                        .option(GeneratorTableSourceFactory.DURATION_SECONDS, 60)
+                        .option(GeneratorTableSourceFactory.OFFSET_SECONDS, 5)
+                        .build());
+
+        tEnv.createTable(
+                "sinkTable",
+                TableDescriptor.forConnector("filesystem")
+                        .schema(
+                                Schema.newBuilder()
+                                        .column("f0", DataTypes.INT())
+                                        .column("f1", DataTypes.TIMESTAMP(3))
+                                        .build())
+                        .option("path", tmpDir.toString())
+                        .format(
+                                FormatDescriptor.forFormat("csv")
+                                        .option(CsvFormatOptions.FIELD_DELIMITER, ",")
+                                        .option(CsvFormatOptions.DISABLE_QUOTE_CHARACTER, true)
+                                        .build())
+                        .build());
 
         LOG.info("Submitting job");
         TableResult result = tEnv.executeSql(sqlStatement);
@@ -114,104 +131,24 @@ class BatchSQLTest {
         jobClient.getJobExecutionResult().get();
 
         final String expected =
-                "1980,1970-01-01 00:00:00.0\n"
-                        + "1980,1970-01-01 00:00:20.0\n"
-                        + "1980,1970-01-01 00:00:40.0\n";
+                "1980,1970-01-01 00:00:00\n"
+                        + "1980,1970-01-01 00:00:20\n"
+                        + "1980,1970-01-01 00:00:40\n";
 
         LOG.info("Job finished");
+
+        List<String> files =
+                Stream.of(Objects.requireNonNull(new File(tmpDir.toString()).listFiles()))
+                        .filter(file -> !file.isDirectory())
+                        .map(File::getPath)
+                        .collect(Collectors.toList());
+        assertEquals(1, files.size());
+        Path resultFile = Paths.get(files.get(0));
+
+        LOG.info("Result found at {}", resultFile);
         String actual = new String(Files.readAllBytes(resultFile));
         LOG.info("Actual result is: '{}'", actual);
 
         assertEquals(expected, actual);
-    }
-
-    /** TableSource for generated data. */
-    public static class GeneratorTableSource extends InputFormatTableSource<Row> {
-
-        private final int numKeys;
-        private final float recordsPerKeyAndSecond;
-        private final int durationSeconds;
-        private final int offsetSeconds;
-
-        GeneratorTableSource(
-                int numKeys, float recordsPerKeyAndSecond, int durationSeconds, int offsetSeconds) {
-            this.numKeys = numKeys;
-            this.recordsPerKeyAndSecond = recordsPerKeyAndSecond;
-            this.durationSeconds = durationSeconds;
-            this.offsetSeconds = offsetSeconds;
-        }
-
-        @Override
-        public InputFormat<Row, ?> getInputFormat() {
-            return new IteratorInputFormat<>(
-                    DataGenerator.create(
-                            numKeys, recordsPerKeyAndSecond, durationSeconds, offsetSeconds));
-        }
-
-        @Override
-        public DataType getProducedDataType() {
-            return getTableSchema().toRowDataType();
-        }
-
-        @Override
-        public TableSchema getTableSchema() {
-            return TableSchema.builder()
-                    .field("key", DataTypes.INT())
-                    .field("rowtime", DataTypes.TIMESTAMP(3))
-                    .field("payload", DataTypes.STRING())
-                    .build();
-        }
-    }
-
-    /** Iterator for generated data. */
-    public static class DataGenerator implements Iterator<Row>, Serializable {
-        private static final long serialVersionUID = 1L;
-
-        final int numKeys;
-
-        private int keyIndex = 0;
-
-        private final long durationMs;
-        private final long stepMs;
-        private final long offsetMs;
-        private long ms = 0;
-
-        static DataGenerator create(
-                int numKeys, float rowsPerKeyAndSecond, int durationSeconds, int offsetSeconds) {
-            int sleepMs = (int) (1000 / rowsPerKeyAndSecond);
-            return new DataGenerator(
-                    numKeys, durationSeconds * 1000, sleepMs, offsetSeconds * 2000L);
-        }
-
-        DataGenerator(int numKeys, long durationMs, long stepMs, long offsetMs) {
-            this.numKeys = numKeys;
-            this.durationMs = durationMs;
-            this.stepMs = stepMs;
-            this.offsetMs = offsetMs;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return ms < durationMs;
-        }
-
-        @Override
-        public Row next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            Row row =
-                    Row.of(
-                            keyIndex,
-                            LocalDateTime.ofInstant(
-                                    Instant.ofEpochMilli(ms + offsetMs), ZoneOffset.UTC),
-                            "Some payload...");
-            ++keyIndex;
-            if (keyIndex >= numKeys) {
-                keyIndex = 0;
-                ms += stepMs;
-            }
-            return row;
-        }
     }
 }
