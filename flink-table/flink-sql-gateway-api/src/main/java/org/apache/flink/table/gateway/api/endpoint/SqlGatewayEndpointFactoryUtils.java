c3221a649b9daa8374cc24ab039f6352fb2c6edf
Weijie Guo <reswqa@163.com>
2024-05-23 15:38:46 +0800

    [hotfix][sqlgateway] Do not introduce new method in SqlGatewayEndpointFactory#Context

--- flink-table/flink-sql-gateway-api/src/main/java/org/apache/flink/table/gateway/api/endpoint/SqlGatewayEndpointFactoryUtils.java
+++ flink-table/flink-sql-gateway-api/src/main/java/org/apache/flink/table/gateway/api/endpoint/SqlGatewayEndpointFactoryUtils.java
@@ -142,11 +142,6 @@ public class SqlGatewayEndpointFactoryUtils {
         }
 
         @Override
-        public Map<String, String> getFlinkConfigurationOptions() {
-            return flinkConfiguration.toMap();
-        }
-
-        @Override
         public Map<String, String> getEndpointOptions() {
             return endpointConfig;
         }
