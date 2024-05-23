77111f7d33d64005a583dce4fbb72ab8e19f7a99
Zakelly <zakelly.lan@gmail.com>
2024-05-23 15:04:23 +0800

    [FLINK-35410][state] Avoid sync waiting in coordinator thread of ForSt executor (#24819)

--- flink-state-backends/flink-statebackend-forst/src/main/java/org/apache/flink/state/forst/ForStStateExecutor.java
+++ flink-state-backends/flink-statebackend-forst/src/main/java/org/apache/flink/state/forst/ForStStateExecutor.java
@@ -21,7 +21,6 @@ package org.apache.flink.state.forst;
 import org.apache.flink.runtime.asyncprocessing.StateExecutor;
 import org.apache.flink.runtime.asyncprocessing.StateRequest;
 import org.apache.flink.runtime.asyncprocessing.StateRequestContainer;
-import org.apache.flink.util.FlinkRuntimeException;
 import org.apache.flink.util.Preconditions;
 import org.apache.flink.util.concurrent.ExecutorThreadFactory;
 import org.apache.flink.util.concurrent.FutureUtils;
@@ -34,7 +33,6 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -77,7 +75,8 @@ public class ForStStateExecutor implements StateExecutor {
         Preconditions.checkArgument(stateRequestContainer instanceof ForStStateRequestClassifier);
         ForStStateRequestClassifier stateRequestClassifier =
                 (ForStStateRequestClassifier) stateRequestContainer;
-        return CompletableFuture.supplyAsync(
+        CompletableFuture<Void> resultFuture = new CompletableFuture<>();
+        coordinatorThread.execute(
                 () -> {
                     long startTime = System.currentTimeMillis();
                     List<CompletableFuture<Void>> futures = new ArrayList<>(2);
@@ -98,22 +97,20 @@ public class ForStStateExecutor implements StateExecutor {
                         futures.add(getOperations.process());
                     }
 
-                    try {
-                        FutureUtils.waitForAll(futures).get();
-                    } catch (InterruptedException | ExecutionException e) {
-                        throw new FlinkRuntimeException(
-                                "Exception when executing ForSt writeBatch or multiGet operation",
-                                e);
-                    }
-                    long duration = System.currentTimeMillis() - startTime;
-                    LOG.debug(
-                            "Complete executing a batch of state requests, putRequest size {}, getRequest size {}, duration {} ms",
-                            putRequests.size(),
-                            getRequests.size(),
-                            duration);
-                    return null;
-                },
-                coordinatorThread);
+                    FutureUtils.combineAll(futures)
+                            .thenAcceptAsync(
+                                    (e) -> {
+                                        long duration = System.currentTimeMillis() - startTime;
+                                        LOG.debug(
+                                                "Complete executing a batch of state requests, putRequest size {}, getRequest size {}, duration {} ms",
+                                                putRequests.size(),
+                                                getRequests.size(),
+                                                duration);
+                                        resultFuture.complete(null);
+                                    },
+                                    coordinatorThread);
+                });
+        return resultFuture;
     }
 
     @Override
