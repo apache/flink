4bad5839462356c183f7efd9dcc535f74352651d
Piotr Nowojski <piotr.nowojski@gmail.com>
2024-05-27 18:55:17 +0200

    [FLINK-38158][metrics] Allow to adjust transformation via SinkV2Provider

--- flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/plan/nodes/exec/common/CommonExecSink.java
+++ flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/plan/nodes/exec/common/CommonExecSink.java
@@ -451,16 +451,20 @@ public abstract class CommonExecSink extends ExecNodeBase<Object>
                         sinkMeta,
                         sinkParallelism);
             } else if (runtimeProvider instanceof SinkV2Provider) {
+                SinkV2Provider sinkV2Provider = (SinkV2Provider) runtimeProvider;
                 Transformation<RowData> sinkTransformation =
                         applyRowtimeTransformation(
                                 inputTransform, rowtimeFieldIndex, sinkParallelism, config);
                 final DataStream<RowData> dataStream = new DataStream<>(env, sinkTransformation);
-                final Transformation<?> transformation =
+                final Transformation<RowData> transformation =
                         DataStreamSink.forSink(
                                         dataStream,
-                                        ((SinkV2Provider) runtimeProvider).createSink(),
+                                        sinkV2Provider.createSink(),
                                         CustomSinkOperatorUidHashes.DEFAULT)
                                 .getTransformation();
+                sinkV2Provider
+                        .getAdditionalMetricVariables()
+                        .forEach(transformation::addMetricVariable);
                 transformation.setParallelism(sinkParallelism, sinkParallelismConfigured);
                 sinkMeta.fill(transformation);
                 return transformation;
