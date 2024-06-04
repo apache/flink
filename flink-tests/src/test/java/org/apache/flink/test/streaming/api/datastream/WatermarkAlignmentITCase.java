460ffd0e9ddec983f73a16cca6279f94e37ba178
elon-X <919928110@qq.com>
2024-06-04 23:03:04 +0800

    fixed watermarkAlignmentITCase

--- flink-tests/src/test/java/org/apache/flink/test/streaming/api/datastream/WatermarkAlignmentITCase.java
+++ flink-tests/src/test/java/org/apache/flink/test/streaming/api/datastream/WatermarkAlignmentITCase.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.test.streaming.api.datastream;
 
 import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
@@ -7,17 +25,17 @@ import org.apache.flink.api.connector.source.lib.NumberSequenceSource;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.junit.Assert.assertThat;
-
 /** This ITCase class tests the behavior of task execution with watermark alignment. */
-public class SubTaskFinishedWithWatermarkAlignmentITCase {
+public class WatermarkAlignmentITCase {
 
     /**
      * Test method to verify whether the watermark alignment works well with finished task.
@@ -45,8 +63,10 @@ public class SubTaskFinishedWithWatermarkAlignmentITCase {
 
         // Execute the stream and collect the results
         final List<Long> result = stream.executeAndCollect(101);
+        Collections.sort(result);
 
-        // Assert that the collected result contains all numbers from 0 to 100 in any order
-        assertThat(result, containsInAnyOrder(LongStream.rangeClosed(0, 100).boxed().toArray()));
+        // Assert that the collected result contains all numbers from 0 to 100
+        Assertions.assertIterableEquals(
+                result, LongStream.rangeClosed(0, 100).boxed().collect(Collectors.toList()));
     }
 }
