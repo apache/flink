23fa2aeb01861a6f8b85f911c5a3f989a1f75898
Yuxin Tan <tanyuxinwork@gmail.com>
2024-06-06 11:48:48 +0800

    [FLINK-35533][runtime] Add a option to config the external remote tier factory class

--- flink-core/src/main/java/org/apache/flink/configuration/NettyShuffleEnvironmentOptions.java
+++ flink-core/src/main/java/org/apache/flink/configuration/NettyShuffleEnvironmentOptions.java
@@ -490,6 +490,24 @@ public class NettyShuffleEnvironmentOptions {
                                     + " is configured. The new mode is currently in an experimental phase. It can be set to false to fallback to the legacy mode "
                                     + " if something unexpected. Once the new mode reaches a stable state, the legacy mode as well as the option will be removed.");
 
+    /** The option to configure the tiered factory creator remote class name for hybrid shuffle. */
+    @Documentation.Section(Documentation.Sections.ALL_TASK_MANAGER_NETWORK)
+    @Experimental
+    public static final ConfigOption<String>
+            NETWORK_HYBRID_SHUFFLE_EXTERNAL_REMOTE_TIER_FACTORY_CLASS_NAME =
+                    key("taskmanager.network.hybrid-shuffle.external-remote-tier-factory.class")
+                            .stringType()
+                            .noDefaultValue()
+                            .withDescription(
+                                    "The option configures the class that is responsible for creating an "
+                                            + "external remote tier factory for hybrid shuffle. If "
+                                            + "configured, the hybrid shuffle will only initialize "
+                                            + "the specified remote tier according to the given class "
+                                            + "name. Currently, since the tier interfaces are not yet "
+                                            + "public and are still actively evolving, it is recommended "
+                                            + "that users do not independently implement the external "
+                                            + "remote tier until the tier interfaces are stabilized. ");
+
     /** The option to configure the base remote storage path for hybrid shuffle. */
     @Documentation.Section(Documentation.Sections.ALL_TASK_MANAGER_NETWORK)
     @Experimental
