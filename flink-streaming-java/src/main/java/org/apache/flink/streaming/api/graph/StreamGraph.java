960363cd3c6c82f7e56ef781295756105f7b5eba
Peter Huang <huangzhenqiu0825@gmail.com>
2024-07-02 18:58:18 -0700

    [FLINK-33211][table] support flink table lineage (#24618)

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/graph/StreamGraph.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/graph/StreamGraph.java
@@ -124,6 +124,7 @@ public class StreamGraph implements Pipeline {
     private CheckpointStorage checkpointStorage;
     private Set<Tuple2<StreamNode, StreamNode>> iterationSourceSinkPairs;
     private InternalTimeServiceManager.Provider timerServiceProvider;
+    private LineageGraph lineageGraph;
     private JobType jobType = JobType.STREAMING;
     private Map<String, ResourceProfile> slotSharingGroupResources;
     private PipelineOptions.VertexDescriptionMode descriptionMode =
@@ -190,6 +191,14 @@ public class StreamGraph implements Pipeline {
 
     public void setJobName(String jobName) {
         this.jobName = jobName;
+    }
+
+    public LineageGraph getLineageGraph() {
+        return lineageGraph;
+    }
+
+    public void setLineageGraph(LineageGraph lineageGraph) {
+        this.lineageGraph = lineageGraph;
     }
 
     public void setStateBackend(StateBackend backend) {
