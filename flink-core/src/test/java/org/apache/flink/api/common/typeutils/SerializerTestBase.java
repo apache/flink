44520825c33585ee380259fc275a20346b8a6cc0
xuyang <xyzhong188@163.com>
2024-08-14 12:10:16 +0800

    check all subtask of TypeSerializer manually and refactor the test

--- flink-core/src/test/java/org/apache/flink/api/common/typeutils/SerializerTestBase.java
+++ flink-core/src/test/java/org/apache/flink/api/common/typeutils/SerializerTestBase.java
@@ -40,9 +40,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.IdentityHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CyclicBarrier;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -170,22 +168,29 @@ public abstract class SerializerTestBase<T> {
 
         T[] testData = getData();
 
-        Map<T, Void> originalData = new IdentityHashMap<>();
-        Map<T, Void> copiedData = new IdentityHashMap<>();
-
         for (T datum : testData) {
-            originalData.put(datum, null);
             T copy = serializer.copy(datum);
             checkToString(copy);
             deepEquals("Copied element is not equal to the original element.", datum, copy);
-            copiedData.put(copy, null);
+        }
+    }
+
+    @Test
+    void testCopyIndependently() {
+        TypeSerializer<T> serializer = getSerializer();
+        T[] originalData = getData();
+        List<T> copiedData = new ArrayList<>(originalData.length);
+
+        for (T datum : originalData) {
+            T copy = serializer.copy(datum, serializer.createInstance());
+            copiedData.add(copy);
         }
 
-        // ensure that all copied data is not reused
-        // Note: Some data may be reused before `copy` but may not be reused
-        // afterward, such as strings in the constant pool. See more at
-        // SimpleVersionedSerializerTypeSerializerProxy.
-        assertThat(copiedData.size()).isGreaterThanOrEqualTo(originalData.size());
+        for (int i = 0; i < originalData.length; i++) {
+            T original = originalData[i];
+            T copied = copiedData.get(i);
+            deepEquals("Copied element is not equal to the original element.", original, copied);
+        }
     }
 
     @Test
@@ -440,7 +445,7 @@ public abstract class SerializerTestBase<T> {
 
     // --------------------------------------------------------------------------------------------
 
-    private void deepEquals(String message, T should, T is) {
+    protected void deepEquals(String message, T should, T is) {
         assertThat(is)
                 .as(message)
                 .matches(CustomEqualityMatcher.deeplyEquals(should).withChecker(checker));
