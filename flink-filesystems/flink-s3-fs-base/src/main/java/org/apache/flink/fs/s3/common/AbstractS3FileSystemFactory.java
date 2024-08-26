confluentinc/flink-public

[FLINK-35771][s3] Limit the amount of work per s5cmd call
Motivation:
- limit CPU usage by S5cmd to prevent TM overload
- pick up potentially updated credentials for every s5cmd call

flink-filesystems/flink-s3-fs-base/src/main/java/org/apache/flink/fs/s3/common/AbstractS3FileSystemFactory.java
@@ -23,6 +23,7 @@ import org.apache.flink.configuration.ConfigOptions;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.ConfigurationUtils;
 import org.apache.flink.configuration.IllegalConfigurationException;
+import org.apache.flink.configuration.MemorySize;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.core.fs.FileSystemFactory;
 import org.apache.flink.fs.s3.common.FlinkS3FileSystem.S5CmdConfiguration;
@@ -76,6 +77,18 @@ public abstract class AbstractS3FileSystemFactory implements FileSystemFactory {
                     .defaultValue("-r 0")
                     .withDescription(
                             "Extra arguments to be passed to s5cmd. For example, --no-sign-request for public buckets and -r 10 for 10 retries");
+
+    public static final ConfigOption<MemorySize> S5CMD_BATCH_MAX_STATE_SIZE =
+            ConfigOptions.key("s3.s5cmd.batch.max-state")
+                    .memoryType()
+                    .defaultValue(MemorySize.ofMebiBytes(1024))
+                    .withDescription("Maximum size of files to download per one call to s5cmd.");
+
+    public static final ConfigOption<Integer> S5CMD_BATCH_MAX_FILES =
+            ConfigOptions.key("s3.s5cmd.batch.max-files")
+                    .intType()
+                    .defaultValue(100)
+                    .withDescription("Maximum number of files to download per one call to s5cmd");
 
     public static final ConfigOption<Long> PART_UPLOAD_MIN_SIZE =
             ConfigOptions.key("s3.upload.min.part.size")

flink-filesystems/flink-s3-fs-base/src/main/java/org/apache/flink/fs/s3/common/FlinkS3FileSystem.java
@@ -62,6 +62,8 @@ import java.util.concurrent.atomic.AtomicReference;
 
 import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.ACCESS_KEY;
 import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.ENDPOINT;
+import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.S5CMD_BATCH_MAX_FILES;
+import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.S5CMD_BATCH_MAX_STATE_SIZE;
 import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.S5CMD_EXTRA_ARGS;
 import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.S5CMD_PATH;
 import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.SECRET_KEY;
@@ -112,6 +114,8 @@ public class FlinkS3FileSystem extends HadoopFileSystem
         @Nullable private final String accessArtifact;
         @Nullable private final String secretArtifact;
         @Nullable private final String endpoint;
+        private long maxBatchSizeFiles;
+        private long maxBatchSizeBytes;
 
         /** All parameters can be empty. */
         public S5CmdConfiguration(
@@ -119,7 +123,9 @@ public class FlinkS3FileSystem extends HadoopFileSystem
                 String args,
                 @Nullable String accessArtifact,
                 @Nullable String secretArtifact,
-                @Nullable String endpoint) {
+                @Nullable String endpoint,
+                int maxBatchSizeFiles,
+                long maxBatchSizeBytes) {
             if (!path.isEmpty()) {
                 File s5CmdFile = new File(path);
                 checkArgument(s5CmdFile.isFile(), "Unable to find s5cmd binary under [%s]", path);
@@ -131,6 +137,8 @@ public class FlinkS3FileSystem extends HadoopFileSystem
             this.accessArtifact = accessArtifact;
             this.secretArtifact = secretArtifact;
             this.endpoint = endpoint;
+            this.maxBatchSizeFiles = maxBatchSizeFiles;
+            this.maxBatchSizeBytes = maxBatchSizeBytes;
         }
 
         public static Optional<S5CmdConfiguration> of(Configuration flinkConfig) {
@@ -143,7 +151,11 @@ public class FlinkS3FileSystem extends HadoopFileSystem
                                             flinkConfig.getString(S5CMD_EXTRA_ARGS),
                                             flinkConfig.get(ACCESS_KEY),
                                             flinkConfig.get(SECRET_KEY),
-                                            flinkConfig.get(ENDPOINT)));
+                                            flinkConfig.get(ENDPOINT),
+                                            flinkConfig.get(S5CMD_BATCH_MAX_FILES),
+                                            flinkConfig
+                                                    .get(S5CMD_BATCH_MAX_STATE_SIZE)
+                                                    .getBytes()));
         }
 
         private void configureEnvironment(Map<String, String> environment) {
@@ -263,7 +275,32 @@ public class FlinkS3FileSystem extends HadoopFileSystem
         artefacts.add(s5CmdConfiguration.path);
         artefacts.addAll(s5CmdConfiguration.args);
         artefacts.add("run");
-        castSpell(convertToSpells(requests), closeableRegistry, artefacts.toArray(new String[0]));
+
+        ArrayList<CopyRequest> batch = new ArrayList<>();
+        long runningSizeBytes = 0L;
+        long runningSizeFiles = 0L;
+        for (int i = 0; i < requests.size(); i++) {
+            CopyRequest request = requests.get(i);
+            batch.add(request);
+            runningSizeBytes += request.getSize();
+            runningSizeFiles++;
+            if (runningSizeBytes >= s5CmdConfiguration.maxBatchSizeBytes
+                    || runningSizeFiles >= s5CmdConfiguration.maxBatchSizeFiles
+                    || i == requests.size() - 1) {
+                LOG.info(
+                        "Copy {} files using s5cmd, total size: {}, args: {}",
+                        requests.size(),
+                        runningSizeBytes,
+                        artefacts);
+                castSpell(
+                        convertToSpells(batch),
+                        closeableRegistry,
+                        artefacts.toArray(new String[0]));
+                runningSizeFiles = 0;
+                runningSizeBytes = 0;
+                batch.clear();
+            }
+        }
     }
 
     private List<String> convertToSpells(List<CopyRequest> requests) throws IOException {
@@ -282,7 +319,6 @@ public class FlinkS3FileSystem extends HadoopFileSystem
     private void castSpell(
             List<String> spells, ICloseableRegistry closeableRegistry, String... artefacts)
             throws IOException {
-        LOG.info("Casting spell: {}", Arrays.toString(artefacts));
         int exitCode = 0;
         final AtomicReference<IOException> maybeCloseableRegistryException =
                 new AtomicReference<>();

apache/flink

flink-filesystems/flink-s3-fs-base/src/main/java/org/apache/flink/fs/s3/common/AbstractS3FileSystemFactory.java
@@ -23,6 +23,7 @@ import org.apache.flink.configuration.ConfigOptions;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.ConfigurationUtils;
 import org.apache.flink.configuration.IllegalConfigurationException;
+import org.apache.flink.configuration.MemorySize;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.core.fs.FileSystemFactory;
 import org.apache.flink.fs.s3.common.FlinkS3FileSystem.S5CmdConfiguration;
@@ -76,6 +77,18 @@ public abstract class AbstractS3FileSystemFactory implements FileSystemFactory {
                     .defaultValue("-r 0")
                     .withDescription(
                             "Extra arguments to be passed to s5cmd. For example, --no-sign-request for public buckets and -r 10 for 10 retries");
+
+    public static final ConfigOption<MemorySize> S5CMD_BATCH_MAX_SIZE =
+            ConfigOptions.key("s3.s5cmd.batch.max-size")
+                    .memoryType()
+                    .defaultValue(MemorySize.ofMebiBytes(1024))
+                    .withDescription("Maximum size of files to download per one call to s5cmd.");
+
+    public static final ConfigOption<Integer> S5CMD_BATCH_MAX_FILES =
+            ConfigOptions.key("s3.s5cmd.batch.max-files")
+                    .intType()
+                    .defaultValue(100)
+                    .withDescription("Maximum number of files to download per one call to s5cmd");
 
     public static final ConfigOption<Long> PART_UPLOAD_MIN_SIZE =
             ConfigOptions.key("s3.upload.min.part.size")

flink-filesystems/flink-s3-fs-base/src/main/java/org/apache/flink/fs/s3/common/FlinkS3FileSystem.java
@@ -62,6 +62,8 @@ import java.util.concurrent.atomic.AtomicReference;
 
 import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.ACCESS_KEY;
 import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.ENDPOINT;
+import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.S5CMD_BATCH_MAX_FILES;
+import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.S5CMD_BATCH_MAX_SIZE;
 import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.S5CMD_EXTRA_ARGS;
 import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.S5CMD_PATH;
 import static org.apache.flink.fs.s3.common.AbstractS3FileSystemFactory.SECRET_KEY;
@@ -112,6 +114,8 @@ public class FlinkS3FileSystem extends HadoopFileSystem
         @Nullable private final String accessArtifact;
         @Nullable private final String secretArtifact;
         @Nullable private final String endpoint;
+        private long maxBatchSizeFiles;
+        private long maxBatchSizeBytes;
 
         /** All parameters can be empty. */
         public S5CmdConfiguration(
@@ -119,7 +123,9 @@ public class FlinkS3FileSystem extends HadoopFileSystem
                 String args,
                 @Nullable String accessArtifact,
                 @Nullable String secretArtifact,
-                @Nullable String endpoint) {
+                @Nullable String endpoint,
+                int maxBatchSizeFiles,
+                long maxBatchSizeBytes) {
             if (!path.isEmpty()) {
                 File s5CmdFile = new File(path);
                 checkArgument(s5CmdFile.isFile(), "Unable to find s5cmd binary under [%s]", path);
@@ -131,6 +137,8 @@ public class FlinkS3FileSystem extends HadoopFileSystem
             this.accessArtifact = accessArtifact;
             this.secretArtifact = secretArtifact;
             this.endpoint = endpoint;
+            this.maxBatchSizeFiles = maxBatchSizeFiles;
+            this.maxBatchSizeBytes = maxBatchSizeBytes;
         }
 
         public static Optional<S5CmdConfiguration> of(Configuration flinkConfig) {
@@ -143,7 +151,9 @@ public class FlinkS3FileSystem extends HadoopFileSystem
                                             flinkConfig.getString(S5CMD_EXTRA_ARGS),
                                             flinkConfig.get(ACCESS_KEY),
                                             flinkConfig.get(SECRET_KEY),
-                                            flinkConfig.get(ENDPOINT)));
+                                            flinkConfig.get(ENDPOINT),
+                                            flinkConfig.get(S5CMD_BATCH_MAX_FILES),
+                                            flinkConfig.get(S5CMD_BATCH_MAX_SIZE).getBytes()));
         }
 
         private void configureEnvironment(Map<String, String> environment) {
@@ -263,7 +273,32 @@ public class FlinkS3FileSystem extends HadoopFileSystem
         artefacts.add(s5CmdConfiguration.path);
         artefacts.addAll(s5CmdConfiguration.args);
         artefacts.add("run");
-        castSpell(convertToSpells(requests), closeableRegistry, artefacts.toArray(new String[0]));
+
+        ArrayList<CopyRequest> batch = new ArrayList<>();
+        long runningSizeBytes = 0L;
+        long runningSizeFiles = 0L;
+        for (int i = 0; i < requests.size(); i++) {
+            CopyRequest request = requests.get(i);
+            batch.add(request);
+            runningSizeBytes += request.getSize();
+            runningSizeFiles++;
+            if (runningSizeBytes >= s5CmdConfiguration.maxBatchSizeBytes
+                    || runningSizeFiles >= s5CmdConfiguration.maxBatchSizeFiles
+                    || i == requests.size() - 1) {
+                LOG.info(
+                        "Copy {} files using s5cmd, total size: {}, args: {}",
+                        requests.size(),
+                        runningSizeBytes,
+                        artefacts);
+                castSpell(
+                        convertToSpells(batch),
+                        closeableRegistry,
+                        artefacts.toArray(new String[0]));
+                runningSizeFiles = 0;
+                runningSizeBytes = 0;
+                batch.clear();
+            }
+        }
     }
 
     private List<String> convertToSpells(List<CopyRequest> requests) throws IOException {
@@ -282,7 +317,6 @@ public class FlinkS3FileSystem extends HadoopFileSystem
     private void castSpell(
             List<String> spells, ICloseableRegistry closeableRegistry, String... artefacts)
             throws IOException {
-        LOG.info("Casting spell: {}", Arrays.toString(artefacts));
         int exitCode = 0;
         final AtomicReference<IOException> maybeCloseableRegistryException =
                 new AtomicReference<>();