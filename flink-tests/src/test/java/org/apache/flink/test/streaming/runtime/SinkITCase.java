7d0bb785de22d2f8b224cb6f00eafa72e052e2cf
Arvid Heise <arvid@apache.org>
2024-09-05 15:25:12 +0200

    [FLINK-25920] Handle duplicate EOI in Sink
    In case of a failure after final checkpoint, EOI is called twice.
    
    SinkWriter should ignore the second call to avoid emitting more dummy committables = transactional objects containing no data since no data can arrive when recovering from final checkpoint. The commit uses a boolean list state to remember if EOI has been emitted. The cases are discussed in code.
    
    Since rescaling may still result in these dummy committables, the committer needs merge them into the CommittableCollector as these committables still need to be committed as systems like Kafka don't provide transactional isolation.
    
    (cherry picked from commit 37e6724813bd92f8d323ddec80308241f693a5e1)

--- flink-tests/src/test/java/org/apache/flink/test/streaming/runtime/SinkITCase.java
+++ flink-tests/src/test/java/org/apache/flink/test/streaming/runtime/SinkITCase.java
@@ -17,25 +17,38 @@
 
 package org.apache.flink.test.streaming.runtime;
 
+import org.apache.flink.api.common.JobStatus;
 import org.apache.flink.api.common.RuntimeExecutionMode;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.configuration.CheckpointingOptions;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.RestartStrategyOptions;
 import org.apache.flink.connector.datagen.source.TestDataGenerators;
+import org.apache.flink.core.execution.JobClient;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.runtime.operators.sink.TestSink;
-import org.apache.flink.test.util.AbstractTestBaseJUnit4;
+import org.apache.flink.streaming.runtime.operators.sink.TestSinkV2;
+import org.apache.flink.test.util.AbstractTestBase;
+import org.apache.flink.testutils.junit.SharedObjectsExtension;
+import org.apache.flink.testutils.junit.SharedReference;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BooleanSupplier;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -48,7 +61,7 @@ import static org.hamcrest.Matchers.containsInAnyOrder;
 /**
  * Integration test for {@link org.apache.flink.api.connector.sink.Sink} run time implementation.
  */
-public class SinkITCase extends AbstractTestBaseJUnit4 {
+class SinkITCase extends AbstractTestBase {
     static final List<Integer> SOURCE_DATA =
             Arrays.asList(
                     895, 127, 148, 161, 148, 662, 822, 491, 275, 122, 850, 630, 682, 765, 434, 970,
@@ -109,14 +122,17 @@ public class SinkITCase extends AbstractTestBaseJUnit4 {
                             COMMIT_QUEUE_RECEIVE_ALL_DATA.getAsBoolean()
                                     && GLOBAL_COMMIT_QUEUE_RECEIVE_ALL_DATA.getAsBoolean();
 
-    @Before
+    @RegisterExtension
+    private final SharedObjectsExtension sharedObjects = SharedObjectsExtension.create();
+
+    @BeforeEach
     public void init() {
         COMMIT_QUEUE.clear();
         GLOBAL_COMMIT_QUEUE.clear();
     }
 
     @Test
-    public void writerAndCommitterAndGlobalCommitterExecuteInStreamingMode() throws Exception {
+    void writerAndCommitterAndGlobalCommitterExecuteInStreamingMode() throws Exception {
         final StreamExecutionEnvironment env = buildStreamEnv();
 
         final DataStream<Integer> stream =
@@ -153,7 +169,7 @@ public class SinkITCase extends AbstractTestBaseJUnit4 {
     }
 
     @Test
-    public void writerAndCommitterAndGlobalCommitterExecuteInBatchMode() throws Exception {
+    void writerAndCommitterAndGlobalCommitterExecuteInBatchMode() throws Exception {
         final StreamExecutionEnvironment env = buildBatchEnv();
 
         env.fromData(SOURCE_DATA)
@@ -177,7 +193,7 @@ public class SinkITCase extends AbstractTestBaseJUnit4 {
     }
 
     @Test
-    public void writerAndCommitterExecuteInStreamingMode() throws Exception {
+    void writerAndCommitterExecuteInStreamingMode() throws Exception {
         final StreamExecutionEnvironment env = buildStreamEnv();
 
         final DataStream<Integer> stream =
@@ -198,8 +214,45 @@ public class SinkITCase extends AbstractTestBaseJUnit4 {
                 containsInAnyOrder(EXPECTED_COMMITTED_DATA_IN_STREAMING_MODE.toArray()));
     }
 
+    /**
+     * Creates a bounded stream with a failing committer. The test verifies that the Sink correctly
+     * recovers and handles multiple endInput().
+     */
     @Test
-    public void writerAndCommitterExecuteInBatchMode() throws Exception {
+    void duplicateEndInput() throws Exception {
+        // we need at least 2 attempts but add a bit of a safety margin for unexpected retries
+        int maxAttempts = 10;
+        final Configuration conf = new Configuration();
+        conf.set(CheckpointingOptions.TOLERABLE_FAILURE_NUMBER, maxAttempts);
+        conf.set(RestartStrategyOptions.RESTART_STRATEGY, "fixed-delay");
+        conf.set(RestartStrategyOptions.RESTART_STRATEGY_FIXED_DELAY_ATTEMPTS, maxAttempts);
+
+        final StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.getExecutionEnvironment(conf);
+        env.enableCheckpointing(100);
+
+        AtomicBoolean failedOnce = new AtomicBoolean(false);
+        List<String> committed = new ArrayList<>();
+        FailingOnceCommitter committer =
+                new FailingOnceCommitter(
+                        sharedObjects.add(failedOnce), sharedObjects.add(committed));
+        env.<Object>fromData("bounded")
+                .sinkTo(TestSinkV2.newBuilder().setCommitter(committer).build());
+
+        JobClient jobClient = env.executeAsync();
+        // wait for job to finish including restarts
+        jobClient.getJobExecutionResult().get();
+        // Did we successfully finish?
+        Assertions.assertThat(jobClient.getJobStatus().get()).isEqualTo(JobStatus.FINISHED);
+
+        // check that we error'ed once as expected
+        Assertions.assertThat(failedOnce).isTrue();
+        // but also eventually succeed to commit (size > 1 in case of unexpected retries)
+        Assertions.assertThat(committed).isNotEmpty();
+    }
+
+    @Test
+    void writerAndCommitterExecuteInBatchMode() throws Exception {
         final StreamExecutionEnvironment env = buildBatchEnv();
 
         env.fromData(SOURCE_DATA)
@@ -214,7 +267,7 @@ public class SinkITCase extends AbstractTestBaseJUnit4 {
     }
 
     @Test
-    public void writerAndGlobalCommitterExecuteInStreamingMode() throws Exception {
+    void writerAndGlobalCommitterExecuteInStreamingMode() throws Exception {
         final StreamExecutionEnvironment env = buildStreamEnv();
 
         final DataStream<Integer> stream =
@@ -245,7 +298,7 @@ public class SinkITCase extends AbstractTestBaseJUnit4 {
     }
 
     @Test
-    public void writerAndGlobalCommitterExecuteInBatchMode() throws Exception {
+    void writerAndGlobalCommitterExecuteInBatchMode() throws Exception {
         final StreamExecutionEnvironment env = buildBatchEnv();
 
         env.fromData(SOURCE_DATA)
@@ -279,5 +332,27 @@ public class SinkITCase extends AbstractTestBaseJUnit4 {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setRuntimeMode(RuntimeExecutionMode.BATCH);
         return env;
+    }
+
+    static class FailingOnceCommitter extends TestSinkV2.DefaultCommitter {
+        private final SharedReference<AtomicBoolean> failedOnce;
+        private final SharedReference<List<String>> committed;
+
+        public FailingOnceCommitter(
+                SharedReference<AtomicBoolean> failedOnce,
+                SharedReference<List<String>> committed) {
+            this.failedOnce = failedOnce;
+            this.committed = committed;
+        }
+
+        @Override
+        public void commit(Collection<CommitRequest<String>> committables) {
+            if (failedOnce.get().compareAndSet(false, true)) {
+                throw new RuntimeException("Fail to commit");
+            }
+            for (CommitRequest<String> committable : committables) {
+                this.committed.get().add(committable.getCommittable());
+            }
+        }
     }
 }
