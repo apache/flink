c4cab106bc82222fdc8e921d80129c6de849ef52
Weijie Guo <reswqa@163.com>
2024-09-23 11:52:13 +0800

    fixup! [FLINK-34125][serializer] Remove deprecated TypeInformation#createSerializer(ExecutionConfig) method.

--- flink-python/pyflink/datastream/connectors/tests/test_cassandra.py
+++ flink-python/pyflink/datastream/connectors/tests/test_cassandra.py
@@ -15,6 +15,8 @@
 #  See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
+import unittest
+
 from pyflink.common import Types
 from pyflink.datastream.connectors.cassandra import CassandraSink, MapperOptions, ConsistencyLevel
 from pyflink.testing.test_case_utils import PyFlinkStreamingTestCase
