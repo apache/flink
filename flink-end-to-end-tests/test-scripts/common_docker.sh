51a7940ac2fea0bfe39208d1ae9774eb42263290
Weijie Guo <reswqa@163.com>
2024-10-10 22:48:47 +0800

    Commit for release 2.0-preview1

--- flink-end-to-end-tests/test-scripts/common_docker.sh
+++ flink-end-to-end-tests/test-scripts/common_docker.sh
@@ -47,7 +47,7 @@ function build_image() {
     local server_pid=$!
 
     echo "Preparing Dockeriles"
-    retry_times_with_exponential_backoff 5 git clone https://github.com/apache/flink-docker.git --branch dev-master --single-branch
+    retry_times_with_exponential_backoff 5 git clone https://github.com/apache/flink-docker.git --branch dev-2.0 --single-branch
 
     local java_version=11
     if [[ ${PROFILE} == *"jdk17"* ]]; then
