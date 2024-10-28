ca987e4938e458b7349298231ae6dc34d0b0a064
Sergey Nuyanzin <snuyanzin@gmail.com>
2024-10-28 13:30:40 +0100

    [FLINK-36613][tests] Ensure RescaleCheckpointManuallyITCase.testCheckpointRescalingInKeyedState runs with large enough value for STARTING_MEMORY_SEGMENT_SIZE

--- flink-tests/src/test/java/org/apache/flink/test/checkpointing/RescaleCheckpointManuallyITCase.java
+++ flink-tests/src/test/java/org/apache/flink/test/checkpointing/RescaleCheckpointManuallyITCase.java
@@ -27,8 +27,10 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.CheckpointingOptions;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.ExternalizedCheckpointRetention;
+import org.apache.flink.configuration.MemorySize;
 import org.apache.flink.configuration.RestartStrategyOptions;
 import org.apache.flink.configuration.StateBackendOptions;
+import org.apache.flink.configuration.TaskManagerOptions;
 import org.apache.flink.runtime.jobgraph.JobGraph;
 import org.apache.flink.runtime.minicluster.MiniCluster;
 import org.apache.flink.runtime.state.FunctionInitializationContext;
@@ -87,6 +89,9 @@ public class RescaleCheckpointManuallyITCase extends TestLogger {
         Configuration config = new Configuration();
         config.set(StateBackendOptions.STATE_BACKEND, "rocksdb");
         config.set(CheckpointingOptions.INCREMENTAL_CHECKPOINTS, true);
+        config.set(
+                TaskManagerOptions.STARTING_MEMORY_SEGMENT_SIZE,
+                MemorySize.parse(String.valueOf(Short.MAX_VALUE)));
 
         cluster =
                 new MiniClusterWithClientResource(
