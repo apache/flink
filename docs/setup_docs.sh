3fca180f4c6396d227dc9b8843fa5ba4128b0ea2
Hong Teoh <liangtl@amazon.com>
2024-11-08 14:13:53 +0000

    [FLINK-36289][Connectors/Prometheus][docs] Integrate Prometheus connector docs into Flink website

--- docs/setup_docs.sh
+++ docs/setup_docs.sh
@@ -62,6 +62,7 @@ if [ "$SKIP_INTEGRATE_CONNECTOR_DOCS" = false ]; then
   integrate_connector_docs opensearch v1.1
   integrate_connector_docs kafka v3.3
   integrate_connector_docs hbase v3.0
+  integrate_connector_docs prometheus v1.0
 
   cd ..
   rm -rf tmp
