036a8b8de48042a824cbea6ab8633911c4e6ed78
Gantigmaa Selenge <tina.selenge@gmail.com>
2024-11-15 12:44:54 +0000

    Address review comment

--- flink-table/flink-table-common/src/main/java/org/apache/flink/table/api/dataview/MapView.java
+++ flink-table/flink-table-common/src/main/java/org/apache/flink/table/api/dataview/MapView.java
@@ -85,6 +85,12 @@ import java.util.Objects;
  * }
  *
  * }</pre>
+ *
+ * @param <K> key type
+ * @param <V> value type
+ *
+ * {@code MapView(TypeInformation<?> keyType, TypeInformation<?> valueType)} method was
+ * deprecated and removed. Please use a {@link DataTypeHint} instead.
  */
 @PublicEvolving
 public class MapView<K, V> implements DataView {
