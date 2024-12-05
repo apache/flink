e79b187242a8555df37efb4427a07f41a5d5714a
fredia <fredialei@gmail.com>
2024-12-05 15:14:41 +0800

    [FLINK-34974][state] Support getOrCreateKeyedState for AsyncKeyedStateBackend

--- flink-runtime/src/main/java/org/apache/flink/streaming/api/operators/StreamOperatorStateHandler.java
+++ flink-runtime/src/main/java/org/apache/flink/streaming/api/operators/StreamOperatorStateHandler.java
@@ -416,7 +416,7 @@ public class StreamOperatorStateHandler {
             throws Exception {
 
         if (asyncKeyedStateBackend != null) {
-            return asyncKeyedStateBackend.createState(
+            return asyncKeyedStateBackend.getOrCreateKeyedState(
                     defaultNamespace, namespaceSerializer, stateDescriptor);
         } else {
             throw new IllegalStateException(
