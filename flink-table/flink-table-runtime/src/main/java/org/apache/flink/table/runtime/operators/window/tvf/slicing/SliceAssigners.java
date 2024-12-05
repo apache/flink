960d447ce1f5c0c16c29ef045a95094216de3dba
Kaitian Hu <xingmo31@gmail.com>
2024-12-05 21:18:09 +0800

    fix

--- flink-table/flink-table-runtime/src/main/java/org/apache/flink/table/runtime/operators/window/tvf/slicing/SliceAssigners.java
+++ flink-table/flink-table-runtime/src/main/java/org/apache/flink/table/runtime/operators/window/tvf/slicing/SliceAssigners.java
@@ -594,7 +594,7 @@ public final class SliceAssigners {
 
         @Override
         public long getWindowOffset() {
-            return 0;
+            return innerAssigner.getWindowOffset();
         }
 
         @Override
