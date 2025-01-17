aef8c868545d70b743ee5cf29e17489f8a3bd848
Mina Asham <mina.asham@hotmail.com>
2025-01-17 13:59:56 +0000

    [FLINK-37192][python] Replace deprecated avro-python3 with avro
    - avro-python3 was deprecated and replaced by avro, the first hasn't had a release since March 17, 2021 while the second has had multiple fixes and updated, latest is August 5, 2024
    - Both libraries are the exact same (i.e. `avro-python3` was just renamed to `avro` and had multiple updates since), but the problem is that they overlap in package name, so using PyFlink with any updated library that relies on `avro` fails starting the pipeline even if the pipeline doesn't actually do any avro encoding/decoding
    - This updates the library to the latest one, and fixes a few imports, other than that the library's functionality is exactly the same

--- flink-python/setup.py
+++ flink-python/setup.py
@@ -318,7 +318,7 @@ try:
 
     install_requires = ['py4j==0.10.9.7', 'python-dateutil>=2.8.0,<3',
                         'apache-beam>=2.54.0,<=2.61.0',
-                        'cloudpickle>=2.2.0', 'avro-python3>=1.8.1,!=1.9.2',
+                        'cloudpickle>=2.2.0', 'avro>=1.12.0',
                         'pytz>=2018.3', 'fastavro>=1.1.0,!=1.8.0', 'requests>=2.26.0',
                         'protobuf>=3.19.0',
                         'numpy>=1.22.4',
