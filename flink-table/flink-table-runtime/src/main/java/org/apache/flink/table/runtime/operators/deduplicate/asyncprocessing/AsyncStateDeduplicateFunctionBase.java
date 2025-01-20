0cef14d6fe5ea501708d5575b3fdb678bd3cdda5
xuyang <xyzhong188@163.com>
2025-01-20 10:51:45 +0800

    [FLINK-37172][table-runtime] Add logs in all existing async state ops to check if they are under async state when running

--- flink-table/flink-table-runtime/src/main/java/org/apache/flink/table/runtime/operators/deduplicate/asyncprocessing/AsyncStateDeduplicateFunctionBase.java
+++ flink-table/flink-table-runtime/src/main/java/org/apache/flink/table/runtime/operators/deduplicate/asyncprocessing/AsyncStateDeduplicateFunctionBase.java
@@ -27,6 +27,9 @@ import org.apache.flink.api.common.typeutils.TypeSerializer;
 import org.apache.flink.streaming.api.operators.StreamingRuntimeContext;
 import org.apache.flink.table.runtime.operators.deduplicate.DeduplicateFunctionBase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static org.apache.flink.table.runtime.util.StateConfigUtil.createTtlConfig;
 
 /**
@@ -42,6 +45,9 @@ abstract class AsyncStateDeduplicateFunctionBase<T, K, IN, OUT>
 
     private static final long serialVersionUID = 1L;
 
+    private static final Logger LOG =
+            LoggerFactory.getLogger(AsyncStateDeduplicateFunctionBase.class);
+
     // state stores previous message under the key.
     protected ValueState<T> state;
 
@@ -53,6 +59,8 @@ abstract class AsyncStateDeduplicateFunctionBase<T, K, IN, OUT>
     @Override
     public void open(OpenContext openContext) throws Exception {
         super.open(openContext);
+
+        LOG.info("Deduplicate is using async state");
 
         ValueStateDescriptor<T> stateDesc =
                 new ValueStateDescriptor<>("deduplicate-state", typeInfo);
