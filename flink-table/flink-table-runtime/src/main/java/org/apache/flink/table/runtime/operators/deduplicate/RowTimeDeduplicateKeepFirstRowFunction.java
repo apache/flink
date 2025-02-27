confluentinc/flink-public

[FLINK-37005][table] Add RowTimeDeduplicateKeepFirstRowFunction

flink-table/flink-table-runtime/src/main/java/org/apache/flink/table/runtime/operators/deduplicate/RowTimeDeduplicateKeepFirstRowFunction.java
@@ -1,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.runtime.operators.deduplicate;
+
+import org.apache.flink.api.common.functions.OpenContext;
+import org.apache.flink.api.common.state.StateTtlConfig;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.runtime.typeutils.InternalTypeInfo;
+import org.apache.flink.util.Collector;
+
+import static org.apache.flink.table.runtime.operators.deduplicate.DeduplicateFunctionHelper.checkInsertOnly;
+import static org.apache.flink.table.runtime.operators.deduplicate.DeduplicateFunctionHelper.shouldKeepCurrentRow;
+import static org.apache.flink.table.runtime.util.StateConfigUtil.createTtlConfig;
+
+/**
+ * This function is used to deduplicate on keys and keeps only first row on row time. It produces
+ * append only stream thanks to emitting results only via firing the timers.
+ */
+public class RowTimeDeduplicateKeepFirstRowFunction
+        extends KeyedProcessFunction<RowData, RowData, RowData> {
+
+    private static final long serialVersionUID = 1L;
+
+    // the TypeInformation of the values in the state.
+    private final TypeInformation<RowData> typeInfo;
+    private final long stateRetentionTime;
+    private final int rowtimeIndex;
+
+    // state stores previous message under the key.
+    protected ValueState<RowData> waitingToEmitOnTimerState;
+    protected ValueState<Boolean> alreadyEmittedState;
+
+    public RowTimeDeduplicateKeepFirstRowFunction(
+            InternalTypeInfo<RowData> typeInfo, long minRetentionTime, int rowtimeIndex) {
+        this.typeInfo = typeInfo;
+        this.stateRetentionTime = minRetentionTime;
+        this.rowtimeIndex = rowtimeIndex;
+    }
+
+    @Override
+    public void open(OpenContext openContext) throws Exception {
+        super.open(openContext);
+
+        // We don't enable TTL on the timer's state, because we rely on the state cleaning up on
+        // watermark. Also otherwise TTL clean up before firing the watermark would cause a data
+        // loss.
+        ValueStateDescriptor<RowData> timerStateDesc =
+                new ValueStateDescriptor<>("waiting-to-emit-on-timer", typeInfo);
+        waitingToEmitOnTimerState = getRuntimeContext().getState(timerStateDesc);
+
+        ValueStateDescriptor<Boolean> stateDesc =
+                new ValueStateDescriptor<>("already-emitted-state-boolean", Types.BOOLEAN);
+        StateTtlConfig ttlConfig = createTtlConfig(stateRetentionTime);
+        if (ttlConfig.isEnabled()) {
+            stateDesc.enableTimeToLive(ttlConfig);
+        }
+        alreadyEmittedState = getRuntimeContext().getState(stateDesc);
+    }
+
+    @Override
+    public void processElement(RowData input, Context ctx, Collector<RowData> out)
+            throws Exception {
+        checkInsertOnly(input);
+        Boolean allreadyEmitted = alreadyEmittedState.value();
+        if (allreadyEmitted != null && allreadyEmitted) {
+            // result has already been emitted, we can not retract/emit anything different.
+            return;
+        }
+        long rowtime = input.getLong(rowtimeIndex);
+        if (rowtime < ctx.timerService().currentWatermark()) {
+            // ignore late record
+            return;
+        }
+        RowData preRow = waitingToEmitOnTimerState.value();
+        if (shouldKeepCurrentRow(preRow, input, rowtimeIndex, false)) {
+            if (preRow != null) {
+                ctx.timerService().deleteEventTimeTimer(preRow.getLong(rowtimeIndex));
+            }
+            ctx.timerService().registerEventTimeTimer(rowtime);
+            waitingToEmitOnTimerState.update(input);
+        }
+    }
+
+    @Override
+    public void onTimer(
+            long timestamp,
+            KeyedProcessFunction<RowData, RowData, RowData>.OnTimerContext ctx,
+            Collector<RowData> out)
+            throws Exception {
+        out.collect(waitingToEmitOnTimerState.value());
+        waitingToEmitOnTimerState.clear();
+        alreadyEmittedState.update(true);
+    }
+}

flink-table/flink-table-runtime/src/test/java/org/apache/flink/table/runtime/operators/deduplicate/RowTimeDeduplicateKeepFirstRowFunctionTest.java
@@ -1,0 +1,119 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.runtime.operators.deduplicate;
+
+import org.apache.flink.runtime.checkpoint.OperatorSubtaskState;
+import org.apache.flink.streaming.api.operators.KeyedProcessOperator;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.util.OneInputStreamOperatorTestHarness;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.types.RowKind;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.flink.table.runtime.util.StreamRecordUtils.insertRecord;
+import static org.apache.flink.table.runtime.util.StreamRecordUtils.record;
+
+/** Harness tests for {@link RowTimeDeduplicateKeepFirstRowFunction}. */
+public class RowTimeDeduplicateKeepFirstRowFunctionTest extends RowTimeDeduplicateFunctionTestBase {
+
+    @Test
+    public void testRowTimeDeduplicateKeepFirstRow() throws Exception {
+        List<Object> expectedOutput = new ArrayList<>();
+        OneInputStreamOperatorTestHarness<RowData, RowData> testHarness =
+                createTestHarness(
+                        new KeyedProcessOperator<>(
+                                new RowTimeDeduplicateKeepFirstRowFunction(
+                                        inputRowType, minTtlTime.toMilliseconds(), rowTimeIndex)));
+
+        List<Object> actualOutput = new ArrayList<>();
+        testHarness.open();
+
+        testHarness.processWatermark(new Watermark(50));
+        // ignore late records
+        testHarness.processElement(insertRecord("key1", 0, 1L));
+        expectedOutput.add(new Watermark(50));
+
+        testHarness.processElement(insertRecord("key1", 14, 101L));
+        testHarness.processElement(insertRecord("key1", 13, 99L));
+        testHarness.processElement(insertRecord("key1", 15, 99L));
+        testHarness.processElement(insertRecord("key1", 12, 100L));
+        testHarness.processElement(insertRecord("key2", 11, 101L));
+
+        // test 1: keep first row with row time
+        testHarness.processWatermark(new Watermark(102));
+        actualOutput.addAll(testHarness.getOutput());
+        expectedOutput.add(record(RowKind.INSERT, "key1", 13, 99L));
+        expectedOutput.add(record(RowKind.INSERT, "key2", 11, 101L));
+        expectedOutput.add(new Watermark(102));
+
+        // do a snapshot, close and restore again
+        OperatorSubtaskState snapshot = testHarness.snapshot(0L, 0);
+        testHarness.close();
+
+        testHarness =
+                createTestHarness(
+                        new KeyedProcessOperator<>(
+                                new RowTimeDeduplicateKeepFirstRowFunction(
+                                        inputRowType, minTtlTime.toMilliseconds(), rowTimeIndex)));
+
+        testHarness.setup();
+        testHarness.initializeState(snapshot);
+        testHarness.open();
+
+        testHarness.processElement(insertRecord("key1", 12, 300L));
+        testHarness.processElement(insertRecord("key2", 11, 301L));
+        testHarness.processElement(insertRecord("key3", 5, 299L));
+
+        // test 2:  load snapshot state
+        testHarness.processWatermark(new Watermark(302));
+        expectedOutput.add(record(RowKind.INSERT, "key3", 5, 299L));
+        expectedOutput.add(new Watermark(302));
+
+        // test 3: expire the state
+        testHarness.setStateTtlProcessingTime(minTtlTime.toMilliseconds() + 1);
+        testHarness.processElement(insertRecord("key1", 12, 400L));
+        testHarness.processElement(insertRecord("key2", 11, 401L));
+        // previously emitted records have expired so new records should be emitted
+        testHarness.processWatermark(402);
+        expectedOutput.add(record(RowKind.INSERT, "key1", 12, 400L));
+        expectedOutput.add(record(RowKind.INSERT, "key2", 11, 401L));
+        expectedOutput.add(new Watermark(402));
+
+        // test 4: expire the state again to test ttl not losing records
+        testHarness.setStateTtlProcessingTime(2 * minTtlTime.toMilliseconds() + 1);
+        testHarness.processElement(insertRecord("key1", 22, 500L));
+        testHarness.processElement(insertRecord("key2", 21, 501L));
+        // we test that ttl doesn't expire records that are still in not-yet-fired timers
+        testHarness.setStateTtlProcessingTime(3 * minTtlTime.toMilliseconds() + 1);
+        // previously emitted records have expired so new records should be emitted
+        testHarness.processWatermark(502);
+        expectedOutput.add(record(RowKind.INSERT, "key1", 22, 500L));
+        expectedOutput.add(record(RowKind.INSERT, "key2", 21, 501L));
+        expectedOutput.add(new Watermark(502));
+
+        actualOutput.addAll(testHarness.getOutput());
+
+        assertor.assertOutputEqualsSorted("output wrong.", expectedOutput, actualOutput);
+        testHarness.close();
+    }
+}

apache/flink

flink-table/flink-table-runtime/src/main/java/org/apache/flink/table/runtime/operators/deduplicate/RowTimeDeduplicateKeepFirstRowFunction.java
@@ -1,0 +1,128 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.runtime.operators.deduplicate;
+
+import org.apache.flink.annotation.VisibleForTesting;
+import org.apache.flink.api.common.functions.OpenContext;
+import org.apache.flink.api.common.state.StateTtlConfig;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.metrics.Counter;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.runtime.typeutils.InternalTypeInfo;
+import org.apache.flink.util.Collector;
+
+import static org.apache.flink.table.runtime.operators.deduplicate.utils.DeduplicateFunctionHelper.checkInsertOnly;
+import static org.apache.flink.table.runtime.operators.deduplicate.utils.DeduplicateFunctionHelper.shouldKeepCurrentRow;
+import static org.apache.flink.table.runtime.operators.over.AbstractRowTimeUnboundedPrecedingOver.LATE_ELEMENTS_DROPPED_METRIC_NAME;
+import static org.apache.flink.table.runtime.util.StateConfigUtil.createTtlConfig;
+
+/**
+ * This function is used to deduplicate on keys and keeps only first row on row time. It produces
+ * append only stream thanks to emitting results only via firing the timers.
+ */
+public class RowTimeDeduplicateKeepFirstRowFunction
+        extends KeyedProcessFunction<RowData, RowData, RowData> {
+
+    private static final long serialVersionUID = 1L;
+
+    // the TypeInformation of the values in the state.
+    private final TypeInformation<RowData> typeInfo;
+    private final long stateRetentionTime;
+    private final int rowtimeIndex;
+
+    // state stores previous message under the key.
+    protected ValueState<RowData> waitingToEmitOnTimerState;
+    protected ValueState<Boolean> alreadyEmittedState;
+
+    private transient Counter numLateRecordsDropped;
+
+    public RowTimeDeduplicateKeepFirstRowFunction(
+            InternalTypeInfo<RowData> typeInfo, long minRetentionTime, int rowtimeIndex) {
+        this.typeInfo = typeInfo;
+        this.stateRetentionTime = minRetentionTime;
+        this.rowtimeIndex = rowtimeIndex;
+    }
+
+    @Override
+    public void open(OpenContext openContext) throws Exception {
+        super.open(openContext);
+
+        // We don't enable TTL on the timer's state, because we rely on the state cleaning up on
+        // watermark. Also otherwise TTL clean up before firing the watermark would cause a data
+        // loss.
+        ValueStateDescriptor<RowData> timerStateDesc =
+                new ValueStateDescriptor<>("waiting-to-emit-on-timer", typeInfo);
+        waitingToEmitOnTimerState = getRuntimeContext().getState(timerStateDesc);
+
+        ValueStateDescriptor<Boolean> stateDesc =
+                new ValueStateDescriptor<>("already-emitted-state-boolean", Types.BOOLEAN);
+        StateTtlConfig ttlConfig = createTtlConfig(stateRetentionTime);
+        if (ttlConfig.isEnabled()) {
+            stateDesc.enableTimeToLive(ttlConfig);
+        }
+        alreadyEmittedState = getRuntimeContext().getState(stateDesc);
+        numLateRecordsDropped =
+                getRuntimeContext().getMetricGroup().counter(LATE_ELEMENTS_DROPPED_METRIC_NAME);
+    }
+
+    @Override
+    public void processElement(RowData input, Context ctx, Collector<RowData> out)
+            throws Exception {
+        checkInsertOnly(input);
+        long rowtime = input.getLong(rowtimeIndex);
+        if (rowtime < ctx.timerService().currentWatermark()) {
+            // ignore late record
+            numLateRecordsDropped.inc();
+            return;
+        }
+        Boolean allreadyEmitted = alreadyEmittedState.value();
+        if (allreadyEmitted != null && allreadyEmitted) {
+            // result has already been emitted, we can not retract/emit anything different.
+            return;
+        }
+        RowData preRow = waitingToEmitOnTimerState.value();
+        if (shouldKeepCurrentRow(preRow, input, rowtimeIndex, false)) {
+            if (preRow != null) {
+                ctx.timerService().deleteEventTimeTimer(preRow.getLong(rowtimeIndex));
+            }
+            ctx.timerService().registerEventTimeTimer(rowtime);
+            waitingToEmitOnTimerState.update(input);
+        }
+    }
+
+    @Override
+    public void onTimer(
+            long timestamp,
+            KeyedProcessFunction<RowData, RowData, RowData>.OnTimerContext ctx,
+            Collector<RowData> out)
+            throws Exception {
+        out.collect(waitingToEmitOnTimerState.value());
+        waitingToEmitOnTimerState.clear();
+        alreadyEmittedState.update(true);
+    }
+
+    @VisibleForTesting
+    protected Counter getNumLateRecordsDropped() {
+        return numLateRecordsDropped;
+    }
+}

flink-table/flink-table-runtime/src/test/java/org/apache/flink/table/runtime/operators/deduplicate/RowTimeDeduplicateKeepFirstRowFunctionTest.java
@@ -1,0 +1,122 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.runtime.operators.deduplicate;
+
+import org.apache.flink.runtime.checkpoint.OperatorSubtaskState;
+import org.apache.flink.streaming.api.operators.KeyedProcessOperator;
+import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.util.OneInputStreamOperatorTestHarness;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.types.RowKind;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.flink.table.runtime.util.StreamRecordUtils.insertRecord;
+import static org.apache.flink.table.runtime.util.StreamRecordUtils.record;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/** Harness tests for {@link RowTimeDeduplicateKeepFirstRowFunction}. */
+public class RowTimeDeduplicateKeepFirstRowFunctionTest extends RowTimeDeduplicateFunctionTestBase {
+
+    @Test
+    public void testRowTimeDeduplicateKeepFirstRow() throws Exception {
+        List<Object> expectedOutput = new ArrayList<>();
+        RowTimeDeduplicateKeepFirstRowFunction deduplicateFunction =
+                new RowTimeDeduplicateKeepFirstRowFunction(
+                        inputRowType, minTtlTime.toMillis(), rowTimeIndex);
+        OneInputStreamOperatorTestHarness<RowData, RowData> testHarness =
+                createTestHarness(new KeyedProcessOperator<>(deduplicateFunction));
+
+        List<Object> actualOutput = new ArrayList<>();
+        testHarness.open();
+
+        testHarness.processWatermark(new Watermark(50));
+        // ignore late records
+        assertThat(deduplicateFunction.getNumLateRecordsDropped().getCount()).isEqualTo(0);
+        testHarness.processElement(insertRecord("key1", 0, 1L));
+        expectedOutput.add(new Watermark(50));
+        assertThat(deduplicateFunction.getNumLateRecordsDropped().getCount()).isEqualTo(1);
+
+        testHarness.processElement(insertRecord("key1", 14, 101L));
+        testHarness.processElement(insertRecord("key1", 13, 99L));
+        testHarness.processElement(insertRecord("key1", 15, 99L));
+        testHarness.processElement(insertRecord("key1", 12, 100L));
+        testHarness.processElement(insertRecord("key2", 11, 101L));
+
+        // test 1: keep first row with row time
+        testHarness.processWatermark(new Watermark(102));
+        actualOutput.addAll(testHarness.getOutput());
+        expectedOutput.add(record(RowKind.INSERT, "key1", 13, 99L));
+        expectedOutput.add(record(RowKind.INSERT, "key2", 11, 101L));
+        expectedOutput.add(new Watermark(102));
+        assertThat(deduplicateFunction.getNumLateRecordsDropped().getCount()).isEqualTo(1);
+
+        // do a snapshot, close and restore again
+        OperatorSubtaskState snapshot = testHarness.snapshot(0L, 0);
+        testHarness.close();
+
+        deduplicateFunction =
+                new RowTimeDeduplicateKeepFirstRowFunction(
+                        inputRowType, minTtlTime.toMillis(), rowTimeIndex);
+        testHarness = createTestHarness(new KeyedProcessOperator<>(deduplicateFunction));
+
+        testHarness.setup();
+        testHarness.initializeState(snapshot);
+        testHarness.open();
+
+        testHarness.processElement(insertRecord("key1", 12, 300L));
+        testHarness.processElement(insertRecord("key2", 11, 301L));
+        testHarness.processElement(insertRecord("key3", 5, 299L));
+
+        // test 2:  load snapshot state
+        testHarness.processWatermark(new Watermark(302));
+        expectedOutput.add(record(RowKind.INSERT, "key3", 5, 299L));
+        expectedOutput.add(new Watermark(302));
+
+        // test 3: expire the state
+        testHarness.setStateTtlProcessingTime(minTtlTime.toMillis() + 1);
+        testHarness.processElement(insertRecord("key1", 12, 400L));
+        testHarness.processElement(insertRecord("key2", 11, 401L));
+        // previously emitted records have expired so new records should be emitted
+        testHarness.processWatermark(402);
+        expectedOutput.add(record(RowKind.INSERT, "key1", 12, 400L));
+        expectedOutput.add(record(RowKind.INSERT, "key2", 11, 401L));
+        expectedOutput.add(new Watermark(402));
+
+        // test 4: expire the state again to test ttl not losing records
+        testHarness.setStateTtlProcessingTime(2 * minTtlTime.toMillis() + 1);
+        testHarness.processElement(insertRecord("key1", 22, 500L));
+        testHarness.processElement(insertRecord("key2", 21, 501L));
+        // we test that ttl doesn't expire records that are still in not-yet-fired timers
+        testHarness.setStateTtlProcessingTime(3 * minTtlTime.toMillis() + 1);
+        // previously emitted records have expired so new records should be emitted
+        testHarness.processWatermark(502);
+        expectedOutput.add(record(RowKind.INSERT, "key1", 22, 500L));
+        expectedOutput.add(record(RowKind.INSERT, "key2", 21, 501L));
+        expectedOutput.add(new Watermark(502));
+
+        actualOutput.addAll(testHarness.getOutput());
+
+        assertor.assertOutputEqualsSorted("output wrong.", expectedOutput, actualOutput);
+        testHarness.close();
+    }
+}