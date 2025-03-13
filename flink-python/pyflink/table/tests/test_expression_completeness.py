21d7a03f973b25769ae383c9f32a5d578859dd82
Mika Naylor <mnaylor@confluent.io>
2025-03-13 15:36:53 +0100

    [FLINK-37457][python] Support asArgument in Python Table API

--- flink-python/pyflink/table/tests/test_expression_completeness.py
+++ flink-python/pyflink/table/tests/test_expression_completeness.py
@@ -58,7 +58,6 @@ class ExpressionCompletenessTests(PythonAPICompletenessTestCase, PyFlinkTestCase
             'times',
             'mod',
             'power',
-            'asArgument',
         }
 
     @classmethod
