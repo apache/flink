65c72e2b5adaad1232201efd3da2798c9b026c96
lincoln lee <lincoln.86xy@gmail.com>
2025-03-14 18:44:49 +0800

    [FLINK-30687][table] Fix wrong result of agg with fiter which references first input column

--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/codegen/agg/AggsHandlerCodeGenerator.scala
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/codegen/agg/AggsHandlerCodeGenerator.scala
@@ -230,7 +230,7 @@ class AggsHandlerCodeGenerator(
     val aggCodeGens = aggInfoList.aggInfos.map {
       aggInfo =>
         val filterExpr =
-          createFilterExpression(aggInfo.agg.filterArg, aggInfo.aggIndex, aggInfo.agg.name)
+          createFilterExpression(aggInfo.agg.filterArg, aggInfo.agg.name)
 
         val codegen = aggInfo.function match {
           case _: DeclarativeAggregateFunction =>
@@ -282,9 +282,8 @@ class AggsHandlerCodeGenerator(
     val distinctCodeGens = aggInfoList.distinctInfos.zipWithIndex.map {
       case (distinctInfo, index) =>
         val innerCodeGens = distinctInfo.aggIndexes.map(aggCodeGens(_)).toArray
-        val distinctIndex = aggCodeGens.length + index
-        val filterExpr = distinctInfo.filterArgs.map(
-          createFilterExpression(_, distinctIndex, "distinct aggregate"))
+        val filterExpr =
+          distinctInfo.filterArgs.map(createFilterExpression(_, "distinct aggregate"))
         val codegen = new DistinctAggCodeGen(
           ctx,
           distinctInfo,
@@ -325,12 +324,9 @@ class AggsHandlerCodeGenerator(
   }
 
   /** Creates filter argument access expression, none if no filter */
-  private def createFilterExpression(
-      filterArg: Int,
-      aggIndex: Int,
-      aggName: String): Option[Expression] = {
+  private def createFilterExpression(filterArg: Int, aggName: String): Option[Expression] = {
 
-    if (filterArg > 0) {
+    if (filterArg >= 0) {
       val filterType = inputFieldTypes(filterArg)
       if (!filterType.isInstanceOf[BooleanType]) {
         throw new TableException(
