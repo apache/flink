c6a1f653de74d3493ead7802f9ac470bd3aea0a6
Julian Lawrence <julianlawrence@gmail.com>
2025-03-25 22:02:30 -0700

    fixed tests

--- flink-kubernetes/src/test/java/org/apache/flink/kubernetes/kubeclient/decorators/InternalServiceDecoratorTest.java
+++ flink-kubernetes/src/test/java/org/apache/flink/kubernetes/kubeclient/decorators/InternalServiceDecoratorTest.java
@@ -87,8 +87,9 @@ class InternalServiceDecoratorTest extends KubernetesJobManagerTestBase {
                                 .build());
         assertThat(internalService.getSpec().getPorts()).isEqualTo(expectedServicePorts);
 
-        expectedLabels.put(Constants.LABEL_COMPONENT_KEY, Constants.LABEL_COMPONENT_JOB_MANAGER);
-        assertThat(internalService.getSpec().getSelector()).isEqualTo(expectedLabels);
+        final Map<String, String> expectedSelectors = getCommonLabels();
+        expectedSelectors.put(Constants.LABEL_COMPONENT_KEY, Constants.LABEL_COMPONENT_JOB_MANAGER);
+        assertThat(internalService.getSpec().getSelector()).isEqualTo(expectedSelectors);
     }
 
     @Test
