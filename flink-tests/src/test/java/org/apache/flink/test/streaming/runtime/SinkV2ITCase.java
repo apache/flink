73a19f410311c86f64ed16484f01171d0eeae195
Arvid Heise <arvid@apache.org>
2025-04-13 10:10:58 +0200

    [FLINK-37605][runtime] Cleanup writer test
    Remove factory methods and InspectableSink because we don't need the abstraction anymore. Make test setup and assertions more explicit by using sink builder directly in tests.
    
    Remove unused methods.
    
    (cherry picked from commit 98284c4d24e61d12425c8d5f5fa17cfe40e816f9)

--- flink-tests/src/test/java/org/apache/flink/test/streaming/runtime/SinkV2ITCase.java
+++ flink-tests/src/test/java/org/apache/flink/test/streaming/runtime/SinkV2ITCase.java
@@ -18,159 +18,163 @@
 package org.apache.flink.test.streaming.runtime;
 
 import org.apache.flink.api.common.RuntimeExecutionMode;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.typeinfo.IntegerTypeInfo;
 import org.apache.flink.api.connector.sink2.Committer;
-import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.connector.source.Source;
+import org.apache.flink.api.connector.source.util.ratelimit.GatedRateLimiter;
+import org.apache.flink.api.connector.source.util.ratelimit.RateLimiter;
+import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
+import org.apache.flink.connector.datagen.source.DataGeneratorSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.runtime.operators.sink.TestSinkV2;
-import org.apache.flink.streaming.util.FiniteTestSource;
-import org.apache.flink.test.util.AbstractTestBaseJUnit4;
+import org.apache.flink.streaming.runtime.operators.sink.TestSinkV2.Record;
+import org.apache.flink.streaming.runtime.operators.sink.TestSinkV2.RecordSerializer;
+import org.apache.flink.test.util.AbstractTestBase;
+import org.apache.flink.testutils.junit.SharedObjectsExtension;
+import org.apache.flink.testutils.junit.SharedReference;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Queue;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.function.BooleanSupplier;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Integration test for {@link org.apache.flink.api.connector.sink2.Sink} run time implementation.
  */
-public class SinkV2ITCase extends AbstractTestBaseJUnit4 {
+public class SinkV2ITCase extends AbstractTestBase {
+    private static final Logger LOG = LoggerFactory.getLogger(SinkV2ITCase.class);
+
     static final List<Integer> SOURCE_DATA =
             Arrays.asList(
                     895, 127, 148, 161, 148, 662, 822, 491, 275, 122, 850, 630, 682, 765, 434, 970,
                     714, 795, 288, 422);
 
-    // source send data two times
-    static final int STREAMING_SOURCE_SEND_ELEMENTS_NUM = SOURCE_DATA.size() * 2;
-
-    static final List<String> EXPECTED_COMMITTED_DATA_IN_STREAMING_MODE =
+    static final List<Record<Integer>> EXPECTED_COMMITTED_DATA_IN_STREAMING_MODE =
             SOURCE_DATA.stream()
                     // source send data two times
                     .flatMap(
                             x ->
-                                    Collections.nCopies(
-                                            2, Tuple3.of(x, null, Long.MIN_VALUE).toString())
+                                    Collections.nCopies(2, new Record<>(x, null, Long.MIN_VALUE))
                                             .stream())
                     .collect(Collectors.toList());
 
-    static final List<String> EXPECTED_COMMITTED_DATA_IN_BATCH_MODE =
+    static final List<Record<Integer>> EXPECTED_COMMITTED_DATA_IN_BATCH_MODE =
             SOURCE_DATA.stream()
-                    .map(x -> Tuple3.of(x, null, Long.MIN_VALUE).toString())
+                    .map(x -> new Record<>(x, null, Long.MIN_VALUE))
                     .collect(Collectors.toList());
 
-    static final Queue<Committer.CommitRequest<String>> COMMIT_QUEUE =
-            new ConcurrentLinkedQueue<>();
-
-    static final BooleanSupplier COMMIT_QUEUE_RECEIVE_ALL_DATA =
-            (BooleanSupplier & Serializable)
-                    () -> COMMIT_QUEUE.size() == STREAMING_SOURCE_SEND_ELEMENTS_NUM;
-
-    @Before
-    public void init() {
-        COMMIT_QUEUE.clear();
-    }
+    @RegisterExtension
+    static final SharedObjectsExtension SHARED_OBJECTS = SharedObjectsExtension.create();
 
     @Test
     public void writerAndCommitterExecuteInStreamingMode() throws Exception {
         final StreamExecutionEnvironment env = buildStreamEnv();
-        final FiniteTestSource<Integer> source =
-                new FiniteTestSource<>(COMMIT_QUEUE_RECEIVE_ALL_DATA, SOURCE_DATA);
+        SharedReference<Queue<Committer.CommitRequest<Record<Integer>>>> committed =
+                SHARED_OBJECTS.add(new ConcurrentLinkedQueue<>());
+        final Source<Integer, ?, ?> source = createStreamingSource();
 
-        env.addSource(source, IntegerTypeInfo.INT_TYPE_INFO)
+        env.fromSource(source, WatermarkStrategy.noWatermarks(), "source")
                 .sinkTo(
                         TestSinkV2.<Integer>newBuilder()
-                                .setDefaultCommitter(
-                                        (Supplier<Queue<Committer.CommitRequest<String>>>
-                                                        & Serializable)
-                                                () -> COMMIT_QUEUE)
+                                .setCommitter(
+                                        new TrackingCommitter(committed), RecordSerializer::new)
                                 .build());
         env.execute();
-        assertThat(
-                COMMIT_QUEUE.stream()
-                        .map(Committer.CommitRequest::getCommittable)
-                        .collect(Collectors.toList()),
-                containsInAnyOrder(EXPECTED_COMMITTED_DATA_IN_STREAMING_MODE.toArray()));
+        assertThat(committed.get())
+                .extracting(Committer.CommitRequest::getCommittable)
+                .containsExactlyInAnyOrderElementsOf(EXPECTED_COMMITTED_DATA_IN_STREAMING_MODE);
     }
 
     @Test
     public void writerAndPrecommitToplogyAndCommitterExecuteInStreamingMode() throws Exception {
         final StreamExecutionEnvironment env = buildStreamEnv();
-        final FiniteTestSource<Integer> source =
-                new FiniteTestSource<>(COMMIT_QUEUE_RECEIVE_ALL_DATA, SOURCE_DATA);
+        SharedReference<Queue<Committer.CommitRequest<Record<Integer>>>> committed =
+                SHARED_OBJECTS.add(new ConcurrentLinkedQueue<>());
+        final Source<Integer, ?, ?> source = createStreamingSource();
 
-        env.addSource(source, IntegerTypeInfo.INT_TYPE_INFO)
+        env.fromSource(source, WatermarkStrategy.noWatermarks(), "source")
                 .sinkTo(
                         TestSinkV2.<Integer>newBuilder()
-                                .setDefaultCommitter(
-                                        (Supplier<Queue<Committer.CommitRequest<String>>>
-                                                        & Serializable)
-                                                () -> COMMIT_QUEUE)
-                                .setWithPreCommitTopology(true)
+                                .setCommitter(
+                                        new TrackingCommitter(committed), RecordSerializer::new)
+                                .setWithPreCommitTopology(SinkV2ITCase::flipValue)
                                 .build());
         env.execute();
-        assertThat(
-                COMMIT_QUEUE.stream()
-                        .map(Committer.CommitRequest::getCommittable)
-                        .collect(Collectors.toList()),
-                containsInAnyOrder(
+        assertThat(committed.get())
+                .extracting(Committer.CommitRequest::getCommittable)
+                .containsExactlyInAnyOrderElementsOf(
                         EXPECTED_COMMITTED_DATA_IN_STREAMING_MODE.stream()
-                                .map(s -> s + "Transformed")
-                                .toArray()));
+                                .map(SinkV2ITCase::flipValue)
+                                .collect(Collectors.toList()));
+    }
+
+    private static Record<Integer> flipValue(Record<Integer> r) {
+        return r.withValue(-r.getValue());
     }
 
     @Test
     public void writerAndCommitterExecuteInBatchMode() throws Exception {
         final StreamExecutionEnvironment env = buildBatchEnv();
+        SharedReference<Queue<Committer.CommitRequest<Record<Integer>>>> committed =
+                SHARED_OBJECTS.add(new ConcurrentLinkedQueue<>());
 
-        env.fromData(SOURCE_DATA)
+        final DataGeneratorSource<Integer> source =
+                new DataGeneratorSource<>(
+                        l -> SOURCE_DATA.get(l.intValue()),
+                        SOURCE_DATA.size(),
+                        IntegerTypeInfo.INT_TYPE_INFO);
+
+        env.fromSource(source, WatermarkStrategy.noWatermarks(), "source")
                 .sinkTo(
                         TestSinkV2.<Integer>newBuilder()
-                                .setDefaultCommitter(
-                                        (Supplier<Queue<Committer.CommitRequest<String>>>
-                                                        & Serializable)
-                                                () -> COMMIT_QUEUE)
+                                .setCommitter(
+                                        new TrackingCommitter(committed), RecordSerializer::new)
                                 .build());
         env.execute();
-        assertThat(
-                COMMIT_QUEUE.stream()
-                        .map(Committer.CommitRequest::getCommittable)
-                        .collect(Collectors.toList()),
-                containsInAnyOrder(EXPECTED_COMMITTED_DATA_IN_BATCH_MODE.toArray()));
+        assertThat(committed.get())
+                .extracting(Committer.CommitRequest::getCommittable)
+                .containsExactlyInAnyOrderElementsOf(EXPECTED_COMMITTED_DATA_IN_BATCH_MODE);
     }
 
     @Test
     public void writerAndPrecommitToplogyAndCommitterExecuteInBatchMode() throws Exception {
         final StreamExecutionEnvironment env = buildBatchEnv();
+        SharedReference<Queue<Committer.CommitRequest<Record<Integer>>>> committed =
+                SHARED_OBJECTS.add(new ConcurrentLinkedQueue<>());
 
-        env.fromData(SOURCE_DATA)
+        final DataGeneratorSource<Integer> source =
+                new DataGeneratorSource<>(
+                        l -> SOURCE_DATA.get(l.intValue()),
+                        SOURCE_DATA.size(),
+                        IntegerTypeInfo.INT_TYPE_INFO);
+
+        env.fromSource(source, WatermarkStrategy.noWatermarks(), "source")
                 .sinkTo(
                         TestSinkV2.<Integer>newBuilder()
-                                .setDefaultCommitter(
-                                        (Supplier<Queue<Committer.CommitRequest<String>>>
-                                                        & Serializable)
-                                                () -> COMMIT_QUEUE)
-                                .setWithPreCommitTopology(true)
+                                .setCommitter(
+                                        new TrackingCommitter(committed), RecordSerializer::new)
+                                .setWithPreCommitTopology(SinkV2ITCase::flipValue)
                                 .build());
         env.execute();
-        assertThat(
-                COMMIT_QUEUE.stream()
-                        .map(Committer.CommitRequest::getCommittable)
-                        .collect(Collectors.toList()),
-                containsInAnyOrder(
+        assertThat(committed.get())
+                .extracting(Committer.CommitRequest::getCommittable)
+                .containsExactlyInAnyOrderElementsOf(
                         EXPECTED_COMMITTED_DATA_IN_BATCH_MODE.stream()
-                                .map(s -> s + "Transformed")
-                                .toArray()));
+                                .map(SinkV2ITCase::flipValue)
+                                .collect(Collectors.toList()));
     }
 
     private StreamExecutionEnvironment buildStreamEnv() {
@@ -184,5 +188,61 @@ public class SinkV2ITCase extends AbstractTestBaseJUnit4 {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setRuntimeMode(RuntimeExecutionMode.BATCH);
         return env;
+    }
+
+    /**
+     * A stream source that: 1) emits a list of elements without allowing checkpoints, 2) then waits
+     * for two more checkpoints to complete, 3) then re-emits the same elements before 4) waiting
+     * for another two checkpoints and 5) exiting.
+     */
+    private Source<Integer, ?, ?> createStreamingSource() {
+        RateLimiterStrategy rateLimiterStrategy =
+                parallelism -> new BurstingRateLimiter(SOURCE_DATA.size() / 4, 2);
+        return new DataGeneratorSource<>(
+                l -> SOURCE_DATA.get(l.intValue() % SOURCE_DATA.size()),
+                SOURCE_DATA.size() * 2L,
+                rateLimiterStrategy,
+                IntegerTypeInfo.INT_TYPE_INFO);
+    }
+
+    private static class BurstingRateLimiter implements RateLimiter {
+        private final RateLimiter rateLimiter;
+        private final int numCheckpointCooldown;
+        private int cooldown;
+
+        public BurstingRateLimiter(int recordPerCycle, int numCheckpointCooldown) {
+            rateLimiter = new GatedRateLimiter(recordPerCycle);
+            this.numCheckpointCooldown = numCheckpointCooldown;
+        }
+
+        @Override
+        public CompletionStage<Void> acquire() {
+            CompletionStage<Void> stage = rateLimiter.acquire();
+            cooldown = numCheckpointCooldown;
+            return stage;
+        }
+
+        @Override
+        public void notifyCheckpointComplete(long checkpointId) {
+            if (cooldown-- <= 0) {
+                rateLimiter.notifyCheckpointComplete(checkpointId);
+            }
+        }
+    }
+
+    private static class TrackingCommitter implements Committer<Record<Integer>>, Serializable {
+        private final SharedReference<Queue<CommitRequest<Record<Integer>>>> committed;
+
+        public TrackingCommitter(SharedReference<Queue<CommitRequest<Record<Integer>>>> committed) {
+            this.committed = committed;
+        }
+
+        @Override
+        public void commit(Collection<CommitRequest<Record<Integer>>> committables) {
+            committed.get().addAll(committables);
+        }
+
+        @Override
+        public void close() {}
     }
 }
