6989c1ea1e30d30c1e83869fd868698e71458d6e
Hang Ruan <ruanhang1993@hotmail.com>
2025-04-22 17:32:44 +0800

    [hotfix] Use jdbc connector v3.3 in docs (#26494)

--- docs/setup_docs.sh
+++ docs/setup_docs.sh
@@ -48,7 +48,7 @@ integrate_connector_docs cassandra v3.2
 integrate_connector_docs elasticsearch v3.0
 integrate_connector_docs gcp-pubsub v3.1
 integrate_connector_docs hbase v4.0
-integrate_connector_docs jdbc v3.2
+integrate_connector_docs jdbc v3.3
 integrate_connector_docs kafka v3.3
 integrate_connector_docs mongodb v2.0
 integrate_connector_docs opensearch v1.2
