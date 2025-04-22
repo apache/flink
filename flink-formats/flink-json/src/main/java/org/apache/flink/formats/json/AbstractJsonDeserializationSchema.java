0f0a646ae3daa7e0d8960722d3afdda53998921e
lianghongjia <lianghongjia@didiglobal.com>
2025-04-22 18:56:10 +0800

    remove comments

--- flink-formats/flink-json/src/main/java/org/apache/flink/formats/json/AbstractJsonDeserializationSchema.java
+++ flink-formats/flink-json/src/main/java/org/apache/flink/formats/json/AbstractJsonDeserializationSchema.java
@@ -105,14 +105,6 @@ public abstract class AbstractJsonDeserializationSchema implements Deserializati
         collector = new ListCollector<>(reusableCollectList);
     }
 
-    /**
-     * NOTICE: We prefer to keep only {@link DeserializationSchema#deserialize(byte[], Collector)},
-     * however, {@link DeserializationSchema#deserialize(byte[])} is not deprecated now, hence it's
-     * better to keep {@link DeserializationSchema#deserialize(byte[])} usable until we remove
-     * {@link DeserializationSchema#deserialize(byte[])}.
-     *
-     * <p>Please do not change this method when you add new features!
-     */
     @Override
     public RowData deserialize(@Nullable byte[] message) throws IOException {
         reusableCollectList.clear();
