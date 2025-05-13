1d2193d1221f34121fbc123acb079bc2cfab7c7e
David Moravek <dmvk@apache.org>
2025-05-13 10:28:07 +0200

    [FLINK-37783] Auto-disable buffer debloating for hybrid shuffle.

--- flink-runtime/src/test/java/org/apache/flink/streaming/runtime/io/MockInputGate.java
+++ flink-runtime/src/test/java/org/apache/flink/streaming/runtime/io/MockInputGate.java
@@ -21,6 +21,7 @@ package org.apache.flink.streaming.runtime.io;
 import org.apache.flink.runtime.checkpoint.channel.InputChannelInfo;
 import org.apache.flink.runtime.event.TaskEvent;
 import org.apache.flink.runtime.io.network.api.EndOfPartitionEvent;
+import org.apache.flink.runtime.io.network.partition.ResultPartitionType;
 import org.apache.flink.runtime.io.network.partition.consumer.BufferOrEvent;
 import org.apache.flink.runtime.io.network.partition.consumer.IndexedInputGate;
 import org.apache.flink.runtime.io.network.partition.consumer.InputChannel;
@@ -167,6 +168,11 @@ public class MockInputGate extends IndexedInputGate {
         if (!blockedChannels.add(channelInfo.getInputChannelIdx())) {
             throw new IllegalArgumentException("Blocking the same channel multiple times");
         }
+    }
+
+    @Override
+    public ResultPartitionType getConsumedPartitionType() {
+        return ResultPartitionType.PIPELINED;
     }
 
     @Override
