3aae1b1ec30249a2c3c38baec12085246b67f14f
Martijn Visser <2989614+MartijnVisser@users.noreply.github.com>
2025-06-06 21:46:04 +0200

    [FLINK-33045] Add AvroConfluentITCase

---
+++ flink-end-to-end-tests/flink-confluent-schema-registry/src/test/java/org/apache/flink/schema/registry/test/SchemaLoader.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.schema.registry.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+/** Utility class for loading Avro schema files from resources. */
+public class SchemaLoader {
+
+    /**
+     * Load an Avro schema from a resource file.
+     *
+     * @param resourcePath Path to the .avsc file in resources
+     * @return Schema content as string
+     * @throws RuntimeException if the file cannot be loaded
+     */
+    public static String loadSchema(String resourcePath) {
+        try (InputStream inputStream =
+                SchemaLoader.class.getClassLoader().getResourceAsStream(resourcePath)) {
+            if (inputStream == null) {
+                throw new RuntimeException("Schema file not found: " + resourcePath);
+            }
+            return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load schema from: " + resourcePath, e);
+        }
+    }
+}
