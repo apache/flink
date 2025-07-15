33fa8a257795a6af6301c267021a124a7fb7fe23
lincoln-lil <lincoln.86xy@gmail.com>
2025-07-15 14:26:55 +0800

    trigger ci

--- flink-streaming-java/src/test/java/org/apache/flink/streaming/api/operators/async/AsyncWaitOperatorTest.java
+++ flink-streaming-java/src/test/java/org/apache/flink/streaming/api/operators/async/AsyncWaitOperatorTest.java
@@ -1493,7 +1493,7 @@ class AsyncWaitOperatorTest {
 
         @Override
         public void timeout(Integer input, ResultFuture<Integer> resultFuture) {
-            // simulate the case reported in https://issues.apache.org/jira/browse/FLINK-38082
+            // simulate the case in https://issues.apache.org/jira/browse/FLINK-38082
             resultFuture.completeExceptionally(new TimeoutException("Dummy timeout error"));
         }
     }
