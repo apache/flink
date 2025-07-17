15d328cedd53257182a86d3d9f1604df089eb946
Laffery <laffery86@gmail.com>
2025-07-17 16:15:31 +0800

    chore: update angular to v20

--- flink-runtime-web/web-dashboard/src/app/components/resize/resize.component.ts
+++ flink-runtime-web/web-dashboard/src/app/components/resize/resize.component.ts
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-import { DOCUMENT } from '@angular/common';
+
 import {
   ChangeDetectionStrategy,
   Component,
@@ -27,7 +27,8 @@ import {
   OnDestroy,
   OnInit,
   Output,
-  ViewChild
+  ViewChild,
+  DOCUMENT
 } from '@angular/core';
 import { fromEvent, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
