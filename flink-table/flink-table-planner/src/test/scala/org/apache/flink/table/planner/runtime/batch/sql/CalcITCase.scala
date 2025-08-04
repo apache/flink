b563e75739a4cee9f867c058dd8b415156cd8769
dylanhz <dylanhz@163.com>
2025-08-04 10:35:31 +0800

    Revert incorrect code formatting

--- flink-table/flink-table-planner/src/test/scala/org/apache/flink/table/planner/runtime/batch/sql/CalcITCase.scala
+++ flink-table/flink-table-planner/src/test/scala/org/apache/flink/table/planner/runtime/batch/sql/CalcITCase.scala
@@ -857,22 +857,22 @@ class CalcITCase extends BatchTestBase {
   }
 
   // TODO
-  //  @Test
-  //  def testUDFWithGetResultTypeFromLiteral(): Unit = {
-  //    registerFunction("hashCode0", LiteralHashCode)
-  //    registerFunction("hashCode1", LiteralHashCode)
-  //    val data = Seq(row("a"), row("b"), row("c"))
-  //    tEnv.registerCollection("MyTable", data, new RowTypeInfo(STRING_TYPE_INFO), "text")
-  //    checkResult(
-  //      "SELECT hashCode0(text, 'int') FROM MyTable",
-  //      Seq(row(97), row(98), row(99)
-  //      ))
-  //
-  //    checkResult(
-  //      "SELECT hashCode1(text, 'string') FROM MyTable",
-  //      Seq(row("str97"), row("str98"), row("str99")
-  //      ))
-  //  }
+//  @Test
+//  def testUDFWithGetResultTypeFromLiteral(): Unit = {
+//    registerFunction("hashCode0", LiteralHashCode)
+//    registerFunction("hashCode1", LiteralHashCode)
+//    val data = Seq(row("a"), row("b"), row("c"))
+//    tEnv.registerCollection("MyTable", data, new RowTypeInfo(STRING_TYPE_INFO), "text")
+//    checkResult(
+//      "SELECT hashCode0(text, 'int') FROM MyTable",
+//      Seq(row(97), row(98), row(99)
+//      ))
+//
+//    checkResult(
+//      "SELECT hashCode1(text, 'string') FROM MyTable",
+//      Seq(row("str97"), row("str98"), row("str99")
+//      ))
+//  }
 
   @Test
   def testInNonConstantValue(): Unit = {
@@ -1692,10 +1692,9 @@ class CalcITCase extends BatchTestBase {
 
     tEnv.createTemporaryView("myTable", source)
 
-    val query =
-      """
-        |select * from myTable where f0 in (1.0, 2.0, 3.0)
-        |""".stripMargin
+    val query = """
+                  |select * from myTable where f0 in (1.0, 2.0, 3.0)
+                  |""".stripMargin
 
     checkResult(
       query,
