78c3f4e60f60ce3fe4502aa0057633483fbccb8d
Pan Yuepeng <panyuepeng@apache.org>
2025-09-26 10:22:55 +0800

    Address David's comments.

--- flink-runtime-web/src/test/java/org/apache/flink/runtime/webmonitor/history/retaining/CompositeJobRetainedStrategyTest.java
+++ flink-runtime-web/src/test/java/org/apache/flink/runtime/webmonitor/history/retaining/CompositeJobRetainedStrategyTest.java
@@ -19,6 +19,7 @@
 package org.apache.flink.runtime.webmonitor.history.retaining;
 
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.IllegalConfigurationException;
 import org.apache.flink.core.fs.FileStatus;
 import org.apache.flink.core.fs.Path;
 
@@ -30,14 +31,26 @@ import java.time.Instant;
 import static org.apache.flink.configuration.HistoryServerOptions.HISTORY_SERVER_RETAINED_JOBS;
 import static org.apache.flink.configuration.HistoryServerOptions.HISTORY_SERVER_RETAINED_TTL;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /** Testing for {@link CompositeJobRetainedStrategy}. */
 class CompositeJobRetainedStrategyTest {
 
     @Test
     void testTimeToLiveBasedJobRetainedStrategy() {
-        // Test the case where no specific retention policy is configured, i.e., all archived files are retained.
-        Configuration conf = new Configuration();
+        final Configuration conf = new Configuration();
+
+        // Test for invalid option value.
+        conf.set(HISTORY_SERVER_RETAINED_TTL, Duration.ZERO);
+        assertThatThrownBy(() -> CompositeJobRetainedStrategy.createFrom(conf))
+                .isInstanceOf(IllegalConfigurationException.class);
+        // Skipped for option value that is less than 0 milliseconds, which will throw a
+        // java.lang.NumberFormatException caused by TimeUtils.
+
+        conf.removeConfig(HISTORY_SERVER_RETAINED_TTL);
+
+        // Test the case where no specific retention policy is configured, i.e., all archived files
+        // are retained.
         JobRetainedStrategy strategy = CompositeJobRetainedStrategy.createFrom(conf);
         assertThat(strategy.shouldRetain(new TestingFileStatus(), 1)).isTrue();
         assertThat(
@@ -48,7 +61,7 @@ class CompositeJobRetainedStrategyTest {
                                 1))
                 .isTrue();
 
-        // Test the case where TTL-based retention policies is specified.
+        // Test the case where TTL-based retention policies is specified only.
         conf.set(HISTORY_SERVER_RETAINED_TTL, Duration.ofMinutes(1L));
         strategy = CompositeJobRetainedStrategy.createFrom(conf);
         assertThat(strategy.shouldRetain(new TestingFileStatus(), 1)).isTrue();
@@ -63,14 +76,25 @@ class CompositeJobRetainedStrategyTest {
 
     @Test
     void testQuantityBasedJobRetainedStrategy() {
-        Configuration conf = new Configuration();
+        final Configuration conf = new Configuration();
 
-        // Test the case where no specific retention policy is configured, i.e., all archived files are retained.
+        // Test for invalid option value.
+        conf.set(HISTORY_SERVER_RETAINED_JOBS, 0);
+        assertThatThrownBy(() -> CompositeJobRetainedStrategy.createFrom(conf))
+                .isInstanceOf(IllegalConfigurationException.class);
+        conf.set(HISTORY_SERVER_RETAINED_JOBS, -2);
+        assertThatThrownBy(() -> CompositeJobRetainedStrategy.createFrom(conf))
+                .isInstanceOf(IllegalConfigurationException.class);
+
+        conf.removeConfig(HISTORY_SERVER_RETAINED_JOBS);
+
+        // Test the case where no specific retention policy is configured, i.e., all archived files
+        // are retained.
         JobRetainedStrategy strategy = CompositeJobRetainedStrategy.createFrom(conf);
         assertThat(strategy.shouldRetain(new TestingFileStatus(), 1)).isTrue();
         assertThat(strategy.shouldRetain(new TestingFileStatus(), 3)).isTrue();
 
-        // Test the case where QUANTITY-based retention policies is specified.
+        // Test the case where QUANTITY-based retention policies is specified only.
         conf.set(HISTORY_SERVER_RETAINED_JOBS, 2);
         strategy = CompositeJobRetainedStrategy.createFrom(conf);
         assertThat(strategy.shouldRetain(new TestingFileStatus(), 1)).isTrue();
@@ -83,7 +107,8 @@ class CompositeJobRetainedStrategyTest {
         final long outOfTtlMillis =
                 Instant.now().toEpochMilli() - Duration.ofMinutes(2L).toMillis();
 
-        // Test the case where no specific retention policy is configured, i.e., all archived files are retained.
+        // Test the case where no specific retention policy is configured, i.e., all archived files
+        // are retained.
         final Configuration conf = new Configuration();
         JobRetainedStrategy strategy = CompositeJobRetainedStrategy.createFrom(conf);
         assertThat(strategy.shouldRetain(new TestingFileStatus(outOfTtlMillis), 1)).isTrue();
