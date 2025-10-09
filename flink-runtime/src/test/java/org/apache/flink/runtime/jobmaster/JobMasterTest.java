583d9ce794e7505e4586c81b0800b42b1e1c590e
Mingliang Liu <liuml07@apache.org>
2025-10-08 17:37:03 -0700

    [FLINK-38404][core] Fix JobMasterTest due to data race

--- flink-runtime/src/test/java/org/apache/flink/runtime/jobmaster/JobMasterTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/jobmaster/JobMasterTest.java
@@ -1706,7 +1706,7 @@ class JobMasterTest {
                                                         .equals(event.getName()))
                                 .map(Event::getAttributes)
                                 .map(x -> x.get("newJobStatus")))
-                .containsExactly(
+                .containsSubsequence(
                         JobStatus.RUNNING.toString(),
                         JobStatus.FAILING.toString(),
                         JobStatus.FAILED.toString());
@@ -1747,7 +1747,7 @@ class JobMasterTest {
                                                         .equals(event.getName()))
                                 .map(Event::getAttributes)
                                 .map(x -> x.get("newJobStatus")))
-                .containsExactly(
+                .containsSubsequence(
                         JobStatus.RUNNING.toString(),
                         JobStatus.FAILING.toString(),
                         JobStatus.FAILED.toString());
