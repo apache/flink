bab62929f6f49f1a74c7e4c621e6ab3daf404d13
Roman Khachatryan <khachatryan.roman@gmail.com>
2025-10-10 14:37:38 +0000

    fixup: SequencedMultiSetStateTest minor update

--- flink-table/flink-table-runtime/src/test/java/org/apache/flink/table/runtime/sequencedmultisetstate/SequencedMultiSetStateTest.java
+++ flink-table/flink-table-runtime/src/test/java/org/apache/flink/table/runtime/sequencedmultisetstate/SequencedMultiSetStateTest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.table.runtime.sequencedmultisetstate;
 
 import org.apache.flink.api.common.JobID;
@@ -116,7 +134,7 @@ public class SequencedMultiSetStateTest {
 
                     keyContext.setCurrentKey("sk1");
                     state.clear();
-                    assertTrue(state.isEmpty());
+                    assertStateContents(state);
                 });
     }
 
@@ -158,14 +176,23 @@ public class SequencedMultiSetStateTest {
                     state.add(row("k1", "x"), 777L);
                     assertStateContents(state, row("k1", "x"), 777L);
 
-                    state.add(row("k1", "y"), 778L);
-                    assertStateContents(state, row("k1", "y"), 778L);
+                    state.add(row("k2", "x"), 777L);
+                    assertStateContents(
+                        state,
+                        Tuple2.of(row("k1", "x"), 777L),
+                        Tuple2.of(row("k2", "x"), 777L));
 
                     state.add(row("k2", "y"), 778L);
                     assertStateContents(
-                            state,
-                            Tuple2.of(row("k1", "y"), 778L),
-                            Tuple2.of(row("k2", "y"), 778L));
+                        state,
+                        Tuple2.of(row("k1", "x"), 777L),
+                        Tuple2.of(row("k2", "y"), 778L));
+
+                    state.add(row("k1", "y"), 778L);
+                    assertStateContents(
+                        state,
+                        Tuple2.of(row("k1", "y"), 778L),
+                        Tuple2.of(row("k2", "y"), 778L));
                 });
     }
 
