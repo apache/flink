dc9196192629ec3f5bc1e9f10abfb62d5d0fa51d
Sergey Nuyanzin <snuyanzin@gmail.com>
2025-12-03 01:32:58 +0100

    Update line numbers

--- flink-table/flink-table-planner/src/main/java/org/apache/calcite/sql/validate/SqlValidatorImpl.java
+++ flink-table/flink-table-planner/src/main/java/org/apache/calcite/sql/validate/SqlValidatorImpl.java
@@ -169,22 +169,23 @@ import static org.apache.calcite.util.Util.first;
  * Default implementation of {@link SqlValidator}, the class was copied over because of
  * CALCITE-4554.
  *
- * <p>Lines 206 ~ 209, Flink improves error message for functions without appropriate arguments in
+ * <p>Lines 207 ~ 210, Flink improves error message for functions without appropriate arguments in
  * handleUnresolvedFunction.
  *
- * <p>Lines 1274 ~ 1276, CALCITE-7217, should be removed after upgrading Calcite to 1.41.0.
+ * <p>Lines 1275 ~ 1277, CALCITE-7217, should be removed after upgrading Calcite to 1.41.0.
  *
- * <p>Lines 2035 ~ 2049, Flink improves error message for functions without appropriate arguments in
+ * <p>Lines 2036 ~ 2050, Flink improves error message for functions without appropriate arguments in
  * handleUnresolvedFunction at {@link SqlValidatorImpl#handleUnresolvedFunction}.
  *
- * <p>Lines 2575 ~ 2594, CALCITE-7217, should be removed after upgrading Calcite to 1.41.0.
+ * <p>Lines 2576 ~ 2595, CALCITE-7217, CALCITE-7312 should be removed after upgrading Calcite to
+ * 1.42.0.
  *
- * <p>Line 2625 ~2643, set the correct scope for VECTOR_SEARCH.
+ * <p>Line 2626 ~2644, set the correct scope for VECTOR_SEARCH.
  *
- * <p>Lines 3922 ~ 3926, 6601 ~ 6607 Flink improves Optimize the retrieval of sub-operands in
+ * <p>Lines 3923 ~ 3927, 6602 ~ 6608 Flink improves Optimize the retrieval of sub-operands in
  * SqlCall when using NamedParameters at {@link SqlValidatorImpl#checkRollUp}.
  *
- * <p>Lines 5342 ~ 5348, FLINK-24352 Add null check for temporal table check on SqlSnapshot.
+ * <p>Lines 5343 ~ 5349, FLINK-24352 Add null check for temporal table check on SqlSnapshot.
  */
 public class SqlValidatorImpl implements SqlValidatorWithHints {
     // ~ Static fields/initializers ---------------------------------------------
