8e1e7c92a40e81c409667802e7735e21e47c10d8
Sergey Nuyanzin <snuyanzin@gmail.com>
2026-01-11 19:06:02 +0100

    [hotfix][javadoc] Skip `skip-webui-build` while `javadoc:aggregate`

--- tools/ci/compile.sh
+++ tools/ci/compile.sh
@@ -86,7 +86,7 @@ javadoc_output=/tmp/javadoc.out
 # use the same invocation as .github/workflows/docs.sh
 $MVN javadoc:aggregate -DadditionalJOption='-Xdoclint:none' \
       -Dmaven.javadoc.failOnError=false -Dcheckstyle.skip=true -Denforcer.skip=true -Dspotless.skip=true -Drat.skip=true \
-      -Dheader=someTestHeader > ${javadoc_output}
+      -Dheader=someTestHeader -Pskip-webui-build > ${javadoc_output}
 EXIT_CODE=$?
 if [ $EXIT_CODE != 0 ] ; then
   echo "ERROR in Javadocs. Printing full output:"
