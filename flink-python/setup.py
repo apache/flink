175de63c3a756548c0fde6679122c0b935dfe893
Xuannan <suxuannan95@gmail.com>
2026-01-21 09:36:05 +0800

    [FLINK-38951][python] Bump pemja to 0.5.6

--- flink-python/setup.py
+++ flink-python/setup.py
@@ -325,7 +325,7 @@ try:
                         'numpy>=1.22.4',
                         'pandas>=1.3.0,<2.3',  # FLINK-38513: 2.3+ drops cp39 wheels
                         'pyarrow>=5.0.0,<21.0.0',
-                        'pemja>=0.5.5,<0.5.6;platform_system != "Windows"',
+                        'pemja>=0.5.6,<0.5.7;platform_system != "Windows"',
                         'httplib2>=0.19.0',
                         'ruamel.yaml>=0.18.4',
                         apache_flink_libraries_dependency]
