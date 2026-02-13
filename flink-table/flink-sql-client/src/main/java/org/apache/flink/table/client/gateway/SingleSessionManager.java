ed1450a48fd67fc24b86c21cd3d565cbb9a8f112
Mika Naylor <mnaylor@confluent.io>
2026-02-13 14:28:50 +0100

    [FLINK-39081] Add ARTIFACT keyword option in CREATE FUNCTION's USING clause

--- flink-table/flink-sql-client/src/main/java/org/apache/flink/table/client/gateway/SingleSessionManager.java
+++ flink-table/flink-sql-client/src/main/java/org/apache/flink/table/client/gateway/SingleSessionManager.java
@@ -188,7 +188,7 @@ public class SingleSessionManager implements SessionManager {
             ClientResourceManager resourceManager =
                     new ClientResourceManager(configuration, userClassLoader);
             try {
-                resourceManager.registerJarResources(
+                resourceManager.registerResources(
                         dependencies.stream()
                                 .map(uri -> new ResourceUri(ResourceType.JAR, uri.toString()))
                                 .collect(Collectors.toList()));
@@ -221,7 +221,8 @@ public class SingleSessionManager implements SessionManager {
         protected ResultFetcher callRemoveJar(OperationHandle operationHandle, String jarPath) {
             URL jarURL =
                     ((ClientResourceManager) sessionContext.getSessionState().resourceManager)
-                            .unregisterJarResource(jarPath);
+                            .unregisterResource(
+                                    jarPath, List.of(ResourceType.JAR, ResourceType.ARTIFACT));
             if (jarURL != null) {
                 ((ClientWrapperClassLoader)
                                 sessionContext
