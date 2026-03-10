d20a126a2dd8dc9a3edd262b2e5f1ebcbc26e3df
Taranpreet Kaur <hundaltanu1997@gmail.com>
2026-03-10 21:58:36 +0530

    tests in favor of Kafka-KRaft

--- flink-end-to-end-tests/flink-sql-client-test/src/test/java/SqlClientITCase.java
+++ flink-end-to-end-tests/flink-sql-client-test/src/test/java/SqlClientITCase.java
@@ -81,6 +81,7 @@ public class SqlClientITCase {
     @Container
     public static final KafkaContainer KAFKA =
             new KafkaContainer(DockerImageName.parse(DockerImageVersions.KAFKA))
+                    .withKraft()
                     .withNetwork(NETWORK)
                     .withNetworkAliases(INTER_CONTAINER_KAFKA_ALIAS)
                     .withLogConsumer(LOG_CONSUMER);
