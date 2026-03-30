d731d5da39d14ff32c31de8b3009bb0d3ee846b7
Timo Walther <twalthr@apache.org>
2026-03-30 16:16:56 +0200

    [FLINK-39355][table-planner] Fix field names in code generation for JSON_OBJECT/ARRAY/STRING

--- flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/codegen/JsonGenerateUtils.scala
+++ flink-table/flink-table-planner/src/main/scala/org/apache/flink/table/planner/codegen/JsonGenerateUtils.scala
@@ -31,6 +31,7 @@ import org.apache.flink.table.runtime.typeutils.TypeCheckUtils.isCharacterString
 import org.apache.flink.table.types.logical._
 import org.apache.flink.table.types.logical.LogicalTypeRoot._
 import org.apache.flink.table.types.logical.utils.LogicalTypeChecks
+import org.apache.flink.table.utils.EncodingUtils
 
 import org.apache.calcite.rex.{RexCall, RexNode}
 
@@ -264,7 +265,7 @@ object JsonGenerateUtils {
 
   /** Generates a method to convert rows into [[ObjectNode]]. */
   private def generateRowConverter(ctx: CodeGeneratorContext, rowType: LogicalType): String = {
-    val fieldNames = toScala(LogicalTypeChecks.getFieldNames(rowType))
+    val fieldNames = toScala(LogicalTypeChecks.getFieldNames(rowType)).map(EncodingUtils.escapeJava)
     val fieldTypes = toScala(LogicalTypeChecks.getFieldTypes(rowType))
 
     val populateObjectCode = fieldNames.zipWithIndex.map {
