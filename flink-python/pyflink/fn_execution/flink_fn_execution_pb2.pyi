576dc23dd891daed4ceb1d4c04cd41bb10946b40
florian <florian@harfanglab.fr>
2026-05-04 11:56:43 +0200

    chore(deps): update dependency versions for grpcio and protobuf in flink-python

--- flink-python/pyflink/fn_execution/flink_fn_execution_pb2.pyi
+++ flink-python/pyflink/fn_execution/flink_fn_execution_pb2.pyi
@@ -19,7 +19,8 @@ from google.protobuf.internal import containers as _containers
 from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+from collections.abc import Iterable as _Iterable, Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
