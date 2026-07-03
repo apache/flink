4c65cd3bda0f068df755122514f839354a48227b
Martijn Visser <2989614+MartijnVisser@users.noreply.github.com>
2026-07-03 22:16:24 +0200

    [FLINK-33045][e2e] Replace disabled bash-based schema registry test with AvroConfluentITCase

---
+++ flink-end-to-end-tests/flink-confluent-schema-registry/src/test/resources/avro_confluent_roundtrip_e2e.sql
@@ -0,0 +1,61 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+SET 'table.dml-sync' = 'true';
+
+CREATE TABLE avro_input (
+  name STRING,
+  favoriteNumber STRING,
+  favoriteColor STRING,
+  eventType STRING
+) WITH (
+  'connector' = 'kafka',
+  'topic' = '$INPUT_TOPIC',
+  'properties.bootstrap.servers' = '$BOOTSTRAP_SERVERS',
+  'properties.group.id' = 'test-group-$INPUT_TOPIC',
+  'scan.startup.mode' = 'earliest-offset',
+  'scan.bounded.mode' = 'latest-offset',
+  'format' = 'avro-confluent',
+  'avro-confluent.url' = '$SCHEMA_REGISTRY_URL',
+  'avro-confluent.auto.register.schemas' = '$AUTO_REGISTER_SCHEMAS'
+);
+
+CREATE TABLE avro_output (
+  name STRING,
+  favoriteNumber STRING,
+  favoriteColor STRING,
+  eventType STRING
+) WITH (
+  'connector' = 'kafka',
+  'topic' = '$OUTPUT_TOPIC',
+  'properties.bootstrap.servers' = '$BOOTSTRAP_SERVERS',
+  'properties.group.id' = 'test-group-$OUTPUT_TOPIC',
+  'scan.startup.mode' = 'earliest-offset',
+  'scan.bounded.mode' = 'latest-offset',
+  'format' = 'avro-confluent',
+  'avro-confluent.url' = '$SCHEMA_REGISTRY_URL',
+  'avro-confluent.auto.register.schemas' = '$AUTO_REGISTER_SCHEMAS'
+);
+
+INSERT INTO avro_input VALUES
+  ('Alice', '42', 'blue', 'INSERT'),
+  ('Bob', '7', 'red', 'INSERT'),
+  ('Charlie', '73', 'green', 'INSERT');
+
+INSERT INTO avro_output
+SELECT * FROM avro_input;
