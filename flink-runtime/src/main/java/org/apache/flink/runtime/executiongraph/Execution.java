b8c0969b679e15fda4ad6f7de08c555fa236a41e
Matthias Pohl <mpohl@confluent.io>
2026-07-09 16:38:01 +0200

    [FLINK-40086][runtime] Adds error handling to newly added preCompletionAction

--- flink-runtime/src/main/java/org/apache/flink/runtime/executiongraph/Execution.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/executiongraph/Execution.java
@@ -1708,7 +1708,11 @@ public class Execution
                 initializingOrRunningFuture.complete(null);
             } else if (targetState.isTerminal()) {
                 if (preCompletionAction != null) {
-                    preCompletionAction.run();
+                    try {
+                        preCompletionAction.run();
+                    } catch (Exception e) {
+                        LOG.error("Error while executing pre-completion action.", e);
+                    }
                 }
                 // complete the terminal state future
                 terminalStateFuture.complete(targetState);
