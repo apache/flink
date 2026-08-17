bceae33f69ea4f093fb09996d53174f53077d422
Rui Fan <1996fanrui@gmail.com>
2026-08-17 11:03:44 +0200

    enable checkpointing during recovery

--- flink-test-utils-parent/flink-test-utils/src/main/java/org/apache/flink/streaming/util/TestStreamEnvironment.java
+++ flink-test-utils-parent/flink-test-utils/src/main/java/org/apache/flink/streaming/util/TestStreamEnvironment.java
@@ -145,11 +145,9 @@ public class TestStreamEnvironment extends StreamExecutionEnvironment {
     private static void randomizeConfiguration(MiniCluster miniCluster, Configuration conf) {
         // randomize ITTests for enabling unaligned checkpoint
         if (RANDOMIZE_CHECKPOINTING_CONFIG) {
-            randomize(conf, CheckpointingOptions.ENABLE_UNALIGNED, true, false);
-            randomize(
-                    conf, CheckpointingOptions.UNALIGNED_RECOVER_OUTPUT_ON_DOWNSTREAM, true, false);
-            randomize(
-                    conf, CheckpointingOptions.CHECKPOINTING_DURING_RECOVERY_ENABLED, true, false);
+            randomize(conf, CheckpointingOptions.ENABLE_UNALIGNED, true);
+            randomize(conf, CheckpointingOptions.UNALIGNED_RECOVER_OUTPUT_ON_DOWNSTREAM, true);
+            randomize(conf, CheckpointingOptions.CHECKPOINTING_DURING_RECOVERY_ENABLED, true);
             randomize(
                     conf,
                     CheckpointingOptions.ALIGNED_CHECKPOINT_TIMEOUT,
