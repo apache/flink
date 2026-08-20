daeebe2bf5c514fd9fec517d7692707029da83fd
Rui Fan <1996fanrui@gmail.com>
2026-08-20 14:26:47 +0200

    [FLINK-40446][tests] Make CommonTestUtils.terminateJob wait for the CANCELED terminal state
    JobClient#cancel().get() only acknowledges that cancellation was initiated, not
    that the job has reached a terminal state. Tests that cancel a job and then let
    JUnit delete its checkpoint directory (a @TempDir) can therefore race with the
    source subtasks' async snapshot of the last completed checkpoint, which keeps
    writing SourceReaderState files into the file-merging taskowned directory after
    cancel() returns. This shows up as a flaky
    UnalignedCheckpointRescaleWithMixedExchangesITCase teardown failure:
    "Failed to delete temp directory ... DirectoryNotEmptyException".
    
    Make CommonTestUtils.terminateJob wait for the CANCELED terminal state after
    cancel(), and route the UC rescale ITCases and the connector test suites
    (which previously duplicated this wait in a local killJob helper) through it.
    
    (cherry picked from commit 9c1015353f8735bf28b76d01db544514fab60c87)

--- flink-test-utils-parent/flink-connector-test-utils/src/main/java/org/apache/flink/connector/testframe/testsuites/SinkTestSuiteBase.java
+++ flink-test-utils-parent/flink-connector-test-utils/src/main/java/org/apache/flink/connector/testframe/testsuites/SinkTestSuiteBase.java
@@ -291,7 +291,7 @@ public abstract class SinkTestSuiteBase<T extends Comparable<T>> {
             waitForJobStatus(jobClient, Collections.singletonList(JobStatus.FINISHED));
         } catch (Exception e) {
             executorService.shutdown();
-            killJob(jobClient);
+            terminateJob(jobClient);
             throw e;
         }
 
@@ -330,7 +330,7 @@ public abstract class SinkTestSuiteBase<T extends Comparable<T>> {
                     externalContext.createSinkDataReader(sinkSettings), testRecords, semantic);
         } finally {
             executorService.shutdown();
-            killJob(restartJobClient);
+            terminateJob(restartJobClient);
             iterator.close();
         }
     }
@@ -408,7 +408,7 @@ public abstract class SinkTestSuiteBase<T extends Comparable<T>> {
         } finally {
             // Clean up
             executorService.shutdown();
-            killJob(jobClient);
+            terminateJob(jobClient);
         }
     }
 
@@ -563,11 +563,6 @@ public abstract class SinkTestSuiteBase<T extends Comparable<T>> {
 
     private TestingSinkSettings getTestingSinkSettings(CheckpointingMode checkpointingMode) {
         return TestingSinkSettings.builder().setCheckpointingMode(checkpointingMode).build();
-    }
-
-    private void killJob(JobClient jobClient) throws Exception {
-        terminateJob(jobClient);
-        waitForJobStatus(jobClient, Collections.singletonList(JobStatus.CANCELED));
     }
 
     private DataStreamSink<T> tryCreateSink(
