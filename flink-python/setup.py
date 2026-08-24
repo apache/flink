aeab637c60dbd3c9fc3393d19f9da8fb12c95f69
florian <florian@harfanglab.fr>
2026-08-24 17:12:50 +0200

    ci: build wheels for python 3.13

--- flink-python/setup.py
+++ flink-python/setup.py
@@ -357,7 +357,8 @@ try:
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
             'Programming Language :: Python :: 3.11',
-            'Programming Language :: Python :: 3.12'],
+            'Programming Language :: Python :: 3.12',
+            'Programming Language :: Python :: 3.13'],
         ext_modules=extensions
     )
 finally:
